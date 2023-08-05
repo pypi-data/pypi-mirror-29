e0a4f8c843b113b93bc26858cb39038b0a4bfc53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:19 +0000

    Add package squad/0.38/squad-0.38.tar.gz

---
+++ packages/squad/squad-0.38.tar.gz/squad-0.38/test/settings.py
@@ -0,0 +1,14 @@
+from squad.settings import *  # noqa
+import logging
+
+
+LOGGING['loggers']['django']['level'] = 999  # noqa
+LOGGING['loggers']['']['level'] = 999  # noqa
+
+# see  https://github.com/evansd/whitenoise/issues/94
+MIDDLEWARE_CLASSES.remove('whitenoise.middleware.WhiteNoiseMiddleware')  # noqa
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+CELERY_ALWAYS_EAGER = True
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+BROKER_BACKEND = 'memory'
