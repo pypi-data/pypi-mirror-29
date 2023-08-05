adcfe73cbd78847ff6f17be51cd5781c94d4c137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package bowtie/0.8.0/bowtie-0.8.0.tar.gz

---
+++ packages/bowtie/bowtie-0.8.0.tar.gz/bowtie-0.8.0/bowtie/__init__.py
@@ -0,0 +1,8 @@
+"""Interactive Dashboard Toolkit."""
+
+__version__ = '0.8.0'
+
+from bowtie._app import App, View
+from bowtie._command import command
+from bowtie.pager import Pager
+from bowtie._cache import cache
