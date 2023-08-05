f03b30acb2bf85a4fca9d895266f470a6a2e2bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:52 +0000

    Add package EagleSCR/0.2.0/EagleSCR-0.2.0.tar.gz

---
+++ packages/EagleSCR/EagleSCR-0.2.0.tar.gz/EagleSCR-0.2.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+
+setup(
+      name = 'EagleSCR'
+   ,  version = '0.2.0'
+   ,  description = 'Parser for the Eagle scripting language'
+   ,  long_description = read_md('README.md')
+   ,  author = 'Derp Ston'
+   ,  author_email = 'derpston+pypi@sleepygeek.org'
+   ,  url = 'https://github.com/derpston/pyEagleSCR'
+   ,  packages = ['eaglescr']
+   )
+
