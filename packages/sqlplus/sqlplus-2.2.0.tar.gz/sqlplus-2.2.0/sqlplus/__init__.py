98e137ddddcb2e8c892cad72e043d3018c9de412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:04 +0000

    Add package sqlplus/2.2.0/sqlplus-2.2.0.tar.gz

---
+++ packages/sqlplus/sqlplus-2.2.0.tar.gz/sqlplus-2.2.0/sqlplus/__init__.py
@@ -0,0 +1,11 @@
+"""
+"""
+
+from .core import Row, Rows, readxl, process, Load, Apply, Join, connect
+from .util import isnum, dconv, dmath, isconsec
+
+
+__all__ = ['process', 'Row', 'Rows', 'isnum', 'dconv', 'dmath', 'readxl',
+           'isconsec', 'Load', 'Apply', 'Join', 'connect']
+
+
