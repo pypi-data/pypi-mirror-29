4f1f70c5636325753f7652bc533d5ec8b0b200bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:39 +0000

    Add package jira-python/0.2/jira-python-0.2.tar.gz

---
+++ packages/jira-python/jira-python-0.2.tar.gz/jira-python-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jira-python',
+      version='0.2',
+      description='API wrapper for Jira CRM written in Python',
+      url='https://github.com/GearPlug/jira-python',
+      author='Miguel Ferrer',
+      author_email='ingferrermiguel@gmail.com',
+      license='GPL',
+      packages=['jira'],
+      zip_safe=False)
