cb500ef3aaec33ef6c3bc2a41955e00f6faa24cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:46 +0000

    Add package bureaucrate/0.3.5/bureaucrate-0.3.5.tar.gz

---
+++ packages/bureaucrate/bureaucrate-0.3.5.tar.gz/bureaucrate-0.3.5/bureaucrate/__main__.py
@@ -0,0 +1,38 @@
+from sys import argv
+from os.path import expanduser, join
+from argparse import ArgumentParser
+
+from . import __version__
+from .bureaucrate import init
+from .utils import Config
+
+
+def process_account(conf: Config, account: str):
+    acc = init(join(conf.get("base_path"), account))
+    for mailbox in conf.get_mailboxes(account):
+        for message in acc[mailbox]:
+            message.exec_rules(conf.get('rules', [], mailbox, account))
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--version', help="returns the version and exists")
+    parser.add_argument('-a', '--account', help="Restrict to an account")
+    parser.add_argument('-c', '--config', default='~/.bureaucraterc',
+                        help='specify an alternate configuration file')
+    opts = vars(parser.parse_args(argv[1:]))
+    conf = Config()
+    conf.parse(expanduser(opts.get('config')))
+
+    if opts.get('version', None):
+        print("bureaucrate v{}".format(__version__))
+        return
+
+    if opts.get('account', None):
+        process_account(conf, opts.get('account'))
+    else:
+        for account in conf.get_accounts():
+            process_account(conf, account)
+
+if '__main__' in __name__:
+    main()
