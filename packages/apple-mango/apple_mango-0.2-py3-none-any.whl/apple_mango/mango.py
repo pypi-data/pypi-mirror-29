2992d1c2a3657c66355d36f645f2683e0d6ad0b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:03 +0000

    Add package apple-mango/0.2/apple_mango-0.2-py3-none-any.whl

---
+++ packages/apple-mango/apple_mango-0.2-py3-none-any.whl/apple_mango/mango.py
@@ -0,0 +1,80 @@
+from functools import wraps
+import sys
+from unittest import TestCase
+
+import wrapt
+
+whens = []
+thens = []
+
+
+class Emtpy:
+    pass
+
+
+def given(desc=''):
+
+    @wrapt.decorator
+    def wrapper(wrapped, instance, args, kwargs):
+        nonlocal desc
+        verbose('\nGiven:', desc)
+
+        if args and isinstance(args[0], TestCase):
+            instance = args[0]
+
+        clear(instance)
+
+        result = wrapped(*args, **kwargs)
+
+        for when_, desc in whens:
+            with instance.subTest(when_=when_):
+                call_when(instance, when_, desc)
+
+        clear(instance)
+
+        return result
+
+    def clear(instance):
+        instance.given = Emtpy()
+        instance.when = Emtpy()
+        instance.then = Emtpy()
+
+        global whens
+        whens = []
+
+    return wrapper
+
+
+def when(desc=''):
+
+    def decorate(func):
+        whens.append((func, desc,))
+    return decorate
+
+
+def then(desc=''):
+
+    def decorate(func):
+        thens.append((func, desc,))
+    return decorate
+
+
+def call_when(instance, func, desc):
+    verbose('When:', desc)
+
+    result = func()
+
+    global thens
+    for then_, desc in thens:
+        with instance.subTest(then_=then_):
+            verbose('Then:', desc)
+            then_()
+
+    thens = []
+    verbose()
+
+    return result
+
+def verbose(mode='', desc=''):
+    if '-v' in sys.argv or '--verbose' in sys.argv:
+        print(mode, desc)
