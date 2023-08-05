d550f6198fafcd67ad57dee77c8e2c6b29c969f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:01 +0000

    Add package waldur-mastermind/2.8.8/waldur-mastermind-2.8.8.tar.gz

---
+++ packages/waldur-mastermind/waldur-mastermind-2.8.8.tar.gz/waldur-mastermind-2.8.8/src/waldur_mastermind/support/admin.py
@@ -0,0 +1,44 @@
+from __future__ import unicode_literals
+
+from django import forms
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+from waldur_core.structure import admin as structure_admin
+
+from . import models
+
+
+User = get_user_model()
+
+
+class UserChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, user):
+        return '{} - {}'.format(user.full_name, user.username)
+
+
+class SupportUserAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(SupportUserAdminForm, self).__init__(*args, **kwargs)
+        self.fields['user'] = UserChoiceField(queryset=User.objects.all().order_by('full_name'))
+
+
+class SupportUserAdmin(admin.ModelAdmin):
+    form = SupportUserAdminForm
+
+
+class OfferingAdmin(admin.ModelAdmin):
+    list_display = ('type', 'name', 'unit_price', 'unit', 'state')
+    fields = ('name', 'unit_price', 'unit', 'type', 'issue',
+              'project', 'state', 'product_code', 'article_code')
+
+
+class IssueAdmin(structure_admin.BackendModelAdmin):
+    exclude = ('resource_content_type', 'resource_object_id')
+
+
+admin.site.register(models.Offering, OfferingAdmin)
+admin.site.register(models.Issue, IssueAdmin)
+admin.site.register(models.Comment, structure_admin.BackendModelAdmin)
+admin.site.register(models.Attachment)
+admin.site.register(models.SupportUser, SupportUserAdmin)
