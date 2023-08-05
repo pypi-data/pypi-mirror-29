1b9d203e8eea909f7c9348c6fa0f1e0bf37e9b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:44 +0000

    Add package jsonconfigreader/1.2.1/jsonconfigreader-1.2.1.tar.gz

---
+++ packages/jsonconfigreader/jsonconfigreader-1.2.1.tar.gz/jsonconfigreader-1.2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+#converts markdown to reStructured
+try:
+    import pypandoc
+except ImportError:
+    print("Install pypandoc to generate the field long_description")
+    pypandoc = None
+if pypandoc:
+    converted_readme = pypandoc.convert('README.md', 'rst', format='md', )
+    long_description = converted_readme
+else:
+    long_description = "[pypandoc missing]"
+
+setup(name='jsonconfigreader',
+      version='1.2.1',
+      description='Python JSON configuration reader and parser',
+      long_description=long_description,
+      author='Andrei Surzhan',
+      author_email='surzhan.a.y@gmail.com',
+      url='https://github.com/AndreySurzhan/jsonconfigreader',
+      packages=['jsonconfigreader'],
+      classifiers=[
+          'Topic :: Utilities',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Other Audience',
+          'Operating System :: Microsoft :: Windows :: Windows 10',
+          'Operating System :: MacOS',
+          'Operating System :: Unix',
+          'Programming Language :: Python'
+          ]
+      )
