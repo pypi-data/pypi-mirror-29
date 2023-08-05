ef9eed05a2e8fd6d66b92ce12ed5fc11a536286a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:25 +0000

    Add package tinypackage/1.0/tinypackage-1.0.tar.gz

---
+++ packages/tinypackage/tinypackage-1.0.tar.gz/tinypackage-1.0/README.md
@@ -0,0 +1,12 @@
+# tinypackage
+a tiny package to say hello
+
+# installation
+pip3 install -e .
+
+p3 setup.py sdist
+
+p3 install wheel
+
+p3 setup.py bdist_wheel --universal
+
---
+++ packages/tinypackage/tinypackage-1.0.tar.gz/tinypackage-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tinypackage',
+      version='1.0',
+      description='say hello!',
+      url='http://github.com/haiwangyang/tinypackage',
+      author='Haiwang Yang',
+      author_email='haiwangyang@gmail.com',
+      license='MIT',
+      packages=['tinypackage'],
+      zip_safe=False)
