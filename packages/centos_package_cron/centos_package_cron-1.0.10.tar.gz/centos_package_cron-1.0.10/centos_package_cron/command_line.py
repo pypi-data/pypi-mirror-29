8e6defcf8f34c2be387ac95affdc6679301e8053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package centos_package_cron/1.0.10/centos_package_cron-1.0.10.tar.gz

---
+++ packages/centos_package_cron/centos_package_cron-1.0.10.tar.gz/centos_package_cron-1.0.10/centos_package_cron/command_line.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import socket
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+from report_producer import ReportProducer
+from db_session_fetcher import db_session_fetcher
+import smtplib
+from email.mime.text import MIMEText
+import pkg_resources
+
+__VERSION__ = pkg_resources.require("centos_package_cron")[0].version
+
+def main():
+    args = parse_args()
+    if args.output not in ['email', 'stdout']:
+        print "%s is not valid for the -o/--output parameter. Must be email or stdout" % (args.output)
+        raise
+
+    repos_to_exclude_list = []
+    if args.disablerepo is not None:
+        repos_to_exclude_list = args.disablerepo.split(',')
+    repos_to_include_list = []
+    if args.enablerepo is not None:
+        repos_to_include_list = args.enablerepo.split(',')
+
+    skipold = not args.forceold
+    producer = ReportProducer(repos_to_exclude_list, repos_to_include_list, skipold, args.skip_sqlite_file_path, include_depends_on=args.include_depends_on)
+
+    report_content = producer.get_report_content_as_json() if args.json else producer.get_report_content()
+
+    if report_content != '':
+        if args.output == 'stdout':
+            print report_content
+        else:
+            server = smtplib.SMTP('localhost')
+            message = MIMEText(report_content.encode('utf-8'), 'plain', 'utf-8')
+            message['Subject'] = args.email_subject
+            message['From'] = args.email_from
+            message['To'] = args.email_to
+            server.sendmail(from_addr=args.email_from, to_addrs=[args.email_to], msg=message.as_string())
+            server.quit
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Emails administrators with CentOS security updates and changelogs of non-security updates. Version %s" % __VERSION__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-e', '--email_to',
+    type=str,
+    default='root',
+    help='Email following user with the output')
+
+    parser.add_argument('-o', '--output',
+    type=str,
+    default='email',
+    help='How should report be sent, email or stdout are valid values')
+
+    parser.add_argument('-f', '--email_from',
+    type=str,
+    default="CentOS Update Check on %s <noreply@centos.org>" %(socket.gethostname()),
+    help='Send the email from this user.')
+
+    parser.add_argument('-s', '--email_subject',
+    type=str,
+    default="CentOS Update Check on %s" %(socket.gethostname()),
+    help='Send the email using this subject')
+
+    parser.add_argument('-dr','--disablerepo',
+    type=str,
+    help='List of comma separated repos to exclude when dealing with Yum')
+
+    parser.add_argument('-er','--enablerepo',
+    type=str,
+    help='List of comma separated repos to include when dealing with Yum')
+
+    parser.add_argument('-fo','--forceold',
+    help='Instead of the default behavior to only complain once for a given advisory/package update notice, repeats them with each run.',
+    action="store_true")
+
+    parser.add_argument('-db','--skip-sqlite-file-path',
+    type=str,
+    default=db_session_fetcher.DEFAULT_DB_PATH,
+    help='The location of the Sqlite DB used to track which notifications you have already received.')
+
+    parser.add_argument('-do','--include-depends-on',
+    help='When a package update is listed, show what packages on your system depend on that package',
+    action="store_true")
+
+    parser.add_argument('-j','--json',
+    help='Output the results in JSON format',
+    action="store_true")
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/centos_package_cron/centos_package_cron-1.0.10.tar.gz/centos_package_cron-1.0.10/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import sys
+import setuptools
+
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+version = '1.0.10'
+setuptools.setup(name='centos_package_cron',
+      version=version,
+      description='CentOS Package Update Utilities',
+      author='Brady Wied',
+      author_email='support@bswtechconsulting.com',
+      url='https://github.com/wied03/centos-package-cron',
+      download_url='https://github.com/wied03/centos-package-cron/archive/releases/%s.tar.gz' % (version),
+      license='BSD 2-Clause',
+      packages=setuptools.find_packages(),
+      setup_requires="pytest",
+      install_requires=['sqlalchemy'],
+      tests_require="mock",
+      cmdclass = {'test': PyTest},
+	  entry_points = {
+	        'console_scripts': ['centos-package-cron=centos_package_cron.command_line:main'],
+	  }
+     )
