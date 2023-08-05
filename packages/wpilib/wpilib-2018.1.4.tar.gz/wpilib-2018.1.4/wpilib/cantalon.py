1fb878c132aab27fe57bdf55014c2c358028f38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:18 +0000

    Add package wpilib/2018.1.4/wpilib-2018.1.4.tar.gz

---
+++ packages/wpilib/wpilib-2018.1.4.tar.gz/wpilib-2018.1.4/wpilib/cantalon.py
@@ -0,0 +1,10 @@
+# notrack
+# Placeholder for the CANTalon class, which has moved to its own package
+
+__all__ = ["CANTalon"]
+
+class CANTalon:
+
+    def __init__(*args, **kwargs):
+        raise ValueError("CANTalon has moved to ctre.CANTalon, you must install robotpy-ctre")
+
