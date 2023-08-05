53e1fac3ed294513e5119edf4d226636de0bef70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:25 +0000

    Add package deepscan/0.43/deepscan-0.43-py3-none-any.whl

---
+++ packages/deepscan/deepscan-0.43-py3-none-any.whl/deepscan/utils.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  4 11:23:18 2017
+
+@author: Dan
+"""
+
+import numpy as np
+from astropy.io import fits
+import os
+
+
+def read_fits(fname, read_header=False, extension=0):
+    
+    '''Read fits data'''
+    
+    #Use astropy to read FITS
+    hdulist = fits.open(fname)
+    data = hdulist[int(extension)].data
+    hdulist.close()
+    
+    if read_header:
+        header = hdulist[int(extension)].header
+        return data, header
+    else:
+        return data
+
+
+def save_to_fits(data, fname, header=None, overwrite=False, dtype=None):
+    
+    '''Save to fits'''
+    
+    #Check for ofile
+    if overwrite == False:
+        if os.path.isfile(fname):
+            raise(IOError('File %s exists. Set overwrite=True to overwrite.' % fname))
+            
+    if dtype is None:
+        dtype = data.dtype
+        
+    hdulist = fits.PrimaryHDU(data.astype(dtype), header=header)
+    hdulist.writeto(fname, overwrite=overwrite)
+    
+    
+def save_to_MEF(datas, headers, fname, overwrite=False):
+    
+    #Check if file exists
+    if overwrite == False:
+        if os.path.isfile(fname):
+            raise(IOError('File %s exists. Set overwrite=True to overwrite.' % fname))
+          
+    
+    new_hdul = fits.HDUList()
+    for data, header in zip(datas, headers):
+        new_hdul.append(fits.ImageHDU(data, header=header))
+    
+    new_hdul.writeto(fname, overwrite=overwrite)
