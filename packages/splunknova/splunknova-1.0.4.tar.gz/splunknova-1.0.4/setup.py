e3c38c0ddaf0f3bb495c2bfaa8d66c440a325fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:09 +0000

    Add package splunknova/1.0.4/splunknova-1.0.4.tar.gz

---
+++ packages/splunknova/splunknova-1.0.4.tar.gz/splunknova-1.0.4/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='splunknova',
+      version='1.0.4',
+      description='Python Client for Splunk Project Nova',
+      author='Scott Odle',
+      author_email='scott@sjodle.com',
+      url='https://github.com/sodle/pysplunknova',
+      download_url='https://github.com/sodle/pysplunknova/archive/1.0.4.tar.gz',
+      license='GPL v3',
+      packages=['splunknova'],
+      install_requires=[
+            'six',
+            'requests'
+      ]
+      )
---
+++ packages/splunknova/splunknova-1.0.4.tar.gz/splunknova-1.0.4/splunknova/__init__.py
@@ -0,0 +1,13 @@
+# ~*~ coding: utf-8 ~*~
+"""
+Python Client for Splunk Project Nova
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Simple, Requests- and Django-inspired library for reading, writing,
+searching, and aggregating events and metrics in Splunk Project Nova.
+"""
+
+from .__version__ import __title__, __description__, __url__, __version__
+from .__version__ import __author__, __author_email__, __license__, __buttercup__
+
+from .client import Client
