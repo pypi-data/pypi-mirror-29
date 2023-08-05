0fab0545512f225b788e02d99c01a5656b0e6c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:36 +0000

    Add package sipyapns/1.0.0/sipyapns-1.0.0.tar.gz

---
+++ packages/sipyapns/sipyapns-1.0.0.tar.gz/sipyapns-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    author='KyoungMook Ryu',
+    author_email='rockeron@rockeron.net',
+    description='A python library for interacting with the Apple Push Notification Service',
+    download_url='https://github.com/nrise/SIPyAPNs',
+    license='unlicense.org',
+    name='sipyapns',
+    py_modules=['apns'],
+    scripts=['apns-send'],
+    version='1.0.0',
+)
