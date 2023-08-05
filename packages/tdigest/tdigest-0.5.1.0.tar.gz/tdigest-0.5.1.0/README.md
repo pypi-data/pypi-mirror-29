2c4fd3131daaa60e685b96f1d376e0ffe25ef826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:52 +0000

    Add package tdigest/0.5.1.0/tdigest-0.5.1.0.tar.gz

---
+++ packages/tdigest/tdigest-0.5.1.0.tar.gz/tdigest-0.5.1.0/README.md
@@ -0,0 +1,63 @@
+# tdigest
+### Efficient percentile estimation of streaming or distributed data
+[![PyPI version](https://badge.fury.io/py/tdigest.svg)](https://badge.fury.io/py/tdigest)
+[![Build Status](https://travis-ci.org/CamDavidsonPilon/tdigest.svg?branch=master)](https://travis-ci.org/CamDavidsonPilon/tdigest)
+
+
+This is a Python implementation of Ted Dunning's [t-digest](https://github.com/tdunning/t-digest) data structure. The t-digest data structure is designed around computing accurate estimates from either streaming data, or distributed data. These estimates are percentiles, quantiles, trimmed means, etc. Two t-digests can be added, making the data structure ideal for map-reduce settings, and can be serialized into much less than 10kB (instead of storing the entire list of data).
+
+See a blog post about it here: [Percentile and Quantile Estimation of Big Data: The t-Digest](http://dataorigami.net/blogs/napkin-folding/19055451-percentile-and-quantile-estimation-of-big-data-the-t-digest)
+
+
+### Installation
+*tdigest* is compatible with both Python 2 and Python 3. 
+
+```
+pip install tdigest
+```
+
+### Usage
+
+#### Update the digest sequentially
+
+```
+from tdigest import TDigest
+from numpy.random import random
+
+digest = TDigest()
+for x in range(5000):
+    digest.update(random())
+
+print(digest.percentile(15))  # about 0.15, as 0.15 is the 15th percentile of the Uniform(0,1) distribution
+```
+
+#### Update the digest in batches
+
+```
+another_digest = TDigest()
+another_digest.batch_update(random(5000))
+print(another_digest.percentile(15))
+```
+
+#### Sum two digests to create a new digest
+
+```
+sum_digest = digest + another_digest 
+sum_digest.percentile(30)  # about 0.3
+```
+
+### API 
+
+`TDigest.`
+
+ - `update(x, w=1)`: update the tdigest with value `x` and weight `w`.
+ - `batch_update(x, w=1)`: update the tdigest with values in array `x` and weight `w`.
+ - `compress()`: perform a compression on the underlying data structure that will shrink the memory footprint of it, without hurting accuracy. Good to perform after adding many values. 
+ - `percentile(p)`: return the `p`th percentile. Example: `p=50` is the median.
+ - `cdf(x)`: return the CDF the value `x` is at. 
+ - `trimmed_mean(p1, p2)`: return the mean of data set without the values below and above the `p1` and `p2` percentile respectively. 
+
+ 
+
+
+
---
+++ packages/tdigest/tdigest-0.5.1.0.tar.gz/tdigest-0.5.1.0/tdigest/tdigest.py
@@ -0,0 +1,268 @@
+from __future__ import print_function
+
+from random import choice
+from itertools import chain
+from accumulation_tree import AccumulationTree
+import pyudorandom
+
+
+class Centroid(object):
+
+    def __init__(self, mean, count):
+        self.mean = float(mean)
+        self.count = float(count)
+
+    def __repr__(self):
+        return """<Centroid: mean=%.8f, count=%d>""" % (self.mean, self.count)
+
+    def __eq__(self, other):
+        return self.mean == other.mean and self.count == other.count
+
+    def update(self, x, weight):
+        self.count += weight
+        self.mean += weight * (x - self.mean) / self.count
+        return
+
+
+class TDigest(object):
+
+    def __init__(self, delta=0.01, K=25):
+        self.C = AccumulationTree(lambda centroid: centroid.count)
+        self.n = 0
+        self.delta = delta
+        self.K = K
+
+    def __add__(self, other_digest):
+        data = list(chain(self.C.values(), other_digest.C.values()))
+        new_digest = TDigest(self.delta, self.K)
+
+        if len(data) > 0:
+            for c in pyudorandom.items(data):
+                new_digest.update(c.mean, c.count)
+
+        return new_digest
+
+    def __len__(self):
+        return len(self.C)
+
+    def __repr__(self):
+        return """<T-Digest: n=%d, centroids=%d>""" % (self.n, len(self))
+
+    def _add_centroid(self, centroid):
+        if centroid.mean not in self.C:
+            self.C.insert(centroid.mean, centroid)
+        else:
+            self.C[centroid.mean].update(centroid.mean, centroid.count)
+
+    def _compute_centroid_quantile(self, centroid):
+        denom = self.n
+        cumulative_sum = self.C.get_left_accumulation(centroid.mean)
+        return (centroid.count / 2. + cumulative_sum) / denom
+
+    def _update_centroid(self, centroid, x, w):
+        self.C.pop(centroid.mean)
+        centroid.update(x, w)
+        self._add_centroid(centroid)
+
+    def _find_closest_centroids(self, x):
+        try:
+            ceil_key = self.C.ceiling_key(x)
+        except KeyError:
+            floor_key = self.C.floor_key(x)
+            return [self.C[floor_key]]
+
+        try:
+            floor_key = self.C.floor_key(x)
+        except KeyError:
+            ceil_key = self.C.ceiling_key(x)
+            return [self.C[ceil_key]]
+
+        if abs(floor_key - x) < abs(ceil_key - x):
+            return [self.C[floor_key]]
+        elif abs(floor_key - x) == abs(ceil_key - x) and (ceil_key != floor_key):
+            return [self.C[ceil_key], self.C[floor_key]]
+        else:
+            return [self.C[ceil_key]]
+
+    def _theshold(self, q):
+        return 4 * self.n * self.delta * q * (1 - q)
+
+    def update(self, x, w=1):
+        """
+        Update the t-digest with value x and weight w.
+
+        """
+        self.n += w
+
+        if len(self) == 0:
+            self._add_centroid(Centroid(x, w))
+            return
+
+        S = self._find_closest_centroids(x)
+
+        while len(S) != 0 and w > 0:
+            j = choice(list(range(len(S))))
+            c_j = S[j]
+
+            q = self._compute_centroid_quantile(c_j)
+
+            # This filters the out centroids that do not satisfy the second part
+            # of the definition of S. See original paper by Dunning.
+            if c_j.count + w > self._theshold(q):
+                S.pop(j)
+                continue
+
+            delta_w = min(self._theshold(q) - c_j.count, w)
+            self._update_centroid(c_j, x, delta_w)
+            w -= delta_w
+            S.pop(j)
+
+        if w > 0:
+            self._add_centroid(Centroid(x, w))
+
+        if len(self) > self.K / self.delta:
+            self.compress()
+
+        return
+
+    def batch_update(self, values, w=1):
+        """
+        Update the t-digest with an iterable of values. This assumes all points have the
+        same weight.
+        """
+        for x in values:
+            self.update(x, w)
+        self.compress()
+        return
+
+    def compress(self):
+        T = TDigest(self.delta, self.K)
+        C = list(self.C.values())
+        for c_i in pyudorandom.items(C):
+            T.update(c_i.mean, c_i.count)
+        self.C = T.C
+
+    def percentile(self, p):
+        """
+        Computes the percentile of a specific value in [0,100].
+        """
+
+        if not (0 <= p <= 100):
+            raise ValueError("p must be between 0 and 100, inclusive.")
+
+        p = float(p)/100.
+        p *= self.n
+        c_i = None
+        t = 0
+
+        if p == 0:
+            return self.C.min_item()[1].mean
+
+        for i, key in enumerate(self.C.keys()):
+            c_i_plus_one = self.C[key]
+            if i == 0:
+                k = c_i_plus_one.count / 2
+
+            else:
+                k = (c_i_plus_one.count + c_i.count) / 2.
+                if p < t + k:
+                    z1 = p - t
+                    z2 = t + k - p
+                    return (c_i.mean * z2 + c_i_plus_one.mean * z1) / (z1 + z2)
+            c_i = c_i_plus_one
+            t += k
+
+        return self.C.max_item()[1].mean
+
+    def cdf(self, x):
+        """
+        Computes the cdf of a specific value, ie. computes F(x) where F denotes
+        the CDF of the distribution.
+        """
+        t = 0
+        N = float(self.n)
+
+        if len(self) == 1:  # only one centroid
+            return int(x >= self.C.min_key())
+
+        for i, key in enumerate(self.C.keys()):
+            c_i = self.C[key]
+            if i == len(self) - 1:
+                delta = (c_i.mean - self.C.prev_item(key)[1].mean) / 2.
+            else:
+                delta = (self.C.succ_item(key)[1].mean - c_i.mean) / 2.
+            z = max(-1, (x - c_i.mean) / delta)
+
+            if z < 1:
+                return t / N + c_i.count / N * (z + 1) / 2
+
+            t += c_i.count
+        return 1
+
+    def trimmed_mean(self, p1, p2):
+        """
+        Computes the mean of the distribution between the two percentiles p1 and p2.
+        This is a modified algorithm than the one presented in the original t-Digest paper.
+
+        """
+        if not (p1 < p2):
+            raise ValueError("p1 must be between 0 and 100 and less than p2.")
+
+        s = k = t = 0
+        p1 /= 100.
+        p2 /= 100.
+        p1 *= self.n
+        p2 *= self.n
+        for i, key in enumerate(self.C.keys()):
+            c_i = self.C[key]
+            k_i = c_i.count
+            if p1 < t + k_i:
+                if t < p1:
+                    nu = self.__interpolate(i,key,p1-t)
+                else:
+                    nu = 1
+                s += nu * k_i * c_i.mean
+                k += nu * k_i
+
+            if p2 < t + k_i:
+                nu = self.__interpolate(i,key,p2-t)
+                s -= nu * k_i * c_i.mean
+                k -= nu * k_i
+                break
+
+            t += k_i
+
+        return s/k
+
+    def __interpolate(self, i, key, diff):
+        c_i = self.C[key]
+        k_i = c_i.count
+
+        if i == 0:
+            delta = self.C.succ_item(key)[1].mean - c_i.mean
+        elif i == len(self) - 1:
+            delta = c_i.mean - self.C.prev_item(key)[1].mean
+        else:
+            delta = (self.C.succ_item(key)[1].mean - self.C.prev_item(key)[1].mean) / 2.
+        return (diff / k_i - 0.5) * delta
+
+
+
+if __name__ == '__main__':
+    from numpy import random
+
+    T1 = TDigest()
+    x = random.random(size=10000)
+    T1.batch_update(x)
+
+    print(abs(T1.percentile(50) - 0.5))
+    print(abs(T1.percentile(10) - .1))
+    print(abs(T1.percentile(90) - 0.9))
+    print(abs(T1.percentile(1) - 0.01))
+    print(abs(T1.percentile(0.1) - 0.001))
+    print(T1.trimmed_mean(0.5, 1.))
+
+
+
+
+
