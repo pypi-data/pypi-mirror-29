9e004cf397c8189d5877aae1a59f47ff59bf4e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:24 +0000

    Add package elib/0.1.27/elib-0.1.27.tar.gz

---
+++ packages/elib/elib-0.1.27.tar.gz/elib-0.1.27/test/conftest.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+"""
+Pytest config
+"""
+import os
+import sys
+
+import pytest
+
+
+# noinspection PyUnusedLocal
+def pytest_configure(config):
+    """Setup"""
+    sys._called_from_test = True
+
+
+# noinspection PyUnusedLocal,SpellCheckingInspection
+def pytest_unconfigure(config):
+    """Tear down"""
+    # noinspection PyUnresolvedReferences,PyProtectedMember
+    del sys._called_from_test
+
+
+@pytest.fixture(autouse=True)
+def cleandir(request, tmpdir):
+    """Provides a clean working dir"""
+    if 'nocleandir' in request.keywords:
+        yield
+    else:
+        current_dir = os.getcwd()
+        os.chdir(str(tmpdir))
+        yield os.getcwd()
+        os.chdir(current_dir)
+
+
+def pytest_addoption(parser):
+    """Add option for long tests"""
+    parser.addoption("--long", action="store_true",
+                     help="run long tests")
+
+
+def pytest_runtest_setup(item):
+    """Skip long tests"""
+    long_marker = item.get_marker("long")
+    if long_marker is not None and not item.config.getoption('long'):
+        pytest.skip('skipping long tests')
