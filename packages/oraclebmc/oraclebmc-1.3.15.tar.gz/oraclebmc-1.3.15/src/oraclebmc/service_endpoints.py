9112dd0b930a5d075d075b533deb434262f2edaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:37 +0000

    Add package oraclebmc/1.3.15/oraclebmc-1.3.15.tar.gz

---
+++ packages/oraclebmc/oraclebmc-1.3.15.tar.gz/oraclebmc-1.3.15/src/oraclebmc/service_endpoints.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
+
+SERVICE_ENDPOINTS = {
+    "audit": "https://audit.{domain}/20160918",
+    "blockstorage": "https://iaas.{domain}/20160918",
+    "compute": "https://iaas.{domain}/20160918",
+    "virtual_network": "https://iaas.{domain}/20160918",
+    "database": "https://database.{domain}/20160918",
+    "dns": "https://dns.{domain}/20180115",
+    "file_storage": "https://filestorage.{domain}/20171215",
+    "identity": "https://identity.{domain}/20160918",
+    "load_balancer": "https://iaas.{domain}/20170115",
+    "object_storage": "https://objectstorage.{domain}"
+}
