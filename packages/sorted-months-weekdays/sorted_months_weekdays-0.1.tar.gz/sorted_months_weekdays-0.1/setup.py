e95492a44e8555f4ccfe8114f782db6242932b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:12 +0000

    Add package sorted-months-weekdays/0.1/sorted_months_weekdays-0.1.tar.gz

---
+++ packages/sorted-months-weekdays/sorted_months_weekdays-0.1.tar.gz/sorted_months_weekdays-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sorted_months_weekdays',
+      version='0.1',
+      description='Get sorted Months and Weeks by chronological order',
+      url='',
+      author='Yeddu Dinesh Babu',
+      author_email='ydineshy225@gmail.com',
+      license='',
+      packages=['sorted_months_weekdays'],
+      zip_safe=False)
