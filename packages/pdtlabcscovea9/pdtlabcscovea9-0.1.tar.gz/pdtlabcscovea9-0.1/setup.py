e2b2ba61270adb7bfa92dafd66617df7204e1954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package pdtlabcscovea9/0.1/pdtlabcscovea9-0.1.tar.gz

---
+++ packages/pdtlabcscovea9/pdtlabcscovea9-0.1.tar.gz/pdtlabcscovea9-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pdtlabcscovea9',
+      version='0.1',
+      description='Package Datalab',
+      url='http://github.com/.../...',
+      author='coveadtlabteam123',
+      author_email='dtlab-team@getnada.com',
+      license='MIT',
+      packages=['pdtlabcscovea9'],
+      zip_safe=False)
