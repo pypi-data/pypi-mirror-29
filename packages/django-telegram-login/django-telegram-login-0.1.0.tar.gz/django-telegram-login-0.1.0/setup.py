9b774252ef3ab6d91d86e1504e27444d83fc9af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package django-telegram-login/0.1.0/django-telegram-login-0.1.0.tar.gz

---
+++ packages/django-telegram-login/django-telegram-login-0.1.0.tar.gz/django-telegram-login-0.1.0/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import find_packages, setup
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-telegram-login',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Telegram login interface and widgets for Django projects, that allows to handle user based on Telegram data.',
+    url='https://github.com/dmytrostriletskyi/django-telegram-login',
+    author='Dmytro Striletskyi',
+    author_email='dmytro.striletskyi@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
