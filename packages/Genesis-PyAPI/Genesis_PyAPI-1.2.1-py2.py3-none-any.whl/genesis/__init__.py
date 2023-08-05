4137802a8896d8adddfe329be097f119ea87cf9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:26 +0000

    Add package Genesis-PyAPI/1.2.1/Genesis_PyAPI-1.2.1-py2.py3-none-any.whl

---
+++ packages/Genesis-PyAPI/Genesis_PyAPI-1.2.1-py2.py3-none-any.whl/genesis/__init__.py
@@ -0,0 +1,6 @@
+"""Python API for the Genesis platform"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from .genesis import Genesis
+from .project import GenProject
+from .data import GenData
