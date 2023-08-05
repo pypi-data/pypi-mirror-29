eb2bc9dfbcb58fc496eafa4fc7e41b8667f1b249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:25 +0000

    Add package nester-1426/1.0.0/nester_1426-1.0.0.tar.gz

---
+++ packages/nester-1426/nester_1426-1.0.0.tar.gz/nester_1426-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name         = 'nester_1426',
+	version      = '1.0.0',
+	py_modules   = ['nester'],
+	author       = 'sherman',
+	author_email = 'sherman@mail.ustc.edu.cn',
+	url          = 'http://www.google.com',
+	description  = 'A simple printer of nested lists',
+     )
