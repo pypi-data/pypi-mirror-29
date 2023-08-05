d2732e717b92b9d91b5c310d4db5dae31dce21ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:34 +0000

    Add package path2insight/1.0b1/path2insight-1.0b1-py2.py3-none-any.whl

---
+++ packages/path2insight/path2insight-1.0b1-py2.py3-none-any.whl/path2insight/__init__.py
@@ -0,0 +1,18 @@
+import sys
+
+from path2insight.core import WindowsFilePath, PosixFilePath
+from path2insight.parse import *
+from path2insight.collect import *
+from path2insight.handling import *
+from path2insight.explore import *
+from path2insight.external.nltk import (ngrams,
+                                        bigrams,
+                                        trigrams,
+                                        everygrams,
+                                        skipgrams)
+
+__all__ = [
+    'tokenizers', 'datasets'
+]
+
+__version__ = "1.0b1"
