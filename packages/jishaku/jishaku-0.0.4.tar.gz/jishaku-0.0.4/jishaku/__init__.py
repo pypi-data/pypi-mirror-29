8e5db168e835b57a3e597d6ff38a00a9c4c4de26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:20 +0000

    Add package jishaku/0.0.4/jishaku-0.0.4.tar.gz

---
+++ packages/jishaku/jishaku-0.0.4.tar.gz/jishaku-0.0.4/jishaku/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+Jishaku
+~~~~~~~
+
+A debugging, testing and experimentation cog for Discord bots.
+
+:copyright: (c) 2017 Devon R
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'jishaku'
+__author__ = 'Gorialis'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Devon R'
+__version__ = '0.0.4'
+
+
+from . import cog
+
+
+def setup(bot):
+    bot.add_cog(cog.Jishaku(bot))
