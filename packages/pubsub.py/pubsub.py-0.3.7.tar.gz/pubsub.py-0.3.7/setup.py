7a4a36e7156aa3bfd972b90578be1b8fa4788489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package pubsub.py/0.3.7/pubsub.py-0.3.7.tar.gz

---
+++ packages/pubsub.py/pubsub.py-0.3.7.tar.gz/pubsub.py-0.3.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pubsub.py',
+    author='Superbalist Engineering',
+    author_email='tech@superbalist.com',
+    version='0.3.7',
+    description='Python PubSub Adapter for gcloud',
+    url='https://github.com/Superbalist/python-pubsub',
+    install_requires=[
+        'google.cloud.pubsub==0.29.0',
+        'jsonschema',
+        'requests',
+    ],
+    packages=find_packages(),
+    zip_safe=False)
