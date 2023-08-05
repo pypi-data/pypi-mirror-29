2cf42d22fbafe0a94eaae0d10e45e9123e86b078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:26 +0000

    Add package tensorflow_forward_ad/0.3.5/tensorflow_forward_ad-0.3.5.tar.gz

---
+++ packages/tensorflow_forward_ad/tensorflow_forward_ad-0.3.5.tar.gz/tensorflow_forward_ad-0.3.5/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+
+import numpy
+
+try:
+  from Cython.Distutils import build_ext
+except ImportError:
+  use_cython = False
+else:
+  use_cython = True
+
+cmdclass = {}
+ext_modules = []
+include_dirs = [numpy.get_include()]
+
+if use_cython:
+  ext_modules += [
+      Extension(
+          "tensorflow_forward_ad.cbfs", ["tensorflow_forward_ad/cbfs.pyx"],
+          include_dirs=include_dirs),
+  ]
+  cmdclass.update({'build_ext': build_ext})
+else:
+  ext_modules += [
+      Extension(
+          "tensorflow_forward_ad.cbfs", ["tensorflow_forward_ad/cbfs.c"],
+          include_dirs=include_dirs),
+  ]
+
+setup(
+    name='tensorflow_forward_ad',
+    packages=['tensorflow_forward_ad'],
+    cmdclass=cmdclass,
+    ext_modules=ext_modules,
+    version='0.3.5',
+    description='TensorFlow forward-mode automatic differentiation',
+    author='Mengye Ren',
+    author_email='renmengye@gmail.com',
+    url='https://github.com/renmengye/tensorflow-forward-ad',
+    download_url='https://github.com/renmengye/tensorflow-forward-ad/archive/0.3.5.tar.gz',
+    keywords=['tensorflow', 'automatic', 'differentiation'],
+    classifiers=[],)
