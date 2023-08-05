7abe21f179ef92afe9b36d500eed9c223dfdb240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:20 +0000

    Add package onegov.feriennet/1.3.18/onegov.feriennet-1.3.18.tar.gz

---
+++ packages/onegov.feriennet/onegov.feriennet-1.3.18.tar.gz/onegov.feriennet-1.3.18/onegov/feriennet/collections/occasion_attendees.py
@@ -0,0 +1,62 @@
+from collections import namedtuple, OrderedDict
+from onegov.activity import Activity, Attendee, Occasion, OccasionCollection
+from onegov.user import User
+
+
+OccasionAttendee = namedtuple('OccasionAttendee', ('attendee', 'contact'))
+
+
+class OccasionAttendeeCollection(OccasionCollection):
+
+    def __init__(self, session, period, username=None):
+        super().__init__(session)
+        self.period = period
+        self.username = username
+
+    @property
+    def period_id(self):
+        return self.period.id
+
+    def for_period(self, period):
+        return self.__class__(self.session, period, self.username)
+
+    def query(self):
+        q = super().query()
+        q = q.join(Occasion.activity)
+
+        # this will implicitly filter out all occasions without attendees:
+        q = q.join(Occasion.accepted)
+
+        q = q.filter(Occasion.period_id == self.period_id)
+
+        if self.username:
+            q = q.filter(Activity.username == self.username)
+
+        return q.order_by(Activity.title, Occasion.order, Occasion.id)
+
+    def occasions(self):
+        occasions = OrderedDict()
+
+        attendees = {
+            attendee.id: attendee for attendee in self.session.query(Attendee)
+        }
+
+        contacts = {
+            u.username: u.data and u.data.get('emergency')
+            for u in self.session.query(
+                User.username, User.data
+            )
+        }
+
+        for o in self.query():
+            occasions[o] = [
+                OccasionAttendee(
+                    attendees[b.attendee_id],
+                    contacts[b.username]
+                ) for b in sorted(
+                    o.accepted,
+                    key=lambda b: attendees[b.attendee_id].name
+                )
+            ]
+
+        return occasions
