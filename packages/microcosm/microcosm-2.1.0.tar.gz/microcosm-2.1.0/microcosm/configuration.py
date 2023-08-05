e431c4e391de648582a47611355a91571adccab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:06 +0000

    Add package microcosm/2.1.0/microcosm-2.1.0.tar.gz

---
+++ packages/microcosm/microcosm-2.1.0.tar.gz/microcosm-2.1.0/microcosm/configuration.py
@@ -0,0 +1,2 @@
+# backwards compatbility import
+from microcosm.config.model import Configuration  # noqa: F401
---
+++ packages/microcosm/microcosm-2.1.0.tar.gz/microcosm-2.1.0/microcosm/decorators.py
@@ -0,0 +1,36 @@
+"""
+Factory decorators
+
+"""
+from microcosm.registry import _registry
+
+
+DEFAULTS = "_defaults"
+
+
+def binding(key, registry=None):
+    """
+    Creates a decorator that binds a factory function to a key.
+
+    :param key: the binding key
+    :param: registry: the registry to bind to; defaults to the global registry
+
+    """
+    if registry is None:
+        registry = _registry
+
+    def decorator(func):
+        registry.bind(key, func)
+        return func
+    return decorator
+
+
+def defaults(**kwargs):
+    """
+    Creates a decorator that saves the provided kwargs as defaults for a factory function.
+
+    """
+    def decorator(func):
+        setattr(func, DEFAULTS, kwargs)
+        return func
+    return decorator
