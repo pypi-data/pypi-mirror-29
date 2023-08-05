3008af37d3be88ee35bd1b7b57f7067700dac208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:41 +0000

    Add package elib/0.1.35/elib-0.1.35-py36-none-any.whl

---
+++ packages/elib/elib-0.1.35-py36-none-any.whl/elib/__init__.py
@@ -0,0 +1,17 @@
+"""
+Placeholder for future personal library
+"""
+from pkg_resources import DistributionNotFound, get_distribution
+
+from . import config, console, custom_logging, custom_random, paste, path, tts
+from ._run import run
+from .pretty import pretty_format
+
+try:
+    __version__ = get_distribution('elib').version
+except DistributionNotFound:  # pragma: no cover
+    # package is not installed
+    __version__ = 'not installed'
+
+MAIN_LOGGER = custom_logging.get_logger('ELIB')
+MAIN_LOGGER.info(f'ELIB version {__version__}')
---
+++ packages/elib/elib-0.1.35-py36-none-any.whl/elib/paste.py
@@ -0,0 +1,55 @@
+# coding=utf-8
+"""
+Creates a snippet at glot.io for text sharing
+"""
+
+import json
+import logging
+import typing
+from collections import namedtuple
+
+import requests
+
+LOGGER = logging.getLogger('ELIB')
+
+BASE_URL = 'https://snippets.glot.io/snippets'
+
+PasteContent = namedtuple('PasteContent', 'filename content')
+
+
+class PasteError(requests.HTTPError):
+    """Raised on paste error"""
+    pass
+
+
+def create_new_paste(title, files: typing.List[PasteContent], public: bool = False, language='lua'):
+    """
+    Creates a paste on Glot
+
+    :param title: name of the paste
+    :param files: list of PasteContent
+    :param public: whether or not paste is public
+    :param language: syntax highlighting
+    :return: URL of the newly created paste
+    """
+    headers = {
+        'Content-type': 'application/json',
+    }
+    data = {
+        'language': language,
+        'title': title,
+        'public': public,
+        'files': [
+            {
+                'name': file.filename,
+                'content': file.content
+            } for file in files
+        ],
+    }
+    req = requests.post(BASE_URL, json=data, headers=headers)
+    if req.ok:
+        resp = json.loads(req.text)
+        url = resp['url'].replace('https://snippets.glot.io', 'https://glot.io')
+        return url
+    else:
+        raise PasteError(f'failed to post content; reason: {req.reason}')
