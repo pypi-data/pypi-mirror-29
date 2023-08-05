8a027b3521a4fabddcb0b101e4cf239d50b0c8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:24 +0000

    Add package yuna/1.0.0/yuna-1.0.0.tar.gz

---
+++ packages/yuna/yuna-1.0.0.tar.gz/yuna-1.0.0/setup.py
@@ -0,0 +1,32 @@
+"""
+    First version of Yuna
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+from setuptools import setup, find_packages
+from termcolor import colored
+from pprint import pprint
+from yuna import __version__
+
+setup(
+    name="yuna",
+    version=__version__,
+    author="Ruben van Staden",
+    author_email="rubenvanstaden@gmail.com",
+    description="Processes the layers from a GDS file.",
+    setup_requires=['setuptools-markdown'],
+    long_description_markdown_filename='README.md',
+    license="BSD",
+    keywords="yuna",
+    url="https://github.com/rubenvanstaden/yuna",
+    packages=['yuna'],
+    package_dir={'yuna': 'yuna'},
+    install_requires=['gdsyuna', 'pyclipper', 'setuptools', 'numpy', 'matplotlib', 'docopt', 'future', 'termcolor'],
+    entry_points={
+        'console_scripts': [
+            'yuna = yuna:layout'
+        ]
+    }
+)
