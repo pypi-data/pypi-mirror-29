8c07a65fd18317af232d215afddd71683e1c7941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:47 +0000

    Add package pyNCSre/20180216.post1/pyNCSre-20180216.post1.tar.gz

---
+++ packages/pyNCSre/pyNCSre-20180216.post1.tar.gz/pyNCSre-20180216.post1/README.md
@@ -0,0 +1,5 @@
+pyNCS
+=====
+
+pyNCSre is a python library that allows easy access to Neuromorphic Chips and Setups
+
---
+++ packages/pyNCSre/pyNCSre-20180216.post1.tar.gz/pyNCSre-20180216.post1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='pyNCSre',
+	version='20180216_r1',
+	description='Python Neurormophic Chips and Systems, reworked',
+	author='Emre Neftci',
+	author_email='eneftci@uci.edu',
+	url='https://github.com/nmi-lab/pyNCS',
+        download_url='https://github.com/nmi-lab/pyNCS/tarball/stable_20170814',
+	packages = ['pyNCSre', 'pyNCSre.pyST', 'pyNCSre.api'],
+        package_dir={'' : 'src'},
+	package_data={'pyNCSre' : ['data/*.dtd',
+                             'data/chipfiles/*.csv',
+                             'data/chipfiles/*.nhml',
+                             'data/chipfiles/*.xml']},
+        include_package_data=True,
+        install_requires=['numpy',
+                          'lxml',
+                          'matplotlib'],
+     )
