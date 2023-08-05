06200da5144ba959724be76894f8b1944aa80d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:53 +0000

    Add package datachile/0.0.2/datachile-0.0.2.tar.gz

---
+++ packages/datachile/datachile-0.0.2.tar.gz/datachile-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='datachile',
+    version='0.0.2',
+    description='Python client for manage DataChile API',
+    url='http://github.com/cnavarreteliz/datachile-api',
+    download_url='https://github.com/cnavarreteliz/datachile-api/archive/0.0.2.tar.gz',
+    author='Carlos Navarrete - Datawheel LLC',
+    author_email='cnavarreteliz@gmail.com',
+    license='MIT',
+    packages=['datachile'],
+    install_requires=[
+        'mondrian_rest==0.1',
+        'numpy',
+        'requests'
+    ],
+    dependency_links=[
+        "git+https://github.com/jazzido/py-mondrian-rest.git#egg=mondrian_rest-0.1"
+    ],
+    keywords = ['python', 'mondrian', 'olap', 'api'],
+    zip_safe=True
+)
