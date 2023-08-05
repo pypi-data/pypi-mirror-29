2edbc0633ba96b976d084d98391c4aede262f493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:35 +0000

    Add package geokey-export/0.4.2/geokey-export-0.4.2.tar.gz

---
+++ packages/geokey-export/geokey-export-0.4.2.tar.gz/geokey-export-0.4.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from os.path import join
+from setuptools import setup, find_packages
+
+
+name = 'geokey-export'
+version = __import__(name.replace('-', '_')).__version__
+repository = join('https://github.com/ExCiteS', name)
+
+setup(
+    name=name,
+    version=version,
+    description='Export data from GeoKey to KML or GeoJSON',
+    url=repository,
+    download_url=join(repository, 'tarball', version),
+    author='ExCiteS',
+    author_email='excites@ucl.ac.uk',
+    license='MIT',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*']),
+    include_package_data=True,
+)
