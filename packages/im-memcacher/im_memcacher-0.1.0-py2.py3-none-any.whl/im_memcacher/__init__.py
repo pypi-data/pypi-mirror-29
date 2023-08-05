e317605b3743572bdbb0e215a07448e6ee0c5b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:49 +0000

    Add package im-memcacher/0.1.0/im_memcacher-0.1.0-py2.py3-none-any.whl

---
+++ packages/im-memcacher/im_memcacher-0.1.0-py2.py3-none-any.whl/im_memcacher/__init__.py
@@ -0,0 +1,24 @@
+import functools
+from im_util import make_flash
+from google.appengine.api import memcache
+from im_util import logdebug
+
+#decorator to add caching to a function
+def memcacher(f = None, cachekey=None, expiresec = 600):
+    if not f:
+        return functools.partial(memcacher, cachekey = cachekey, expiresec = expiresec)
+
+    def getvalue(*args, **kwargs):
+        lcachekey = cachekey if cachekey else make_flash(f, *args, **kwargs)
+        
+        retval = memcache.get(lcachekey) #@UndefinedVariable
+        if retval is None:
+            logdebug("MISS: %s" % lcachekey)
+            retval = f(*args, **kwargs)
+            memcache.add(key=lcachekey, value=retval, time=expiresec) #@UndefinedVariable
+        else:
+            logdebug("HIT: %s" % lcachekey)
+
+        return retval
+
+    return getvalue
