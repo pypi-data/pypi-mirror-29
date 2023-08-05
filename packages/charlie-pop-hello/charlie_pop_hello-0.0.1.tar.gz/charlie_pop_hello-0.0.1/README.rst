721f41a69df91d00e6050cd1385a6da038dfa3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:23 +0000

    Add package charlie-pop-hello/0.0.1/charlie_pop_hello-0.0.1.tar.gz

---
+++ packages/charlie-pop-hello/charlie_pop_hello-0.0.1.tar.gz/charlie_pop_hello-0.0.1/README.rst
@@ -0,0 +1,18 @@
+Hello.py
+=========
+
+A script which will greet the user.
+
+
+Installation
+--------------
+
+Install with pip
+
+
+Usage
+------
+
+Simply pass a name to hello.py and the program will print out 'Hello, {name}'
+
+
---
+++ packages/charlie-pop-hello/charlie_pop_hello-0.0.1.tar.gz/charlie_pop_hello-0.0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from setuptools import setup
+from hello import __version__
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('CHANGES.rst') as f:
+    changes = f.read()
+
+setup(
+    name='charlie_pop_hello', # name of package
+    version=__version__, # version number of package
+    # A short description of the package
+    description=' A module to say hello', 
+    # A long description displayed on PyPi
+    long_description = readme + '\n\n' + changes,
+    author='Charlie Allatson', # The maintainer
+    author_email='charles.allatson@gmail.com', # Their email
+    url='https://github.com/charlieallatson/hello_world', # package's website
+    py_modules=['hello',], # The python modules to include
+    license='MIT', # The license type
+    entry_points={ # Links the "hello" command with a function to call
+        'console_scripts': ['hello=hello:main'],
+        }
+)
+
