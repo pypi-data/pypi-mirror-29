21b9e735bd46c2bdf935c69a23a907486081e524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:00 +0000

    Add package requests-mv-integrations/0.7.7/requests_mv_integrations-0.7.7-py3-none-any.whl

---
+++ packages/requests-mv-integrations/requests_mv_integrations-0.7.7-py3-none-any.whl/requests_mv_integrations/support/response/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  @copyright 2018 TUNE, Inc. (http://www.tune.com)
+#  @namespace requests_mv_integrations
+
+from .csv import (
+    csv_skip_last_row,
+)
+from .parse import (
+    requests_response_text_html,
+    requests_response_text_xml,
+)
+from .validate import (
+    build_response_error_details,
+    handle_json_decode_error,
+    requests_response_json,
+    validate_json_response,
+    validate_response,
+)
