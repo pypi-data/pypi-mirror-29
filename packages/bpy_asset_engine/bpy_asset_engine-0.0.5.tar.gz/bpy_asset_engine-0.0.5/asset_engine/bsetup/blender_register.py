9caf84e1107377b5f71c4137f1d210d7ea5881f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:04 +0000

    Add package bpy_asset_engine/0.0.5/bpy_asset_engine-0.0.5.tar.gz

---
+++ packages/bpy_asset_engine/bpy_asset_engine-0.0.5.tar.gz/bpy_asset_engine-0.0.5/asset_engine/bsetup/blender_register.py
@@ -0,0 +1,40 @@
+import bpy
+# from .addon_updater_ops import register, unregister
+
+
+#TODO: Add AssetManager imports and registration handling
+# # from archive.handlers import registerHandler, unregisterHandler
+# from .blender_utils.operators.asset_ops import registerDNA_Handler, unregisterDNA_Handler
+# from .blender_utils.operators.vehicular_ops import registerRenderHandler, unregisterRenderHandler
+# from .blender_utils.properties.pipe_props import registerProp, unregisterProp
+# from .blender_utils.ui.info_menu import registerMenu, unregisterMenu
+# from .blender_utils.checks.file_check import registerIntegrityHandler, unregisterIntegrityHandler
+# 
+# 
+# 
+# 
+# def registerFiles():
+#     registerMenu()
+#     # registerHandler()
+#     registerDNA_Handler()
+#     registerProp()
+#     registerRenderHandler()
+#     registerIntegrityHandler()
+# 
+# def unregisterFiles():
+#     unregisterMenu()
+#     # unregisterHandler()
+#     unregisterDNA_Handler()
+#     unregisterProp()
+#     unregisterRenderHandler()
+#     unregisterIntegrityHandler()
+
+
+
+def registerFiles():
+    pass
+    # register(bl_info)
+
+def unregisterFiles():
+    pass
+    # unregister()
---
+++ packages/bpy_asset_engine/bpy_asset_engine-0.0.5.tar.gz/bpy_asset_engine-0.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='bpy_asset_engine',
+      version='0.0.5',
+      description='Blender Asset Engine Library',
+      url='https://gitlab.com/lvxejay/asset_engine',
+      author='Jared Webber',
+      author_email='info@onelvxe.com',
+      license='GNU GPLv3',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+      keywords='blender bpy asset engine pipeline',
+      python_requires='~=3.5',
+      include_package_data=True,
+      #install_requires=['setuptools', 'wheel', 'requests', 'numpy'],
+      zip_safe=False)
