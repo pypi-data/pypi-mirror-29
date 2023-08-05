db6a1005eba77bf73b46c1dc74a6cb3acc61e3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:03 +0000

    Add package cryptakings-api/0.1/cryptakings-api-0.1.tar.gz

---
+++ packages/cryptakings-api/cryptakings-api-0.1.tar.gz/cryptakings-api-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='cryptakings-api',
+      version='0.1',
+      description='Python wrapper for REST API of CryptaKings Service',
+      url='https://github.com/vegeta1k95/CryptaKings-API-Wrapper',
+      author='vegeta1k95',
+      author_email='tunerzinc@gmail.com',
+      py_modules=['api_wrapper'],
+      install_requires=['requests'])
