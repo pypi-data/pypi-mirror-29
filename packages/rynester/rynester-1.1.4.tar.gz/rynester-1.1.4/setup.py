1bb38212d9a351843c95e1671e0ad711abe492ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:23 +0000

    Add package rynester/1.1.4/rynester-1.1.4.tar.gz

---
+++ packages/rynester/rynester-1.1.4.tar.gz/rynester-1.1.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+
+        name               = 'rynester',
+        version            = '1.1.4',
+        py_modules         = ['rynester'],
+        author             = 'Ryan',
+        description        = 'A simple printer of nested lists',
+        
+      )
