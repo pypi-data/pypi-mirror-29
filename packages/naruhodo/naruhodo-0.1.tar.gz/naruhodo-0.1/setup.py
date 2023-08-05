b66a4d40bd68aaa8fd7b9520e6d8a988e533b2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:05 +0000

    Add package naruhodo/0.1/naruhodo-0.1.tar.gz

---
+++ packages/naruhodo/naruhodo-0.1.tar.gz/naruhodo-0.1/setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'naruhodo',
+    packages = find_packages(exclude=['contrib', 'docs', 'test']),
+    version = '0.1',
+    description = 'A python library for generating dependency graph(DG) and knowledge graph(KG) from Japanese text.',
+    author = 'superkerokero',
+    author_email = 'superkerokero@outlook.com',
+    url = 'https://github.com/superkerokero/naruhodo',
+    download_url = 'https://github.com/superkerokero/naruhodo',
+    keywords = ['nlp', 'japanese'],
+    classifiers = [],
+    license = "MIT",
+    install_requires = [
+        'networkx',
+        'beautifulsoup4',
+        'nxpd',
+        'lxml'
+    ],
+    python_requires='>=3.4',
+)
