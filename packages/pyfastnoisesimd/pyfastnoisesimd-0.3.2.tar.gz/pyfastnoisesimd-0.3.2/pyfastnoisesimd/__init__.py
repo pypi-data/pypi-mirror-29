a765f7bb3bc2dfb360f6f23916b1ab4fd4bb8346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:00 +0000

    Add package pyfastnoisesimd/0.3.2/pyfastnoisesimd-0.3.2.tar.gz

---
+++ packages/pyfastnoisesimd/pyfastnoisesimd-0.3.2.tar.gz/pyfastnoisesimd-0.3.2/pyfastnoisesimd/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+########################################################################
+#
+#       PyFastNoiseSIMD
+#       License: BSD 3-clause
+#       Created: August 13, 2017
+#       Library Author: Jordan Peck - https://github.com/Auburns
+#       Python Extension Author:  Robert A. McLeod - robbmcleod@gmail.com
+#
+########################################################################
+
+from pyfastnoisesimd.version import __version__
+
+from pyfastnoisesimd.helpers import (
+    Noise, emptyCoords, num_virtual_cores,
+    generate, setNumWorkers, 
+    NoiseType, FractalType, PerturbType, 
+    CellularReturnType, CellularDistanceFunction)
+
+import pyfastnoisesimd.extension as _ext
+
+from pyfastnoisesimd.test_fns import test
+
+# from pyfastnoisesimd.cpuinfo import get_cpu_info
+# cpu_info = get_cpu_info()
