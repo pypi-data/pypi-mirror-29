9c8b0d38cbb70afdfc53df0dc0bfc01f8fb1e127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package hellodepils/0.1/hellodepils-0.1.tar.gz

---
+++ packages/hellodepils/hellodepils-0.1.tar.gz/hellodepils-0.1/hellodepils/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+	return ('Some joke that is not funny')
---
+++ packages/hellodepils/hellodepils-0.1.tar.gz/hellodepils-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hellodepils',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/daugapvils/hellodepils',
+      author='SV',
+      author_email='daugavpils@gmail.com',
+      license='MIT',
+      packages=['hellodepils'],
+      zip_safe=False)
