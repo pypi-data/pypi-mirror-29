72b5b580ac2779899d2eaa2a07413d6b72fce675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:52 +0000

    Add package jsoncensor/0.0.1/jsoncensor-0.0.1.tar.gz

---
+++ packages/jsoncensor/jsoncensor-0.0.1.tar.gz/jsoncensor-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'jsoncensor',
+    version = '0.0.1',
+    description = 'Check json data in your python web service.',
+    url = 'https://github.com/Humbertzhang/jsoncensor',
+    author = 'HumbertZhang',
+    author_email = 'zhangke980623@icloud.com'
+)
