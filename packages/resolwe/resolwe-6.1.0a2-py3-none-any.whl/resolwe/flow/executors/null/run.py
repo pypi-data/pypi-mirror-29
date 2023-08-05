7afecfaf898ceae744a275daf282c5db509b7229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:03 +0000

    Add package resolwe/6.1.0a2/resolwe-6.1.0a2-py3-none-any.whl

---
+++ packages/resolwe/resolwe-6.1.0a2-py3-none-any.whl/resolwe/flow/executors/null/run.py
@@ -0,0 +1,28 @@
+"""Local workflow executor."""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+
+from ..manager_commands import send_manager_command
+from ..protocol import ExecutorProtocol  # pylint: disable=import-error
+from ..run import BaseFlowExecutor
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+class FlowExecutor(BaseFlowExecutor):  # pylint: disable=abstract-method
+    """Null dataflow executor proxy.
+
+    This executor is intended to be used in tests where you want to save
+    the object to the database but don't need to run it.
+    """
+
+    name = 'null'
+
+    def run(self, data_id, script, verbosity=1):
+        """Do nothing :)."""
+        send_manager_command(ExecutorProtocol.FINISH)
+
+    def terminate(self, data_id):
+        """Do nothing :)."""
+        pass
