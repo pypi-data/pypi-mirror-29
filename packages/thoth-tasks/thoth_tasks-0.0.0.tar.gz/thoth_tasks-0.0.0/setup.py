dca09f2df0773adc9598d65a85d062afffca9621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:26 +0000

    Add package thoth-tasks/0.0.0/thoth_tasks-0.0.0.tar.gz

---
+++ packages/thoth-tasks/thoth_tasks-0.0.0.tar.gz/thoth_tasks-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='thoth_tasks',
+    version='0.0.0',
+    description='Tasks available in project Thoth',
+    long_description='Tasks available in project Thoth',
+    author='Fridolin Pokorny',
+    author_email='fridolin@redhat.com',
+    license='GPLv2+',
+    packages=['thoth.tasks'],
+    zip_safe=False,
+)
