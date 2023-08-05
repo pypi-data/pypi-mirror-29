8c08e6a3d598e6633eb8b1f5c7c97c20506589be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:14 +0000

    Add package sampleapp/0.0.1/sampleapp-0.0.1-py3.5.egg

---
+++ packages/sampleapp/sampleapp-0.0.1-py3.5.egg/sampleapp/app.py
@@ -0,0 +1,22 @@
+# app entry
+
+APP_DESC = """
+
+          --           ------       ------
+        /    \         | ___  \     | ___  \ 
+       /______\        | |_ / /     | |_ / /
+      /        \       |_____/      |_____/
+     /          \      |            |
+    /            \     |            |
+
+                        ---- A Sample App
+
+                        @author YT (hyteer@gmail.com)
+
+"""
+
+def main():
+    print(APP_DESC)
+
+if __name__ == '__main__':
+    main()
