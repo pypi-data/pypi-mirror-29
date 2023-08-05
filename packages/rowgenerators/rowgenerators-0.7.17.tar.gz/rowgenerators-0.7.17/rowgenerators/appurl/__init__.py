abf30e2b07b34c12caaf7332f1b58cf3da64919e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:00 +0000

    Add package rowgenerators/0.7.17/rowgenerators-0.7.17.tar.gz

---
+++ packages/rowgenerators/rowgenerators-0.7.17.tar.gz/rowgenerators-0.7.17/rowgenerators/appurl/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2017 Civic Knowledge. This file is licensed under the terms of the
+# MIT, included in this distribution as LICENSE
+
+"""
+
+
+"""
+
+from .url import Url, parse_app_url, match_url_classes
+from .file import *
+from .archive import *
+from .web import *
+from .util import get_cache
+
+
