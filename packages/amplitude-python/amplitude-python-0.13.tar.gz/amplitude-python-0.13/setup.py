0b30e4152e8865794f2898b37a70a925f9fad5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:10 +0000

    Add package amplitude-python/0.13/amplitude-python-0.13.tar.gz

---
+++ packages/amplitude-python/amplitude-python-0.13.tar.gz/amplitude-python-0.13/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amplitude-python',
+    keywords='python wrapper for amplitude.com HTTP API',
+    version='0.13',
+    packages=find_packages(),
+    long_description=open('README.txt').read(),
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    install_requires=['requests','nose','requests-mock[fixture]','testtools','twine'],
+    url='https://github.com/atveit/amplitude-python',
+    author='Amund Tveit',
+    author_email='atveit@gmail.com',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Logging',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        ],
+)
