172ca251d8f66bbd83faeab129299ac6140eafbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:07 +0000

    Add package aldryn-boilerplates/0.7.7/aldryn-boilerplates-0.7.7.tar.gz

---
+++ packages/aldryn-boilerplates/aldryn-boilerplates-0.7.7.tar.gz/aldryn-boilerplates-0.7.7/HISTORY.rst
@@ -0,0 +1,59 @@
+0.7.5 (2017-02-28)
+------------------
+
+* add Django 1.10 support
+
+
+0.7.4 (2016-01-27)
+------------------
+
+* add Django 1.9 support
+
+
+0.7.3 (2015-10-29)
+------------------
+
+* add Django 1.5 support
+
+
+0.7.2 (2015-08-19)
+------------------
+
+* add Django 1.8 support
+
+
+0.7.1 (2015-06-18)
+------------------
+
+* add Django 1.7 support
+
+
+0.6 (2015-02-02)
+----------------
+
+* new folder conventions:
+    ``templates_for_boilerplates/<my-boilerplate>`` is now ``boilerplates/<my-boilerplate>/templates``
+
+    ``static_for_boilerplates/<my-boilerplate>`` is now ``boilerplates/<my-boilerplate>/static``
+
+
+0.5 (2014-12-28)
+----------------
+
+* fix template discovery for apps without a “templates” folder
+* more tests
+
+
+0.4 (2014-12-28)
+----------------
+
+* more tests
+* document how to add support to existing packages
+* add djangocms-helper test dependencies
+* add tests for loading templates from different boilerplates
+
+
+0.3 (2014-12-16)
+----------------
+
+Initial Release
---
+++ packages/aldryn-boilerplates/aldryn-boilerplates-0.7.7.tar.gz/aldryn-boilerplates-0.7.7/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from setuptools import setup, find_packages
+import re
+import sys
+
+module_file = open("aldryn_boilerplates/__init__.py").read()
+metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", module_file))
+py26 = (2, 7, 0) > sys.version_info >= (2, 6, 0)
+
+REQUIREMENTS = [
+    'Django>=1.8,<2.0',
+    'YURL>=0.13',
+    'django-appconf',
+]
+
+if py26:
+    REQUIREMENTS += [
+        'ordereddict',
+    ]
+
+
+setup(
+    name='aldryn-boilerplates',
+    author=metadata['author'],
+    author_email=metadata['email'],
+    version=metadata['version'],
+    url=metadata['url'],
+    license=metadata['license'],
+    platforms=['OS Independent'],
+    description=metadata['doc'],
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
