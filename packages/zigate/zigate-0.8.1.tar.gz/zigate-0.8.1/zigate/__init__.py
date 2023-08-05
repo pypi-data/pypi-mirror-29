2c69f0c894e65c58f66220321f42d79e03c4d3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:45 +0000

    Add package zigate/0.8.1/zigate-0.8.1.tar.gz

---
+++ packages/zigate/zigate-0.8.1.tar.gz/zigate-0.8.1/zigate/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from .core import (ZiGate, ZiGateWiFi)
+from .const import *
+from pydispatch import dispatcher
+
+__version__ = '0.8.1'
+
+__all__ = ['ZiGate', 'ZiGateWiFi']
