aa1774f6a0e98cbda6ed953f12d39539f0ea0636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:54 +0000

    Add package ezprogress/1.0.0/ezprogress-1.0.0.tar.gz

---
+++ packages/ezprogress/ezprogress-1.0.0.tar.gz/ezprogress-1.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='ezprogress',
+    version='1.0.0',
+    description='Simple, easy to use progress bar.',
+    author='Jonas Kloster Jacobsen',
+    author_email='joklost@gmail.com',
+    license='GPL-3.0',
+    packages=['ezprogress'],
+    url='https://github.com/Joklost/ezprogress',
+    zip_safe=False
+)
