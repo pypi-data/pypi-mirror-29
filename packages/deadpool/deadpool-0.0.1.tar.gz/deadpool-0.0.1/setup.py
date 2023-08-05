8bacd40996fca52b43d1ff7641e363587e9fbed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:21 +0000

    Add package deadpool/0.0.1/deadpool-0.0.1.tar.gz

---
+++ packages/deadpool/deadpool-0.0.1.tar.gz/deadpool-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="deadpool",
+    version="0.0.1",
+    author="Deji Joseph",
+    author_email="wrecodde@gmail.com",
+    url="https://github.com/wrecodde/deadpool",
+    description="wait.. hes gonna do a super hero landing",
+    )
