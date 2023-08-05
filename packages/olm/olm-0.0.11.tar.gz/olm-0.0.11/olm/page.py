b864c478751e5438671eccf3353869d852df08bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:53 +0000

    Add package olm/0.0.11/olm-0.0.11.tar.gz

---
+++ packages/olm/olm-0.0.11.tar.gz/olm-0.0.11/olm/page.py
@@ -0,0 +1,28 @@
+import os
+from os.path import dirname, splitext, relpath, join
+import datetime
+import codecs
+import re
+from olm.signals import signals, Signal
+from olm.source import Source
+
+class Page(Source):
+    """Object representing an article"""
+
+    def __init__(self, context, filepath=None):
+        super().__init__(context, filepath)
+
+        self.template        = 'page.html'
+        self.title           = self.metadata['title'] if 'title' in self.metadata else basename
+        self.url             = self.relpath
+        self.output_filepath = join(context.OUTPUT_FOLDER, self.relpath)
+
+        self.cache_id = self.output_filepath
+        self.cache_type = 'PAGE'
+
+        signal_sender = Signal(signals.AFTER_PAGE_READ)
+        signal_sender.send(context=context, page=self)
+
+
+    def write_file(self, context=None):
+        super().write_file(context, page=self)
