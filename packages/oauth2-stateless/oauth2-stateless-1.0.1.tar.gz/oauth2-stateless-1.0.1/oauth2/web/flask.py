a38cfc045557b2aa8d55aa3ae93e457c246979d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:10 +0000

    Add package oauth2-stateless/1.0.1/oauth2-stateless-1.0.1.tar.gz

---
+++ packages/oauth2-stateless/oauth2-stateless-1.0.1.tar.gz/oauth2-stateless-1.0.1/oauth2/web/flask.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Classes for handling flask HTTP request/response flow.
+"""
+
+from functools import wraps
+from typing import Dict, Optional
+
+from flask import request
+
+
+class Request(object):
+    """Contains data of the current HTTP request."""
+
+    def __init__(self, request):
+        self.request = request
+
+    @property
+    def method(self):
+        return self.request.method
+
+    @property
+    def path(self):
+        return self.request.path
+
+    @property
+    def query_string(self):
+        return self.request.query_string
+
+    def get_param(self, name, default=None):
+        return self.request.args.get(name, default)
+
+    def post_param(self, name, default=None):
+        if self.header('Content-Type') == 'application/json':
+            return self.request.json.get(name, default)
+        return self.request.form.get(name, default)
+
+    def header(self, name, default=None):
+        return self.request.headers.get(name, default)
+
+
+def oauth_request_hook(oauth2_server):
+    """Initialise Oauth2 interface bewtween flask and oauth2 server"""
+
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_fn(*args, **kwargs):
+            # We are not call fn(args, kwargs) because oauth.dispatch should doing that.
+            response = oauth2_server.provider.dispatch(Request(request), request.environ)
+            return (response.body, response.status_code, response.headers.items())
+        return decorated_fn
+    return wrapper
