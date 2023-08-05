af0de355a8af894782b181d3d7ebab27fca4bcd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:18 +0000

    Add package pymap3d/1.5.2/pymap3d-1.5.2.tar.gz

---
+++ packages/pymap3d/pymap3d-1.5.2.tar.gz/pymap3d-1.5.2/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2014-2018 Michael Hirsch, Ph.D.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+install_requires = ['six','python-dateutil','pytz']
+tests_require = ['nose','coveralls']
+# %%
+from setuptools import setup,find_packages
+
+setup(name='pymap3d',
+      packages=find_packages(),
+      version = '1.5.2',
+      description='pure Python coordinate conversions, following convention of several popular Matlab routines.',
+      long_description=open('README.rst').read(),
+      author = 'Michael Hirsch, Ph.D.',
+      url = 'https://github.com/scivision/pymap3d',
+      classifiers=[
+      'Intended Audience :: Science/Research',
+      'Development Status :: 5 - Production/Stable',
+      'License :: OSI Approved :: BSD License',
+      'Topic :: Scientific/Engineering :: GIS',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python',
+      ],
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require={'tests':tests_require,
+                      'full':['numpy','astropy']},
+      python_requires='>=2.6', 
+	  )
+
