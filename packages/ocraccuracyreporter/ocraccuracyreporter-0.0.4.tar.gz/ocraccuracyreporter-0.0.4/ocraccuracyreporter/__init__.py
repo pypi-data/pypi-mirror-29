727ae6189af5be61356596af320ce785a563de8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:18 +0000

    Add package ocraccuracyreporter/0.0.4/ocraccuracyreporter-0.0.4.tar.gz

---
+++ packages/ocraccuracyreporter/ocraccuracyreporter-0.0.4.tar.gz/ocraccuracyreporter-0.0.4/ocraccuracyreporter/__init__.py
@@ -0,0 +1,7 @@
+"""
+Your OCR pipeline may have various stages and may use various tools.
+
+You need a simple way to run samples as a whole or piece by piece and
+have a way to state your OCR accuracy.
+"""
+from .version import __version__
---
+++ packages/ocraccuracyreporter/ocraccuracyreporter-0.0.4.tar.gz/ocraccuracyreporter-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+from ocraccuracyreporter import __version__
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='ocraccuracyreporter',
+    version=__version__,
+    author='Lucid Programmer',
+    author_email='lucidprogrammer@hotmail.com',
+    packages=find_packages(exclude=['docs', 'tests']),
+    py_modules=["oar"],
+    url='https://github.com/lucidprogrammer/ocraccuracyreporter',
+    license='MIT',
+    description='OCR Accuracy Reporter',
+    long_description=long_description,
+    test_suite='tests.oar_suite',
+    install_requires=[
+        'python-Levenshtein',
+        'fuzzywuzzy',
+    ]
+)
