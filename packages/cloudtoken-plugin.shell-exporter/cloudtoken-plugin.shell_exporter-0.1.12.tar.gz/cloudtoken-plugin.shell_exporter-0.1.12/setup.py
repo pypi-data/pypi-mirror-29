0ca4c0eb38ca9232b0b43379660040c4e8cb6888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:45 +0000

    Add package cloudtoken-plugin.shell-exporter/0.1.12/cloudtoken-plugin.shell_exporter-0.1.12.tar.gz

---
+++ packages/cloudtoken-plugin.shell-exporter/cloudtoken-plugin.shell_exporter-0.1.12.tar.gz/cloudtoken-plugin.shell_exporter-0.1.12/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os
+
+version = os.environ['VERSION']
+
+setup(
+    name='cloudtoken-plugin.shell_exporter',
+    version=version,
+    description='Shell exporter for cloudtoken.',
+    url='https://bitbucket.org/atlassian/cloudtoken',
+    author='Atlassian Cloud Engineering',
+    author_email='cloud-team@atlassian.com',
+    license='Apache',
+    py_modules=['cloudtoken_plugin.export_credentials_shell'],
+    zip_safe=False,
+    python_requires='>=3.5',
+    install_requires=[],
+)
