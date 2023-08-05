a69c81642aa367b0ef414975fa239a52bb296093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:54 +0000

    Add package niltechArduino/1.0.3/niltechArduino-1.0.3.tar.gz

---
+++ packages/niltechArduino/niltechArduino-1.0.3.tar.gz/niltechArduino-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'niltechArduino',
+  packages = ['niltechArduino'], # this must be the same as the name above
+  version = '1.0.3',
+  description = 'A lib to automatically detect and give usb port which is connected to arduino and also extract incoming data using start and stop bytes',
+  author = 'Vaibhav VK - Niltech',
+  author_email = 'vaibhav@niltech.in',
+  #url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  #download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['Arduino','getPort','readData','extractData'], # arbitrary keywords
+  classifiers = [],
+)
