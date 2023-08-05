864873ef603412904fc07b412d774446dcc593c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:19 +0000

    Add package cutil/2.6.2/cutil-2.6.2.tar.gz

---
+++ packages/cutil/cutil-2.6.2.tar.gz/cutil-2.6.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+
+setup(
+    name='cutil',
+    packages=['cutil'],
+    version='2.6.2',
+    description='A collection of useful functions',
+    author='Eddy Hintze',
+    author_email="eddy.hintze@gmail.com",
+    url="https://github.com/xtream1101/cutil",
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    install_requires=['hashids',
+                      'psycopg2',
+                      'pytz',
+                      ]
+)
