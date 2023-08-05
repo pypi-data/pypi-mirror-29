9b94ef5ebcfcf8306aa0b28b0d59a1365dfef5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:35 +0000

    Add package pyproach/0.0.0/pyproach-0.0.0.tar.gz

---
+++ packages/pyproach/pyproach-0.0.0.tar.gz/pyproach-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+from setuptools import setup
+
+setup(
+    name="pyproach",
+    version="0.0.0",
+    maintainer='Thibaud Castaing',
+    maintainer_email='t-cas@users.noreply.github.com',
+    # url='https://www.python.org/sigs/distutils-sig/',
+)
