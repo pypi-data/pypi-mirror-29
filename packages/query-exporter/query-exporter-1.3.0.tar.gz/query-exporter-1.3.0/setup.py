af90cf24abb22f20b022f0475e8f8a1219ac2a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:34 +0000

    Add package query-exporter/1.3.0/query-exporter-1.3.0.tar.gz

---
+++ packages/query-exporter/query-exporter-1.3.0.tar.gz/query-exporter-1.3.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+from query_exporter import __version__, __doc__ as description
+
+tests_require = ['asynctest']
+
+config = {
+    'name': 'query-exporter',
+    'version': __version__,
+    'license': 'GPLv3+',
+    'description': description,
+    'long_description': open('README.rst').read(),
+    'author': 'Alberto Donato',
+    'author_email': 'alberto.donato@gmail.com',
+    'maintainer': 'Alberto Donato',
+    'maintainer_email': 'alberto.donato@gmail.com',
+    'url': 'https://github.com/albertodonato/query-exporter',
+    'packages': find_packages(),
+    'include_package_data': True,
+    'entry_points': {'console_scripts': [
+        'query-exporter = query_exporter.main:script']},
+    'test_suite': 'query_exporter',
+    'install_requires': [
+        'aiohttp',
+        'prometheus-client',
+        'prometheus-aioexporter >= 1.4.0',
+        'PyYaml',
+        'SQLAlchemy',
+        'sqlalchemy_aio',
+        'toolrack'],
+    'tests_require': tests_require,
+    'extras_require': {'testing': tests_require},
+    'keywords': 'sql metric prometheus exporter',
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities']}
+
+setup(**config)
