6d386db7e5193621420695599705388ab08a6a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:31 +0000

    Add package tfweb/0.1/tfweb-0.1.tar.gz

---
+++ packages/tfweb/tfweb-0.1.tar.gz/tfweb-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(name='tfweb',
+      packages=['tfweb'],
+      version='0.1',
+      description='Server for exposing tensorflow models though HTTP JSON API',
+      author='Olav Nymoen',
+      author_email='olav@olavnymoen.com',
+      url='https://github.com/olavhn/tfweb',
+      download_url='https://github.com/olavhn/tfweb/archive/0.2.tar.gz',
+      keywords=['serving', 'tensorflow', 'asyncio', 'aiohttp', 'grpc'],
+      classifiers=[],
+      scripts=['bin/tfweb'])
