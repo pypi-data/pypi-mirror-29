721523c5e251be602a9c6834aede6f172f52a977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:45 +0000

    Add package django-rest-mock/0.1.3/django-rest-mock-0.1.3.tar.gz

---
+++ packages/django-rest-mock/django-rest-mock-0.1.3.tar.gz/django-rest-mock-0.1.3/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='django-rest-mock',
+    version='0.1.3',
+    packages=['rest_mock_server'],
+    description='Mock Express server generated based on your views, that can come in handy when developing REST APIs with Django',
+    long_description=README,
+    author='Thomas Jiang',
+    author_email='thomasjiangcy@gmail.com',
+    url='https://github.com/thomasjiangcy/django-rest-mock-server',
+    license='MIT',
+    install_requires=[
+        'Django>=1.11',
+        'Faker>=0.8.11'
+    ],
+    zip_safe=False
+)
