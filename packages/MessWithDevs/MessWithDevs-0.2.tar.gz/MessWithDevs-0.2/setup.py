56e068411461418b1188d1b78cafda35d05b1eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package MessWithDevs/0.2/MessWithDevs-0.2.tar.gz

---
+++ packages/MessWithDevs/MessWithDevs-0.2.tar.gz/MessWithDevs-0.2/setup.py
@@ -0,0 +1,24 @@
+#============================================================================
+# Name        : setup.py
+# Author      : Alex James Wright
+# Version     : 0.1
+# Copyright   : MIT
+# Description : PyPi setup file
+#============================================================================
+
+from setuptools import setup
+
+setup(name='MessWithDevs',
+      version='0.2',
+      description='Swaps characters in a text file with indistinguishable doppelgangers to break code (damage is reversible!)',
+      url='https://github.com/AlexJamesWright/MessWithDevs',
+      author='Alex James Wright',
+      author_email='a.j.wright@soton.ac.uk',
+      license='MIT',
+      packages=['MessWithDevs'],
+      entry_points={'console_scripts': ['mwd=MessWithDevs.commandLine:main']},
+      include_package_data=True,
+      keywords='MessWithDevs',
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'])
