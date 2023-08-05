b01535854ef49c27c65fe20aee2847771fdfb361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:53 +0000

    Add package airflow-xplenty/3.2.0/airflow_xplenty-3.2.0.tar.gz

---
+++ packages/airflow-xplenty/airflow_xplenty-3.2.0.tar.gz/airflow_xplenty-3.2.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from airflow_xplenty.version import version
+
+def do_setup():
+    setup(
+      name = 'airflow_xplenty',
+      packages = ['airflow_xplenty', 'airflow_xplenty.operators'],
+      version = version,
+      description = 'Airflow wrappers for the Xplenty API',
+      author = 'Tom Collier',
+      author_email = 'collier@apartmentlist.com',
+      url = 'https://github.com/apartmentlist/airflow_xplenty',
+      download_url =
+        ('https://github.com/apartmentlist/airflow_xplenty/archive/' + version + '.tar.gz'),
+      keywords = ['airflow', 'xplenty'],
+      install_requires = ['apache-airflow (>= 1.8.2)', 'xplenty (>= 1.1.2)'],
+      classifiers = []
+    )
+
+if __name__ == "__main__":
+    do_setup()
