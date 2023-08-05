aa1ebab329c2006f2d20f96ed4c4cf77f1ea71e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:26 +0000

    Add package fPrintNester/1.0.3/fPrintNester-1.0.3.zip

---
+++ packages/fPrintNester/fPrintNester-1.0.3.zip/fPrintNester-1.0.3/PrintNester.py
@@ -0,0 +1,28 @@
+
+"""
+这是nester.py模块，提供了一个名为print_list（）的函数，这个函数的作用是打印列表，
+其中有可能包含（可能不包含）嵌套列表
+"""
+'''
+This is the "nester.py" module and it privdes one function called "print_list()"
+which prints lists that may or may not include nested listed.
+'''
+
+def print_list (the_list,indent=False,level=0):
+    """
+    这个函数去一个位置参数the_list，这可以是任何Python列表（也可以是包含嵌套列表的列表）。
+    所指定的列表中的每个数据项会（递归地）输出到屏幕上，各数据项各占一行
+    """
+    '''
+    This function takes one positional argument called "the_list", which is any Python
+    list (of-possibly-nested lists).Each data item in the privided list is (recursicely)
+    printed to the screen on it's own line.
+    '''
+    for each_item in the_list:
+        if isinstance (each_item,list):
+            print_list(each_item, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(each_item)
---
+++ packages/fPrintNester/fPrintNester-1.0.3.zip/fPrintNester-1.0.3/setup.py
@@ -0,0 +1,32 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'fPrintNester',
+    version = '1.0.3',
+    py_modules = ['PrintNester'],
+    author = "qsl",  
+    author_email = "Qsl_xj@outlook.com",
+    url = "http://test.com",
+    description = '一个简单的打印列表模块，函数def print_list (the_list,indent=False,level=0)，可以自由设定打印是否需要tab'
+    )
+    
+
+
+'''
+from setuptools import setup
+  
+setup(  
+    name = "nester",  
+    version = "1.0.0",
+    py_modules = ['nester'],
+      
+    author = "qsl",  
+    author_email = "Qsl_xj@outlook.com",
+    url = "http://test.com",
+    
+    description = 'A simple printer of nested lists'
+
+)
+
+'''
