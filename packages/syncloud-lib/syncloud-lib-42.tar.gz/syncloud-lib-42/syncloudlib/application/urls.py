4ea0759f4a06e13830bacfd0eff4f82ffc7cca9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:07 +0000

    Add package syncloud-lib/42/syncloud-lib-42.tar.gz

---
+++ packages/syncloud-lib/syncloud-lib-42.tar.gz/syncloud-lib-42/syncloudlib/application/urls.py
@@ -0,0 +1,9 @@
+from connection import api_get
+
+
+def get_app_url(app):
+    return api_get('/app/url?name={0}'.format(app))
+
+
+def get_app_domain_name(app):
+    return api_get('/app/domain_name?name={0}'.format(app))
