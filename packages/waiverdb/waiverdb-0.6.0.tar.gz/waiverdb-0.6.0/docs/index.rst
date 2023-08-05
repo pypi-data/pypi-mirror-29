4e27335b3d15a44421ee375f145fd9a2b86dc302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:38 +0000

    Add package waiverdb/0.6.0/waiverdb-0.6.0.tar.gz

---
+++ packages/waiverdb/waiverdb-0.6.0.tar.gz/waiverdb-0.6.0/docs/index.rst
@@ -0,0 +1,25 @@
+========
+WaiverDB
+========
+WaiverDB is a companion service to `ResultsDB`_, for recording waivers against
+test results.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   developer-guide
+   api
+   release-notes
+   todo
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+.. _ResultsDB: https://pagure.io/taskotron/resultsdb
---
+++ packages/waiverdb/waiverdb-0.6.0.tar.gz/waiverdb-0.6.0/tests/conftest.py
@@ -0,0 +1,60 @@
+# SPDX-License-Identifier: GPL-2.0+
+
+import pytest
+from waiverdb.app import create_app, init_db
+
+
+@pytest.fixture(scope='session')
+def app(request):
+    app = create_app('waiverdb.config.TestingConfig')
+    # Establish an application context before running the tests.
+    ctx = app.app_context()
+    ctx.push()
+
+    def teardown():
+        ctx.pop()
+
+    request.addfinalizer(teardown)
+    return app
+
+
+@pytest.fixture(scope='session')
+def db(app):
+    """Session-wide test database."""
+    db = init_db(app)
+    return db
+
+
+@pytest.yield_fixture
+def session(db, monkeypatch):
+    """Patch Flask-SQLAlchemy to use a specific connection"""
+    connection = db.engine.connect()
+    transaction = connection.begin()
+
+    # Patch Flask-SQLAlchemy to use our connection
+    monkeypatch.setattr(db, 'get_engine', lambda *args: connection)
+
+    yield db.session
+
+    db.session.remove()
+    transaction.rollback()
+    connection.close()
+
+
+@pytest.yield_fixture
+def client(app):
+    """A Flask test client. An instance of :class:`flask.testing.TestClient`
+    by default.
+    """
+    with app.test_client() as client:
+        yield client
+
+
+@pytest.fixture()
+def enable_kerberos(app, monkeypatch):
+    monkeypatch.setitem(app.config, 'AUTH_METHOD', 'Kerberos')
+
+
+@pytest.fixture()
+def enable_ssl(app, monkeypatch):
+    monkeypatch.setitem(app.config, 'AUTH_METHOD', 'SSL')
