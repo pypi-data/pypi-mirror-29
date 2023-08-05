0958992b0c533aed0c660fb3806367e4d9cd2f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:37 +0000

    Add package smsc-python/1.0.2/smsc-python-1.0.2.tar.gz

---
+++ packages/smsc-python/smsc-python-1.0.2.tar.gz/smsc-python-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='smsc-python',
+    packages=['smsc'],
+    version='1.0.2',
+    description='Send SMS with https://www.smsc.com.ar/',
+    author='Joaquin Moine',
+    author_email='joaquinmoine@gmail.com',
+    url='https://github.com/joaquinmoine/smsc-python',
+    license='MIT',
+    keywords=['sms', 'send'],
+    install_requires=['requests>=2.18.4'],
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
