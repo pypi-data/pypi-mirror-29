940135c20a89f9b199144f29d9c456f6c3ae9c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:25 +0000

    Add package utilities-package/0.0.4/utilities-package-0.0.4.tar.gz

---
+++ packages/utilities-package/utilities-package-0.0.4.tar.gz/utilities-package-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import sys
+if sys.version_info.major < 3:
+    sys.exit('Python 3 required but lower version found. Aborted.')
+
+from setuptools import setup
+
+setup(
+    name='utilities-package',
+    version='0.0.4',
+    description='utilities package',
+    url='https://github.com/terminal-labs/utilities',
+    author='Terminal Labs',
+    author_email='solutions@terminallabs.com',
+    license="license",
+    packages=['utilities'],
+    zip_safe=False,
+    install_requires=[
+        'six',
+        'bash',
+        'termcolor',
+    ],
+    )
