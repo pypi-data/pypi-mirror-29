ee90feb318e19a38918baadc62659ceb9e044888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:06 +0000

    Add package numina/0.15/numina-0.15-cp34-cp34m-manylinux1_x86_64.whl

---
+++ packages/numina/numina-0.15-cp34-cp34m-manylinux1_x86_64.whl/numina/frame/__init__.py
@@ -0,0 +1,98 @@
+#
+# Copyright 2008-2014 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# Numina is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Numina is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Numina.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import warnings
+import six
+from astropy.io import fits
+
+from numina.array import resize_array
+
+
+def get_hdu_shape(header):
+    ndim = header['naxis']
+    return tuple(header.get('NAXIS%d' % i) for i in range(1, ndim + 1))
+
+
+def custom_slice_to_str(slc):
+    if slc.step is None:
+        return '%i:%i' % (slc.start, slc.stop)
+    else:
+        return '%i:%i:%i' % (slc.start, slc.stop, slc.step)
+
+
+def custom_region_to_str(region):
+    jints = [custom_slice_to_str(slc) for slc in region]
+    return '[' + ','.join(jints) + ']'
+
+
+def resize_hdu(hdu, newshape, region, window=None, fill=0.0,
+               scale=1, conserve=True, dtype=None):
+    basedata = hdu.data
+    newdata = resize_array(basedata, newshape, region, window=window,
+                           fill=fill, scale=scale, conserve=conserve,
+                           dtype=dtype)
+    hdu.header['NVALREGI'] = (custom_region_to_str(region),
+                              'Valid region of resized FITS')
+    if window:
+        hdu.header['OVALREGI'] = (custom_region_to_str(window),
+                                  'Valid region of original FITS')
+    newhdu = fits.PrimaryHDU(newdata, hdu.header)
+    return newhdu
+
+
+def resize_hdul(hdul, newshape, region, extensions=None, window=None,
+                scale=1, fill=0.0, conserve=True):
+    from numina.frame import resize_hdu
+    if extensions is None:
+        extensions = [0]
+
+    nhdul = [None] * len(hdul)
+    for ext, hdu in enumerate(hdul):
+        if ext in extensions:
+            nhdul[ext] = resize_hdu(hdu, newshape,
+                                    region, fill=fill,
+                                    window=window,
+                                    scale=scale,
+                                    conserve=conserve)
+        else:
+            nhdul[ext] = hdu
+    return fits.HDUList(nhdul)
+
+
+def resize_fits(fitsfile, newfilename, newshape, region, window=None,
+                scale=1, fill=0.0, clobber=True, conserve=True, dtype=None):
+
+    close_on_exit = False
+    if isinstance(fitsfile, six.string_types):
+        hdulist = fits.open(fitsfile, mode='readonly')
+        close_on_exit = True
+    else:
+        hdulist = fitsfile
+
+    try:
+        hdu = hdulist['primary']
+        newhdu = resize_hdu(hdu, newshape, region, fill=fill,
+                            window=window, scale=scale, conserve=conserve,
+                            dtype=dtype)
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            newhdu.writeto(newfilename, clobber=clobber)
+    finally:
+        if close_on_exit:
+            hdulist.close()
---
+++ packages/numina/numina-0.15-cp34-cp34m-manylinux1_x86_64.whl/numina/types/dataframe.py
@@ -0,0 +1,58 @@
+#
+# Copyright 2008-2014 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# Numina is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Numina is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Numina.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+'''
+Basic Data Products
+'''
+
+from astropy.io import fits
+
+
+class DataFrame(object):
+    '''A handle to a image in disk or in memory.'''
+    def __init__(self, frame=None, filename=None):
+        if frame is None and filename is None:
+            raise ValueError('only one in frame and filename can be None')
+        self.frame = frame
+        self.filename = filename
+
+    def open(self):
+        if self.frame is None:
+            return fits.open(self.filename, mode='readonly')
+        else:
+            return self.frame
+
+    @property
+    def label(self):
+        return self.filename
+
+    def __repr__(self):
+        if self.frame is None:
+            return "DataFrame(filename=%r)" % self.filename
+        elif self.filename is None:
+            return "DataFrame(frame=%r)" % self.frame
+        else:
+            fmt = "DataFrame(filename=%r, frame=%r)"
+            return fmt % (self.filename, self.frame)
+
+    def __numina_load__(self, obj):
+        if obj is None:
+            return None
+        else:
+            return DataFrame(filename=obj)
