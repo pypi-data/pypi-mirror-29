e2b21f76afdfb077e997c85efe81fcd820661920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:46 +0000

    Add package niltechArduino/0.3/niltechArduino-0.3.tar.gz

---
+++ packages/niltechArduino/niltechArduino-0.3.tar.gz/niltechArduino-0.3/niltechArduino/arduinoPort.py
@@ -0,0 +1,14 @@
+
+import serial.tools.list_ports
+ports = list(serial.tools.list_ports.comports())
+
+for i in range (len(ports)):
+    print ports[i]
+    if 'Arduino' in str(ports[i]):
+        #print 'yes'
+        l = str(ports[i]).split(" ")
+        #print l[0]
+        return l[0]
+        break
+    else:
+        print 'Arduino Not Found'
---
+++ packages/niltechArduino/niltechArduino-0.3.tar.gz/niltechArduino-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'niltechArduino',
+  packages = ['niltechArduino'], # this must be the same as the name above
+  version = '0.3',
+  description = 'A lib to automatically detect and give usb port which is connected to arduino',
+  author = 'Vaibhav VK - Niltech',
+  author_email = 'vaibhav@niltech.in',
+  #url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  #download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['arduinoPort'], # arbitrary keywords
+  classifiers = [],
+)
