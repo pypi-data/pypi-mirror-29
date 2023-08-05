781b12d31f95db2b7c7f367e0ab53544c4025522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:51 +0000

    Add package guillotina/2.3.24/guillotina-2.3.24.tar.gz

---
+++ packages/guillotina/guillotina-2.3.24.tar.gz/guillotina-2.3.24/guillotina/interfaces/exceptions.py
@@ -0,0 +1,29 @@
+from zope.interface.common.interfaces import IAttributeError
+from zope.interface.common.interfaces import IException
+
+
+class ISerializableException(IException):
+    """
+    An exception that can be deserialized
+    """
+
+    def json_data():
+        """
+        return json serializable data to be used
+        with exception data in responses
+        """
+
+
+class IUnauthorized(IException):
+    '''
+    '''
+
+
+class IForbidden(IException):
+    '''
+    '''
+
+
+class IForbiddenAttribute(IForbidden, IAttributeError):
+    '''
+    '''
