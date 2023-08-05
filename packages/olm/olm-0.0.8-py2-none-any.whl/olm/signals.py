ea3139d87beb67b34e27ac1561a2ac0bffdd7b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:37 +0000

    Add package olm/0.0.8/olm-0.0.8-py2-none-any.whl

---
+++ packages/olm/olm-0.0.8-py2-none-any.whl/olm/signals.py
@@ -0,0 +1,31 @@
+from blinker import signal
+from olm.helper import Map
+from olm.logger import get_logger
+
+logger = get_logger('olm.signal')
+
+signals = Map({
+    'INITIALISED':              "INITIALISED",             # args: context
+    'SITE_INITIALISED':         "SITE_INITIALISED",        # args: context
+    "BEFORE_MD_CONVERT":        "BEFORE_MD_CONVERT",       # args: context, content      
+    'AFTER_ARTICLE_READ':       "AFTER_ARTICLE_READ",      # args: context, article
+    'AFTER_PAGE_READ':          "AFTER_PAGE_READ",         # args: context, article
+    'AFTER_ALL_ARTICLES_READ':  "AFTER_ALL_ARTICLES_READ", # args: context, articles
+    'BEFORE_WRITING':           "BEFORE_WRITING",          # args: context
+    'BEFORE_ARTICLE_WRITE':     "BEFORE_ARTICLE_WRITE",    # args: context, article
+    'AFTER_WRITING':            "AFTER_WRITING"            # args: context, article
+})
+
+class Signal:
+    def __init__(self, signal_value):
+        logger.debug('Instantiating new Signal "%s"', signal_value)
+        if signal_value not in signals:
+            logger.error('No such signal %s', signal_value)
+            raise Exception('No such signal %s', signal_value)
+        self.signal_value = signal_value
+        self.sender = signal(signal_value)
+
+    def send(self, **kwargs):
+        logger.debug('Sending signal "%s"', self.signal_value)
+        self.sender.send(self.signal_value, **kwargs)
+
