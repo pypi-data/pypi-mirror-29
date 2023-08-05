60b71a801d9c98e4c85dbe665023bcf63250370d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package quality-report/2.42.1/quality_report-2.42.1.tar.gz

---
+++ packages/quality-report/quality_report-2.42.1.tar.gz/quality_report-2.42.1/tests/unittests/metric/metric_source_mixin_tests.py
@@ -0,0 +1,89 @@
+"""
+Copyright 2012-2018 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+
+from hqlib import domain, metric_source
+from hqlib.metric.metric_source_mixin import SonarMetric, BirtTestDesignMetric
+
+
+class SonarMetricTest(unittest.TestCase):
+    """ Unit tests for the Sonar metric source class. """
+
+    def test_url(self):
+        """ Test the url. """
+        sonar = metric_source.Sonar('http://sonar/')
+        project = domain.Project(metric_sources={metric_source.Sonar: sonar})
+        product = domain.Product(metric_source_ids={sonar: 'sonar id'})
+        self.assertEqual({sonar.metric_source_name: sonar.url()}, SonarMetric(product, project).url())
+
+    def test_url_without_sonar(self):
+        """ Test that the metric has no url when the project has no Sonar configured. """
+        project = domain.Project()
+        product = domain.Product()
+        self.assertEqual(dict(), SonarMetric(product, project).url())
+
+    def test_url_without_sonar_id(self):
+        """ Test that the metric has a url when the product has no Sonar id configured. """
+        sonar = metric_source.Sonar('http://sonar/')
+        project = domain.Project(metric_sources={metric_source.Sonar: sonar})
+        product = domain.Product()
+        self.assertEqual(dict(), SonarMetric(product, project).url())
+
+
+class FakeBirt(object):
+    """ Fake a Birt instance for test purposes. """
+    # pylint: disable=too-few-public-methods
+
+    metric_source_name = metric_source.Birt.metric_source_name
+
+    @staticmethod
+    def whats_missing_url():
+        """ Return the url for the What's Missing report. """
+        return 'http://birt/whatsmissing/'
+
+
+class BirtTestDesignMetricTest(unittest.TestCase):
+    """ Unit tests for the Birt test design report metric source class. """
+
+    def test_url(self):
+        """ Test the url. """
+        birt = FakeBirt()
+        project = domain.Project(metric_sources={metric_source.Birt: birt})
+        product = domain.Product(metric_source_ids={birt: 'birt id'})
+        self.assertEqual({birt.metric_source_name: birt.whats_missing_url()},
+                         BirtTestDesignMetric(product, project).url())
+
+    def test_url_with_multiple_birts(self):
+        """ Test the url with multiple Birt instances. """
+        birt1, birt2 = FakeBirt(), FakeBirt()
+        project = domain.Project(metric_sources={metric_source.Birt: [birt1, birt2]})
+        product = domain.Product(metric_source_ids={birt1: 'birt id'})
+        self.assertEqual({birt1.metric_source_name: birt1.whats_missing_url()},
+                         BirtTestDesignMetric(product, project).url())
+
+    def test_url_without_birt(self):
+        """ Test that the metric has no url when Birt hasn't been configured. """
+        project = domain.Project()
+        product = domain.Product()
+        self.assertEqual(dict(), BirtTestDesignMetric(product, project).url())
+
+    def test_url_without_birt_id(self):
+        """ Test that the metric has no url when the product has no Birt id. """
+        birt = FakeBirt()
+        project = domain.Project(metric_sources={metric_source.Birt: birt})
+        product = domain.Product()
+        self.assertEqual(dict(), BirtTestDesignMetric(product, project).url())
---
+++ packages/quality-report/quality_report-2.42.1.tar.gz/quality_report-2.42.1/tests/unittests/metric/project/bug_metrics_tests.py
@@ -0,0 +1,168 @@
+"""
+Copyright 2012-2018 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+
+from hqlib import metric, domain, metric_source, requirement
+
+
+class FakeJiraFilter(object):
+    """ Fake Jira filter. """
+
+    metric_source_name = metric_source.JiraFilter.metric_source_name
+
+    # pylint: disable=unused-argument
+
+    @staticmethod
+    def nr_issues(*metric_source_ids):
+        """ Return a fake number of issues. """
+        return 12
+
+    @staticmethod
+    def sum_field(*metric_source_ids):
+        """ Return a fake duration. """
+        return 120
+
+    @staticmethod
+    def nr_issues_with_field_empty(*metric_source_ids):
+        """ Return a fake number of manual test cases without duration filled in. """
+        return 4
+
+    @staticmethod
+    def metric_source_urls(*metric_source_ids):
+        """ Return a fake url for each query. """
+        return ['http://filter/']
+
+
+class OpenBugsTest(unittest.TestCase):
+    """ Unit tests for the number of open bugs metric. """
+
+    def setUp(self):
+        jira = FakeJiraFilter()
+        self.__project = domain.Project(metric_sources={metric_source.BugTracker: jira},
+                                        metric_source_ids={jira: '1234'},
+                                        requirements=[requirement.TrackBugs])
+        self.__metric = metric.OpenBugs(project=self.__project, subject=self.__project)
+
+    def test_value(self):
+        """ Test that the value is correct. """
+        self.assertEqual(FakeJiraFilter.nr_issues(), self.__metric.value())
+
+    def test_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual({FakeJiraFilter.metric_source_name: FakeJiraFilter.metric_source_urls()[0]},
+                         self.__metric.url())
+
+
+class OpenBugsWithProductTest(unittest.TestCase):
+    """ Unit tests for the number of open bugs metric applied to products. """
+
+    def setUp(self):
+        jira = FakeJiraFilter()
+        self.__project = domain.Project(metric_sources={metric_source.BugTracker: jira})
+        self.__product = domain.Product(metric_source_ids={jira: '1234'},
+                                        added_requirements=[requirement.TrackBugs])
+        self.__metric = metric.OpenBugs(project=self.__project, subject=self.__product)
+
+    def test_value(self):
+        """ Test that the value is correct. """
+        self.assertEqual(FakeJiraFilter.nr_issues(), self.__metric.value())
+
+    def test_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual({FakeJiraFilter.metric_source_name: FakeJiraFilter.metric_source_urls()[0]},
+                         self.__metric.url())
+
+
+class OpenSecurityBugsTest(unittest.TestCase):
+    """ Unit tests for the number of open security bugs metric. """
+
+    def setUp(self):
+        jira = FakeJiraFilter()
+        self.__project = domain.Project(metric_sources={metric_source.SecurityBugTracker: jira},
+                                        metric_source_ids={jira: '1234'},
+                                        requirements=[requirement.TrackBugs])
+        self.__metric = metric.OpenSecurityBugs(project=self.__project, subject=self.__project)
+
+    def test_value(self):
+        """ Test that the value is correct. """
+        self.assertEqual(FakeJiraFilter.nr_issues(), self.__metric.value())
+
+    def test_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual({FakeJiraFilter.metric_source_name: FakeJiraFilter.metric_source_urls()[0]},
+                         self.__metric.url())
+
+
+class OpenSecurityBugsWithProductTest(unittest.TestCase):
+    """ Unit tests for the number of open security bugs metric applied to products. """
+
+    def setUp(self):
+        jira = FakeJiraFilter()
+        self.__project = domain.Project(metric_sources={metric_source.SecurityBugTracker: jira},
+                                        metric_source_ids={jira: '1234'},
+                                        requirements=[requirement.TrackBugs])
+        self.__product = domain.Product(metric_source_ids={jira: '1234'},
+                                        added_requirements=[requirement.TrackSecurityBugs])
+        self.__metric = metric.OpenSecurityBugs(project=self.__project, subject=self.__product)
+
+    def test_value(self):
+        """ Test that the value is correct. """
+        self.assertEqual(FakeJiraFilter.nr_issues(), self.__metric.value())
+
+    def test_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual({FakeJiraFilter.metric_source_name: FakeJiraFilter.metric_source_urls()[0]},
+                         self.__metric.url())
+
+
+class OpenStaticSecurityAnalysisBugsTest(unittest.TestCase):
+    """ Unit tests for the number of open static security analysis bugs metric. """
+
+    def setUp(self):
+        jira = FakeJiraFilter()
+        self.__project = domain.Project(metric_sources={metric_source.StaticSecurityBugTracker: jira},
+                                        metric_source_ids={jira: '1234'},
+                                        requirements=[requirement.TrackBugs])
+        self.__metric = metric.OpenStaticSecurityAnalysisBugs(project=self.__project, subject=self.__project)
+
+    def test_value(self):
+        """ Test that the value is correct. """
+        self.assertEqual(FakeJiraFilter.nr_issues(), self.__metric.value())
+
+    def test_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual({FakeJiraFilter.metric_source_name: FakeJiraFilter.metric_source_urls()[0]},
+                         self.__metric.url())
+
+
+class TechnicalDebtIssuesTest(unittest.TestCase):
+    """ Unit tests for the number of technical debt issues metric. """
+
+    def setUp(self):
+        jira = FakeJiraFilter()
+        self.__project = domain.Project(metric_sources={metric_source.TechnicalDebtTracker: jira},
+                                        metric_source_ids={jira: '1234'},
+                                        requirements=[requirement.TrackTechnicalDebt])
+        self.__metric = metric.TechnicalDebtIssues(project=self.__project, subject=self.__project)
+
+    def test_value(self):
+        """ Test that the value is correct. """
+        self.assertEqual(FakeJiraFilter().nr_issues(), self.__metric.value())
+
+    def test_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual({'Jira filter': FakeJiraFilter.metric_source_urls()[0]}, self.__metric.url())
