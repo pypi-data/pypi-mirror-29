ce907cd7cc5649cb1534867af01e3d228b41dd86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:53 +0000

    Add package environment-kernels/1.1.1/environment_kernels-1.1.1.tar.gz

---
+++ packages/environment-kernels/environment_kernels-1.1.1.tar.gz/environment_kernels-1.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="environment_kernels",
+    description="Launch Jupyter kernels installed in environments",
+    url="https://github.com/Cadair/jupyter_environment_kernels/",
+    author="Stuart Mumford",
+    author_email="stuart@cadair.com",
+    license="BSD",
+    packages=['environment_kernels'],
+    include_package_data=True,
+    version="1.1.1",
+    classifiers=[
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
