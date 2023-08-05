f4f01349a10c25ad526126b91954ff9087cdd389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:00 +0000

    Add package wsync/0.9.1/wsync-0.9.1.tar.gz

---
+++ packages/wsync/wsync-0.9.1.tar.gz/wsync-0.9.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name = 'wsync',
+    version = '0.9.1',
+    description = 'A small utility for synchronizing a local folder '\
+    'with a remote web repository',
+    packages = [ 'wsync' ],
+    author = 'Roberto Reale',
+    author_email = 'rober.reale@gmail.com',
+    url = 'https://github.com/robertoreale/wsync',
+    keywords = [ 'wsync', 'sync', 'http', 'https', 'web' ],
+    install_requires = [ 
+        'requests',
+        'PyYAML',
+        'pyCheckableString'
+    ],
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
+    entry_points={
+        'console_scripts': [
+            'wsync = wsync.__main__:main'
+            ]
+        },
+)
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
