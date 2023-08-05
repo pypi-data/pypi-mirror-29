99cf70a1e266a3bbccb8155683213d706be7c785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:09 +0000

    Add package dedupe/1.9.1/dedupe-1.9.1-cp27-cp27m-macosx_10_12_intel.whl

---
+++ packages/dedupe/dedupe-1.9.1-cp27-cp27m-macosx_10_12_intel.whl/dedupe/_init.py
@@ -0,0 +1,4 @@
+from dedupe.api import StaticDedupe, Dedupe
+from dedupe.api import StaticRecordLink, RecordLink
+from dedupe.api import StaticGazetteer, Gazetteer
+from dedupe.convenience import consoleLabel, trainingDataDedupe, trainingDataLink, canonicalize
