d5d46ee504e8c482bb9eea8aff83e99610bc457a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:08 +0000

    Add package InnoCleaner/1.0.14.dev1/InnoCleaner-1.0.14.dev1-py3-none-any.whl

---
+++ packages/InnoCleaner/InnoCleaner-1.0.14.dev1-py3-none-any.whl/InnoCleaner/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+
+# Custom Imports
+import cleaningUtility as cleanUtil
+
+
+def main():
+    """
+    Entry Point to InnoCleaner Application
+    :return: None
+    """
+    cleanUtil.CleaningUtility().main()
