f40f5d0c7fbd1064b6582b7e5c996cd49912532e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:58 +0000

    Add package resolwe/6.1.0a1/resolwe-6.1.0a1.tar.gz

---
+++ packages/resolwe/resolwe-6.1.0a1.tar.gz/resolwe-6.1.0a1/resolwe/flow/signals.py
@@ -0,0 +1,43 @@
+""".. Ignore pydocstyle D400.
+
+===============
+Signal Handlers
+===============
+
+"""
+from django.conf import settings
+from django.db import transaction
+from django.db.models.signals import post_save, pre_delete
+from django.dispatch import receiver
+
+from resolwe.flow.managers import manager
+from resolwe.flow.models import Data, Entity
+
+
+def commit_signal(data_id):
+    """Nudge manager at the end of every Data object save event."""
+    if not getattr(settings, 'FLOW_MANAGER_DISABLE_AUTO_CALLS', False):
+        immediate = getattr(settings, 'FLOW_MANAGER_SYNC_AUTO_CALLS', False)
+        manager.communicate(data_id=data_id, verbosity=0, save_settings=False, run_sync=immediate)
+
+
+@receiver(post_save, sender=Data)
+def manager_post_save_handler(sender, instance, created, **kwargs):
+    """Run newly created (spawned) processes."""
+    if instance.status == Data.STATUS_DONE or instance.status == Data.STATUS_ERROR or created:
+        # Run manager at the end of the potential transaction. Otherwise
+        # tasks are send to workers before transaction ends and therefore
+        # workers cannot access objects created inside transaction.
+        transaction.on_commit(lambda: commit_signal(instance.id))
+
+
+@receiver(pre_delete, sender=Data)
+def delete_entity(sender, instance, **kwargs):
+    """Delete Entity when last Data object is deleted."""
+    try:
+        entity = Entity.objects.get(data=instance.pk)
+    except Entity.DoesNotExist:  # pylint: disable=no-member
+        return
+
+    if entity.data.count() == 1:  # last Data object will be just deleted
+        entity.delete()
