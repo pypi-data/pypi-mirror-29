01e67620a25a28a8fad07b82aab945420ee769cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:20 +0000

    Add package PythonLoginAndRegister/0.1/PythonLoginAndRegister-0.1.tar.gz

---
+++ packages/PythonLoginAndRegister/PythonLoginAndRegister-0.1.tar.gz/PythonLoginAndRegister-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PythonLoginAndRegister',
+      version='0.1',
+      description='Basic Login and Registration in Python',
+      url='https://github.com/coolhobo77/Python-Login-And-Register',
+      author='coolhobo77',
+      author_email='joshuaboddy1@gmail.com',
+      license='MIT',
+      packages=['src'],
+      zip_safe=False)
