fbc18280b94643d8494ffcf8846f5ea7064aca32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package odoo10-addon-medical-center/10.0.1.0.0.99.dev8/odoo10_addon_medical_center-10.0.1.0.0.99.dev8-py2-none-any.whl

---
+++ packages/odoo10-addon-medical-center/odoo10_addon_medical_center-10.0.1.0.0.99.dev8-py2-none-any.whl/odoo/addons/medical_center/models/medical_patient.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2008 Luis Falcon <lfalcon@gnusolidario.org>
+# Copyright 2016 LasLabs Inc.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from odoo import fields, models
+
+
+class MedicalPatient(models.Model):
+    _inherit = 'medical.patient'
+    medical_center_primary_id = fields.Many2one(
+        string='Primary Medical Center',
+        comodel_name='medical.center',
+    )
+    medical_center_secondary_ids = fields.Many2many(
+        string='Secondary Medical Centers',
+        comodel_name='medical.center',
+    )
