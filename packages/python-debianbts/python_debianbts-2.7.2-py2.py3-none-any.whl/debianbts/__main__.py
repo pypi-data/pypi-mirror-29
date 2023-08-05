428fe0e4c0cf5f0ec009a884ccfd7b9e5b1487c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:24 +0000

    Add package python-debianbts/2.7.2/python_debianbts-2.7.2-py2.py3-none-any.whl

---
+++ packages/python-debianbts/python_debianbts-2.7.2-py2.py3-none-any.whl/debianbts/__main__.py
@@ -0,0 +1,17 @@
+import logging
+
+import debianbts as bts
+
+logger = logging.getLogger('debianbts')
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(name)s %(message)s',
+    level=logging.WARNING
+)
+
+
+def main():
+    logger.warning('Not implemented yet, sorry!')
+
+
+if __name__ == '__main__':
+    main()
