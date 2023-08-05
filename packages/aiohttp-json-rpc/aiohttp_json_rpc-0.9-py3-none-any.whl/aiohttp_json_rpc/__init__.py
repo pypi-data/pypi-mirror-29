53b2e7afb3e1543e47a5f6c7a5dedb71a371930d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:45 +0000

    Add package aiohttp-json-rpc/0.9/aiohttp_json_rpc-0.9-py3-none-any.whl

---
+++ packages/aiohttp-json-rpc/aiohttp_json_rpc-0.9-py3-none-any.whl/aiohttp_json_rpc/__init__.py
@@ -0,0 +1,8 @@
+from .client import JsonRpcClient  # NOQA
+from .rpc import JsonRpc  # NOQA
+
+from .exceptions import (  # NOQA
+    RpcInvalidRequestError,
+    RpcMethodNotFoundError,
+    RpcInvalidParamsError,
+)
