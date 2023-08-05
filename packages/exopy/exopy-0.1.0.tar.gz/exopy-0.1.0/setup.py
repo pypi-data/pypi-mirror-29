c7b3e9383b9bfe5d50a07772da93b05f000a18f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:07 +0000

    Add package exopy/0.1.0/exopy-0.1.0.tar.gz

---
+++ packages/exopy/exopy-0.1.0.tar.gz/exopy-0.1.0/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import os.path
+import sys
+
+sys.path.insert(0, os.path.abspath('.'))
+from exopy.version import __version__
+
+
+def lond_description():
+    """Read the project description from the README file.
+
+    """
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+        return f.read()
+
+
+setup(
+    name='exopy',
+    description='Experiment control application',
+    version=__version__,
+    long_description=lond_description(),
+    author='Exopy Developers (see AUTHORS)',
+    author_email='m.dartiailh@gmail.com',
+    url='http://github.com/exopy/exopy',
+    download_url='http://github.com/exopy/exopy/tarball/master',
+    keywords='experiment automation GUI',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        ],
+    zip_safe=False,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    package_data={'': ['*.enaml', '*.txt']},
+    requires=['future', 'pyqt', 'atom', 'enaml', 'kiwisolver',
+              'configobj', 'watchdog', 'setuptools', 'qtawesome',
+              'numpy'],
+    setup_requires=['setuptools'],
+    install_requires=['future', 'atom>=0.4.1', 'enaml>=0.10.2',
+                      'kiwisolver>=1.0.0', 'configobj',
+                      'watchdog', 'qtawesome', 'numpy'],
+    entry_points={'gui_scripts': 'exopy = exopy.__main__:main'},
+)
