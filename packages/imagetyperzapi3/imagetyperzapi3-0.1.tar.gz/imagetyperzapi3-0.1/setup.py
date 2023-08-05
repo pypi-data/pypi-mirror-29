7f8bd82a28e804174584493203a71c32b0b71d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:47 +0000

    Add package imagetyperzapi3/0.1/imagetyperzapi3-0.1.tar.gz

---
+++ packages/imagetyperzapi3/imagetyperzapi3-0.1.tar.gz/imagetyperzapi3-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+  name = 'imagetyperzapi3',
+  packages = ['imagetyperzapi3'], # this must be the same as the name above
+  version = '0.1',
+  description = 'ImagetyperzAPI Python3 - is a super easy to use bypass captcha API wrapper for imagetyperz.com captcha service',
+  author = 'Imagetyperz Dev',
+  author_email = 'imagetyperz@gmail.com',
+  url = 'https://github.com/imagetyperz-api/imagetyperz-api-python3', # use the URL to the github repo
+  download_url = 'https://github.com/imagetyperz-api/imagetyperz-api-python3/archive/0.1.tar.gz', # create git tag and push to get archive
+  keywords = ['captcha', 'bypasscaptcha', 'decaptcher', 'decaptcha'], # arbitrary keywords
+  classifiers = [],
+  install_requires=['requests'], # Optional
+  license='MIT',
+  python_requires='==3.*'
+)
