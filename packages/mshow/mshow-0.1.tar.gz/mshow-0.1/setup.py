263a7d61839cfac5e671b74087fa51f7ed626bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:20 +0000

    Add package mshow/0.1/mshow-0.1.tar.gz

---
+++ packages/mshow/mshow-0.1.tar.gz/mshow-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mshow',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/nishantsinghdev/mshow',
+      author='Nishant Singh',
+      author_email='nishant@example.com',
+      license='MIT',
+      packages=['mshow'],
+      zip_safe=False)
