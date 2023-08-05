dd06c390dc3e7ebf56c440ace8c2722462e49863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package e13tools/0.4.2a4/e13tools-0.4.2a4.tar.gz

---
+++ packages/e13tools/e13tools-0.4.2a4.tar.gz/e13tools-0.4.2a4/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+Setup file for the e13Tools package.
+
+"""
+
+from setuptools import find_packages, setup
+from codecs import open
+from e13tools.version import version as __version__
+
+# Get the long description from the README file
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(name="e13tools",
+      version=__version__,
+      author="Ellert van der Velden",
+      author_email='ellert_vandervelden@outlook.com',
+      description=("Provides a collection of functions that were created by "
+                   "1313e."),
+      long_description=long_description,
+      url='https://www.github.com/1313e/e13Tools',
+      license='BSD-3',
+      platforms=["Windows", "Linux", "Unix"],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: Unix',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Utilities',
+          ],
+      keywords='e13tools utilities latin hypercube sampling math tools',
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+      packages=find_packages(exclude=['tests']),
+      package_dir={'e13tools': "e13tools"},
+      include_package_data=True,
+      install_requires=['numpy>=1.8', 'matplotlib>=1.4.3', 'astropy>=1.3'],
+      zip_safe=False,
+      )
