c7bfdf8c2ac8796d08528fa0eb042f0017d94a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:36 +0000

    Add package table-enforcer/0.4.0/table_enforcer-0.4.0.tar.gz

---
+++ packages/table-enforcer/table_enforcer-0.4.0.tar.gz/table_enforcer-0.4.0/docs/usage.rst
@@ -0,0 +1,5 @@
+=====
+Usage
+=====
+
+Have a look at this `Demo Notebook <https://nbviewer.jupyter.org/github/xguse/table_enforcer/blob/master/docs/demo_notebook/Usage_Demo.ipynb>`_
---
+++ packages/table-enforcer/table_enforcer-0.4.0.tar.gz/table_enforcer-0.4.0/tests/test_column.py
@@ -0,0 +1,24 @@
+"""Test the unit: Column."""
+import pytest
+from .conftest import col4, col4_no_recoders, source_table  # noqa: F401
+import table_enforcer.errors as e
+
+
+# TODO: run the actual init in this function
+def test_init(col4):
+    assert col4.name == 'col4'
+    assert col4.dtype == str
+    assert col4.unique is False  # noqa: E712
+
+
+def test_column(col4, col4_no_recoders, source_table):
+    assert sorted(["length_is_one", "upper", "valid_sex"]) == sorted(col4.validators.keys())
+
+    col4.recode(table=source_table)
+    col4.recode(table=source_table, validate=True)
+
+    with pytest.raises(e.ValidationError):
+        col4_no_recoders.recode(table=source_table, validate=True)
+
+    assert sorted(["length_is_one", "upper", "valid_sex",
+                   "dtype"]) == sorted(col4.validate(table=source_table).columns.values)
