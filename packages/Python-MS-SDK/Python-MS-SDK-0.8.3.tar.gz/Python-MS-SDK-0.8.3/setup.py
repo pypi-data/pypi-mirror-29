1f41322b0fae748c44d683a147501498b7bc4b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package Python-MS-SDK/0.8.3/Python-MS-SDK-0.8.3.tar.gz

---
+++ packages/Python-MS-SDK/Python-MS-SDK-0.8.3.tar.gz/Python-MS-SDK-0.8.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='Python-MS-SDK',
+    version='0.8.3',
+    packages=find_packages(),
+    description='Repo: https://github.com/SphereMall/Python-MS-SDK',
+    author='Kravets Roman',
+    author_email='r.kravets@spheremall.com',
+    install_requires=[
+        'requests==2.18.4'
+    ],
+    url='https://github.com/SphereMall/Python-MS-SDK/wiki',
+)
