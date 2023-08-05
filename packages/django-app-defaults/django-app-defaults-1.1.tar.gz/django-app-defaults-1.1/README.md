261b38c7b8d3b4a27a29f27cf8a66ad934256955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:32 +0000

    Add package django-app-defaults/1.1/django-app-defaults-1.1.tar.gz

---
+++ packages/django-app-defaults/django-app-defaults-1.1.tar.gz/django-app-defaults-1.1/README.md
@@ -0,0 +1,101 @@
+# django-app-defaults
+
+[![Build Status](https://img.shields.io/travis/nitely/django-app-defaults.svg?style=flat-square)](https://travis-ci.org/nitely/django-app-defaults)
+[![Coverage Status](https://img.shields.io/coveralls/nitely/django-app-defaults.svg?style=flat-square)](https://coveralls.io/r/nitely/django-app-defaults)
+[![pypi](https://img.shields.io/pypi/v/django-app-defaults.svg?style=flat-square)](https://pypi.python.org/pypi/django-app-defaults)
+[![licence](https://img.shields.io/pypi/l/django-app-defaults.svg?style=flat-square)](https://raw.githubusercontent.com/nitely/django-app-defaults/master/LICENSE)
+
+A library for managing apps and projects default static settings.
+Define default settings for apps and override them in project settings when needed.
+
+## Requirements
+
+* Python 2.7, +3.4
+* Django 1.8, 1.11, +2.0
+
+## Install
+
+```
+pip install django-app-defaults
+```
+
+## Usage
+
+```python
+# my_app/defaults.py
+
+# `django.conf.settings` or any other
+# module can be imported and used
+
+# required
+DEFAULT_SETTINGS_MODULE = True
+
+# define default settings below
+MY_DEFAULT_SETTING = "yey"
+```
+
+> Note: all `defaults`'s settings can be
+> overridden in `my_project/settings.py`
+
+Then anywhere within your project or app:
+
+```python
+from app_defaults import settings
+
+print(settings.MY_DEFAULT_SETTING)
+# yey
+
+# All `django.conf.settings` are also available
+print(settings.DEBUG)
+# True
+```
+
+To load default settings for a single app instead of all of the apps, just do:
+
+```python
+from app_defaults import Settings
+
+settings = Settings(apps=["my_app"])
+
+# or
+
+from my_app import defaults
+settings = Settings(modules=[defaults])
+```
+
+> Note: the `DEFAULT_SETTINGS_MODULE` variable is not required
+> when explicitly passing `apps` or `modules`
+
+## How it works
+
+It's an extremely simple library. It goes through all installed apps and
+looks for a `defaults.py` module at the root of the app. If a `DEFAULT_SETTINGS_MODULE`
+var is found, the module is loaded.
+
+It's similar to the following pattern, but generalized in a reusable lib:
+
+```python
+from my_app import defaults
+from django.conf import settings
+
+class Settings:
+    def __getattr__(self, attr):
+        try:
+            return getattr(settings, attr)
+        except AttributeError:
+            return getattr(defaults, attr)
+```
+
+## Documentation
+
+[django-app-defaults.readthedocs.io](http://django-app-defaults.readthedocs.io)
+
+## Tests
+
+```
+make test
+```
+
+## License
+
+MIT
---
+++ packages/django-app-defaults/django-app-defaults-1.1.tar.gz/django-app-defaults-1.1/app_defaults/defaults.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import importlib
+import pkgutil
+import collections
+import os
+
+from django.conf import settings as django_settings
+
+__all__ = ['settings', 'Settings']
+
+_SCRIPT_DIR = os.getcwd()
+
+
+def _from_locals(apps):
+    """Return apps that lives in the local dir (i.e not python packages)"""
+    return [
+        app for app in apps
+        if os.path.isdir(os.path.join(_SCRIPT_DIR, *app.split('.')))]
+
+
+class Settings:
+    """
+    Get a setting from django settings or\
+    app defaults. In that order
+
+    :param list apps: List of apps to search for a ``defaults`` module.\
+    It's usually a subset of the ``INSTALLED_APPS`` setting. The order matters
+    :param list modules: List of module objects to lookup for settings
+    :param bool locals_only: This is ``False`` by default,\
+    mainly because it only works when the app's path\
+    is relative to the running script. It's considered\
+    better to pass the list of local ``apps`` instead or\
+    make use of ``check_magic_var``. This may be deprecated\
+    in the future
+    :param bool check_magic_var: This checks the magic var\
+    ``DEFAULT_SETTINGS_MODULE = True`` is present in\
+    each app ``defaults`` module. If the var is ``False`` or\
+    does not exists, then the module is ignored.
+    """
+
+    def __init__(self, apps=None, modules=None, locals_only=False, check_magic_var=False):
+        apps = apps or []
+        modules = modules or []
+        defaults = collections.OrderedDict()
+        if locals_only:
+            apps = _from_locals(apps)
+        for app in apps:
+            module = '.'.join((app, 'defaults'))
+            if pkgutil.find_loader(module) is None:
+                continue
+            m = importlib.import_module(module)
+            has_magic_var = getattr(m, 'DEFAULT_SETTINGS_MODULE', False)
+            if check_magic_var and not has_magic_var:
+                continue
+            defaults[app] = m
+        for module in modules:
+            defaults[module.__file__] = module
+        self._defaults = defaults
+
+    def __getattr__(self, item):
+        try:
+            return getattr(django_settings, item)
+        except AttributeError as err:
+            for d in self._defaults.values():
+                try:
+                    return getattr(d, item)
+                except AttributeError:
+                    pass
+            raise err
+
+
+#: A :py:class:`.Settings` instance.
+#: All installed apps with a ``defaults`` module
+#: and a ``DEFAULT_SETTINGS_MODULE = True``
+#: var are loaded
+settings = Settings(
+    apps=django_settings.INSTALLED_APPS,
+    check_magic_var=True)
