847c1423dcd2fd21495a0e192e0e2c695e33f9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:16 +0000

    Add package ldap-simple-search/0.1.1/ldap-simple-search-0.1.1.tar.gz

---
+++ packages/ldap-simple-search/ldap-simple-search-0.1.1.tar.gz/ldap-simple-search-0.1.1/ldap_simple_search/__init__.py
@@ -0,0 +1,11 @@
+import logging
+from .search import LDAPSearch
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
---
+++ packages/ldap-simple-search/ldap-simple-search-0.1.1.tar.gz/ldap-simple-search-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='ldap-simple-search',
+      version='0.1.1',
+      description='Easily perform simple LDAP queries',
+      url='http://github.com/chaordic/python-ldap-simple-search',
+      author='Raphael P. Ribeiro',
+      author_email='raphael.ribeiro@chaordicsystems.com',
+      license='GPL',
+      packages=['ldap_simple_search'],
+      install_requires=[
+          'python-ldap',
+      ],
+      zip_safe=False)
