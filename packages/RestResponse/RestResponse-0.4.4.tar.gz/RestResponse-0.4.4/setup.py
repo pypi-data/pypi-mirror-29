96abfb29113ac1873ad4daab6a4f14d1bd9ad61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:22 +0000

    Add package RestResponse/0.4.4/RestResponse-0.4.4.tar.gz

---
+++ packages/RestResponse/RestResponse-0.4.4.tar.gz/RestResponse-0.4.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.4.4'
+
+setup(
+    name='RestResponse',
+    version=__version__,
+    description='A fluent python object for interfacing with RESTful JSON APIs.',
+    url='https://github.com/MobiusWorksLLC/RestResponse.git',
+    author='Tyson Holub',
+    author_email='tholub@mobiusworks.com',
+    license='MIT',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=[
+        'simplejson',
+        'six',
+        'SQLAlchemy'
+    ]
+)
