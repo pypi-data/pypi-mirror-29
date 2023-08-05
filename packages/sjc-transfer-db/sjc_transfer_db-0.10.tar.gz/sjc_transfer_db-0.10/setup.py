00d386a210564990469ec2bbfb95184cb1622604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:24 +0000

    Add package sjc-transfer-db/0.10/sjc_transfer_db-0.10.tar.gz

---
+++ packages/sjc-transfer-db/sjc_transfer_db-0.10.tar.gz/sjc_transfer_db-0.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sjc_transfer_db",
+    version='0.10',
+    description='San Jacinto College Transfer Database',
+    long_description='',
+    classifiers=[
+        'Programming Language :: Python :: 3.6'
+    ],
+    url="http://github.com/mattl3w1s/sjc_transfer_db",
+    author="Matt Lewis",
+    author_email="matthew.lewis@sjcd.edu",
+    packages=find_packages(),
+    install_requires=[
+        'sqlalchemy',
+    ],
+    package_data={
+        'db':['db/transfer_experiment.db']
+    },
+    data_files=[('db',['db/transfer_experiment.db'])],
+    include_package_data=True,
+    zip_safe=False
+)
