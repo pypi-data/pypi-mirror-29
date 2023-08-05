d84a2595eac4c174922b8b4b378307b622ad6b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:57 +0000

    Add package Flask-Qedit/0.0.2/Flask-Qedit-0.0.2.tar.gz

---
+++ packages/Flask-Qedit/Flask-Qedit-0.0.2.tar.gz/Flask-Qedit-0.0.2/flask_qedit/finit.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+@version:0.1
+@author: ysicing
+@file: /finit.py
+@time: 18-1-23 上午12:19
+"""
+
+import os
+import click
+
+
+@click.group()
+def cli():
+    """A simple cmd line tool"""
+
+
+@click.command(help='create new project')
+def new():
+    click.echo("add record. {}".format(os.getcwd()))
+
+
+cli.add_command(new)
+
+
