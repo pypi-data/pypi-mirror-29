569e91afc2e9cdb2a309900b7a4ed9ef76250934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:29 +0000

    Add package zmail/0.0.5/zmail-0.0.5.tar.gz

---
+++ packages/zmail/zmail-0.0.5.tar.gz/zmail-0.0.5/zmail/api.py
@@ -0,0 +1,29 @@
+"""
+zmail.api
+~~~~~~~~~~~~
+This module implements the zmail API.
+"""
+
+from .server import MailServer
+from .utils import get_attachment, show
+
+
+def server(user, password):
+    """A shortcut to use MailServer.
+
+    SMTP:
+        server.send_mail([recipient,], mail)
+
+    POP3:
+        server.get_mail(which)
+        server.get_mails(subject, from, after, before)
+        server.get_latest()
+        server.get_info()
+        server.stat()
+
+
+    Parse mail:
+        server.show(mail)
+        server.get_attachment(mail)
+    """
+    return MailServer(user, password)
