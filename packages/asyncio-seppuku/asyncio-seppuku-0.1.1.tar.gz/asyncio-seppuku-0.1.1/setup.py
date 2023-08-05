fae37b0b7c59c149063b46e4eaee987023b0dd22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:31 +0000

    Add package asyncio-seppuku/0.1.1/asyncio-seppuku-0.1.1.tar.gz

---
+++ packages/asyncio-seppuku/asyncio-seppuku-0.1.1.tar.gz/asyncio-seppuku-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='asyncio-seppuku',
+    packages=['seppuku'],
+    version='0.1.1',
+    description='Sending kill signal after specified delay',
+    author='jar3b',
+    author_email='hellotan@live.ru',
+    url='https://github.com/jar3b/asyncio-seppuku',
+    download_url='https://github.com/jar3b/asyncio-seppuku/archive/0.1.1.tar.gz',
+    keywords='asyncio signal python3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
