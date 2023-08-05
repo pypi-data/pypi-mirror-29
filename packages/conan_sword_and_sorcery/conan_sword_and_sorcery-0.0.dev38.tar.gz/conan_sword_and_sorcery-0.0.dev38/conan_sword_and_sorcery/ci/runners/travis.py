2adc775f70bb7915764b7e90ce974bd623772644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:35 +0000

    Add package conan_sword_and_sorcery/0.0.dev38/conan_sword_and_sorcery-0.0.dev38.tar.gz

---
+++ packages/conan_sword_and_sorcery/conan_sword_and_sorcery-0.0.dev38.tar.gz/conan_sword_and_sorcery-0.0.dev38/conan_sword_and_sorcery/ci/runners/travis.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+from .base_runner import BaseRunner
+from .registry import RunnerRegistry
+from conan_sword_and_sorcery import __version__
+
+log = logging.getLogger(__name__)
+
+
+@RunnerRegistry.register("TRAVIS")
+class TravisRunner(BaseRunner):
+    docker_conan_project_dirname = "/home/conan/project"
+
+    def __init__(self, *args, **kwargs):
+        super(TravisRunner, self).__init__(*args, **kwargs)
+        self.use_docker = ("CONAN_DOCKER_IMAGE" in os.environ) or (os.environ.get("CONAN_USE_DOCKER", False))
+        if self.use_docker:
+            self.docker_image = os.environ.get("CONAN_DOCKER_IMAGE", None)
+            log.info("TravisRunner will use docker image '{}'".format(self.docker_image))
+            os.system("docker pull {}".format(self.docker_image))
+            self._run_in_docker('sudo pip install -U conan conan_sword_and_sorcery=={} && conan user'.format(__version__))
+
+    def set_profile(self, profile):
+        super(TravisRunner, self).set_profile(profile)
+        # TODO: Copy profile file to docker container, smarter than mounting its directory below.
+
+    def _run_in_docker(self, command, mnt=False):
+        docker_command = 'docker run '                              \
+                         ' -v {cwd}:{docker_dirname} '              \
+                         ' -v {home}:{home} '                       \
+                         ' {image} /bin/sh -c "{command}"'.format(
+            cwd=os.getcwd(),
+            docker_dirname=self.docker_conan_project_dirname,
+            home=os.path.expanduser("~"),
+            image=self.docker_image,
+            command=command
+            )
+        return super(TravisRunner, self).cmd(docker_command)
+
+    def cmd(self, command):
+        if not self.use_docker:
+            return super(TravisRunner, self).cmd(command)
+        else:
+            command = command.replace(os.getcwd(), self.docker_conan_project_dirname)  # TODO: Make a better approach
+            return self._run_in_docker(command, mnt=True)
+
+
+
