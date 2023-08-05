99fe4eea5def89bd2ea7ad1df806dc5732185f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:52 +0000

    Add package funniest-david-brakman/0.2/funniest-david-brakman-0.2.tar.gz

---
+++ packages/funniest-david-brakman/funniest-david-brakman-0.2.tar.gz/funniest-david-brakman-0.2/setup.py
@@ -0,0 +1,15 @@
+# Following example at http://python-packaging.readthedocs.io/en/latest/minimal.html
+from setuptools import setup
+
+setup(name='funniest-david-brakman',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest-david-brakman'],
+      install_requires=[
+          'markdown',
+      ],
+      zip_safe=False)
