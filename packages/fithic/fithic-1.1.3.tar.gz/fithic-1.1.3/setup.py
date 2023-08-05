b2530687a11da12bf619aad81f5d9e17f49e70c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:51 +0000

    Add package fithic/1.1.3/fithic-1.1.3.tar.gz

---
+++ packages/fithic/fithic-1.1.3.tar.gz/fithic-1.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name = "fithic",
+    version = '1.1.3',
+    description = 'Hi-C Analysis software created and maintained by the Ay Lab',
+    url = 'http://github.com/ay-lab/fithic',
+    entry_points = {
+        "console_scripts": ['fithic = fithic.fithic:main']
+        },
+    python_requires = '~=2.7',
+    author = 'Ferhat Ay',
+    author_email = 'ferhatay@lji.org',
+    license = 'MIT',
+    packages = ['fithic'],
+    install_requires = [
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'scikit-learn',
+    ],
+    zip_safe = False,
+  )
