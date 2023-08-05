23ace6212c76f38e86e52042b66f4c8445ecbd45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package shap/0.9.1/shap-0.9.1.tar.gz

---
+++ packages/shap/shap-0.9.1.tar.gz/shap-0.9.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+# to publish use:
+# > python setup.py sdist upload
+# which depends on ~/.pypirc
+
+setup(name='shap',
+      version='0.9.1',
+      description='Explains the output of any machine learning model using expectations and Shapley values.',
+      url='http://github.com/slundberg/shap',
+      author='Scott Lundberg',
+      author_email='slund1@cs.washington.edu',
+      license='MIT',
+      packages=['shap'],
+      install_requires=['numpy', 'scipy', 'iml>=0.3.4', 'scikit-learn', 'matplotlib', 'pandas'],
+      test_suite='nose.collector',
+      tests_require=['nose', 'xgboost'],
+      zip_safe=False)
