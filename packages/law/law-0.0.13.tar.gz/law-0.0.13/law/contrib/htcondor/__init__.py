3d84afc87f75d3e0c617790c9d624e9ee25db66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package law/0.0.13/law-0.0.13.tar.gz

---
+++ packages/law/law-0.0.13.tar.gz/law-0.0.13/law/contrib/htcondor/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+"""
+HTCondor contrib functionality.
+"""
+
+
+__all__ = ["HTCondorWorkflow"]
+
+
+# provisioning imports
+from law.contrib.htcondor.workflow import HTCondorWorkflow
