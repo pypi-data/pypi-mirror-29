49fb5721dc62cd7bb3d3c8cd4290cc2fc4cdeed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:15 +0000

    Add package flask-bourbon/0.0.4/flask-bourbon-0.0.4.tar.gz

---
+++ packages/flask-bourbon/flask-bourbon-0.0.4.tar.gz/flask-bourbon-0.0.4/flask_bourbon/blueprint.py
@@ -0,0 +1,74 @@
+# coding: utf8
+import operator
+import flask
+import re
+from flask import request
+import yaml
+import simplejson as json
+
+from . import registry
+
+dev = flask.Blueprint('swagger', __name__)
+
+REST_METHODS = set(["POST", "GET", "PATCH", "PUT", "DELETE"])
+
+@dev.route("/")
+@registry.skip
+def index():
+    """Generate a swagger schema from all known routes."""
+    definition = {
+        "swagger": "2.0",
+        "info": {
+            "title": flask.current_app.config.get("APPNAME", "Not specified"),
+            "version": flask.current_app.config.get("VERSION", "Not specified"),
+        },
+        "host": request.host,
+        "schemes": ["http"],
+        "consumes": ["application/json"],
+        "produces": ["application/json"],
+        "definitions": registry._definitions,
+        "paths": {}
+    }
+
+    rules = list(flask.current_app.url_map.iter_rules())
+    for r in sorted(rules, key=operator.attrgetter('rule')):
+        if r.rule.startswith('/static'):
+            continue
+        if r.endpoint in registry._skipped:
+            continue
+
+        rule = re.sub(r"<(?:[_a-zA-Z0-9\(\)]+:)?([a-zA-Z0-9_]+)>", r"{\1}", r.rule)
+        if rule not in definition['paths']:
+            definition['paths'][rule] = {}
+
+        methods_handled = r.methods & REST_METHODS
+        handler = flask.current_app.view_functions.get(r.endpoint)
+        doc = handler.func_doc
+
+        if len(methods_handled) == 1:
+            method = methods_handled.pop().lower()
+            try:
+                validated = yaml.safe_load(doc)
+                if not isinstance(validated, dict):
+                    raise Exception("Not a descriptor")
+                definition['paths'][rule][method] = validated
+            except Exception:
+                pass
+
+        else:
+            # We need to handle multi-method docstrings differently
+            # because the documentation needs to define both, and
+            # it's a higher level of the swagger hierarchy
+            try:
+                validated = yaml.safe_load(doc)
+                if not isinstance(validated, dict):
+                    raise Exception("Not a descriptor")
+                definition['paths'][rule].update(validated)
+            except Exception:
+                definition['paths'][rule] = {}
+
+    resp = flask.make_response(
+            json.dumps(definition, for_json=True))
+    resp.headers.set("Content-type", 'application/json')
+    resp.headers.set("Access-Control-Allow-Origin", "*")
+    return resp
---
+++ packages/flask-bourbon/flask-bourbon-0.0.4.tar.gz/flask-bourbon-0.0.4/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# <python_jsonschema_objects - An object wrapper for JSON Schema definitions>
+# Copyright (C) <2018-2018>  Chris Wacek <cwacek@gmail.com
+
+from setuptools import setup, find_packages
+
+setup(name='flask-bourbon',
+      description='Flask + Bourbon makes you swagger',
+      author='Chris Wacek',
+      license="MIT",
+      author_email='cwacek@gmail.com',
+      packages=find_packages(),
+      zip_safe=False,
+      use_scm_version=True,
+      setup_requires=["setuptools>=18.0.0", 'setuptools_scm'],
+      install_requires=[
+          "python-jsonschema-objects~=0.3.2",
+          "pyyaml",
+          "flask"
+      ],
+      classifiers=[
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Intended Audience :: Developers',
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent'
+      ]
+      )
