dcb295144b9f11b4b115b3850499331ef84d58f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:15 +0000

    Add package edx-analytics-data-api-client/0.14.0/edx-analytics-data-api-client-0.14.0.tar.gz

---
+++ packages/edx-analytics-data-api-client/edx-analytics-data-api-client-0.14.0.tar.gz/edx-analytics-data-api-client-0.14.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from analyticsclient import __version__ as VERSION
+
+setup(
+    name='edx-analytics-data-api-client',
+    version=VERSION,
+    packages=['analyticsclient', 'analyticsclient.constants'],
+    url='https://github.com/edx/edx-analytics-data-api-client',
+    description='Client used to access edX analytics data warehouse',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "requests==2.12.4",
+    ],
+    tests_require=[
+        "coverage==4.3.1",
+        "nose==1.3.7",
+        "httpretty==0.8.14",
+        "pep8==1.7.0",
+        "pylint==1.6.4",
+        "pep257==0.7.0"
+    ]
+)
