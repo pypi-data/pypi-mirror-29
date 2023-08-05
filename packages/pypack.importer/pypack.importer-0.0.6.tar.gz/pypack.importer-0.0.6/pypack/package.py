fd1d00bcbce36ab1871892bbb3ed86cd86247b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:47 +0000

    Add package pypack.importer/0.0.6/pypack.importer-0.0.6.tar.gz

---
+++ packages/pypack.importer/pypack.importer-0.0.6.tar.gz/pypack.importer-0.0.6/pypack/package.py
@@ -0,0 +1,18 @@
+import os
+
+PACKAGE = 'pypack'
+LIBRARY = 'importer'
+
+NAME        = PACKAGE + '.' + LIBRARY
+VERSION     = '0.0.6'
+LICENSE     = 'MIT'
+URL         = 'https://github.com/pypack/pypack.importer'
+GIT_URL     = 'https://github.com/pypack/pypack.importer'
+DOC_URL     = ''
+DESCRIPTION = 'Pypack importer package'
+LONG_DESCR  = '' #open(os.path.dirname(os.path.abspath(__file__)) + '/DESCRIPTION', 'r').read()
+AUTHOR      = 'Giovanni Cardamone'
+EMAIL       = 'g.cardamone2@gmail.com'
+COPY        = '2018, Giovanni Cardamone'
+LANG        = 'en'
+KEYWORDS    = 'PyPack '
