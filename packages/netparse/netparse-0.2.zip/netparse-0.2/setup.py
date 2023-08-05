7d0741cfdf794ef147e21ca7082786df90cc2303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:39 +0000

    Add package netparse/0.2/netparse-0.2.zip

---
+++ packages/netparse/netparse-0.2.zip/netparse-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'netparse',
+  packages = ['netparse'], # this must be the same as the name above
+  version = '0.2',
+  description = 'Automatic NetEng CLI Parser.',
+  author = 'Syedur Rahman',
+  author_email = 'syedur.work@gmail.com',
+  url = 'https://github.com/syedur-rahman/netparse', # use the URL to the github repo
+  download_url = 'https://github.com/syedur-rahman/netparse/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['automation', 'network-engineering', 'parse'], # arbitrary keywords
+  classifiers = [],
+)
