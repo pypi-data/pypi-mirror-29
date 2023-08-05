acbe2fc7f3666fda6f1c0440f448cbfa8d4ea6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:48 +0000

    Add package robin-stocks/0.3/robin_stocks-0.3.tar.gz

---
+++ packages/robin-stocks/robin_stocks-0.3.tar.gz/robin_stocks-0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='robin_stocks',
+      version='0.3',
+      description='Made to interact with the Robinhood API',
+      long_description=long_description,
+      url='https://github.com/jmfernandes/robin_stocks',
+      author='Josh Fernandes',
+      author_email='joshfernandes@mac.com',
+      license='MIT',
+      python_requires='>=3',
+      packages=['robin_stocks'],
+      requires=['requests'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
