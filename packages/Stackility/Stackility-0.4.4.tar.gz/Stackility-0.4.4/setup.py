d63b625b7c354a9fd29e18ec3655e708d3b4ac6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:19 +0000

    Add package Stackility/0.4.4/Stackility-0.4.4.tar.gz

---
+++ packages/Stackility/Stackility-0.4.4.tar.gz/Stackility-0.4.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(
+    name="Stackility",
+    version='0.4.4',
+    packages=['stackility'],
+    description='Python CloudFormation utility',
+    author='Chuck Muckamuck',
+    author_email='Chuck.Muckamuck@gmail.com',
+    install_requires=[
+        "boto3>=1.4.3",
+        "Click>=6.7",
+        "PyYAML>=3.12",
+        "pymongo>=3.4.0"
+    ],
+    entry_points="""
+        [console_scripts]
+        stackility=stackility.command:cli
+    """
+)
