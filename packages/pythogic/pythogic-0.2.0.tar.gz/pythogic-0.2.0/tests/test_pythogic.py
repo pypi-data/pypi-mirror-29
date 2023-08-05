8f1ff30d4f2f7f55c55b946494bc6bbf1db8a8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:51 +0000

    Add package pythogic/0.2.0/pythogic-0.2.0.tar.gz

---
+++ packages/pythogic/pythogic-0.2.0.tar.gz/pythogic-0.2.0/tests/test_pythogic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `pythogic` package."""
+
+import unittest
+
+
+class TestPythogic(unittest.TestCase):
+    """Tests for `pythogic` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
