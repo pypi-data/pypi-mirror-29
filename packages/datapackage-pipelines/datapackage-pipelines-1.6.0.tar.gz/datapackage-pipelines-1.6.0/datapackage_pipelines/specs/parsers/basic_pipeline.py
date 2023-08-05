48beeb971b112ed4d9185f625e8b4203f28db5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:36 +0000

    Add package datapackage-pipelines/1.6.0/datapackage-pipelines-1.6.0.tar.gz

---
+++ packages/datapackage-pipelines/datapackage-pipelines-1.6.0.tar.gz/datapackage-pipelines-1.6.0/datapackage_pipelines/specs/parsers/basic_pipeline.py
@@ -0,0 +1,23 @@
+import os
+from typing import Iterator
+
+from .base_parser import BaseParser, PipelineSpec
+
+
+class BasicPipelineParser(BaseParser):
+
+    SPEC_FILENAME = 'pipeline-spec.yaml'
+
+    @classmethod
+    def check_filename(cls, filename):
+        return filename == cls.SPEC_FILENAME
+
+    @classmethod
+    def to_pipeline(cls, spec, fullpath) -> Iterator[PipelineSpec]:
+        dirpath = os.path.dirname(fullpath)
+
+        for pipeline_id, pipeline_details in spec.items():
+            pipeline_id = os.path.join(dirpath, pipeline_id)
+            yield PipelineSpec(path=dirpath,
+                               pipeline_id=pipeline_id,
+                               pipeline_details=pipeline_details)
