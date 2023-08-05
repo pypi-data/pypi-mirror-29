f5669bfe6ea59dca983554c0ba4b907dae3ea66c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:45 +0000

    Add package koalanlp/1.1.6/koalanlp-1.1.6-py3-none-any.whl

---
+++ packages/koalanlp/koalanlp-1.1.6-py3-none-any.whl/koalanlp/api/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .const import API
+from .initialize import initialize
+from .tagger import Parser, Tagger, SentenceSplitter, sentences, Dictionary
