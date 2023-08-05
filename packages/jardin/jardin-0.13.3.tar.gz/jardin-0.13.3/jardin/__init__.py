880933c72dd971b2f5ea05fd315f0a5fc49a7617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:45 +0000

    Add package jardin/0.13.3/jardin-0.13.3.tar.gz

---
+++ packages/jardin/jardin-0.13.3.tar.gz/jardin-0.13.3/jardin/__init__.py
@@ -0,0 +1,11 @@
+from model import Model, Collection
+import config
+
+__author__ = 'Emmanuel Turlay'
+__copyright__ = 'Copyright 2017, Instacart'
+__credits__ = ['Emmanuel Turlay', 'Mathieu Ripert']
+__license__ = 'MIT'
+__version__ = '0.13.3'
+__maintainer__ = 'Emmanuel Turlay'
+__email__ = 'emmanuel@instacart.com'
+__status__ = 'Prototype'
