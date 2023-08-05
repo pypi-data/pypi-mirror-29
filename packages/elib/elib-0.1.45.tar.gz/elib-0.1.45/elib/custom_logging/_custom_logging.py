092ae2815e03b0781380c3200f63dd342ed34d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:02 +0000

    Add package elib/0.1.45/elib-0.1.45.tar.gz

---
+++ packages/elib/elib-0.1.45.tar.gz/elib-0.1.45/elib/custom_logging/_custom_logging.py
@@ -0,0 +1,140 @@
+# coding=utf-8
+"""
+Convenience functions to manage logging
+"""
+
+import logging as base
+import logging.handlers as base_handlers
+import sys
+from pathlib import Path
+
+from elib import LOGGER as ELIB_LOGGER
+from . import _constants
+
+
+# noinspection SpellCheckingInspection
+
+
+def set_handler_level(logger_name, handler_name, level):
+    """
+    Sets output level for console handlers
+
+    Args:
+        logger_name: name of the logger to update
+        level: desired level
+    """
+    if isinstance(level, str):
+        level = _constants.LEVELS[level.upper()]
+    _constants.LOGGERS[logger_name][handler_name].setLevel(level)
+
+
+# pylint: disable=too-many-arguments
+def _setup_file_logging(logger: base.Logger,
+                        logger_name: str,
+                        rotate_logs: bool,
+                        rotate_log_when: str,
+                        rotate_log_backup_count: int,
+                        file_level,
+                        file_format,
+                        **_):
+    file_name = Path(f'{logger_name}.log').absolute()
+    if rotate_logs:
+        file_handler = base_handlers.TimedRotatingFileHandler(
+            filename=file_name,
+            when=rotate_log_when,
+            backupCount=rotate_log_backup_count,
+            encoding='utf8',
+        )
+    else:
+        if Path(file_name).exists():
+            Path(file_name).unlink()
+        file_handler = base.FileHandler(
+            filename=file_name,
+            encoding='utf8'
+        )
+
+    file_handler.setLevel(file_level)
+    file_handler.setFormatter(base.Formatter(file_format))
+
+    logger.addHandler(file_handler)
+    if rotate_logs:
+        logger.debug(f'added rotating file logging handler: {file_name}')
+    else:
+        logger.debug(f'added file logging handler: {file_name}')
+
+    return file_handler
+
+
+# pylint: disable= unused-argument
+def get_logger(
+        logger_name: str,
+        log_to_file: bool = False,
+        rotate_logs: bool = False,
+        rotate_log_when: str = 'midnight',
+        rotate_log_backup_count: int = 7,
+        console_level=base.ERROR,
+        file_level=base.DEBUG,
+        console_format=_constants.DEFAULT_CONSOLE_FORMAT,
+        file_format=_constants.DEFAULT_FILE_FORMAT,
+) -> base.Logger:
+    """
+    Set up the logger
+
+    :param logger_name: name of the logger
+    :param log_to_file: path to log file [optional]
+    :param rotate_logs: whether or not to rotate the log
+    :param rotate_log_when: when log rotation should occur
+    :param rotate_log_backup_count: number of log files to keep
+    :param console_level: level for console logging
+    :param file_level: level for file logging
+    :param console_format: console formatter string
+    :param file_format: file formatter string
+
+    :return: logger object
+    """
+    if logger_name in _constants.LOGGERS:
+        _constants.LOGGERS[logger_name]['logger'].debug(f'logger already initialized: {logger_name}')
+        return _constants.LOGGERS[logger_name]['logger']
+
+    kwargs = locals()
+
+    if _constants.ROOT_LOGGER is None:
+        logger = base.getLogger(logger_name)
+        _constants.ROOT_LOGGER = logger
+    else:
+        logger = _constants.ROOT_LOGGER.getChild(logger_name)
+
+    logger.setLevel(base.DEBUG)
+
+    console_handler = base.StreamHandler(sys.stdout)
+    console_handler.setLevel(console_level)
+    console_handler.setFormatter(base.Formatter(console_format))
+
+    logger.addHandler(console_handler)
+    logger.debug('added console logging handler')
+
+    _constants.LOGGERS[logger.name] = {
+        'logger': logger,
+        'ch': console_handler,
+    }
+
+    if log_to_file:
+        file_handler = _setup_file_logging(logger, **kwargs)
+        _constants.LOGGERS[logger.name]['fh'] = file_handler
+
+    return logger
+
+
+def activate_elib_logging():
+    """
+    Attaches all handlers of the root logger to the ELIB logger
+    """
+    for handler in _constants.ROOT_LOGGER.handlers:
+        ELIB_LOGGER.addHandler(handler)
+
+
+def get_elib_logger() -> base.Logger:
+    """
+    Returns ELIB's logger
+    """
+    return ELIB_LOGGER
---
+++ packages/elib/elib-0.1.45.tar.gz/elib-0.1.45/test/test_logging.py
@@ -0,0 +1,208 @@
+# coding=utf-8
+"""
+Tests logging package
+"""
+
+import logging
+import logging.handlers
+from pathlib import Path
+
+import elib
+import pytest
+
+import elib.custom_logging._constants
+import elib.custom_logging._custom_logging_handler
+
+
+@pytest.fixture(scope='function')
+def setup_logging(caplog):
+    """Provides a logger and the caplog fixture"""
+    logger = elib.custom_logging.get_logger(logger_name='TEST_LOGGER', console_level=logging.DEBUG)
+    yield logger, caplog
+
+
+# noinspection PyShadowingNames
+def test_logger(setup_logging):
+    logger, caplog = setup_logging
+    assert isinstance(logger, logging.Logger)
+    assert logger is logging.getLogger(logger.name)
+    caplog.set_level(logging.DEBUG)
+    logger.info('test_message_debug')
+    assert 'test_message_debug' in caplog.text
+    assert elib.custom_logging.get_logger(logger.name) is logging.getLogger(logger.name)
+
+
+# noinspection PyShadowingNames
+def test_double_instantiation():
+    first_logger = elib.custom_logging.get_logger('test_logger')
+    second_logger = elib.custom_logging.get_logger('test_logger')
+    assert first_logger is second_logger
+
+
+# noinspection PyShadowingNames
+@pytest.mark.parametrize('levels', [
+    (logging.DEBUG, logging.INFO, 'debug'),
+    (logging.INFO, logging.WARNING, 'info'),
+    (logging.WARNING, logging.ERROR, 'warning'),
+    (logging.ERROR, logging.CRITICAL, 'error'),
+])
+def test_logger_levels(setup_logging, levels):
+    logger, caplog = setup_logging
+    logger.setLevel(levels[1])
+    # caplog.set_level(levels[1], logger='TEST_LOGGER')
+    out_func = getattr(logger, levels[2])
+    out_func('test_message_debug')
+    assert 'test_message_debug' not in caplog.text
+    logger.setLevel(levels[0])
+    # caplog.set_level(levels[0], logger='TEST_LOGGER')
+    out_func('test_message_debug')
+    assert 'test_message_debug' in caplog.text
+
+
+# noinspection PyShadowingNames
+@pytest.mark.parametrize('level', ['debug', 'info', 'warning', 'error', 'critical'])
+def test_logger_not_set(setup_logging, level):
+    logger, caplog = setup_logging
+    logger.setLevel(1)
+    caplog.set_level(logging.NOTSET)
+    out_func = getattr(logger, level)
+    out_func('test_message_debug')
+    assert 'test_message_debug' in caplog.text
+
+
+def test_file_handler():
+    logger_name = elib.custom_random.random_string()
+    log_file = Path(f'./{logger_name}.log')
+    assert not log_file.exists()
+    logger = elib.custom_logging.get_logger(logger_name, log_to_file=True)
+    logger.warning('test')
+    assert log_file.exists()
+    assert 'test' in log_file.read_text()
+
+
+def test_file_handler_levels():
+    logger_name = elib.custom_random.random_string()
+    log_file = Path(f'./{logger_name}.log')
+    assert not log_file.exists()
+    logger = elib.custom_logging.get_logger(logger_name, file_level=logging.ERROR, log_to_file=True)
+    logger.warning('__warning__')
+    logger.error('__error__')
+    assert log_file.exists()
+    assert '__warning__' not in log_file.read_text()
+    assert '__error__' in log_file.read_text()
+
+
+def test_rotating_file_handler():
+    logger_name = elib.custom_random.random_string()
+    log_file = Path(f'./{logger_name}.log')
+    assert not log_file.exists()
+    logger = elib.custom_logging.get_logger(logger_name, rotate_logs=True, log_to_file=True)
+    logger.warning('test')
+    assert log_file.exists()
+    assert 'test' in log_file.read_text()
+    assert len(list(log_file.parent.glob('*'))) == 1
+    for handler in logger.handlers:
+        if isinstance(handler, logging.handlers.TimedRotatingFileHandler):
+            handler.doRollover()
+            assert len(list(log_file.parent.glob('*'))) == 2
+            break
+    else:
+        assert logger.handlers is None
+        raise LookupError('no rotating file handler found')
+
+
+# noinspection PyShadowingNames
+def test_custom_handler(setup_logging):
+    logger, _ = setup_logging
+
+    result = False
+
+    def _callback(*_):
+        nonlocal result
+        result = True
+
+    handler = elib.custom_logging._custom_logging_handler.CustomLoggingHandler('handler')
+    handler.emit = _callback
+
+    handler.register(logger)
+
+    logger.info('test')
+    assert result
+
+
+@pytest.mark.parametrize(
+    'level',
+    [logging.DEBUG, logging.INFO, logging.ERROR, logging.WARN, logging.CRITICAL]
+)
+def test_handler_level(level):
+    logger = elib.custom_logging.get_logger(elib.custom_random.random_string(4), log_to_file=True)
+    elib.custom_logging.set_handler_level(logger.name, 'ch', level)
+    elib.custom_logging.set_handler_level(logger.name, 'fh', level)
+
+    for handler in logger.handlers:
+        assert handler.level is level
+
+    handler = elib.custom_logging._custom_logging_handler.CustomLoggingHandler('handler')
+    handler.register(logger)
+
+    elib.custom_logging.set_handler_level(logger.name, 'handler', level)
+
+    for handler in logger.handlers:
+        assert handler.level is level
+
+
+def test_console_handler_level_as_string():
+    logger = elib.custom_logging.get_logger(elib.custom_random.random_string(4), log_to_file=True)
+
+    elib.custom_logging.set_handler_level(logger.name, 'ch', 'critical')
+    elib.custom_logging.set_handler_level(logger.name, 'fh', 'critical')
+    for handler in logger.handlers:
+        assert handler.level is logging.CRITICAL
+
+    elib.custom_logging.set_handler_level(logger.name, 'ch', 'debug')
+    elib.custom_logging.set_handler_level(logger.name, 'fh', 'debug')
+    for handler in logger.handlers:
+        assert handler.level is logging.DEBUG
+
+    elib.custom_logging.set_handler_level(logger.name, 'ch', 'WaRN')
+    elib.custom_logging.set_handler_level(logger.name, 'fh', 'warning')
+    for handler in logger.handlers:
+        assert handler.level is logging.WARN
+
+    elib.custom_logging.set_handler_level(logger.name, 'ch', 'err')
+    elib.custom_logging.set_handler_level(logger.name, 'fh', 'error')
+    for handler in logger.handlers:
+        assert handler.level is logging.ERROR
+
+
+def test_removal_of_log_file():
+    logger_name = elib.custom_random.random_string(4)
+    log_file = Path(f'./{logger_name}.log')
+    log_file.write_text('dummy content')
+    logger = elib.custom_logging.get_logger(logger_name, log_to_file=True)
+    assert f'added file logging handler: {log_file.absolute()}' in log_file.read_text()
+    assert not 'dummy content' in log_file.read_text()
+    logger.debug('some text')
+    assert 'some text' in log_file.read_text()
+
+
+def test_no_log_file():
+    logger_name = elib.custom_random.random_string(4)
+    log_file = Path(f'./{logger_name}.log')
+    logger = elib.custom_logging.get_logger(logger_name, log_to_file=False)
+    assert not log_file.exists()
+    assert len(logger.handlers) == 1
+
+
+def test_activate_elib_logging():
+    logger = logging.getLogger('some_logger')
+    handler = logging.StreamHandler()
+    logger.addHandler(handler)
+    elib.custom_logging._constants.ROOT_LOGGER = logger
+    elib.custom_logging.activate_elib_logging()
+    from elib import LOGGER as ELIB_LOGGER
+    assert handler in ELIB_LOGGER.handlers
+
+
+def test_get_elib_logger():
+    assert elib.custom_logging.get_elib_logger() is elib.LOGGER
