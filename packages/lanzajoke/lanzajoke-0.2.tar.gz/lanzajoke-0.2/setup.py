54fcbb7113e41b17da24dac88302bf14f0db1093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:31 +0000

    Add package lanzajoke/0.2/lanzajoke-0.2.tar.gz

---
+++ packages/lanzajoke/lanzajoke-0.2.tar.gz/lanzajoke-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lanzajoke',
+        version='0.2',
+        description='The funniest joke in the world',
+        url='http://github.com/lanza/lanzajoke',
+        author='Nathan Lanza',
+        author_email='nathan@lanza.io',
+        license='MIT',
+        packages=['lanzajoke'],
+        zip_safe=False)
