99a086c75b9bc9adf8ec24c604307920e9ab0129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:13 +0000

    Add package egta/0.0.25/egta-0.0.25-py3-none-any.whl

---
+++ packages/egta/egta-0.0.25-py3-none-any.whl/egta/script/brute.py
@@ -0,0 +1,87 @@
+import argparse
+import json
+import logging
+
+import numpy as np
+from gameanalysis import nash
+from gameanalysis import paygame
+from gameanalysis import reduction
+from gameanalysis import regret
+from gameanalysis import restrict
+
+from egta import schedgame
+
+
+_log = logging.getLogger(__name__)
+
+
+def add_parser(subparsers):
+    parser = subparsers.add_parser(
+        'brute', help="""Compute equilibria by sampling all profiles""",
+        description="""Samples profiles from the entire game, and then runs
+        standard equilibrium finding. For games with a large number of players,
+        a reduction should be specified. A list of is returned where each
+        element has an "equilibrium" and the corresponding "regret" in the full
+        game.""")
+    parser.add_argument(
+        '--regret-thresh', metavar='<reg>', type=float, default=1e-3,
+        help="""Regret threshold for a mixture to be considered an equilibrium.
+        (default: %(default)g)""")
+    parser.add_argument(
+        '--dist-thresh', metavar='<norm>', type=float, default=0.1,
+        help="""Norm threshold for two mixtures to be considered distinct.
+        (default: %(default)g)""")
+    parser.add_argument(
+        '--restrict', '-r', metavar='<restriction-file>',
+        type=argparse.FileType('r'), help="""Specify an optional restricted
+        game to sample instead of the whole game. Only deviations from the
+        restricted strategy set will be scheduled.""")
+    reductions = parser.add_mutually_exclusive_group()
+    reductions.add_argument(
+        '--dpr', metavar='<role:count;role:count,...>', help="""Specify a
+        deviation preserving reduction.""")
+    reductions.add_argument(
+        '--hr', metavar='<role:count;role:count,...>', help="""Specify a
+        hierarchical reduction.""")
+    return parser
+
+
+def run(scheduler, args):
+    game = scheduler.game()
+    if args.dpr is not None:
+        red_players = game.role_from_repr(args.dpr, dtype=int)
+        red = reduction.deviation_preserving
+    elif args.hr is not None:
+        red_players = game.role_from_repr(args.hr, dtype=int)
+        red = reduction.hierarchical
+    else:
+        red = reduction.identity
+        red_players = None
+
+    rest = (np.ones(game.num_strats, bool) if args.restrict is None
+            else game.restriction_from_json(json.load(args.restrict)))
+    game = schedgame.schedgame(scheduler, red, red_players)
+    data = paygame.game_copy(game.restrict(rest))
+    eqa = restrict.translate(nash.mixed_nash(
+        data, regret_thresh=args.regret_thresh,
+        dist_thresh=args.dist_thresh), rest)
+    reg_info = []
+    for eqm in eqa:
+        gains = regret.mixture_deviation_gains(game, eqm)
+        bri = np.argmax(gains)
+        reg_info.append((gains[bri],) + game.role_strat_names[bri])
+
+    _log.error(
+        "brute sampling finished finding %d equilibria:\n%s",
+        eqa.shape[0], '\n'.join(
+            '{:d}) {} with regret {:g} to {} {}'.format(
+                i, game.mixture_to_repr(eqm), reg, role, strat)
+            for i, (eqm, (reg, role, strat))
+            in enumerate(zip(eqa, reg_info), 1)))
+
+    json.dump([{'equilibrium': game.mixture_to_json(eqm),
+                'regret': reg,
+                'best_response': {'role': role, 'strat': strat}}
+               for eqm, (reg, role, strat)
+               in zip(eqa, reg_info)], args.output)
+    args.output.write('\n')
---
+++ packages/egta/egta-0.0.25-py3-none-any.whl/egta/simsched.py
@@ -0,0 +1,202 @@
+"""A scheduler that gets payoffs from a local simulation"""
+import json
+import logging
+import queue
+import subprocess
+import threading
+
+from gameanalysis import paygame
+from gameanalysis import rsgame
+
+from egta import profsched
+
+
+_log = logging.getLogger(__name__)
+
+
+class SimulationScheduler(profsched.Scheduler):
+    """Schedule profiles using a command line program
+
+    Parameters
+    ----------
+    game : RsGame
+        A gameanalysis game that indicates how array profiles should be turned
+        into json profiles.
+    config : {key: value}
+        A dictionary mapping string keys to values that will be passed to the
+        simulator in the standard simulation spec format.
+    command : [str]
+        A list of strings that represents a command line program to run. This
+        program must accept simulation spec files as flushed lines of input to
+        standard in, and write the resulting output as an observation to
+        standard out. After all input lines have been read, this must flush the
+        output otherwise this could hang waiting for results that are trapped
+        in a buffer.
+    sleep : int, optional
+        Time in seconds to wait before checking programs stdout for results.
+        Too low and a lot of cycles will be wasted querying an empty buffer,
+        too fast and programs may be waiting for results while this is
+        sleeping.
+    """
+
+    def __init__(self, game, config, command, sleep=1):
+        self._game = paygame.game_copy(rsgame.emptygame_copy(game))
+        self.base = {'configuration': config}
+        self.command = command
+        self.sleep = sleep
+
+        self._running = False
+        self._proc = None
+        self._inthread = None
+        self._outthread = None
+        self._lock = threading.Lock()
+        self._inqueue = queue.Queue()
+        self._outqueue = queue.Queue()
+
+        self._exception = None
+
+    def schedule(self, profile):
+        assert self._running and self._proc is not None, \
+            "can't call schedule before entering scheduler"
+        if self._exception is not None:
+            raise self._exception
+        promise = _SimPromise(self, profile)
+        self._inqueue.put(promise)
+        return promise
+
+    def game(self):
+        return self._game
+
+    def _enqueue(self):
+        """Thread used to push lines to stdin"""
+        try:
+            while self._running:
+                prom = self._inqueue.get()
+                if prom is None:
+                    return  # told to terminate
+                jprof = self._game.profile_to_json(prom._prof)
+                self.base['assignment'] = jprof
+                json.dump(self.base, self._proc.stdin, separators=(',', ':'))
+                self._proc.stdin.write('\n')
+                self._proc.stdin.flush()
+                self._outqueue.put(prom)
+                _log.debug("sent profile: %s",
+                           self._game.profile_to_repr(prom._prof))
+        except Exception as ex:  # pragma: no cover
+            self._exception = ex
+        finally:
+            while not self._inqueue.empty():
+                prom = self._inqueue.get()
+                prom is None or prom._set(None)
+
+    def _dequeue(self):
+        """Thread used to get output from simulator
+
+        This thread is constantly polling the simulator process and processing
+        payoff data when its found."""
+        try:
+            while self._running:
+                # FIXME If proc is killed, it doesn't close the streams, and as
+                # a result, this call blocks indefinitely. I can't figure out a
+                # way around this, but as it currently stands, this thread
+                # doesn't die in that case.
+                line = self._proc.stdout.readline()
+                if not line and self._outqueue.empty():
+                    # Process closed stdout and had nothing else to run
+                    return
+                elif not line or self._proc.poll() is not None:
+                    # Process terminated unexpectedly
+                    raise RuntimeError(
+                        "Process died unexpectedly with code {}".format(
+                            self._proc.poll()))
+                else:
+                    try:
+                        jpays = json.loads(line)
+                    except json.JSONDecodeError:
+                        raise RuntimeError(
+                            "Couldn't decode \"{}\" as json".format(line))
+                    payoffs = self._game.payoff_from_json(jpays)
+                    payoffs.setflags(write=False)
+                    promise = self._outqueue.get()
+                    if promise is None:
+                        return  # told to exit
+                    _log.debug("read payoff for profile: %s",
+                               self._game.profile_to_repr(promise._prof))
+                    promise._set(payoffs)
+        except Exception as ex:  # pragma: no cover
+            self._exception = ex
+        finally:
+            while not self._outqueue.empty():
+                prom = self._outqueue.get()
+                prom is None or prom._set(None)
+
+    def __enter__(self):
+        self._running = True
+
+        self._proc = subprocess.Popen(
+            self.command, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+            universal_newlines=True)
+
+        # We start these as daemons so that if we fail to close them for
+        # whatever reason, python still exits
+        self._inthread = threading.Thread(target=self._enqueue, daemon=True)
+        self._inthread.start()
+        self._outthread = threading.Thread(target=self._dequeue, daemon=True)
+        self._outthread.start()
+        return self
+
+    def __exit__(self, *args):
+        self._running = False
+
+        # This tells threads to die
+        self._inqueue.put(None)
+        self._outqueue.put(None)
+
+        # killing the process should close the streams and kill the threads
+        if self._proc is not None and self._proc.poll() is None:
+            # Kill process nicely, and then not nicely
+            try:
+                self._proc.terminate()
+            except ProcessLookupError:  # pragma: no cover
+                pass  # race condition, process died
+            try:
+                self._proc.wait(self.sleep)
+            except subprocess.TimeoutExpired:
+                _log.warning("couldn't terminate simulation, killing it...")
+                self._proc.kill()
+            try:
+                self._proc.wait(self.sleep)
+            except subprocess.TimeoutExpired:  # pragma: no cover
+                _log.error("couldn't kill simulation")
+
+        # Threads should be dead at this point, but we close anyways
+        if self._inthread is not None and self._inthread.is_alive():  # pragma: no cover # noqa
+            self._inthread.join(self.sleep * 2)
+            if self._inthread.is_alive():
+                _log.warning("couldn't kill dequeue thread...")
+        if self._outthread is not None and self._outthread.is_alive():  # pragma: no cover # noqa
+            self._outthread.join(self.sleep * 2)
+            if self._outthread.is_alive():
+                _log.warning("couldn't kill dequeue thread...")
+
+
+class _SimPromise(profsched.Promise):
+    def __init__(self, sched, prof):
+        self._event = threading.Event()
+        self._sched = sched
+        self._prof = prof
+
+    def _set(self, value):
+        self._value = value
+        self._event.set()
+
+    def get(self):
+        self._event.wait()
+        assert self._sched._running, \
+            "can't get promise when scheduler is not running"
+        if self._sched._exception is not None:
+            raise self._sched._exception
+        elif self._prof is None:
+            raise ValueError("profile was None, likely due to an exception")
+        else:
+            return self._value
