e07b0f3073aeae2253ca27b7bc686974f90af945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:37 +0000

    Add package asyncio-seppuku/0.1.2/asyncio-seppuku-0.1.2.tar.gz

---
+++ packages/asyncio-seppuku/asyncio-seppuku-0.1.2.tar.gz/asyncio-seppuku-0.1.2/seppuku/__init__.py
@@ -0,0 +1,20 @@
+import os, signal
+
+
+def killme_after(delay, loop, sig='SIGINT', pid=None):
+    """
+    :param delay: delay in seconds to execute signal
+    :param loop: asyncio event loop
+    :param sig: signal (string), must presents in signal package
+    :param pid: PID (default is current PID)
+    :return:
+    """
+    if not hasattr(signal, sig):
+        print('unknown signal "%s"' % sig)
+    else:
+        if not getattr(loop, 'sig_%s' % sig, False):
+            loop.call_later(delay, os.kill, pid or os.getpid(), getattr(signal, sig))
+            setattr(loop, 'sig_%s' % sig, True)
+            return True
+
+    return False
---
+++ packages/asyncio-seppuku/asyncio-seppuku-0.1.2.tar.gz/asyncio-seppuku-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='asyncio-seppuku',
+    packages=['seppuku'],
+    version='0.1.2',
+    description='Sending kill signal after specified delay',
+    author='jar3b',
+    author_email='hellotan@live.ru',
+    license="MIT license",
+    url='https://github.com/jar3b/asyncio-seppuku',
+    download_url='https://github.com/jar3b/asyncio-seppuku/archive/0.1.2.tar.gz',
+    keywords='asyncio signal python3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
