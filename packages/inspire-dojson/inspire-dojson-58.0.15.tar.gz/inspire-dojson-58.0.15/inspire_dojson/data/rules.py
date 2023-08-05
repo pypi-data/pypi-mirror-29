086849470945e2140bcf3a3db1ed3faa41f24a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:53 +0000

    Add package inspire-dojson/58.0.15/inspire-dojson-58.0.15.tar.gz

---
+++ packages/inspire-dojson/inspire-dojson-58.0.15.tar.gz/inspire-dojson-58.0.15/inspire_dojson/data/rules.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2014-2017 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+"""DoJSON rules for Data."""
+
+from __future__ import absolute_import, division, print_function
+
+from .model import data
+from ..utils import force_single_element, get_record_ref
+
+
+@data.over('new_record', '^970..')
+def new_record(self, key, value):
+    new_recid = force_single_element(value.get('d'))
+    if new_recid:
+        return get_record_ref(new_recid, 'data')
