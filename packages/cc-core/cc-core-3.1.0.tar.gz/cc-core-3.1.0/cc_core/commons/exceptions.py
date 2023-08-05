3df762bc539feaed63b157419087bda794a085b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:46 +0000

    Add package cc-core/3.1.0/cc-core-3.1.0.tar.gz

---
+++ packages/cc-core/cc-core-3.1.0.tar.gz/cc-core-3.1.0/cc_core/commons/exceptions.py
@@ -0,0 +1,45 @@
+from traceback import format_exc
+
+
+def exception_format():
+    return [l.replace('"', '').replace("'", '') for l in format_exc().split('\n') if l]
+
+
+class AgentError(Exception):
+    pass
+
+
+class EngineError(Exception):
+    pass
+
+
+class FileError(Exception):
+    pass
+
+
+class JobExecutionError(Exception):
+    pass
+
+
+class CWLSpecificationError(Exception):
+    pass
+
+
+class JobSpecificationError(Exception):
+    pass
+
+
+class RedSpecificationError(Exception):
+    pass
+
+
+class ConnectorError(Exception):
+    pass
+
+
+class AccessValidationError(Exception):
+    pass
+
+
+class AccessError(Exception):
+    pass
