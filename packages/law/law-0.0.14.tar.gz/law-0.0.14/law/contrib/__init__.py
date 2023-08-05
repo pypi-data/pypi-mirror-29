4cb1c954d0e207c82ead052ec2717a40ac1823a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:34 +0000

    Add package law/0.0.14/law-0.0.14.tar.gz

---
+++ packages/law/law-0.0.14.tar.gz/law-0.0.14/law/contrib/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package containing optional and third-party functionality.
+"""
---
+++ packages/law/law-0.0.14.tar.gz/law-0.0.14/law/contrib/glite/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+"""
+gLite contrib functionality.
+"""
+
+
+__all__ = ["GLiteWorkflow"]
+
+
+# provisioning imports
+from law.contrib.glite.workflow import GLiteWorkflow
