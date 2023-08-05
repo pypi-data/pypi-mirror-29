29b0ee64538b6d10d967626538a7a5440be9e465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:40 +0000

    Add package bddrest/0.3.1a1.dev4/bddrest-0.3.1a1.dev4.tar.gz

---
+++ packages/bddrest/bddrest-0.3.1a1.dev4.tar.gz/bddrest-0.3.1a1.dev4/bddrest/__init__.py
@@ -0,0 +1,26 @@
+
+from .helpers import ObjectProxy
+from .specification import Call, Story, OverriddenCall, VerifyError
+from .authoring import Given, ComposingMixin, When, ComposingCall
+
+
+__version__ = '0.3.1a1.dev4'
+
+
+story = ObjectProxy(Given.get_current)
+response = ObjectProxy(lambda: story.current_call.response)
+
+
+def given(application, *args, **kwargs):
+    return Given(application, *args, **kwargs)
+
+
+def when(*args, **kwargs):
+    story.when(*args, **kwargs)
+
+
+def then(*args, **kwargs):
+    story.then(*args, **kwargs)
+
+
+and_ = then
