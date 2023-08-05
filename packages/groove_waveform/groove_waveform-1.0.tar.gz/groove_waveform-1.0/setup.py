16cdf0d74e3371a7607743a81dfcd376775d8a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:16 +0000

    Add package groove_waveform/1.0/groove_waveform-1.0.tar.gz

---
+++ packages/groove_waveform/groove_waveform-1.0.tar.gz/groove_waveform-1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+
+setup(name='groove_waveform',
+      version='1.0',
+      description='waveform generation using libgroove',
+      author='Basit Raza',
+      author_email='basit.raza11@gmail.com',
+      url='https://github.com/geek96/waveform-python',
+      package_dir={'waveform': 'waveform'}   
+  )
