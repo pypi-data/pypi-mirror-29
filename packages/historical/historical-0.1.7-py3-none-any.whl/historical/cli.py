cc698463985394e5517756f366c1b7fdf632114b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:13 +0000

    Add package historical/0.1.7/historical-0.1.7-py3-none-any.whl

---
+++ packages/historical/historical-0.1.7-py3-none-any.whl/historical/cli.py
@@ -0,0 +1,13 @@
+import os
+import logging
+
+import click
+import delegator
+
+logger = logging.getLogger(__name__)
+
+
+def cli():
+    """Historical commandline for managing historical functions."""
+    pass
+
