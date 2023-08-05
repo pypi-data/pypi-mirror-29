03aedbc7d9595a592edb802c18d8a46a7dd8318d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:02 +0000

    Add package brawlstars/0.2.8/brawlstars-0.2.8.tar.gz

---
+++ packages/brawlstars/brawlstars-0.2.8.tar.gz/brawlstars-0.2.8/brawlstars/__init__.py
@@ -0,0 +1,9 @@
+from .client import *
+from .asyncclient import *
+from .errors import *
+
+__author__ = 'Umbresp'
+__title__ = 'brawlstars'
+__license__ = 'MIT'
+__version__ = '0.2.8'
+__github__ = 'https://www.github.com/umbresp/brawlstars'
