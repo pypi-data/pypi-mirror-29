28ce5cb0111c8c917c8c5a5bc6371eca97fcf7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:54 +0000

    Add package django-slackchat-serializer/0.1.7/django_slackchat_serializer-0.1.7-py3-none-any.whl

---
+++ packages/django-slackchat-serializer/django_slackchat_serializer-0.1.7-py3-none-any.whl/slackchat/tasks/channel.py
@@ -0,0 +1,32 @@
+from celery import shared_task
+from slackclient import SlackClient
+
+from slackchat.conf import settings
+from slackchat.models import Channel
+
+
+@shared_task(acks_late=True)
+def create_private_channel(pk):
+    instance = Channel.objects.get(pk=pk)
+    client = SlackClient(settings.SLACK_API_TOKEN)
+    response = client.api_call(
+        "conversations.create",
+        name='slackchat-{}'.format(instance.id.hex[:10]),
+        is_private=True
+    )
+    if response.get('ok', False):
+        channel = response.get('channel')
+        instance.api_id = channel.get('id')
+
+        # Add team info
+        response = client.api_call("team.info")
+        if response.get('ok', False):
+            team = response.get('team')
+            instance.team_id = team.get('id')
+
+        client.api_call(
+            "groups.invite",
+            channel=instance.api_id,
+            user=instance.owner
+        )
+    instance.save()
