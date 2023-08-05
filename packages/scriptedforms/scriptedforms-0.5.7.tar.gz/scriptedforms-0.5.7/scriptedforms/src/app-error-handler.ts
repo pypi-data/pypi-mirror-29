5409a92b72b89066f21f76948403fd6f378f82b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:21 +0000

    Add package scriptedforms/0.5.7/scriptedforms-0.5.7.tar.gz

---
+++ packages/scriptedforms/scriptedforms-0.5.7.tar.gz/scriptedforms-0.5.7/scriptedforms/src/app-error-handler.ts
@@ -0,0 +1,59 @@
+// Scripted Forms -- Making GUIs easy for everyone on your team.
+// Copyright (C) 2017 Simon Biggs
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version (the "AGPL-3.0+").
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License and the additional terms for more
+// details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+// ADDITIONAL TERMS are also included as allowed by Section 7 of the GNU
+// Affrero General Public License. These aditional terms are Sections 1, 5,
+// 6, 7, 8, and 9 from the Apache License, Version 2.0 (the "Apache-2.0")
+// where all references to the definition "License" are instead defined to
+// mean the AGPL-3.0+.
+
+// You should have received a copy of the Apache-2.0 along with this
+// program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
+
+
+/*
+Handler to print error messages at the top of the form.
+
+If the user makes an error within the form template at least the user will be
+presented with some form of error message.
+
+In the future the display of this message could integrate better with
+jupyterlab.
+*/
+
+import { ErrorHandler } from '@angular/core';
+
+export class AppErrorHandler extends ErrorHandler {
+
+  handleError(error: any) {
+   const errorbox = document.getElementsByClassName('errorbox');
+   if (errorbox.length > 0) {
+    errorbox[0].innerHTML = `<h2>Error</h2>
+<p>
+  While rendering your form an error occured.
+</p>
+<p>
+  The error from the Angular compiler is printed below:
+</p>
+<pre style="white-space: pre-wrap;">
+  ` + error + '</pre>';
+   }
+
+   // delegate to the default handler
+   super.handleError(error);
+  }
+}
