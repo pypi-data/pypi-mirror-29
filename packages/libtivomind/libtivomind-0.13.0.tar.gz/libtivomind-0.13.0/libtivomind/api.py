d9f274000fc90b4534433059949fa0848407f994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:57 +0000

    Add package libtivomind/0.13.0/libtivomind-0.13.0.tar.gz

---
+++ packages/libtivomind/libtivomind-0.13.0.tar.gz/libtivomind-0.13.0/libtivomind/api.py
@@ -0,0 +1,369 @@
+import copy
+import contextlib
+import enum
+import threading
+
+import libtivomind.rpc as rpc
+
+
+class RemoteKey(enum.Enum):
+    back = "back"
+    channelUp = "channelUp"
+    lab_channel = "lab_channel"
+    channelDown = "channelDown"
+    left = "left"
+    zoom = "zoom"
+    tivo = "tivo"
+    up = "up"
+    select = "select"
+    down = "down"
+    liveTv = "liveTv"
+    info = "info"
+    right = "right"
+    guide = "guide"
+    num1 = "num1"
+    num2 = "num2"
+    num3 = "num3"
+    num4 = "num4"
+    num5 = "num5"
+    num6 = "num6"
+    num7 = "num7"
+    num8 = "num8"
+    num9 = "num9"
+    clear = "clear"
+    num0 = "num0"
+    enter = "enter"
+    actionA = "actionA"
+    actionB = "actionB"
+    actionC = "actionC"
+    actionD = "actionD"
+    thumbsDown = "thumbsDown"
+    reverse = "reverse"
+    replay = "replay"
+    play = "play"
+    pause = "pause"
+    slow = "slow"
+    record = "record"
+    thumbsUp = "thumbsUp"
+    forward = "forward"
+    advance = "advance"
+
+
+class SearchFilter(object):
+
+    def __init__(self):
+        self.dict = {}
+
+    def by_keywordable(self, field, value, exact_match=False):
+        key = field
+        if not exact_match:
+            key += "Keyword"
+        self.dict[key] = value
+
+    def by_keyword(self, keyword):
+        self.dict['keyword'] = keyword
+
+    def by_title(self, title, exact_match=False):
+        self.by_keywordable('title', title, exact_match)
+
+    def by_subtitle(self, subtitle, exact_match=False):
+        self.by_keywordable('subtitle', subtitle, exact_match)
+
+    def by_description(self, description, exact_match=False):
+        self.by_keywordable('description', description, exact_match)
+
+    def by_credit(self, credit, exact_match=False):
+        self.by_keywordable('credit', credit, exact_match)
+
+    def by_start_time(self, min_utc_time=None, max_utc_time=None):
+        if min_utc_time:
+            self.dict['minStartTime'] = rpc.MRPCSession.get_date_string(min_utc_time)
+        if max_utc_time:
+            self.dict['maxStartTime'] = rpc.MRPCSession.get_date_string(max_utc_time)
+
+    def by_end_time(self, min_utc_time=None, max_utc_time=None):
+        if min_utc_time:
+            self.dict['minEndTime'] = rpc.MRPCSession.get_date_string(min_utc_time)
+        if max_utc_time:
+            self.dict['maxEndTime'] = rpc.MRPCSession.get_date_string(max_utc_time)
+
+    def by_category_id(self, category_id):
+        if isinstance(category_id, dict):
+            category_id = category_id['categoryId']
+        self.dict['categoryId'] = category_id
+
+    def by_content_id(self, content_id):
+        if isinstance(content_id, dict):
+            content_id = content_id['contentId']
+        self.dict['contentId'] = content_id
+
+    def by_collection_id(self, collection_id):
+        if isinstance(collection_id, dict):
+            collection_id = collection_id['collectionId']
+        self.dict['collectionId'] = collection_id
+
+    def by_offer_id(self, offer_id):
+        if isinstance(offer_id, dict):
+            offer_id = offer_id['offerId']
+        self.dict['offerId'] = offer_id
+
+    def by_recording_id(self, recording_id):
+        if isinstance(recording_id, dict):
+            recording_id = recording_id['recordingId']
+        self.dict['recordingId'] = recording_id
+
+    def by_recording_folder_item_id(self, recording_folder_item_id):
+        if isinstance(recording_folder_item_id, dict):
+            recording_folder_item_id = recording_folder_item_id['recordingFolderItemId']
+        self.dict['recordingFolderItemId'] = recording_folder_item_id
+
+    def by_station_id(self, station_id):
+        self.dict['stationId'] = station_id
+
+    def by_user_fields(self, field_values):
+        self.dict.update(field_values)
+
+    def order_by(self, sort_field):
+        self.dict['orderBy'] = sort_field
+
+    def set_level_of_detail(self, level_of_detail=None):
+        if level_of_detail is None:
+            try:
+                del self.dict['levelOfDetail']
+            except KeyError:
+                pass
+        elif 'responseTemplate' in self.dict:
+            raise ValueError('level_of_detail and response_template are conflicting options.  Clear response '
+                             'templates before setting level of detail.')
+        else:
+            self.dict['levelOfDetail'] = level_of_detail
+
+    def set_response_template(self, template_list=None):
+        if template_list is None:
+            try:
+                del self.dict['responseTemplate']
+            except KeyError:
+                pass
+        elif 'levelOfDetail' in self.dict:
+            raise ValueError('level_of_detail and response_template are conflicting options.  Clear level of '
+                             'detail before setting response templates.')
+        else:
+            self.dict['responseTemplate'] = template_list[:]
+
+    def pop(self, key, *args):
+        return self.dict.pop(key, *args)
+
+    def setdefault(self, key, default=None):
+        return self.dict.setdefault(key, default)
+
+    def get_payload(self):
+        return copy.copy(self.dict)
+
+
+class Mind(object):
+
+    def __init__(self, session, level_of_detail="medium"):
+        self.session = session
+        self.level_of_detail = level_of_detail
+
+    def _get_paged_response(self, req_type, payload, target_array, count=20, offset=0, fetch_all=False):
+        results = []
+        payload['count'] = count
+        payload['offset'] = offset
+        req_id = self.session.send_request(req_type, payload)
+        h, b = self.session.get_response(req_id)
+        while target_array in b and len(b[target_array]) > 0:
+            results.extend(b[target_array])
+            if 'isBottom' not in b or b['isBottom'] or not fetch_all:
+                break
+            payload['offset'] = len(results)
+            req_id = self.session.send_request(req_type, payload)
+            h, b = self.session.get_response(req_id)
+        return results
+
+    def _prepare_search(self, search_type, result_type, filt=None, options=None, count=20, offset=0, fetch_all=False):
+        payload = filt if filt is not None else {}
+        updates = options if options is not None and isinstance(options, dict) else {}
+        if isinstance(payload, SearchFilter):
+            payload = payload.get_payload()
+        if not (payload.keys() | options.keys()) & {'levelOfDetail', 'responseTemplate'}:
+            updates['levelOfDetail'] = self.level_of_detail
+        payload.update(updates)
+        return self._get_paged_response(req_type=search_type,
+                                        payload=payload,
+                                        target_array=result_type,
+                                        count=count,
+                                        offset=offset,
+                                        fetch_all=fetch_all)
+
+    def channel_search(self, filt=None, count=20, offset=0, fetch_all=False, no_limit=False):
+        return self._prepare_search(search_type='channelSearch',
+                                    result_type='channel',
+                                    filt=filt,
+                                    options={'bodyId': self.session.body_id, 'flatten': True, 'noLimit': no_limit},
+                                    count=count,
+                                    offset=offset,
+                                    fetch_all=fetch_all)
+
+    def recording_folder_item_search(self, filt=None, count=20, offset=0, fetch_all=False):
+        return self._prepare_search(search_type="recordingFolderItemSearch",
+                                    result_type="recordingFolderItem",
+                                    filt=filt,
+                                    options={'bodyId': self.session.body_id, 'flatten': True},
+                                    count=count,
+                                    offset=offset,
+                                    fetch_all=fetch_all)
+
+    def recording_search(self, filt=None, count=20, offset=0, fetch_all=False):
+        return self._prepare_search(search_type="recordingSearch",
+                                    result_type="recording",
+                                    filt=filt,
+                                    options={'bodyId': self.session.body_id, 'state': ['inProgress', 'scheduled']},
+                                    count=count,
+                                    offset=offset,
+                                    fetch_all=fetch_all)
+
+    def offer_search(self, filt=None, count=20, offset=0, fetch_all=False):
+        return self._prepare_search(search_type="offerSearch",
+                                    result_type="offer",
+                                    filt=filt,
+                                    options={'bodyId': self.session.body_id},
+                                    count=count,
+                                    offset=offset,
+                                    fetch_all=fetch_all)
+
+    def content_search(self, filt=None, count=20, offset=0, fetch_all=False):
+        return self._prepare_search(search_type="contentSearch",
+                                    result_type="content",
+                                    filt=filt,
+                                    options={'bodyId': self.session.body_id},
+                                    count=count,
+                                    offset=offset,
+                                    fetch_all=fetch_all)
+
+    def collection_search(self, filt=None, count=20, offset=0, fetch_all=False):
+        return self._prepare_search(search_type="collectionSearch",
+                                    result_type="collection",
+                                    filt=filt,
+                                    options={'bodyId': self.session.body_id, 'omitPgdImages': True},
+                                    count=count,
+                                    offset=offset,
+                                    fetch_all=fetch_all)
+
+    def category_search(self, filt=None, count=20, offset=0, fetch_all=False, top_level_only=False):
+        return self._prepare_search(search_type="categorySearch",
+                                    result_type="category",
+                                    filt=filt,
+                                    options={'bodyId': self.session.body_id,
+                                             'topLevelOnly': top_level_only},
+                                    count=count,
+                                    offset=offset,
+                                    fetch_all=fetch_all)
+
+    def whats_on_search(self):
+        return self._prepare_search(search_type="whatsOnSearch",
+                                    result_type="whatsOn",
+                                    filt=None,
+                                    options={'bodyId': self.session.body_id})
+
+    def tuner_state(self):
+        return self._prepare_search(search_type="tunerStateEventRegister",
+                                    result_type="state",
+                                    filt=None,
+                                    options={'bodyId': self.session.body_id})
+
+    def send_key(self, key_event, value=None):
+        req_id = None
+        if value is None:
+            if isinstance(key_event, RemoteKey):
+                key_event = key_event.value
+            req_id = self.session.send_request('keyEventSend', {'event': key_event})
+        elif value is not None and key_event == 'ascii':
+            req_id = self.session.send_request('keyEventSend', {'event': 'ascii', 'value': value})
+        h, b = self.session.get_response(req_id)
+        return b
+
+    def change_channel(self, channel_id):
+        req_id = self.session.send_request('channelChange', {'channelId': channel_id})
+        h, b = self.session.get_response(req_id)
+        return b
+
+    def playback(self, recording_id, hide_banner=True):
+        """Initiate playback on the connected device.
+
+        Note that the required id is the childRecordingId of a recording folder item."""
+        req_id = self.session.send_request('uiNavigate', {'uri': 'x-tivo:classicui:playback',
+                                                          'parameters': {'fUseTrioId': True,
+                                                                         'recordingId': recording_id,
+                                                                         'fHideBannerOnEnter': hide_banner}
+                                                          })
+        h, b = self.session.get_response(req_id)
+        return b
+
+    @staticmethod
+    def new_session(cert_path, cert_password, address, credential, port=1413, debug=False):
+        mrpc = rpc.MRPCSession.new_session(cert_path=cert_path,
+                                           cert_password=cert_password,
+                                           address=address,
+                                           credential=credential,
+                                           port=port,
+                                           debug=debug)
+        mrpc.connect()
+        return Mind(session=mrpc)
+
+    @staticmethod
+    def new_local_session(cert_path, cert_password, address, mak, port=1413, debug=False):
+        mrpc = rpc.MRPCSession.new_local_session(cert_path=cert_path,
+                                                 cert_password=cert_password,
+                                                 address=address,
+                                                 mak=mak,
+                                                 port=port,
+                                                 debug=debug)
+        mrpc.connect()
+        return Mind(session=mrpc)
+
+
+class MindManager(object):
+
+    def __init__(self, cert_path, cert_password, address, credential,
+                 port=1413, debug=False, timeout=120):
+        self.__cert_path = cert_path
+        self.__cert_password = cert_password
+        self.__address = address
+        self.__credential = credential
+        self.__port = port
+        self.__debug = debug
+        self.__timeout = timeout
+        self.__mind = None
+        self.__timer = None
+        pass
+
+    def disconnect(self):
+        try:
+            self.__mind.session.close()
+        except AttributeError:
+            pass
+        finally:
+            self.__mind = None
+        if threading.current_thread().getName() == threading.main_thread().getName():
+            try:
+                self.__timer.cancel()
+            except AttributeError:
+                pass
+            finally:
+                self.__timer = None
+
+    @contextlib.contextmanager
+    def mind(self):
+        if self.__timer is not None:
+            self.__timer.cancel()
+            self.__timer = None
+        if self.__mind is None:
+            self.__mind = Mind.new_session(cert_path=self.__cert_path,
+                                           cert_password=self.__cert_password,
+                                           address=self.__address,
+                                           credential=self.__credential,
+                                           port=self.__port,
+                                           debug=self.__debug)
+        yield self.__mind
+        self.__timer = threading.Timer(self.__timeout, self.disconnect)
---
+++ packages/libtivomind/libtivomind-0.13.0.tar.gz/libtivomind-0.13.0/libtivomind/rpc.py
@@ -0,0 +1,231 @@
+import collections
+import io
+import json
+import random
+import re
+import socket
+import ssl
+
+
+class MRPCError(Exception):
+    pass
+
+
+class MRPCCredential(object):
+
+    TYPES = {"MAK_CREDENTIAL": ("mak", ),
+             "WEB_CREDENTIAL": ("username", "password", "unit_name")}
+
+    def __init__(self, cred_type, **kwargs):
+        if cred_type not in MRPCCredential.TYPES:
+            raise ValueError("cred_type must be one of the keys in MRPCCredential.TYPES.")
+        self.__cred_type = cred_type
+        self.__mak = None
+        self.__username = None
+        self.__password = None
+        self.__unit_name = None
+        for param in MRPCCredential.TYPES[cred_type]:
+            if param not in kwargs:
+                raise ValueError('{} is required for {} types.'.format(param, cred_type))
+        if cred_type == "MAK_CREDENTIAL":
+            self.__mak = kwargs["mak"]
+        else:
+            self.__username = kwargs["username"]
+            self.__password = kwargs["password"]
+            self.__unit_name = kwargs["unit_name"]
+
+    @property
+    def cred_type(self):
+        return self.__cred_type
+
+    @property
+    def unit_name(self):
+        return self.__unit_name
+
+    def payload(self):
+        if self.__cred_type == "MAK_CREDENTIAL":
+            return {"credential": {"type": "makCredential", "key": self.__mak}}
+        else:
+            return {"credential": {"type": "mmaCredential", "username": self.__username, "password": self.__password}}
+
+    @staticmethod
+    def new_mak(mak):
+        return MRPCCredential("MAK_CREDENTIAL", mak=mak)
+
+    @staticmethod
+    def new_web(username, password, unit_name):
+        return MRPCCredential("WEB_CREDENTIAL", username=username, password=password, unit_name=unit_name)
+
+
+class SocketMaker(object):
+
+    def __init__(self, cert_path, cert_password):
+        self.ctx = ssl.create_default_context(purpose=ssl.Purpose.SERVER_AUTH)
+        self.ctx.check_hostname = False
+        self.ctx.verify_mode = ssl.CERT_NONE
+        self.ctx.load_cert_chain(cert_path, password=cert_password)
+
+    def get_socket(self):
+        s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+        return self.ctx.wrap_socket(s)
+
+
+class MRPCSession(object):
+
+    eol = '\r\n'
+    response_count = {True: "multiple", False: "single"}
+    schema_version = "17"
+    proto_pat = re.compile("^.*MRPC/2 (?P<h_size>\d+) (?P<b_size>\d+)\r\n")
+
+    def __init__(self, socket_maker, address, credential, port=1413, debug=False):
+        self.sm = socket_maker
+        self.credential = credential
+        self.address = address
+        self.port = port
+        self.socket = None
+        self.session_id = random.randint(0, 2**32 - 1)
+        self.rpc_id = 0
+        self.body_id = ""
+        self.debug = debug
+        self.queue = collections.deque()
+
+    def connect(self):
+        self.socket = self.sm.get_socket()
+        self.socket.connect((self.address, self.port))
+        h, b = self.do_auth()
+        if 'status' not in b or b["status"] != "success":
+            import pprint; pprint.pprint(b); pprint.pprint(self.credential.payload())
+            self.close()
+            raise MRPCError("Auth Failure")
+        if self.credential.cred_type == "WEB_CREDENTIAL":
+            try:
+                devices = [d for d in b["deviceId"] if d["friendlyName"] == self.credential.unit_name]
+                if len(devices) > 0:
+                    self.body_id = devices[0]["id"]
+                else:
+                    raise KeyError("No device entry matching unit_name.")
+            except KeyError:
+                self.body_id = "-"
+        else:
+            self.send_request("bodyConfigSearch", {"bodyId": "-"})
+            h, r = self.get_response()
+            try:
+                self.body_id = r['bodyConfig'][0]['bodyId']
+            except KeyError:
+                self.body_id = "-"
+
+    def close(self):
+        if self.socket is not None:
+            self.socket.shutdown(socket.SHUT_RDWR)
+            self.socket.close()
+            self.socket = None
+
+    def send_request(self, req_type, payload_json, multiple_responses=False):
+        body_id = ""
+        if "bodyId" in payload_json:
+            body_id = payload_json["bodyId"]
+
+        header_tuples = (
+            ("Type", "request"),
+            ("RpcId", "{:d}".format(self.rpc_id)),
+            ("SchemaVersion", "{}".format(self.schema_version)),
+            ("Content-Type", "application/json"),
+            ("RequestType", req_type),
+            ("ResponseCount", self.response_count[multiple_responses]),
+            ("BodyId", body_id),
+            ("X-ApplicationName", "Quicksilver"),
+            ("X-ApplicationVersion", "1.2"),
+            ("X-ApplicationSessionId", "0x{:x}".format(self.session_id)),
+        )
+
+        headers = self.eol.join(["{}: {}".format(k, v) for k, v in header_tuples]) + self.eol
+
+        payload_json['type'] = req_type
+        payload = json.dumps(payload_json)
+        preamble = "MRPC/2 {:d} {:d}".format(len(headers) + 2, len(payload))
+        request = preamble + self.eol + headers + self.eol + payload + "\n"
+        self.rpc_id += 1
+
+        self.socket.sendall(request.encode('ascii'))
+        return self.rpc_id - 1
+
+    @staticmethod
+    def parse_headers(buffer):
+        return dict([line.split(': ', 1) for line in buffer.split('\r\n') if len(line) > 0])
+
+    def __get_response(self):
+        buffer = io.BytesIO()
+        buffer.write(self.socket.recv())
+        while b"\n" not in buffer.getvalue():
+            buffer.write(self.socket.recv())
+        buf_val = buffer.getvalue().decode()
+        m = self.proto_pat.search(buf_val)
+        h_size = int(m.groupdict()['h_size'])
+        b_size = int(m.groupdict()['b_size'])
+        h_start = m.span()[-1]
+        if self.debug:
+            print("RPC Response (Offset: {:d}, H Size: {:d}, B Size: {:d})".format(h_start, h_size, b_size))
+            print("RPC Response (Bytes Loaded: {:d})".format(buffer.tell() - h_start))
+        while buffer.tell() - h_start < h_size + b_size:
+            buffer.write(self.socket.recv())
+            if self.debug:
+                print("RPC Response (Bytes Loaded: {:d})".format(buffer.tell() - h_start))
+        buf_val = buffer.getvalue().decode()
+        headers = self.parse_headers(buf_val[h_start:h_start+h_size])
+        if self.debug:
+            print("RPC Response ID: {}".format(headers['RpcId']))
+        response_json = json.loads(buf_val[h_start+h_size:])
+        return headers, response_json
+
+    def get_response(self, rpcid=None):
+        response = next((rsp for id, rsp in self.queue if id == rpcid or rpcid == None), None)
+        if response:
+            self.queue.remove((response[0]['RpcId'], response))
+            return response
+        headers, body = self.__get_response()
+        while int(headers['RpcId']) != rpcid and rpcid is not None:
+            self.queue.append((int(headers['RpcId']), (headers, body)))
+            headers, body = self.__get_response()
+        return headers, body
+
+    def do_auth(self):
+        self.send_request("bodyAuthenticate", self.credential.payload())
+        h, b = self.get_response()
+        try:
+            return h, b
+        except KeyError:
+            pass
+        raise MRPCError("Auth Error, No Auth Status Response.")
+
+    @staticmethod
+    def get_date_string(date_time):
+        return date_time.strftime("%Y-%m-%d %H:%M:%S")
+
+    @staticmethod
+    def new_session(cert_path, cert_password, address, credential, port=1413, debug=False):
+        sm = SocketMaker(cert_path=cert_path, cert_password=cert_password)
+        return MRPCSession(socket_maker=sm,
+                           address=address,
+                           credential=credential,
+                           port=port,
+                           debug=debug)
+
+    @staticmethod
+    def new_local_session(cert_path, cert_password, address, mak, port=1413, debug=False):
+        cred = MRPCCredential.new_mak(mak=mak)
+        return MRPCSession.new_session(cert_path=cert_path,
+                                       cert_password=cert_password,
+                                       address=address,
+                                       credential=cred,
+                                       port=port,
+                                       debug=debug)
+
+    @staticmethod
+    def new_web_session(cert_path, cert_password, username, password, unit_name, debug=False):
+        cred = MRPCCredential.new_web(username=username, password=password, unit_name=unit_name)
+        return MRPCSession.new_session(cert_path=cert_path,
+                                       cert_password=cert_password,
+                                       address="middlemind.tivo.com",
+                                       credential=cred,
+                                       port=443,
+                                       debug=debug)
