074b055b9169af4036af2e1cdde9c19a16a125f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:20 +0000

    Add package hrjota/0.0.3/hrjota-0.0.3-py2.py3-none-any.whl

---
+++ packages/hrjota/hrjota-0.0.3-py2.py3-none-any.whl/hrjota/globals.py
@@ -0,0 +1,16 @@
+"""
+global vars for hrjota
+"""
+
+__version__ = '0.0.3'
+
+DEFAULT_WAIT_TIME = 120
+DEFAULT_INTERVAL = 10
+
+BAD_FIELDS = [
+    'compatibilities',
+    'revision',
+    'status',
+    'taskDefinitionArn',
+    'requiresAttributes'
+]
