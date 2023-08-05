4986bda9add7a28fc6930f93eb642744e542a3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:07 +0000

    Add package kforce/0.1.2/kforce-0.1.2.tar.gz

---
+++ packages/kforce/kforce-0.1.2.tar.gz/kforce-0.1.2/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = "0.1.2"
+
+ROOT_DIR = os.path.dirname(__file__)
+
+with open(os.path.join(ROOT_DIR, "requirements", "base.txt"), "r") as f:
+    install_requires = f.read().splitlines()
+    if not install_requires:
+        print(
+            "Unable to read requirements from the requirements.txt file"
+            "That indicates this copy of the source code is incomplete."
+        )
+        sys.exit(2)
+
+with open(os.path.join(ROOT_DIR, "README.md"), "r") as f:
+    long_description = f.read()
+
+if __name__ == "__main__":
+    setup(
+        name="kforce",
+        version=VERSION,
+        author="Yang Kelvin Liu",
+        author_email="ycliuhw@gmail.com",
+        license="New BSD license",
+        url="https://github.com/ycliuhw/kforce",
+        description="KOPS template automation",
+        # long_description=long_description,
+        packages=find_packages(),
+        scripts=["bin/kforce"],
+        keywords=['k8s', 'kops', 'kubernetes', 'template'],
+        install_requires=install_requires,
+    )
