f0c2a766f7731eab30f92167a4f388ce5dce7819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:22 +0000

    Add package tensorci-client/0.0.10/tensorci-client-0.0.10.tar.gz

---
+++ packages/tensorci-client/tensorci-client-0.0.10.tar.gz/tensorci-client-0.0.10/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='tensorci-client',
+      version='0.0.10',
+      description='TensorCI API Client',
+      url='https://github.com/tensorci/tensorci-client',
+      author='Ben Whittle',
+      author_email='benwhittle31@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+        'requests',
+        'awesome-slugify',
+        'redis'
+      ],
+      zip_safe=False)
