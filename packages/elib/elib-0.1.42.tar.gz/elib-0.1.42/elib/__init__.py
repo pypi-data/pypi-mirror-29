9ae28fc144038c86e0c80696a915566a86f34ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:58 +0000

    Add package elib/0.1.42/elib-0.1.42.tar.gz

---
+++ packages/elib/elib-0.1.42.tar.gz/elib-0.1.42/elib/__init__.py
@@ -0,0 +1,18 @@
+"""
+Placeholder for future personal library
+"""
+from pkg_resources import DistributionNotFound, get_distribution
+
+from . import config, console, custom_logging, custom_random, paste, path, tts, settings, hash_, downloader
+from ._run import run
+from .pretty import pretty_format
+
+try:
+    __version__ = get_distribution('elib').version
+except DistributionNotFound:  # pragma: no cover
+    # package is not installed
+    __version__ = 'not installed'
+
+if __package__ == 'elib':  # pragma: no cover
+    LOGGER = custom_logging.get_logger('ELIB')
+    LOGGER.info(f'ELIB version {__version__}')
