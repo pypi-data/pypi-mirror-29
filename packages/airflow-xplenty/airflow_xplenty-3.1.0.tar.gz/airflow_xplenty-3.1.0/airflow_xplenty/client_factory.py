20256989bebaedb3b46a1363000c262c18fcfcdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package airflow-xplenty/3.1.0/airflow_xplenty-3.1.0.tar.gz

---
+++ packages/airflow-xplenty/airflow_xplenty-3.1.0.tar.gz/airflow_xplenty-3.1.0/airflow_xplenty/client_factory.py
@@ -0,0 +1,11 @@
+from airflow import configuration
+from xplenty import XplentyClient
+
+"""Convenience factory for constructing an XplentyClient with proper credentials"""
+class ClientFactory:
+    def __init__(self):
+        self.account_id = configuration.get('xplenty', 'account_id')
+        self.api_key = configuration.get('xplenty', 'api_key')
+
+    def client(self):
+        return XplentyClient(self.account_id, self.api_key)
