e236aff344e8f3698c03fbb01f65c784eb9d03b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:23 +0000

    Add package charlie-pop-hello/0.0.1/charlie_pop_hello-0.0.1-py2.py3-none-any.whl

---
+++ packages/charlie-pop-hello/charlie_pop_hello-0.0.1-py2.py3-none-any.whl/hello.py
@@ -0,0 +1,31 @@
+"""
+A new python package to learn how to create python packages
+"""
+import sys
+
+__version__ = '0.0.1'
+
+def hello(name='world'):
+    """
+    Return a greeting for the given name
+    """
+    return 'Hello, {}'.format(name)
+
+
+def main():
+   """
+   Reads input from the args passed into the script and prints the
+   output to stdout.
+   """
+   args = sys.argv[1:]
+   name = ' '.join(args)
+   if name:
+       print(hello(name))
+   else:
+       print(hello())
+       
+
+if __name__ == '__main__':
+    main()
+
+
