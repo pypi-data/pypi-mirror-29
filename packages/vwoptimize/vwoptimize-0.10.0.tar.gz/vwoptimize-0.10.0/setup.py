89a9b16dd437cef37edf9b1a50549d6e432117a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:25 +0000

    Add package vwoptimize/0.10.0/vwoptimize-0.10.0.tar.gz

---
+++ packages/vwoptimize/vwoptimize-0.10.0.tar.gz/vwoptimize-0.10.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import re
+
+version = re.search(r"__version__\s*=\s*'(.*)'", open('vwoptimize.py').read(500), re.M).group(1)
+assert version
+
+setup(name='vwoptimize',
+      version=version,
+      description='Hyper-parameter search, text preprocessing and reporting for Vowpal Wabbit',
+      author='Denis Bilenko',
+      author_email='denis.bilenko@gmail.com',
+      url='https://github.com/denik/vwoptimize',
+      py_modules=['vwoptimize'],
+      scripts=['vwoptimize.py']
+      )
