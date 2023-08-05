f641b83a2503024882f04f013f314cc5ea69cf21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package tensorci/0.0.19/tensorci-0.0.19.tar.gz

---
+++ packages/tensorci/tensorci-0.0.19.tar.gz/tensorci-0.0.19/tensorci/commands/__init__.py
@@ -0,0 +1,25 @@
+from login import login
+from logout import logout
+from create import create
+from init import init
+from push import push
+from train import train
+from serve import serve
+from get import get
+from version import version
+from logs import logs
+from dash import dash
+
+all_cmds = [
+  login,
+  logout,
+  create,
+  init,
+  push,
+  train,
+  serve,
+  get,
+  version,
+  logs,
+  dash
+]
