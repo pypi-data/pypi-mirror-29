7e74919e563fb10a210d2cc361eae7f61c7c2b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:10 +0000

    Add package faktory/0.4.0/faktory-0.4.0.tar.gz

---
+++ packages/faktory/faktory-0.4.0.tar.gz/faktory-0.4.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='faktory',
+      version='0.4.0',
+      description='Python worker for the Faktory project',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3',
+          'Topic :: System :: Distributed Computing'
+      ],
+      keywords='faktory worker',
+      url='http://github.com/cdrx/faktory_python_worker',
+      author='Chris R',
+      license='BSD',
+      packages=['faktory'],
+      zip_safe=False)
