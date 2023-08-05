05f6287baf1a22a128a5d696c78fe95c188177f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:29 +0000

    Add package arm-pyart/1.9.0/arm_pyart-1.9.0.tar.gz

---
+++ packages/arm-pyart/arm_pyart-1.9.0.tar.gz/arm_pyart-1.9.0/pyart/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYART SETUP.PY
+short_version = '1.9.0'
+version = '1.9.0'
+full_version = '1.9.0'
+git_revision = '752e4f6b046e3ded2d48ba7c01b76b3a9b28d387'
+release = True
+
+if not release:
+    version = full_version
