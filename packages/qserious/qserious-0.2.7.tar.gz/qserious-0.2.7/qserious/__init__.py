28c913118556815ebc787f59f9cdeb149ce97a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:34 +0000

    Add package qserious/0.2.7/qserious-0.2.7.tar.gz

---
+++ packages/qserious/qserious-0.2.7.tar.gz/qserious-0.2.7/qserious/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    qserious
+    ~~~~~
+"""
+
+from .qserious import deserialize, serialize, setKey, bracketsToDots, keysTobrackets
+
+__version__ = '0.2.7'
+
+# __all__ = ['deserialize', 'serialize', 'setKey', 'bracketsToDots', 'keysTobrackets']
