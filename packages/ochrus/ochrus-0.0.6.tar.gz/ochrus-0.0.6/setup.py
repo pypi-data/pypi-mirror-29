e2a0a9fd57ea862c822bfeb087fc5348c9467991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:49 +0000

    Add package ochrus/0.0.6/ochrus-0.0.6.tar.gz

---
+++ packages/ochrus/ochrus-0.0.6.tar.gz/ochrus-0.0.6/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'ochrus',
+  packages = ['ochrus'], # this must be the same as the name above
+  license = 'MIT',
+  version = '0.0.6',
+  description = 'Ochrus functional test automation infrastructure',
+  long_description=open('README.rst').read(),
+  author = 'Roni Eliezer',
+  author_email = 'roniezr@gmail.com',
+  url = 'https://github.com/ochrus/ochrus', # use the URL to the github repo
+  download_url = 'https://github.com/ochrus/ochrus-0.0.6.tar.gz',
+  keywords = ['functional', 'testing', 'automation', 'infrastructure'], 
+  classifiers = [],
+  install_requires=['paramiko', 'requests'],
+)
