0162515abd6c566a0a51bfe25534d19664ec7f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:11 +0000

    Add package pbkdf2helper/0.0.3/pbkdf2helper-0.0.3.tar.gz

---
+++ packages/pbkdf2helper/pbkdf2helper-0.0.3.tar.gz/pbkdf2helper-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+      name='pbkdf2helper',
+      packages=['pbkdf2helper'],
+      version='0.0.3',
+      description='This Python module implements the basic PBKDF2 Helper-functions',
+
+      author='Alexander Kleinschmidt (kungalex)',
+      author_email='kung4lex@gmail.ooo',
+      license='MIT',
+
+      url='http://github.com/kungalex/pbkdf2helper',
+      download_url='https://github.com/kungalex/pbkdf2helper/archive/0.0.1.tar.gz',
+
+      keywords=['pbkdf2', 'hashed', 'password'],
+      classifiers=[],
+      )
+
