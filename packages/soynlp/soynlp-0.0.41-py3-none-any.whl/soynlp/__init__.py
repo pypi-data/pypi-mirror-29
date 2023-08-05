7a00a12ac7ba82384e6fb951981530f9da1fba05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:04 +0000

    Add package soynlp/0.0.41/soynlp-0.0.41-py3-none-any.whl

---
+++ packages/soynlp/soynlp-0.0.41-py3-none-any.whl/soynlp/__init__.py
@@ -0,0 +1,17 @@
+__title__ = 'soynlp'
+__version__ = '0.0.41'
+__author__ = 'Lovit'
+__license__ = 'GPL v3'
+__copyright__ = 'Copyright 2017 Lovit'
+
+from . import hangle
+from . import normalizer
+from . import noun
+from . import pos
+from . import tokenizer
+from . import vectorizer
+from . import word
+from .utils import get_available_memory
+from .utils import get_process_memory
+from .utils import sort_by_alphabet
+from .utils import DoublespaceLineCorpus
