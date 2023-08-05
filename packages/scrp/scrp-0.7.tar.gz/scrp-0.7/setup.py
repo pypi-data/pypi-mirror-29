b4f49808e84e6a010129e081539495b9d376fcf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:28 +0000

    Add package scrp/0.7/scrp-0.7.tar.gz

---
+++ packages/scrp/scrp-0.7.tar.gz/scrp-0.7/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'scrp',
+  packages = ['scrp'],
+  package_dir = {'scrp': 'scrp'},
+  package_data = {'scrp': ['*']},
+  version = '0.7',
+  description = 'Methods for scraping the web',
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/DanielJDufour/scrp',
+  download_url = 'https://github.com/DanielJDufour/scrp',
+  keywords = ['scraping'],
+  classifiers = [],
+)
