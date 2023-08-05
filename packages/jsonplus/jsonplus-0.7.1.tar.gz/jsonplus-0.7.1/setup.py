c969bec774cf5cb09c46c8c04d8af4e9185a6d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:31 +0000

    Add package jsonplus/0.7.1/jsonplus-0.7.1.tar.gz

---
+++ packages/jsonplus/jsonplus-0.7.1.tar.gz/jsonplus-0.7.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='jsonplus',
+    version='0.7.1',
+    description="Custom datatypes (like datetime) serialization to/from JSON.",
+    long_description=open('README.rst').read(),
+    author='Radomir Stevanovic',
+    author_email='radomir.stevanovic@gmail.com',
+    url='https://github.com/randomir/jsonplus',
+    license='MIT',
+    packages=['jsonplus'],
+    package_dir={'jsonplus': 'jsonplus'},
+    install_requires=[i.strip() for i in open('requirements.txt').readlines()],
+    extras_require={
+        ':python_version<"2.7"': ['ordereddict>=1.1']
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='json custom python type serialization deserialization datetime set'
+)
