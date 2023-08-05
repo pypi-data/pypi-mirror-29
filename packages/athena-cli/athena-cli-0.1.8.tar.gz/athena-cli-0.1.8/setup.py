b3c05055fb0762e76f0ac6f51680ead9554282d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:24 +0000

    Add package athena-cli/0.1.8/athena-cli-0.1.8.tar.gz

---
+++ packages/athena-cli/athena-cli-0.1.8.tar.gz/athena-cli-0.1.8/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup, find_packages
+
+version = '0.1.8'
+
+setup(
+    name="athena-cli",
+    version=version,
+    description='Presto-like CLI for AWS Athena',
+    url='https://github.com/guardian/athena-cli',
+    license='Apache License 2.0',
+    author='Nick Satterly',
+    author_email='nick.satterly@theguardian.com',
+    packages=find_packages(),
+    py_modules=[
+        'athena_cli'
+    ],
+    install_requires=[
+        'boto3',
+        'cmd2',
+        'tabulate>=0.8.1'
+    ],
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'athena = athena_cli:main'
+        ]
+    },
+    keywords='aws athena presto cli',
+    classifiers=[
+        'Topic :: Utilities'
+    ]
+)
