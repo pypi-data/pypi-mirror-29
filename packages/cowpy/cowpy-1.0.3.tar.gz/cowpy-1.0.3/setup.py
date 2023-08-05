5e9d61751f94acf2bd8dd8317774985a9db18b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package cowpy/1.0.3/cowpy-1.0.3.tar.gz

---
+++ packages/cowpy/cowpy-1.0.3.tar.gz/cowpy-1.0.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup
+
+setup(name='cowpy',
+      description="A cowsay clone for python in one file.",
+      author="Jeff Buttars",
+      author_email="jeffbuttars@gmail.com",
+      url="https://github.com/jeffbuttars/cowpy",
+      version='1.0.3',
+      packages=['cowpy'],
+      scripts=['scripts/cowpy']
+      )
