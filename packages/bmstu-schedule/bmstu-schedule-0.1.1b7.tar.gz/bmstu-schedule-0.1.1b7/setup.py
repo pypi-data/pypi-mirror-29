4b8cdbc997fc3e739955f41a1a1ba2479e010687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:28 +0000

    Add package bmstu-schedule/0.1.1b7/bmstu-schedule-0.1.1b7.tar.gz

---
+++ packages/bmstu-schedule/bmstu-schedule-0.1.1b7.tar.gz/bmstu-schedule-0.1.1b7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='bmstu-schedule',
+    version='0.1.1-beta-7',
+    author='George Gabolaev',
+    author_email='gabolaev98@gmail.com',
+    url='https://github.com/gabolaev/bmstuSchedule',
+    license='MIT',
+    python_requires='>=3.5',
+    description='BMSTU EU Schedule iCal parser',
+    packages=['bmstu_schedule', 'bmstu_schedule.configs'],
+    install_requires=[
+        'lxml',
+        'bs4',
+        'requests',
+        'termcolor'
+    ],
+    entry_points={
+        'console_scripts': [
+            'bmstu-schedule = bmstu_schedule.__main__:main'
+        ]
+    },
+)
