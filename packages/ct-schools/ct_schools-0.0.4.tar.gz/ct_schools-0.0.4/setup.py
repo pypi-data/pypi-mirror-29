87c9b2b079e61092df803c4df1a6fbd6c444b4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:56 +0000

    Add package ct-schools/0.0.4/ct_schools-0.0.4.tar.gz

---
+++ packages/ct-schools/ct_schools-0.0.4.tar.gz/ct_schools-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='ct_schools',
+    description="Get data about Connecticut schools",
+    version='0.0.4',    
+    author="Jake Kara",
+    author_email="jake@jakekara.com",
+    url="https://github.com/jakekara/ct_schools",
+    packages=['ct_schools'],
+    package_data={'ct_schools': ['data/*.csv']},
+    install_requires=["fuzzywuzzy==0.16.0",
+                      "pandas==0.22.0",
+                      "python-Levenshtein==0.12.0"],
+    license="GPL-3",
+)
+
