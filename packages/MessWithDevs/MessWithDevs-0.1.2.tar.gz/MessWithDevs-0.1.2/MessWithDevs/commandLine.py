95bece5fa1cb0653f2ed3686a11cefeb7419acc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package MessWithDevs/0.1.2/MessWithDevs-0.1.2.tar.gz

---
+++ packages/MessWithDevs/MessWithDevs-0.1.2.tar.gz/MessWithDevs-0.1.2/MessWithDevs/commandLine.py
@@ -0,0 +1,12 @@
+#============================================================================
+# Name        : commandLine.py
+# Author      : Alex James Wright
+# Version     : 0.1
+# Copyright   : MIT
+# Description : For command line tool
+#============================================================================
+
+from . import ruin
+
+def main():
+    ruin()
