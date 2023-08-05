2dd8dbe045e14fe55bea65123b03624670bd1e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:30 +0000

    Add package pygaps/1.2.0/pygaps-1.2.0-py2.py3-none-any.whl

---
+++ packages/pygaps/pygaps-1.2.0-py2.py3-none-any.whl/pygaps/graphing/isothermgraphs.py
@@ -0,0 +1,474 @@
+"""
+This module contains the functions for plotting and comparing isotherms.
+"""
+
+import matplotlib.pyplot as plt
+from cycler import cycler
+from matplotlib import cm
+from numpy import linspace
+
+from ..utilities.exceptions import GraphingError
+from ..utilities.exceptions import ParameterError
+from ..utilities.string_utilities import convert_chemformula
+
+# ! list of plot types
+_PLOT_TYPES = ("isotherm", "property", "combined")
+
+# ! list of branch types
+_BRANCH_TYPES = ("ads", "des")
+
+
+def plot_iso(isotherms,
+             plot_type='isotherm', secondary_key=None,
+             branch=_BRANCH_TYPES, logx=False, color=True,
+             match_points=False,
+
+             adsorbent_basis="mass",
+             adsorbent_unit="g",
+             loading_basis="molar",
+             loading_unit="mmol",
+             pressure_mode="absolute",
+             pressure_unit="bar",
+
+             x_range=(None, None),
+             y1_range=(None, None),
+             y2_range=(None, None),
+
+             legend_list=None,
+             fig_title=None,
+             legend_bottom=False,
+
+             save=False, path=None,
+             **other_parameters):
+    """
+    Plots the isotherm(s) provided on a single graph.
+
+    Parameters
+    ----------
+    isotherms : list
+        An iterable of the isotherms to be plotted.
+    plot_type : {'isotherm', 'property', 'combined'}
+        The plot type, to display: isotherm, a property or a combination.
+        The 'isotherm' graph type displays only isotherm data and is the standard.
+        If other data is recorded in the isotherm object, such as enthalpy, it
+        can be displayed at the same time as the isotherm or in a property v
+        loading graph by selecting one of the other graph types.
+    secondary_key : 'str'
+        The key which has the column with the supplementary data to be plotted.
+        This parameter is only required in the 'property' and 'combined' graphs.
+    branch : list
+        List with branches to disply, options: 'ads', 'des'.
+    logx : bool
+        Whether the graph x axis should be logarithmic.
+    color : bool, optional
+        Whether the graph should be coloured or grayscale. Grayscale graphs
+        are usually preffered for publications or print media.
+    match_points : bool, optional
+        Whether the plotting should attempt to match all the pressure
+        points with the first passed isotherm.
+
+    adsorbent_basis : str, optional
+        Whether the adsorption is read in terms of either 'per volume'
+        or 'per mass'.
+    adsorbent_unit : str, optional
+        Unit of loading.
+    loading_basis : str, optional
+        Loading basis.
+    loading_unit : str, optional
+        Unit of loading.
+    pressure_mode : str, optional
+        The pressure mode, either absolute pressures or relative in
+        the form of p/p0.
+    pressure_unit : str, optional
+        Unit of pressure.
+
+    x_range : tuple
+        Range for data on the x axis. eg: (0, 1). Is applied to each
+        isotherm, in the unit/mode/basis requested.
+    y1_range : tuple
+        Range for data on the regular y axis. eg: (0, 1). Is applied to each
+        isotherm, in the unit/mode/basis requested.
+    y2_range : tuple
+        Range for data on the secondary y axis. eg: (0, 1). Is applied to each
+        isotherm, in the unit/mode/basis requested.
+
+    legend_list : iterable
+        The components of the isotherm which are displayed on the legend. For example
+        pass ['sample_batch', 'adsorbate'] to have the legend labels display only these
+        two components. Defaults to the sample name and adsorbate.
+    fig_title : str
+        Title of the graph. Defaults to type of graph.
+    legend_bottom : bool
+        Specify as True to have the legend at the bottom of the graph. Alternatively,
+        when plotting a lot of data, the legend is best displayed separately to the
+        side of the graph. In that case, set this to False.
+
+    save : bool, optional
+        Whether to save the graph or not.
+    path : str, optional
+        The path where the graph will be saved.
+
+    Returns
+    -------
+
+    fig : Matplotlib figure
+        The figure object generated.
+    axes1 : Matplotlib ax
+        Ax object for primary graph.
+    axes2 : Matplotlib ax
+        Ax object for secondary graph.
+
+    """
+#######################################
+#
+# Initial checks
+    #
+    # Check for plot type validity
+    if plot_type is None:
+        raise ParameterError("Specify a plot type to graph"
+                             " e.g. plot_type=\'isotherm\'")
+    if plot_type not in _PLOT_TYPES:
+        raise ParameterError("Plot type {0} not an option. Viable plot"
+                             "types are {1}".format(plot_type, _PLOT_TYPES))
+
+    if plot_type == 'property' or plot_type == 'combined':
+        if secondary_key is None:
+            raise ParameterError("No secondary_key parameter specified")
+        if all(secondary_key not in isotherm.other_keys for isotherm in isotherms):
+            raise GraphingError(
+                "None of the isotherms supplied have {} data".format(secondary_key))
+
+    # Store which branches will be displayed
+    if branch is None:
+        raise ParameterError("Specify a branch to display"
+                             " e.g. branch=\'ads\'")
+    if not [i for i in branch if i in _BRANCH_TYPES]:
+        raise GraphingError("One of the supplied branch types is not valid."
+                            "Viable types are {}".format(_BRANCH_TYPES))
+
+    ads = False
+    des = False
+    if 'ads' in branch:
+        ads = True
+    if 'des' in branch:
+        des = True
+
+    # Create empty axes object
+    axes2 = None
+
+    # Add limits for data x and y
+    range_pressure = None
+    range_property = None
+    range_loading = None
+
+#######################################
+#
+# Settings and graph generation
+    #
+    # Generate the graph iself
+    fig, axes = plt.subplots(1, 1, figsize=(8, 8))
+    if plot_type == 'combined':
+        axes2 = axes.twinx()
+
+    # Build the name of the axes
+    text_xaxis = r'Pressure'
+    text_yaxis = r'Loading'
+    if secondary_key == 'enthalpy':
+        text_y2axis = r'Enthalpy of adsorption $(-kJ\/mol^{-1})$'
+    else:
+        text_y2axis = secondary_key
+    if pressure_mode == "absolute":
+        text_xaxis = text_xaxis + ' ($' + pressure_unit + '$)'
+    elif pressure_mode == "relative":
+        text_xaxis = "Relative " + text_xaxis
+    text_yaxis = text_yaxis + \
+        ' ($' + loading_unit + '/' + adsorbent_unit + '$)'
+
+    # Set the colours of the graph
+    number_of_lines = 6
+    cm_subsection = linspace(0, 1, number_of_lines)
+    colors = [cm.jet(x) for x in cm_subsection]
+    cy1 = cycler('marker', ['o', 's'])
+    cy2 = cycler('marker', ['v', '^'])
+    cy3 = cycler('color', colors)
+    cy4 = cycler('marker', ['v', '^', '<', '>'])
+    cy5 = cycler('linestyle', ['-', '--', ':', '-.'])
+    cy6 = cycler('color', ['black', 'grey', 'silver'])
+
+    if color:
+        pc_iso = (cy1 * cy3)
+        pc_prop = (cy2 * cy3)
+    else:
+        pc_iso = (cy1 * cy5 * cy6)
+        pc_prop = (cy4 * cy5 * cy6)
+
+    # Set the colours and ranges for the axes
+    if plot_type == 'isotherm':
+        axes.set_prop_cycle(pc_iso)
+        range_pressure = x_range
+        range_loading = y1_range
+        range_property = (None, None)
+    elif plot_type == 'property':
+        axes.set_prop_cycle(pc_prop)
+        range_pressure = (None, None)
+        range_loading = x_range
+        range_property = y1_range
+    elif plot_type == 'combined':
+        axes.set_prop_cycle(pc_iso)
+        axes2.set_prop_cycle(pc_prop)
+        range_pressure = x_range
+        range_loading = y1_range
+        range_property = y2_range
+
+    # Styles in the graph
+    title_style = dict(horizontalalignment='center',
+                       fontsize=25, fontdict={'family': 'monospace'})
+    label_style = dict(horizontalalignment='center',
+                       fontsize=20, fontdict={'family': 'monospace'})
+    line_style = dict(linewidth=2, markersize=4)
+    tick_style = dict(labelsize=17)
+    styles = dict(title_style=title_style, label_style=label_style,
+                  line_style=line_style, tick_style=tick_style)
+
+    styles.update(other_parameters)  # Update with any user provided styles
+
+    # Put grid on plot
+    axes.grid(True, zorder=5)
+
+    # Graph title
+    if fig_title is None:
+        fig_title = plot_type.capitalize() + ' Graph'
+    axes.set_title(fig_title, **title_style)
+
+    # Graph legend builder
+    def build_label(lbl_components, isotherm):
+        """
+        Builds a label for the legend depending on requested parameters
+        """
+        if lbl_components is None:
+            return isotherm.sample_name + ' ' + convert_chemformula(isotherm.adsorbate)
+        else:
+            text = []
+            for selected in lbl_components:
+                if selected == 'branch':
+                    continue
+                val = getattr(isotherm, selected)
+                if val:
+                    if selected == 'adsorbate':
+                        text.append(convert_chemformula(val))
+                    else:
+                        text.append(str(val))
+
+            return " ".join(text)
+###########################################
+#
+# Individual raphing functions
+    #
+
+    def isotherm_graph(axes, data_x, data_y, line_label, styles_dict):
+        "Plot an isotherm graph, regular or logx"
+
+        # Labels and ticks
+        axes.set_xlabel(text_xaxis, **styles_dict['label_style'])
+        axes.set_ylabel(text_yaxis, **styles_dict['label_style'])
+        axes.tick_params(axis='both', which='major',
+                         **styles_dict['tick_style'])
+
+        # Generate a linestyle from the incoming dictionary
+        line_style = styles_dict['line_style']
+        specific_line_style = dict(linewidth=2, markersize=8)
+        line_style.update(specific_line_style)
+
+        # Plot line
+        line, = axes.plot(data_x, data_y, label=line_label, **line_style)
+
+        return line
+
+    def property_graph(axes, data_x, data_y, line_label, styles_dict):
+        "Plot an property graph versus amount adsorbed"
+
+        # Labels and ticks
+        axes.set_xlabel(text_yaxis, **styles_dict['label_style'])
+        axes.set_ylabel(text_y2axis, **styles_dict['label_style'])
+        axes.tick_params(axis='both', which='major',
+                         **styles_dict['tick_style'])
+        axes.set_ymargin(1)
+
+        # Generate a linestyle from the incoming dictionary
+        line_style = styles_dict['line_style']
+        specific_line_style = dict(linewidth=0, markersize=8)
+        line_style.update(specific_line_style)
+
+        # Plot line
+        line, = axes.plot(data_x, data_y, label=line_label, **line_style)
+
+        return line
+
+    def graph_caller(axes, axes2, isotherm, plot_branch, label, pl_type, styles):
+        """
+        Convenience function to call other graphing functions
+        """
+
+        line = None
+
+        # Get the data from the isotherm
+        def pressure():
+            return isotherm.pressure(
+                branch=plot_branch,
+                pressure_unit=pressure_unit,
+                pressure_mode=pressure_mode,
+                min_range=range_pressure[0],
+                max_range=range_pressure[1],
+                indexed=True,
+            )
+
+        def loading():
+            return isotherm.loading(
+                branch=plot_branch,
+                loading_unit=loading_unit,
+                loading_basis=loading_basis,
+                adsorbent_unit=adsorbent_unit,
+                adsorbent_basis=adsorbent_basis,
+                min_range=range_loading[0],
+                max_range=range_loading[1],
+                indexed=True,
+            )
+
+        def secondary_prop():
+            return isotherm.other_data(
+                secondary_key,
+                branch=plot_branch,
+                min_range=range_property[0],
+                max_range=range_property[1],
+                indexed=True,
+            )
+
+        if pl_type == 'isotherm':
+            p_points, l_points = pressure().align(loading(), join='inner')
+            line = isotherm_graph(axes,
+                                  p_points,
+                                  l_points,
+                                  label, styles_dict=styles)
+
+        elif pl_type == 'property':
+            l_points, o_points = loading().align(secondary_prop(), join='inner')
+            line = property_graph(axes,
+                                  l_points,
+                                  o_points,
+                                  label, styles_dict=styles)
+
+        elif pl_type == 'combined':
+            p_points, l_points = pressure().align(loading(), join='inner')
+            line = isotherm_graph(axes,
+                                  p_points,
+                                  l_points,
+                                  label, styles_dict=styles)
+
+            if secondary_prop is not None and secondary_key in isotherm.other_keys:
+                p_points, o_points = pressure().align(secondary_prop(), join='inner')
+                property_graph(axes2,
+                               p_points,
+                               o_points,
+                               label, styles_dict=styles)
+            else:
+                pass
+
+        return line
+
+#####################################
+#
+# Actual plotting
+    #
+    # Plot the data
+    for isotherm in isotherms:
+
+        # First build the label of the isotherm for the legend
+        line_label = build_label(legend_list, isotherm)
+
+        # Colour of the line, now empty
+        color = None
+
+        # If there's an adsorption branch, plot it
+        if ads:
+            plot_branch = 'ads'
+            if isotherm.has_branch(branch=plot_branch):
+
+                # Label the branch
+                lbl = line_label
+                if legend_list is not None and 'branch' in legend_list:
+                    lbl += ' ads'
+
+                # Call the plotting function
+                line = graph_caller(axes, axes2,
+                                    isotherm, plot_branch,
+                                    lbl, plot_type, styles)
+
+                color = line.get_color()
+
+        if des:
+            plot_branch = 'des'
+            if isotherm.has_branch(branch=plot_branch):
+
+                # Label the branch
+                lbl = line_label
+                if legend_list is not None and 'branch' in legend_list:
+                    lbl += ' ads'
+
+                # Set marker fill to empty, and match the colour from desorption
+                styles['line_style']['mfc'] = 'none'
+                if color is not None:
+                    styles['line_style']['c'] = color
+
+                # Call the plotting function
+                line = graph_caller(axes, axes2,
+                                    isotherm, plot_branch,
+                                    lbl, plot_type, styles)
+
+                # Delete the colour changes to go back to original settings
+                del styles['line_style']['mfc']
+                if color is not None:
+                    del styles['line_style']['c']
+
+
+#####################################
+#
+# Final settings
+    #
+    # Convert the axes into logarithmic if required
+    if logx:
+        axes.set_xscale('log')
+    else:
+        axes.set_xscale('linear')
+        axes.set_xlim(xmin=0)
+
+    axes.set_xlim(x_range)
+    axes.set_ylim(y1_range)
+    if axes2:
+        axes2.set_ylim(y2_range)
+
+    # Add the legend
+    lines, labels = axes.get_legend_handles_labels()
+    if plot_type == 'combined':
+        lines2, labels2 = axes2.get_legend_handles_labels()
+        lines = lines + lines2
+        labels = labels + labels2
+
+    legend_style = dict(handlelength=3, fontsize=15, loc='best')
+    if legend_bottom:
+        legend_style['bbox_to_anchor'] = (0.5, -0.1)
+        legend_style['ncol'] = 2
+        legend_style['loc'] = 'upper center'
+    elif len(lines) > 5:
+        legend_style['bbox_to_anchor'] = (1.15, 0.5)
+        legend_style['loc'] = 'center left'
+
+    lgd = axes.legend(lines, labels, **legend_style)
+
+    # Fix size of graphs
+    plt.tight_layout()
+
+    if save is True:
+        fig.savefig(path, bbox_extra_artists=(lgd,),
+                    bbox_inches='tight', transparent=False)
+
+    return fig, axes, axes2
---
+++ packages/pygaps/pygaps-1.2.0-py2.py3-none-any.whl/pygaps/parsing/excelinterface.py
@@ -0,0 +1,328 @@
+# %%
+
+"""
+This module contains the excel interface for returning data in all formats
+used, such as the parser file.
+"""
+
+import os
+
+import pandas
+
+from ..classes.pointisotherm import PointIsotherm
+from ..utilities.exceptions import ParsingError
+from ..utilities.unit_converter import find_basis
+from ..utilities.unit_converter import find_mode
+
+# chose an implementation, depending on os
+if os.name == 'nt':  # sys.platform == 'win32':
+    import xlwings
+else:
+    print(
+        "xlwings functionality disabled on this platform ( {0} )".format(os.name))
+
+
+def isotherm_to_xl(isotherm, path, fmt=None):
+    '''
+
+    A function that turns the isotherm into an excel file with the data and properties.
+
+    Parameters
+    ----------
+    isotherm : PointIsotherm
+        Isotherm to be written to excel.
+    path : str
+        Path to the file to be written.
+    fmt : {None, 'MADIREL'}, optional
+        If the format is set to MADIREL, then the excel file is a specific version
+        used by the MADIREL lab for internal processing.
+    '''
+
+    if xlwings is None:
+        raise ParsingError(
+            "xlwings functionality disabled on this platform ( {0} )".format(os.name))
+
+    # create a new workbook and select first sheet
+    try:
+        wb = xlwings.Book()
+    except Exception as e_info:
+        raise SystemError(
+            "Failed to connect to excel. Is it available?") from e_info
+
+    wb.app.screen_updating = False
+    sht = wb.sheets[0]
+
+    try:
+        # write the isotherm parameters
+        if isotherm.is_real is True:
+            is_real = "Experience"
+        else:
+            is_real = "Simulation"
+
+        exp_type = isotherm.exp_type
+        if fmt == 'MADIREL':
+            if isotherm.exp_type == "isotherm":
+                exp_type = 'Isotherme'
+            elif isotherm.exp_type == "calorimetry":
+                exp_type = 'Calorimetrie'
+            else:
+                raise ParsingError("Unknown experiment type")
+
+        sht.range('A1').value = [
+            ["Type manip", exp_type],
+            ["Experience ou Simulation", is_real],
+            ["Date de l'expérience", isotherm.date],
+            ["Nom de l'échantillon", isotherm.sample_name],
+            ["Lot de l'échantillon", isotherm.sample_batch],
+            ["Température d'activation (°C)", isotherm.t_act],
+            ["Surnom de l'appareil", isotherm.machine],
+            ["Température de l'expérience (K)", isotherm.t_exp],
+            ["Formule chimique du gaz", isotherm.adsorbate],
+            ["Surnom du contact", isotherm.user],
+            ["Nom du labo", isotherm.lab],
+            ["Nom du projet", isotherm.project],
+        ]
+        sht.range('E1').value = 'Comments'
+        sht.range('E2').value = isotherm.comment
+
+        sht.range('E1').value = 'Properties'
+        rng_prop = 4
+        for index, prop in enumerate(isotherm.other_properties):
+            sht.range((5, rng_prop + index)).value = prop
+            sht.range((6, rng_prop + index)
+                      ).value = isotherm.other_properties.get(prop)
+
+        delimiter_colour = (217, 217, 217)
+        user_cells = (255, 199, 206)
+        xlwings.Range('A1:B2').column_width = 30
+        xlwings.Range('B1:B12').color = user_cells
+        xlwings.Range('A13:B13').color = delimiter_colour
+
+        # Write data
+        if fmt is None:
+            rng_data = 14
+        elif fmt == 'MADIREL':
+            if exp_type == "Isotherme":
+                rng_data = 30
+            elif exp_type == "Calorimetrie":
+                rng_data = 39
+            else:
+                raise ParsingError("Unknown experiment type")
+
+        headings = [
+            isotherm.loading_key,
+            isotherm.pressure_key,
+        ]
+        headings.extend(isotherm.other_keys)
+
+        # Gets the data sorted in the correct order
+        data = isotherm.data()[headings]
+
+        if fmt == 'MADIREL':
+            headings[0] = 'adsorbed' + '(' + isotherm.loading_unit + ')'
+            headings[1] = 'Pressure' + '(' + isotherm.pressure_unit + ')'
+        else:
+            headings[0] = headings[0] + '(' + isotherm.loading_unit + '/'\
+                                            + isotherm.adsorbent_unit + ')'
+            if isotherm.pressure_mode == 'absolute':
+                headings[1] = headings[1] + '(' + isotherm.pressure_unit + ')'
+            else:
+                headings[1] = headings[1] + '(p/p0)'
+
+        sht.range('A' + str(rng_data)).value = headings
+        sht.range('A' + str(rng_data + 1)).value = data.as_matrix()
+
+        # MADIREL specific
+        if fmt == 'MADIREL':
+            sht.range('A14').value = [
+                ["Constante d'Henry", ],
+                ["Langmuir N1", ],
+                ["Langmuir B1", ],
+                ["Langmuir N2", ],
+                ["Langmuir B2", ],
+                ["Langmuir N3", ],
+                ["Langmuir B3", ],
+                ["Langmuir R2", ],
+                ["C1", ],
+                ["C2", ],
+                ["C3", ],
+                ["C4", ],
+                ["C5", ],
+                ["C6", ],
+                ["C_m", ],
+            ]
+            xlwings.Range('A29:B29').color = delimiter_colour
+
+            if exp_type == "Calorimetrie":
+                sht.range('A30').value = [
+                    ["Enthalpie à zéro", ],
+                    ["Polynome Enthalpie A", ],
+                    ["Polynome Enthalpie B", ],
+                    ["Polynome Enthalpie C", ],
+                    ["Polynome Enthalpie D", ],
+                    ["Polynome Enthalpie E", ],
+                    ["Polynome Enthalpie F", ],
+                    ["Polynome Enthalpie R2", ],
+                ]
+                xlwings.Range('A38:C38').color = delimiter_colour
+            else:
+                raise ParsingError("Unknown data type")
+
+    finally:
+        wb.save(path=path)
+        wb.app.screen_updating = True
+        wb.app.quit()
+
+    return
+
+
+def isotherm_from_xl(path, fmt=None):
+    """
+    A function that will get the experiment and sample data from a excel parser
+    file and return the isotherm object.
+
+    Parameters
+    ----------
+    path : str
+        Path to the file to be read.
+    fmt : {None, 'MADIREL'}, optional
+        If the format is set to MADIREL, then the excel file is a specific version
+        used by the MADIREL lab for internal processing.
+
+    Returns
+    -------
+    PointIsotherm
+        The isotherm contained in the excel file.
+    """
+
+    if xlwings is None:
+        raise ParsingError(
+            "xlwings functionality disabled on this platform ( {0} )".format(os.name))
+
+    # Get excel workbook, sheet and range
+    wb = xlwings.Book(path)
+    wb.app.screen_updating = False
+    sht = wb.sheets[0]
+
+    try:
+        sample_info = {}
+
+        # read the isotherm parameters
+        exp_type = sht.range('B1').value
+        sample_info["exp_type"] = exp_type
+
+        if fmt == 'MADIREL':
+            if exp_type == "Isotherme":
+                sample_info["exp_type"] = 'isotherm'
+            elif exp_type == "Calorimetrie":
+                sample_info["exp_type"] = 'calorimetry'
+            else:
+                raise ParsingError("Unknown experiment type")
+
+        is_real = sht.range('B2').value
+
+        if is_real == "Experience":
+            sample_info['is_real'] = True
+        if is_real == "Simulation":
+            sample_info['is_real'] = False
+
+        sample_info['date'] = sht.range('B3').value
+        sample_info['sample_name'] = sht.range('B4').value
+        sample_info['sample_batch'] = sht.range('B5').value
+        sample_info['t_act'] = sht.range('B6').value
+        sample_info['machine'] = sht.range('B7').value
+        sample_info['t_exp'] = sht.range('B8').value
+        sample_info['adsorbate'] = sht.range('B9').value
+        sample_info['user'] = sht.range('B10').value
+        sample_info['lab'] = sht.range('B11').value
+        sample_info['project'] = sht.range('B12').value
+
+        sample_info['comment'] = sht.range('E2').value
+
+        rng_prop = 4
+        while True:
+            prop = sht.range((5, rng_prop)).value
+            if prop is None:
+                break
+            sample_info[prop] = sht.range((6, rng_prop)).value
+            rng_prop += 1
+
+        # read the data in
+
+        if fmt is None:
+            rng_data = 14
+        elif fmt == 'MADIREL':
+            if exp_type == "Isotherme":
+                rng_data = 30
+            elif exp_type == "Calorimetrie":
+                rng_data = 39
+            else:
+                raise ParsingError("Unknown data type")
+
+        experiment_data_df = sht.range('A' + str(rng_data)).options(
+            pandas.DataFrame, expand='table', index=0).value
+
+        loading_key = 'loading'
+        pressure_key = 'pressure'
+        s_loading_key = loading_key
+        s_pressure_key = pressure_key
+        if fmt == 'MADIREL':
+            s_loading_key = 'adsorbed'
+            s_pressure_key = 'pressure'
+        other_keys = []
+
+        for column in experiment_data_df.columns:
+            if s_loading_key in column.lower():
+
+                # Rename with standard name
+                experiment_data_df.rename(
+                    index=str, columns={column: loading_key}, inplace=True)
+
+                if not fmt:
+                    # Get units
+                    units = column[column.find(
+                        '(') + 1:column.rfind(')')].split('/')
+                    loading_basis = find_basis(units[0])
+                    adsorbent_basis = find_basis(units[1])
+                elif fmt == 'MADIREL':
+                    units = ['mmol', 'g']
+                    loading_basis = 'molar'
+                    adsorbent_basis = 'mass'
+
+            elif s_pressure_key in column.lower():
+
+                # Rename with standard name
+                experiment_data_df.rename(
+                    index=str, columns={column: pressure_key}, inplace=True)
+
+                if not fmt:
+                    # Get units
+                    pressure_unit = column[column.find(
+                        '(') + 1:column.rfind(')')]
+                    pressure_mode = find_mode(pressure_unit)
+                elif fmt == 'MADIREL':
+                    pressure_unit = 'bar'
+                    pressure_mode = 'absolute'
+
+            else:
+                other_keys.append(column)
+    finally:
+        wb.app.screen_updating = True
+        wb.app.quit()
+
+    isotherm = PointIsotherm(
+        experiment_data_df,
+        loading_key=loading_key,
+        pressure_key=pressure_key,
+        other_keys=other_keys,
+
+        pressure_unit=pressure_unit,
+        pressure_mode=pressure_mode,
+        loading_unit=units[0],
+        loading_basis=loading_basis,
+        adsorbent_unit=units[1],
+        adsorbent_basis=adsorbent_basis,
+
+        **sample_info)
+
+    return isotherm
