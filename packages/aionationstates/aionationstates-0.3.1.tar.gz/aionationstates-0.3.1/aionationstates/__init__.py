a39e9d5018de0b41ea6dd883e029aad43c977eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package aionationstates/0.3.1/aionationstates-0.3.1.tar.gz

---
+++ packages/aionationstates/aionationstates-0.3.1.tar.gz/aionationstates-0.3.1/aionationstates/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.3.1'
+
+from aionationstates.world_ import *
+from aionationstates.nation_ import *
+from aionationstates.region_ import *
+from aionationstates.shared import *
+from aionationstates.utils import *
+from aionationstates.session import *
+from aionationstates.ns_to_human import *
