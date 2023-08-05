adaa0924bb89f6253ee71258178e3001e36f6f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:10 +0000

    Add package pepperboard/2.3/pepperboard-2.3.tar.gz

---
+++ packages/pepperboard/pepperboard-2.3.tar.gz/pepperboard-2.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+setup(
+    name='pepperboard',
+    version='2.3',
+    packages=['pepperboard', 'pepperboard.core', 'pepperboard.dashboards'],
+    scripts=['scripts/pepperboard'],
+    url='https://github.com/webedia-dev/pepperboard',
+    download_url='https://github.com/webedia-dev/pepperboard/releases/tag/v2.3',
+    license='Apache',
+    author='Cyril LAVIER',
+    author_email='cyril.lavier@webedia-group.com',
+    description='Simple and modular dashboard toolkit for SaltStack',
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: System :: Monitoring',
+    ],
+)
