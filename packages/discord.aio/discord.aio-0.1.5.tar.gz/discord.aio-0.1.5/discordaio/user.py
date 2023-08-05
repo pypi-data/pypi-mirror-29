156eb12667af8081bdd57d9ee4545fac6f3af188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:54 +0000

    Add package discord.aio/0.1.5/discord.aio-0.1.5.tar.gz

---
+++ packages/discord.aio/discord.aio-0.1.5.tar.gz/discord.aio-0.1.5/discordaio/user.py
@@ -0,0 +1,44 @@
+"""Users in Discord are generally considered the base entity. Users can spawn across the entire platform,
+be members of guilds, participate in text and voice chat, and much more. Users are separated by a distinction of "bot" vs "normal." 
+Although they are similar, bot users are automated users that are "owned" by another user. Unlike normal users, 
+bot users do not have a limitation on the number of Guilds they can be a part of."""
+
+from .base import DiscordObject
+from .constants import DISCORD_CDN
+
+class UserConnection(DiscordObject):
+    """A Discord User Connection"""
+
+    def __init__(self, id="", name="", type="", revoked=False, integrations=[]):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.revoked = revoked
+        self.integrations = integrations
+
+
+class User(DiscordObject):
+    """A Discord User Object"""
+
+    def __init__(self, id=0, username="", discriminator="", avatar="", bot=False,
+                 mfa_enabled=False, verified=False, email=""):
+        self.id = id
+        self.username = username
+        self.discriminator = discriminator
+        self.avatar = avatar
+        self.bot = bot
+        self.mfa_enabled = mfa_enabled
+        self.verified = verified
+        self.email = email
+
+    def __str__(self):
+        return "{0}#{1}".format(self.username, self.discriminator)
+
+    def __repr__(self):
+        return "({0}#{1}, {2})".format(self.username, self.discriminator, self.id)
+    
+    def get_avatar_url(self):
+        return DISCORD_CDN + f'/avatars/{self.id}/{self.avatar}.png'
+
+    def get_default_avatar_url(self):
+        return DISCORD_CDN + f'/embed/avatars/{int(self.discriminator) % 5}.png'
