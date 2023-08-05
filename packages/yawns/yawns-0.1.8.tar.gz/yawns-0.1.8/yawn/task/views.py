6a42cc12303edab08eab52996c2c879e4d9428f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:26 +0000

    Add package yawns/0.1.8/yawns-0.1.8.tar.gz

---
+++ packages/yawns/yawns-0.1.8.tar.gz/yawns-0.1.8/yawn/task/views.py
@@ -0,0 +1,37 @@
+from rest_framework import viewsets
+
+from yawn.task.models import Task, Execution
+from yawn.task.serializers import TaskDetailSerializer, ExecutionListSerializer
+
+
+class TaskViewSet(viewsets.GenericViewSet,
+                  viewsets.mixins.RetrieveModelMixin,
+                  viewsets.mixins.UpdateModelMixin):
+    """
+    GET a task, and its executions. PATCH to re-run or terminate.
+    """
+    queryset = Task.objects.all().order_by('id').prefetch_related(
+        'execution_set__worker').select_related('template__workflow__name')
+
+    serializer_class = TaskDetailSerializer
+
+
+class ExecutionViewSet(viewsets.GenericViewSet,
+                       viewsets.mixins.ListModelMixin):
+    """
+    GET a list of Executions
+    """
+    queryset = Execution.objects.all().order_by('id').select_related(
+        'worker', 'task__template__workflow__name')
+
+    serializer_class = ExecutionListSerializer
+
+    def get_queryset(self):
+        """
+        Optionally filter to the executions for a given worker
+        """
+        queryset = self.queryset
+        worker = self.request.query_params.get('worker', None)
+        if worker is not None:
+            queryset = queryset.filter(worker_id=worker)
+        return queryset.order_by('-id')
