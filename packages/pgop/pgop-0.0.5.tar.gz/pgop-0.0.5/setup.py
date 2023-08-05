2c329afebd44684a5a601c424cabb0062e395c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:41 +0000

    Add package pgop/0.0.5/pgop-0.0.5.tar.gz

---
+++ packages/pgop/pgop-0.0.5.tar.gz/pgop-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pgop',
+    version='0.0.5',
+    packages=['pgop'],
+    install_requires=[
+        'requests',
+        'xmltodict',
+        'urllib3',
+    ],
+    url='https://github.com/madmanmatt/pgop',
+    license='MIT',
+    author='Matt Stang',
+    author_email='git@stang.im',
+    description='Interface module to control Greenwave Reality Lights'
+)
