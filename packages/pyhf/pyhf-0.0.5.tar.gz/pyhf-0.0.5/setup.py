bb2b451b40bec2c560c8549ee3847dd0867a2f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:40 +0000

    Add package pyhf/0.0.5/pyhf-0.0.5.tar.gz

---
+++ packages/pyhf/pyhf-0.0.5.tar.gz/pyhf-0.0.5/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'pyhf',
+  version = '0.0.5',
+  description = '(partial) pure python histfactory implementation',
+  url = '',
+  author = 'Lukas Heinrich',
+  author_email = 'lukas.heinrich@cern.ch',
+  packages = find_packages(),
+  include_package_data = True,
+  install_requires = [
+    'numpy',
+    'scipy'
+  ],
+  extras_require = {
+    'xmlimport': [
+       'uproot',
+     ],
+    'torch': [
+      'torch'
+    ],
+    'mxnet':[
+      'mxnet',
+    ],
+    'develop': [
+       'pyflakes',
+       'pytest>=3.2.0',
+       'pytest-cov>=2.5.1',
+       'python-coveralls',
+       'matplotlib',
+       'jupyter',
+       'uproot',
+       'papermill',
+       'torch',
+       'tensorflow',
+       'mxnet>=1.0.0',
+       'graphviz'
+    ]
+  },
+  entry_points = {
+  },
+  dependency_links = [
+  ]
+)
