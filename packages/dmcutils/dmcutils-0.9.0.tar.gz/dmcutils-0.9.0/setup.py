fc84b628ed86e3c362f062ef42aadb44854d2fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:05 +0000

    Add package dmcutils/0.9.0/dmcutils-0.9.0.tar.gz

---
+++ packages/dmcutils/dmcutils-0.9.0.tar.gz/dmcutils-0.9.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+install_requires=['python-dateutil','pytz','pandas','h5py','scikit-image',
+     'histutils','morecvutils']
+tests_require=['nose','coveralls']
+# %%
+from setuptools import setup,find_packages
+
+setup(name='dmcutils',
+      packages=find_packages(),
+      author='Michael Hirsch, Ph.D.',
+      url='https://github.com/scivision/dmcutils',
+      description='Utilities to read and plot DMC Experiment data',
+      version='0.9.0',
+	  install_requires=install_requires,
+	  tests_require=tests_require,
+	  python_requires='>=3.6',
+      extras_require={'web':['flask','flask-limiter'],
+                      'plot':['matplotlib'],
+                      'io':['astrometry_azel'],
+                      'tests':tests_require},
+	  )
+
