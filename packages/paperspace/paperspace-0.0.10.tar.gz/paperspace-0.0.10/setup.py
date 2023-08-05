676316319cd791a5a92c45b6e5abd9d0d4d2ff65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:22 +0000

    Add package paperspace/0.0.10/paperspace-0.0.10.tar.gz

---
+++ packages/paperspace/paperspace-0.0.10.tar.gz/paperspace-0.0.10/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='paperspace',
+    version='0.0.10',
+    description='Paperspace Python',
+    long_description=long_description,
+    url='https://github.com/paperspace/paperspace-python',
+    author='Paperspace Co.',
+    author_email='info@paperspace.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='paperspace api development library',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['requests[security]', 'boto3', 'botocore', 'six'],
+    entry_points={'console_scripts': [
+        'paperspace-python = paperspace.main:main',
+    ]},
+)
