67ddbb9904cee22338efe46c78e56c162c058350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:11 +0000

    Add package Orange3-Bioinformatics/3.0.4/Orange3-Bioinformatics-3.0.4.tar.gz

---
+++ packages/Orange3-Bioinformatics/Orange3-Bioinformatics-3.0.4.tar.gz/Orange3-Bioinformatics-3.0.4/orangecontrib/bioinformatics/geneset/config.py
@@ -0,0 +1,14 @@
+DOMAIN = 'gene_sets'
+
+# cytoband
+CYTOBAND_DOWNLOAD_LINK = 'http://statweb.stanford.edu/~tibs/GSA/cytobands-stanford.gmt'
+
+# Reactome
+REACTOME_DOWNLOAD_LINK = 'http://www.reactome.org/download/current/ReactomePathways.gmt.zip'
+REACTOME_FILE_NAME = 'ReactomePathways.gmt'
+
+# GO
+GO_TERM_LINK = 'http://amigo.geneontology.org/cgi-bin/amigo/term-details.cgi?term={}'
+
+# OMIM
+OMIM_LINK = 'http://www.omim.org/entry/{}'
---
+++ packages/Orange3-Bioinformatics/Orange3-Bioinformatics-3.0.4.tar.gz/Orange3-Bioinformatics-3.0.4/orangecontrib/bioinformatics/ncbi/gene/config.py
@@ -0,0 +1,42 @@
+DOMAIN = 'gene_info'
+FILENAME = 'gene_info.sqlite'
+TITLE = 'NCBI Gene Information'
+TAGS = ['NCBI', 'Gene names', 'Gene Ids', 'Description', 'Essential']
+FTP_FILENAME = 'gene_info.gz'
+FTP_URL = 'ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz'
+
+# dictyBase gene names
+DICTY_GENE_NAMES = \
+    'http://www.dictybase.org/db/cgi-bin/dictyBase/download/download.pl?area=general&ID=gene_information.txt'
+
+# NCBI_INFO - order is important
+GENE_INFO_TAGS = ['tax_id',
+                  'gene_id',
+                  'symbol',
+                  'synonyms',
+                  'db_refs',
+                  'description',
+                  'locus_tag',
+                  'chromosome',
+                  'map_location',
+                  'type_of_gene',
+                  'symbol_from_nomenclature_authority',
+                  'full_name_from_nomenclature_authority',
+                  'nomenclature_status',
+                  'other_designations',
+                  'modification_date']
+
+GENE_MATCHER_TAGS = ['input_name', 'type_of_match', 'ncbi_id', '_possible_hits']
+
+# PICKLED GENE MAPPER
+MATCHER_FILENAME = 'gene_mapper_{}'
+MATCHER_TITLE = 'Gene mapper'
+MATCHER_TAGS = ['source id', 'ncbi id', 'symbols', 'synonyms', 'genes', 'mapping']
+
+MATCHER_TUPLE_TAGS = ['tax_id', 'gene_id', 'symbol', 'synonyms', 'sources', 'locus_tag']
+MAP_GENE_IDS = 'gene_ids'
+MAP_SOURCES = 'sources'
+MAP_SYMBOLS = 'symbols'
+MAP_SYNONYMS = 'synonyms'
+MAP_LOCUS = 'locus_tag'
+
