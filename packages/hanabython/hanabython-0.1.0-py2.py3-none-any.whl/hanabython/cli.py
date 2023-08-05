8e4c7505c395f2ee6e933887a864ef0a842a4d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:54 +0000

    Add package hanabython/0.1.0/hanabython-0.1.0-py2.py3-none-any.whl

---
+++ packages/hanabython/hanabython-0.1.0-py2.py3-none-any.whl/hanabython/cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for hanabython."""
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for hanabython."""
+    click.echo("Replace this message by putting your code into "
+               "hanabython.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
