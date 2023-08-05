fbb17199de0ee1ba6e2934ad3ad83955176afee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:57 +0000

    Add package apistar-shell/1.1/apistar_shell-1.1.tar.gz

---
+++ packages/apistar-shell/apistar_shell-1.1.tar.gz/apistar_shell-1.1/README.md
@@ -0,0 +1,26 @@
+# API Star Shell
+[API Star](https://github.com/encode/apistar) Shell inspired by Django
+
+[![PyPI version](https://badge.fury.io/py/apistar-shell.svg)](https://badge.fury.io/py/apistar-shell)
+
+<img src="docs/usage.png" width="700px" />
+
+
+## Installation
+
+```
+$ pip install apistar-shell
+```
+
+## Setup & Configuration
+
+- [Django Setup](docs/DJANGO_SETUP.md)
+- [SQLAlchemy Setup](docs/SQLALCHEMY_SETUP.md)
+- [Common Setup](docs/COMMON_SETUP.md)
+- [Configuration](docs/CONFIGURATION.md)
+
+## Usage:
+
+```
+$ apistar shell
+```
---
+++ packages/apistar-shell/apistar_shell-1.1.tar.gz/apistar_shell-1.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='apistar_shell',
+    version='1.1',
+    packages=['apistar_shell'],
+    url='https://github.com/bahattincinic/apistar_shell',
+    license='MIT',
+    description='API Star Shell inspired from Django',
+    author='Bahattin Cinic',
+    author_email='bahattincinic@gmail.com',
+    install_requires=['apistar', 'ipython'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
