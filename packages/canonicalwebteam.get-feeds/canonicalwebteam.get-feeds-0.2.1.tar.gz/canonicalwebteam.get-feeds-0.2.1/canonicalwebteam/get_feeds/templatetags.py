15fda3bd7cd5ff3d7b1cde93320f686357d3b1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package canonicalwebteam.get-feeds/0.2.1/canonicalwebteam.get-feeds-0.2.1.tar.gz

---
+++ packages/canonicalwebteam.get-feeds/canonicalwebteam.get-feeds-0.2.1.tar.gz/canonicalwebteam.get-feeds-0.2.1/canonicalwebteam/get_feeds/templatetags.py
@@ -0,0 +1,17 @@
+from django import template
+from . import (
+    get_json_feed_content,
+    get_rss_feed_content
+)
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_json_feed(feed_url, **kwargs):
+    return get_json_feed_content(feed_url, **kwargs)
+
+
+@register.simple_tag
+def get_rss_feed(feed_url, **kwargs):
+    return get_rss_feed_content(feed_url, **kwargs)
