c7216812ae5c28afca850226e5e90470b8dc0348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:16 +0000

    Add package http-server-base/1.0.1/http-server-base-1.0.1.tar.gz

---
+++ packages/http-server-base/http-server-base-1.0.1.tar.gz/http-server-base-1.0.1/src/http_server_base/tools/types.py
@@ -0,0 +1,5 @@
+import re
+from typing import Union, Tuple, Type, Any, Dict, List, TypeVar
+
+re_type = type(re.compile(r'some_regular'))
+JsonSerializable = Union[str, int, float, Dict[str, 'JsonSerializable'], List['JsonSerializable']]
