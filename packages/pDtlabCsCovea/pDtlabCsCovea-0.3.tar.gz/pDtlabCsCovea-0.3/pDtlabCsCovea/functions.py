eb1f34d5ca44a5bc22d0520fa6f3ff673369a54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:59 +0000

    Add package pDtlabCsCovea/0.3/pDtlabCsCovea-0.3.tar.gz

---
+++ packages/pDtlabCsCovea/pDtlabCsCovea-0.3.tar.gz/pDtlabCsCovea-0.3/pDtlabCsCovea/functions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2.7
+#-*- coding: utf-8 -*-
+
+from datetime import datetime
+import git  
+import re
+import os
+
+from git import *
+import gitdb
+from gitdb import *
+
+# Cette fonction affiche un message à l'écran 
+def print_coucou():
+    print( "Bonjour le monde !" )
+
+# Cette fonction affiche la date et l'heure  
+def la_date():
+    print( "{}".format( datetime.now() ))
