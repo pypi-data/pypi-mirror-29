1d1a1270e44cf9ba0b01125ae6c4432a8411b9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:43 +0000

    Add package saram/0.8.2/saram-0.8.2.tar.gz

---
+++ packages/saram/saram-0.8.2.tar.gz/saram-0.8.2/setup.py
@@ -0,0 +1,28 @@
+#from distutils.core import setup
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name = 'saram',
+    packages = ['saram'], # this must be the same as the name above
+    version = '0.8.2',
+    description = 'A library to fetch images from a directory and fetch OCR from it with orientation rotation and pdf support',
+    long_description = readme(),
+    author = 'Sunim Acharya',
+    author_email = 'sunim.54@gmail.com',
+    url = 'https://github.com/aryaminus/saram', # use the URL to the github repo
+    keywords = ['ocr', 'image', 'pdf'], # arbitrary keywords
+    classifiers = [],
+    install_requires=[
+        'setuptools', 'pillow', 'pyocr', 'wand' ,'tesseract', 'tesseract-data-eng'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+    'console_scripts': [
+        'saram = saram.saram:start'
+    ]},
+)
