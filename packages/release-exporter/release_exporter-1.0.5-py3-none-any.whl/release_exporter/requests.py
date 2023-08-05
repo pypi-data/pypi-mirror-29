077aa2bdcbb9e5256d1355176a790373dd7fc820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:47 +0000

    Add package release-exporter/1.0.5/release_exporter-1.0.5-py3-none-any.whl

---
+++ packages/release-exporter/release_exporter-1.0.5-py3-none-any.whl/release_exporter/requests.py
@@ -0,0 +1,157 @@
+import json
+
+import requests
+
+from release_exporter.base import FormatBase
+from release_exporter.exceptions import InvalidToken
+from release_exporter.utils import get_repo_url_info, multi_key_gitlab
+
+
+class GitHubRequest(FormatBase):
+    """
+    GitHub request base.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(GitHubRequest, self).__init__(*args, **kwargs)
+        self.request_headers = {'Authorization': 'token %s' % self.token}
+        self.api_url = 'https://api.github.com/graphql'
+
+        if self.token is None:
+            raise InvalidToken(
+                "Oops! GitHub requires you to generate a private token to get the details. See "
+                "https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/ "
+                "for more information.")
+
+        if self.repo_url is not None:
+            self.info = get_repo_url_info(self.location, repo_url=self.repo_url)
+        else:
+            self.info = get_repo_url_info(self.location)
+
+    def _total_number_releases(self):
+        """
+        Queries total number of releases.
+
+        Returns
+        -------
+        int: int
+            Number of releases.
+        """
+        _json = {"query": """
+            {
+              repository(owner: """ + """\"{}\",""".format(self.info.owner) + """ name: """ + """\"{}\")""".format(
+            self.info.name) + """ {
+                releases {
+                  totalCount
+                }
+              }
+            }
+        """}
+
+        r = requests.post(url=self.api_url, json=_json, headers=self.request_headers)
+        try:
+            return int(json.loads(r.text)['data']['repository']['releases']['totalCount'])
+        except KeyError:
+            raise KeyError('Wrong credentials given. Please check if you have the correct token.')
+
+    def releases(self):
+        """
+        A JSON object with name of the repository, tag name, description and the created date and time.
+
+        Returns
+        -------
+        JSON: dict
+            A JSON object.
+        """
+        _json = {
+            "query": """
+                query {""" +
+                     """repository(owner: \"{}\", name: \"{}\") """.format(self.info.owner, self.info.name) + """{
+                    releases(""" + """first:{}""".format(self._total_number_releases()) + """, orderBy: {field: CREATED_AT, direction: DESC}){
+                      edges{
+                        node{
+                          name
+                          tag{
+                            name
+                          }
+                          description
+                          createdAt
+                        }
+                      }
+                    }
+                  }
+                }
+            """
+        }
+
+        r = requests.post(url=self.api_url, json=_json, headers=self.request_headers)
+        return json.loads(r.text)
+
+
+class GitLabRequest(FormatBase):
+    """
+    GitLab request base.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(GitLabRequest, self).__init__(*args, **kwargs)
+        self.request_headers = {'Private-Token': '%s' % self.token}
+        self.api_url = 'https://gitlab.com/api/v4/'
+
+        if self.token is None:
+            raise InvalidToken(
+                "Oops! GitLab requires you to generate a private token to get the details. See "
+                "https://docs.gitlab.com/ce/user/profile/personal_access_tokens.html "
+                "for more information.")
+
+        if self.repo_url is not None:
+            self.info = get_repo_url_info(self.location, repo_url=self.repo_url)
+        else:
+            self.info = get_repo_url_info(self.location)
+
+    def _repo_id(self):
+        """
+        Searches and returns the repository ID based on the repository name. If the repository is not found then a table
+        of repository is shown so that the user can manually enter the ID of their repository.
+
+        Returns
+        -------
+        id_number: int
+            Repository ID.
+
+        """
+        url = self.api_url + 'projects?search={}'.format(self.info.name)
+
+        r = requests.get(url=url, headers=self.request_headers)
+        id_number = json.loads(r.text)
+
+        if len(id_number) > 1:
+            print(
+                "The search resulted in more that one repository. Please check your repository name and type in it's ID")
+            print('ID - Repository Name - Username')
+
+            for content in id_number:
+                print('{id} - {repo_name} - {user_name}'.format(id=content['id'], repo_name=content['name'],
+                                                                user_name=multi_key_gitlab(content)))
+
+            id_number = input('ID > ')
+            return id_number
+
+        try:
+            return id_number[0]['id']
+        except KeyError:
+            raise KeyError('Wrong credentials given. Please check if you have the correct token.')
+
+    def releases(self):
+        """
+        A JSON object containing name of the repository, tag name, description and the created date and time
+
+        Returns
+        -------
+        JSON: dict
+            A dict object.
+        """
+        url = 'https://gitlab.com/api/v4/projects/{id}/repository/tags'.format(id=self._repo_id())
+
+        r = requests.get(url=url, headers=self.request_headers)
+        return json.loads(r.text)
---
+++ packages/release-exporter/release_exporter-1.0.5-py3-none-any.whl/release_exporter/utils.py
@@ -0,0 +1,112 @@
+import configparser
+import datetime
+import os
+
+import dateutil.parser
+from giturlparse import parse
+from tabulate import tabulate
+
+from .exceptions import ParserError
+
+
+def get_repo_url_info(location=os.getcwd(), repo_url=None):
+    """
+    Returns the parsed URL.
+
+    Parameters
+    ----------
+    location: str
+        Absolute location of the current directory.
+    repo_url: str
+        URL of the repository.
+
+    Returns
+    -------
+    parse: giturlparse.parser.Parsed
+        A named tuple.
+
+    """
+    try:
+        if repo_url is None:
+            config = configparser.ConfigParser()
+            config.read(location + os.sep + '.git' + os.sep + 'config')
+            if 'remote "origin"' in config.sections():
+                return parse(config['remote "origin"']['url'])
+            else:
+                raise ParserError('Git config file does not exist please provide the repository url by using --url.')
+        else:
+            return parse(repo_url + '.git')
+    except configparser.DuplicateSectionError:
+        raise configparser.DuplicateSectionError(
+            'There seems to be a duplicate section in your config. Try giving the repository URL by using --url.')
+
+
+def date_convert(date):
+    """
+    Converts ISO8601 date and time and returns only the date.
+
+    Parameters
+    ----------
+    date: str
+        datetime string.
+
+    Returns
+    -------
+    date: str
+        Date as Y-m-d format..
+
+    """
+    try:
+        date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%Sz')
+        date = date.strftime('%Y-%m-%d')
+    except ValueError:
+        date = dateutil.parser.parse(date).date().strftime('%Y-%m-%d')
+    return date
+
+
+def multi_key_gitlab(value):
+    """
+    Returns the username, if an exception occurs None is returned.
+
+    Parameters
+    ----------
+    value: dict
+        A dictionary of GitLab.
+
+    Returns
+    -------
+    value: str or None
+        Username or none.
+    """
+
+    try:
+        return value['owner']['username']
+    except (KeyError, TypeError):
+        return None
+
+
+def description(provider=None, repo_name=None, tags_number=None):
+    """
+    Description generator.
+
+    Parameters
+    ----------
+    provider: str
+        Name of the Git host.
+    repo_name: str
+        Repository name.
+    tags_number: str or int
+        Number of tags.
+    
+    Return
+    ------
+    tabulate: str
+        A tabulated structure of the input.
+    """
+    table = [
+        ['Provider', provider],
+        ['Repository Name', repo_name],
+        ['Number of Tags', tags_number]
+    ]
+
+    return tabulate(table, tablefmt="grid")
