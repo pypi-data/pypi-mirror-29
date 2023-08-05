b71d0246de4865473e88820d7995bd9d02d167d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:57 +0000

    Add package stripe/1.78.0/stripe-1.78.0.tar.gz

---
+++ packages/stripe/stripe-1.78.0.tar.gz/stripe-1.78.0/setup.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import warnings
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+path, script = os.path.split(sys.argv[0])
+os.chdir(os.path.abspath(path))
+
+install_requires = []
+
+if sys.version_info < (2, 6):
+    warnings.warn(
+        'Python 2.5 is no longer officially supported by Stripe. '
+        'If you have any questions, please file an issue on Github or '
+        'contact us at support@stripe.com.',
+        DeprecationWarning)
+    install_requires.append('requests >= 0.8.8, < 0.10.1')
+    install_requires.append('ssl')
+else:
+    install_requires.append('requests >= 0.8.8')
+
+
+with open('LONG_DESCRIPTION.rst') as f:
+    long_description = f.read()
+
+# Don't import stripe module here, since deps may not be installed
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'stripe'))
+from version import VERSION
+
+# Get simplejson if we don't already have json
+if sys.version_info < (3, 0):
+    try:
+        from util import json
+    except ImportError:
+        install_requires.append('simplejson')
+
+
+setup(
+    name='stripe',
+    version=VERSION,
+    description='Stripe python bindings',
+    long_description=long_description,
+    author='Stripe',
+    author_email='support@stripe.com',
+    url='https://github.com/stripe/stripe-python',
+    packages=['stripe', 'stripe.api_resources',
+              'stripe.api_resources.abstract'],
+    package_data={'stripe': ['data/ca-certificates.crt']},
+    install_requires=install_requires,
+    test_suite='tests',
+    tests_require=['unittest2', 'mock'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ])
