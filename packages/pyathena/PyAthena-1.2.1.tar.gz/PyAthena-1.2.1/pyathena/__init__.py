448f67c686e0b9774cd2e71a6b51bf34580a6d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package pyathena/1.2.1/PyAthena-1.2.1.tar.gz

---
+++ packages/pyathena/PyAthena-1.2.1.tar.gz/PyAthena-1.2.1/pyathena/__init__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import datetime
+
+from pyathena.error import *  # noqa
+
+
+__version__ = '1.2.1'
+
+
+# Globals https://www.python.org/dev/peps/pep-0249/#globals
+apilevel = '2.0'
+threadsafety = 3
+paramstyle = 'pyformat'
+
+
+class DBAPITypeObject:
+    """Type Objects and Constructors
+
+    https://www.python.org/dev/peps/pep-0249/#type-objects-and-constructors
+    """
+    def __init__(self, *values):
+        self.values = values
+
+    def __cmp__(self, other):
+        if other in self.values:
+            return 0
+        if other < self.values:
+            return 1
+        else:
+            return -1
+
+
+STRING = DBAPITypeObject('CHAR', 'NCHAR',
+                         'VARCHAR', 'NVARCHAR',
+                         'LONGVARCHAR', 'LONGNVARCHAR')
+BINARY = DBAPITypeObject('BINARY', 'VARBINARY', 'LONGVARBINARY')
+NUMBER = DBAPITypeObject('BOOLEAN', 'TINYINT', 'SMALLINT', 'BIGINT', 'INTEGER',
+                         'REAL', 'DOUBLE', 'FLOAT', 'DECIMAL', 'NUMERIC')
+DATETIME = DBAPITypeObject('TIMESTAMP')
+ROWID = DBAPITypeObject('')
+
+
+Date = datetime.date
+Time = datetime.time
+Timestamp = datetime.datetime
+
+
+def connect(*args, **kwargs):
+    from pyathena.connection import Connection
+    return Connection(*args, **kwargs)
