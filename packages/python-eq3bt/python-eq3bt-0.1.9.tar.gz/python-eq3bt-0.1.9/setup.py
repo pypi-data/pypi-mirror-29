4a7a40e901569c687c31630350584a7ba1d4f8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:24 +0000

    Add package python-eq3bt/0.1.9/python-eq3bt-0.1.9.tar.gz

---
+++ packages/python-eq3bt/python-eq3bt-0.1.9.tar.gz/python-eq3bt-0.1.9/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='python-eq3bt',
+    version='0.1.9',
+    packages=['eq3bt'],
+    install_requires=['bluepy>=1.0.5', 'click', 'construct', 'click-datetime'],
+    description='EQ3 bluetooth thermostat support library',
+    author='Markus Peter',
+    author_email='mpeter@emdev.de',
+    maintainer='Teemu Rytilahti',
+    maintainer_email='tpr@iki.fi',
+    url='https://github.com/rytilahti/python-eq3bt.git',
+    license="MIT",
+    entry_points={
+        'console_scripts': [
+            'eq3cli = eq3bt.eq3cli:cli'
+        ]
+    }
+)
