8a93259b4ab3bf06fe3ebbd7c5e0770bab1c8279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:06 +0000

    Add package vladiate/0.0.20/vladiate-0.0.20-py3-none-any.whl

---
+++ packages/vladiate/vladiate-0.0.20-py3-none-any.whl/vladiate/test/test_validators.py
@@ -0,0 +1,218 @@
+import pytest
+from pretend import stub, call, call_recorder
+
+from ..validators import (
+    CastValidator, EmptyValidator, FloatValidator, Ignore, IntValidator,
+    NotEmptyValidator, RangeValidator, RegexValidator, SetValidator,
+    UniqueValidator, Validator
+)
+from ..exceptions import BadValidatorException, ValidationException
+
+
+class FakeRow(object):
+    ''' A row with side effects '''
+
+    def __init__(self, fields):
+        self.fields = fields
+
+    def __getitem__(self, key):
+        return self.fields[key].pop(0)
+
+    def keys(self):
+        return self.fields.keys()
+
+
+def test_cast_validator():
+
+    def _raise_valueerror(x):
+        raise ValueError
+
+    validator = CastValidator()
+    validator.cast = call_recorder(_raise_valueerror)
+
+    assert validator.bad == set()
+
+    with pytest.raises(ValidationException):
+        validator.validate('field', 'something')
+
+    assert validator.bad == {'field'}
+
+    assert validator.cast.calls == [
+        call('field')
+    ]
+
+
+@pytest.mark.parametrize('field', [('42'), ('42.0'), ('-42.0'), ])
+def test_float_validator_works(field):
+    FloatValidator().validate(field)
+
+
+@pytest.mark.parametrize('field', [('foo'), (' '), ('7..0'), ('4,200'), ])
+def test_float_validator_fails(field):
+    with pytest.raises(ValidationException):
+        FloatValidator().validate(field)
+
+
+@pytest.mark.parametrize('field', [('42'), ])
+def test_int_validator_works(field):
+    IntValidator().validate(field)
+
+
+@pytest.mark.parametrize('field', [
+    ('foo'),
+    (' '),
+    ('7..0'),
+    ('4,200'),
+    ('42.0'),
+    ('-42.0'),
+])
+def test_int_validator_fails(field):
+    with pytest.raises(ValidationException):
+        IntValidator().validate(field)
+
+
+@pytest.mark.parametrize('field_set, field', [
+    (['foo'], 'foo'),
+    (['foo', 'bar'], 'foo'),
+])
+def test_set_validator_works(field_set, field):
+    validator = SetValidator(field_set)
+    validator.validate(field)
+    assert field in validator.valid_set
+
+
+@pytest.mark.parametrize('field_set, field', [
+    ([], 'bar'),
+    (['foo'], 'bar'),
+    (['foo', 'bar'], 'baz'),
+])
+def test_set_validator_fails(field_set, field):
+    validator = SetValidator(field_set)
+    with pytest.raises(ValidationException):
+        validator.validate(field)
+
+    assert validator.bad == {field}
+
+
+@pytest.mark.parametrize('fields, row, unique_with', [
+    ([], {}, []),
+    (['foo', 'bar'], {}, []),
+    (['foo', 'foo'], FakeRow({'some_field': ['bar', 'baz']}), ['some_field']),
+    (['foo', 'foo', 'foo'], FakeRow({
+        'some_field': ['bar', 'baz', 'bar'],
+        'some_other_field': ['baz', 'bar', 'bar']
+    }), ['some_field', 'some_other_field']),
+])
+def test_unique_validator_works(fields, row, unique_with):
+    validator = UniqueValidator(unique_with=unique_with)
+    for field in fields:
+        validator.validate(field, row)
+
+
+@pytest.mark.parametrize('fields, row, unique_with, exception, bad', [
+    (['foo', 'bar', 'bar'], {}, [], ValidationException, {('bar',)}),
+    (['foo', 'foo'], FakeRow({'some_field': ['bar', 'bar']}), ['some_field'],
+        ValidationException, {('foo', 'bar')}),
+    (['foo', 'foo'], FakeRow({'some_field': ['bar', 'bar']}), ['other_field'],
+        BadValidatorException, set()),
+])
+def test_unique_validator_fails(fields, row, unique_with, exception, bad):
+    validator = UniqueValidator(unique_with=unique_with)
+    with pytest.raises(exception):
+        for field in fields:
+            validator.validate(field, row)
+
+    assert validator.bad == bad
+
+
+@pytest.mark.parametrize('pattern, field', [
+    (r'foo.*', 'foo'),
+    (r'foo.*', 'foobar'),
+])
+def test_regex_validator_works(pattern, field):
+    RegexValidator(pattern).validate(field)
+
+
+@pytest.mark.parametrize('pattern, field', [
+    (r'foo.*', 'afoo'),
+    (r'^$', 'foo'),
+])
+def test_regex_validator_fails(pattern, field):
+    validator = RegexValidator(pattern)
+    with pytest.raises(ValidationException):
+        validator.validate(field)
+
+    assert validator.bad == {field}
+
+
+def test_range_validator_works():
+    RangeValidator(0, 100).validate("42")
+
+
+def test_range_validator_fails():
+    validator = RangeValidator(0, 100)
+    with pytest.raises(ValidationException):
+        validator.validate("-42")
+
+    assert validator.bad == {'-42'}
+
+
+def test_range_validator_handles_bad_values():
+    validator = RangeValidator(0, 100)
+    with pytest.raises(ValidationException):
+        validator.validate("foobar")
+
+    assert validator.bad == {'foobar'}
+
+
+def test_empty_validator_works():
+    EmptyValidator().validate("")
+
+
+def test_empty_validator_fails():
+    validator = EmptyValidator()
+    with pytest.raises(ValidationException):
+        validator.validate("foo")
+
+    assert validator.bad == {'foo'}
+
+
+def test_non_empty_validator_works():
+    NotEmptyValidator().validate("foo")
+
+
+def test_non_empty_validator_fails():
+    validator = NotEmptyValidator()
+    with pytest.raises(ValidationException):
+        validator.validate("")
+
+    assert validator.bad is True
+
+
+def test_ignore_validator():
+    validator = Ignore()
+    validator.validate("foo")
+    assert validator.bad is None
+
+
+def test_base_class_raises():
+    validator = Validator()
+
+    with pytest.raises(NotImplementedError):
+        validator.bad()
+
+    with pytest.raises(NotImplementedError):
+        validator.validate(stub(), stub())
+
+
+@pytest.mark.parametrize('validator_class,args', [
+    (SetValidator, [['foo']]),
+    (RegexValidator, [r'foo.*']),
+    (IntValidator, []),
+    (FloatValidator, []),
+    (RangeValidator, [0, 42]),
+    (UniqueValidator, []),
+])
+def test_all_validators_support_empty_ok(validator_class, args):
+    validator = validator_class(*args, empty_ok=True)
+    validator.validate('')
---
+++ packages/vladiate/vladiate-0.0.20-py3-none-any.whl/vladiate/test/test_vlads.py
@@ -0,0 +1,165 @@
+import pytest
+
+from ..vlad import Vlad
+from ..inputs import LocalFile, String
+from ..validators import (
+    EmptyValidator, FloatValidator, NotEmptyValidator, SetValidator,
+    UniqueValidator,
+)
+
+
+def test_initialize_vlad():
+    source = LocalFile('vladiate/examples/vampires.csv')
+    validators = {
+        'Column A': [
+            UniqueValidator()
+        ],
+        'Column B': [
+            SetValidator(['Vampire', 'Not A Vampire'])
+        ]
+    }
+    assert Vlad(source=source, validators=validators).validate()
+
+
+def test_initialize_vlad_with_alternative_delimiter():
+    source = LocalFile('vladiate/examples/bats.csv')
+    validators = {
+        'Column A': [
+            UniqueValidator()
+        ],
+        'Column B': [
+            SetValidator(['Vampire', 'Not A Vampire'])
+        ]
+    }
+    delimiter = '|'
+    vlad = Vlad(source=source, validators=validators, delimiter=delimiter)
+    assert vlad.validate()
+
+
+def test_initialize_vlad_no_source():
+    with pytest.raises(TypeError):
+        Vlad().validate()
+
+
+def test_unused_validator_fails_validation():
+    source = LocalFile('vladiate/examples/vampires.csv')
+    validators = {
+        'Column A': [
+            UniqueValidator()
+        ],
+        'Column B': [
+            SetValidator(['Vampire', 'Not A Vampire'])
+        ],
+        'Column C': [
+            FloatValidator()
+        ]
+    }
+
+    assert not Vlad(source=source, validators=validators).validate()
+
+
+def test_validators_in_class_variable_are_used():
+    source = LocalFile('vladiate/examples/vampires.csv')
+
+    class TestVlad(Vlad):
+        validators = {
+            'Column A': [
+                UniqueValidator()
+            ],
+            'Column B': [
+                SetValidator(['Vampire', 'Not A Vampire'])
+            ]
+        }
+
+    assert TestVlad(source=source).validate()
+
+
+def test_missing_validators():
+    source = LocalFile('vladiate/examples/vampires.csv')
+
+    class TestVlad(Vlad):
+        validators = {
+            'Column A': [
+                UniqueValidator()
+            ],
+        }
+
+    assert not TestVlad(source=source).validate()
+
+
+def test_no_fieldnames():
+    source = String('')
+
+    class TestVlad(Vlad):
+        validators = {
+            'Foo': [],
+            'Bar': [],
+        }
+
+    assert not TestVlad(source=source).validate()
+
+
+def test_fails_validation():
+    source = LocalFile('vladiate/examples/vampires.csv')
+
+    class TestVlad(Vlad):
+        validators = {
+            'Column A': [
+                EmptyValidator()
+            ],
+            'Column B': [
+                EmptyValidator()
+            ]
+        }
+
+    vlad = TestVlad(source=source)
+    assert not vlad.validate()
+    assert vlad.validators['Column A'][0].fail_count == 3
+    assert vlad.validators['Column B'][0].fail_count == 3
+
+
+def test_gt_99_failures():
+    source = String('\n'.join(['Foo'] + [str(x) for x in range(100)]))
+
+    class TestVlad(Vlad):
+        validators = {
+            'Foo': [
+                EmptyValidator()
+            ]
+        }
+
+    assert not TestVlad(source=source).validate()
+
+
+def test_ignore_missing_validators():
+    source = LocalFile('vladiate/examples/vampires.csv')
+
+    class TestVlad(Vlad):
+        validators = {
+            'Column A': [
+                UniqueValidator()
+            ],
+        }
+
+    vlad = TestVlad(source=source, ignore_missing_validators=True)
+
+    assert vlad.validate()
+    assert vlad.missing_validators == {'Column B'}
+
+
+def test_when_bad_is_non_iterable():
+    source = String('Column A,Column B\n,foo')
+
+    class TestVlad(Vlad):
+        validators = {
+            'Column A': [NotEmptyValidator()],
+            'Column B': [NotEmptyValidator()],
+        }
+
+    vlad = TestVlad(source=source)
+
+    assert not vlad.validate()
+    assert vlad.validators['Column A'][0].fail_count == 1
+    assert vlad.validators['Column A'][0].bad
+    assert vlad.validators['Column B'][0].fail_count == 0
+    assert not vlad.validators['Column B'][0].bad
