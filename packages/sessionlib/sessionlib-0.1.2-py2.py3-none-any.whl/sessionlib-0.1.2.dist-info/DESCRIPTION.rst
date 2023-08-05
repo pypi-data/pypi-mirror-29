36fccd6b81f5372f85ec04accef7014a9f811d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:55 +0000

    Add package sessionlib/0.1.2/sessionlib-0.1.2-py2.py3-none-any.whl

---
+++ packages/sessionlib/sessionlib-0.1.2-py2.py3-none-any.whl/sessionlib-0.1.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,57 @@
+==========
+sessionlib
+==========
+
+
+.. image:: https://img.shields.io/pypi/v/sessionlib.svg
+        :target: https://pypi.python.org/pypi/sessionlib
+
+.. image:: https://img.shields.io/travis/pauloromeira/sessionlib.svg
+        :target: https://travis-ci.org/pauloromeira/sessionlib
+
+.. image:: https://readthedocs.org/projects/sessionlib/badge/?version=latest
+        :target: https://sessionlib.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+Session Library
+
+
+* Free software: MIT license
+* Documentation: https://sessionlib.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
+
+
+=======
+History
+=======
+
+0.1.0 (2017-12-04)
+------------------
+
+* First release on PyPI.
+
+0.1.1 (2017-12-05)
+------------------
+
+* Second release on PyPI.
+
+0.1.2 (2018-02-22)
+------------------
+
+* Third release on PyPI.
+
+
---
+++ packages/sessionlib/sessionlib-0.1.2-py2.py3-none-any.whl/sessionlib/sessionlib.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from contextlib import ExitStack
+from .utils import Observable
+
+logger = logging.getLogger(__name__)
+
+
+class Session(object):
+    _sessions = []
+
+    @classmethod
+    def current(cls):
+        return cls._sessions[-1] if cls._sessions else None
+
+    @classmethod
+    def _push(cls, session):
+        cls._sessions.append(session)
+
+    @classmethod
+    def _pop(cls):
+        cls._sessions.pop()
+
+    def __init__(self, *contextmanagers):
+        self._on_start = Observable(self)
+        self._on_enter = Observable(self)
+        self._on_leave = Observable(self)
+        self._on_close = Observable(self)
+
+        self._exit_stack = ExitStack()
+        self._started = False
+        self._contexts = contextmanagers
+
+    @property
+    def on_start(self):
+        return self._on_start
+
+    @property
+    def on_enter(self):
+        return self._on_enter
+
+    @property
+    def on_leave(self):
+        return self._on_leave
+
+    @property
+    def on_close(self):
+        return self._on_close
+
+    def contexts(self):
+        yield from self._contexts
+
+    @property
+    def started(self):
+        return self._started
+
+    def __enter__(self):
+        self.__class__._push(self)
+        if self._started:
+            self.on_enter()
+            logger.info('{} session entered'.format(self))
+            return
+
+        self.on_start()
+
+        for context in self.contexts():
+            self._exit_stack.enter_context(context)
+
+        self._started = True
+        logger.info('{} session started'.format(self))
+
+        return self
+
+    def __exit__(self, *exc_info):
+        self.__class__._pop()
+        if self in self.__class__._sessions:
+            self.on_leave()
+            logger.info('{} session left'.format(self))
+            return
+
+        try:
+            self.on_close()
+        finally:
+            self._exit_stack.close()
+
+        logger.info('{} session closed'.format(self))
+
+
+def sessionaware(func):
+    def func_wrapper(*args, **kwargs):
+        current_session = Session.current()
+
+        if args and isinstance(args[0], Session):
+            session = args[0]
+            if session is current_session:
+                return func(*args, **kwargs)
+            else:
+                with session:
+                    return func(*args, **kwargs)
+        else:
+            return func(current_session, *args, **kwargs)
+
+    return func_wrapper
