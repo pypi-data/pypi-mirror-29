1db9698a7894920b5d6d3e363217e2d943da2f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:03 +0000

    Add package routemaster/1.0.10/routemaster-1.0.10.tar.gz

---
+++ packages/routemaster/routemaster-1.0.10.tar.gz/routemaster-1.0.10/setup.py
@@ -0,0 +1,81 @@
+"""Package setup."""
+
+from setuptools import setup, find_packages
+
+import routemaster.version
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+try:
+    from m2r import convert
+    long_description = convert(long_description)
+except ImportError:
+    # Fall back to markdown formatted readme when no m2r package.
+    pass
+
+
+setup(
+    name='routemaster',
+    version=routemaster.version.get_version(),
+    url='https://github.com/thread/routemaster',
+    description="State machines as a service.",
+    long_description=long_description,
+
+    author="Thread",
+    author_email="tech@thread.com",
+
+    keywords=(
+    ),
+    license='MIT',
+
+    zip_safe=False,
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Office/Business',
+    ),
+
+    install_requires=(
+        'click',
+        'pyyaml',
+        'jsonschema >=2.6',
+        'flask',
+        'psycopg2',
+        'sqlalchemy',
+        'python-dateutil',
+        'alembic >=0.9.6',
+        'gunicorn >=19.7',
+        'schedule',
+        'freezegun',
+        'requests',
+        'networkx',
+        'dataclasses',
+    ),
+
+    setup_requires=(
+        'pytest-runner',
+    ),
+
+    tests_require=(
+        'pytest',
+        'networkx',
+        'tox',
+        'pytest-cov',
+    ),
+
+    entry_points={
+        'console_scripts': (
+            'routemaster = routemaster.cli:main',
+        ),
+    },
+)
