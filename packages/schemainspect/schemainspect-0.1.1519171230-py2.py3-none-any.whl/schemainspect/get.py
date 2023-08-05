667f2d82e2ae02c1745274b6ac1a7755749ffd68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:54 +0000

    Add package schemainspect/0.1.1519171230/schemainspect-0.1.1519171230-py2.py3-none-any.whl

---
+++ packages/schemainspect/schemainspect-0.1.1519171230-py2.py3-none-any.whl/schemainspect/get.py
@@ -0,0 +1,19 @@
+from .misc import connection_from_s_or_c
+from .inspector import NullInspector
+from .pg import PostgreSQL
+
+SUPPORTED = {'postgresql': PostgreSQL}
+
+
+def get_inspector(x):
+    if x is None:
+        return NullInspector()
+
+    c = connection_from_s_or_c(x)
+
+    try:
+        ic = SUPPORTED[c.dialect.name]
+    except KeyError:
+        raise NotImplementedError
+
+    return ic(c)
