4c9c2951ff2754ba5fd313a391eb0d64aa25b109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:11 +0000

    Add package django-owm-legacy/0.4.1/django-owm-legacy-0.4.1.tar.gz

---
+++ packages/django-owm-legacy/django-owm-legacy-0.4.1.tar.gz/django-owm-legacy-0.4.1/owm_legacy/views.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.shortcuts import get_object_or_404
+
+from django_netjsonconfig.utils import send_config, send_file, update_last_ip
+
+from .utils import forbid_unallowed
+
+if 'django_netjsonconfig' in settings.INSTALLED_APPS:
+    from django_netjsonconfig.models import Config
+elif 'openwisp_controller.config' in settings.INSTALLED_APPS:  # pragma: nocover
+    from openwisp_controller.config.models import Config
+else:  # pragma: nocover
+    raise ImproperlyConfigured('django-owm-legacy depends on django-netjsonconfig or '
+                               'openwisp_controller.config, but neither is present '
+                               'in settings.INSTALLED_APPS')
+
+
+def get_config_md5(request, mac_address):
+    """
+    returns md5 of configuration
+    """
+    forbid_unallowed(request)
+    config = get_object_or_404(Config, device__mac_address__iexact=mac_address)
+    update_last_ip(config, request)
+    return send_file(mac_address, config.checksum)
+
+
+def get_config(request, mac_address):
+    """
+    returns configuration tar.gz
+    """
+    forbid_unallowed(request)
+    config = get_object_or_404(Config, device__mac_address__iexact=mac_address)
+    config.set_status_running(save=False)
+    return send_config(config, request)
