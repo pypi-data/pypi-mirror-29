7b0f48dd1cbcbada5cf0cb63dc67c5ab4c892dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package backup-roll/0.1/backup_roll-0.1.tar.gz

---
+++ packages/backup-roll/backup_roll-0.1.tar.gz/backup_roll-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'backup_roll',
+  packages = ['backup_roll'],
+  version = '0.1',
+  description = 'Tool to roll backup files',
+  author = 'Jan Inowolski',
+  author_email = 'jinowolski@gmail.com',
+  url = 'https://github.com/jinowolski/backup_roll',
+  download_url = 'https://github.com/jinowolski/backup_roll/archive/0.1.tar.gz',
+  keywords = ['backup', 'roll', 'rolling'],
+  classifiers = [],
+)
