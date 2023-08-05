6cd007ab0157c036778e716dc1583dea1038a9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:18 +0000

    Add package manticore/0.1.7/manticore-0.1.7.tar.gz

---
+++ packages/manticore/manticore-0.1.7.tar.gz/manticore-0.1.7/manticore/core/smtlib/__init__.py
@@ -0,0 +1,25 @@
+from expression import Expression, Bool, BitVec, Array
+from constraints import ConstraintSet
+from functools import wraps
+from solver import *
+import operators as Operators
+import math
+
+
+import logging
+logger = logging.getLogger(__name__)
+
+'''
+class OperationNotPermited(SolverException):
+    def __init__(self):
+        super(OperationNotPermited, self).__init__("You cant build this expression")     #no childrens
+
+class ConcretizeException(SolverException):
+    def __init__(self, expression):
+        super(ConcretizeException, self).__init__("Need to concretize the following and retry\n"+str(expression))     #no childrens
+        self.expression = expression
+'''
+
+
+class VisitorException(Exception):
+    pass
