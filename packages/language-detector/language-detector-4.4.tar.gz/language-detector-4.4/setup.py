5e63474d4033e9c872a8650286f1b2dde11f6a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:45 +0000

    Add package language-detector/4.4/language-detector-4.4.tar.gz

---
+++ packages/language-detector/language-detector-4.4.tar.gz/language-detector-4.4/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'language-detector',
+  packages = ['language_detector'],
+  package_dir = {'language_detector': 'language_detector'},
+  package_data = {'language_detector': ['prep/char_language.txt','tests/__init__.py','tests/test.py']},
+  version = '4.4',
+  description = 'Detect language of text',
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/DanielJDufour/language-detector',
+  download_url = 'https://github.com/DanielJDufour/language-detector/tarball/download',
+  keywords = ['arabic','farsi','french','kurdish','kurmanci','language','python','sorani','spanish','tagging','turkish'],
+  classifiers = [],
+)
