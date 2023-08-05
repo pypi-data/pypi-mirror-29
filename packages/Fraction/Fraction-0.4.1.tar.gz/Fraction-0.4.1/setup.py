e56b47f403a369a4d1449cf133f726382477235c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:02 +0000

    Add package Fraction/0.4.1/Fraction-0.4.1.tar.gz

---
+++ packages/Fraction/Fraction-0.4.1.tar.gz/Fraction-0.4.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+	name='Fraction',
+	version='0.4.1',
+	packages=find_packages(),
+	scripts=['fraction/Fraction.py'],
+	url="https://bradley101.github.io/fraction",
+	author='Shantanu Banerjee',
+	author_email='shantanu.banerjeee@gmail.com',
+	description='Fraction carries out all the fraction operations including addition, subtraction, multiplication, division, reciprocation',
+	keywords='Fraction, rational numbers'
+)
