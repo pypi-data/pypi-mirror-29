ba42b211e9e8f2e1583775d0e1c342d299ffcf0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package odoo11-addon-cms-status-message/11.0.1.0.1.99.dev1/odoo11_addon_cms_status_message-11.0.1.0.1.99.dev1-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-cms-status-message/odoo11_addon_cms_status_message-11.0.1.0.1.99.dev1-py2.py3-none-any.whl/odoo/addons/cms_status_message/static/src/js/tool.js
@@ -0,0 +1,63 @@
+odoo.define('cms_status_message.tool', function (require) {
+    'use strict';
+
+    var core = require('web.core'),
+        ajax = require('web.ajax');
+    var qweb = core.qweb;
+
+    // load existing qweb templates
+    ajax.jsonRpc('/web/dataset/call', 'call', {
+        'model': 'ir.ui.view',
+        'method': 'read_template',
+        'args': ['cms_status_message.message_listing_wrapper']
+    }).done(function (data) {
+        qweb.add_template(data);
+    });
+    ajax.jsonRpc('/web/dataset/call', 'call', {
+        'model': 'ir.ui.view',
+        'method': 'read_template',
+        'args': ['cms_status_message.message_wrapper']
+    }).done(function (data) {
+        qweb.add_template(data);
+    });
+
+    var MessageTool = {
+        add_message: function add_message (msg, options) {
+          return ajax.jsonRpc('/web/dataset/call', 'call', {
+            'model': 'website',
+            'method': 'add_status_message',
+            'args': [[msg, ], options]
+          });
+        },
+        get_messages: function get_messages() {
+            return ajax.jsonRpc('/web/dataset/call', 'call', {
+              'model': 'website',
+              'method': 'get_status_message',
+              'args': []
+            });
+        },
+        render_messages: function render_messages(msg, selector) {
+            // defaults
+            var status_message = {
+                'msg': '',
+                'title': null,
+                'type': 'info',
+                'dismissible': true
+            };
+            // inject user values
+            $.extend(status_message, msg);
+            // render it
+            var result = qweb.render(
+                'cms_status_message.message_listing_wrapper',
+                {status_message: [status_message]}
+            );
+            if(selector){
+                $(result).prependTo(selector);
+            }
+            return result;
+        }
+    };
+
+    return MessageTool;
+
+});
