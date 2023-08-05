c92a613a1becf75b15815cd92ef28b0059488189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:53 +0000

    Add package InnoCleaner/1.0.8.dev1/InnoCleaner-1.0.8.dev1-py3-none-any.whl

---
+++ packages/InnoCleaner/InnoCleaner-1.0.8.dev1-py3-none-any.whl/InnoCleaner/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+
+# Custom Imports
+from InnoCleaner import cleaningUtility as cleanUtil
+
+
+def __main__():
+    """
+    Entry Point to InnoCleaner Application
+    :return: None
+    """
+    if sys.version_info < (3, 0):
+        sys.exit("Sorry, Python < 3 not supported for now")
+    cleanUtil.CleaningUtility().main()
+
+
+__main__()
