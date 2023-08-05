d5713978c8acaa6baaa72ed82dd0121e70ae6d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:49 +0000

    Add package deeptime/0.0.0/deeptime-0.0.0.tar.gz

---
+++ packages/deeptime/deeptime-0.0.0.tar.gz/deeptime-0.0.0/deeptime/__init__.py
@@ -0,0 +1,24 @@
+#   This file is part of the markovmodel/deeptime repository.
+#   Copyright (C) 2017, 2018 Computational Molecular Biology Group,
+#   Freie Universitaet Berlin (GER)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+'''Deep learning meets molecular dynamics.'''
+
+__version__ = '0.0.0'
+
+print(
+    'This is a dummy package; see github.com/markovmodel/deeptime for the '
+    'individual packages which will be merged here in the near future.')
