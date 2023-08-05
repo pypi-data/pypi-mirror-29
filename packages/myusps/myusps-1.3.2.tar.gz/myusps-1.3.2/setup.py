797e5dc5174f2eb836b838de1c670c9f4408c2b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:29 +0000

    Add package myusps/1.3.2/myusps-1.3.2.tar.gz

---
+++ packages/myusps/myusps-1.3.2.tar.gz/myusps-1.3.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myusps',
+    version='1.3.2',
+    description='Python 3 API for USPS Informed Delivery, a way to track packages and mailpieces.',
+    url='https://github.com/happyleavesaoc/python-myusps/',
+    license='MIT',
+    author='happyleaves',
+    author_email='happyleaves.tfr@gmail.com',
+    packages=find_packages(),
+    install_requires=['beautifulsoup4==4.6.0', 'python-dateutil==2.6.0', 'requests==2.12.4', 'requests-cache==0.4.13', 'selenium==2.53.6'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ]
+)
