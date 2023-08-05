0a67a37d5bf740644f3c7eca02e992ddf769d976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:02 +0000

    Add package elib/0.1.45/elib-0.1.45-py36-none-any.whl

---
+++ packages/elib/elib-0.1.45-py36-none-any.whl/elib/custom_logging/__init__.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+"""
+Convenience functions to manage logging
+"""
+from elib.custom_logging._custom_logging_handler import CustomLoggingHandler
+from ._custom_logging import activate_elib_logging, get_logger, set_handler_level, get_elib_logger
