892cdf86617b2d4d1aa5d1ab1f9952b44dddbe7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:12 +0000

    Add package http-basic-auth/1.1.1/http-basic-auth-1.1.1.tar.gz

---
+++ packages/http-basic-auth/http-basic-auth-1.1.1.tar.gz/http-basic-auth-1.1.1/setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup
+
+requires = []
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
+
+
+setup(
+    # Basic package information:
+    name='http-basic-auth',
+    version='1.1.1',
+    py_modules=('http_basic_auth',),
+
+    # Packaging options:
+    zip_safe=False,
+    include_package_data=True,
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # Package dependencies:
+    requires=requires,
+    tests_require=requires + ["pytest"],
+    setup_requires=requires + ["pytest-runner"],
+    install_requires=requires,
+
+    # Metadata for PyPI:
+    author='Georgy Bazhukov',
+    author_email='georgy.bazhukov@gmail.com',
+    license='BSD',
+    url='https://github.com/bugov/http-basic-auth',
+    keywords='security basic auth http',
+    description='HTTP Basic Auth implementation',
+    long_description=read('readme.md'),
+)
