7254a45f63caee6570e50b4bb06fcf70149cf040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:10 +0000

    Add package jupyter-wysiwyg/0.1.5/jupyter-wysiwyg-0.1.5.tar.gz

---
+++ packages/jupyter-wysiwyg/jupyter-wysiwyg-0.1.5.tar.gz/jupyter-wysiwyg-0.1.5/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(name='jupyter-wysiwyg',
+      packages=['jupyter_wysiwyg'],
+      version='0.1.5',
+      description='WYSIWYG editing functionality for markdown/HTML cells in Jupyter',
+      license='BSD',
+      author='Thorin Tabor',
+      author_email='thorin@broadinstitute.org',
+      url='https://github.com/genepattern/jupyter-wysiwyg/tree/master/wysiwyg',
+      download_url='https://github.com/genepattern/jupyter-wysiwyg/archive/0.1.5.tar.gz',
+      keywords=['genepattern', 'wysiwyg', 'ipython', 'jupyter'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Framework :: IPython',
+      ],
+      install_requires=[
+          'jupyter',
+          'notebook>=4.2.0',
+      ],
+      package_dir={'jupyter_wysiwyg': 'jupyter_wysiwyg'},
+      package_data={'jupyter_wysiwyg': ['static/*',
+                                        'static/*/*',
+                                        'static/*/*/*',
+                                        'static/*/*/*/*',
+                                        'static/*/*/*/*/*',
+                                        'static/*/*/*/*/*/*']},
+      )
