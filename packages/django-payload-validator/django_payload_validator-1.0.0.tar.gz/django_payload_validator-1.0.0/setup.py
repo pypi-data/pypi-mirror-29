8d053f4664209bc8346b08ba1d240109b8faa3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:28 +0000

    Add package django-payload-validator/1.0.0/django_payload_validator-1.0.0.tar.gz

---
+++ packages/django-payload-validator/django_payload_validator-1.0.0.tar.gz/django_payload_validator-1.0.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django_payload_validator',
+    version='1.0.0',
+    description='validator for django payload',
+    long_description=long_description,
+    url='https://github.com/gladsonvm/django_payload_validator',
+    author='Gladson V Manuel',
+    author_email='gladsonvm@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='django payload validator intended audience is dev',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['django']
+)
