1f06fb6a17373a9c2ce8f43d7e54d9dac97cdaa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:16 +0000

    Add package osmdiff/0.1.8/osmdiff-0.1.8.tar.gz

---
+++ packages/osmdiff/osmdiff-0.1.8.tar.gz/osmdiff-0.1.8/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='osmdiff',
+    url='https://github.com/osmlab/osmdiff',
+    version='0.1.8',
+    packages=['osmdiff', 'osmdiff.osm'],
+    description="Interact with OpenStreetMap (Augmented) Diff files.",
+    long_description="Read the README.md",
+    license='Apache 2.0',
+    author="Martijn van Exel",
+    author_email="m@rtijn.org",
+    download_url="",
+    keywords=["osm", "openstreetmap", "diff", "augmented diff"],
+    classifiers=[],
+    install_requires=[
+        'requests',
+    ],
+)
