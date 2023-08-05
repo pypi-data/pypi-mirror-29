64e5b3f28fe625e4f500d5f22ea18b41aedec506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:38 +0000

    Add package conan_sword_and_sorcery/0.0.dev29/conan_sword_and_sorcery-0.0.dev29.tar.gz

---
+++ packages/conan_sword_and_sorcery/conan_sword_and_sorcery-0.0.dev29.tar.gz/conan_sword_and_sorcery-0.0.dev29/conan_sword_and_sorcery/ci/runners/base_runner.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import sys
+
+log = logging.getLogger(__name__)
+
+
+class BaseRunner(object):
+    profile = None
+    conanfile = None
+    recipe = None
+
+    def __init__(self, compiler):
+        self.compiler = compiler
+
+    def run(self, options, dry_run=False):
+        log.debug("Profile file: {}\n".format(self.profile))
+
+        # TODO: Code to get user/channel
+
+        command = ['conan', 'create', self.conanfile, 'jgsogo/testing',
+                   '--profile', self.profile, '--build=missing']
+        for k, v in options.items():
+            command += ['-o', '{}:{}={}'.format(self.recipe.name, k, v)]
+        return self.compiler.run(' '.join(command), dry_run=dry_run)
---
+++ packages/conan_sword_and_sorcery/conan_sword_and_sorcery-0.0.dev29.tar.gz/conan_sword_and_sorcery-0.0.dev29/conan_sword_and_sorcery/ci/runners/travis.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+from .base_runner import BaseRunner
+from .registry import RunnerRegistry
+from conan_sword_and_sorcery import __version__
+
+log = logging.getLogger(__name__)
+
+
+@RunnerRegistry.register("TRAVIS")
+class TravisRunner(BaseRunner):
+
+    def __init__(self, compiler, *args, **kwargs):
+        super(TravisRunner, self).__init__(compiler=compiler, *args, **kwargs)
+        self.use_docker = ("CONAN_DOCKER_IMAGE" in os.environ) or (os.environ.get("CONAN_USE_DOCKER", False))
+        if self.use_docker:
+            self.docker_image = os.environ.get("CONAN_DOCKER_IMAGE", None)
+            log.info("TravisRunner will use docker image '{}'".format(self.docker_image))
+
+            # Impostor for compiler.cmd method
+            compiler_cmd = compiler.cmd
+            def run_in_docker(command_plain):
+                compiler_cmd('docker run {} /bin/sh -c "{}"'.format(self.docker_image, command_plain))
+            compiler.cmd = run_in_docker
+
+            os.system("docker pull {}".format(self.docker_image))
+            os.system('docker run /bin/sh -c "sudo pip install -U conan conan_sword_and_sorcery=={}" && conan user'.format(__version__))
+            os.system('docker run --rm -v {cwd}:{cwd} /bin/sh -c "{command}"'.format(
+                cmd=os.getcwd(),
+                command="run_ci --dry-run"
+            ))
+
+
+
