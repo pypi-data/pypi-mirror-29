59d0a3725579e854448dabf0129c59a832fb465f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:42 +0000

    Add package abnum/0.1.6/abnum-0.1.6.tar.gz

---
+++ packages/abnum/abnum-0.1.6.tar.gz/abnum-0.1.6/abnum/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+# file: __init__.py
+
+__version__ = "0.1.6"
+
+from .main import Abnum, AbnumException
+"""
+exporting:
+- Abnum
+- AbnumException
+"""
+
+from .letter_value import *
+"""
+exporting:
+- data
+- greek, hebrew, coptic
+- english, sanskrit, arabic
+"""
+
+from .math import *
+"""
+exporting:
+- digital_root
+- digital_sum
+- digital_product
+- digital_root_summary
+"""
+
+from .search import find_cumulative_indices
+"""
+exporting:
+- find_cumulative_indices
+"""
