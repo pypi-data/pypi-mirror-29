9d4100e8f6893ea809bbfac47a897207d6e82a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:38 +0000

    Add package airship-python/0.1.0/airship-python-0.1.0.tar.gz

---
+++ packages/airship-python/airship-python-0.1.0.tar.gz/airship-python-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'airship-python',
+    packages = ['airship-python'], # this must be the same as the name above
+    version = '0.1.0',
+    description = 'Airship Python SDK',
+    author = 'Airship Support',
+    author_email = 'support@airshiphq.com',
+    url = 'https://github.com/airshiphq/airship-python', # use the URL to the github repo
+    download_url = 'https://github.com/airshiphq/airship-python/archive/0.1.0.tar.gz', # I'll explain this in a second
+    keywords = ['feature', 'flag', 'airship'], # arbitrary keywords
+    classifiers = [],
+)
