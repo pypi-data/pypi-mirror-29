31e37d5e767f2e3e81269d69a8d169f2a44d9308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:35 +0000

    Add package gapipy/2.12.0/gapipy-2.12.0.tar.gz

---
+++ packages/gapipy/gapipy-2.12.0.tar.gz/gapipy-2.12.0/gapipy/resources/dossier/country_dossier.py
@@ -0,0 +1,26 @@
+# Python 2 and 3
+from __future__ import unicode_literals
+
+from ..base import Resource
+from .details import DossierDetail
+from .dossier_features import DossierFeature
+
+
+class CountryDossier(Resource):
+    _resource_name = 'country_dossiers'
+
+    _as_is_fields = [
+        'id', 'href', 'name', 'type', 'publish_state',
+    ]
+
+    _date_time_fields_local = ['date_created', 'date_last_modified']
+
+    _resource_fields = [
+        ('country', 'Country'),
+        ('dossier_segment', 'DossierSegment'),
+    ]
+
+    _model_collection_fields = [
+        ('details', DossierDetail),
+        ('features', DossierFeature),
+    ]
