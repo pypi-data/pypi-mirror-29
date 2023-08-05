671c5cd446a5d29b2c02ea3a082c8e4a0ebd1817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package bsetools/0.1/bsetools-0.1.tar.gz

---
+++ packages/bsetools/bsetools-0.1.tar.gz/bsetools-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='bsetools',
+    version='0.1',
+    packages=['bsetools'],
+    url='https://github.com/shahronak47/bsetools',
+    license='MIT',
+    author='Ronak Shah',
+    author_email='shahronak47@yahoo.in',
+    description='Get BSE quotes'
+)
