4ddc9e5f3b30277e311d14194215b62fb65bb932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:21 +0000

    Add package zk_luks/0.1/zk_luks-0.1.tar.gz

---
+++ packages/zk_luks/zk_luks-0.1.tar.gz/zk_luks-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="zk_luks",
+      version="0.1",
+      description="Zymkey LUKS Utilities",
+      author="Zymbit, Inc.",
+      author_email="code@zymbit.com",
+      url="https://zymbit.com",
+      license="LICENSE.TXT")
