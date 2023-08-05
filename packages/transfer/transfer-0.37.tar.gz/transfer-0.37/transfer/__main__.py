698d8a853fe2fe7d38c154076743e52c21f355d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package transfer/0.37/transfer-0.37.tar.gz

---
+++ packages/transfer/transfer-0.37.tar.gz/transfer-0.37/transfer/__main__.py
@@ -0,0 +1,169 @@
+import sys
+import argparse
+import os
+
+import yaml
+from colorama import init
+from termcolor import colored
+try:
+    import tensorflow
+except ModuleNotFoundError:
+    print(colored('Tensorflow not installed!', 'red'))
+    print('Note: there are too many system specific things with this module')
+    print('Please look up how to install it for your system:')
+    print('')
+    print(colored('https://www.tensorflow.org/install/', 'yellow'))
+    print('')
+    raise ModuleNotFoundError
+import keras
+
+from transfer.project import configure, configure_server, select_project, update_config, import_config, export_config
+from transfer import images_to_array, pre_model
+from transfer.model import train_model
+from transfer.predict_model import predict_model
+from transfer.augment_arrays import augment_arrays
+from transfer.input import model_input, model_individual_input, str_input
+from transfer.server import start_server
+
+
+def main(args = None):
+    '''
+    Main entry point for transfer command line tool.
+
+    This essentially will marshall the user to the functions they need.
+    '''
+
+    parser = argparse.ArgumentParser(description = 'Tool to perform transfer learning')
+
+    parser.add_argument('-c','--configure',
+                        action = 'store_true',
+                        help = 'Configure transfer')
+
+    parser.add_argument('-e','--export',
+                        action = 'store_true',
+                        dest = 'export_config',
+                        help = 'Export configuration and models')
+
+    parser.add_argument('-i','--import',
+                        action = 'store',
+                        type = str,
+                        default = None,
+                        dest = 'import_config',
+                        help = 'Import configuration and models')
+
+    parser.add_argument('-p','--project',
+                        action = 'store',
+                        type = str,
+                        default = None,
+                        dest = 'project',
+                        help = 'Specify a project, if not supplied it will be picked from configured projects')
+
+    parser.add_argument('-r','--run',
+                        action = 'store_true',
+                        help = 'Run all transfer learning operations')
+
+    parser.add_argument('-f','--final',
+                        action = 'store_true',
+                        help = 'Run final training on all layers: Warning SLOW!')
+
+    parser.add_argument('-l','--last-weights',
+                        action = 'store_true',
+                        dest = 'last',
+                        help = 'Restart from the last weights, rather than the best intermediate weights')
+
+    parser.add_argument('--predict',
+                        action = 'store',
+                        type = str,
+                        default = None,
+                        const = 'default',
+                        dest = 'predict',
+                        nargs='?',
+                        help = 'Predict model on file or directory')
+
+    parser.add_argument('--prediction-rest-api',
+                        action = 'store_true',
+                        dest = 'rest_api',
+                        help = 'Start rest api to make predictions on files or directories')
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        return
+
+    args = parser.parse_args()
+
+    if args.import_config is not None:
+        import_config(args.import_config)
+        return
+    elif args.export_config:
+        project = select_project(args.project)
+        weights = model_input(project)
+        ind = model_individual_input(project, weights)
+        export_config(project, weights, ind)
+        return
+    elif args.configure:
+        configure()
+        return
+    else:
+        project = select_project(args.project)
+
+    if args.run:
+
+        if project['is_array'] == False:
+            project = images_to_array(project)
+            update_config(project)
+
+        if project['is_augmented'] == False:
+            project = augment_arrays(project)
+            update_config(project)
+
+        if project['is_pre_model'] == False:
+            project = pre_model(project)
+            update_config(project)
+
+        project = train_model(project, final = args.final, last = args.last)
+        update_config(project)
+
+        print('')
+        print(colored('Completed modeling round: ' + str(project['model_round']), 'cyan'))
+        print('')
+        print('Best current model: ', colored(project['best_weights'], 'yellow'))
+        print('Last current model: ', colored(project['last_weights'], 'yellow'))
+        print('')
+        print('To further refine the model, run again with:')
+        print('')
+        print(colored('    transfer --run --project ' + project['name'], 'green'))
+        print('')
+        print('To make predictions:')
+        print('')
+        print(colored('    transfer --predict [optional dir or file] --project ' + project['name'], 'yellow'))
+        print('')
+
+    elif args.rest_api:
+        if project['server_weights'] is not None:
+            start_server(project, 'server_weights')
+
+        elif project['best_weights'] is not None:
+            weights = model_input(project)
+            start_server(project, weights)
+
+        else:
+            print('Model is not trained.  Please first run your project:')
+            print('')
+            print(colored('    transfer --run', 'green'))
+            print('')
+    elif args.predict is not None:
+        if args.predict == 'default':
+            args.predict = str_input('Enter a path to file(s): ')
+        if project['server_weights'] is not None:
+            predict_model(project, 'server_weights', args.predict)
+
+        elif project['best_weights'] is not None:
+            weights = model_input(project)
+            print('Predicting on image(s) in: ', colored(args.predict, 'yellow'))
+            predict_model(project, weights, args.predict)
+
+        else:
+            print('Model is not trained.  Please first run your project:')
+            print('')
+            print(colored('    transfer --run', 'green'))
+            print('')
---
+++ packages/transfer/transfer-0.37.tar.gz/transfer-0.37/transfer/server.py
@@ -0,0 +1,82 @@
+import os
+
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jsonpify import jsonify
+from colorama import init
+from termcolor import colored
+import numpy as np
+
+from transfer.resnet50 import get_resnet_final_model
+from transfer.xception import get_xception_final_model
+from transfer.inception_v3 import get_inception_v3_final_model
+from transfer.predict_model import prep_from_image, gen_from_directory, multi_predict
+
+def start_server(project, weights):
+
+    app = Flask(__name__)
+    api = Api(app)
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('img_path', type = str)
+
+    img_dim = 224 * project['img_size']
+    conv_dim = 7 * project['img_size']
+    models = []
+    for weight in project[weights]:
+        if project['architecture'] == 'resnet50':
+            models.append(get_resnet_final_model(img_dim, conv_dim, project['number_categories'], weight, project['is_final']))
+        elif project['architecture'] == 'xception':
+            models.append(get_xception_final_model(img_dim, conv_dim, project['number_categories'], weight, project['is_final']))
+        else:
+            models.append(get_inception_v3_final_model(img_dim, conv_dim, project['number_categories'], weight, project['is_final']))
+
+    class Predict(Resource):
+        def post(self):
+            args = parser.parse_args(strict = True)
+            img_path = os.path.expanduser(args['img_path'])
+            if os.path.isfile(img_path):
+                if img_path.lower().find('.png') > 0 or img_path.lower().find('.jpg') > 0 or img_path.lower().find('.jpeg') > 0:
+                    aug_gen = prep_from_image(img_path, img_dim, project['augmentations'])
+                    pred_list, predicted = multi_predict(aug_gen, models, project['architecture'])
+                    pred_list = [[float(p) for p in pred] for pred in list(pred_list)]
+                    result = {'weights': project[weights],
+                             'image_path': img_path,
+                             'predicted': project['categories'][np.argmax(predicted)],
+                             'classes': project['categories'],
+                             'class_predictions': pred_list}
+
+                    return jsonify(result)
+                else:
+                    return 'File must be a jpeg or png: ' + args['img_path']
+            elif os.path.isdir(img_path):
+                result = []
+
+                for aug_gen, file_name in gen_from_directory(img_path, img_dim, project):
+                    pred_list, predicted = multi_predict(aug_gen, models, project['architecture'])
+                    pred_list = [[float(p) for p in pred] for pred in list(pred_list)]
+                    result.append({'weights': project[weights],
+                            'image_path': file_name,
+                            'predicted': project['categories'][np.argmax(predicted)],
+                            'classes': project['categories'],
+                            'class_predictions': pred_list})
+                if len(result) > 0:
+                    return jsonify(result)
+                else:
+                    return 'No images found in directory: ' + args['img_path']
+
+            else:
+                return 'Image does not exist locally: ' + args['img_path']
+
+
+    api.add_resource(Predict, '/predict')
+    print('')
+    print('To predict a local image, simply:')
+    print('')
+    print(colored('curl http://localhost:' + str(project['api_port']) + '/predict -d "img_path=/path/to/your/img.png" -X POST', 'green'))
+    print('')
+    print('or')
+    print('')
+    print(colored('curl http://localhost:' + str(project['api_port']) + '/predict -d "img_path=/path/to/your/img_dir" -X POST', 'green'))
+    print('')
+    app.run(port = project['api_port'])
