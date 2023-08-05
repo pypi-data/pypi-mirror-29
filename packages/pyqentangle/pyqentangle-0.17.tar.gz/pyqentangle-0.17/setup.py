1994b3ae57f725c8ef1c2aac9d4f8fe302971920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package pyqentangle/0.17/pyqentangle-0.17.tar.gz

---
+++ packages/pyqentangle/pyqentangle-0.17.tar.gz/pyqentangle-0.17/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='pyqentangle',
+      version="0.17",
+      description="Quantum Entanglement for Python",
+      long_description="Schmidt decomposition for discrete and continuous bi-partite quantum systems",
+      classifiers=[
+          "Topic :: Scientific/Engineering :: Physics",
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "License :: OSI Approved :: MIT License",
+      ],
+      keywords="quantum physics Schmidt decompostion entanglement",
+      url="https://github.com/stephenhky/pyqentangle",
+      author="Kwan-Yuet Ho",
+      author_email="stephenhky@yahoo.com.hk",
+      license='MIT',
+      packages=['pyqentangle'],
+      install_requires=[
+          'numpy',
+      ],
+      tests_require=['unittest2', 'numpy',],
+      test_suite="test",
+      # include_package_data=True,
+      zip_safe=False)
