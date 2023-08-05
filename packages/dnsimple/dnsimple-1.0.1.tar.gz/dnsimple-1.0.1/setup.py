59e40217e0af706d575d675462a05e8ad5478c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:03 +0000

    Add package dnsimple/1.0.1/dnsimple-1.0.1.tar.gz

---
+++ packages/dnsimple/dnsimple-1.0.1.tar.gz/dnsimple-1.0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+from dnsimple import dnsimple
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), 'r') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='dnsimple',
+    version=dnsimple.__version__,
+    description=u' '.join(dnsimple.__doc__.splitlines()).strip(),
+    long_description=readme,
+    maintainer='David Aronsohn',
+    maintainer_email='WagThatTail@Me.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    url='https://github.com/onlyhavecans/dnsimple-python/',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires=['requests>=2.10.0,<=2.18.4']
+)
