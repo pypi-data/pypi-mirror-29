0e67aaff75e9caf34d890642d8a10d6c0a802d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:23 +0000

    Add package easysock/1.0.0/easysock-1.0.0-py2.py3-none-any.whl

---
+++ packages/easysock/easysock-1.0.0-py2.py3-none-any.whl/easysock/main.py
@@ -0,0 +1,29 @@
+from socket import *
+import sys,time
+
+class Esocket():
+    def __init__(self,PORT):
+        self.socket=s = socket(AF_INET, SOCK_DGRAM)
+        self.socket.bind(('', PORT))
+        self.PORT = PORT
+    
+    def bind(self,PORT):
+        self.socket=s = socket(AF_INET, SOCK_DGRAM)
+        self.socket.bind(('', PORT))
+        self.PORT = PORT
+    
+    #reciveing code:
+    def recive(self,timeout=1):
+        start=time.time()
+        while start-time.time() < timeout:
+            data, wherefrom = self.socket.recvfrom(1500, 0)
+            sys.stdout.write(data)
+    
+    #send code:
+    def send(self, data):
+        self.socket.sendto(str(data), (SO_BROADCAST, self.PORT))
+
+    
+    #closeing code:
+    def close(self):
+        self.close()
