f34eaf6ef04f9196de2aae9406eb0244cf4e804e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:22 +0000

    Add package isi-sdk-8-0-1/0.1.11/isi_sdk_8_0_1-0.1.11-py2.py3-none-any.whl

---
+++ packages/isi-sdk-8-0-1/isi_sdk_8_0_1-0.1.11-py2.py3-none-any.whl/test/test_certificate_api.py
@@ -0,0 +1,83 @@
+# coding: utf-8
+
+"""
+Copyright 2016 SmartBear Software
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   ref: https://github.com/swagger-api/swagger-codegen
+"""
+
+from __future__ import absolute_import
+
+import os
+import sys
+import unittest
+
+import swagger_client
+from swagger_client.rest import ApiException
+from swagger_client.apis.certificate_api import CertificateApi
+
+
+class TestCertificateApi(unittest.TestCase):
+    """ CertificateApi unit test stubs """
+
+    def setUp(self):
+        self.api = swagger_client.apis.certificate_api.CertificateApi()
+
+    def tearDown(self):
+        pass
+
+    def test_create_certificate_server_item(self):
+        """
+        Test case for create_certificate_server_item
+
+        
+        """
+        pass
+
+    def test_delete_certificate_server_by_id(self):
+        """
+        Test case for delete_certificate_server_by_id
+
+        
+        """
+        pass
+
+    def test_get_certificate_server_by_id(self):
+        """
+        Test case for get_certificate_server_by_id
+
+        
+        """
+        pass
+
+    def test_list_certificate_server(self):
+        """
+        Test case for list_certificate_server
+
+        
+        """
+        pass
+
+    def test_update_certificate_server_by_id(self):
+        """
+        Test case for update_certificate_server_by_id
+
+        
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
