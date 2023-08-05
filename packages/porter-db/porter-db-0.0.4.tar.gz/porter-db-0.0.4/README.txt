0e7a5b6192201c0cfd3c572bef398e0dfc3f2d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:25 +0000

    Add package porter-db/0.0.4/porter-db-0.0.4.tar.gz

---
+++ packages/porter-db/porter-db-0.0.4.tar.gz/porter-db-0.0.4/README.txt
@@ -0,0 +1,1 @@
+A document-based database setup.
---
+++ packages/porter-db/porter-db-0.0.4.tar.gz/porter-db-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name="porter-db",
+	version="0.0.4",
+	description="simple document-based database",
+	url="https://github.com/wrecodde/porter-db",
+	author="Deji Joseph",
+	author_email="wrecodde@gmail.com",
+	license="MIT",
+        packages=["porter_db"]
+)
