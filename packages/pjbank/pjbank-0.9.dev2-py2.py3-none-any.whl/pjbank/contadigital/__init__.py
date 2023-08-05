8e9a069c5a2f862232d789a157c60cfea162ff33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:49 +0000

    Add package pjbank/0.9.dev2/pjbank-0.9.dev2-py2.py3-none-any.whl

---
+++ packages/pjbank/pjbank-0.9.dev2-py2.py3-none-any.whl/pjbank/contadigital/__init__.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pjbank.api import PJBankAPI
+
+class ContaDigital(PJBankAPI):
+    """docstring for ContaDigital."""
+    def __init__(self, credencial=None, chave=None, webhook_chave=None):
+        super(ContaDigital, self).__init__(credencial, chave)
+        self._endpoint_base = "contadigital"
+        self._webhook_chave = webhook_chave
+
+    def automatico(f):
+        def wrapper(self, *args, **kwargs):
+            if 'c' in kwargs.keys():
+                self.credencial = kwargs['c']
+                kwargs.pop('c')
+            if 'ch' in kwargs.keys():
+                self.chave = kwargs['ch']
+                kwargs.pop('ch')
+            return f(self, *args, **kwargs)
+        return wrapper
+
+    @property
+    def webhook_chave(self):
+        return self._webhook_chave
+
+    @webhook_chave.setter
+    def webhook_chave(self, chave):
+        self._webhook_chave = chave
+
+    def credenciar(self, dados):
+        super(ContaDigital, self).credenciar(dados)
+        self.webhook_chave = self.resposta_credenciamento['webhook_chave']
+        return self
+
+    def _consulta(self, endpoint=None):
+        headers = self.headers_chave        
+        response = self._get(endpoint, headers)
+        return response.json()   
+
+    @automatico    
+    def dados_conta(self):
+        return self._consulta()
+    @automatico        
+    def documentos(self):
+        return self._consulta(['documentos'])
+    
+    @automatico    
+    def status_socio(self, email):
+        return self._consulta(['administradores', email])
+
+    @automatico    
+    def administradores(self):
+        return self._consulta(['administradores'])
