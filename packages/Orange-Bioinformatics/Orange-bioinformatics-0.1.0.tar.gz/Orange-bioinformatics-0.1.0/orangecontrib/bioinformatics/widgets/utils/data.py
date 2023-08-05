179f7e4ead27cee940514c593cb692285d8fbb74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:54 +0000

    Add package Orange-Bioinformatics/0.1.0/Orange-bioinformatics-0.1.0.tar.gz

---
+++ packages/Orange-Bioinformatics/Orange-bioinformatics-0.1.0.tar.gz/Orange-bioinformatics-0.1.0/orangecontrib/bioinformatics/widgets/utils/data.py
@@ -0,0 +1,8 @@
+""" Orange data table and other data manipulation utilities """
+
+# Data hints variables
+TAX_ID = 'Taxonomy_id'
+
+# Will be set to True if gene names are represented as attribute names.
+# If gene names are in rows, we set this value to False. (user must select proper column index)
+GENE_NAME = 'gene_as_attribute_name'
