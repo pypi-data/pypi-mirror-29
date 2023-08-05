501e1d371de0bf056d819c8bc90fc25d14ccde45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:15 +0000

    Add package coreir/0.15a0/coreir-0.15a0.tar.gz

---
+++ packages/coreir/coreir-0.15a0.tar.gz/coreir-0.15a0/coreir/type_gen.py
@@ -0,0 +1,20 @@
+import coreir
+from functools import wraps
+import ctypes
+
+def type_gen(fn):
+    @wraps(fn)
+    def wrapped(context, names, values, num_values):
+        """
+        Only use these arguments in this function, coreir will cleanup the
+        pointers after this function returns, DO NOT store
+        """
+        context = coreir.Context(ctypes.cast(context, coreir.COREContext_p))
+        values = ctypes.cast(values, ctypes.POINTER(coreir.COREValue_p))
+        names = ctypes.cast(names, ctypes.POINTER(ctypes.c_char_p))
+        values_map = {}
+        for i in range(num_values):
+            values_map[names[i].decode()] = coreir.Value(values[i], context)
+        type_obj = fn(context, values_map)
+        return ctypes.addressof(type_obj.ptr.contents)
+    return wrapped
