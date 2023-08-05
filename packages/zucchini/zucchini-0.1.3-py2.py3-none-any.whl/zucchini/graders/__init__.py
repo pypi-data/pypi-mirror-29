44b410ef3e3605d79995a568514606944f4f913a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:13 +0000

    Add package zucchini/0.1.3/zucchini-0.1.3-py2.py3-none-any.whl

---
+++ packages/zucchini/zucchini-0.1.3-py2.py3-none-any.whl/zucchini/graders/__init__.py
@@ -0,0 +1,24 @@
+from .exceptions import InvalidGraderConfigError
+from .grader_interface import GraderInterface, Part
+from .threaded_grader import ThreadedGrader
+from .prompt_grader import PromptGrader
+from .open_file_grader import OpenFileGrader
+from .libcheck_grader import LibcheckGrader
+from .junit_json_grader import JUnitJSONGrader
+from .junit_xml_grader import JUnitXMLGrader
+from .bitwise_json_grader import BitwiseJSONGrader
+
+__all__ = ['InvalidGraderConfigError', 'GraderInterface', 'Part',
+           'ThreadedGrader', 'PromptGrader', 'OpenFileGrader',
+           'LibcheckGrader', 'JUnitJSONGrader', 'JUnitXMLGrader',
+           'BitwiseJSONGrader']
+
+_GRADERS = (
+    PromptGrader,
+    OpenFileGrader,
+    LibcheckGrader,
+    JUnitJSONGrader,
+    JUnitXMLGrader,
+    BitwiseJSONGrader,
+)
+AVAILABLE_GRADERS = {cls.__name__: cls for cls in _GRADERS}
