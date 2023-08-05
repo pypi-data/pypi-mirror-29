757ab10772785e463162b7cb0214ece7dddbf554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:23 +0000

    Add package djio/0.0.4/djio-0.0.4.tar.gz

---
+++ packages/djio/djio-0.0.4.tar.gz/djio-0.0.4/djio/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. currentmodule:: __init__.py
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+Working with djiographic information systems?  Djio can help.
+"""
+
+__version__ = '0.0.4'  #: the working version
+__release__ = '0.0.4'  #: the release version
