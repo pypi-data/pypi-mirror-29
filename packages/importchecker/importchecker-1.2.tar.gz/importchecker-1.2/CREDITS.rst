dcb396563f9e0fe5d99a05b8525b1a7c39652b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:35 +0000

    Add package importchecker/1.2/importchecker-1.2.tar.gz

---
+++ packages/importchecker/importchecker-1.2.tar.gz/importchecker-1.2/CREDITS.rst
@@ -0,0 +1,7 @@
+Credits
+=======
+
+* Martijn Faassen (original developer)
+* Jan-Wijbrand Kolman (packaging)
+* Philipp von Weitershausen
+* various comitters on svn.zope.org.
---
+++ packages/importchecker/importchecker-1.2.tar.gz/importchecker-1.2/setup.py
@@ -0,0 +1,60 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+version = '1.2'
+description = "Importchecker finds unused imports in Python modules."
+test_requirements = ['mock', 'zope.testrunner']
+
+
+def read(*rnames):
+    filename = os.path.join(os.path.dirname(__file__), *rnames)
+    with open(filename, 'r') as txt:
+        return txt.read()
+
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    read('CHANGES.rst')
+    + '\n' +
+    read('CREDITS.rst')
+    + '\n' +
+    'Download\n'
+    '========\n'
+    )
+
+
+setup(
+    name='importchecker',
+    version=version,
+    description=description,
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        ],
+    keywords='',
+    author='Jan-Wijbrand Kolman',
+    author_email='janwijbrand@gmail.com',
+    url='http://pypi.python.org/pypi/importchecker',
+    license='ZPL 2.1',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+    tests_require=test_requirements,
+    extras_require={'test': test_requirements},
+    entry_points={
+        'console_scripts': ['importchecker=importchecker.importchecker:main'],
+        },
+    )
