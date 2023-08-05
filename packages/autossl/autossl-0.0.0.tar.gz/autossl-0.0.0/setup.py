1fb979abeae4f3de40a9ac39ae3664d4347a9ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:36 +0000

    Add package autossl/0.0.0/autossl-0.0.0.tar.gz

---
+++ packages/autossl/autossl-0.0.0.tar.gz/autossl-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+from setuptools import setup
+
+setup(
+    name="autossl",
+    version="0.0.0",
+    author='Thibaud Castaing',
+    author_email='t-cas@users.noreply.github.com',
+    description="Python module to automate SSL certificate renewal and deployment",
+)
