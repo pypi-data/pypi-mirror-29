71e2658dfffd331f0e142dc03ce33a35349e7ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:11 +0000

    Add package stylelens-crawl-amazon/0.0.12/stylelens-crawl-amazon-0.0.12.tar.gz

---
+++ packages/stylelens-crawl-amazon/stylelens-crawl-amazon-0.0.12.tar.gz/stylelens-crawl-amazon-0.0.12/stylelens_crawl_amazon/model/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+# import models into model package
+from .item import Item
+from .item_image import ItemImage
+from .item_price import ItemPrice
