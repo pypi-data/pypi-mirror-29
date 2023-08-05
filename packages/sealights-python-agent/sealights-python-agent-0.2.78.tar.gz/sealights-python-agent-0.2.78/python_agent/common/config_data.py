ca53131a593431801773ce1a54578f831e624f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:04 +0000

    Add package sealights-python-agent/0.2.78/sealights-python-agent-0.2.78.tar.gz

---
+++ packages/sealights-python-agent/sealights-python-agent-0.2.78.tar.gz/sealights-python-agent-0.2.78/python_agent/common/config_data.py
@@ -0,0 +1,26 @@
+from python_agent.common import constants
+
+
+class ConfigData(object):
+    def __init__(self, token, customer_id, server, proxy, build_session_id, is_compress=True):
+        self.token = token
+        self.server = server
+        self.proxy = proxy
+        self.is_compress = is_compress
+        self.buildSessionId = build_session_id
+        self.customerId = customer_id
+        self.appName = None
+        self.buildName = None
+        self.branchName = None
+        self.labId = None
+        self.testStage = None
+        self.additionalParams = {}
+        self.workspacepath = None
+        self.include = None
+        self.exclude = None
+        self.isInitialColor = True
+        self.initialColor = constants.INITIAL_COLOR
+        self.args = None
+        self.program = None
+        self.isSendLogs = False
+        self.isOfflineMode = False
