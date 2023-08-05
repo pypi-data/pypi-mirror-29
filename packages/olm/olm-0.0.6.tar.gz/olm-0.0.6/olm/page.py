3e864124ff6045ca6994625009bebd8bfd9f56cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package olm/0.0.6/olm-0.0.6.tar.gz

---
+++ packages/olm/olm-0.0.6.tar.gz/olm-0.0.6/olm/page.py
@@ -0,0 +1,45 @@
+import os
+from os.path import dirname, splitext, relpath, join
+import datetime
+import codecs
+import re
+from olm.signals import signals, Signal
+from olm.reader import Reader
+from olm.writer import Writer
+
+class Page:
+    """Object representing an article"""
+
+    def __init__(self, context, filepath=None):
+        dirname = os.path.dirname(filepath)
+        basepath, filename = os.path.split(filepath)
+        basename, extension = os.path.splitext(filename)
+        relpath = os.path.relpath(os.path.join(dirname, basename) + '.html', context.SOURCE_FOLDER)
+        
+        # Parse the file for content and metadata
+        with codecs.open(filepath, 'r', encoding='utf8') as md_file:
+            reader = Reader(md_file.read())
+            metadata, raw_content = reader.parse()
+
+        self.content = context.MD(raw_content)
+        self.metadata = metadata
+
+        self.source_filepath = filepath
+        self.template        = 'page.html'
+        self.title           = self.metadata['title'] if 'title' in self.metadata else basename
+        self.url             = relpath
+        self.output_filepath = join(context.OUTPUT_FOLDER, relpath)
+        self.context = context
+
+        signal_sender = Signal(signals.AFTER_PAGE_READ)
+        signal_sender.send(context=context, page=self)
+
+
+    def write_file(self, context=None):
+        self.context = context if context is not None else self.context
+        writer = Writer(
+            self.context, 
+            self.output_filepath, 
+            self.template,
+            page=self)
+        writer.write_file()
