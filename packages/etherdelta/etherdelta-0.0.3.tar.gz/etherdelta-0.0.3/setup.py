a9c5b7b6c8fb1e469488e71634e4459fe32e54cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:00 +0000

    Add package etherdelta/0.0.3/etherdelta-0.0.3.tar.gz

---
+++ packages/etherdelta/etherdelta-0.0.3.tar.gz/etherdelta-0.0.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = 'etherdelta',
+    packages = ['etherdelta'],
+    version = '0.0.3',
+    url = 'https://github.com/miguelmota/py-etherdelta',
+    download_url = 'https://github.com/miguelmota/py-etherdelta/archive/master.zip',
+    author = 'Miguel Mota <hello@miguelmota.com>',
+    author_email = 'hello@miguelmota.com',
+    license = 'MIT License',
+    description = 'Python wrapper for interacting with the EtherDelta API and Smart Contracts.',
+    long_description = open('README.md','r').read(),
+    keywords = ['etherdelta'],
+)
