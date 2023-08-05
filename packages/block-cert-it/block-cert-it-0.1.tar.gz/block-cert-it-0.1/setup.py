13914a44edcb359319c74fdd192a1842dfe6d8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:11 +0000

    Add package block-cert-it/0.1/block-cert-it-0.1.tar.gz

---
+++ packages/block-cert-it/block-cert-it-0.1.tar.gz/block-cert-it-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+     name='block-cert-it',    # This is the name of your PyPI-package.
+     version='0.1',                          # Update the version number for new releases
+     scripts=['__main__.py']                  # The name of your scipt, and also the command you'll be using for calling it
+)
