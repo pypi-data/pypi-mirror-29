35e5d2777b15531777f79b3ae422d181fe88a45a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:13 +0000

    Add package wiki/0.4a1/wiki-0.4a1-py2.py3-none-any.whl

---
+++ packages/wiki/wiki-0.4a1-py2.py3-none-any.whl/wiki/core/utils.py
@@ -0,0 +1,21 @@
+from importlib import import_module
+
+from django.http.response import JsonResponse
+
+
+def get_class_from_str(class_path):
+    """Dynamically load a View class from a string path
+    "myapp.views.MyView" -- used for dynamic settings"""
+    module_path, klass_name = class_path.rsplit('.', 1)
+    module = import_module(module_path)
+    return getattr(module, klass_name)
+
+
+def object_to_json_response(obj, status=200):
+    """
+    Given an object, returns an HttpResponse object with a JSON serialized
+    version of that object
+    """
+    return JsonResponse(
+        data=obj, status=status, safe=False, json_dumps_params={'ensure_ascii': False},
+    )
---
+++ packages/wiki/wiki-0.4a1-py2.py3-none-any.whl/wiki/editors/__init__.py
@@ -0,0 +1,19 @@
+from wiki.conf import settings
+from django.urls import get_callable
+
+_EditorClass = None
+_editor = None
+
+
+def getEditorClass():
+    global _EditorClass
+    if not _EditorClass:
+        _EditorClass = get_callable(settings.EDITOR)
+    return _EditorClass
+
+
+def getEditor():
+    global _editor
+    if not _editor:
+        _editor = getEditorClass()()
+    return _editor
