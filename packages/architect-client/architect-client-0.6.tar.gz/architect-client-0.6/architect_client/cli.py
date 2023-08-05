302ee3de2690038cdff2d8735edc93235de1f2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:42 +0000

    Add package architect-client/0.6/architect-client-0.6.tar.gz

---
+++ packages/architect-client/architect-client-0.6.tar.gz/architect-client-0.6/architect_client/cli.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Inventory script to get the metadata from the Architect service.
+"""
+
+import os
+import json
+import yaml
+import click
+from architect_client.libarchitect import ArchitectClient
+
+
+def load_yaml_file(path):
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            return yaml.safe_load(f)
+    return {}
+
+
+def write_json_file(path, content):
+    file_handler = open(path, "w")
+    json.dump(content, file_handler)
+    file_handler.close()
+
+
+@click.command()
+@click.argument('cluster_name')
+@click.argument('domain_name')
+def client_inventory_create(cluster_name, domain_name):
+    config = load_yaml_file('/etc/architect/client.yml')
+    inventory_api_url = 'http://{}:{}'.format(config['host'],
+                                              config['port'])
+    inventory_name = config['project']
+    client = ArchitectClient(inventory_api_url, inventory_name)
+    data = client.create_inventory(cluster_name, domain_name)
+    print(yaml.safe_dump(data))
+
+
+@click.command()
+@click.argument('manager_name')
+@click.argument('manager_url')
+@click.argument('manager_user')
+@click.argument('manager_password')
+def client_manager_salt_create(manager_name, manager_url, manager_user, manager_password):
+    config = load_yaml_file('/etc/architect/client.yml')
+    manager_api_url = 'http://{}:{}'.format(config['host'],
+                                            config['port'])
+    client = ArchitectClient(manager_api_url)
+    data = client.create_manager(manager_name, manager_url, manager_user, manager_password)
+    print(yaml.safe_dump(data))
+
+
+
+@click.command()
+@click.option('--list', is_flag=True)
+def adapter_ansible_inventory(list):
+    config = load_yaml_file('/etc/architect/client.yml')
+    inventory_api_url = 'http://{}:{}'.format(config['host'],
+                                              config['port'])
+    inventory_name = config['project']
+    client = ArchitectClient(inventory_api_url, inventory_name)
+    data = client.get_data('ansible-inventory')
+    print(yaml.safe_dump(data))
+
+
+@click.command()
+@click.argument('resource_name')
+def adapter_puppet_classifier(resource_name):
+    config = load_yaml_file('/etc/architect/client.yml')
+    inventory_api_url = 'http://{}:{}'.format(config['host'],
+                                              config['port'])
+    inventory_name = config['project']
+    client = ArchitectClient(inventory_api_url, inventory_name)
+    data = client.get_data('puppet-classifier', resource_name)
+    print(yaml.safe_dump({
+        'classes': data[resource_name]['applications'],
+        'parameters': data[resource_name]['parameters']
+    }))
+
+
+@click.command()
+@click.argument('resource_name')
+def adapter_salt_pillar(resource_name):
+    config = load_yaml_file('/etc/architect/client.yml')
+    inventory_api_url = 'http://{}:{}'.format(config['host'],
+                                              config['port'])
+    inventory_name = config['project']
+    client = ArchitectClient(inventory_api_url, inventory_name)
+    data = client.get_data('salt-pillar', resource_name)
+    print(yaml.safe_dump(data[resource_name]['parameters']))
+
+
+@click.command()
+@click.argument('resource_name')
+def adapter_chef_data(resource_name, config_file=None):
+    config = load_yaml_file('/etc/architect/client.yml')
+    inventory_api_url = 'http://{}:{}'.format(config['host'],
+                                              config['port'])
+    inventory_name = config['project']
+    client = ArchitectClient(inventory_api_url, inventory_name)
+    data = client.get_data('chef-data', resource_name)
+    if config_file is None:
+        print(json.dump(data[resource_name]['parameters']))
+    else:
+        write_json_file(data[resource_name]['parameters'], config_file)
+
+
+@click.command()
+@click.argument('resource_name')
+def adapter_salt_top(resource_name):
+    config = load_yaml_file('/etc/architect/client.yml')
+    inventory_api_url = 'http://{}:{}'.format(config['host'],
+                                              config['port'])
+    inventory_name = config['project']
+    client = ArchitectClient(inventory_api_url, inventory_name)
+    data = client.get_data('salt-top', resource_name)
+    print(yaml.safe_dump({'classes': data[resource_name]['applications']}))
---
+++ packages/architect-client/architect-client-0.6.tar.gz/architect-client-0.6/architect_client/libarchitect.py
@@ -0,0 +1,116 @@
+
+try:
+    import urllib.parse as urlparse
+except ImportError:
+    import urlparse
+
+
+class ArchitectException(Exception):
+    pass
+
+
+class ArchitectClient(object):
+
+    def __init__(self, api_url='http://localhost:8181', inventory='default'):
+        self.api_url = api_url
+        self.inventory = inventory
+
+    def _req_get(self, path):
+        '''
+        A thin wrapper to get http method of architect api
+        print(api._req_get('/keys'))
+        '''
+        import requests
+
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        # self._ssl_verify = self.ignore_ssl_errors
+        params = {'url': self._construct_url(path),
+                  'headers': headers}
+        try:
+            resp = requests.get(**params)
+            if resp.status_code == 401:
+                raise ArchitectException(str(resp.status_code) + ':Authentication denied')
+                return
+            if resp.status_code == 500:
+                raise ArchitectException('{}: Server error.'.format(resp.status_code))
+                return
+            if resp.status_code == 404:
+                raise ArchitectException(str(resp.status_code) +' :This request returns nothing.')
+                return
+        except ArchitectException as e:
+            print(e)
+            return
+        return resp.json()
+
+    def _req_post_json(self, path, data):
+        '''
+        A thin wrapper to postt http method of architect api
+        print(api._req_post('/keys'))
+        '''
+        import requests
+
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        # self._ssl_verify = self.ignore_ssl_errors
+        params = {'url': self._construct_url(path),
+                  'headers': headers,
+                  'json': data}
+        try:
+            resp = requests.post(**params)
+            if resp.status_code == 401:
+                raise ArchitectException(str(resp.status_code) + ':Authentication denied')
+                return
+            if resp.status_code == 500:
+                raise ArchitectException('{}: Server error.'.format(resp.status_code))
+                return
+            if resp.status_code == 404:
+                raise ArchitectException(str(resp.status_code) +' :This request returns nothing.')
+                return
+        except ArchitectException as e:
+            print(e)
+            return
+        return resp.json()
+
+    def _construct_url(self, path):
+        '''
+        Construct the url to architect-api for the given path
+        Args:
+            path: the path to the architect-api resource
+        '''
+
+        relative_path = path.lstrip('/')
+        return urlparse.urljoin(self.api_url, relative_path)
+
+    def create_inventory(self, cluster_name, domain_name):
+        path = '/inventory/v1/inventory-create/data.json'
+        data = {
+            'inventory_name': self.inventory,
+            'cluster_name': cluster_name,
+            'domain_name': domain_name
+        }
+        return self._req_post_json(path, data)
+
+    def create_manager(self, name, url, user, password):
+        path = '/manager/v1/manager-create/data.json'
+        data = {
+            'manager_name': name,
+            'manager_url': url,
+            'manager_user': user,
+            'manager_password': password
+        }
+        return self._req_post_json(path, data)
+
+    def get_data(self, source, resource=None):
+        if resource is None:
+            path = '/inventory/v1/{}/data.json?source={}'.format(self.inventory,
+                                                                 source)
+        else:
+            path = '/inventory/v1/{}/{}/data.json?source={}'.format(self.inventory,
+                                                                    resource,
+                                                                    source)
+        return self._req_get(path)
