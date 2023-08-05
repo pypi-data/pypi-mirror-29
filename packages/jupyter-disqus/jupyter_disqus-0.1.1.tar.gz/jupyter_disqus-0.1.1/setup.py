ad6414e7b85dd08fac926921c79f2cfc97427931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:05 +0000

    Add package jupyter-disqus/0.1.1/jupyter_disqus-0.1.1.tar.gz

---
+++ packages/jupyter-disqus/jupyter_disqus-0.1.1.tar.gz/jupyter_disqus-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='jupyter_disqus',
+    version='0.1.1',
+    description='Add disqus to your jupyter notebook',
+    long_description=readme,
+    author='Costa Huang',
+    author_email='costa.huang@outlook.com',
+    install_requires=['IPython', 'htmlmin==0.1.12'],
+    packages=['jupyter_disqus', 'jupyter_disqus.tests'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ],
+    url="https://github.com/vwxyzjn/jupyter_disqus"
+)
