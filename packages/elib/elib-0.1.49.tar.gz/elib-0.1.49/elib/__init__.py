001fd3744cdd612b53747605ce220133801c3742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:29 +0000

    Add package elib/0.1.49/elib-0.1.49.tar.gz

---
+++ packages/elib/elib-0.1.49.tar.gz/elib-0.1.49/elib/__init__.py
@@ -0,0 +1,23 @@
+"""
+Placeholder for future personal library
+"""
+import logging
+
+from pkg_resources import DistributionNotFound, get_distribution
+
+LOGGER = logging.getLogger('ELIB')
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.addHandler(logging.NullHandler())
+
+# pylint: disable=wrong-import-position
+from . import config, console, custom_logging, custom_random, downloader, hash_, paste, path, settings, tts
+# pylint: disable=wrong-import-position
+from ._run import run
+# pylint: disable=wrong-import-position
+from .pretty import pretty_format
+
+try:
+    __version__ = get_distribution('elib').version
+except DistributionNotFound:  # pragma: no cover
+    # package is not installed
+    __version__ = 'not installed'
