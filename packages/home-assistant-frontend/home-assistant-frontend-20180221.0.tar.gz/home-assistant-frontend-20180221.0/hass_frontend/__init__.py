98365ef3ad06101ba2ae9c93d36013c2fcdb0539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:52 +0000

    Add package home-assistant-frontend/20180221.0/home-assistant-frontend-20180221.0.tar.gz

---
+++ packages/home-assistant-frontend/home-assistant-frontend-20180221.0.tar.gz/home-assistant-frontend-20180221.0/hass_frontend/__init__.py
@@ -0,0 +1,27 @@
+"""AUTO-GENERATED. DO NOT MODIFY"""
+import os
+
+
+def where():
+    """Return path to the frontend."""
+    return os.path.dirname(__file__)
+
+VERSION = '346022c48e0e6f7b6df94bcbd76dd0649c35bc64'
+CREATED_AT = 1519187212
+FINGERPRINTS = {
+    "config": "851cd95674ac8a08e3a82d9e8617ae0d",
+    "dev-event": "0893876a9e9912f5f835f7eac53e8c10",
+    "dev-info": "c64300c53bfed0a46f96e8b7c00dfe34",
+    "dev-mqtt": "a54523ca58766ed07d96653d22eb31e1",
+    "dev-service": "74bfe97a0a2dc84d3616bec0859a03af",
+    "dev-state": "e0e77322032dcc055e752acb6c166c39",
+    "dev-template": "bc28e6e7b0ba59cdcd23e248b37622a5",
+    "hassio": "ad7aa9a6055f74475fd3ba2c00bfe512",
+    "history": "c2bc56a14c08d22f23d9763f9ea48611",
+    "iframe": "3f237a6addc81215890e8da2454da977",
+    "kiosk": "e47111bc9da3c12bc554ef415f6d912b",
+    "logbook": "ff5e32160b7e47ab10a4b609b86d04a8",
+    "mailbox": "15c8523c61f6be00cd16278384fc1404",
+    "map": "ccb633f7ba2495d221df17efc466de95",
+    "shopping-list": "7b24a3ca59e3f22040977f40b40ed890"
+}
---
+++ packages/home-assistant-frontend/home-assistant-frontend-20180221.0.tar.gz/home-assistant-frontend-20180221.0/hass_frontend_es5/__init__.py
@@ -0,0 +1,27 @@
+"""AUTO-GENERATED. DO NOT MODIFY"""
+import os
+
+
+def where():
+    """Return path to the frontend."""
+    return os.path.dirname(__file__)
+
+VERSION = '346022c48e0e6f7b6df94bcbd76dd0649c35bc64'
+CREATED_AT = 1519187212
+FINGERPRINTS = {
+    "config": "d74d0caea2b4739f1f59e94c418a3de5",
+    "dev-event": "cec0fa540ea0021090b98af39d9ef648",
+    "dev-info": "740982fe9d43dc577dcddd7e632b964e",
+    "dev-mqtt": "ebf4d71b0871b23655e68e125d9aca05",
+    "dev-service": "9104c193dda804ff5e5a6a7f4306bc0f",
+    "dev-state": "e79a4877a08a9c9c8a1cc921a8ecf631",
+    "dev-template": "1aba8db0b3d638a09919567232e615d7",
+    "hassio": "a96c12c68cd16de57d05c67e0eeca25d",
+    "history": "604a073df5dc024274e52aa2738a91d8",
+    "iframe": "185480826c02fec59d9ad045abee15f4",
+    "kiosk": "4d15ef5a5be516d512bb8a5ba879c3b0",
+    "logbook": "09a72a00b8433e952dd50e97883057eb",
+    "mailbox": "8991dc03d133845eb3e2f1cf30aa3c16",
+    "map": "787ab39a5cc73634a3cad5db41f00edd",
+    "shopping-list": "972f23af4aa731cd37132d48cfbd372d"
+}
