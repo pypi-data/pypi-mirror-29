13ebb9104567cab86e3f99dc1d2fd1b58cb7e2f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:28 +0000

    Add package cfa_zeus/3.5.0/cfa_zeus-3.5.0-py3-none-any.whl

---
+++ packages/cfa_zeus/cfa_zeus-3.5.0-py3-none-any.whl/zeus/_version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding: utf8
+# -----------------------------------------------------------------
+# zeus: _version.py
+#
+# Copyright (C) 2016-2018, Christophe Fauchard
+# -----------------------------------------------------------------
+
+__version_info__ = (3, 5, 0)
+
+__version__ = '.'.join(map(str, __version_info__))
