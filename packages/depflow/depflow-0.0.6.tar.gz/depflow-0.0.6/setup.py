13097378164690691248716ab01ef27fc5032c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:18 +0000

    Add package depflow/0.0.6/depflow-0.0.6.tar.gz

---
+++ packages/depflow/depflow-0.0.6.tar.gz/depflow-0.0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='depflow',
+    version='0.0.6',
+    author='rendaw',
+    url='https://github.com/rendaw/depflow',
+    license='BSD',
+    py_modules=['depflow'],
+)
