cedf772a9f494e1eb0dd2409d9a8184269b4d2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:04 +0000

    Add package randlemur/0.06/randlemur-0.06.tar.gz

---
+++ packages/randlemur/randlemur-0.06.tar.gz/randlemur-0.06/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'randlemur',
+  packages = ['lemur'],
+  version = '0.06',
+  description = 'High level visualization library',
+  author = 'Ryan Marren',
+  author_email = 'nkumar14@jhu.edu',
+  url = 'https://github.com/NeuroDataDesign/lemur',
+  download_url = 'https://github.com/NeuroDataDesign/lemur/archive/0.06.tar.gz',
+  keywords = ['visualization', 'neuroscience', 'embeddings'],
+  classifiers = [],
+)
