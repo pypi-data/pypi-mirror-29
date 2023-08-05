466e5dcd5a4f54e180dc4c62f64f0c411b5d7758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:36 +0000

    Add package imgaug-extension/1.0.4/imgaug_extension-1.0.4.tar.gz

---
+++ packages/imgaug-extension/imgaug_extension-1.0.4.tar.gz/imgaug_extension-1.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='imgaug_extension',
+      version='1.0.4',
+      description="Extension package for imgaug",
+      url='https://github.com/cadenai/imgaug_extension',
+      download_url='https://github.com/cadenai/imgaug_extension/archive/1.0.tar.gz',
+      author='caden.ai',
+      author_email='info@caden.ai',
+      packages=find_packages(),
+      install_requires=["imgaug", "numpy", "scikit-image>=0.13.1"]
+      )
