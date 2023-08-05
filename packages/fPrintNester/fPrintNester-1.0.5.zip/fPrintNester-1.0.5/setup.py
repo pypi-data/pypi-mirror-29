b25fc6adab3a42a684a23314ff36d7ff471d4928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:32 +0000

    Add package fPrintNester/1.0.5/fPrintNester-1.0.5.zip

---
+++ packages/fPrintNester/fPrintNester-1.0.5.zip/fPrintNester-1.0.5/setup.py
@@ -0,0 +1,32 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'fPrintNester',
+    version = '1.0.5',
+    py_modules = ['PrintNester'],
+    author = "qsl",  
+    author_email = "Qsl_xj@outlook.com",
+    url = "http://test.com",
+    description = '修改第四版本运行错误'
+    )
+    
+
+
+'''
+from setuptools import setup
+  
+setup(  
+    name = "nester",  
+    version = "1.0.0",
+    py_modules = ['nester'],
+      
+    author = "qsl",  
+    author_email = "Qsl_xj@outlook.com",
+    url = "http://test.com",
+    
+    description = 'A simple printer of nested lists'
+
+)
+
+'''
