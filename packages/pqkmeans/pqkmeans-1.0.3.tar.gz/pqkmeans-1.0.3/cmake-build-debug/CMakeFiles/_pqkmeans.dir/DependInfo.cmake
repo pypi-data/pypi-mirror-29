42ae8a7efaadf831ee6c38c027d54ef626f5ac99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:54 +0000

    Add package pqkmeans/1.0.3/pqkmeans-1.0.3.tar.gz

---
+++ packages/pqkmeans/pqkmeans-1.0.3.tar.gz/pqkmeans-1.0.3/cmake-build-debug/CMakeFiles/_pqkmeans.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/src/_pqkmeans.cpp" "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/cmake-build-debug/CMakeFiles/_pqkmeans.dir/src/_pqkmeans.cpp.o"
+  "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/src/clustering/bkmeans.cpp" "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/cmake-build-debug/CMakeFiles/_pqkmeans.dir/src/clustering/bkmeans.cpp.o"
+  "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/src/clustering/bkmeans_internal.cpp" "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/cmake-build-debug/CMakeFiles/_pqkmeans.dir/src/clustering/bkmeans_internal.cpp.o"
+  "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/src/clustering/cpp_implemented_clustering_sample.cpp" "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/cmake-build-debug/CMakeFiles/_pqkmeans.dir/src/clustering/cpp_implemented_clustering_sample.cpp.o"
+  "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/src/clustering/pqkmeans.cpp" "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/cmake-build-debug/CMakeFiles/_pqkmeans.dir/src/clustering/pqkmeans.cpp.o"
+  "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/src/encoder/encoder_sample.cpp" "/Users/keisuke_ogaki/.ghq/github.com/kogaki/pqkmeans/cmake-build-debug/CMakeFiles/_pqkmeans.dir/src/encoder/encoder_sample.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ext/pybind/include"
+  "/Users/keisuke_ogaki/.pyenv/versions/anaconda3-5.0.0/include/python3.6m"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ packages/pqkmeans/pqkmeans-1.0.3.tar.gz/pqkmeans-1.0.3/cmake-build-debug/CMakeFiles/_pqkmeans.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_pqkmeans.dir/src/_pqkmeans.cpp.o"
+  "CMakeFiles/_pqkmeans.dir/src/clustering/bkmeans.cpp.o"
+  "CMakeFiles/_pqkmeans.dir/src/clustering/bkmeans_internal.cpp.o"
+  "CMakeFiles/_pqkmeans.dir/src/clustering/cpp_implemented_clustering_sample.cpp.o"
+  "CMakeFiles/_pqkmeans.dir/src/clustering/pqkmeans.cpp.o"
+  "CMakeFiles/_pqkmeans.dir/src/encoder/encoder_sample.cpp.o"
+  "_pqkmeans.pdb"
+  "_pqkmeans.cpython-36m-darwin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_pqkmeans.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
