25a288634380ea45b83f7c821811165d4e000a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:22 +0000

    Add package streamsx/1.8.5/streamsx-1.8.5-py2.py3-none-any.whl

---
+++ packages/streamsx/streamsx-1.8.5-py2.py3-none-any.whl/streamsx/.toolkit/com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/functions.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2015,2017
+
+from __future__ import print_function
+import sys
+
+# Print function that flushes
+def print_flush(v):
+    """
+    Prints argument to stdout flushing after each tuple.
+    :returns: None
+    """
+    print(v)
+    sys.stdout.flush()
+
+def identity(t):
+    """
+    Returns its single argument.
+    :returns: Its argument.
+    """
+    return t;
+
