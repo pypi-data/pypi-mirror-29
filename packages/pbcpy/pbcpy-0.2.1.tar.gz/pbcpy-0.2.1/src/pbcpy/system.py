83ee4502f420f8b4e7ad784ac6bf888639bd5599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:25 +0000

    Add package pbcpy/0.2.1/pbcpy-0.2.1.tar.gz

---
+++ packages/pbcpy/pbcpy-0.2.1.tar.gz/pbcpy-0.2.1/src/pbcpy/system.py
@@ -0,0 +1,8 @@
+class System(object):
+
+    def __init__(self, ions, cell, name=None, field=None):
+
+        self.ions = ions
+        self.cell = cell
+        self.name = name
+        self.field = field
