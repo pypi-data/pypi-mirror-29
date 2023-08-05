4fc224b6a35b80d36291320921c485f3a5e67f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:25 +0000

    Add package helloA/0.0.1/helloA-0.0.1-py3-none-any.whl

---
+++ packages/helloA/helloA-0.0.1-py3-none-any.whl/hello.py
@@ -0,0 +1,26 @@
+"""
+The world's most awesome hello world module
+"""
+import sys
+
+__version__ = '0.0.1'
+
+def hello(name='World'):
+  """
+  Return a greeting for the given name.
+  """
+  return 'Hello, {}'.format(name)
+
+def main():
+    """
+    Reads input from args passed into the script and prints the output to stout.
+    """
+    args=sys.argv[1:]
+    name = ' '.join(args).strip()
+    if name:
+        print(hello(name))
+    else:
+        print(hello())
+
+if __name__ == '__main__':
+    main()
