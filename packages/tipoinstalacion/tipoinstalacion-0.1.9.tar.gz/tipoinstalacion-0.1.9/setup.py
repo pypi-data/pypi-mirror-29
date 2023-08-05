54d02117d0fdbd0adc7b55af736b6712e8b6272a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:11 +0000

    Add package tipoinstalacion/0.1.9/tipoinstalacion-0.1.9.tar.gz

---
+++ packages/tipoinstalacion/tipoinstalacion-0.1.9.tar.gz/tipoinstalacion-0.1.9/setup.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+import setuptools
+
+
+setuptools.setup(
+    name='tipoinstalacion',
+    version='0.1.9',
+    packages=setuptools.find_packages(),
+    url='https://github.com/gisce/tipoinstalacion',
+    license='MIT',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    description="Libreria para calcular de forma automática el Tipo de"
+                " Instalacion (TI)"
+)
