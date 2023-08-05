98d9ef522449403ad36bff8cc5af8deee0182dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package elasticbackup2/0.4.2/elasticbackup2-0.4.2.tar.gz

---
+++ packages/elasticbackup2/elasticbackup2-0.4.2.tar.gz/elasticbackup2-0.4.2/setup.py
@@ -0,0 +1,33 @@
+import sys
+
+from setuptools import setup, find_packages
+
+requirements = ['elasticsearch>=1.0.0,<1.1.0', 'tqdm==4.19.5']
+
+if sys.version_info[:2] < (2, 7):
+    requirements.append('argparse')
+    requirements.append('ordereddict')
+
+setup(
+    name='elasticbackup2',
+    version='0.4.2',
+    packages=find_packages(),
+
+    # metadata for upload to PyPI
+    author='Nick Stenning',
+    author_email='nick@whiteink.com',
+    url='https://github.com/nickstenning/elasticbackup',
+    description=('elasticbackup: tools to backup and restore ElasticSearch '
+                 'indices'),
+    license='MIT',
+    keywords='sysadmin elasticsearch backup restore',
+
+    install_requires=requirements,
+
+    entry_points={
+        'console_scripts': [
+            'elasticbackup2 = elasticbackup.backup:main',
+            'elasticrestore2 = elasticbackup.restore:main'
+        ]
+    }
+)
