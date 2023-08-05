0156edaddbd18678039e90e375e1c18aa047d511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:27 +0000

    Add package qserious/0.2.5/qserious-0.2.5.tar.gz

---
+++ packages/qserious/qserious-0.2.5.tar.gz/qserious-0.2.5/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+setup(
+  name = 'qserious',
+  packages=find_packages(exclude=['contrib', 'docs', 'tests/*']),
+  version = '0.2.5',
+  description = 'query string serializer/deserializer',
+  author = 'Jesus Germade',
+  author_email = 'jesus@germade.es',
+  url = 'https://github.com/pyKilt/qserious', # use the URL to the github repo
+  # download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['querystring', 'query', 'string', 'serializer', 'deserializer', 'serialize', 'deserialize'], # arbitrary keywords
+  classifiers = [],
+  license='MIT',
+)
