18536be23f1f14e949e8331546fc9c54fd6ac6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:13 +0000

    Add package rec-rnn-a3c/0.04/rec_rnn_a3c-0.04.tar.gz

---
+++ packages/rec-rnn-a3c/rec_rnn_a3c-0.04.tar.gz/rec_rnn_a3c-0.04/rec_rnn_a3c/src/supervised_rnn.py
@@ -0,0 +1,93 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import slim
+
+from rec_rnn_a3c.src.embedding import BloomEmbedding
+from rec_rnn_a3c.src.util import normalized_columns_initializer
+
+
+class SupervisedRNN(object):
+    def __init__(self, optimizer, params, scope):
+        self.scope = scope
+
+        with tf.variable_scope(self.scope):
+            self.optimizer = optimizer
+
+            self.item_dim = params['item_dim']
+            self.output_dim = params['output_dim']
+            self.hidden_dim = params['hidden_dim']
+            self.batch_dim = params['batch_dim']
+            self.unfold_dim = params['unfold_dim']
+            self.embedding_dim = params['embedding_dim']
+            self.compression_ratio = params['compression_ratio']
+
+            bloom_embedding = BloomEmbedding(self.item_dim, compression_ratio=self.compression_ratio)
+
+            self.input = tf.placeholder(tf.int32, shape=[None, None], name='input')
+
+            ####### Bloom Embeddings #######
+            self.hashed_input = bloom_embedding.forward(self.input)
+            with tf.device("/cpu:0"):
+                E_i = tf.get_variable(name="input_embeddings", shape=[self.embedding_dim, self.hidden_dim])
+                input = tf.nn.embedding_lookup(params=E_i, ids=self.hashed_input)
+            input = tf.reduce_sum(input, axis=2)
+
+            ####### RNN Cells #######
+            rnn_cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=self.hidden_dim, state_is_tuple=True)
+            rnn_cell = tf.nn.rnn_cell.DropoutWrapper(rnn_cell, output_keep_prob=0.5)
+
+            c_init = np.zeros((self.batch_dim, rnn_cell.state_size.c), np.float32)
+            h_init = np.zeros((self.batch_dim, rnn_cell.state_size.h), np.float32)
+            self.rnn_zero_state = [c_init, h_init]
+
+            self.c_input = tf.placeholder(tf.float32, [None, rnn_cell.state_size.c], name='c_input')
+            self.h_input = tf.placeholder(tf.float32, [None, rnn_cell.state_size.h], name='h_input')
+            state_input = tf.nn.rnn_cell.LSTMStateTuple(self.c_input, self.h_input)
+
+            rnn_output, (rnn_c, rnn_h) = tf.nn.dynamic_rnn(
+                inputs=input,
+                cell=rnn_cell,
+                dtype=tf.float32,
+                initial_state=state_input,
+            )
+
+            self.state_output = (rnn_c, rnn_h)
+
+            self.logit_dist = slim.fully_connected(
+                inputs=rnn_output,
+                num_outputs=self.embedding_dim,
+                activation_fn=None,
+                weights_initializer=normalized_columns_initializer(0.01),
+                biases_initializer=None
+            )
+            self.prob_dist = tf.nn.softmax(self.logit_dist)
+            self.sigmoids = tf.nn.sigmoid(self.logit_dist)
+
+            ####### Likelihoods #######
+            hashed_indices = bloom_embedding.forward(tf.range(self.item_dim))
+            hashed_likelihoods = tf.gather(
+                params=tf.reshape(self.sigmoids, [-1, self.embedding_dim]),
+                indices=hashed_indices,
+                axis=1)
+            self.likelihoods = tf.reduce_prod(hashed_likelihoods, axis=2)
+            self.likelihoods = tf.reshape(self.likelihoods, shape=[-1, self.unfold_dim, self.item_dim])
+            self.predictions = tf.argmax(self.likelihoods, axis=2)
+
+            self.labels = tf.placeholder(tf.int32, [self.batch_dim, None], name='target')
+
+            self.hashed_labels = bloom_embedding.forward(self.labels)
+            self.hashed_labels = tf.one_hot(self.hashed_labels, self.embedding_dim, axis=2)
+            self.hashed_labels = tf.reduce_sum(self.hashed_labels, axis=3)
+
+            ####### Loss #######
+            self.loss = tf.nn.sigmoid_cross_entropy_with_logits(
+                logits=self.logit_dist,
+                labels=self.hashed_labels
+            )
+            self.loss = tf.reduce_mean(self.loss, axis=0)
+            self.loss = tf.reduce_sum(self.loss)
+
+            trainable_vars = tf.trainable_variables()
+            grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, trainable_vars), 40.0)
+            self.optimizer = tf.train.AdamOptimizer(0.1)
+            self.train_op = self.optimizer.apply_gradients(zip(grads, trainable_vars))
---
+++ packages/rec-rnn-a3c/rec_rnn_a3c-0.04.tar.gz/rec_rnn_a3c-0.04/rec_rnn_a3c/src/supervised_rnn_model.py
@@ -0,0 +1,134 @@
+import numpy as np
+import tensorflow as tf
+
+from rec_rnn_a3c.src.supervised_rnn import SupervisedRNN
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+class SupervisedRNNModel(object):
+    def __init__(self, optimizer, params, scope='supervised_rnn'):
+        self.optimizer = optimizer
+
+        self.params = params
+
+        self.unfold_dim = params['unfold_dim']
+
+        self.reward_network = SupervisedRNN(optimizer=optimizer, params=params, scope=scope)
+        rnn_state_c, rnn_state_h = self.reward_network.rnn_zero_state
+        self.rnn_state_c = rnn_state_c
+        self.rnn_state_h = rnn_state_h
+
+        self.files = tf.placeholder(tf.string, shape=[None])
+
+    def predict(self, seq, sess):
+        feed_dict = {
+            self.reward_network.input: seq,
+            self.reward_network.c_input: self.rnn_state_c,
+            self.reward_network.h_input: self.rnn_state_h,
+        }
+
+        fetches = [self.reward_network.predictions]
+
+        predictions = sess.run(feed_dict=feed_dict, fetches=fetches)
+
+        #ction_probs = np.max(likelihoods, axis=2)
+        #est_actions = np.argmax(likelihoods, axis=2)
+
+        return predictions
+
+    def eval(self, sess, iterator):
+        next_element = iterator.get_next()
+
+        total_loss = 0.0
+        total_steps = 0
+        while True:
+            try:
+                total_steps += 1
+                step_loss = 0.0
+                sequence, label_sequence = sess.run(next_element)
+
+                num_sequence_splits = np.max([1, np.shape(sequence)[1] // self.unfold_dim])
+
+                for split in range(num_sequence_splits):
+                    seq_split = sequence[:, split * self.unfold_dim:(split + 1) * self.unfold_dim]
+                    label_seq_split = label_sequence[:, split * self.unfold_dim:(split + 1) * self.unfold_dim]
+
+                    feed_dict = {
+                        self.reward_network.input: seq_split,
+                        self.reward_network.c_input: self.rnn_state_c,
+                        self.reward_network.h_input: self.rnn_state_h,
+                        self.reward_network.labels: label_seq_split
+                    }
+
+                    fetches = [
+                        self.reward_network.state_output,
+                        self.reward_network.logit_dist,
+                        self.reward_network.prob_dist,
+                        self.reward_network.loss,
+                    ]
+
+                    (self.rnn_state_c, self.rnn_state_h), logit_dist, prob_dist, loss = sess.run(
+                        feed_dict=feed_dict,
+                        fetches=fetches
+                    )
+
+                    step_loss += loss
+
+                total_loss += step_loss / num_sequence_splits
+            except tf.errors.OutOfRangeError:
+                break
+
+        tf.logging.info("Evaluation -- Error: %s " % (total_loss / total_steps))
+
+    def fit(self, sess, iterator, iteration, num_iterations, num_steps, saver=None, save_path=None):
+        next_element = iterator.get_next()
+
+        total_loss = 0.0
+        for step in range(num_steps):
+            try:
+                step += 1
+                step_loss = 0.0
+                # TODO: Possible without feeding? (Improves performance)
+                sequence, label_sequence = sess.run(next_element)
+
+                num_sequence_splits = np.max([1, np.shape(sequence)[1] // self.unfold_dim])
+
+                for split in range(num_sequence_splits):
+                    seq_split = sequence[:, split * self.unfold_dim:(split + 1) * self.unfold_dim]
+                    label_seq_split = label_sequence[:, split * self.unfold_dim:(split + 1) * self.unfold_dim]
+
+                    feed_dict = {
+                        self.reward_network.input: seq_split,
+                        self.reward_network.c_input: self.rnn_state_c,
+                        self.reward_network.h_input: self.rnn_state_h,
+                        self.reward_network.labels: label_seq_split
+                    }
+
+                    fetches = [
+                        self.reward_network.state_output,
+                        self.reward_network.logit_dist,
+                        self.reward_network.prob_dist,
+                        self.reward_network.loss,
+                        self.reward_network.train_op,
+                    ]
+
+                    (self.rnn_state_c, self.rnn_state_h), logit_dist, prob_dist, loss, _ = sess.run(
+                        feed_dict=feed_dict,
+                        fetches=fetches
+                    )
+
+                    step_loss += loss
+
+                total_loss += step_loss / num_sequence_splits
+            except tf.errors.OutOfRangeError:
+                break
+
+            tf.logging.info(
+                "Episode [%d/%d] -- Step [%d/%d] -- Error: %s "
+                % (iteration, num_iterations, step, num_steps, total_loss / step))
+
+            if step % 10 == 0:
+                #saver.save(sess, save_path, global_step=iteration * num_steps + step)
+                saver.save(sess, save_path)
+                tf.logging.info("Saved model to: %s" % save_path)
