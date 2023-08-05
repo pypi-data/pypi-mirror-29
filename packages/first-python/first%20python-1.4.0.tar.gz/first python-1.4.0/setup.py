55908e8e52e0b232e8f2a01c39703e399231ec86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:07 +0000

    Add package first-python/1.4.0/first%20python-1.4.0.tar.gz

---
+++ packages/first-python/first%20python-1.4.0.tar.gz/first python-1.4.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name  = 'first python',
+        version = '1.4.0',
+        py_modules = ['nester'],
+        author = 'hongqi1029',
+        author_email = 'hongqi1029@126.com',
+        url = 'http://wwww.nchu.edu.cn',
+        description = ' A simple printer of nested lists',
+
+)
