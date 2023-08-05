d50a855c51f283198c99f559008cac17b1e0175a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:23 +0000

    Add package resolwe-bio/6.2.2a1/resolwe_bio-6.2.2a1-py3-none-any.whl

---
+++ packages/resolwe-bio/resolwe_bio-6.2.2a1-py3-none-any.whl/resolwe_bio/tools/cufflinks_sample_groups.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+# pylint: disable=missing-docstring,invalid-name
+# XXX: Refactor to a comand line tool and remove pylint disable
+"""Prepare groups and replicates info."""
+from __future__ import absolute_import, division, print_function
+
+import argparse
+from collections import defaultdict
+
+parser = argparse.ArgumentParser(description="Prepare groups and replicates info")
+parser.add_argument('samples', help="All samples")
+parser.add_argument('replicates', help="Define replicates and groups")
+args = parser.parse_args()
+
+samples = args.samples.split(",")
+replicates = args.replicates.split(",")
+
+d = defaultdict(list)
+for s, r in zip(samples, replicates):
+    d[r].append(s)
+
+return_string = ""
+for x in sorted(d):
+    return_string = return_string + ",".join(d[x]) + " "
+
+print(return_string)
