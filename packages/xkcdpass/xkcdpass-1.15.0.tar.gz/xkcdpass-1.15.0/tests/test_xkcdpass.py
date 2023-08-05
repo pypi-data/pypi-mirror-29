e3f7de300519b55d4e2fa62ed0d482f2435c1a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:28 +0000

    Add package xkcdpass/1.15.0/xkcdpass-1.15.0.tar.gz

---
+++ packages/xkcdpass/xkcdpass-1.15.0.tar.gz/xkcdpass-1.15.0/tests/test_xkcdpass.py
@@ -0,0 +1,71 @@
+import re
+import subprocess
+import sys
+import unittest
+
+from xkcdpass import xkcd_password
+
+WORDFILE = 'xkcdpass/static/legacy'
+
+
+class XkcdPasswordTests(unittest.TestCase):
+    def setUp(self):
+        self.wordlist_full = xkcd_password.generate_wordlist(
+            wordfile=WORDFILE,
+            min_length=5,
+            max_length=8,)
+        self.wordlist_small = xkcd_password.generate_wordlist(
+            wordfile='tests/test_list.txt',
+            valid_chars='[a-z]')
+
+    def test_loadwordfile(self):
+        self.assertEqual(len(self.wordlist_full), 29611)
+
+    def test_regex(self):
+        self.assertNotIn("__$$$__", self.wordlist_small)
+
+    def test_acrostic(self):
+        word = "face"
+        result = xkcd_password.generate_xkcdpassword(
+            self.wordlist_small,
+            acrostic=word)
+        self.assertEqual("".join(map(lambda x: x[0], result.split())), word)
+
+    def test_commandlineCount(self):
+        count = 5
+        result = subprocess.check_output([
+            sys.executable, "xkcdpass/xkcd_password.py",
+            "-w", WORDFILE,
+            "-c", str(count),
+        ])
+        self.assertTrue(result.count(b"\n"), count)
+
+    def test_delim(self):
+        tdelim = "_"
+        result = xkcd_password.generate_xkcdpassword(
+            self.wordlist_small,
+            delimiter=tdelim)
+        self.assertIsNotNone(re.match('([a-z]+(_|$))+', result))
+
+    def test_separator(self):
+        count = 3
+        result = subprocess.check_output(
+            ["python", "xkcdpass/xkcd_password.py",
+             "--count", str(count),
+             "--delimiter", "|",
+             "--separator", " "])
+        self.assertEqual(result.count(b" "), 3)
+
+    def test_separator_no_end(self):
+        "Pipe output to other program. e.g. `xkcdpass -c 1 -s "" | xsel -b`"
+        count = 1
+        result = subprocess.check_output(
+            ["python", "xkcdpass/xkcd_password.py",
+             "--count", str(count),
+             "--separator", ""])
+        self.assertEqual(result.find(b"\n"), -1)
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(XkcdPasswordTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
