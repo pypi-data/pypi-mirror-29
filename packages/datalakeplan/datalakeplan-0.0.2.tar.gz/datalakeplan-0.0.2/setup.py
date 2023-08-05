4e2330485f662f18a29efd48014d15f3d1808c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:35 +0000

    Add package datalakeplan/0.0.2/datalakeplan-0.0.2.tar.gz

---
+++ packages/datalakeplan/datalakeplan-0.0.2.tar.gz/datalakeplan-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'datalakeplan',
+    packages = ['datalakeplan'],
+    version = '0.0.2',  # Ideally should be same as your GitHub release tag varsion
+    description = 'data lake plan vault excel',
+    author = 'musicnova',
+    author_email = 'musicnova@yandex.ru',
+    url = 'https://github.com/musicnova/DataLakePlan',
+    download_url = 'https://github.com/musicnova/DataLakePlan/archive/0.0.2.tar.gz',
+    keywords = ['data', 'lake'],
+    classifiers = [],
+)
