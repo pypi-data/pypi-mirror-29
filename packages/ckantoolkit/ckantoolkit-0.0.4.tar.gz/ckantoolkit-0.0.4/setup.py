89b27882fd816b3a5e8c0132cc1189b7a50f51e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:47 +0000

    Add package ckantoolkit/0.0.4/ckantoolkit-0.0.4.tar.gz

---
+++ packages/ckantoolkit/ckantoolkit-0.0.4.tar.gz/ckantoolkit-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+version = '0.0.4'
+
+setup(
+    name='ckantoolkit',
+    version=version,
+    description="",
+    long_description="""
+    """,
+    classifiers=[],
+    keywords='',
+    author='Ian Ward',
+    author_email='ian@excess.org',
+    url='https://github.com/ckan/ckantoolkit',
+    license='AGPL',
+    packages=['ckantoolkit'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+)
