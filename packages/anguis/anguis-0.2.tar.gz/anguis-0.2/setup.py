4d21a7cc15a6af0782a57c459f6e6b94bc4a8ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:00 +0000

    Add package anguis/0.2/anguis-0.2.tar.gz

---
+++ packages/anguis/anguis-0.2.tar.gz/anguis-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name = 'anguis',
+    version = '0.2',
+    description = 'A generic key-store library',
+    packages = [ 'anguis' ],
+    author = 'Roberto Reale',
+    author_email = 'rober.reale@gmail.com',
+    url = 'https://github.com/robertoreale/anguis',
+    keywords = [ 'cache', 'key-value store' ],
+    install_requires = [
+        'gitpython',
+        'paramiko',
+        'python-etcd',
+        'redis'
+    ],
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
+)
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
