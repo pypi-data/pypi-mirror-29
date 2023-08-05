e0ea0e969273b5e26ebcafca7b3c3a1e4d4aede8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:35 +0000

    Add package django-automated-logging/3.1.0/django-automated-logging-3.1.0.tar.gz

---
+++ packages/django-automated-logging/django-automated-logging-3.1.0.tar.gz/django-automated-logging-3.1.0/automated_logging/settings.py
@@ -0,0 +1,26 @@
+from django.conf import settings as st
+from logging import INFO
+
+DEFAULT_AUTOMATED_LOGGING = {
+    'exclude': ['Session', 'automated_logging', 'basehttp'],
+    'modules': ['request', 'model'],
+    'to_database': True,
+    'loglevel': {'model': INFO,
+                 'request': INFO}
+}
+
+
+def auto_complete(setting, default):
+  for k, v in default.items():
+    if k not in setting.keys():
+      setting[k] = v
+    elif isinstance(setting[k], dict):
+      setting[k] = auto_complete(setting[k], default[k])
+
+  return setting
+
+
+if hasattr(st, 'AUTOMATED_LOGGING'):
+  AUTOMATED_LOGGING = auto_complete(st.AUTOMATED_LOGGING, DEFAULT_AUTOMATED_LOGGING)
+else:
+  AUTOMATED_LOGGING = DEFAULT_AUTOMATED_LOGGING
