2c5ecf367619ae12c684d6a41939b49b673ad52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:45 +0000

    Add package dummy_opentracing/1.0/dummy_opentracing-1.0.tar.gz

---
+++ packages/dummy_opentracing/dummy_opentracing-1.0.tar.gz/dummy_opentracing-1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+version = '1.0'
+setup(
+    name='dummy_opentracing',
+    version=version,
+    url='https://github.com/carlosalberto/opentracing-dummy/',
+    download_url='https://github.com/carlosalberto/opentracing-dummy/tarball/'+version,
+    license='BSD',
+    author='Carlos Alberto Cortez',
+    author_email='calberto.cortez@gmail.com',
+    description='OpenTracing Dummy package',
+    long_description=open('README.rst').read(),
+    packages=['dummy_opentracing'],
+    platforms='any',
+    install_requires=[
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
