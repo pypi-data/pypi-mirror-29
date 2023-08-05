b7c63d2493342781d8983ff2a197399adfb8c613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:48 +0000

    Add package synamic/0.2.dev1/synamic-0.2.dev1.tar.gz

---
+++ packages/synamic/synamic-0.2.dev1.tar.gz/synamic-0.2.dev1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='synamic',
+    version='0.2.dev1',
+    description='Synamic',
+    author='Md. Sabuj Sarker',
+    author_email='md.sabuj.sarker@gmail.com',
+    url='https://github.com/SabujXi/Synamic',
+    packages=find_packages('src', exclude=('synamic',)),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'synamic = synamic.entry_points.synamic_shell:main'
+        ]
+    },
+    data_files=[('', ['README.md', 'LICENSE'])],
+    install_requires=[
+        'Jinja2>=2.10',
+        'MarkupSafe>=1.0',
+        'mistune>=0.8.3',
+        'ruamel.yaml>=0.15.34'
+    ],
+    python_requires='>=3.5',
+)
+
+# print(find_packages('src', exclude=('synamic',)))
