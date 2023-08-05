267955ad3bed447ce4e8b94ba8e7b5a7ee30bbc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:47 +0000

    Add package release-exporter/1.0.5/release-exporter-1.0.5.tar.gz

---
+++ packages/release-exporter/release-exporter-1.0.5.tar.gz/release-exporter-1.0.5/release_exporter/cli.py
@@ -0,0 +1,107 @@
+import os
+
+import click
+
+from release_exporter.exceptions import UnknownRepo
+from release_exporter.formatter import github
+from release_exporter.formatter import gitlab
+from release_exporter.utils import get_repo_url_info
+from release_exporter.version import __version__
+
+
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(__version__)
+    ctx.exit()
+
+
+@click.group()
+@click.option('--token', help='Token number if its a private repository.', default=None)
+@click.option('--url',
+              help="URL of your repository. This is optional if your current directory has .git folder with remote url.",
+              default=None)
+@click.option('--location', help='Local location of your repository.', default=os.getcwd())
+@click.option('--version', is_flag=True, callback=print_version, expose_value=False, is_eager=True)
+@click.pass_context
+def cli(ctx, token, url, location):
+    ctx.obj['token'] = token
+    ctx.obj['repo_url'] = url
+    ctx.obj['location'] = location
+
+
+# TODO: Implement init as a go to token area.
+# @cli.command(help='Creates .rex file.')
+# def init():
+#     pass
+
+
+@cli.command(help='Creates markdown file.')
+@click.pass_context
+def markdown(ctx):
+    if os.name == 'nt':
+        ctx.obj['location'] = r'{}'.format(ctx.obj['location'])
+
+    if "github" in get_repo_url_info(location=ctx.obj['location'], repo_url=ctx.obj['repo_url']).resource:
+        print('GitHub detected. \n')
+
+        github(force=True, token=ctx.obj['token'], location=ctx.obj['location'], repo_url=ctx.obj['repo_url'],
+               file_type='markdown').write_markdown()
+
+    elif "gitlab" in get_repo_url_info(location=ctx.obj['location'], repo_url=ctx.obj['repo_url']).resource:
+        print('GitLab detected. \n')
+
+        gitlab(force=True, token=ctx.obj['token'], location=ctx.obj['location'], repo_url=ctx.obj['repo_url'],
+               file_type='markdown').write_markdown()
+
+    else:
+        raise UnknownRepo("Sorry, couldn't find the repository. Trying giving the repository URL by adding --url flag.")
+
+
+@cli.command(help='Creates JSON file.')
+@click.pass_context
+def json(ctx):
+    if os.name == 'nt':
+        ctx.obj['location'] = r'{}'.format(ctx.obj['location'])
+
+    if "github" in get_repo_url_info(location=ctx.obj['location'], repo_url=ctx.obj['repo_url']).resource:
+        click.echo('GitHub detected. \n')
+
+        github(force=True, token=ctx.obj['token'], location=ctx.obj['location'], repo_url=ctx.obj['repo_url'],
+               file_type='json').write_json()
+
+    elif "gitlab" in get_repo_url_info(location=ctx.obj['location'], repo_url=ctx.obj['repo_url']).resource:
+        click.echo('GitLab detected. \n')
+
+        gitlab(force=True, token=ctx.obj['token'], location=ctx.obj['location'], repo_url=ctx.obj['repo_url'],
+               file_type='json').write_json()
+
+    else:
+        raise UnknownRepo("Sorry, couldn't find the repository. Trying giving the repository URL by adding --url flag.")
+
+
+@cli.command(help='Creates reStructuredText file. Coming soon.')
+@click.pass_context
+def rest(ctx):
+    # if os.name == 'nt':
+    #     ctx.obj['location'] = r'{}'.format(ctx.obj['location'])
+    #
+    # if "github" in get_repo_url_info(location=ctx.obj['location'], repo_url=ctx.obj['repo_url']).resource:
+    #     click.echo('GitHub detected. \n')
+    #
+    #     github(force=True, token=ctx.obj['token'], location=ctx.obj['location'], repo_url=ctx.obj['repo_url'],
+    #            file_type='rest').write_json()
+    #
+    # elif "gitlab" in get_repo_url_info(location=ctx.obj['location'], repo_url=ctx.obj['repo_url']).resource:
+    #     click.echo('GitLab detected. \n')
+    #
+    #     gitlab(force=True, token=ctx.obj['token'], location=ctx.obj['location'], repo_url=ctx.obj['repo_url'],
+    #            file_type='rest').write_json()
+    #
+    # else:
+    #     raise UnknownRepo("Sorry, couldn't find the repository. Trying giving the repository URL by adding --url flag.")
+    raise NotImplementedError("Coming soon.")
+
+
+def main():
+    cli(obj={})
---
+++ packages/release-exporter/release-exporter-1.0.5.tar.gz/release-exporter-1.0.5/setup.py
@@ -0,0 +1,41 @@
+import codecs
+import os
+from setuptools import setup
+
+from release_exporter.version import __version__
+
+here = os.path.abspath(os.path.dirname(__file__)) + os.sep
+
+
+def get_requirements(*parts):
+    return codecs.open(os.path.join(here, *parts), 'r').read().splitlines()
+
+
+setup(
+    name='release-exporter',
+    version=__version__,
+    install_requires=get_requirements('requirements.txt'),
+    packages=['tests', 'release_exporter'],
+    url='https://blog.gollahalli.com/export-your-github-and-gitlab-releases-as-a-changelog/',
+    license='MIT',
+    author='Akshay Raj Gollahalli',
+    author_email='akshay@gollahalli.com',
+    description='Release exporter for GitHub and GitLab.',
+    long_description=open('README.rst').read(),
+    keywords=['changelog', 'releases'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Utilities'
+    ],
+    entry_points={
+        'console_scripts': [
+            'rex = release_exporter.cli:main'
+        ]
+    }
+)
