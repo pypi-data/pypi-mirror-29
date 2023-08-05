8c1b1c78dddc029fc438d873f428623b9f11a3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:07 +0000

    Add package fundamentals/1.5.10/fundamentals-1.5.10.tar.gz

---
+++ packages/fundamentals/fundamentals-1.5.10.tar.gz/fundamentals-1.5.10/fundamentals/fmultiprocess.py
@@ -0,0 +1,79 @@
+#!/usr/local/bin/python
+# encoding: utf-8
+"""
+*A function to quickly add multiprocessing to any program*
+
+:Author:
+    David Young
+
+:Date Created:
+    November  9, 2017
+"""
+################# GLOBAL IMPORTS ####################
+import sys
+import os
+os.environ['TERM'] = 'vt100'
+from fundamentals import tools
+from multiprocess import cpu_count, Pool
+from functools import partial
+
+
+def fmultiprocess(
+        log,
+        function,
+        inputArray,
+        poolSize=False,
+        **kwargs):
+    """multiprocess pool
+
+    **Key Arguments:**
+        - ``log`` -- logger
+        - ``function`` -- the function to multiprocess
+        - ``inputArray`` -- the array to be iterated over
+
+    **Return:**
+        - ``resultArray`` -- the array of results
+
+    **Usage:**
+
+        .. code-block:: python 
+
+            from fundamentals import multiprocess
+            # DEFINE AN INPUT ARRAY
+            inputArray = range(10000)
+            results = multiprocess(log=log, function=functionName,
+                                  inputArray=inputArray, otherFunctionKeyword="cheese")
+    """
+    log.info('starting the ``multiprocess`` function')
+
+    # DEFINTE POOL SIZE - NUMBER OF CPU CORES TO USE (BEST = ALL - 1)
+    # if cpu_count() > 1:
+    #     poolSize = cpu_count() - 1
+    # else:
+    #     poolSize = 1
+
+    # if len(inputArray) < poolSize:
+    #     poolSize = len(inputArray)
+    if poolSize:
+        p = Pool(processes=poolSize)
+    else:
+        p = Pool()
+
+    # MAP-REDUCE THE WORK OVER MULTIPLE CPU CORES
+    try:
+        mapfunc = partial(function, log=log, **kwargs)
+        resultArray = p.map(mapfunc, inputArray)
+    except:
+        try:
+            mapfunc = partial(function, **kwargs)
+            resultArray = p.map(mapfunc, inputArray)
+        except:
+            mapfunc = partial(function, log=log, **kwargs)
+            resultArray = p.map(mapfunc, inputArray)
+
+    p.close()
+    p.terminate()
+    p.join()
+
+    log.info('completed the ``multiprocess`` function')
+    return resultArray
