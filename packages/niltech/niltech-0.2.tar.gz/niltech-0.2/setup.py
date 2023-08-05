73d9ce61e209d4b6d550b2be19ecb2e02e5ba8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:36 +0000

    Add package niltech/0.2/niltech-0.2.tar.gz

---
+++ packages/niltech/niltech-0.2.tar.gz/niltech-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'niltech',
+  packages = ['niltech'], # this must be the same as the name above
+  version = '0.2',
+  description = 'A various purpose library intended to be used in Raspberry pi workshops',
+  author = 'Vaibhav VK - Niltech',
+  author_email = 'vaibhav@niltech.in',
+  #url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  #download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['Arduino','getPort','readData','extractData','read','DHT11.read','Arduino.getPort','Arduino.readData','Arduino.extractData'], # arbitrary keywords
+  classifiers = [],
+)
