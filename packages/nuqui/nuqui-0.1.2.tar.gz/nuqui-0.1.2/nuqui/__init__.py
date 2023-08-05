5808d6ceb969f310a92e35130dc04f4246ccc64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:48 +0000

    Add package nuqui/0.1.2/nuqui-0.1.2.tar.gz

---
+++ packages/nuqui/nuqui-0.1.2.tar.gz/nuqui-0.1.2/nuqui/__init__.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .dbobjects import Base, Question, User, Meal, Score
+import pkg_resources
+
+db_path = pkg_resources.resource_filename('nuqui', 'data/nuqui.db')
+engine = create_engine('sqlite:///'+db_path)
+Base.metadata.bind = engine
+SESSION = sessionmaker(bind=engine)
+Base.metadata.create_all()
+session = SESSION()
+session.close()
+
+from .nuqui import remove_user, create_user, add_meal, evaluate, get_predefined_question_dict_with_random_answers
