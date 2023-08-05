0381426fda56a77a445393a6118d4cf1a827e4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package kinet/0.1.0/kinet-0.1.0.tar.gz

---
+++ packages/kinet/kinet-0.1.0.tar.gz/kinet-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+ 
+setup(
+    name = 'kinet',
+    description = 'library for controlling color kinetics lights',
+    version = '0.1.0',
+    packages = ['kinet'],
+    author='Nchinda2',
+    author_email='nchinda2@gmail.com',
+    url='https://github.com/Firescar96/kinet',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Environment :: Console'
+    ]
+)
