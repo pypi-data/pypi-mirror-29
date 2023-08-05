67036667e8446e614670b0b3b3f60f59ff353fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package climlab/0.6.3.dev1/climlab-0.6.3.dev1.tar.gz

---
+++ packages/climlab/climlab-0.6.3.dev1.tar.gz/climlab-0.6.3.dev1/climlab/__init__.py
@@ -0,0 +1,23 @@
+'''
+This chapter documents the source code of the ``climlab`` package.
+The focus is on the methods and functions that the user invokes
+while using the package.
+
+Nevertheless also the underlying code of the ``climlab`` architecture
+has been documented for a comprehensive understanding and traceability.
+'''
+
+__version__ = '0.6.3.dev1'
+
+# this should ensure that we can still import constants.py as climlab.constants
+from climlab.utils import constants
+from climlab.utils import thermo, legendre
+# some more useful shorcuts
+from climlab.model.column import GreyRadiationModel, RadiativeConvectiveModel, BandRCModel
+from climlab.model.ebm import EBM, EBM_annual, EBM_seasonal
+from climlab.domain.field import Field, global_mean
+from climlab.domain.axis import Axis
+from climlab.domain.initial import column_state, surface_state
+from climlab.process import Process, TimeDependentProcess, ImplicitProcess, DiagnosticProcess, EnergyBudget
+from climlab.process import process_like, get_axes
+from climlab.domain.xarray import to_xarray
