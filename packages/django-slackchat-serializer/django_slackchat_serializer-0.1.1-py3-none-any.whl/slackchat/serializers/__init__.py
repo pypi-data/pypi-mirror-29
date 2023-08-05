4c96d0947c41ab7a777193c2dc7577ee86ed0b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:10 +0000

    Add package django-slackchat-serializer/0.1.1/django_slackchat_serializer-0.1.1-py3-none-any.whl

---
+++ packages/django-slackchat-serializer/django_slackchat_serializer-0.1.1-py3-none-any.whl/slackchat/serializers/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+from .attachment import AttachmentSerializer
+from .channel import ChannelSerializer
+from .message import MessageSerializer
+from .reaction import ReactionSerializer
+from .user import UserSerializer
