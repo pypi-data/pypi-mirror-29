8d0e1c4f81920ac666449ff2c13eb3aa5262b00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:13 +0000

    Add package bancardconnectorpython/0.1/bancardconnectorpython-0.1.tar.gz

---
+++ packages/bancardconnectorpython/bancardconnectorpython-0.1.tar.gz/bancardconnectorpython-0.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+
+setup(
+	name='bancardconnectorpython',
+	version='0.1',
+	author='Victor Cajes',
+	author_email='vcajes@gmail.com',
+	packages=['bancardconnectorpython'],
+	scripts=[],
+	url='https://github.com/vcajes/bancard-connector-python',
+	license='MIT License',
+	download_url='https://github.com/vcajes/bancard-connector-python/archive/0.1.tar.gz',
+	description='The Bancard Python connector provides Python APIs to create, process and manage payments.',
+	long_description='The Bancard Python connector provides Python APIs to create, process and manage payments.',
+	package_data={'bancardconnectorpython': []},
+	install_requires=['requests>=2.18.4'],
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: Implementation :: PyPy',
+		'Topic :: Software Development :: Libraries :: Python Modules'
+	],
+	keywords=['bancard', 'python', 'rest', 'sdk', 'charges', 'webhook']
+)
