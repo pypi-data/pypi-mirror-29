691159c7e2dd4e8451437b7a3beee8f9db278fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:46 +0000

    Add package Flask-JWT-Extended/3.7.0/Flask-JWT-Extended-3.7.0.tar.gz

---
+++ packages/Flask-JWT-Extended/Flask-JWT-Extended-3.7.0.tar.gz/Flask-JWT-Extended-3.7.0/setup.py
@@ -0,0 +1,42 @@
+"""
+Flask-JWT-Extended
+-------------------
+Flask-Login provides jwt endpoint protection for Flask.
+"""
+from setuptools import setup
+
+setup(name='Flask-JWT-Extended',
+      version='3.7.0',
+      url='https://github.com/vimalloc/flask-jwt-extended',
+      license='MIT',
+      author='Landon Gilbert-Bland',
+      author_email='landogbland@gmail.com',
+      description='Extended JWT integration with Flask',
+      long_description='Extended JWT integration with Flask',
+      keywords=['flask', 'jwt', 'json web token'],
+      packages=['flask_jwt_extended'],
+      zip_safe=False,
+      platforms='any',
+      install_requires=[
+          'Werkzeug>=0.14',  # needed for samestie cookie functionality
+          'Flask',
+          'PyJWT',
+      ],
+      extras_require={
+        'asymmetric_crypto':  ["cryptography"]
+      },
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ])
