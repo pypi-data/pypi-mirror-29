133e03ed13cded611bc76544c485ca6f3dca7ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:05 +0000

    Add package laniakea/0.8/laniakea-0.8-py2.py3-none-any.whl

---
+++ packages/laniakea/laniakea-0.8-py2.py3-none-any.whl/laniakea/userdata/default.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+# Essentials
+apt-get --yes --quiet update
+apt-get --yes --quiet install python python-pip python-dev git puppet-common build-essential
+
+# Fetch puppet configuration
+# mv /etc/puppet /etc/puppet.orig
+# git clone https://bitbucket.org/rimey/hello-ec2-puppetboot.git /etc/puppet
+
+# Run puppet
+# puppet apply /etc/puppet/manifests/init.pp
+
+# Run fuzzers
