b90c170bcd84de9dccc756c8a999b880522ad197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package elib/0.1.25/elib-0.1.25-py36-none-any.whl

---
+++ packages/elib/elib-0.1.25-py36-none-any.whl/elib/__init__.py
@@ -0,0 +1,15 @@
+"""
+Placeholder for future personal library
+"""
+from pkg_resources import DistributionNotFound, get_distribution
+
+from . import config, console, custom_logging, custom_random, paste, path, tts
+
+try:
+    __version__ = get_distribution('esst').version
+except DistributionNotFound:  # pragma: no cover
+    # package is not installed
+    __version__ = 'not installed'
+
+MAIN_LOGGER = custom_logging.get_logger('ELIB')
+MAIN_LOGGER.info(f'ELIB version {__version__}')
