3cd29232adea32aa43e661c38c163a48decb7be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:09 +0000

    Add package toga-core/0.3.0.dev5/toga-core-0.3.0.dev5.tar.gz

---
+++ packages/toga-core/toga-core-0.3.0.dev5.tar.gz/toga-core-0.3.0.dev5/toga/widgets/progressbar.py
@@ -0,0 +1,93 @@
+from .base import Widget
+
+
+class ProgressBar(Widget):
+    """
+    """
+    MIN_WIDTH = 100
+
+    def __init__(self,
+                 id=None,
+                 style=None,
+                 max=1,
+                 value=0,
+                 running=False,
+                 factory=None):
+        """
+
+        Args:
+            id (str):  An identifier for this widget.
+            style (:obj:`Style`): An optional style object. If no style is provided then a
+                new one will be created for the widget.
+            max (float): The maximum value of the progressbar.
+            value (float): To define the current progress of the progressbar.
+            running (bool): Set the inital running mode.
+            factory (:obj:`module`): A python module that is capable to return a
+                implementation of this class with the same name. (optional & normally not needed)
+        """
+        super().__init__(id=id, style=style, factory=factory)
+
+        self._is_running = False
+        self._impl = self.factory.ProgressBar(interface=self)
+        self._value = value
+
+        self.max = max
+
+        if running:
+            self.start()
+        else:
+            self.stop()
+
+        self.value = value
+
+    @property
+    def is_running(self):
+        return self._is_running
+
+    @property
+    def is_determinate(self):
+        return self.max is not None
+
+    def start(self):
+        self.enabled = True
+        if not self.is_running:
+            self._impl.start()
+        self._is_running = True
+
+    def stop(self):
+        self._enabled = bool(self.max)
+        if self.is_running:
+            self._impl.stop()
+        self._is_running = False
+
+    @property
+    def value(self):
+        """
+        Returns:
+            The current value as a ``int`` or ``float``.
+        """
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if self.max:
+            # default to 0 if value is None
+            # bound value between 0 and self.max
+            self._value = max(0, min(self.max, value or 0))
+            self._impl.set_value(value)
+
+    @property
+    def max(self):
+        """ The maximum value of the progressbar.
+
+        Returns:
+            The maximum value as a ``int`` or ``float``.
+        """
+        return self._max
+
+    @max.setter
+    def max(self, value):
+        self.enabled = bool(value or self.is_running)
+
+        self._max = value
+        self._impl.set_max(value)
