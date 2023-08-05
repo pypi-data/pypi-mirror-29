968de613e1fccab7ce6ca61ef8d8accd0517d56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:24 +0000

    Add package dgenies/1.0.0/dgenies-1.0.0-3.tar.gz

---
+++ packages/dgenies/dgenies-1.0.0-3.tar.gz/dgenies-1.0.0/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session='hack')
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+version = '1.0.0'
+
+if os.name == "posix":
+
+    setup(
+        name='dgenies',
+        version=version,
+        packages=find_packages('src'),
+        package_dir={'dgenies': 'src/dgenies'},
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=reqs,
+        data_files=[('/etc/dgenies', ['application.properties']),
+                    ('/var/www/dgenies', ['dgenies.wsgi'])],
+        scripts=['src/bin/dgenies'],
+    )
+
+else:
+
+    setup(
+        name='dgenies',
+        version=version,
+        packages=find_packages('src'),
+        package_dir={'dgenies': 'src/dgenies'},
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=reqs,
+        data_files=[('.dgenies', ['application.properties'])],
+        scripts=['src/bin/dgenies'],
+    )
