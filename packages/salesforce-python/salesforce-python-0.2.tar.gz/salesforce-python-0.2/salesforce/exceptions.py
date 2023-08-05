2e18559a4a1d3b2bb4f79630d3056fcbf1ae70d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:26 +0000

    Add package salesforce-python/0.2/salesforce-python-0.2.tar.gz

---
+++ packages/salesforce-python/salesforce-python-0.2.tar.gz/salesforce-python-0.2/salesforce/exceptions.py
@@ -0,0 +1,18 @@
+class BaseError(Exception):
+    pass
+
+
+class UnknownError(BaseError):
+    pass
+
+
+class AccessTokenRequired(BaseError):
+    pass
+
+
+class BadOAuthTokenError(BaseError):
+    pass
+
+
+class BadRequestError(BaseError):
+    pass
---
+++ packages/salesforce-python/salesforce-python-0.2.tar.gz/salesforce-python-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='salesforce-python',
+      version='0.2',
+      description='API wrapper for Salesforce written in Python',
+      url='https://github.com/GearPlug/salesforce-python',
+      author='Miguel Ferrer',
+      author_email='ingferrermiguel@gmail.com',
+      license='GPL',
+      packages=['salesforce'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
