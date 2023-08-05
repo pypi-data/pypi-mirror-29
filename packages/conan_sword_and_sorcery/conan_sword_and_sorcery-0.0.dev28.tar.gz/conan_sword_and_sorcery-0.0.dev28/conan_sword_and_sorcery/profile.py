4521e1147e15a8e2eefa1cf1baa74ba385329591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:35 +0000

    Add package conan_sword_and_sorcery/0.0.dev28/conan_sword_and_sorcery-0.0.dev28.tar.gz

---
+++ packages/conan_sword_and_sorcery/conan_sword_and_sorcery-0.0.dev28.tar.gz/conan_sword_and_sorcery-0.0.dev28/conan_sword_and_sorcery/profile.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import os
+import tempfile
+from contextlib import contextmanager
+
+
+@contextmanager
+def profile_for(compiler):
+    # Create profile file
+    tmp = tempfile.NamedTemporaryFile(delete=False)
+    tmp.write("include(default)\n\n")  # Profile 'default'
+    tmp.write("[settings]\n")
+    compiler.populate_profile_settings(tmp)
+    tmp.write("[options]\n[build_requires]\n[env]\n")  # TODO: Is it needed?
+    tmp.close()
+    yield tmp.name
+    os.unlink(tmp.name)
+
