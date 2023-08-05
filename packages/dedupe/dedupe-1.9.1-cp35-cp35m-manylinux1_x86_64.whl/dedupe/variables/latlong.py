7221749f11cb2bf6f7cad02eeb40c80bf814a4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:10 +0000

    Add package dedupe/1.9.1/dedupe-1.9.1-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/dedupe/dedupe-1.9.1-cp35-cp35m-manylinux1_x86_64.whl/dedupe/variables/latlong.py
@@ -0,0 +1,14 @@
+from math import sqrt
+
+from .base import FieldType
+from dedupe import predicates
+from haversine import haversine
+
+class LatLongType(FieldType) :
+    type = "LatLong"
+
+    _predicate_functions = [predicates.latLongGridPredicate]
+
+    @staticmethod
+    def comparator(x, y) :
+        return sqrt(haversine(x, y))
