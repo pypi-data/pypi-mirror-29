b67926161875f182eb30eb6d8b9c1e0681643768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:02 +0000

    Add package alphabet/0.0.2/alphabet-0.0.2.tar.gz

---
+++ packages/alphabet/alphabet-0.0.2.tar.gz/alphabet-0.0.2/README.md
@@ -0,0 +1,32 @@
+# alphabet
+
+[![PyPI](https://img.shields.io/pypi/v/alphabet.svg)](https://pypi.python.org/pypi/alphabet)
+
+`alphabet` is a Python string class enhancement.
+
+## Install
+
+Just do
+
+```
+pip3 install alphabet
+```
+
+## Example
+
+```
+from alphabet import alphabet
+
+key = "foobar"
+s = alphabet.alphabet("python")
+
+print(s)
+> python
+
+t = s.obfuscate(key)
+print(bytes(t, 'utf-8'))
+> b'\x16\x16\x1b\n\x0e\x1c'
+
+print(t.obfuscate(key))
+> python
+```
---
+++ packages/alphabet/alphabet-0.0.2.tar.gz/alphabet-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'alphabet',
+    version = '0.0.2',
+    description = 'A string class enhancement',
+    packages = [ 'alphabet' ],
+    author = 'Roberto Reale',
+    author_email = 'rober.reale@gmail.com',
+    url = 'https://github.com/robertoreale/alphabet',
+    keywords = [ ],
+    install_requires = [ ],
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
+)
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
