f218dd7106ac073a58634b42122677619a3e2fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package pulp-smash/2018.2.22/pulp-smash-2018.2.22.tar.gz

---
+++ packages/pulp-smash/pulp-smash-2018.2.22.tar.gz/pulp-smash-2018.2.22/pulp_smash/tests/pulp3/constants.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+"""Constants for Pulp 3 tests."""
+from urllib.parse import urljoin
+
+
+BASE_PATH = '/api/v3/'
+
+BASE_IMPORTER_PATH = urljoin(BASE_PATH, 'importers/')
+
+BASE_PUBLISHER_PATH = urljoin(BASE_PATH, 'publishers/')
+
+DISTRIBUTION_PATH = urljoin(BASE_PATH, 'distributions/')
+
+FILE_IMPORTER_PATH = urljoin(BASE_IMPORTER_PATH, 'file/')
+
+FILE_PUBLISHER_PATH = urljoin(BASE_PUBLISHER_PATH, 'file/')
+
+IMPORTER_DOWN_POLICY = {'background', 'immediate', 'on_demand'}
+"""Download policies for an importer.
+
+See `pulpcore.app.models.Importer
+<https://docs.pulpproject.org/en/3.0/nightly/contributing/platform_api/app/models.html#pulpcore.app.models.Importer>`_.
+"""
+
+IMPORTER_SYNC_MODE = {'mirror'}
+"""Sync modes for an importer.
+
+See `pulpcore.app.models.Importer
+<https://docs.pulpproject.org/en/3.0/nightly/contributing/platform_api/app/models.html#pulpcore.app.models.Importer>`_.
+"""
+
+JWT_PATH = urljoin(BASE_PATH, 'jwt/')
+
+PUBLICATIONS_PATH = urljoin(BASE_PATH, 'publications/')
+
+REPO_PATH = urljoin(BASE_PATH, 'repositories/')
+
+STATUS_PATH = urljoin(BASE_PATH, 'status/')
+
+USER_PATH = urljoin(BASE_PATH, 'users/')
