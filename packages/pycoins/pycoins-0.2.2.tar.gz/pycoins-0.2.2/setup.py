d88e6f25322c53921fb118d0d825340de9180c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:55 +0000

    Add package pycoins/0.2.2/pycoins-0.2.2.tar.gz

---
+++ packages/pycoins/pycoins-0.2.2.tar.gz/pycoins-0.2.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='pycoins',
+      version='0.2.2',
+      description='A little command line tool for tracking cryptocurrency prices.',
+      url='http://github.com/ptbrodie/coins',
+      author='Patrick Brodie',
+      author_email='ptbrodie@gmail.com',
+      install_requires=[
+          'requests==2.18.4',
+          'termcolor==1.1.0',
+          'terminaltables==3.1.0',
+          'beautifulsoup4==4.6.0',
+          'dateparser==0.6.0'
+      ],
+      scripts=['./coins'])
