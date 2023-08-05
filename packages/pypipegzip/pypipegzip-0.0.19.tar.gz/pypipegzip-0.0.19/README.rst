35b41904e8d2ee3831e725e554c877a34e68e0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:52 +0000

    Add package pypipegzip/0.0.19/pypipegzip-0.0.19.tar.gz

---
+++ packages/pypipegzip/pypipegzip-0.0.19.tar.gz/pypipegzip-0.0.19/README.rst
@@ -0,0 +1,2 @@
+# pypipegzip
+Python module to provide gzip like open interfaces but using a zcat subprocess
---
+++ packages/pypipegzip/pypipegzip-0.0.19.tar.gz/pypipegzip-0.0.19/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name='pypipegzip',
+    version='0.0.19',
+    description='faster read of gzip files using a pipe to zcat(1)',
+    long_description='pypipegzip helps you read gzipped files faster by using a subprocess',
+    url='https://veltzer.github.io/pypipegzip',
+    download_url='https://github.com/veltzer/pypipegzip',
+    author='Mark Veltzer',
+    author_email='mark.veltzer@gmail.com',
+    maintainer='Mark Veltzer',
+    maintainer_email='mark.veltzer@gmail.com',
+    license='MIT',
+    platforms=['python'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+    ],
+    keywords='pipe pipeline gzip read speed pypipegzip',
+    packages=setuptools.find_packages(),
+)
