d62ef055fddee16f2795d70c46f2b8e787febacd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:39 +0000

    Add package ooquery/0.13.0/ooquery-0.13.0.tar.gz

---
+++ packages/ooquery/ooquery-0.13.0.tar.gz/ooquery-0.13.0/ooquery/ooquery.py
@@ -0,0 +1,72 @@
+# coding=utf-8
+from __future__ import absolute_import
+from functools import reduce
+
+from sql import Table
+from sql.aggregate import Aggregate
+from ooquery.parser import Parser
+
+
+class OOQuery(object):
+    def __init__(self, table, foreign_key=None):
+        self._fields = []
+        self.table = Table(table)
+        self.foreign_key = foreign_key
+        self._select = self.table.select()
+        self.parser = self.create_parser()
+        self.select_opts = {}
+
+    def create_parser(self):
+        return Parser(self.table, self.foreign_key)
+
+    @property
+    def select_on(self):
+        if self.parser.joins:
+            return self.parser.joins[-1]
+        else:
+            return self.table
+
+    @property
+    def fields(self):
+        fields = []
+        for field in self._fields:
+            aggr = None
+            if isinstance(field, Aggregate):
+                aggr = field.__class__
+                field = field.expression
+            table_field = self.parser.get_table_field(self.table, field)
+            if aggr:
+                table_field = aggr(table_field)
+                field = '{}_{}'.format(aggr._sql, field).lower()
+            fields.append(table_field.as_(field))
+        return fields
+
+    def select(self, fields=None, **kwargs):
+        self.parser = self.create_parser()
+        self._fields = fields
+        self.select_opts = kwargs
+        order_by = kwargs.pop('order_by', None)
+        group_by = kwargs.pop('group_by', None)
+        if order_by:
+            kwargs['order_by'] = []
+            for item in order_by:
+                kwargs['order_by'].append(
+                    reduce(getattr, item.split('.'), self.select_on)
+                )
+        if group_by:
+            kwargs['group_by'] = []
+            for item in group_by:
+                table_field = table_field = self.parser.get_table_field(
+                    self.table, item
+                )
+                kwargs['group_by'].append(
+                    table_field
+                )
+        self._select = self.select_on.select(*self.fields, **self.select_opts)
+        return self
+
+    def where(self, domain):
+        where = self.parser.parse(domain)
+        self._select = self.select_on.select(*self.fields, **self.select_opts)
+        self._select.where = where
+        return self._select
