be3fec29d6b9935314f37a827196b7dce9dd75da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:00 +0000

    Add package talisker/0.9.6/talisker-0.9.6.tar.gz

---
+++ packages/talisker/talisker-0.9.6.tar.gz/talisker-0.9.6/talisker/requests.py
@@ -0,0 +1,106 @@
+# Copyright (C) 2016- Canonical Ltd
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+from builtins import *  # noqa
+
+import logging
+import threading
+import functools
+from future.utils import native
+import requests
+from . import statsd
+from .util import parse_url
+from . import request_id
+
+__all__ = [
+    'HEADER',
+    'get_session',
+    'configure',
+    'enable_requests_logging',
+]
+
+# wsgi requires native strings
+HEADER = native(request_id.HEADER)
+storage = threading.local()
+storage.sessions = {}
+
+
+def get_session(cls=requests.Session):
+    session = storage.sessions.get(cls)
+    if session is None:
+        session = storage.sessions[cls] = cls()
+        configure(session)
+    return session
+
+
+def configure(session):
+    # insert metrics hook first as it needs response, and does't
+    # alter hook_data for later hooks
+    if metrics_response_hook not in session.hooks['response']:
+        session.hooks['response'].insert(0, metrics_response_hook)
+    # for some reason, requests doesn't have a pre_request hook anymore
+    # so, we do something horrible - we decorate the *instance* method
+    # this allows us to inject request id into the request, but without
+    # requiring a particular subclass of Session, leaving the user free to use
+    # whatever, but still use talisker's enhancements.
+    # If requests ever regains request hooks, this can go away
+    # If anyone has a better solution, *please* tell me!
+    if not hasattr(session.prepare_request, '_inject_request_id'):
+        session.prepare_request = inject_request_id(session.prepare_request)
+
+
+def inject_request_id(func):
+    @functools.wraps(func)
+    def prepare_request(request):
+        prepared = func(request)
+        id = request_id.get()
+        if id and HEADER not in prepared.headers:
+            prepared.headers[HEADER] = id
+        return prepared
+    prepare_request._inject_request_id = True
+    return prepare_request
+
+
+def metrics_response_hook(response, **kwargs):
+    """Response hook that records statsd metrics"""
+    prefix, duration = get_timing(response)
+    statsd.get_client().timing(prefix, duration)
+
+
+def get_timing(response):
+    parsed = parse_url(response.request.url)
+    duration = response.elapsed.total_seconds() * 1000
+    prefix = 'requests.{}.{}.{}'.format(
+        parsed.hostname.replace('.', '-'),
+        response.request.method.upper(),
+        str(response.status_code),
+    )
+    return prefix, duration
+
+
+def enable_requests_logging():  # pragma: nocover
+    """Full requests debug output is tricky to enable"""
+    from http.client import HTTPConnection
+    HTTPConnection.debuglevel = 1
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
---
+++ packages/talisker/talisker-0.9.6.tar.gz/talisker-0.9.6/talisker/util.py
@@ -0,0 +1,116 @@
+# Copyright (C) 2016- Canonical Ltd
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+from builtins import *  # noqa
+
+import functools
+import logging
+import pkg_resources
+
+from future.moves.urllib.parse import urlparse
+
+
+def parse_url(url, proto='http'):
+    # urlparse won't parse properly without a protocol
+    if '://' not in url:
+        url = proto + '://' + url
+    return urlparse(url)
+
+
+def pkg_is_installed(name):
+    try:
+        return pkg_resources.get_distribution(name)
+    except pkg_resources.DistributionNotFound:
+        return False
+
+
+class TaliskerVersionException(Exception):
+    pass
+
+
+def ensure_extra_versions_supported(extra):
+    # as this is an optional aid, don't fail if on old version of pip
+    try:
+        talisker_pkg = pkg_resources.get_distribution('talisker')
+        extra_deps = (
+            set(talisker_pkg.requires([extra])) - set(talisker_pkg.requires())
+        )
+        for requirement in extra_deps:
+            pkg = pkg_resources.get_distribution(requirement.project_name)
+            if pkg.version not in requirement.specifier:
+                raise TaliskerVersionException(
+                    '{} {} is not supported ({})'.format(
+                        requirement.project_name, pkg.version, requirement))
+    except Exception:
+        logging.getLogger(__name__).debug(
+            'skipping ensure_extra_versions_supported as exception occured')
+
+    return True
+
+
+# module level caches for global objects, means we can store all globals in
+# a single place. This is useful when testing, as we can reset globals easily.
+_global_cache = {}
+_global_dicts = []
+_context_locals = []
+
+
+def module_cache(func):
+    """Decorates a function to cache its result in a module dict."""
+
+    # Maybe use id(func) instead? Strings are more debug friendly, though.
+    id = func.__module__ + '.' + func.__name__
+
+    @functools.wraps(func)
+    def get(*args, **kwargs):
+        """Return the object from cache, or create it"""
+        if id not in _global_cache:
+            _global_cache[id] = func(*args, **kwargs)
+        return _global_cache[id]
+
+    @functools.wraps(func)
+    def update(*args, **kwargs):
+        """Force update of the cached object"""
+        _global_cache[id] = func(*args, **kwargs)
+        return _global_cache[id]
+
+    def raw_update(item):
+        """Set the object in the cache directly"""
+        _global_cache[id] = item
+
+    # expose the raw function, useful for testing
+    get.uncached = func
+    get.update = update
+    get.raw_update = raw_update
+
+    return get
+
+
+def module_dict():
+    d = {}
+    _global_dicts.append(d)
+    return d
+
+
+def clear_globals():
+    _global_cache.clear()
+    for d in _global_dicts:
+        d.clear()
