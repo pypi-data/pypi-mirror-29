56f5fea42f4cd9e4221f324cf817147eeecdebd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:23 +0000

    Add package dask-ml/0.4.1/dask_ml-0.4.1-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/dask-ml/dask_ml-0.4.1-cp27-cp27mu-manylinux1_x86_64.whl/dask_ml/cluster/minibatch.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+from sklearn import cluster as _cluster
+
+from dask_ml.base import _BigPartialFitMixin, _copy_partial_doc
+
+
+@_copy_partial_doc
+class PartialMiniBatchKMeans(_BigPartialFitMixin, _cluster.MiniBatchKMeans):
+    pass
