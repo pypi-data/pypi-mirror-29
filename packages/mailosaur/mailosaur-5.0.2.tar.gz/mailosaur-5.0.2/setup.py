df64d6b8866651bbda53ca8b90387189f5699918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:37 +0000

    Add package mailosaur/5.0.2/mailosaur-5.0.2.tar.gz

---
+++ packages/mailosaur/mailosaur-5.0.2.tar.gz/mailosaur-5.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='mailosaur',
+      version='5.0.2',
+      description='Python client library for Mailosaur',
+      url='https://mailosaur.com',
+      author='Mailosaur Ltd',
+      author_email='code@mailosaur.com',
+      keywords='email automation testing selenium robot framework',
+      license='MIT',
+      packages=['mailosaur'],
+      install_requires=[
+          'msrestazure'
+      ],
+      zip_safe=False, requires=['msrestazure'])
+
