e4c56b8f29c193f0e1d3df049dabd5ceeb524fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:16 +0000

    Add package handi/0.1.8/handi-0.1.8.tar.gz

---
+++ packages/handi/handi-0.1.8.tar.gz/handi-0.1.8/handy/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '0.1.8'
+
+from . import compress
+
+from . import pkg
+
+from . import list2txt
+from . import dict2txt
+
+from . import download
+from . import portscan
+from . import ftp
+
+from . import search
+from . import misc
+
+from . import setenv
+
+from . import docx2xml
+
