fdfefb83b7657ad2387b0b1a2a07cc4a176ae9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:44 +0000

    Add package faps/1.1.1/faps-1.1.1.tar.gz

---
+++ packages/faps/faps-1.1.1.tar.gz/faps-1.1.1/faps/__init__.py
@@ -0,0 +1,27 @@
+from alogsumexp import alogsumexp
+# data import and export
+from genotypeArray import genotypeArray
+from read_genotypes import read_genotypes
+from convert_genotypes import convert_genotypes
+from read_paternity_array import read_paternity_array
+from export_to_colony import export_to_colony
+from write_paternity_array import write_paternity_array
+# Simulation
+from make_parents import make_parents
+from make_offspring import make_offspring
+from make_sibships import make_sibships
+from make_generation import make_generation
+from make_power import make_power
+# paternity arrays
+from effective_nloci import effective_nloci
+from paternityArray import paternityArray
+from pr_transition import pr_transition
+from lik_sampled_fathers import lik_sampled_fathers
+from lik_unsampled_fathers import lik_unsampled_fathers
+from paternity_array import paternity_array
+from draw_fathers import draw_fathers
+# clustering
+from sibshipCluster import sibshipCluster
+from lik_partition import lik_partition
+from sibship_clustering import sibship_clustering
+
---
+++ packages/faps/faps-1.1.1.tar.gz/faps-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='faps',
+      version='1.1.1',
+      description=' Inference of paternity and sibling relationships accounting for uncertainty in genealogy',
+      url='http://github.com/ellisztamas/faps',
+      author='Tom Ellis',
+      author_email='thomas.ellis@ebc.uu.se',
+      license='MIT',
+      packages=['faps'],
+      install_requires=['numpy','scipy','pandas','fastcluster'],
+      zip_safe=False)
