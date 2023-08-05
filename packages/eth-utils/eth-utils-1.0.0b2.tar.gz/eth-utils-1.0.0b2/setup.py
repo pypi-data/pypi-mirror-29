4f89c2999b41932d84c453afe39a33b715315edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:17 +0000

    Add package eth-utils/1.0.0b2/eth-utils-1.0.0b2.tar.gz

---
+++ packages/eth-utils/eth-utils-1.0.0b2.tar.gz/eth-utils-1.0.0b2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+setup(
+    name='eth-utils',
+    # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+    version='1.0.0-beta.2',
+    description="""Common utility functions for ethereum codebases.""",
+    long_description_markdown_filename='README.md',
+    author='Piper Merriam',
+    author_email='pipermerriam@gmail.com',
+    url='https://github.com/ethereum/eth_utils',
+    include_package_data=True,
+    install_requires=[
+        "eth-hash>=0.1.0a3,<0.2.0",
+        "cytoolz>=0.8.2,<1.0.0",
+    ],
+    setup_requires=['setuptools-markdown'],
+    py_modules=['eth_utils'],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
