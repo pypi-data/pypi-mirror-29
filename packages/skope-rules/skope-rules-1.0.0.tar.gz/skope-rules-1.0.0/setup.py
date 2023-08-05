1cb853269d349d553d75ba504e4a5e73f8c280d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:46 +0000

    Add package skope-rules/1.0.0/skope-rules-1.0.0.tar.gz

---
+++ packages/skope-rules/skope-rules-1.0.0.tar.gz/skope-rules-1.0.0/setup.py
@@ -0,0 +1,30 @@
+import sys
+from setuptools import setup, find_packages
+
+
+try:
+    import numpy
+except ImportError:
+    print('numpy is required during installation')
+    sys.exit(1)
+
+try:
+    import scipy
+except ImportError:
+    print('scipy is required during installation')
+    sys.exit(1)
+
+setup(name='skope-rules',
+      version='1.0.0',
+      description='Machine Learning with Interpretable Rules',
+      url='https://github.com/skope-rules/skope-rules',
+      author='see AUTHORS.rst',
+      license='BSD 3 clause',
+      packages=find_packages(),
+      keywords=['learning with rules',
+                'interpretable machine learning'],
+      install_requires=['numpy>=1.10.4',
+                        'scikit-learn>=0.17.1',
+                        'scipy>=0.17.0',
+                        'pandas>=0.18.1'
+                        ])
