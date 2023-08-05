b0e79a5080edcc75ebfe17f316a6479ab64df03b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package pycoraldb/1.1.4.1/pycoraldb-1.1.4.1.zip

---
+++ packages/pycoraldb/pycoraldb-1.1.4.1.zip/pycoraldb-1.1.4.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+# try:
+#     from setuptools import setup, find_packages
+# except ImportError:
+#     from distutils.core import setup, find_packages
+
+import __init__ as pycoaldb
+
+NAME = "pycoraldb"
+VERSION = pycoaldb.__version__
+# VERSION = "1.1.4.0"
+
+install_requires = [] # ['websocket-client>=0.44.0', 'dill>=0.2.7.1']
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=['pycoraldb'],
+    package_dir={'': '..'},
+    package_data={'': ['*.md', '*.py']},
+    install_requires=install_requires,
+    # install_requires=['pandas>=2.1'],
+    # metadata for upload to PyPI
+    author='FanCapital',
+    author_email='public@fancapital.com',
+    description='Python CoralDB Client',
+    long_description='Python CoralDB Client',
+    license='FanCapital Public License',
+    url='https://pypi.python.org/pypi/pycoraldb',
+    keywords='pycoraldb coraldb')
