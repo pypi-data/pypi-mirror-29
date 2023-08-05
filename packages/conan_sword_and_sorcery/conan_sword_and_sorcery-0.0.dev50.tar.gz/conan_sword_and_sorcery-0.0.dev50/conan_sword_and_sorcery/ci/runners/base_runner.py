fd32d3a808b4846fa41a4c6569cf7a304f020818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:40 +0000

    Add package conan_sword_and_sorcery/0.0.dev50/conan_sword_and_sorcery-0.0.dev50.tar.gz

---
+++ packages/conan_sword_and_sorcery/conan_sword_and_sorcery-0.0.dev50.tar.gz/conan_sword_and_sorcery-0.0.dev50/conan_sword_and_sorcery/ci/runners/base_runner.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import sys
+import os
+
+log = logging.getLogger(__name__)
+
+
+class BaseRunner(object):
+    profile = None
+
+    def __init__(self, conanfile, recipe, dry_run=False):
+        self.conanfile = conanfile
+        self.recipe = recipe
+        self.dry_run = dry_run
+
+    def set_compiler(self, compiler):
+        self.compiler = compiler
+        self.compiler.cmd = self.cmd  # TODO: There is a better way, for sure :S
+
+    def set_profile(self, profile):
+        self.profile = profile
+
+    def run(self, options):
+        log.debug("Profile file: {}\n".format(self.profile))
+
+        # TODO: Code to get user/channel
+
+        command = ['conan', 'create', self.conanfile, 'jgsogo/testing',
+                   '--profile', self.profile, '--build=missing']
+        for k, v in options.items():
+            command += ['-o', '{}:{}={}'.format(self.recipe.name, k, v)]
+        return self.compiler.run(' '.join(command))
+
+    def cmd(self, command):
+        log.info("command to run: {}".format(command))
+        if not self.dry_run:
+            ret = os.system(command)  # TODO: May use subprocess
+            return "OK" if ret == 0 else "FAIL"
+        return "DRY_RUN"
