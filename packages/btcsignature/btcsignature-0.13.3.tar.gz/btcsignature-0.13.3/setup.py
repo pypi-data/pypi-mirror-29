cd8a12c315a3e809fce07281cbfabc9d5f30c433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:28 +0000

    Add package btcsignature/0.13.3/btcsignature-0.13.3.tar.gz

---
+++ packages/btcsignature/btcsignature-0.13.3.tar.gz/btcsignature-0.13.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='btcsignature',
+      version='0.13.3',
+      description='secure offline bitcoin transaction signing tool',
+      url='http://github.com/superarius/sendbtcsimply/',
+      author='sendbtcsimply',
+      author_email='sendbtcsimply@protonmail.com',
+      license='MIT',
+      packages=['btcsignature'],
+      entry_points = {'console_scripts': ['sign-offline=btcsignature.command_line:main', 'read-tx=btcsignature.command_line:m2'],},
+      install_requires=['bitcoin==2.6.11','blockchain', 'enum', 'ecdsa'],
+      zip_safe=False)
