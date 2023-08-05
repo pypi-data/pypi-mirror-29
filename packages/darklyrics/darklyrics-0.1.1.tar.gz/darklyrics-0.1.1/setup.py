e807e47e0e5c4fc3dbda7b766e7a399a3b66c69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:44 +0000

    Add package darklyrics/0.1.1/darklyrics-0.1.1.tar.gz

---
+++ packages/darklyrics/darklyrics-0.1.1.tar.gz/darklyrics-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+version = '0.1.1'
+
+setup(name='darklyrics',
+      version=version,
+      description='Darklyrics API for song lyrics',
+      long_description='Darklyrics API for song lyrics',
+      license='MIT',
+      author='Raihaan',
+      author_email='raihaanhimself@gmail.com',
+      url='http://github.com/res0nance/darklyrics',
+      packages=['darklyrics'],
+      setup_requires=['pytest-runner'],
+      install_requires=['beautifulsoup4', 'requests'],
+      tests_require=['pytest'],
+      keywords='darklyrics lyric lyrics song api')
