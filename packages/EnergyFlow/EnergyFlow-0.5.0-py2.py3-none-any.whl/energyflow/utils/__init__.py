d551f3610c6681df7e54f02ded159369731470cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:52 +0000

    Add package EnergyFlow/0.5.0/EnergyFlow-0.5.0-py2.py3-none-any.whl

---
+++ packages/EnergyFlow/EnergyFlow-0.5.0-py2.py3-none-any.whl/energyflow/utils/__init__.py
@@ -0,0 +1,67 @@
+"""A subpackage containing utility functions and classes. Not meant to be 
+imported directly in energyflow."""
+
+from __future__ import absolute_import
+
+import numpy as np
+
+from . import graph
+from . import measure
+from .graph import *
+from .measure import *
+
+def igraph_import():
+    """
+    Determines if igraph can be imported. 
+
+    Returns
+    -------
+    output : {igraph, False}
+        The igraph module if it was successfully imported, otherwise False.
+    """
+    
+    try:
+        import igraph
+    except:
+        igraph = False
+    return igraph
+
+def kwargs_check(name, kwargs, allowed=[]):
+    for k in kwargs:
+        if k in allowed:
+            continue
+        message = name + '() got an unexpected keyword argument \'{}\''.format(k)
+        raise TypeError(message)
+
+comp_map = {'>':  '__gt__', 
+            '<':  '__lt__', 
+            '>=': '__ge__', 
+            '<=': '__le__',
+            '==': '__eq__', 
+            '!=': '__ne__'}
+
+def explicit_comp(obj, comp, val):
+    return getattr(obj, comp_map[comp])(val)
+
+def concat_specs(c_specs, d_specs):
+    if len(d_specs):
+        return np.concatenate((c_specs, d_specs), axis=0)
+    else:
+        return c_specs
+
+def transfer(obj1, obj2, attrs):
+    if isinstance(obj2, dict):
+        for attr in attrs:
+            setattr(obj1, attr, obj2[attr])
+    else:
+        for attr in attrs:
+            setattr(obj1, attr, getattr(obj2, attr))
+
+def unique_dim_nlows(efm_specs, d={}):
+    for spec in efm_specs:
+        d.setdefault(spec[0], set()).update(spec[1:])
+    return d
+
+def nonegen():
+    while True:
+        yield None
---
+++ packages/EnergyFlow/EnergyFlow-0.5.0-py2.py3-none-any.whl/energyflow/utils/measure.py
@@ -0,0 +1,284 @@
+"""Implementation of the Measure class and its helpers."""
+
+from __future__ import absolute_import, division
+
+from abc import ABCMeta, abstractmethod
+
+import numpy as np
+from six import add_metaclass
+
+__all__ = ['Measure']
+
+@add_metaclass(ABCMeta)
+class Measure:
+    
+    """Class for dealing with any kind of measure."""
+
+    def __new__(cls, *args, **kwargs):
+        if cls is Measure:
+            measure = args[0]
+            if 'hadr' in measure:
+                return super(Measure, cls).__new__(HadronicMeasure._factory(measure))
+            if 'ee' in measure:
+                return super(Measure, cls).__new__(EEMeasure._factory(measure))
+            raise NotImplementedError('measure {} is unknown'.format(measure))
+        else:
+            return super(Measure, cls).__new__(cls)
+
+    def __init__(self, measure, beta, normed, check_type):
+        """Processes inputs according to the measure choice.
+
+        Arguments
+        ---------
+        measure : string
+            - The string specifying the measure.
+        beta : float/int
+            - The exponent $\beta$.
+        normed : bool
+            - Whether or not to use normalized energies.
+        check_type : bool
+            - Whether or not to check the input for each new event.
+        """
+
+        self.measure = measure
+        self.normed = normed
+        self.check_type = check_type
+
+        if 'efm' in self.measure:
+            self.beta = beta
+            self.sqrt2 = np.sqrt(2)
+        else:
+            assert beta > 0, 'beta must be greater than zero'
+            self.beta = float(beta)
+            self._half_beta = self.beta/2
+
+        self._lacks_meas_func = True
+
+    def __call__(self, arg):
+
+        # check type only if needed
+        if self._lacks_meas_func or self.check_type:
+            self._set_meas_func(arg)
+
+        # get zs and angles 
+        zs, angles = self._meas_func(arg)
+
+        return (zs/np.sum(zs) if self.normed else zs), angles
+
+    def _set_meas_func(self, arg):
+
+        # support arg as numpy.ndarray
+        if isinstance(arg, np.ndarray):
+            if not self._ndarray_handler(arg.shape[1]):
+                raise IndexError('second dimension of arg must be in {}'.format(self._allowed_dims))
+
+        # support arg as list (of lists)
+        elif isinstance(arg, list):
+            if not self._list_handler(len(arg[0])):
+                raise IndexError('second dimension of arg must be in {}'.format(self._allowed_dims))
+
+        # support arg as fastjet pseudojet
+        elif hasattr(arg, 'constituents'):
+            self._pseudojet_handler()
+
+        # raise error if not one of these types
+        else:
+            raise TypeError('arg is not one of numpy.ndarray, list, or fastjet.PseudoJet')
+
+        self._lacks_meas_func = False
+
+    @abstractmethod
+    def _ndarray_handler(self, dim):
+        pass
+
+    @abstractmethod
+    def _list_handler(self, dim):
+        pass
+
+    @abstractmethod
+    def _pseudojet_handler(self):
+        pass
+
+    def _p4s_dot(self, p4s, Es):
+        p4hats = p4s/Es[:,np.newaxis]
+        X = (p4hats[:,np.newaxis]*p4hats[np.newaxis,:]).T
+        return (2*np.abs(X[0] - X[1] - X[2] - X[3]))**self._half_beta
+
+class HadronicMeasure(Measure):
+
+    @staticmethod
+    def _factory(measure):
+        if 'efm' in measure:
+            return HadronicEFMMeasure
+        if 'dot' in measure:
+            return HadronicDotMeasure
+        return HadronicDefaultMeasure 
+
+    _allowed_dims = [3, 4]
+
+    def _ndarray_handler(self, dim):
+        if dim == 3:
+            self._meas_func = self._ndarray_dim3
+        elif dim == 4:
+            self._meas_func = self._ndarray_dim4
+        else:
+            return False
+        return True
+
+    @abstractmethod
+    def _ndarray_dim3(self, arg):
+        pass
+
+    @abstractmethod
+    def _ndarray_dim4(self, arg):
+        pass
+        
+    def _list_handler(self, dim):
+        if dim == 3:
+            self._meas_func = self._list_dim3
+        elif dim == 4:
+            self._meas_func = self._list_dim4
+        else:
+            return False
+        return True    
+
+    def _list_dim3(self, arg):
+        return self._ndarray_dim3(np.asarray(arg))
+
+    def _list_dim4(self, arg):
+        return self._ndarray_dim4(np.asarray(arg))
+
+    def _pseudojet_handler(self):
+        self._meas_func = self._pseudojet
+        return True
+
+    @abstractmethod
+    def _pseudojet(self, arg):
+        constituents = arg.constituents()
+        pts = np.asarray([c.pt() for c in constituents])
+        return pts, constituents
+
+    def _pts(self, p4s):
+        return np.sqrt(p4s[:,1]**2 + p4s[:,2]**2)
+
+    def _p4s_from_ptyphis(self, ptyphis):
+        pts, ys, phis = ptyphis[:,0], ptyphis[:,1], ptyphis[:,2]
+        return (pts*np.vstack([np.cosh(ys), np.cos(phis), np.sin(phis), np.sinh(ys)])).T
+
+class EEMeasure(Measure):
+
+    @staticmethod
+    def _factory(measure):
+        if 'efm' in measure:
+            return EEEFMMeasure
+        return EEDefaultMeasure
+
+    _allowed_dims = [4]
+
+    def _ndarray_handler(self, dim):
+        if dim == 4:
+            self._meas_func = self._ndarray_dim4
+        else:
+            return False
+        return True
+
+    @abstractmethod
+    def _ndarray_dim4(self, arg):
+        pass
+        
+    def _list_handler(self, dim):
+        if dim == 4:
+            self._meas_func = self._list_dim4
+        else:
+            return False
+        return True
+
+    def _list_dim4(self, arg):
+        return self._ndarray_dim4(np.asarray(arg))
+
+    def _pseudojet_handler(self):
+        self._meas_func = self._pseudojet
+        return True
+
+    @abstractmethod
+    def _pseudojet(self, arg):
+        constituents = arg.constituents()
+        Es = np.asarray([c.e() for c in constituents])
+        return Es, constituents
+
+class HadronicDefaultMeasure(HadronicMeasure):
+
+    def _yphis(self, p4s):
+        return np.vstack([0.5*np.log((p4s[:,0]+p4s[:,3])/(p4s[:,0]-p4s[:,3])),
+                          np.arctan2(p4s[:,2], p4s[:,1])]).T
+
+    def _thetas_from_yphis(self, yphis):
+        X = yphis[:,np.newaxis] - yphis[np.newaxis,:]
+        X[:,:,0] **= 2
+        X[:,:,1] = (np.pi - np.abs(np.abs(X[:,:,1]) - np.pi))**2
+        return (X[:,:,0] + X[:,:,1])**self._half_beta
+
+    def _ndarray_dim3(self, arg):
+        return arg[:,0], self._thetas_from_yphis(arg[:,(1,2)])
+
+    def _ndarray_dim4(self, arg):
+        return self._pts(arg), self._thetas_from_yphis(self._yphis(arg))
+
+    def _pseudojet(self, arg):
+        pts, constituents = super()._pseudojet(arg)
+        thetas = np.asarray([[c1.delta_R(c2) for c2 in constituents] for c1 in constituents])**self.beta
+        return pts, thetas
+
+class HadronicDotMeasure(HadronicMeasure):
+
+    def _ndarray_dim3(self, arg):
+        pts = arg[:,0]
+        p4s = self._p4s_from_ptyphis(arg)
+        return pts, self._p4s_dot(p4s, pts)
+
+    def _ndarray_dim4(self, arg):
+        pts = self._pts(arg)
+        return pts, self._p4s_dot(arg, pts)
+
+    def _pseudojet(self, arg):
+        pts, constituents = super()._pseudojet(arg)
+        p4s = np.asarray([[c.e(), c.px(), c.py(), c.pz()] for c in constituents])
+        return pts, self._p4s_dot(p4s, pts)
+
+class HadronicEFMMeasure(HadronicMeasure):
+
+    def _ndarray_dim3(self, arg):
+        pts = arg[:,0]
+        p4s = self._p4s_from_ptyphis(arg)
+        return pts, p4s/pts[:,np.newaxis]
+
+    def _ndarray_dim4(self, arg):
+        pts = self._pts(arg)
+        return pts, arg/pts[:,np.newaxis]
+
+    def _pseudojet(self, arg):
+        pts, constituents = super()._pseudojet(arg)
+        p4s = np.asarray([[c.e(), c.px(), c.py(), c.pz()] for c in constituents])
+        return pts, p4s/pts[:,np.newaxis]
+
+class EEDefaultMeasure(EEMeasure):
+
+    def _ndarray_dim4(self, arg):
+        Es = arg[:,0]
+        return Es, self._p4s_dot(arg, Es)
+
+    def _pseudojet(self, arg):
+        Es, constituents =  super()._pseudojet(arg)
+        p4s = np.asarray([[c.e(), c.px(), c.py(), c.pz()] for c in constituents])
+        return Es, self._p4s_dot(p4s, Es)
+
+class EEEFMMeasure(EEMeasure):
+
+    def _ndarray_dim4(self, arg):
+        Es = arg[:,0]
+        return Es, arg/Es[:,np.newaxis]
+
+    def _pseudojet(self, arg):
+        Es, constituents = super()._pseudojet(arg)
+        p4s = np.asarray([[c.e(), c.px(), c.py(), c.pz()] for c in constituents])
+        return Es, p4s/Es[:,np.newaxis]
