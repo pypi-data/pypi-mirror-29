fe5f67e2cb13fbb95798758142271d89c416d3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:13 +0000

    Add package kforce/0.1.17/kforce-0.1.17.tar.gz

---
+++ packages/kforce/kforce-0.1.17.tar.gz/kforce-0.1.17/setup.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from setuptools import setup
+from pip.req import parse_requirements
+from pip.download import PipSession
+
+VERSION = "0.1.17"
+
+scripts = ["bin/kforce"]
+
+install_requires = [str(ir.req) for ir in parse_requirements("requirements/base.txt", session=PipSession())]
+
+if __name__ == "__main__":
+    setup(
+        name="kforce",
+        version=VERSION,
+        author="Yang Kelvin Liu",
+        author_email="ycliuhw@gmail.com",
+        license="Apache License 2.0",
+        url="https://github.com/ycliuhw/kforce",
+        description="KOPS template automation",
+        packages=["kforce"],
+        scripts=scripts,
+        keywords=["k8s", "kops", "kubernetes", "template"],
+        install_requires=install_requires,
+    )
