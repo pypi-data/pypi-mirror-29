a7f859ecf50c3da99348c982ff7a9567eebbc268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:05 +0000

    Add package naruhodo/0.1/naruhodo-0.1-py3-none-any.whl

---
+++ packages/naruhodo/naruhodo-0.1-py3-none-any.whl/naruhodo/core/__init__.py
@@ -0,0 +1,6 @@
+"""
+Module for core analyzers of different backends.
+"""
+
+from naruhodo.core.dependencyAnalyzer import DependencyAnalyzer
+from naruhodo.core.knowledgeAnalyzer import KnowledgeAnalyzer
