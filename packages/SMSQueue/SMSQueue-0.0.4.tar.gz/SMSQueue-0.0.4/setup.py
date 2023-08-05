0f72bd3e5e33f869d315deaf1f19abbf7b763c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package SMSQueue/0.0.4/SMSQueue-0.0.4.tar.gz

---
+++ packages/SMSQueue/SMSQueue-0.0.4.tar.gz/SMSQueue-0.0.4/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='SMSQueue',
+    version='0.0.4',
+    packages=['smsqueue'],
+    license=['BSD 3 Clause'],
+    install_requires=['requests==2.18.4'],
+)
