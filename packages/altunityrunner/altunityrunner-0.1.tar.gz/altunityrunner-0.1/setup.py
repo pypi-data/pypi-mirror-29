fe58434c822e2d3b9efa32e9ed941765581bdf1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:53 +0000

    Add package altunityrunner/0.1/altunityrunner-0.1.tar.gz

---
+++ packages/altunityrunner/altunityrunner-0.1.tar.gz/altunityrunner-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='altunityrunner',
+      version='0.1',
+      description='Python Binding to allow Appium tests to be run against Unity games and apps using AltUnityTester',
+      url='https://gitlab.com/altom/altunitytester',
+      author='Altom Consulting',
+      author_email='altunitytester@altom.fi',
+      license='GNU General Public License v3.0',
+      packages=['altunityrunner'],
+      zip_safe=False)
