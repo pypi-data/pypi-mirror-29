5a35f1fa88ccaf3faedb9e04933d4eec4cd4e57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:17 +0000

    Add package buildbot-dbconfig/0.1.0/buildbot-dbconfig-0.1.0.tar.gz

---
+++ packages/buildbot-dbconfig/buildbot-dbconfig-0.1.0.tar.gz/buildbot-dbconfig-0.1.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+try:
+    from buildbot_pkg import setup_www_plugin
+except ImportError:
+    import sys
+    print("Please install buildbot_pkg module in order to install that package, or use the pre-build .whl modules available on pypi", file=sys.stderr)
+    sys.exit(1)
+
+
+setup_www_plugin(
+    name='buildbot-dbconfig',
+    description='Buildbot DbConfig plugin',
+    author=u'Alexey Demakov',
+    author_email=u'moris.aye.aye@gmail.com',
+    url='https://github.com/ayaye/buildbot-dbconfig',
+    long_description=open('README.rst', 'r').read(),
+    packages=['dbconfig'],
+    package_data={
+        '': [
+            'VERSION',
+            'static/*'
+        ]
+    },
+    entry_points="""
+        [buildbot.www]
+        dbconfig = dbconfig:ep
+    """,
+    install_requires=[
+          'klein',
+    ],
+)
