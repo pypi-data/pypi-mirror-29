1bbb3bb8460d6e507fccdfe45b52d8b53b19f559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:07 +0000

    Add package dynamic-rest/1.7.1/dynamic-rest-1.7.1.tar.gz

---
+++ packages/dynamic-rest/dynamic-rest-1.7.1.tar.gz/dynamic-rest-1.7.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+NAME = 'dynamic-rest'
+DESCRIPTION = 'Adds Dynamic API support to Django REST Framework.'
+URL = 'http://github.com/AltSchool/dynamic-rest'
+VERSION = '1.7.1'
+SCRIPTS = ['manage.py']
+
+setup(
+    description=DESCRIPTION,
+    include_package_data=True,
+    dependency_links=open('dependency_links.txt').readlines(),
+    install_requires=open('install_requires.txt').readlines(),
+    long_description=open('README.rst').read(),
+    name=NAME,
+    packages=find_packages(),
+    scripts=SCRIPTS,
+    url=URL,
+    version=VERSION
+)
