632fe4083989d1ee0f630d18f19c9732a26f5b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:10 +0000

    Add package naruhodo/0.1.4/naruhodo-0.1.4-py3-none-any.whl

---
+++ packages/naruhodo/naruhodo-0.1.4-py3-none-any.whl/naruhodo-0.1.4.dist-info/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+
+Naruhodo is a python library for generating dependency-graph(DG) and knowledge-graph(KG) in networkx format from Japanese text or urls that contains Japanese texts. You can visualize these graphs directly in jupyter notebook or export graphs to JSON file for visualization using external programs.
+For detailed documentation and tutorials, visit https://github.com/superkerokero/naruhodo
+
+
---
+++ packages/naruhodo/naruhodo-0.1.4-py3-none-any.whl/naruhodo/utils/dicts.py
@@ -0,0 +1,63 @@
+"""
+This module contains various dicts used in naruhodo.
+"""
+
+
+ProDict = {
+    'demonstrative-loc': ["ここ", "そこ", "あそこ", "こっち", "そっち", "あっち", "こちら", "そちら", "あちら"],
+    'demonstrative-obj': ["これ", "それ", "あれ", "こいつ", "そいつ", "あいつ"],
+    'personal1st': ["私", "わたし", "俺", "おれ", "オレ", "僕", "ぼく", "ボク", "我ら", "我々"],
+    'personal2nd': ["君", "きみ", "キミ", "あなた", "貴方", "お前"],
+    'personal3rd': ["やつ", "奴", "彼", "彼女"],
+    'indefinite': ["どこ", "どれ", "どっち", "どなた", "どちら"],
+    'inclusive': ["皆", "みんな", "みな"]
+}
+"""
+Pronoun identification dictionary.
+"""
+
+
+TypeList = ['名詞', '形容詞', '動詞', '接続詞', '感動詞', '副詞', '連体詞', '接頭詞']
+"""
+Dict of word types.
+"""
+
+
+NEList = ['PERSON', 'LOCATION', 'ORGANIZATION']
+"""
+Dict of named entity types.
+"""
+
+
+NodeShapeDictN2Json = {
+    "square": "circle",
+    "Mdiamond": "circle",
+    "doublecircle": "circle",
+    "parallelogram": "ellipse",
+    "pentagon": "diamond",
+    "box": "hexagon",
+    "circle": "square",
+    "underline": "text"
+}
+"""
+Dict to convert the node shape from notebook to vis.js json format.
+"""
+
+
+UncountedDict = set([
+    "なる", "ある", "よる", "する", "ない", "前", "後", "こと", "もの", "ため"
+])
+"""
+Dict of common words that are not cumulatively counted.
+"""
+
+MeaninglessDict = set([
+    "前", "後", "こと", "もの", "ため", "意", "中", "する"
+])
+"""
+Dict that contains meaningless entities that has to be integrated with its child properties to remain meaningful in the graph.
+"""
+
+VerbLikeFuncDict = set([
+    "する", "しいるて", "し", "した", "しいるて・が", "いるて"
+])
