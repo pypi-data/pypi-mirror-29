51bf54dfd81878608d82be6560f12157861abd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:07 +0000

    Add package zydis/0.0.1/zydis-0.0.1.tar.gz

---
+++ packages/zydis/zydis-0.0.1.tar.gz/zydis-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'zydis',
+  packages = ['zydis'],
+  version = '0.0.1',
+  description = 'Future home of official Zydis Python bindings',
+  author = 'Joel Höner',
+  author_email = 'athre0z@zyantific.com',
+  url = 'https://github.com/zyantific/zydis-rs',
+  download_url = 'https://github.com/zyantific/zydis-rs',
+  keywords = [],
+  classifiers = [],
+)
