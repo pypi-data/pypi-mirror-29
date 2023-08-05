1a870fce81f41b519758803a5bb1f788127836b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:47 +0000

    Add package mozilla-taar2/0.0.23/mozilla_taar2-0.0.23-py3.5.egg

---
+++ packages/mozilla-taar2/mozilla_taar2-0.0.23-py3.5.egg/taar/recommenders/__init__.py
@@ -0,0 +1,17 @@
+from .collaborative_recommender import CollaborativeRecommender
+from .locale_recommender import LocaleRecommender
+from .legacy_recommender import LegacyRecommender
+from .similarity_recommender import SimilarityRecommender
+from .recommendation_manager import RecommendationManager, RecommenderFactory
+from .ensemble_recommender import EnsembleRecommender
+
+
+__all__ = [
+    'CollaborativeRecommender',
+    'EnsembleRecommender',
+    'LegacyRecommender',
+    'LocaleRecommender',
+    'SimilarityRecommender',
+    'RecommendationManager',
+    'RecommenderFactory',
+]
