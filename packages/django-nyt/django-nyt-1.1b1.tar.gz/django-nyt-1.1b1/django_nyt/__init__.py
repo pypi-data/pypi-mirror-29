2a8cf9afc82d53376e1e8b95a17bd9953f895b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:12 +0000

    Add package django-nyt/1.1b1/django-nyt-1.1b1.tar.gz

---
+++ packages/django-nyt/django-nyt-1.1b1.tar.gz/django-nyt-1.1b1/django_nyt/__init__.py
@@ -0,0 +1,14 @@
+_disable_notifications = False
+
+VERSION = "1.1b1"
+__version__ = VERSION
+
+default_app_config = "django_nyt.apps.DjangoNytConfig"
+
+
+def notify(*args, **kwargs):
+    """
+    DEPRECATED - please access django_nyt.utils.notify
+    """
+    from django_nyt.utils import notify
+    return notify(*args, **kwargs)
---
+++ packages/django-nyt/django-nyt-1.1b1.tar.gz/django-nyt-1.1b1/setup.py
@@ -0,0 +1,40 @@
+import os
+from django_nyt import VERSION
+from setuptools import setup, find_packages
+
+def get_path(fname):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), fname)
+
+def read(fname):
+    return open(get_path(fname)).read()
+
+packages = find_packages()
+
+setup(
+    name="django-nyt",
+    version=VERSION,
+    author="Benjamin Bach",
+    author_email="benjamin@overtag.dk",
+    url="https://github.com/benjaoming/django-nyt",
+    description="A pluggable notification system written for the Django framework.",
+    license="Apache License 2.0",
+    keywords=["django", "notification" "alerts"],
+    packages=find_packages(exclude=["testproject", "testproject.*"]),
+    zip_safe=False,
+    install_requires=read('requirements.txt').split("\n"),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    include_package_data=True,
+)
