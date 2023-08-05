002058e34bcbb3105d3fc44dc634a116e129a74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:03 +0000

    Add package biostream-schemas/0.0.8/biostream-schemas-0.0.8.tar.gz

---
+++ packages/biostream-schemas/biostream-schemas-0.0.8.tar.gz/biostream-schemas-0.0.8/bmeg/conversions.py
@@ -0,0 +1,25 @@
+import json
+import bmeg.hugo_ensembl
+import bmeg.pubchem_table
+
+def build_mapping(inpath, outpath):
+    i = open(inpath, 'r')
+    lines = []
+    for line in i:
+        lines.append(line.split('\t'))
+    lines = lines[1:]
+    i.close()
+
+    mapping = {}
+    for line in lines:
+        mapping[line[1]] = line[36] if len(line[18]) == 0 else line[18]
+    out = json.dumps(mapping)
+    o = open(outpath, 'w')
+    o.write(out)
+    o.close()
+
+def hugo_ensembl(hugo):
+    return bmeg.hugo_ensembl.hugo_ensembl(hugo)
+
+def pubchem(random):
+    return bmeg.pubchem_table.convert(random)
---
+++ packages/biostream-schemas/biostream-schemas-0.0.8.tar.gz/biostream-schemas-0.0.8/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='biostream-schemas',
+    version='0.0.8',
+    packages=['bmeg']
+)
