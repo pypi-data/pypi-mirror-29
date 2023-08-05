5aaba7cabbdbb80ea2b6003d6c5a020b4f77a23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:22 +0000

    Add package rednose/1.3.0/rednose-1.3.0.tar.gz

---
+++ packages/rednose/rednose-1.3.0.tar.gz/rednose-1.3.0/setup.py
@@ -0,0 +1,34 @@
+from os import path
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    try:
+        return open(path.join(path.dirname(__file__), fname)).read()
+    except IOError:
+        return """A module for making nose pretty using colors."""
+
+setup(
+    packages=find_packages(exclude=['test', 'test.*']),
+    description='coloured output for nosetests',
+    entry_points={'nose.plugins.0.10': ['NOSETESTS_PLUGINS = rednose:RedNose']},
+    install_requires=['setuptools', 'termstyle >=0.1.7', 'colorama'],
+    tests_require=['six==1.10.0'],
+    long_description=read('README.rst'),
+    name='rednose',
+    py_modules=['rednose'],
+    url='https://github.com/JBKahn/rednose',
+    version='1.3.0',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Testing",
+    ],
+    keywords='test nosetests nose nosetest output colour console',
+    license='MIT',
+    test_suite='test_files.new_tests',
+)
