196fbbe562dd0d152d7e757153718ad169f24f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:27 +0000

    Add package bscrp/0.6/bscrp-0.6.tar.gz

---
+++ packages/bscrp/bscrp-0.6.tar.gz/bscrp-0.6/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'bscrp',
+  packages = ['bscrp'],
+  package_dir = {'bscrp': 'bscrp'},
+  package_data = {'bscrp': ['*']},
+  version = '0.6',
+  description = 'Better Scrape, a collection of open source scraping methods used by Better Know The Opposition',
+  author = 'BKTO',
+  author_email = 'hello@betterknowtheopposition.com',
+  url = 'https://github.com/BKTO/bscrp',
+  download_url = 'https://github.com/BKTO/bscrp',
+  keywords = ['scraping'],
+  classifiers = [],
+)
