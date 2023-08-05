6e0241fd785e6c0ab127467fb42fe25ed90e99e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:36 +0000

    Add package splunknova/1.0.3/splunknova-1.0.3.tar.gz

---
+++ packages/splunknova/splunknova-1.0.3.tar.gz/splunknova-1.0.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='splunknova',
+      version='1.0.3',
+      description='Python Client for Splunk Project Nova',
+      author='Scott Odle',
+      author_email='scott@sjodle.com',
+      url='https://github.com/sodle/pysplunknova',
+      download_url='https://github.com/sodle/pysplunknova/archive/1.0.3.tar.gz',
+      license='GPL v3',
+      packages=['splunknova']
+      )
