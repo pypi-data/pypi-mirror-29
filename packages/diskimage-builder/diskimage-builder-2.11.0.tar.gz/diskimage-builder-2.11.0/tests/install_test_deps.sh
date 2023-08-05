5e2784a45c9aa12b2e7f3788277806c203f93c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:15 +0000

    Add package diskimage-builder/2.11.0/diskimage-builder-2.11.0.tar.gz

---
+++ packages/diskimage-builder/diskimage-builder-2.11.0.tar.gz/diskimage-builder-2.11.0/tests/install_test_deps.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -eux
+set -o pipefail
+
+sudo apt-get update || true
+sudo apt-get install -y --force-yes \
+        apt-transport-https \
+        bzip2 \
+        debootstrap \
+        docker.io \
+        inetutils-ping \
+        lsb-release \
+        kpartx \
+        python-lzma \
+        qemu-utils \
+        rpm \
+        uuid-runtime \
+        yum-utils || \
+    sudo yum -y install --enablerepo=epel \
+        bzip2 \
+        dpkg \
+        debootstrap \
+        docker \
+        kpartx \
+        util-linux \
+        qemu-img \
+        policycoreutils-python || \
+    sudo zypper -n install \
+        bzip2 \
+        debootstrap \
+        docker \
+        kpartx \
+        util-linux \
+        python-pyliblzma \
+        yum-utils \
+        qemu-tools || \
+    sudo emerge \
+        app-arch/bzip2 \
+        app-emulation/qemu \
+        dev-python/pyyaml \
+        sys-block/parted \
+        sys-fs/multipath-tools \
+        qemu-img \
+        yum-utils
