c5841a530c58b8e0c696ee9c257e894817a75722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:14 +0000

    Add package itunes-iap/2.5.1/itunes-iap-2.5.1.tar.gz

---
+++ packages/itunes-iap/itunes-iap-2.5.1.tar.gz/itunes-iap-2.5.1/itunesiap/__init__.py
@@ -0,0 +1,37 @@
+"""
+itunes-iap
+~~~~~~~~~~
+
+Itunes In-app Purchase verification api.
+
+:copyright: (c) 2013 Jeong YunWon - 2014 Andy Briggs
+:license: 2-clause BSD.
+"""
+
+from six import PY3
+
+from .request import Request
+from .receipt import Response, Receipt, InApp
+from .shortcut import verify, aioverify
+
+from . import exceptions
+from . import environment
+
+exc = exceptions
+env = environment  # env.default, env.sandbox, env.review
+
+try:
+    import pkg_resources
+except ImportError:  # pragma: no cover
+    __version__ = VERSION = None
+else:
+    __version__ = pkg_resources.resource_string(
+        'itunesiap', 'version.txt').strip()
+    if PY3:
+        __version__ = __version__.decode('ascii')  # pragma: no cover
+    VERSION = tuple(int(v) for v in __version__.split('.'))
+
+
+__all__ = (
+    'Request', 'Response', 'Receipt', 'InApp', 'verify', 'aioverify',
+    'exceptions', 'exc', 'environment', 'env')
