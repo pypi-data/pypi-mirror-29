0eb8f11b93c671b2a12124c8024f69c93a2e996b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:56 +0000

    Add package django-cloud-browser/0.4.0/django-cloud-browser-0.4.0.tar.gz

---
+++ packages/django-cloud-browser/django-cloud-browser-0.4.0.tar.gz/django-cloud-browser-0.4.0/cloud_browser/__init__.py
@@ -0,0 +1,9 @@
+"""Cloud browser application.
+
+Provides a simple filesystem-like browser interface for cloud blob datastores.
+"""
+
+VERSION = (0, 4, 0)
+
+__version__ = ".".join(str(v) for v in VERSION)
+__version_full__ = __version__
