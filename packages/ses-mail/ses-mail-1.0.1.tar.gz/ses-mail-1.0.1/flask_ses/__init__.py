9dcb2b4d4d6385d015eee5f7ccb0a5e684440236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:28 +0000

    Add package ses-mail/1.0.1/ses-mail-1.0.1.tar.gz

---
+++ packages/ses-mail/ses-mail-1.0.1.tar.gz/ses-mail-1.0.1/flask_ses/__init__.py
@@ -0,0 +1,64 @@
+import boto3
+
+
+class SESMailer:
+    """AWS Simple Email Service Extension."""
+
+    def __init__(self, app=None):
+        """Initialize SES interface.
+
+        :param app: Flask application
+        """
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Initialize SESMailer."""
+        self.ses_sender = app.config.get('AWS_SES_SENDER')
+        self.aws_region = app.config['AWS_SES_REGION_NAME']
+        self.aws_access_key_id = app.config['AWS_ACCESS_KEY_ID']
+        self.aws_secret_access_key = app.config['AWS_SECRET_ACCESS_KEY']
+
+    @property
+    def connection(self):
+        if not hasattr(self, '_ses_mailer'):
+            self._ses_mailer = boto3.client(
+                'ses', region_name=self.aws_region,
+                aws_access_key_id=self.aws_access_key_id,
+                aws_secret_access_key=self.aws_secret_access_key,
+            )
+        return self._ses_mailer
+
+    def send_email(self, recipients, sender=None, subject='',
+                   content='', is_html=False):
+        """Send e-mail.
+
+        :param recipients: Recipients e-mails list.
+        :param sender: Sender e-mail.
+        :param subject: E-mail subject.
+        :param content: E-mail body.
+        :param is_html: E-mail body contains html?
+        """
+        body = {'Body': {}}
+
+        if sender is None:
+            sender = self.ses_sender
+
+        if is_html:
+            body['Body'].update({
+                'Html': {'Data': content},
+            })
+        else:
+            body['Body'].update({
+                'Text': {'Data': content},
+            })
+
+        email = self.connection.send_email(
+            Source=sender,
+            Destination={'ToAddresses': recipients},
+            Message={
+                'Subject': {'Data': subject},
+                'Body': body,
+            }
+        )
+        return email
---
+++ packages/ses-mail/ses-mail-1.0.1.tar.gz/ses-mail-1.0.1/setup.py
@@ -0,0 +1,36 @@
+"""
+Flask-SES
+=========
+
+This tiny library will help you to send email using boto3 through AWS SES.
+"""
+from setuptools import setup
+
+
+setup(
+    name='ses-mail',
+    version='1.0.1',
+    url='https://github.com/michaeltcoelho/flask-ses',
+    license='MIT',
+    author='Michael Coelho',
+    author_email='michael.tcoelho@gmail.com',
+    description='Sending email using AWS SES',
+    long_description=__doc__,
+    packages=['flask_ses'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'boto3'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
