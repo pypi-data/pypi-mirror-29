582c85d4f157bc70d429f41de0c2486f8a82e8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:15 +0000

    Add package ytest/0.1.2/ytest-0.1.2.tar.gz

---
+++ packages/ytest/ytest-0.1.2.tar.gz/ytest-0.1.2/setup.py
@@ -0,0 +1,24 @@
+#-*- encoding: UTF-8 -*-
+from setuptools import setup, find_packages
+""" 打包的用的setup必须引入 """
+
+VERSION = '0.1.2'
+
+setup(name='ytest',
+    version=VERSION,
+    description="a demo project",
+    long_description='just a simple demo',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='python ytest demo',
+    author='yotong',
+    author_email='yotong@qq.com',
+    url='https://github.com/yotong/ytest',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[ 'requests', ],
+    entry_points={
+        'console_scripts': [ 'ytool = ytest:main' ]
+    },
+)
