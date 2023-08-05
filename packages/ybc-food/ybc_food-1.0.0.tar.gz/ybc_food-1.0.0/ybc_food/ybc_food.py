87cadef960527a23446e0c4dbf5c86d90d3dd4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:24 +0000

    Add package ybc-food/1.0.0/ybc_food-1.0.0.tar.gz

---
+++ packages/ybc-food/ybc_food-1.0.0.tar.gz/ybc_food-1.0.0/ybc_food/ybc_food.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import base64
+import os
+import math
+
+def check(filename=''):
+    '''美食图片检测'''
+    if not filename:
+        return -1
+
+    url = 'https://www.phpfamily.org/imgFoodCheck.php'
+    filepath = os.path.abspath(filename)
+    b64img= base64.b64encode(open(filepath, 'rb').read()).rstrip().decode('utf-8')
+    data = {}
+    data['b64img'] = b64img
+    r = requests.post(url, data=data)
+    res = r.json()
+    if res['ret'] == 0 and res['data'] :
+        res['data']['confidence'] = str(math.ceil(res['data']['confidence']*100)) + '%'
+        return res['data']['food']
+    else:
+        return -1
+def main():
+    res = food_check('2.jpg')
+    print(res)
+if __name__ == '__main__':
+    main()
