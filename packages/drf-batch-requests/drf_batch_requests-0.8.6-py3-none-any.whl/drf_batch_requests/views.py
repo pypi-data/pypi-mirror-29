ea358683dfa705674e730c0156408101f05aede0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:48 +0000

    Add package drf-batch-requests/0.8.6/drf_batch_requests-0.8.6-py3-none-any.whl

---
+++ packages/drf-batch-requests/drf_batch_requests-0.8.6-py3-none-any.whl/drf_batch_requests/views.py
@@ -0,0 +1,51 @@
+import json
+
+from django.core.handlers.base import BaseHandler
+from django.db import transaction
+from rest_framework.response import Response
+from rest_framework.status import is_success
+from rest_framework.views import APIView
+
+from drf_batch_requests.request import BatchRequestsFactory
+
+
+class BatchView(APIView):
+    permission_classes = []
+
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+        responses = []
+
+        requests_factory = BatchRequestsFactory(request)
+
+        for current_request in requests_factory:
+            handler = BaseHandler()
+            handler.load_middleware()
+
+            response = handler.get_response(current_request)
+            result = {
+                'code': response.status_code,
+                'headers': [
+                    {'name': key, 'value': value}
+                    for key, value in response._headers.values()
+                ],
+                'body': response.content.decode('utf-8'),
+            }
+
+            if is_success(response.status_code):
+                result['_data'] = json.loads(result['body'])
+
+            if not is_success(response.status_code) or \
+               is_success(response.status_code) and not current_request.omit_response_on_success:
+                result['return_body'] = True
+
+            if current_request.name:
+                requests_factory.named_responses[current_request.name] = result
+
+            responses.append({k: v for k, v in result.items() if not k.startswith('_')})
+
+            if not is_success(response.status_code):
+                # todo: handle requests dependencies
+                break
+
+        return self.finalize_response(request, Response(responses))
