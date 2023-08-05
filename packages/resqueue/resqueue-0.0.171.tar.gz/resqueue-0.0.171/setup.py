d4c058c308c2b1aa9701b13295e82649677e6fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:58 +0000

    Add package resqueue/0.0.171/resqueue-0.0.171.tar.gz

---
+++ packages/resqueue/resqueue-0.0.171.tar.gz/resqueue-0.0.171/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="resqueue",
+    packages = ["resqueue"],
+    version="0.0.171",
+    description = "job submission in python using job managers", 
+    url="https://github.com/YSanchezAraujo/resqueue",
+    author="Yoel Sanchez Araujo",
+    author_email="yoelsanchez006@gmail.com",
+    download_url = "https://github.com/YSanchezAraujo/archive/0.0.171.tar.gz",
+    keywords = ["clusters", "slurm"],
+    classifiers = [],
+)
+
+
