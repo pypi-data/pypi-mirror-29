2af058dc3796f517c92777f0b536755017069cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package ebuildtester/0.1.3/ebuildtester-0.1.3.tar.gz

---
+++ packages/ebuildtester/ebuildtester-0.1.3.tar.gz/ebuildtester-0.1.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ebuildtester",
+    version="0.1.3",
+    packages=["ebuildtester"],
+    entry_points={
+        "console_scripts": ["ebuildtester = ebuildtester.main:main"]
+    },
+    author="Nicolas Bock",
+    author_email="nicolasbock@gmail.com",
+    description="A dockerized approach to test a Gentoo package within a clean stage3",
+    license="BSD",
+    url="https://github.com/nicolasbock/gentoo-test-package",
+    project_urls={
+        "Releases": "https://github.com/nicolasbock/gentoo-test-package/releases"
+    }
+)
