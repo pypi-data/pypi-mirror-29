cf1deda0e944aaf122bf9fe8f5236ee9df8b0d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:18 +0000

    Add package bpynodes_lib/1.0.3/bpynodes_lib-1.0.3.tar.gz

---
+++ packages/bpynodes_lib/bpynodes_lib-1.0.3.tar.gz/bpynodes_lib-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='bpynodes_lib',
+      version='1.0.3',
+      description='Blender Python Nodes extension that provides a python library for creating Python Node systems',
+      url='https://gitlab.com/blenderone/bpynodes_lib',
+      author='Jared Webber',
+      author_email='info@onelvxe.com',
+      license='GNU GPLv3',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+      keywords='blender bpy nodes custom pynodes',
+      python_requires='~=3.5',
+      #install_requires=['setuptools', 'wheel', 'requests', 'numpy'],
+      zip_safe=False)
