d04973e38b198fd166e38d853deaa8b4bb166272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:25 +0000

    Add package travistest/0.0.1/travistest-0.0.1.tar.gz

---
+++ packages/travistest/travistest-0.0.1.tar.gz/travistest-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='travistest',
+    packages=['travistest'],
+    version='0.0.1',
+    description='Test project to get acquainted with TravisCI',
+    author='lqdev',
+    url='https://github.com/lqdev/TravisTest',    
+)
