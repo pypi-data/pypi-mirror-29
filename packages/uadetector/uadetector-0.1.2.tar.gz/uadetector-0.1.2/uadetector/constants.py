e9b2f05151106f092d367e8033f62931711d0a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:59 +0000

    Add package uadetector/0.1.2/uadetector-0.1.2.tar.gz

---
+++ packages/uadetector/uadetector-0.1.2.tar.gz/uadetector-0.1.2/uadetector/constants.py
@@ -0,0 +1,18 @@
+"""
+    uadetector.constants
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    Manage constant values for uadatector.
+
+    :author: tell-k <ffk2005 at gmail.com>
+    :copyright: tell-k. All Rights Reserved.
+"""
+
+# WSGI environ key name
+ENVIRON_KEY = 'uadetector.useragent'
+
+# Name of UserAgent object to assign to request object in web frameworks
+REQUEST_PROPERTY_NAME = 'ua'
+
+# Custom UserAgent class
+USERAGENT_CLASS = 'uadetector.UserAgent'
