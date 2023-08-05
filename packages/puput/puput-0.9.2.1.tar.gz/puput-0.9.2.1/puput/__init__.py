d4d9f5371cdb7bf2e52dc239e0d9b57fecfd7771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:06 +0000

    Add package puput/0.9.2.1/puput-0.9.2.1.tar.gz

---
+++ packages/puput/puput-0.9.2.1.tar.gz/puput-0.9.2.1/puput/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Marc Tudurí'
+__email__ = 'marctc@gmail.com'
+__version__ = '0.9.2.1'
+
+PUPUT_APPS = (
+    # Wagtail apps
+    'wagtail.wagtailcore',
+    'wagtail.wagtailadmin',
+    'wagtail.wagtaildocs',
+    'wagtail.wagtailsnippets',
+    'wagtail.wagtailusers',
+    'wagtail.wagtailimages',
+    'wagtail.wagtailembeds',
+    'wagtail.wagtailsearch',
+    'wagtail.wagtailsites',
+    'wagtail.wagtailredirects',
+    'wagtail.wagtailforms',
+    'wagtail.contrib.wagtailsitemaps',
+    'wagtail.contrib.wagtailroutablepage',
+
+    # Third-party apps
+    'taggit',
+    'modelcluster',
+
+    # Puput apps
+    'puput',
+)
