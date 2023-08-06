0df51a72299e47457d35701ccb2185d3751b0335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:13 +0000

    Add package munidatapipeline/0.1.73/munidatapipeline-0.1.73.tar.gz

---
+++ packages/munidatapipeline/munidatapipeline-0.1.73.tar.gz/munidatapipeline-0.1.73/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='munidatapipeline',
+      version='00.01.073',
+      description='An ETL tool for working with municipal open data (especially GiS data) on resource constrained systems.',
+      author='Charles Landau, Andrew Gobbi',
+      author_email='munidatapipelinedevelopment@gmail.com',
+      url='https://github.com/mdp-dev/munidatapipeline',
+      keywords=['munidatapipeline', 'ETL', 'multiprocessing'],
+      packages=['munidatapipeline', 'core'],
+     )
