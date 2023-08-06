fa5fc8aa0b12c10ac86fc1e9c8e24cad81f29ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:56 +0000

    Add package kattcmd/0.0.2/kattcmd-0.0.2.tar.gz

---
+++ packages/kattcmd/kattcmd-0.0.2.tar.gz/kattcmd-0.0.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# How to update:
+# 1. Create a new tag on gitlab and find the download url (archive.tar.gz) for that tag
+# 2. Update the version in setup()
+# 3. run `python setup.py sdist upload -r pypi`
+
+from distutils.core import setup
+
+setup(
+    name='kattcmd',
+    packages=['kattcmd', 'kattcmd.commands'],
+    version='0.0.2',
+    description='Kattis solution management CLI written in python',
+    author='Henrik Adolfsson',
+    author_email='anting004@gmail.com',
+    url='https://git.lysator.liu.se/catears/kattis-command',
+    download_url='https://git.lysator.liu.se/catears/kattis-command/repository/0.0.2/archive.tar.gz',
+    keywords=['kattis', 'cli', 'competitive programming'],
+    scripts=['bin/kattcmd'],
+    package_dir={
+        'kattcmd.commands': 'kattcmd/commands'
+    },
+    package_data={
+        'kattcmd.commands': ['default_templates/*']
+    },
+    classifiers=[]
+)
