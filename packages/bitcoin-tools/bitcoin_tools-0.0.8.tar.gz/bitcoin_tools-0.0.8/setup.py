b5784e997803e457c199de3080e8ad025ed6323a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:03 +0000

    Add package bitcoin-tools/0.0.8/bitcoin_tools-0.0.8.tar.gz

---
+++ packages/bitcoin-tools/bitcoin_tools-0.0.8.tar.gz/bitcoin_tools-0.0.8/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='bitcoin_tools',
+      version='0.0.8',
+      description='Bitcoin Tools Library, WIP: CURRENTLY ONLY USED FOR TESTNET',
+      author='Christopher Coverdale',
+      author_email='chris.coverdale24@gmail.com',
+      url='https://github.com/ccdle12/python-bitcoin-tools',
+      nclude_package_data=True,
+      packages=['bitcoin_tools'],
+      )
