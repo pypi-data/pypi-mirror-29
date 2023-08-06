92c3b732a25bed9dc89ec587d07cea6d35906c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:26 +0000

    Add package MultiRunner/1.1/MultiRunner-1.1.tar.gz

---
+++ packages/MultiRunner/MultiRunner-1.1.tar.gz/MultiRunner-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MultiRunner",
+    version = "1.1",
+    url='https://www.python.org/sigs/distutils-sig/',
+    author = "Statham",
+    author_email = "statham.stone@gmail.com",
+    packages=[""]
+)
