d8ffe8ac3aef5f3d5a4d5aec46d2857f764fa5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:28 +0000

    Add package dodo-commands/0.13.1/dodo_commands-0.13.1.tar.gz

---
+++ packages/dodo-commands/dodo_commands-0.13.1.tar.gz/dodo_commands-0.13.1/dodo_commands/extra/standard_commands/docker.py
@@ -0,0 +1,17 @@
+"""This command opens a bash shell in the docker container."""
+from . import DodoCommand
+
+
+class Command(DodoCommand):  # noqa
+    def add_arguments_imp(self, parser):  # noqa
+        parser.add_argument('command', nargs='?')
+        parser.add_argument('--name')
+
+    def handle_imp(self, command, name, **kwargs):  # noqa
+        self.get_config('/DOCKER', {}) \
+            .setdefault('aliases', {}) \
+            .setdefault('docker', name)
+
+        self.runcmd(
+            ["/bin/bash"] + (["-c", command] if command else []),
+            cwd=self.get_config("/DOCKER/default_cwd", None))
