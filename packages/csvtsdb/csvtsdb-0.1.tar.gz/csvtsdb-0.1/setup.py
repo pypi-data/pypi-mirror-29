4bd9ce665838f856561a201021a9d383ae085afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:06 +0000

    Add package csvtsdb/0.1/csvtsdb-0.1.tar.gz

---
+++ packages/csvtsdb/csvtsdb-0.1.tar.gz/csvtsdb-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='csvtsdb',
+    version='0.1',
+    description='CSV-backed timeseries database usable standalone or as a Twisted resource',
+    url='http://github.com/anotherkamila/csvtsdb',
+    author='Kamila Součková',
+    author_email='kamisouckova@gmail.com',
+    license='MIT',
+    packages=['csvtsdb'],
+)
