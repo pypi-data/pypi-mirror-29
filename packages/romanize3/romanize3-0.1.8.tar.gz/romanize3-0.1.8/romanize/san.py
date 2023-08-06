242247f7a5f935de7cccb1998b00067f57545dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:05 +0000

    Add package romanize3/0.1.8/romanize3-0.1.8.tar.gz

---
+++ packages/romanize3/romanize3-0.1.8.tar.gz/romanize3-0.1.8/romanize/san.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+# file: san.py
+
+import re
+from collections import OrderedDict
+from .romanizer import romanizer
+
+has_capitals = False
+
+data = OrderedDict()
+
+r = romanizer(data)
+
+def filter(string):
+    """
+    Preprocess string to remove all other characters but sanskrit ones
+
+    :param string:
+    :return:
+    """
+    # remove all unwanted characters
+    return string
+
+def preprocess(string):
+    """
+    Preprocess string to transform all diacritics and remove other special characters
+
+    :param string:
+    :return:
+    """
+    return string
+
+def convert(string, sanitize=False):
+    """
+    Swap characters from script to transliterated version and vice versa.
+    Optionally sanitize string by using preprocess function.
+
+    :param sanitize:
+    :param string:
+    :return:
+    """
+    return r.convert(string, (preprocess if sanitize else False))
