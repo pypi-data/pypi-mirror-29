4a233fdce5d4170dc1d7db0143c5aaf9260c3b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:05 +0000

    Add package pychannels/0.0.1/PyChannels-0.0.1.tar.gz

---
+++ packages/pychannels/PyChannels-0.0.1.tar.gz/PyChannels-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='PyChannels',
+    version='0.0.1',
+    packages=['pychannels',],
+    license='The MIT License',
+    long_description=open('README.md').read(),
+    description = 'API client for the Channels app - https://getchannels.com',
+    author = 'Fancy Bits, LLC',
+    author_email = 'jon@getchannels.com',
+    url = 'https://github.com/fancybits/pychannels',
+    keywords = ['api', 'client', 'channels', 'automation'],
+    install_requires = ['requests'],
+)
