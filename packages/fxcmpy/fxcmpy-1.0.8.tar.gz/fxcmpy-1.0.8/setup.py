fac03c587516a9eb972a55a0b42bb0591fb25477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:11 +0000

    Add package fxcmpy/1.0.8/fxcmpy-1.0.8.tar.gz

---
+++ packages/fxcmpy/fxcmpy-1.0.8.tar.gz/fxcmpy-1.0.8/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+def readme():
+    try:
+        with open('README_short.rst', 'r') as f:
+            return f.read()
+    except:
+        return ''
+
+setup(
+    name = 'fxcmpy',
+    packages = ['fxcmpy'], # this must be the same as the name above
+    version = '1.0.8',
+    description = 'A Python Wrapper Class for the RESTful API as provided by FXCM Forex Capital Markets Ltd.',
+    long_description = readme(),
+    author = 'The Python Quants GmbH',
+    author_email = 'admin@tpq.io',
+    license='BSD',
+    url = 'https://github.com/fxcm/fxcmpy', 
+    download_url = 'https://github.com/fxcm/fxcmpy/archive/0.1.tar.gz', 
+    keywords = 'FXCM API Python Wrapper Finance Algo Trading',
+    install_requires=['pandas', 'socketIO_client', 'configparser', 'requests'], 
+    classifiers = [],
+)
