da4ed569bf8a4da215e608023bd05719ed709902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:32 +0000

    Add package genomelink/0.1.6/genomelink-0.1.6.tar.gz

---
+++ packages/genomelink/genomelink-0.1.6.tar.gz/genomelink-0.1.6/genomelink/__init__.py
@@ -0,0 +1,6 @@
+api_base = 'https://genomelink.io'
+# api_base = 'http://localhost:8000'
+# api_base = 'https://staging.genomelink.io'
+
+from genomelink.oauth import OAuth
+from genomelink.resource.report import Report
