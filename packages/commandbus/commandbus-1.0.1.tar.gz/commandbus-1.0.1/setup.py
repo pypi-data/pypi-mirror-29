7307ddbf8fe2decff51847db0f07e5a7da2f9de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:22 +0000

    Add package commandbus/1.0.1/commandbus-1.0.1.tar.gz

---
+++ packages/commandbus/commandbus-1.0.1.tar.gz/commandbus-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="commandbus",
+    version="1.0.1",
+    packages=find_packages(),
+    author="Petru Rares Sincraian",
+    author_email="psincraian@gmail.com",
+    description="Command Bus Pattern in Python made easy",
+    keywords="commandbus command bus python pattern patterns",
+    url="https://github.com/psincraian/commandbus",
+    license="MIT",
+)
