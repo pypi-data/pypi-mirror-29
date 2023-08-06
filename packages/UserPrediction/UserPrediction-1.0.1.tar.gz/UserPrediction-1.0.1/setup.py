ad0449af22f97707be8f20910565e62d9039e9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:38 +0000

    Add package UserPrediction/1.0.1/UserPrediction-1.0.1.tar.gz

---
+++ packages/UserPrediction/UserPrediction-1.0.1.tar.gz/UserPrediction-1.0.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 19:58:29 2018
+
+@author: sarth
+"""
+
+from setuptools import setup
+
+setup(name = 'UserImageProcessing', version = '1.0.1', scripts=['SAFMI_UserImageProcessing'] )
+
+setup(name = 'UserPrediction', version = '1.0.1', scripts=['SAFMI_UserPrediction'] )
