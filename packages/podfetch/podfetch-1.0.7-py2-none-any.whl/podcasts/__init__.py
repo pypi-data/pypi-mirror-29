694916481a638e198d60dd448b806ab6e7bbdb80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:37 +0000

    Add package podfetch/1.0.7/podfetch-1.0.7-py2-none-any.whl

---
+++ packages/podfetch/podfetch-1.0.7-py2-none-any.whl/podcasts/__init__.py
@@ -0,0 +1,5 @@
+"""Podcast Series / Episodes Data Retrieval and Storage in Python"""
+
+__version__ = '1.0.7'
+
+from . import *
---
+++ packages/podfetch/podfetch-1.0.7-py2-none-any.whl/podcasts/constants.py
@@ -0,0 +1,4 @@
+ITUNES_SEARCH_URL = 'https://itunes.apple.com/search?'
+ITUNES_LOOKUP_URL = 'https://itunes.apple.com/lookup?'
+ITUNES_GENRES_URL = 'https://itunes.apple.com/us/genre/podcasts/id26?mt=2'
+LOGGER = 'podfetch'
