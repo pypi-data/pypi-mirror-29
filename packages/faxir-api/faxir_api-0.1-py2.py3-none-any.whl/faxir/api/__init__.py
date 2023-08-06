6592562b8b5c32ac8d5156c853934d48e63b3083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:51 +0000

    Add package faxir-api/0.1/faxir_api-0.1-py2.py3-none-any.whl

---
+++ packages/faxir-api/faxir_api-0.1-py2.py3-none-any.whl/faxir/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from faxir.api.accounts_api import AccountsApi
+from faxir.api.archives_api import ArchivesApi
+from faxir.api.files_api import FilesApi
+from faxir.api.numbers_api import NumbersApi
+from faxir.api.outbox_api import OutboxApi
