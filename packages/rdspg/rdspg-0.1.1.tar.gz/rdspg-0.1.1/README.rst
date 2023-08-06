7afebdc7b493edbd2d5acdf29625127369eeb570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:42 +0000

    Add package rdspg/0.1.1/rdspg-0.1.1.tar.gz

---
+++ packages/rdspg/rdspg-0.1.1.tar.gz/rdspg-0.1.1/README.rst
@@ -0,0 +1,92 @@
+rdspg
+======
+
+Command-line toolkit to help understand information about your AWS RDS Parameter Groups.
+
+Installation
+------------
+
+.. code:: bash
+
+    pip install rdspg
+    
+Purpose
+-------
+
+When it comes to analyzing parameter groups for RDS, AWS suggested in a `blog post <https://aws.amazon.com/premiumsupport/knowledge-center/default-custom-groups/>`_ that:
+
+:: 
+
+    There is no AWS CLI command to compare two parameter groups simultaneously; this feature is only available by using the RDS console.
+    You can then compare the plain text files that list the parameter groups using a Linux tool such as the diff command, or a source code editor like Notepad++.
+
+This tool is to help us make that task a lot easier. Also adding a few other features to help analyzing changes.
+    
+Usage
+-----
+Listing Parameter Groups:
+
+::
+
+    $ rdspg list
+    DBParameterGroupName             DBParameterGroupFamily    Description
+    -------------------------------  ------------------------  ----------------------------------------------------------
+    default.aurora-postgresql9.6     aurora-postgresql9.6      Default parameter group for aurora-postgresql9.6
+    default.aurora5.6                aurora5.6                 Default parameter group for aurora5.6
+    default.postgres9.3              postgres9.3               Default parameter group for postgres9.3
+    default.postgres9.4              postgres9.4               Default parameter group for postgres9.4
+    default.postgres9.5              postgres9.5               Default parameter group for postgres9.5
+    default.postgres9.6              postgres9.6               Default parameter group for postgres9.6
+    my-parameter-group               postgres9.6               My Parameter Group
+
+Getting parameters in parameter group, filtering out default values:
+
+::
+
+    $ rdspg get my-replica
+    ParameterName                      ParameterValue  ApplyMethod    ApplyType
+    -------------------------------  ----------------  -------------  -----------
+    autovacuum_analyze_scale_factor               0.1  immediate      dynamic
+    checkpoint_segments                           512  immediate      dynamic
+    checkpoint_timeout                            300  immediate      dynamic
+    checkpoint_warning                             60  immediate      dynamic
+    default_statistics_target                     100  immediate      dynamic
+    hot_standby_feedback                            1  immediate      dynamic
+    log_autovacuum_min_duration                     0  immediate      dynamic
+    log_connections                                 1  immediate      dynamic
+    log_disconnections                              1  immediate      dynamic
+
+Compare differences between two parameter groups:
+
+::
+
+    $ rdspg diff my-replica-a my-replica-b
+    ParameterName          my-replica-a  my-replica-b
+    -------------------  --------------  ---------------------
+    checkpoint_timeout              300  450
+    checkpoint_warning               60  <not-set>
+    checkpoint_segments             512  32
+
+Export parameter group in terraform template format:
+
+::
+
+    $ rdspg terraform my-parameter-group
+    resource "aws_db_parameter_group" "my-parameter-group" {
+      name   = "my-parameter-group"
+      family = "postgres9.5"
+      description = "My awesome parameter group"
+    
+      parameter {
+        name         = "autovacuum_analyze_scale_factor"
+        value        = "0.01"
+        apply_method = "immediate"
+      }
+    
+      parameter {
+        name         = "autovacuum_vacuum_scale_factor"
+        value        = "0.01"
+        apply_method = "immediate"
+      }
+    
+    }
---
+++ packages/rdspg/rdspg-0.1.1.tar.gz/rdspg-0.1.1/rdspg/__main__.py
@@ -0,0 +1,184 @@
+import click
+import boto3
+import tabulate
+import os
+import jinja2
+
+
+def rds_get_parameters(parameter_group_name):
+    client = boto3.client('rds')
+    resp = client.describe_db_parameters(
+        DBParameterGroupName=parameter_group_name
+    )
+    return resp['Parameters']
+
+
+def rds_get_parameter_groups():
+    client = boto3.client('rds')
+    resp = client.describe_db_parameter_groups()
+    return resp['DBParameterGroups']
+
+
+def rds_get_pg_info(parameter_group_name):
+    client = boto3.client('rds')
+    resp = client.describe_db_parameter_groups(
+        DBParameterGroupName=parameter_group_name
+    )
+    parameter_group_info = resp['DBParameterGroups'][0]
+    info = {}
+    info['family'] = parameter_group_info['DBParameterGroupFamily']
+    info['description'] = parameter_group_info['Description']
+    return info
+
+
+def params_to_kv(params):
+    out = {}
+    for param in params:
+        key = param['ParameterName']
+        value = param.get('ParameterValue')
+        out[key] = value
+    return out
+
+
+def params_list_to_dict(params, detail=False):
+    headers = ['ParameterName', 'ParameterValue', 'ApplyMethod', 'ApplyType']
+    if detail:
+        headers += ['AllowedValues', 'DataType', 'Source']
+    out = []
+    for param in params:
+        p = list(param.get(h) for h in headers)
+        out.append(p)
+    return out, headers
+
+
+def calculate_diff(params_a, params_b):
+    params_a = params_to_kv(params_a)
+    params_b = params_to_kv(params_b)
+    out = []
+    for k, v in params_a.iteritems():
+        if v != params_b.get(k):
+            out.append((k, v, params_b.get(k, '<not-set>')))
+        if k in params_b:
+            del params_b[k]
+    for k, v in params_b.iteritems():
+        out.append((k, None, v))
+    return out
+
+
+def only_important_columns_pg(pgs):
+    for pg in pgs:
+        for k in ('DBParameterGroupArn',):
+            if k in pg:
+                del pg[k]
+    return pgs
+
+
+def only_user_params(params):
+    out = []
+    for param in params:
+        if param['Source'] not in ('system', 'engine-default'):
+            out.append(param)
+    return out
+
+
+def only_important_columns(params):
+    columns = ('Description', 'DataType', 'IsModifiable',
+               'AllowedValues', 'Source')
+    for param in params:
+        for k in columns:
+            if k in param:
+                del param[k]
+    return params
+
+
+def terraform(parameter_group_name, info, params):
+    def render(context):
+        curr_dir = os.path.dirname(__file__)
+        template_file_path = os.path.join(curr_dir, 'terraform.jinja')
+        path, filename = os.path.split(template_file_path)
+        return jinja2.Environment(
+            loader=jinja2.FileSystemLoader(path or './')
+        ).get_template(filename).render(context)
+
+    context = {'parameter_group_name': parameter_group_name,
+               'params': params, 'info': info}
+    return render(context)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(name='list')
+@click.option('--detail', is_flag=True, default=False)
+@click.option('--no-header', is_flag=True, default=False)
+def cmd_list(detail, no_header):
+    pgs = rds_get_parameter_groups()
+    if not detail:
+        pgs = only_important_columns_pg(pgs)
+    if no_header:
+        kwargs = {'tablefmt': 'plain'}
+    else:
+        kwargs = {'tablefmt': 'simple', 'headers': 'keys'}
+    output = tabulate.tabulate(pgs, numalign='right', **kwargs)
+    click.echo(output)
+
+
+@cli.command(name='get')
+@click.argument('parameter-group')
+@click.option('--all-params', is_flag=True, default=False)
+@click.option('--detail', is_flag=True, default=False)
+@click.option('--no-header', is_flag=True, default=False)
+def cmd_get(parameter_group, all_params, detail, no_header):
+    params = rds_get_parameters(parameter_group)
+    if not all_params:
+        params = only_user_params(params)
+    if not detail:
+        params = only_important_columns(params)
+    params, headers = params_list_to_dict(params, detail=detail)
+    if no_header:
+        kwargs = {'tablefmt': 'plain'}
+    else:
+        kwargs = {'tablefmt': 'simple', 'headers': headers}
+    output = tabulate.tabulate(params, numalign='right', **kwargs)
+    click.echo(output)
+
+
+@cli.command(name='diff')
+@click.argument('parameter-group-a')
+@click.argument('parameter-group-b')
+@click.option('--all-params', is_flag=True, default=False)
+@click.option('--no-header', is_flag=True, default=False)
+def cmd_diff(parameter_group_a, parameter_group_b, all_params, no_header):
+    params_a = rds_get_parameters(parameter_group_a)
+    params_b = rds_get_parameters(parameter_group_b)
+    if not all_params:
+        params_a = only_user_params(params_a)
+        params_b = only_user_params(params_b)
+    diff = calculate_diff(params_a, params_b)
+    headers = ['ParameterName', parameter_group_a, parameter_group_b]
+    if no_header:
+        kwargs = {'tablefmt': 'plain'}
+    else:
+        kwargs = {'tablefmt': 'simple', 'headers': headers}
+    output = tabulate.tabulate(diff, numalign='right', **kwargs)
+    click.echo(output)
+
+
+@cli.command(name='terraform')
+@click.argument('parameter-group')
+def cmd_terraform(parameter_group):
+    params = rds_get_parameters(parameter_group)
+    info = rds_get_pg_info(parameter_group)
+    params = only_user_params(params)
+    template = terraform(parameter_group, info, params)
+    click.echo(template)
+
+
+def main():
+    cli()
+
+
+if __name__ == '__main__':
+    main()
