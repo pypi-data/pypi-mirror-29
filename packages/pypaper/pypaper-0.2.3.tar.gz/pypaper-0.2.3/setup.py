3b697f35b77ace77da35bad5584d5ccf9cfb100e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:14 +0000

    Add package pypaper/0.2.3/pypaper-0.2.3.tar.gz

---
+++ packages/pypaper/pypaper-0.2.3.tar.gz/pypaper-0.2.3/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(name='pypaper',
+      version='0.2.3',
+      description='A command line tool to aid the writing of research papers',
+      url='https://github.com/eng-tools/pypaper',  # The URL to the github repo
+      author='Maxim Millen',
+      author_email='mmi46@uclive.ac.nz',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+      ],
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+      install_requires=[],
+      keywords="research latex",
+      # List additional groups of dependencies here (e.g. development
+      # dependencies). You can install these using the following syntax,
+      # for example:
+      # $ pip install -e .[dev,test]
+      extras_require={
+          'test': ['pytest'],
+      },
+      python_requires='>=3',
+      package_data={
+          'models': ['models_data.dat'],
+      },
+      zip_safe=False)
+
+
+# From python packaging guides
+# versioning is a 3-part MAJOR.MINOR.MAINTENANCE numbering scheme,
+# where the project author increments:
+
+# MAJOR version when they make incompatible API changes,
+# MINOR version when they add functionality in a backwards-compatible manner, and
+# MAINTENANCE version when they make backwards-compatible bug fixes.
+
+
+# run:
+# python setup.py sdist
+# twine upload dist/*
