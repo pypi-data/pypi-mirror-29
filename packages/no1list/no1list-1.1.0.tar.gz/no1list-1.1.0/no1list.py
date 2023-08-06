e2eb66d7f549419e3e8d3a644db9a26b50e99b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:45 +0000

    Add package no1list/1.1.0/no1list-1.1.0.tar.gz

---
+++ packages/no1list/no1list-1.1.0.tar.gz/no1list-1.1.0/no1list.py
@@ -0,0 +1,16 @@
+import sys
+
+movie = [1,[2,2],3,[4,4,[5,5]]]
+'''This the nested.py module, and it provides one function called print_lol() which prints lists that may or may not included nested list'''
+def print_lol(list1,indent = False ,level = 0):
+
+	for each_item in list1:
+		if isinstance(each_item,list):
+			print_lol(each_item, indent , level+1)
+		else:
+			if indent:
+				for _  in range(level):
+					print('\t',end = '')
+
+			print(each_item)
+
