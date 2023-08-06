9431f24d769d124e99a9217b337d52a2a1d92f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:53 +0000

    Add package onegov.gazette/1.11.8/onegov.gazette-1.11.8-py3-none-any.whl

---
+++ packages/onegov.gazette/onegov.gazette-1.11.8-py3-none-any.whl/onegov/gazette/models/__init__.py
@@ -0,0 +1,18 @@
+from onegov.gazette.models.category import Category
+from onegov.gazette.models.issue import Issue
+from onegov.gazette.models.issue import IssueName
+from onegov.gazette.models.notice import GazetteNotice
+from onegov.gazette.models.organization import Organization
+from onegov.gazette.models.organization import OrganizationMove
+from onegov.gazette.models.principal import Principal
+
+
+__all__ = [
+    'Category',
+    'GazetteNotice',
+    'Issue',
+    'IssueName',
+    'Organization',
+    'OrganizationMove',
+    'Principal',
+]
