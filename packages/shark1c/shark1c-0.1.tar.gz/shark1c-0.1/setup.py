36e2c7681dc180382d8093b6c799b6e6f0a168fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:34 +0000

    Add package shark1c/0.1/shark1c-0.1.tar.gz

---
+++ packages/shark1c/shark1c-0.1.tar.gz/shark1c-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+import shark1c
+
+setup(
+    name='shark1c',
+    version=shark1c.__version__,
+    py_modules=['shark1c'],
+    install_requires=[
+        'pycli',
+        'scapy',
+
+    ],
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    entry_points='''
+        [console_scripts]
+        shark1c=shark1c:run_sniffer
+    ''',
+)
