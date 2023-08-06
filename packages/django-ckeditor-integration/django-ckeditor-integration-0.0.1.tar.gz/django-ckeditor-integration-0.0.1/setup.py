7e9dbe99636f2ad9ebac09ade4f135051102b2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:45 +0000

    Add package django-ckeditor-integration/0.0.1/django-ckeditor-integration-0.0.1.tar.gz

---
+++ packages/django-ckeditor-integration/django-ckeditor-integration-0.0.1.tar.gz/django-ckeditor-integration-0.0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='django-ckeditor-integration',
+    version='0.0.1',
+    description='Django CKEditor integration',
+    author='Aleksei Iakovlev',
+    author_email='scorpius98.alex@gmail.com',
+    url='https://github.com/SkoSC/django-ckeditor-integration',
+    zip_safe=False,
+    install_requires=[
+
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+    ],
+)
