53b5a0a676839cb45a2ae5dd71999cbdb5323676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:35 +0000

    Add package pdl/0.2.0/pdl-0.2.0.tar.gz

---
+++ packages/pdl/pdl-0.2.0.tar.gz/pdl-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="pdl",
+    version="0.2.0",
+    author="Zero to singularity",
+    author_email="jan@zerotosingularity.com",
+    url="https://github.com/zerotosingularity/pdl",
+    description="Python Download Lib",
+    license="MIT",
+    packages=['pdl']
+)
