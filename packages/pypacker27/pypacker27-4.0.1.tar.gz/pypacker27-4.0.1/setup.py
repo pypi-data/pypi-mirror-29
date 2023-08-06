d37bcfd9f79d901cefa73e071d236078b76d2d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:14 +0000

    Add package pypacker27/4.0.1/pypacker27-4.0.1.tar.gz

---
+++ packages/pypacker27/pypacker27-4.0.1.tar.gz/pypacker27-4.0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+setup(name="pypacker27",
+	version="4.0.1",
+	author="Michael Stahn",
+	author_email="michael.stahn.42@gmail.com",
+	url="https://github.com/mike01/pypacker/tree/python27",
+	description="Pypacker: The fast and simple packet creating and parsing module",
+	license="BSD",
+	packages=[
+		"pypacker",
+		"pypacker.layer12",
+		"pypacker.layer3",
+		"pypacker.layer4",
+		"pypacker.layer567"
+	],
+	package_data={"pypacker": ["oui_stripped.txt"]},
+	classifiers=[
+		"Development Status :: 6 - Mature",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: BSD License",
+		"Natural Language :: English",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: Implementation :: CPython",
+		"Programming Language :: Python :: Implementation :: PyPy"
+	],
+    python_requires=">=2.7"
+)
