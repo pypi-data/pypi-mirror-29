f2270dab50a85baf4f2fad3b4c724fe299873d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:43 +0000

    Add package alerta/5.0.12/alerta-5.0.12-py2.py3-none-any.whl

---
+++ packages/alerta/alerta-5.0.12-py2.py3-none-any.whl/alertaclient/commands/cmd_login.py
@@ -0,0 +1,21 @@
+
+import click
+
+from alertaclient.auth import save_token
+from alertaclient.exceptions import AuthError
+
+
+@click.command('login', short_help='Login with user credentials')
+@click.option('--username', metavar='EMAIL', prompt='Email', help='Email address')
+@click.password_option(confirmation_prompt=False, help='Password')
+@click.pass_obj
+def cli(obj, username, password):
+    """Use this tool with username/password instead of API key."""
+    client = obj['client']
+    r = client.login(username, password)
+    if 'token' in r:
+        token = r['token']
+    else:
+        raise AuthError
+    save_token(client.endpoint, username, token)
+    click.echo('Logged in as {}'.format(username))
