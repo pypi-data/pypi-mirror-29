fb47075c03f39ee72d84b7919e591530eb8fef74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:45 +0000

    Add package shado_client/0.0.4/shado_client-0.0.4.tar.gz

---
+++ packages/shado_client/shado_client-0.0.4.tar.gz/shado_client-0.0.4/shado_client/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from shado_client.api.account_api import AccountApi
+from shado_client.api.group_api import GroupApi
+from shado_client.api.invitation_api import InvitationApi
+from shado_client.api.item_api import ItemApi
+from shado_client.api.membership_api import MembershipApi
+from shado_client.api.plugin_api import PluginApi
+from shado_client.api.plugin_instance_api import PluginInstanceApi
+from shado_client.api.result_api import ResultApi
+from shado_client.api.sharing_api import SharingApi
