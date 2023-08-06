a77a1da736fb01a6925db4805083dd8ace7fd612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:36 +0000

    Add package robotpy-wpilib-utilities/2018.1.3/robotpy-wpilib-utilities-2018.1.3.tar.gz

---
+++ packages/robotpy-wpilib-utilities/robotpy-wpilib-utilities-2018.1.3.tar.gz/robotpy-wpilib-utilities-2018.1.3/magicbot/magic_button.py
@@ -0,0 +1,13 @@
+
+class MagicButton:
+    '''
+        def createObjects(self):
+            self.joystick1.button3.whenPressed(self.foo.bar)
+            
+        def setupButtons(self, joystick):
+            joystick[0].button[1].whenPressed(self.arm.shoot)
+            
+            joystick[0].button[1].whenPressed(self.foo.bar)
+            
+    '''
+    
