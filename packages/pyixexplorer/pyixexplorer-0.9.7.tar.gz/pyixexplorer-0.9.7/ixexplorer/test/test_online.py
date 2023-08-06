885239495a3bba5eba9905c08e89e9e448ffb8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:28 +0000

    Add package pyixexplorer/0.9.7/pyixexplorer-0.9.7.tar.gz

---
+++ packages/pyixexplorer/pyixexplorer-0.9.7.tar.gz/pyixexplorer-0.9.7/ixexplorer/test/test_online.py
@@ -0,0 +1,107 @@
+"""
+TestCenter package tests that require actual Ixia chassis and active ports.
+
+Test setup:
+Two Ixia ports connected back to back.
+
+@author yoram@ignissoft.com
+"""
+
+from os import path
+import time
+import json
+
+from ixexplorer.ixe_statistics_view import IxePortsStats, IxeStreamsStats, IxeCapFileFormat
+from ixexplorer.test.test_base import IxeTestBase
+from ixexplorer.ixe_port import StreamWarningsError
+
+
+class IxeTestOnline(IxeTestBase):
+
+    def testPortStats(self):
+        cfg1 = path.join(path.dirname(__file__), 'configs/stats_config_1.prt')
+        cfg2 = path.join(path.dirname(__file__), 'configs/stats_config_2.prt')
+        self._load_config(cfg1, cfg2)
+
+        self.ixia.session.start_transmit()
+        port_stats = IxePortsStats(self.ixia.session)
+        port_stats.read_stats()
+        print(json.dumps(port_stats.statistics, indent=1, sort_keys=True))
+        assert(port_stats.statistics[self.port1]['framesSent'] > 0)
+        assert(port_stats.statistics[self.port2]['framesSent_rate'] > 0)
+        self.ixia.session.stop_transmit()
+
+        self.ports[self.port1].start_transmit()
+        time.sleep(4)
+        self.ports[self.port1].stop_transmit()
+        port1_stats = self.ports[self.port1].read_stats('framesSent', 'framesReceived')
+        print(json.dumps(port1_stats, indent=1))
+        assert(port1_stats['framesSent'] > 0)
+        assert(port1_stats['framesSent_rate'] == 0)
+
+    def testStreamStats(self):
+        cfg1 = path.join(path.dirname(__file__), 'configs/stats_config_1.prt')
+        cfg2 = path.join(path.dirname(__file__), 'configs/stats_config_2.prt')
+        self._load_config(cfg1, cfg2)
+
+        self.ixia.session.start_transmit()
+        time.sleep(2)
+        self.ixia.session.stop_transmit()
+
+        stream_stats = IxeStreamsStats(self.ixia.session)
+        self.ports[self.port1].streams[1].rx_ports = [self.ports[self.port2]]
+        self.ports[self.port1].streams[2].rx_ports = [self.ports[self.port2]]
+        self.ports[self.port2].rx_ports = self.ports[self.port1]
+        stream_stats.read_stats()
+        print(json.dumps(stream_stats.statistics, indent=1, sort_keys=True))
+        assert(stream_stats.statistics[str(self.ports[self.port1].streams[1])]['tx']['framesSent'] > 0)
+        assert(stream_stats.statistics[str(self.ports[self.port1].streams[1])]['tx']['frameRate'] == 0)
+        assert(stream_stats.statistics[str(self.ports[self.port1].streams[1])]['rx']['totalFrames'] > 0)
+        assert(stream_stats.statistics[str(self.ports[self.port1].streams[1])]['rx']['frameRate'] == 0)
+
+        print(json.dumps(self.ports[self.port1].read_stream_stats('totalFrames'), indent=1))
+        print(json.dumps(self.ports[self.port1].streams[1].read_stats('totalFrames'), indent=1))
+
+    def testCapture(self):
+        cfg1 = path.join(path.dirname(__file__), 'configs/cap_config.prt')
+        cfg2 = path.join(path.dirname(__file__), 'configs/cap_config.prt')
+        self._load_config(cfg1, cfg2)
+
+        self.ports[self.port1].start_capture()
+        self.ports[self.port2].start_transmit(blocking=True)
+        # Order matters to make sure nPackets is refreshed, so we read port2, port1 and then port1, port2
+        nPackets = self.ports[self.port2].stop_capture()
+        assert(nPackets == 0)
+        assert(not self.ports[self.port1].get_cap_file())
+        nPackets = self.ports[self.port1].stop_capture()
+        assert(nPackets == 800)
+        assert(not self.ports[self.port2].get_cap_file())
+        print(self.ports[self.port1].get_cap_frames(1, 3, 5))
+
+        self.ports[self.port1].clear_stats()
+        self.ports[self.port2].clear_stats()
+
+        self.ixia.session.start_capture()
+        self.ixia.session.start_transmit()
+        self.ixia.session.stop_transmit()
+        nPackets = self.ixia.session.stop_capture(cap_file_name='c:/temp/ixia__session_cap',
+                                                  cap_file_format=IxeCapFileFormat.txt)
+        for name, port in self.ports.items():
+            assert(nPackets[port] < 800)
+            print(name)
+            print(port.cap_file_name)
+            print(port.get_cap_file())
+
+    def testLongCapture(self):
+        cfg1 = path.join(path.dirname(__file__), 'configs/long_frame_config.prt')
+        cfg2 = path.join(path.dirname(__file__), 'configs/long_frame_config.prt')
+        try:
+            self._load_config(cfg1, cfg2)
+        except StreamWarningsError as _:
+            pass
+
+        self.ixia.session.start_capture()
+        self.ixia.session.start_transmit(blocking=True)
+        self.ixia.session.stop_capture(cap_file_name=None, cap_file_format=IxeCapFileFormat.mem)
+        for p in range(1, self.ports[self.port2].capture.nPackets + 1):
+            print('frame len = {}'.format(len(self.ports[self.port2].get_cap_frames(p)[0]) / 3 + 1))
