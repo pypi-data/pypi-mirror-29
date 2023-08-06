d6a4f83888fe1bd7494fdd944fb7c0fa264a1a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:16 +0000

    Add package pycoraldb/1.1.4.23/pycoraldb-1.1.4.23.tar.gz

---
+++ packages/pycoraldb/pycoraldb-1.1.4.23.tar.gz/pycoraldb-1.1.4.23/compress.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import gzip
+import StringIO
+
+
+def smart_compress_gzip(data):
+    ret = data
+    if len(data) >= 1024:
+        buf = StringIO.StringIO()
+        f = gzip.GzipFile(fileobj=buf, mode='wb')
+        f.write(data)
+        f.flush()
+        f.close()
+        zip_data = buf.getvalue()
+        buf.close()
+        if len(zip_data) < len(data):
+            ret = zip_data
+    return ret
+
+
+def smart_uncompress_gzip(data):
+    ret = data
+    if len(data) >= 2 and ord(data[0]) == 0x1F and ord(data[1]) == 0x8B:
+        buf = StringIO.StringIO(data)
+        f = gzip.GzipFile(fileobj=buf)
+        ret = f.read()
+        f.close()
+    return ret
+
+
+if __name__ == '__main__':
+    data = 'abc'
+    s = smart_uncompress_gzip(smart_compress_gzip(data))
+    if data == s:
+        print 'test ok: %s' % (s)
+    else:
+        print 'test failed: %s' % (s)
