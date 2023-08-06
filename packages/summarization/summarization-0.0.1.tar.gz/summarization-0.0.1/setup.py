79a0e307e90ace6dfa0b1b86f1d2af4464730dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:53 +0000

    Add package summarization/0.0.1/summarization-0.0.1.tar.gz

---
+++ packages/summarization/summarization-0.0.1.tar.gz/summarization-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='summarization',
+    version='0.0.1',
+    packages=['summarization'],
+    url='https://github.com/autosummarization/summarization',
+    license='MIT',
+    author='taibai',
+    author_email='connection@autosummarization.com',
+    description='Automatic summarization tools'
+)
