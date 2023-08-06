a4b3846224b8654221be00c401fbe1beddec31e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:44 +0000

    Add package azure-iot-edge-dev-tool/0.64.0/azure_iot_edge_dev_tool-0.64.0-py2.py3-none-any.whl

---
+++ packages/azure-iot-edge-dev-tool/azure_iot_edge_dev_tool-0.64.0-py2.py3-none-any.whl/iotedgedev/iothub.py
@@ -0,0 +1,16 @@
+class IoTHub:
+    def __init__(self, envvars, output, utility):
+        self.envvars = envvars
+        self.envvars.check()
+        self.output = output
+        self.utility = utility
+
+    def monitor_events(self):
+
+        try:
+            self.utility.call_proc(['iothub-explorer', '--login', self.envvars.IOTHUB_CONNECTION_STRING,
+                                    'monitor-events', self.envvars.DEVICE_CONNECTION_INFO.DeviceId], shell=not self.envvars.is_posix())
+        except Exception as ex:
+            self.output.error(
+                "Problem while trying to call iothub-explorer. Please ensure that you have installed the iothub-explorer npm package with: npm i -g iothub-explorer.")
+            self.output.error(str(ex))
