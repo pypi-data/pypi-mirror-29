396dd80102dcf7e7b03e3811cb135c37e9c1e806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:39 +0000

    Add package RATTSeminarFri/0.1/RATTSeminarFri-0.1.tar.gz

---
+++ packages/RATTSeminarFri/RATTSeminarFri-0.1.tar.gz/RATTSeminarFri-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'RATTSeminarFri',
+  packages = ['RATTSeminarFri'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A guess game',
+  author = 'Sam',
+  author_email = 'samueltwum1@gmail.com',
+  url = 'https://github.com/samueltwum1/guess1to20', # use the URL to the github repo
+  download_url = 'https://github.com/samueltwum1/guess1to20/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['gaming', 'smart', 'women'], # arbitrary keywords
+  classifiers = [],
+)
