01a3366050fe6a1a6c861c177ad93f825c09e864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:37 +0000

    Add package netflix-spectator-py/0.1/netflix-spectator-py-0.1.tar.gz

---
+++ packages/netflix-spectator-py/netflix-spectator-py-0.1.tar.gz/netflix-spectator-py-0.1/netflix/spectator/__init__.py
@@ -0,0 +1,7 @@
+
+from .clock import Clock        # noqa: F401
+from .clock import ManualClock  # noqa: F401
+from .clock import SystemClock  # noqa: F401
+from .registry import Registry
+
+GlobalRegistry = Registry()
