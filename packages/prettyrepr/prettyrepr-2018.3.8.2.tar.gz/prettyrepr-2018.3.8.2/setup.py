8b1f5a1422c62ba47994ede94a45c3d16b04659d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:54 +0000

    Add package prettyrepr/2018.3.8.2/prettyrepr-2018.3.8.2.tar.gz

---
+++ packages/prettyrepr/prettyrepr-2018.3.8.2.tar.gz/prettyrepr-2018.3.8.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='prettyrepr',
+    version='2018.03.08.2',
+    py_modules=['prettyrepr'],
+    author='Bartek Brak',
+    author_email='bartek0brak@gmail.com',
+    install_requires=['Pygments'],
+    url='https://github.com/bartekbrak/prettyrepr',
+)
