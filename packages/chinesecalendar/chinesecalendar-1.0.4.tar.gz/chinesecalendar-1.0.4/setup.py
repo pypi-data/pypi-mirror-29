13dbe0c5e6b8297bed1524e9e14d774dcce5a502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:45 +0000

    Add package chinesecalendar/1.0.4/chinesecalendar-1.0.4.tar.gz

---
+++ packages/chinesecalendar/chinesecalendar-1.0.4.tar.gz/chinesecalendar-1.0.4/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import pypandoc
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = [_.strip() for _ in f.readlines() if _]
+
+setup(
+    name='chinesecalendar',
+    version='1.0.4',
+    description='check if some day is holiday in China',
+    long_description=pypandoc.convert('README.md', 'rst'),
+    author='Lirian Su',
+    author_email='liriansu@gmail.com',
+    url='https://github.com/LKI/chinese-calendar',
+    license='MIT License',
+    packages=['chinese_calendar'],
+    install_requires=requirements,
+)
