effcc4b1963adcdeaaa65012b02d74260151b672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:29 +0000

    Add package pyBAR/3.0.2/pyBAR-3.0.2-py2-none-any.whl

---
+++ packages/pyBAR/pyBAR-3.0.2-py2-none-any.whl/pyBAR-3.0.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+PyBAR is a versatile readout and test system for the ATLAS FEI4 pixel readout chip.
+It uses the Basil framework to access the readout hardware. PyBAR's FPGA firmware and host software includes support for different hardware platforms.
+
---
+++ packages/pyBAR/pyBAR-3.0.2-py2-none-any.whl/pybar/scans/scan_init.py
@@ -0,0 +1,26 @@
+import logging
+
+from pybar.fei4_run_base import Fei4RunBase
+from pybar.run_manager import RunManager
+
+
+class InitScan(Fei4RunBase):
+    '''Init scan
+    '''
+    _default_run_conf = {
+        "broadcast_commands": False,
+        "threaded_scan": False
+    }
+
+    def configure(self):
+        pass
+
+    def scan(self):
+        pass
+
+    def analyze(self):
+        pass
+
+
+if __name__ == "__main__":
+    RunManager('configuration.yaml').run_run(InitScan)
