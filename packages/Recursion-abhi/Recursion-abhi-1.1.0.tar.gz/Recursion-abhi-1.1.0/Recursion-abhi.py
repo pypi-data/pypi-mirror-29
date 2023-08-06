a5c1e0f56c5be8bfb5ca8cdcdfe6a12e9997921a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:37 +0000

    Add package Recursion-abhi/1.1.0/Recursion-abhi-1.1.0.tar.gz

---
+++ packages/Recursion-abhi/Recursion-abhi-1.1.0.tar.gz/Recursion-abhi-1.1.0/Recursion-abhi.py
@@ -0,0 +1,14 @@
+"""This is the nester.py module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists."""
+
+
+
+
+def print_List(Lists,level):
+    for list_element in Lists :
+        if isinstance(list_element,list):
+            print_List(list_element,level+1)
+        else:
+            for tabnum in range(level):
+                print("\t",end="")
+            print(list_element)
