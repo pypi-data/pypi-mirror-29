269db809f2bcaf20379469de2996c5aaa422108e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:59 +0000

    Add package athletestmodel/1.0.0/athletestmodel-1.0.0.zip

---
+++ packages/athletestmodel/athletestmodel-1.0.0.zip/athletestmodel-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'athletestmodel',
+    version = '1.0.0',
+    py_modules = ['athletemodel'],
+    author = 'qsl',
+    author_email = 'Qsl_xj@outlook.com',
+    url = "http://test.com",
+    description = '运动员数据建模'
+    )
