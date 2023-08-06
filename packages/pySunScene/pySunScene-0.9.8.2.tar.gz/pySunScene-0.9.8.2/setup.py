05b198f1e461500d88d918db3d02a2b071b9d198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:47 +0000

    Add package pySunScene/0.9.8.2/pySunScene-0.9.8.2.tar.gz

---
+++ packages/pySunScene/pySunScene-0.9.8.2.tar.gz/pySunScene-0.9.8.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pySunScene',
+    version='0.9.8.2',
+    py_modules=['raytrace'],
+    url='',
+    install_requires=['numpy==1.12', 'enum', 'dill'],
+    license='MIT',
+    author='Gabriel Cardona, Ramon Pujol',
+    author_email='gabriel.cardona@uib.es, ramon.pujol@uib.es',
+    description='',
+)
+
