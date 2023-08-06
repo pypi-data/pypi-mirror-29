24c198e09371681fcc4d8961b773104632746ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:55 +0000

    Add package penaltymodel/0.13.1/penaltymodel-0.13.1.tar.gz

---
+++ packages/penaltymodel/penaltymodel-0.13.1.tar.gz/penaltymodel-0.13.1/penaltymodel/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+from penaltymodel.classes import *
+import penaltymodel.classes
+
+from penaltymodel.exceptions import *
+import penaltymodel.exceptions
+
+from penaltymodel.interface import *
+import penaltymodel.interface
+
+from penaltymodel.package_info import *
+import penaltymodel.package_info
