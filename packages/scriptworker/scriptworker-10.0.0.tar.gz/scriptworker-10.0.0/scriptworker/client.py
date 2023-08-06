5e11738f7084222fffe5059e11e6ff63a70ace13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:20 +0000

    Add package scriptworker/10.0.0/scriptworker-10.0.0.tar.gz

---
+++ packages/scriptworker/scriptworker-10.0.0.tar.gz/scriptworker-10.0.0/scriptworker/client.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+"""Scripts running in scriptworker will use functions in this file.
+
+This module should be largely standalone.  This should only depend on
+scriptworker.exceptions and scriptworker.constants, or other standalone
+modules, to avoid circular imports.
+
+"""
+import jsonschema
+import os
+from urllib.parse import unquote
+
+from scriptworker.constants import STATUSES
+from scriptworker.exceptions import ScriptWorkerTaskException
+from scriptworker.utils import load_json_or_yaml, match_url_regex
+
+
+def get_task(config):
+    """Read the task.json from work_dir.
+
+    Args:
+        config (dict): the running config, to find work_dir.
+
+    Returns:
+        dict: the contents of task.json
+
+    Raises:
+        ScriptWorkerTaskException: on error.
+
+    """
+    path = os.path.join(config['work_dir'], "task.json")
+    message = "Can't read task from {}!\n%(exc)s".format(path)
+    contents = load_json_or_yaml(path, is_path=True, message=message)
+    return contents
+
+
+def validate_json_schema(data, schema, name="task"):
+    """Given data and a jsonschema, let's validate it.
+
+    This happens for tasks and chain of trust artifacts.
+
+    Args:
+        data (dict): the json to validate.
+        schema (dict): the jsonschema to validate against.
+        name (str, optional): the name of the json, for exception messages.
+            Defaults to "task".
+
+    Raises:
+        ScriptWorkerTaskException: on failure
+
+    """
+    try:
+        jsonschema.validate(data, schema)
+    except jsonschema.exceptions.ValidationError as exc:
+        raise ScriptWorkerTaskException(
+            "Can't validate {} schema!\n{}".format(name, str(exc)),
+            exit_code=STATUSES['malformed-payload']
+        )
+
+
+def validate_artifact_url(valid_artifact_rules, valid_artifact_task_ids, url):
+    """Ensure a URL fits in given scheme, netloc, and path restrictions.
+
+    If we fail any checks, raise a ScriptWorkerTaskException with
+    ``malformed-payload``.
+
+    Args:
+        valid_artifact_rules (tuple): the tests to run, with ``schemas``, ``netlocs``,
+            and ``path_regexes``.
+        valid_artifact_task_ids (list): the list of valid task IDs to download from.
+        url (str): the url of the artifact.
+
+    Returns:
+        str: the ``filepath`` of the path regex.
+
+    Raises:
+        ScriptWorkerTaskException: on failure to validate.
+
+    """
+    def callback(match):
+        path_info = match.groupdict()
+        # make sure we're pointing at a valid task ID
+        if 'taskId' in path_info and \
+                path_info['taskId'] not in valid_artifact_task_ids:
+            return
+        if 'filepath' not in path_info:
+            return
+        return path_info['filepath']
+
+    filepath = match_url_regex(valid_artifact_rules, url, callback)
+    if filepath is None:
+        raise ScriptWorkerTaskException(
+            "Can't validate url {}".format(url),
+            exit_code=STATUSES['malformed-payload']
+        )
+    return unquote(filepath).lstrip('/')
---
+++ packages/scriptworker/scriptworker-10.0.0.tar.gz/scriptworker-10.0.0/scriptworker/test/test_client.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""Test scriptworker.client
+"""
+import arrow
+from copy import deepcopy
+import json
+import os
+import pytest
+from shutil import copyfile
+import scriptworker.client as client
+from scriptworker.constants import DEFAULT_CONFIG
+from scriptworker.exceptions import ScriptWorkerTaskException
+from . import tmpdir
+
+assert tmpdir  # silence pyflakes
+
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+PARTIAL_CREDS = os.path.join(TEST_DATA_DIR, "partial_credentials.json")
+CLIENT_CREDS = os.path.join(TEST_DATA_DIR, "client_credentials.json")
+SCHEMA = os.path.join(TEST_DATA_DIR, "basic_schema.json")
+BASIC_TASK = os.path.join(TEST_DATA_DIR, "basic_task.json")
+
+
+# constants helpers and fixtures {{{1
+# LEGAL_URLS format:
+#  1. valid_artifact_rules: tuple-of-dicts with `schemes`, `netlocs`, and `path_regexes`
+#  2. valid_artifact_task_ids: list
+#  3. url to test
+#  4. expected `filepath` return value from `validate_artifact_url()`
+LEGAL_URLS = ((
+    deepcopy(DEFAULT_CONFIG['valid_artifact_rules']),
+    ["VALID_TASK_ID1", "VALID_TASK_ID2"],
+    "https://queue.taskcluster.net/v1/task/VALID_TASK_ID2/artifacts/FILE_DIR%2FFILE_PATH",
+    "FILE_DIR/FILE_PATH",
+), (
+    ({
+        'schemes': ("ftp", "http"),
+        'netlocs': ("example.com", "localhost"),
+        'path_regexes': ('(?P<filepath>.*.baz)', ),
+    }, ),
+    [],
+    "http://localhost/FILE/PATH.baz",
+    "FILE/PATH.baz",
+))
+
+# ILLEGAL_URLS format:
+#  1. valid_artifact_rules: dict with `schemes`, `netlocs`, and `path_regexes`
+#  2. valid_artifact_task_ids: list
+#  3. url to test
+ILLEGAL_URLS = ((
+    deepcopy(DEFAULT_CONFIG['valid_artifact_rules']),
+    ["VALID_TASK_ID1", "VALID_TASK_ID2"],
+    "https://queue.taskcluster.net/v1/task/INVALID_TASK_ID/artifacts/FILE_PATH"
+), (
+    deepcopy(DEFAULT_CONFIG['valid_artifact_rules']),
+    ["VALID_TASK_ID1", "VALID_TASK_ID2"],
+    "https://queue.taskcluster.net/v1/task/VALID_TASK_ID1/BAD_FILE_PATH"
+), (
+    deepcopy(DEFAULT_CONFIG['valid_artifact_rules']),
+    ["VALID_TASK_ID1", "VALID_TASK_ID2"],
+    "BAD_SCHEME://queue.taskcluster.net/v1/task/VALID_TASK_ID1/artifacts/FILE_PATH"
+), (
+    deepcopy(DEFAULT_CONFIG['valid_artifact_rules']),
+    ["VALID_TASK_ID1", "VALID_TASK_ID2"],
+    "https://BAD_NETLOC/v1/task/VALID_TASK_ID1/artifacts/FILE_PATH"
+), (
+    ({'schemes': ['https'], 'netlocs': ['example.com'],
+      # missing filepath
+      'path_regexes': ['.*BAD_REGEX.*']}, ),
+    [],
+    "https://example.com/BAD_REGEX",
+))
+
+
+@pytest.fixture(scope='function')
+def config(tmpdir):
+    work_dir = os.path.join(tmpdir, "work")
+    os.makedirs(work_dir)
+    return {
+        'work_dir': work_dir,
+        'log_dir': os.path.join(tmpdir, "log"),
+        'artifact_dir': os.path.join(tmpdir, "artifact"),
+        'task_log_dir': os.path.join(tmpdir, "artifact", "public", "logs"),
+        'provisioner_id': 'provisioner_id',
+        'worker_type': 'worker_type',
+    }
+
+
+@pytest.fixture(scope='function')
+def schema():
+    with open(SCHEMA, "r") as fh:
+        return json.load(fh)
+
+
+def populate_credentials(config, sources, start=None):
+    start = start or arrow.utcnow().replace(minutes=-20)
+    for count, path in enumerate(sources):
+        new_time = start.replace(minutes=count)
+        copyfile(path, os.path.join(config['work_dir'], "credentials.{}.json".format(new_time.timestamp)))
+
+
+def no_sleep(*args, **kwargs):
+    return 0
+
+
+# tests {{{1
+def test_get_missing_task(config):
+    with pytest.raises(ScriptWorkerTaskException):
+        client.get_task(config)
+
+
+def test_get_task(config):
+    copyfile(BASIC_TASK, os.path.join(config['work_dir'], "task.json"))
+    assert client.get_task(config)["this_is_a_task"] is True
+
+
+def test_validate_task(schema):
+    with open(BASIC_TASK, "r") as fh:
+        task = json.load(fh)
+    client.validate_json_schema(task, schema)
+
+
+def test_invalid_task(schema):
+    with open(BASIC_TASK, "r") as fh:
+        task = json.load(fh)
+    with pytest.raises(ScriptWorkerTaskException):
+        client.validate_json_schema({'foo': task}, schema)
+
+
+@pytest.mark.parametrize("valid_artifact_rules,valid_artifact_task_ids,url,expected", LEGAL_URLS)
+def test_artifact_url(valid_artifact_rules, valid_artifact_task_ids, url, expected):
+    value = client.validate_artifact_url(valid_artifact_rules, valid_artifact_task_ids, url)
+    assert value == expected
+
+
+@pytest.mark.parametrize("valid_artifact_rules,valid_artifact_task_ids,url", ILLEGAL_URLS)
+def test_bad_artifact_url(valid_artifact_rules, valid_artifact_task_ids, url):
+    with pytest.raises(ScriptWorkerTaskException):
+        client.validate_artifact_url(valid_artifact_rules, valid_artifact_task_ids, url)
