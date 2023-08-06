56321589794b3d98a6822bf7aca1bc6e90cb203f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:00 +0000

    Add package noisyduck/0.2.1/noisyduck-0.2.1-py2.py3-none-any.whl

---
+++ packages/noisyduck/noisyduck-0.2.1-py2.py3-none-any.whl/noisyduck/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Noisy Duck."""
+
+__author__ = """Nathan Wukie"""
+__email__ = 'nathan.wukie@gmail.com'
+__version__ = '0.2.1'
+
+from . import annulus
+from . import filter
+from .io import write_decomposition
+
+# silence flake8 PEP8 violation
+__all__ = ['annulus', 'filter', 'write_decomposition']
