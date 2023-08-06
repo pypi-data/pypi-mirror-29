2ebfc0a76e112c7144b5dc82fe8c85a07ab5c70d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:58 +0000

    Add package RTool/0.0.1.9.8/RTool-0.0.1.9.8.tar.gz

---
+++ packages/RTool/RTool-0.0.1.9.8.tar.gz/RTool-0.0.1.9.8/RTool/test.py
@@ -0,0 +1,15 @@
+'''
+this is a hierarchy test
+'''
+
+import sys, os, time
+rootPath = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.split(rootPath)[0])
+
+import RTool.prototype
+'''
+import RTool.util.importer as i
+
+i.ImportHandler(["Cython"])
+'''
+
