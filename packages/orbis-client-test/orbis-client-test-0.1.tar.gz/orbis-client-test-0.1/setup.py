e18b82a292f66ef0725ba58c3b01f77948cbc500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:16 +0000

    Add package orbis-client-test/0.1/orbis-client-test-0.1.tar.gz

---
+++ packages/orbis-client-test/orbis-client-test-0.1.tar.gz/orbis-client-test-0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'orbis-client-test',
+  packages = ['orbis-client-test'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Chanwoo Lee',
+  author_email = 'cjswosa2@gmail.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/leechanwoo/orbistest/blob/master/tfclient_package/package.tar', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+  install_requires=[
+      'tensorflow>=1.4',
+      'scipy>=0.9',
+      'numpy>=1.0'
+      ],
+  entry_points={
+      'console_scripts':[
+          'encrypt=tf_client.InferenceRequest'
+          ]
+      },
+)
