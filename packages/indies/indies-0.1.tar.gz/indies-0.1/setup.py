f97c21e259fef6a5005a411578441360e494d0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:14 +0000

    Add package indies/0.1/indies-0.1.tar.gz

---
+++ packages/indies/indies-0.1.tar.gz/indies-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='indies',
+      version='0.1',
+      description='Financial Indicators Library',
+      url='https://github.com/miti0/indies',
+      author='Miro Karpis',
+      author_email='miroslav.karpis@gmail.com',
+      license='Apache License 2.0',
+      packages=['indies'],
+      zip_safe=False)
