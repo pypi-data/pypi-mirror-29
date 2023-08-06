6047f4ff437edddc7cb60d9880f3e339a7245498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:02 +0000

    Add package hn2016-falwa/0.1.7/hn2016_falwa-0.1.7-py3.6.egg

---
+++ packages/hn2016-falwa/hn2016_falwa-0.1.7-py3.6.egg/hn2016_falwa/api.py
@@ -0,0 +1,5 @@
+# This is the api to load all packages
+
+# from solve_uref_noslip_hemisphere import solve_uref_both_bc
+# from static_stability import static_stability
+# from qgpv import compute_qgpv_givenvort
