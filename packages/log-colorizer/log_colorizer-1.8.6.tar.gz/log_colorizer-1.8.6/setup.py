e81d606d80344d841fa5ef9f09b583ecf8992a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:56 +0000

    Add package log-colorizer/1.8.6/log_colorizer-1.8.6.tar.gz

---
+++ packages/log-colorizer/log_colorizer-1.8.6.tar.gz/log_colorizer-1.8.6/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Log colorizer
+"""
+import sys
+
+from setuptools import find_packages, setup
+
+VERSION = "1.8.6"
+
+requires = []
+
+if sys.platform == 'win32':
+    requires.append('colorama')
+
+options = dict(
+    name="log_colorizer",
+    version=VERSION,
+    description="A color formater for python logging",
+    long_description=__doc__,
+    author="Kozea",
+    author_email="florian.mounier@kozea.fr",
+    license="BSD",
+    platforms="Any",
+    use_2to3=True,
+    packages=find_packages(),
+    install_requires=requires,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
+
+setup(**options)
