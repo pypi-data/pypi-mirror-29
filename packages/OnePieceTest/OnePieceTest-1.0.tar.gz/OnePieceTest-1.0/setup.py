602bb82c77fde425fd70436f0c6d660e7badce85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:21 +0000

    Add package OnePieceTest/1.0/OnePieceTest-1.0.tar.gz

---
+++ packages/OnePieceTest/OnePieceTest-1.0.tar.gz/OnePieceTest-1.0/setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='OnePieceTest',
+      version='1.0',
+      description='APP UItest with Python',
+      author='sunm',
+      author_email='www.smeng@gmail.com',
+      url='',
+      packages=['OnePieceTest']
+     )
