946d8f531001bf6d5a99e1a254e9e15e0719059c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:12 +0000

    Add package ravenpackapi/1.0.12/ravenpackapi-1.0.12.tar.gz

---
+++ packages/ravenpackapi/ravenpackapi-1.0.12.tar.gz/ravenpackapi-1.0.12/ravenpackapi/utils/constants.py
@@ -0,0 +1,8 @@
+VERSION = '1.0.12'
+
+JSON_AVAILABLE_FIELDS = {
+    'start_date', 'end_date', 'time_zone',
+    'frequency', 'fields',
+    'having', 'filters',
+    'product', 'product_version',
+}
