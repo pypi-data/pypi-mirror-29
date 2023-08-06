719437a1dea5973cc97c2d3f1bf93e49df8bbf4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:52 +0000

    Add package PriceDb/1.3.1/PriceDb-1.3.1.tar.gz

---
+++ packages/PriceDb/PriceDb-1.3.1.tar.gz/PriceDb-1.3.1/pricedb/download/download.py
@@ -0,0 +1,53 @@
+"""
+Price downloaders and parsers.
+These can be used as the first aid, to populate the price database but are not
+a comprehensive solution.
+"""
+import logging
+from enum import Enum, auto
+
+from pricedb import utils
+
+from .alphavantage import AlphaVantageDownloader
+from .currencyrates import CurrencyRatesRetriever
+from .morningstar import MorningstarDownloader
+from .vanguard_au import VanguardAuDownloader
+
+
+class DownloadAgents(Enum):
+    """ Available agents for price download """
+    alphavantage = auto(),
+    fixerio = auto(),
+    morningstar = auto(),
+    vanguard_au = auto()
+
+
+class PriceDownloader:
+    """ Proxy class for downloading prices """
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def download(self, symbol: str, currency: str, agent: str):
+        """ Download single latest price """
+
+        symbol = symbol.upper()
+        currency = currency.upper()
+        agent = agent.lower()
+
+        namespace, mnemonic = utils.split_symbol(symbol)
+
+        if agent == DownloadAgents.morningstar.name:
+            actor = MorningstarDownloader()
+        elif agent == DownloadAgents.vanguard_au.name:
+            actor = VanguardAuDownloader()
+        elif agent == DownloadAgents.alphavantage.name:
+            actor = AlphaVantageDownloader()
+        elif agent == DownloadAgents.fixerio.name:
+            actor = CurrencyRatesRetriever()
+        else:
+            raise ValueError("No agent specified for price download.")
+
+        actor.logger = self.logger
+        price = actor.download(namespace, mnemonic, currency)
+
+        return price
