e78d317738eb45bcf9e9cc6a34c3669964d8314f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:22 +0000

    Add package tvdb-api-client/0.1.0/tvdb_api_client-0.1.0.tar.gz

---
+++ packages/tvdb-api-client/tvdb_api_client-0.1.0.tar.gz/tvdb_api_client-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+PKG_NAME = 'tvdb_api_client'
+__version__ = '0.1.0'
+
+
+def listify(filename):
+    with open(filename) as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def contents(filename):
+    with open(filename) as f:
+        return f.read()
+
+setup(
+    name=PKG_NAME,
+    packages=[PKG_NAME],
+    version=__version__,
+    description='A python client for TVDB rest API',
+    license='MIT',
+    long_description=contents('README.txt'),
+    author='Stephanos Papanikolopoulos',
+    author_email='spapanik21@gmail.com',
+    url='https://github.com/spapanik/{pkg_name}'.format(pkg_name=PKG_NAME),
+    download_url='https://github.com/spapanik/{pkg_name}/tarball/{ver}'.format(
+        pkg_name=PKG_NAME,
+        ver=__version__,
+    ),
+    install_requires=listify('requirements.txt'),
+    tests_require=listify('requirements_test.txt'),
+    keywords=listify('KEYWORDS.txt'),
+    classifiers=listify('CLASSIFIERS.txt'),
+)
