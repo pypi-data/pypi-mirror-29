b3cb211e2e7727a5b1257a14f4f2370929489ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:04 +0000

    Add package niapy/0.1.3a1/NiaPy-0.1.3a1-py3-none-any.whl

---
+++ packages/niapy/NiaPy-0.1.3a1-py3-none-any.whl/NiaPy/algorithms/basic/__init__.py
@@ -0,0 +1,24 @@
+"""Implementation of basic nature-inspired algorithms."""
+
+from NiaPy.algorithms.basic.ba import BatAlgorithm
+from NiaPy.algorithms.basic.fa import FireflyAlgorithm
+from NiaPy.algorithms.basic.de import DifferentialEvolutionAlgorithm
+from NiaPy.algorithms.basic.fpa import FlowerPollinationAlgorithm
+from NiaPy.algorithms.basic.gwo import GreyWolfOptimizer
+from NiaPy.algorithms.basic.ga import GeneticAlgorithm
+from NiaPy.algorithms.basic.abc import ArtificialBeeColonyAlgorithm
+from NiaPy.algorithms.basic.pso import ParticleSwarmAlgorithm
+from NiaPy.algorithms.basic.cs import CuckooSearchAlgorithm
+
+
+__all__ = [
+    'BatAlgorithm',
+    'FireflyAlgorithm',
+    'DifferentialEvolutionAlgorithm',
+    'FlowerPollinationAlgorithm',
+    'GreyWolfOptimizer',
+    'GeneticAlgorithm',
+    'ArtificialBeeColonyAlgorithm',
+    'ParticleSwarmAlgorithm',
+    'CuckooSearchAlgorithm'
+]
