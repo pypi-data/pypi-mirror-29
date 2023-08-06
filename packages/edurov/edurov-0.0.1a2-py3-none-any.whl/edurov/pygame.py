29547f5654b9e7a975875ccb56920e506b34d581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:02 +0000

    Add package edurov/0.0.1a2/edurov-0.0.1a2-py3-none-any.whl

---
+++ packages/edurov/edurov-0.0.1a2-py3-none-any.whl/edurov/pygame.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import argparse
+from edurov.rov_functions import rov
+from edurov.controller_functions import controller
+from edurov.support import valid_resolution, args_resolution_help, STANDARD_RESOLUTIONS
+
+
+if __name__ == '__main__':
+    choices = {'controller': controller, 'rov': rov}
+    parser = argparse.ArgumentParser(
+        description='Stream video from picamera to machine on same network',
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument(
+        'role',
+        choices=choices,
+        help='which role to play')
+    parser.add_argument(
+        'ip',
+        help="""IP address the server listens at and ROV sends to\nuse "" for server to listen at all ports""",
+        type=str)
+    parser.add_argument(
+        '-p',
+        metavar='PORT',
+        type=int,
+        default=1060,
+        help='IP port (default 1060)')
+    parser.add_argument(
+        '-r',
+        metavar='RESOLUTION',
+        type=str,
+        default='1024x768',
+        help='''resolution, use format WIDTHxHEIGHT or an integer 0-{}\n(default 1024x600)'''.format(len(STANDARD_RESOLUTIONS)-1))
+    parser.add_argument(
+        '-f',
+        action="store_true",
+        help='when used, video will show in fullscreen')
+    parser.add_argument(
+        '--resolutions',
+        action="store_true",
+        help='print the resolutions to use with the -r flag')
+
+    args = parser.parse_args()
+    if args.resolutions:
+        args_resolution_help()
+
+    res = valid_resolution(args.r)
+
+    function_ = choices[args.role]
+    if function_ is rov:
+        rov(host=args.ip,
+            port=args.p,
+            resolution=res)
+    elif function_ is controller:
+        controller(ip=args.ip,
+                   port=args.p,
+                   resolution=res,
+                   fullscreen=args.f)
---
+++ packages/edurov/edurov-0.0.1a2-py3-none-any.whl/edurov/support.py
@@ -0,0 +1,59 @@
+import platform
+import socket
+import struct
+
+if 'raspberrypi' in platform._syscmd_uname('-a'):
+    import fcntl
+
+STANDARD_RESOLUTIONS = ['160x120', '240x160', '640x360', '640x480', '960x540',
+                        '960x640', '1024x576', '1024x600', '1024x768',
+                        '1152x864', '1280x720', '1296x972', '1640x1232',
+                        '1920x1080']
+
+
+def detect_pi():
+    if 'raspberrypi' in platform._syscmd_uname('-a'):
+        return True
+    else:
+        return False
+
+
+def valid_resolution(resolution):
+    if 'x' in resolution:
+        if len(resolution.split('x')) is 2:
+            return resolution
+        else:
+            raise ValueError('Resolution must be WIDTHxHEIGHT or an integer')
+    try:
+        idx = int(resolution)
+        if idx in range(0, len(STANDARD_RESOLUTIONS)):
+            return STANDARD_RESOLUTIONS[idx]
+        else:
+            raise ValueError('Resolution index must be inr range 0-{}, not {}'
+                             .format(len(STANDARD_RESOLUTIONS), idx))
+    except ValueError:
+        raise ValueError('Resolution must be WIDTHxHEIGHT or an integer')
+
+
+def args_resolution_help():
+    print('{:<8} {:<10}'.format('Number', 'Resolution'))
+    for idx, res in enumerate(STANDARD_RESOLUTIONS):
+        print('{:<8} {:<10}'.format(idx, res))
+
+
+def server_ip(port):
+    online_ips = []
+    for interface in [b'eth0', b'wlan0']:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        try:
+            ip = socket.inet_ntoa(fcntl.ioctl(
+                sock.fileno(),
+                0x8915,
+                struct.pack('256s', interface[:15])
+            )[20:24])
+            online_ips.append(ip)
+        except OSError:
+            pass
+        sock.close()
+    return ' or '.join(['{}:{}'.format(ip, port) for ip in online_ips])
