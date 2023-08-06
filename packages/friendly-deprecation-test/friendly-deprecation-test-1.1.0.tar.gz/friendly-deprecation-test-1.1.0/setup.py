bd4f1c2e8d284000ef3badc60388c4c6f7554918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:14 +0000

    Add package friendly-deprecation-test/1.1.0/friendly-deprecation-test-1.1.0.tar.gz

---
+++ packages/friendly-deprecation-test/friendly-deprecation-test-1.1.0.tar.gz/friendly-deprecation-test-1.1.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+
+setup(
+    name='friendly-deprecation-test',
+    version='1.1.0',
+    description='Testing phasing out Python runtimes',
+    author='Anthony Shaw',
+    author_email='anthonyshaw@apache.org',
+    url='https://github.com/tonybaloney/friendly-deprecation-test',
+    packages=['friends'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
