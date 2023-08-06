51b8edc8f06c2c8a34674d38f895beb10e8e647d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:07 +0000

    Add package wujietextProject/1.0.1/wujietextProject-1.0.1.tar.gz

---
+++ packages/wujietextProject/wujietextProject-1.0.1.tar.gz/wujietextProject-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name ='wujietextProject',
+        version='1.0.1',
+        py_modules=['wujietextProject'],
+        author='holybird',
+        author_email='wujie19891224a@gmail.com',
+        url='',
+        description='try to updata',
+    )
