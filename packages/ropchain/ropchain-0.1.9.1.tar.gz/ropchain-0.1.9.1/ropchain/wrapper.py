76eeb34c2e75c80c4c114696b4476e3affc0a2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:45 +0000

    Add package ropchain/0.1.9.1/ropchain-0.1.9.1.tar.gz

---
+++ packages/ropchain/ropchain-0.1.9.1.tar.gz/ropchain-0.1.9.1/ropchain/wrapper.py
@@ -0,0 +1,32 @@
+import libropchain
+from register import *
+
+def toCharVec(s):
+    ret = ropchain.CharVec()
+    for c in s:
+        ret.append(c)
+    return ret
+
+def toRegValue(dest):
+    ret = ropchain.RegValue()
+    for k in dest:
+        ret[k] = dest[k]
+    return ret
+
+def toInsnStr(gadgetDict):
+    ret = ropchain.InsnStr()
+    for k in gadgetDict:
+        ret[k] = gadgetDict[k]
+    return ret
+
+def solve(_dests, fileName, base, _avoids):
+    avoids = toCharVec(_avoids)
+    dests = toRegValue(_dests)
+    return ropchain.solve(dests, fileName, base, avoids)
+
+#For Debug
+def solveFromDict(_dests, _gadgetDict, base, _avoids):
+    avoids = toCharVec(_avoids)
+    dests = toRegValue(_dests)
+    gadgetDict = toInsnStr(_gadgetDict)
+    return ropchain.solveWithMap(dests, gadgetDict, base, avoids)
