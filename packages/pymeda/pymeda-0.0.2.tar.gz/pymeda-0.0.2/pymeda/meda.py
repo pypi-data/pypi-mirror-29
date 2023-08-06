719868bab785a303657e84ce51751cfcf7d57cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:00 +0000

    Add package pymeda/0.0.2/pymeda-0.0.2.tar.gz

---
+++ packages/pymeda/pymeda-0.0.2.tar.gz/pymeda-0.0.2/pymeda/meda.py
@@ -0,0 +1,161 @@
+import numpy as np
+import pandas as pd
+import lemur.datasets as lds
+import lemur.plotters as lpl
+import lemur.clustering as lcl
+import knor
+
+from scipy.stats import zscore
+from wrappers import cached_property
+
+from plotly.offline import (download_plotlyjs, init_notebook_mode)
+
+
+class Meda:
+    """
+    TODO: Write docs. 
+    """
+
+    def __init__(self,
+                 csv_file,
+                 index_col=None,
+                 title=None,
+                 cluster_levels=1,
+                 showticklabels=True,
+                 mode='notebook'):
+        """
+        Parameters
+        ----------
+        csv_file : str
+            Path to the csv_file to plot
+        index_col : str, optional
+            Name of the index column in csv_file
+        title : str, optional
+            Title of the dataset.
+        cluster_levels : int (default=1)
+            Number of levels for hierarchial clustering
+        showticklabels : bool
+            Default True
+        mode : str
+            notebook or div
+        """
+        self._ds = lds.CSVDataSet(csv_file, name=title, index_column=index_col)
+        self._ds_normed = lds.DataSet(self._ds.D.apply(zscore), name=title)
+        self.showticklabels = showticklabels
+        self.cluster_levels = cluster_levels
+        self.mode = mode
+        if mode == 'notebook':
+            init_notebook_mode()
+        self._cluster_ds = None
+
+    def d1_heatmap(self):
+        """
+        Generate 1d heatmaps
+        """
+        if self._ds.n > 1000:  #if sample size is > 1000, run kmeans++ initialization
+            ret = knor.Kmeans(
+                self._ds_normed.D.values, 1000, max_iters=0, init='kmeanspp')
+            centroids_df = pd.DataFrame(
+                ret.get_centroids(), columns=self._ds.D.columns)
+            centroids_ds = lds.DataSet(centroids_df, name=self._ds.name)
+
+            return lpl.HistogramHeatmap(
+                centroids_ds,
+                mode=self.mode).plot(showticklabels=self.showticklabels)
+        else:
+            return lpl.HistogramHeatmap(
+                self._ds_normed,
+                mode=self.mode).plot(showticklabels=self.showticklabels)
+
+    def scree_plot(self):
+        """
+        Genereate scree plot of PCA results
+        """
+        return lpl.ScreePlotter(self._ds_normed, mode=self.mode).plot()
+
+    def correlation_matrix(self):
+        """
+        Correlation matrix 
+        """
+        return lpl.CorrelationMatrix(
+            self._ds_normed,
+            mode=self.mode).plot(showticklabels=self.showticklabels)
+
+    def _compute_clusters(self, cluster_mode=None):
+        """
+        Run hierarchial GMM clustering
+
+        Parameters
+        ----------
+        cluster_mode : str 
+            Not implemented yet
+        """
+        cluster_ds = lcl.HGMMClustering(
+            self._ds_normed, levels=self.cluster_levels)
+        self._cluster_ds = cluster_ds
+
+    def cluster_dendrogram(self):
+        if not self._cluster_ds:
+            self._compute_clusters()
+
+        return lpl.HGMMClusterMeansDendrogram(
+            self._cluster_ds, mode=self.mode).plot()
+
+    def cluster_means_stacked(self):
+        """
+        Generate hierarchial clustering stacked means plot
+        """
+        if not self._cluster_ds:
+            self._compute_clusters()
+
+        return lpl.HGMMStackedClusterMeansHeatmap(
+            self._cluster_ds,
+            mode=self.mode).plot(showticklabels=self.showticklabels)
+
+    def cluster_means_heatmap(self):
+        """
+        Generate hierarchial cluster means plot of the lowest level
+        """
+        if not self._cluster_ds:
+            self._compute_clusters()
+
+        return lpl.HGMMClusterMeansLevelHeatmap(
+            self._cluster_ds,
+            mode=self.mode).plot(showticklabels=self.showticklabels)
+
+    def cluster_means_lines(self):
+        """
+        Generate hierarchial cluster mean lines plot of the lowest level
+        """
+        if not self._cluster_ds:
+            self._compute_clusters()
+
+        return lpl.HGMMClusterMeansLevelLines(
+            self._cluster_ds,
+            mode=self.mode).plot(showticklabels=self.showticklabels)
+
+    def cluster_pair_plot(self):
+        """
+        Generate pair plot for clusters
+        """
+        if not self._cluster_ds:
+            self._compute_clusters()
+
+        return lpl.HGMMPairsPlot(self._cluster_ds, mode=self.mode).plot()
+
+    def location_lines(self):
+        """
+        Generate location estimates using line graph
+        """
+        return lpl.LocationLines(
+            self._ds, mode=self.mode).plot(showticklabels=self.showticklabels)
+
+    def location_heatmap(self):
+        """
+        Generate location estimates using heatmap
+        """
+        return lpl.LocationHeatmap(
+            self._ds, mode=self.mode).plot(showticklabels=self.showticklabels)
+
+    def run_all(self):
+        pass
---
+++ packages/pymeda/pymeda-0.0.2.tar.gz/pymeda-0.0.2/setup.py
@@ -0,0 +1,69 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pymeda',
+    version='0.0.2',
+    description='Matrix Exploratory Data Analysis',
+    url='https://github.com/neurodata-nomads/pymeda',
+    download_url='https://github.com/neurodata/ndmg/tarball/0.0.2',
+    author='Jaewon Chung',
+    author_email='j1c@jhu.edu',
+    license='MIT',
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Visualization',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='data visualization analysis',
+    packages=['pymeda'],  # Required
+    install_requires=[
+        'colorlover==0.2.1', 'jupyter==1.0.0', 'numpy==1.13.1',
+        'pandas==0.21.0', 'plotly==2.2.3', 'scipy==1.0.0', 'redlemur==0.10.0',
+        'scikit-learn==0.19.1', 'knor', 'cython', 'boto3==1.4.7',
+        'nilearn==0.4.0', 'matplotlib==2.1.0', 'nose', 'imageio'
+    ]
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    #
+    # If using Python 2.6 or earlier, then these have to be included in
+    # MANIFEST.in as well.
+    #package_data={  # Optional
+    #    'sample': ['package_data.dat'],
+    #},
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    #
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    #data_files=[('my_data', ['data/data_file'])],  # Optional
+)
