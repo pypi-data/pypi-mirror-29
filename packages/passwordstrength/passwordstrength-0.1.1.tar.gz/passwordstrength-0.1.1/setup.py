999aa7a22643db33e7dc89f8123c73ef212e03ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:45 +0000

    Add package passwordstrength/0.1.1/passwordstrength-0.1.1.tar.gz

---
+++ packages/passwordstrength/passwordstrength-0.1.1.tar.gz/passwordstrength-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='passwordstrength',
+      version='0.1.1',
+      description='Editable password strength calculator for Python',
+      author='Pacharapol Withayasakpunt',
+      author_email='patarapolw@gmail.com',
+      license='MIT',
+      keywords='password password_strength password_meter',
+      packages=['passwordstrength'],
+      )
