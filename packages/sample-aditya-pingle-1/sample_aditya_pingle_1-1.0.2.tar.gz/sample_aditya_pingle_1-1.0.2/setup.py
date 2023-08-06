3bc553a954934381d81e41f9a2394c28da064ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:13 +0000

    Add package sample-aditya-pingle-1/1.0.2/sample_aditya_pingle_1-1.0.2.tar.gz

---
+++ packages/sample-aditya-pingle-1/sample_aditya_pingle_1-1.0.2.tar.gz/sample_aditya_pingle_1-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(name= "sample_aditya_pingle_1", version="1.0.2",
+        description = "Sample project for aditya's pip experiment.",
+        long_description = "This will hold very long_description for aditya's pip experiment.",
+        url = "https://github.com/adiping1",
+        author = "Aditya Pingle",
+        author_email = "adiping1@umbc.edu",
+        license = "MIT",
+        classifiers = ['Development Status :: 3 - Alpha',
+                        'Intended Audience :: Developers',
+                        'Topic :: Software Development :: Build Tools',
+                        'License :: OSI Approved :: MIT License',
+                        'Programming Language :: Python :: 2.7'],
+        keywords = "sample aditya piper",
+        packages = find_packages(exclude=['contrib', 'docs', 'tests']),
+        scripts = ['sample_aditya_pingle_1/bin/sampler'],
+        install_requires = ['twine'],
+        project_urls = {
+                        'Bug Reports': 'https://github.com/adiping1/bugs',
+                        'Funding' : 'https://github.com/adiping1/funds',
+                        'Say Thanks!' : 'https://github.com/adiping1/thanks',
+                        'Source' : 'https://github.com/adiping1/src'
+                        }
+        )
