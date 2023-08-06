8f198b83bc7e5152e6b3d4b0bd3c41b978bd4493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:51 +0000

    Add package metecli/0.7.0/metecli-0.7.0-py2.py3-none-any.whl

---
+++ packages/metecli/metecli-0.7.0-py2.py3-none-any.whl/metecli/main.py
@@ -0,0 +1,14 @@
+def fail_on_python2() -> None:
+    """Exits if we are using Python 2."""
+    import sys
+    if sys.version_info.major == 2:
+        print("You are still using Python 2. Please upgrade.")
+        sys.exit(-1)
+
+def run() -> None:
+    fail_on_python2()
+    from . import cmdline
+    cmdline.do()
+
+if __name__ == '__main__':
+    run()
