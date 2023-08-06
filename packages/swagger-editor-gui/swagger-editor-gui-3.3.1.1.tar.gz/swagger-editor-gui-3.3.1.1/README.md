68b3bb801c422925a1b33b8f0dc3a46a9a4e719f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:23 +0000

    Add package swagger-editor-gui/3.3.1.1/swagger-editor-gui-3.3.1.1.tar.gz

---
+++ packages/swagger-editor-gui/swagger-editor-gui-3.3.1.1.tar.gz/swagger-editor-gui-3.3.1.1/README.md
@@ -0,0 +1,3 @@
+# Swagger Editor GUI
+
+A Qt wrap of https://github.com/swagger-api/swagger-editor
---
+++ packages/swagger-editor-gui/swagger-editor-gui-3.3.1.1.tar.gz/swagger-editor-gui-3.3.1.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='swagger-editor-gui',
+    version='3.3.1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    author='Victor Häggqvist',
+    author_email='pypi@snilius.com',
+    url='https://victorhaggqvist.com',
+    entry_points = {
+        'gui_scripts': [
+            'swagger-editor-gui = gui.gui:main'
+        ]
+    }
+)
