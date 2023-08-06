1778ad0e531b0c68a90f2701302e1880017107a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:48 +0000

    Add package cloudshell-cp-aws/2.2.0/cloudshell-cp-aws-2.2.0.tar.gz

---
+++ packages/cloudshell-cp-aws/cloudshell-cp-aws-2.2.0.tar.gz/cloudshell-cp-aws-2.2.0/cloudshell/cp/aws/domain/common/vm_details_provider.py
@@ -0,0 +1,116 @@
+class VmDetailsProvider(object):
+    def __init__(self):
+        pass
+
+    def create(self, instance):
+        vm_details = VmDetails()
+
+        vm_details.vm_instance_data = self._get_vm_instance_data(instance, instance.vpc_id)
+        vm_details.vm_network_data = self._get_vm_network_data(instance)
+        return vm_details
+
+    def _get_vm_instance_data(self, instance, vpc_id):
+        # if not windows, instance platform is empty; therefore we default to linux
+        platform = instance.platform or 'linux'
+        volume = self._get_volume(instance)
+        data = [AdditionalData('AMI ID', instance.image_id),
+                AdditionalData('instance type', instance.instance_type),
+                AdditionalData('platform', platform),
+                AdditionalData('Storage Name', self._get_volume_id(volume)),
+                AdditionalData('Storage Type', self._get_volume_type(volume)),
+                AdditionalData('Storage Size', self._get_volume_size(volume)),
+                AdditionalData('VPC ID', vpc_id, hidden=True),
+                AdditionalData('Availability Zone', self._get_availability_zone(instance), hidden=True)
+                ]
+        return data
+
+    def _get_vm_network_data(self, instance):
+        network_interface_objects = []
+
+        instance.reload()
+        if instance.network_interfaces:
+            network_interfaces = sorted(instance.network_interfaces, key=lambda x: x.attachment.get("DeviceIndex"))
+            for network_interface in network_interfaces:
+                network_interface_object = {
+                    "interface_id": network_interface.network_interface_id,
+                    "network_id": network_interface.subnet_id,
+                    "network_data": [AdditionalData("IP", network_interface.private_ip_address)]
+                }
+
+                is_attached_to_elastic_ip = self._has_elastic_ip(network_interface)
+                is_primary = self._is_primary_interface(network_interface)
+                public_ip = self._calculate_public_ip(network_interface)
+
+                if is_primary:
+                    network_interface_object["is_primary"] = is_primary
+
+                network_interface_object["network_data"].append(AdditionalData("Public IP", public_ip))
+                network_interface_object["network_data"].append(AdditionalData("Elastic IP", is_attached_to_elastic_ip))
+
+                network_interface_object["network_data"].append(AdditionalData("MAC Address", network_interface.mac_address))
+                network_interface_object["network_data"].append(AdditionalData("Device Index", network_interface.attachment.get("DeviceIndex")))
+
+                network_interface_objects.append(network_interface_object)
+
+        return network_interface_objects
+
+    def _calculate_public_ip(self, interface):
+        # interface has public ip if:
+        # a. is elastic ip
+        # b. not elastic, but primary and instance has public ip
+
+        if interface.association_attribute!=None and "PublicIp" in interface.association_attribute:
+            return interface.association_attribute.get("PublicIp")
+        return ""
+
+    def _has_elastic_ip(self, interface):
+        # IpOwnerId: amazon - temporary public ip
+        # IpOwnerId: some guid - elastic ip
+        return interface.association_attribute!=None and 'IpOwnerId' in interface.association_attribute \
+               and interface.association_attribute.get('IpOwnerId') != 'amazon'
+
+    def _is_primary_interface(self, interface):
+        return interface.attachment.get("DeviceIndex") == 0
+
+    @staticmethod
+    def _get_volume(instance):
+        return next((v for v in instance.volumes.all()), None)
+
+    def _get_availability_zone(self, instance):
+        return instance.placement.get('AvailabilityZone', None)
+
+    def _get_volume_size(self, volume):
+        return '{0} {1}'.format(volume.size, 'GiB') if volume else None
+
+    def _get_volume_type(self, volume):
+        return volume.volume_type if volume else None
+
+    def _get_volume_id(self, volume):
+        return volume.volume_id if volume else None
+
+
+class VmDetails(object):
+    def __init__(self):
+        self.vm_instance_data = {} # type: dict
+        self.vm_network_data = [] # type: list[VmNetworkData]
+
+
+class VmNetworkData(object):
+    def __init__(self):
+        self.interface_id = {} # type: str
+        self.network_id = {} # type: str
+        self.is_primary = False # type: bool
+        self.network_data = {} # type: dict
+
+
+def AdditionalData(key, value, hidden=False):
+    """
+    :type key: str
+    :type value: str
+    :type hidden: bool
+    """
+    return {
+        "key": key,
+        "value": value,
+        "hidden": hidden
+    }
---
+++ packages/cloudshell-cp-aws/cloudshell-cp-aws-2.2.0.tar.gz/cloudshell-cp-aws-2.2.0/cloudshell/cp/aws/domain/services/parsers/aws_model_parser.py
@@ -0,0 +1,212 @@
+import re
+
+import jsonpickle
+from cloudshell.shell.core.driver_context import ReservationContextDetails
+
+from cloudshell.cp.aws.common.converters import convert_to_bool
+from cloudshell.cp.aws.common.deploy_data_holder import DeployDataHolder
+from cloudshell.cp.aws.domain.services.parsers.network_actions import NetworkActionsParser
+from cloudshell.cp.aws.domain.services.parsers.security_group_parser import SecurityGroupParser
+from cloudshell.cp.aws.models.aws_ec2_cloud_provider_resource_model import AWSEc2CloudProviderResourceModel
+from cloudshell.cp.aws.models.deploy_aws_ec2_ami_instance_resource_model import DeployAWSEc2AMIInstanceResourceModel
+from cloudshell.cp.aws.models.reservation_model import ReservationModel
+
+from cloudshell.cp.aws.models.app_security_groups_model import AppSecurityGroupModel, DeployedApp, VmDetails, \
+    SecurityGroupConfiguration
+
+
+class AWSModelsParser(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def convert_app_resource_to_deployed_app(resource):
+        json_str = jsonpickle.decode(resource.app_context.deployed_app_json)
+        data_holder = DeployDataHolder(json_str)
+        return data_holder
+
+    @staticmethod
+    def get_app_security_groups_from_request(request):
+        json_str = jsonpickle.decode(request)
+        data_holder = DeployDataHolder.create_obj_by_type(json_str)
+        return data_holder
+
+    @staticmethod
+    def convert_to_app_security_group_models(request):
+        """
+        :rtype list[AppSecurityGroupModel]:
+        """
+        security_group_models = []
+
+        security_groups = AWSModelsParser.get_app_security_groups_from_request(request)
+
+        for security_group in security_groups:
+            security_group_model = AppSecurityGroupModel()
+            security_group_model.deployed_app = DeployedApp()
+            security_group_model.deployed_app.name = security_group.deployedApp.name
+            security_group_model.deployed_app.vm_details = VmDetails()
+            security_group_model.deployed_app.vm_details.uid = security_group.deployedApp.vmdetails.uid
+            security_group_model.security_group_configurations = SecurityGroupParser.parse_security_group_configurations(
+                security_group.securityGroupsConfigurations)
+
+            security_group_models.append(security_group_model)
+
+        return security_group_models
+
+    @staticmethod
+    def convert_to_aws_resource_model(resource):
+        """
+        :rtype AWSEc2CloudProviderResourceModel:
+        """
+        resource_context = resource.attributes
+        aws_ec2_resource_model = AWSEc2CloudProviderResourceModel()
+        aws_ec2_resource_model.region = resource_context['Region']
+        aws_ec2_resource_model.max_storage_iops = resource_context['Max Storage IOPS']
+        aws_ec2_resource_model.max_storage_size = resource_context['Max Storage Size']
+        aws_ec2_resource_model.aws_secret_access_key = resource_context['AWS Secret Access Key']
+        aws_ec2_resource_model.aws_access_key_id = resource_context['AWS Access Key ID']
+        aws_ec2_resource_model.key_pairs_location = resource_context['Keypairs Location']
+        aws_ec2_resource_model.aws_management_vpc_id = resource_context['AWS Mgmt VPC ID']
+        aws_ec2_resource_model.aws_management_sg_id = resource_context['AWS Mgmt SG ID']
+        aws_ec2_resource_model.instance_type = resource_context['Instance Type']
+        # aws_ec2_resource_model.reserved_ips_in_subnet = resource_context['Reserved IPs in Subnet']
+
+        return aws_ec2_resource_model
+
+    @staticmethod
+    def convert_to_deployment_resource_model(deployment_request, resource):
+        """
+        :param str deployment_request: JSON string representing the deployment request
+        :param ResourceContextDetails resource: The details of the resource using the driver
+        """
+        data = jsonpickle.decode(deployment_request)
+        deployment_resource_model = DeployAWSEc2AMIInstanceResourceModel()
+
+        deployment_resource_model.cloud_provider = resource.name
+        deployment_resource_model.app_name = data["AppName"]
+
+        deployment_resource_model.aws_ami_id = data["Attributes"]['AWS AMI Id']
+        deployment_resource_model.allow_all_sandbox_traffic = convert_to_bool(
+            data["Attributes"]['Allow all Sandbox Traffic'])
+        deployment_resource_model.storage_size = data["Attributes"]['Storage Size']
+        deployment_resource_model.storage_iops = data["Attributes"]['Storage IOPS']
+        deployment_resource_model.storage_type = data["Attributes"]['Storage Type']
+        deployment_resource_model.instance_type = data["Attributes"]['Instance Type']
+        deployment_resource_model.root_volume_name = data["Attributes"]['Root Volume Name']
+        deployment_resource_model.wait_for_ip = convert_to_bool(data["Attributes"]['Wait for IP'])
+        deployment_resource_model.wait_for_status_check = convert_to_bool(
+            data["Attributes"]['Wait for Status Check'])
+        deployment_resource_model.autoload = convert_to_bool(data["Attributes"]['Autoload'])
+        deployment_resource_model.inbound_ports = data["Attributes"]['Inbound Ports']
+        deployment_resource_model.wait_for_credentials = \
+            convert_to_bool(data["Attributes"]['Wait for Credentials'])
+
+        (deployment_resource_model.add_public_ip, deployment_resource_model.allocate_elastic_ip) = \
+            AWSModelsParser.parse_public_ip_options_attribute(data["Attributes"]['Public IP Options'])
+
+        deployment_resource_model.user = \
+            AWSModelsParser.get_attribute_value_by_name_ignoring_namespace(
+                data["LogicalResourceRequestAttributes"], "User")
+
+        deployment_resource_model.network_configurations = \
+            AWSModelsParser.parse_deploy_networking_configurations(data)
+
+        return deployment_resource_model
+
+    @staticmethod
+    def parse_public_ip_options_attribute(attr_value):
+        """
+        Parses the value of the "Public IP Options" attribute into a boolean tuple:
+          (Add Public IP, Allocate Elastic IP)
+        :param str attr_value: "Public IP Options" attribute value
+        :return: Tuple of (Add Public IP, Allocate Elastic IP)
+        :rtype: (boolean, boolean)
+        """
+        if re.match("^elastic", attr_value, flags=re.IGNORECASE):
+            return False, True
+
+        if re.match("^public ", attr_value, flags=re.IGNORECASE):
+            return True, False
+
+        return False, False
+
+    @staticmethod
+    def get_attribute_value_by_name_ignoring_namespace(attributes, name):
+        """
+        Finds the attribute value by name ignoring attribute namespaces.
+        :param dict attributes: Attributes key value dict to search on.
+        :param str name: Attribute name to search for.
+        :return: Attribute str value. None if not found.
+        :rtype: str
+        """
+        for key, val in attributes.iteritems():
+            last_part = key.split(".")[-1]  # get last part of namespace.
+            if name == last_part:
+                return val
+        return None
+
+    @staticmethod
+    def get_allow_all_storage_traffic_from_connected_resource_details(resource_context):
+        allow_traffic_on_resource = ""
+        allow_all_storage_traffic = 'Allow all Sandbox Traffic'
+        if resource_context.remote_endpoints is not None:
+            data = jsonpickle.decode(resource_context.remote_endpoints[0].app_context.app_request_json)
+            attributes = {d["name"]: d["value"] for d in data["deploymentService"]["attributes"]}
+            allow_traffic_on_resource = AWSModelsParser.get_attribute_value_by_name_ignoring_namespace(
+                attributes, allow_all_storage_traffic)
+        return allow_traffic_on_resource
+
+    @staticmethod
+    def get_public_ip_from_connected_resource_details(resource_context):
+        public_ip_on_resource = ""
+        public_ip = 'Public IP'
+        if resource_context.remote_endpoints is not None:
+            public_ip_on_resource = AWSModelsParser.get_attribute_value_by_name_ignoring_namespace(
+                resource_context.remote_endpoints[0].attributes, public_ip)
+        return public_ip_on_resource
+
+    @staticmethod
+    def get_private_ip_from_connected_resource_details(resource_context):
+        private_ip_on_resource = ""
+        if resource_context.remote_endpoints is not None:
+            private_ip_on_resource = resource_context.remote_endpoints[0].address
+        return private_ip_on_resource
+
+    @staticmethod
+    def try_get_deployed_connected_resource_instance_id(resource_context):
+        try:
+            deployed_instance_id = str(
+                jsonpickle.decode(resource_context.remote_endpoints[0].app_context.deployed_app_json)['vmdetails'][
+                    'uid'])
+        except Exception as e:
+            raise ValueError('Could not find an ID of the AWS Deployed instance' + e.message)
+        return deployed_instance_id
+
+    @staticmethod
+    def get_connectd_resource_fullname(resource_context):
+        if resource_context.remote_endpoints[0]:
+            return resource_context.remote_endpoints[0].fullname
+        else:
+            raise ValueError('Could not find resource fullname on the deployed app.')
+
+    @staticmethod
+    def convert_to_reservation_model(reservation_context):
+        """
+        :param ReservationContextDetails reservation_context:
+        :rtype: ReservationModel
+        """
+        return ReservationModel(reservation_context)
+
+    @staticmethod
+    def parse_deploy_networking_configurations(deployment_request):
+        """
+        :param deployment_request: request object to parse
+        :return:
+        """
+        if "NetworkConfigurationsRequest" not in deployment_request:
+            return None
+
+        actions = deployment_request["NetworkConfigurationsRequest"]["actions"]
+        # actions = deployment_request["NetworkConfigurationsRequest"]
+
+        return NetworkActionsParser.parse_network_actions_data(actions)
