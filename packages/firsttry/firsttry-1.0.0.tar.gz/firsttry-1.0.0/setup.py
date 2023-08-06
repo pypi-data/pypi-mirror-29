c73c12b4cbe27adabff0259738eb272312cf2563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:40 +0000

    Add package firsttry/1.0.0/firsttry-1.0.0.tar.gz

---
+++ packages/firsttry/firsttry-1.0.0.tar.gz/firsttry-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='firsttry',
+    version='1.0.0',
+    py_modules=['firsttry'],
+    author='wycwindy',
+    author_email='291149763@qq.com',
+    url='http://www.headfirstlabs.com',
+    description='simple try'
+    )
