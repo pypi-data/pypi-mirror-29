2e028e7326e80e85c46263f21caaa00a2650717b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:49 +0000

    Add package runtimestamp/0.0.2/runtimestamp-0.0.2.tar.gz

---
+++ packages/runtimestamp/runtimestamp-0.0.2.tar.gz/runtimestamp-0.0.2/setup.py
@@ -0,0 +1,14 @@
+import sys
+from setuptools import setup
+
+setup(name='runtimestamp',
+	packages=['runtimestamp'],
+	version='0.0.2',
+	description='Leaves an timestamp and signature at the top of every notebook',
+	author='leon yin',
+	author_email='ly501@nyu.edu',
+	url='https://github.com/yinleon/runtimestamp',
+	keywords='jupyter notebook, timestamp',
+	license='MIT',
+	install_requires=[]
+)
