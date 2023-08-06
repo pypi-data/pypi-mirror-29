d5f111420d442e0d10816c408e98a9ac3561a7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:15 +0000

    Add package django-ohm2-accounts-light/0.4.2/django-ohm2-accounts-light-0.4.2.tar.gz

---
+++ packages/django-ohm2-accounts-light/django-ohm2-accounts-light-0.4.2.tar.gz/django-ohm2-accounts-light-0.4.2/ohm2_accounts_light/decorators.py
@@ -0,0 +1,16 @@
+from django.shortcuts import redirect
+from ohm2_handlers_light.decorators import ohm2_handlers_light_safe_request
+from functools import wraps
+
+def ohm2_accounts_light_safe_request(function):
+	return ohm2_handlers_light_safe_request(function, "accounts")
+
+def ohm2_accounts_light_redirect_if_authenticated(view_function, redirect_url = "/"):
+
+	@wraps(view_function)
+	def wrapped_view(request, *args, **kwargs):
+		if request.user.is_authenticated:
+			return redirect(redirect_url)
+		return view_function(request, *args, **kwargs)
+	
+	return wrapped_view
