116a6246f5710345c01f56f5fd2cb82e88df7afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:41 +0000

    Add package rainflow/2.0.0/rainflow-2.0.0.tar.gz

---
+++ packages/rainflow/rainflow-2.0.0.tar.gz/rainflow-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='rainflow',
+    py_modules=['rainflow'],
+    version='2.0.0',
+    description='Implementation of ASTM E1049-85 rainflow cycle counting algorythm',
+    author='Piotr Janiszewski',
+    author_email='i.am.like.me@gmail.com',
+    url='https://github.com/iamlikeme/rainflow/',
+    download_url='https://github.com/iamlikeme/rainflow/archive/2.0.0.tar.gz',
+    classifiers=[],
+)
