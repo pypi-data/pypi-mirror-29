7fab402b55f371f9b4da9626ee8c350a33e70fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:45 +0000

    Add package obidrfid/0.3/obidrfid-0.3.tar.gz

---
+++ packages/obidrfid/obidrfid-0.3.tar.gz/obidrfid-0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='obidrfid',
+    py_modules=['obidrfid'],
+    version='0.3',
+    description='Python wrapper to communicate with eh OBID RFID IP antennas',
+    author='Fabrice Tereszkiewicz',
+    author_email='fabrice.tz@51stfloor.com',
+    url='https://github.com/fatzh/obidrfid',
+    download_url='https://github.com/fatzh/obidrfid/archive/0.3.tar.gz',
+    keywords=['rfid', 'obid'],
+    classifiers=[],
+)
