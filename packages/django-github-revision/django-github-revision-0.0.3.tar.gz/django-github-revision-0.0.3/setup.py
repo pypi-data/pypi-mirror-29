656e5cabf3ef6c6af3ea3e0fa86467b9100bbc85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:52 +0000

    Add package django-github-revision/0.0.3/django-github-revision-0.0.3.tar.gz

---
+++ packages/django-github-revision/django-github-revision-0.0.3.tar.gz/django-github-revision-0.0.3/setup.py
@@ -0,0 +1,48 @@
+import sys
+from os import path
+from os import system
+
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+install_deps = [
+    'dealer',
+    'django'
+]
+
+tests_deps = [
+    'mock'
+]
+
+extras = {
+    'tests': tests_deps,
+}
+
+version = '0.0.3'
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+if sys.argv[-1] == 'publish':
+    try:
+        import wheel
+    except ImportError:
+        raise ImportError("Fix: pip install wheel")
+    system('python setup.py sdist bdist_wheel --universal upload')
+    sys.exit()
+
+setup(
+    name='django-github-revision',
+    version=version,
+    packages=['github_revision', 'github_revision.templatetags'],
+    url='https://github.com/watchdogpolska/django-github-revision',
+    license='MIT',
+    author='Adam Dobrawy',
+    author_email='karol.bregula@siecobywatelska.pl',
+    description='Publishes information about the git revision that is running and links to the GitHub repository.',
+    install_requires=install_deps,
+    tests_require=tests_deps,
+    extras_require=extras
+)
