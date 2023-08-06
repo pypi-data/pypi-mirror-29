60fcafefdf28292782fb64b7ebb4df84ef56c53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:05 +0000

    Add package mastercard-stp/1.0.0/mastercard_stp-1.0.0.tar.gz

---
+++ packages/mastercard-stp/mastercard_stp-1.0.0.tar.gz/mastercard_stp-1.0.0/mastercardstp/__init__.py
@@ -0,0 +1,13 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardstp.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardstp.payment import *
+from mastercardstp.payments import *
+from mastercardstp.provider import *
+from mastercardstp.providers import *
+from mastercardstp.supplier import *
+from mastercardstp.suppliers import *
