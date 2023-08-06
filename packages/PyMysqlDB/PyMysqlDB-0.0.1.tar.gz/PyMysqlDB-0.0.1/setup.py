18073c5e85b2b61e44898592bc541de4766dc43a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:19 +0000

    Add package PyMysqlDB/0.0.1/PyMysqlDB-0.0.1.tar.gz

---
+++ packages/PyMysqlDB/PyMysqlDB-0.0.1.tar.gz/PyMysqlDB-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='PyMysqlDB',
+    version='0.0.1',
+    author='jun',
+    author_email='jun.mr@qq.com',
+    url='http://docs.51pub.cn/python/MysqlDB',
+    packages=['PyTools'],
+    description='manage mysql tool',
+    license='MIT',
+    install_requires=['pymysql'],
+)
