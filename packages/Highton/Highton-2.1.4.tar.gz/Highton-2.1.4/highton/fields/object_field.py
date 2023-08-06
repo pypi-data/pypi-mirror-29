b04a36cee732f9a59685aa80b5b79974c91d3ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:30 +0000

    Add package Highton/2.1.4/Highton-2.1.4.tar.gz

---
+++ packages/Highton/Highton-2.1.4.tar.gz/Highton-2.1.4/highton/fields/object_field.py
@@ -0,0 +1,24 @@
+from highton.fields import Field
+
+
+class ObjectField(Field):
+    """
+    The ObjectFIeld parses the init_class object
+
+    :ivar init_class: a Highton model
+    """
+    def __init__(self, name, init_class):
+        super().__init__(name)
+        self.init_class = init_class
+
+    def encode(self):
+        """
+        Just encode the object you have as value
+        
+        :return: the parsed element
+        :rtype: xml.etree.ElementTree.Element
+        """
+        return self.value.encode()
+
+    def decode(self, element):
+        return self.init_class.decode(element)
