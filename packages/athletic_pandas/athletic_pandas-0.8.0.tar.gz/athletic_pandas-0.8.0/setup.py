40ecfc4646675c882be621312a63e1d93106c341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:31 +0000

    Add package athletic_pandas/0.8.0/athletic_pandas-0.8.0.tar.gz

---
+++ packages/athletic_pandas/athletic_pandas-0.8.0.tar.gz/athletic_pandas-0.8.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'athletic_pandas',
+    packages = find_packages(),
+    version = '0.8.0',
+    description = 'Workout analysis',
+    author='Aart Goossens',
+    author_email='aart@goossens.me',
+    url='https://github.com/AartGoossens/athletic_pandas',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+    ]
+)
