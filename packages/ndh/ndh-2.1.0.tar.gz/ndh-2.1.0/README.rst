16ccba70723762ea7f6d66e8c4e0aa7091906591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:29 +0000

    Add package ndh/2.1.0/ndh-2.1.0.tar.gz

---
+++ packages/ndh/ndh-2.1.0.tar.gz/ndh-2.1.0/README.rst
@@ -0,0 +1,19 @@
+Nim’s Django Helpers
+====================
+
+|Build Status| |Coverage Status|
+
+Requirements
+------------
+
+-  Python 3.6
+-  Django 2.0
+-  `django-autoslug <https://github.com/nim65s/django-autoslug>`__
+   (waiting for https://github.com/neithere/django-autoslug/issues/31)
+-  `django-bootstrap3 <https://github.com/dyve/django-bootstrap3>`__
+   (will be django-bootstrap4 soon)
+
+.. |Build Status| image:: https://travis-ci.org/nim65s/ndh.svg?branch=master
+   :target: https://travis-ci.org/nim65s/ndh
+.. |Coverage Status| image:: https://coveralls.io/repos/github/nim65s/ndh/badge.svg?branch=master
+   :target: https://coveralls.io/github/nim65s/ndh?branch=master
---
+++ packages/ndh/ndh-2.1.0.tar.gz/ndh-2.1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.in')) as requirements:
+    REQUIREMENTS = [req.split('#egg=')[1] if '#egg=' in req else req for req in requirements.readlines()]
+
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='ndh',
+    version='2.1.0',
+    packages=['ndh'],
+    install_requires=REQUIREMENTS,
+    include_package_data=True,
+    license='GPL License',
+    description='Nim’s Django Helpers',
+    long_description=README,
+    url='https://github.com/nim65s/ndh',
+    author='Guilhem Saurel',
+    author_email='webmaster@saurel.me',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
