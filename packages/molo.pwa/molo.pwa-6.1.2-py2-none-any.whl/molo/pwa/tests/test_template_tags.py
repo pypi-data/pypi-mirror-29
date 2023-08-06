e7348d8fc24767148c2e1c6140813a56167a3faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:35 +0000

    Add package molo.pwa/6.1.2/molo.pwa-6.1.2-py2-none-any.whl

---
+++ packages/molo.pwa/molo.pwa-6.1.2-py2-none-any.whl/molo/pwa/tests/test_template_tags.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
+from django.template import Context, Template
+from django.test import TestCase
+
+from molo.core.tests.base import MoloTestCaseMixin
+
+
+class TemplateTagsTestCase(TestCase, MoloTestCaseMixin):
+    def render_template(self, template_string, context):
+        return Template(template_string).render(Context(context))
+
+    def test_template_renders_without_error(self):
+        template = self.render_template(
+            '{% load molo_pwa %} {% molo_pwa_meta %}',
+            {
+                'request': {'user': AnonymousUser()},
+            },
+        )
+        self.assertIn('<link rel="manifest" href="/manifest.json">', template)
+
+    def test_template_no_fcm_config_if_user_anonymous(self):
+        template = self.render_template(
+            '{% load molo_pwa %} {% molo_pwa_meta %}',
+            {
+                'request': {'user': AnonymousUser()},
+            },
+        )
+        self.assertNotIn('fcmConfig', template)
+
+    def test_template_has_fcm_config_if_user_authed(self):
+        self.mk_main()
+        user = get_user_model().objects.create()
+        template = self.render_template(
+            '{% load molo_pwa %} {% molo_pwa_meta %}',
+            {
+                'request': {'user': user},
+            },
+        )
+        self.assertIn('fcmConfig', template)
