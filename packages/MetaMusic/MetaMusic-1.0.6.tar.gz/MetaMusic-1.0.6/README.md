6777a73329b6866f8840afa50024ba8a95073ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:34 +0000

    Add package MetaMusic/1.0.6/MetaMusic-1.0.6.tar.gz

---
+++ packages/MetaMusic/MetaMusic-1.0.6.tar.gz/MetaMusic-1.0.6/README.md
@@ -0,0 +1,26 @@
+# Meta-Music
+Meta-Music is an open-source project that lets people add metadata to their Music library.
+
+# Installation from source:
+
+        cd static
+        npm install 
+        cd ..
+        virtualenv py3
+        pip install -r requirements
+        python app.py
+        npm run watch
+# Using PIP
+        sudo pip install MetaMusic
+        sudo npm install -g random-material-color react react-dom
+        meta-music
+
+# Demo
+
+![demo](https://media.giphy.com/media/8PBFETWIZ39tme3vow/giphy.gif)
+
+
+**Note for MacOS users**: If you experience problems with fork in macOS, put `OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES` in your environment variable to fix the issue. This is an issue with macOS 10.13 where Apple changed the way fork() works on the OS which is incompatible with Python fork().
+ 
+        
+
---
+++ packages/MetaMusic/MetaMusic-1.0.6.tar.gz/MetaMusic-1.0.6/metamusic/app.py
@@ -0,0 +1,100 @@
+from flask import Flask, request, render_template, jsonify, abort
+import multiprocessing
+from multiprocessing import Pool
+import time
+import os
+import numpy as np
+import sys
+import webbrowser
+import string
+import glob
+import random
+from metamusic.fetcher import process_init
+from metamusic.model import db, fetcher_database
+import random
+import os
+
+app = Flask(__name__, static_folder="./static/dist",
+            template_folder="./static")
+
+database_name = ''.join(random.choices(
+    string.ascii_uppercase, k=10))
+db_path = os.getcwd()
+for i in glob.glob(os.path.join(db_path, '*.db')):
+    os.remove(i)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    db_path + '/' + database_name + '.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "thisisyunik"
+db.init_app(app)
+
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+
+@app.route("/process", methods=["GET"])
+def process():
+
+    path = request.args['path']
+    if not os.path.exists(path):
+        time.sleep(.3)
+        return render_template("nofile.html")
+    total_songs = 0
+
+    if not os.path.isfile(path):
+        p = Pool()
+        folders = []
+        for root, dirs, files in os.walk(path):
+
+            results = p.map(f, files)
+            numbers = np.sum(np.array(results) > 0)
+            if numbers:
+                folders.append(root)
+            total_songs += numbers
+        p.close()
+        p.join()
+
+        t = multiprocessing.Process(
+            target=process_init, args=(path, app, db, folders))
+    else:
+        total_songs = 1
+        folders = [os.path.dirname(path)]
+        t = multiprocessing.Process(
+            target=process_init, args=(path, app, db, folders))
+    t.daemon = True
+    t.start()
+    time.sleep(0.4)
+    return render_template("process.html", totalsongs=total_songs)
+
+
+@app.route('/fetch/<int:no>')
+def fetch(no):
+    return_data = fetcher_database.query.filter_by(uid=no).first()
+
+    while return_data is None:
+        time.sleep(0.01)
+        return_data = fetcher_database.query.filter_by(uid=no).first()
+    if not return_data.status:
+        return abort(404)
+    return jsonify(trackname=return_data.trackname, tracknumber=return_data.tracknumber, albumname=return_data.albumname, image_url=return_data.image_url, releasedate=return_data.releasedate,
+                   genre=return_data.genre, artistname=return_data.artistname, uid=return_data.uid, loading=True)
+
+
+def f(n):
+    if (n.endswith('.mp3')):
+        return 1
+    else:
+        return 0
+
+
+def run():
+    if len(sys.argv) >= 2:
+
+        folders = [os.path.dirname(sys.argv[1])]
+        process_init(sys.argv[1], app, db, folders)
+    else:
+        webbrowser.open('http://127.0.0.1:5000/')
+        app.run(threaded=True)
