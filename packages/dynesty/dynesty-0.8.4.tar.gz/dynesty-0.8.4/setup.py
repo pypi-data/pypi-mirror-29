1f0768b3be4a8dff6bb3e044d37d0ade91aade96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:01 +0000

    Add package dynesty/0.8.4/dynesty-0.8.4.tar.gz

---
+++ packages/dynesty/dynesty-0.8.4.tar.gz/dynesty-0.8.4/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+import glob
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+init_string = open(os.path.join(dir_path, 'dynesty', '__init__.py')).read()
+VERS = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VERS, init_string, re.M)
+__version__ = mo.group(1)
+
+    
+setup(
+    name="dynesty",
+    url="https://github.com/joshspeagle/dynesty",
+    version=__version__,
+    author="Joshua S Speagle",
+    author_email="jspeagle@cfa.harvard.edu",
+    packages=["dynesty"],
+    license="LICENSE",
+    description=("A dynamic nested sampling package for computing Bayesian "
+                 "posteriors and evidences."),
+    long_description=open("README.md").read(),
+    package_data={"": ["README.md", "LICENSE", "AUTHORS.md"]},
+    include_package_data=True,
+    install_requires=["numpy", "scipy", "matplotlib", "six"],
+    keywords=["nested sampling", "dynamic", "monte carlo", "bayesian",
+              "inference", "modeling"],
+    classifiers=["Development Status :: 4 - Beta",
+                 "License :: OSI Approved :: MIT License",
+                 "Natural Language :: English",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.6",
+                 "Operating System :: OS Independent",
+                 "Topic :: Scientific/Engineering",
+                 "Intended Audience :: Science/Research"]
+)
