45936211781fd7463ffd8c1a3d29f80592f266c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:38 +0000

    Add package peony-twitter/1.0.4/peony-twitter-1.0.4.tar.gz

---
+++ packages/peony-twitter/peony-twitter-1.0.4.tar.gz/peony-twitter-1.0.4/peony/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+===============
+ peony-twitter
+===============
+
+  An asynchronous Twitter API client for Python 3.5+
+
+"""
+
+import logging
+
+__author__ = "Florian Badie"
+__author_email__ = "florianbadie@gmail.com"
+__url__ = "https://github.com/odrling/peony-twitter"
+
+__version__ = "1.0.4"
+
+__license__ = "MIT License"
+
+__keywords__ = "twitter, asyncio, asynchronous"
+
+logger = logging.getLogger(__name__)
+
+from .client import BasePeonyClient, PeonyClient  # noqa
+from .commands import EventStream, event_handler, events, task, init_task  # noqa
+from .utils import set_debug  # noqa
