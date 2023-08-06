d2849a4ff167f09bfc68ce73b1073879ec740226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:54 +0000

    Add package zdict/0.10.1/zdict-0.10.1.tar.gz

---
+++ packages/zdict/zdict-0.10.1.tar.gz/zdict-0.10.1/zdict/constants.py
@@ -0,0 +1,10 @@
+import os
+
+
+VERSION = '0.10.1'
+
+BASE_DIR_NAME = '.zdict'
+BASE_DIR = os.path.join(os.getenv('HOME'), BASE_DIR_NAME)
+
+DB_NAME = 'zdict.db'
+DB_FILE = os.path.join(BASE_DIR, DB_NAME)
