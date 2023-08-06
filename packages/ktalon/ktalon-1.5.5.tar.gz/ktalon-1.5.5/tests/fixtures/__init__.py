a8b4cfa66992254b2da1d6d075f3491b135900af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:46 +0000

    Add package ktalon/1.5.5/ktalon-1.5.5.tar.gz

---
+++ packages/ktalon/ktalon-1.5.5.tar.gz/ktalon-1.5.5/tests/fixtures/__init__.py
@@ -0,0 +1,20 @@
+import glob
+
+STANDARD_REPLIES = "tests/fixtures/standard_replies"
+
+with open("tests/fixtures/reply-quotations-share-block.eml") as f:
+    REPLY_QUOTATIONS_SHARE_BLOCK = f.read()
+
+with open("tests/fixtures/OLK_SRC_BODY_SECTION.html") as f:
+    OLK_SRC_BODY_SECTION = f.read()
+
+with open("tests/fixtures/reply-separated-by-hr.html") as f:
+    REPLY_SEPARATED_BY_HR = f.read()
+
+
+REAL_HTML = []
+
+for htmlfile in glob.glob('tests/fixtures/real_data/*.html'):
+    with open(htmlfile) as f:
+        REAL_HTML.append(f.read())
+
