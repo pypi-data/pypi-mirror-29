1c53efe18ef3e9488ebf35114bcb477c6d6a55b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:56 +0000

    Add package livescore/2018.0.0/livescore-2018.0.0.tar.gz

---
+++ packages/livescore/livescore-2018.0.0.tar.gz/livescore-2018.0.0/README.md
@@ -0,0 +1,104 @@
+# FRC Livescore
+
+A package which can determine the score of a live FRC game from an image.
+
+## Features
+
+- Access to many parts of the scoreboard (and more coming soon)
+- Easy to use
+- Super fast
+- Template matching (it doesn't matter where the scoreboard is on the screen)
+
+## Installation
+
+```bash
+$ pip install livescore
+```
+
+You will also need to have [Tesseract](https://github.com/tesseract-ocr/tesseract/wiki#installation)
+and OpenCV 3 (instructions for
+[macOS](http://www.pyimagesearch.com/2016/12/19/install-opencv-3-on-macos-with-homebrew-the-easy-way/),
+[Windows](http://docs.opencv.org/3.2.0/d5/de5/tutorial_py_setup_in_windows.html) and
+[Linux](http://docs.opencv.org/3.2.0/d7/d9f/tutorial_linux_install.html))
+installed in order for `frc-livescore` to work.
+
+## Usage
+
+*Check out the `examples` directory for full examples on the usage of
+`frc-livescore`.*
+
+A very simple example program would be to just get the score data from a single
+image. To do this, we'll use OpenCV to read the image.
+
+```python
+from livescore import Livescore2018
+import cv2
+
+# Initialize a new Livescore instance
+frc = Livescore2018()
+
+# Read the image from disk
+image = cv2.imread('./examples/scenes/scene1.png')
+
+# Get score data
+data = frc.read(image)
+
+print(data)
+```
+
+## API
+
+## Constructor
+
+#### LivescoreYEAR(debug=False, save_training_data=False, training_data=None)
+
+> Currently supported years: 2017, 2018
+>
+> e.g. Livescore2017() or Livescore2018()
+
+- `debug` - Debug mode, where outputs are displayed.
+- `save_training_data` - Whether the training should be saved to disk.
+- `training_data` - Path to save training data.
+
+Creates and returns a new Livescore instance with specified options.
+
+### Methods
+
+#### .read(img, force_find_overlay=False)
+
+- `img` - The image to read from.
+- `force_find_overlay` - Whether we should forcefully find the overlay or only do
+   so if the overlay cannot be found.
+
+Reads an image and returns an [OngoingMatchDetails](#ongoingmatchdetails) class
+containing the score data. Values that could not be determined from the input
+image will be `False`.
+
+### Classes
+
+#### AllianceYEAR
+
+- `score` - The alliance's score.
+- ... many more year-specific properties.
+
+Stores year-specific properties for an alliance, such as whether the switch or
+scale is owned for the 2018 game.
+
+#### OngoingMatchDetails
+
+- `match_key` - The match key, such as "qf1m2".
+- `match_name` - The match name, such as "Qualifications 16 of 128".
+- `mode` - The current game mode, one of `pre_match`, `auto`, `teleop`, or
+  `post_match`.
+- `time` - The time remaining in the match.
+- `red` - An [Alliance](#alliance) class for the red alliance.
+- `blue` - An [Alliance](#alliance) class for the blue alliance.
+
+<!--
+#### CompletedMatchDetails
+
+- `match` - The match identification, such as "Qualifications 16"
+- `winner` - A string containing the match winner; either "red" or "blue"
+- `red` - An [Alliance](#alliance) class for the red alliance
+- `blue` - An [Alliance](#alliance) class for the blue alliance
+-->
---
+++ packages/livescore/livescore-2018.0.0.tar.gz/livescore-2018.0.0/livescore/details.py
@@ -0,0 +1,92 @@
+class Alliance(object):
+    def __init__(self, score=None, teams=[]):
+        self.teams = teams
+        self.score = score
+
+
+class Alliance2017(Alliance):
+    def __init__(
+            self,
+            score=None,
+            teams=[],
+            fuel_score=None,
+            fuel_count=None,
+            rotor_count=None,
+            touchpad_count=None):
+        super(Alliance2017, self).__init__(score=score, teams=teams)
+        self.fuel_score = fuel_score
+        self.fuel_count = fuel_count
+        self.rotor_count = rotor_count
+        self.touchpad_count = touchpad_count
+
+    def getString(self, prefix):
+        return (
+            '{} score: {}\n'.format(prefix, self.score) + \
+            '{} fuel_score: {}\n'.format(prefix, self.fuel_score) + \
+            '{} fuel_count: {}\n'.format(prefix, self.fuel_count) + \
+            '{} rotor_count: {}\n'.format(prefix, self.rotor_count) + \
+            '{} touchpad_count: {}\n'.format(prefix, self.touchpad_count)
+        )
+
+class Alliance2018(Alliance):
+    def __init__(
+            self,
+            score=None,
+            teams=[],
+            boost_count=None,
+            boost_played=None,
+            force_count=None,
+            force_played=None,
+            levitate_count=None,
+            levitate_played=None,
+            switch_owned=None,
+            scale_owned=None,
+            current_powerup=None,
+            powerup_time_remaining=None,
+            auto_quest=None,
+            face_the_boss=None,
+        ):
+        super(Alliance2018, self).__init__(score=score, teams=teams)
+        self.boost_count = boost_count
+        self.boost_played = boost_played
+        self.force_count = force_count
+        self.force_played = force_played
+        self.levitate_count = levitate_count
+        self.levitate_played = levitate_played
+        self.switch_owned = switch_owned
+        self.scale_owned = scale_owned
+        self.current_powerup = current_powerup
+        self.powerup_time_remaining = powerup_time_remaining
+        self.auto_quest = auto_quest
+        self.face_the_boss = face_the_boss
+
+    def getString(self, prefix):
+        return (
+            '{} score: {}\n'.format(prefix, self.score) + \
+            '{} force_count: {}\n'.format(prefix, self.force_count) + \
+            '{} force_played: {}\n'.format(prefix, self.force_played) + \
+            '{} levitate_count: {}\n'.format(prefix, self.levitate_count) + \
+            '{} levitate_played: {}\n'.format(prefix, self.levitate_played) + \
+            '{} boost_count: {}\n'.format(prefix, self.boost_count) + \
+            '{} boost_played: {}\n'.format(prefix, self.boost_played) + \
+            '{} switch_owned: {}\n'.format(prefix, self.switch_owned) + \
+            '{} scale_owned: {}\n'.format(prefix, self.scale_owned) + \
+            '{} current_powerup: {}\n'.format(prefix, self.current_powerup) + \
+            '{} powerup_time_remaining: {}\n'.format(prefix, self.powerup_time_remaining) + \
+            '{} auto_quest: {}\n'.format(prefix, self.auto_quest) + \
+            '{} face_the_boss: {}\n'.format(prefix, self.face_the_boss)
+        )
+
+class OngoingMatchDetails(object):
+    def __init__(self, match_key=None, match_name=None, mode=None, time=None, red=Alliance(), blue=Alliance()):
+        self.match_key = match_key
+        self.match_name = match_name
+        self.mode = mode
+        self.time = time
+        self.red = red
+        self.blue = blue
+
+    def __str__(self):
+
+        return 'Match Key: {}\nMatch Name: {}\nMode: {}\nTime remaining: {}\n{}{}'.format(
+            self.match_key, self.match_name, self.mode, self.time, self.red.getString('Red'), self.blue.getString('Blue'))
