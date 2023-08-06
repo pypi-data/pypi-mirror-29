354fe037643b3f17b37c41d76736ef467bfad2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:16 +0000

    Add package gouvlu/0.1.7.dev0/gouvlu-0.1.7.dev0-py2.py3-none-any.whl

---
+++ packages/gouvlu/gouvlu-0.1.7.dev0-py2.py3-none-any.whl/gouvlu/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''
+Official udata theme and extensions of the Open Data Portal of Luxembourg
+'''
+from __future__ import unicode_literals
+
+__version__ = '0.1.7.dev'
+__description__ = 'Official udata theme and extensions of the Open Data Portal of Luxembourg'
