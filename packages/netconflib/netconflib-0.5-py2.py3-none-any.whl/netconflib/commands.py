07482b5bdac186cfa4b63c2c76cc77d1717fdbc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:02 +0000

    Add package netconflib/0.5/netconflib-0.5-py2.py3-none-any.whl

---
+++ packages/netconflib/netconflib-0.5-py2.py3-none-any.whl/netconflib/commands.py
@@ -0,0 +1,13 @@
+"""This module encapsulates constant shell commands."""
+
+class Commands:
+    """This class holds many shell commands.
+
+    You can use this class to access many constant shell commands.
+    """
+
+    cmd_echo = "echo hello"
+    cmd_ipforward = "sysctl -w net.ipv4.ip_forward=1"
+    cmd_check_key = "grep -q '{}' ~/.ssh/authorized_keys && echo $?"
+    cmd_generate_ed25519_key = 'ssh-keygen -f {} -t ed25519 -N ""'
+    cmd_start_client = "netconf -sniff"
