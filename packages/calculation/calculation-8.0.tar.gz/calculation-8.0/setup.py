6bf6030fbb4b3a790f06cc434870b3ad497ab7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:17 +0000

    Add package calculation/8.0/calculation-8.0.tar.gz

---
+++ packages/calculation/calculation-8.0.tar.gz/calculation-8.0/setup.py
@@ -0,0 +1,31 @@
+
+
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+ 
+
+setup(
+    name='calculation',
+    version='8.0',
+    description='A simple calculation package',
+    author='krushithreddy',
+    author_email='krushithreddy567@gmail.com',
+    packages=['calculation'],
+    url='https://github.com/krushithreddy/calculation',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords = 'math',
+)
