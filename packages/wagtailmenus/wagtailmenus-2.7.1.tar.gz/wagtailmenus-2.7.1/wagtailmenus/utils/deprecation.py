be57a1a038cd2e08a9b11f2ba6cf1e9724deefb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:49 +0000

    Add package wagtailmenus/2.7.1/wagtailmenus-2.7.1.tar.gz

---
+++ packages/wagtailmenus/wagtailmenus-2.7.1.tar.gz/wagtailmenus-2.7.1/wagtailmenus/utils/deprecation.py
@@ -0,0 +1,13 @@
+class RemovedInWagtailMenus28Warning(DeprecationWarning):
+    pass
+
+
+removed_in_next_version_warning = RemovedInWagtailMenus28Warning
+
+
+class RemovedInWagtailMenus29Warning(PendingDeprecationWarning):
+    pass
+
+
+class RemovedInWagtailMenus30Warning(PendingDeprecationWarning):
+    pass
