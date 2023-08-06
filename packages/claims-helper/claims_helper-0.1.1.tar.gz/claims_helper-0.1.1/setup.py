1401fac7dc25114b9571c057a4aa3f44dd6f170d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:17 +0000

    Add package claims-helper/0.1.1/claims_helper-0.1.1.tar.gz

---
+++ packages/claims-helper/claims_helper-0.1.1.tar.gz/claims_helper-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='claims_helper',
+      version='0.1.1',
+      description='Deodes roles from custom claims',
+      url='',
+      author='Blaze devices',
+      author_email='devices@blaze.cc',
+      license='MIT',
+      packages=['claims_helper'],
+      zip_safe=False)
