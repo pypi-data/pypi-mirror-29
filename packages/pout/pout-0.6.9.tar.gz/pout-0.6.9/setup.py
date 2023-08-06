5315e73d655aa59312fcc7492a58ab02a564509f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:17 +0000

    Add package pout/0.6.9/pout-0.6.9.tar.gz

---
+++ packages/pout/pout-0.6.9.tar.gz/pout-0.6.9/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# http://docs.python.org/distutils/setupscript.html
+# http://docs.python.org/2/distutils/examples.html
+
+from setuptools import setup, find_packages
+import re
+import os
+
+name = "pout"
+with open(os.path.join("{}/__init__.py".format(name))) as f:
+    version = re.search("^__version__\s*=\s*[\'\"]([^\'\"]+)", f.read(), flags=re.I | re.M).group(1)
+
+setup(
+    name=name,
+    version=version,
+    description='Prints out python variables in an easy to read way, handy for debugging',
+    author='Jay Marcyes',
+    author_email='jay@marcyes.com',
+    url='http://github.com/Jaymon/{}'.format(name),
+    #py_modules=[name],
+    packages=find_packages(),
+    license="MIT",
+    classifiers=[ # https://pypi.python.org/pypi?:action=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    tests_require=['testdata'],
+    #test_suite = "pout_test",
+    entry_points = {
+        'console_scripts': [
+            '{}.json = {}:console_json'.format(name, name),
+            '{}.char = {}:console_char'.format(name, name)
+        ],
+    }
+)
