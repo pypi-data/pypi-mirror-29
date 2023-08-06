2387577c36ddabfa7665ef0fd159442d0113933b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:39 +0000

    Add package hyperion-logs/0.0.4/hyperion-logs-0.0.4.tar.gz

---
+++ packages/hyperion-logs/hyperion-logs-0.0.4.tar.gz/hyperion-logs-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='hyperion-logs',
+    version='0.0.4',
+    packages=['hyperion'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    author="Scott Snyder",
+    author_email="scott.snyder@capitalone.com",
+    long_description=open('README.txt').read()
+)
