11cdd3d25e0a4fb85a5173ece21df584fabeaf43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:34 +0000

    Add package bcolors/1.0.2/bcolors-1.0.2.tar.gz

---
+++ packages/bcolors/bcolors-1.0.2.tar.gz/bcolors-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='bcolors',
+    version='1.0.2',
+    packages=['bcolors'],
+    license='GPL',
+    author='ysharma',
+    author_email='yks0000@gmail.com',
+    platforms="Linux, OSX",
+    description="A module for coloring print statement.",
+    long_description=open('README.txt', 'rt').read()
+)
