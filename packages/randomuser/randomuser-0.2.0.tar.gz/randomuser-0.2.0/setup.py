c80736abae39b7a785c7aa7bf0dfff9323107829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:24 +0000

    Add package randomuser/0.2.0/randomuser-0.2.0.tar.gz

---
+++ packages/randomuser/randomuser-0.2.0.tar.gz/randomuser-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='randomuser',
+        version='0.2.0',
+        description='Python class for generating random user _data using https://randomuser.me API',
+        url='https://github.com/connordelacruz/python-randomuser',
+        download_url='https://github.com/connordelacruz/python-randomuser/archive/0.2.0.tar.gz',
+        author='Connor de la Cruz',
+        author_email='connor.c.delacruz@gmail.com',
+        license='MIT',
+        packages=find_packages(),
+        zip_safe=False)
