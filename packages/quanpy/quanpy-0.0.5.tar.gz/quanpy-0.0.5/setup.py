e5ae420a5a82750947ef256eb75dee1bb9b3fa48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:25 +0000

    Add package quanpy/0.0.5/quanpy-0.0.5.tar.gz

---
+++ packages/quanpy/quanpy-0.0.5.tar.gz/quanpy-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.0.5'
+requires = []
+
+setup(
+    name='quanpy',
+    version=__version__,
+    description='Quantum Annealing Algorithm Library',
+    long_description='',
+    url='https://github.com/ymurata/quanpy',
+    author='ymurata',
+    author_email='m.yuma0531@gmail.com',
+    license='Apach 2.0',
+    keywords='quantum annealing',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3',
+    ],
+)
