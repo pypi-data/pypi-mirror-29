8956988308ac35ffdfa35b0d35ca943cbf539d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:22 +0000

    Add package networkutil/1.12.0/networkutil-1.12.0.tar.gz

---
+++ packages/networkutil/networkutil-1.12.0.tar.gz/networkutil-1.12.0/networkutil/api_support/template_api/apis/template/v0/__init__.py
@@ -0,0 +1,4 @@
+
+from .api import TemplateApiV0
+
+__all__ = [TemplateApiV0]
---
+++ packages/networkutil/networkutil-1.12.0.tar.gz/networkutil-1.12.0/networkutil/generate_requests_pem_for_windows.py
@@ -0,0 +1,17 @@
+
+import ssl
+
+__author__ = u'Oli Davis'
+__copyright__ = u'Copyright (C) 2016 Oli Davis'
+
+
+def generate_pem_from_system(output_file):
+    context = ssl.create_default_context()
+    der_certs = context.get_ca_certs(binary_form=True)
+    pem_certs = [ssl.DER_cert_to_PEM_cert(der) for der in der_certs]
+
+    with open(output_file, 'w') as outfile:
+        for pem in pem_certs:
+            outfile.write(pem + '\n')
+
+    return output_file
