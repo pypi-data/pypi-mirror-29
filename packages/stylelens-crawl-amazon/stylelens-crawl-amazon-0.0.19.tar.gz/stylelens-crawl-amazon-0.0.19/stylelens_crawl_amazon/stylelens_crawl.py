531e0e2954af6baf3a2fd7c08042bec59ad8c56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:53 +0000

    Add package stylelens-crawl-amazon/0.0.19/stylelens-crawl-amazon-0.0.19.tar.gz

---
+++ packages/stylelens-crawl-amazon/stylelens-crawl-amazon-0.0.19.tar.gz/stylelens-crawl-amazon-0.0.19/stylelens_crawl_amazon/stylelens_crawl.py
@@ -0,0 +1,33 @@
+from .aws_product_api import AwsProductApi
+import traceback
+
+class StylensCrawler(object):
+  def __init__(self):
+    self._aws = AwsProductApi()
+
+  def get_item_searches(self):
+    return self._aws.get_item_searchs()
+
+  def get_items(self, item_searches):
+    item_list = self._aws.item_search(item_searches)
+    try:
+      for items in item_list:
+        for item in items:
+          yield item
+    except Exception as e:
+      print(e)
+      traceback.print_exc(limit=None)
+      yield None
+
+  def get_similar_items(self):
+    item_list = self._aws.item_lookups()
+    try:
+      for items in item_list:
+        for item in items:
+          yield item
+    except Exception as e:
+      print(e)
+      traceback.print_exc(limit=None)
+      yield None
+
+
