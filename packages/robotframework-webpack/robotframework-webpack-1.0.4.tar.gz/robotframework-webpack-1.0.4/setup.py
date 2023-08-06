a4a128bf01649d71db46c2e2b31c0306b662f750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:15 +0000

    Add package robotframework-webpack/1.0.4/robotframework-webpack-1.0.4.tar.gz

---
+++ packages/robotframework-webpack/robotframework-webpack-1.0.4.tar.gz/robotframework-webpack-1.0.4/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+version = '1.0.4'
+
+long_description = (
+    open('README.rst').read() +
+    '\n' +
+    '\n' +
+    open('CHANGES.rst').read() +
+    '\n')
+
+setup(
+    name='robotframework-webpack',
+    version=version,
+    description="A Robot Framework library for Webpack.",
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Environment :: Web Environment',
+        'Framework :: Robot Framework',
+        'Framework :: Robot Framework :: Library',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='robotframework webpack javascript react angular vue',
+    author='Timo Stollenwerk | kitconcept GmbH',
+    author_email='stollenwerk@kitconcept.com',
+    url='https://kitconcept.com',
+    license='Apache License 2.0',
+    packages=find_packages(
+        exclude=['ez_setup', 'examples', 'tests']
+    ),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'robotframework',
+        'robotframework-selenium2library',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
