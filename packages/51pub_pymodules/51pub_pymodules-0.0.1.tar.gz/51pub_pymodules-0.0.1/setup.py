e027153e612c0d539f92d158438efe6776be04bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:53 +0000

    Add package 51pub_pymodules/0.0.1/51pub_pymodules-0.0.1.tar.gz

---
+++ packages/51pub_pymodules/51pub_pymodules-0.0.1.tar.gz/51pub_pymodules-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='51pub_pymodules',
+    version='0.0.1',
+    author='jun',
+    author_email='jun.mr@qq.com',
+    url='http://docs.51pub.cn/python/51pub_pymodules',
+    packages=['opmysql'],
+    description='system manage modules',
+    license='MIT',
+    install_requires=['pymysql']
+)
