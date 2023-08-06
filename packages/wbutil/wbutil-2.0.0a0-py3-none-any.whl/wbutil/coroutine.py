cfc680f5f7ee62b3a9a7f10e3b073c37cf5adefd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:48 +0000

    Add package wbutil/2.0.0a0/wbutil-2.0.0a0-py3-none-any.whl

---
+++ packages/wbutil/wbutil-2.0.0a0-py3-none-any.whl/wbutil/coroutine.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+'''
+wbutil/coroutine.py
+
+Helpers for easy coroutine usage.
+
+Will Badart <wbadart@live.com>
+created: FEB 2018
+'''
+
+from pprint import pprint
+from typing import Callable, Coroutine, Generator
+
+__all__ = [
+    'broadcast',
+    'prime_coroutine',
+]
+
+
+def prime_coroutine(func: Callable) -> Callable:
+    '''
+    Takes a generator-based coroutine and starts it immediately after
+    construction (so you don't have to remember to prime the coroutine).
+    TODO: complete example
+    '''
+    def _impl(*args, **kwargs):
+        routine = func(*args, **kwargs)
+        routine.send(None)
+        return routine
+    return _impl
+
+
+@prime_coroutine
+def broadcast(*targets: Coroutine) -> Generator:
+    '''
+    Send an item to multiple target coroutines.
+    TODO: complete example
+    '''
+    while True:
+        item = (yield)
+        for target in targets:
+            target.send(item)
+
+
+@prime_coroutine
+def printer():
+    '''
+    Sink for printing pipeline items.
+    TODO: complete example
+    '''
+    while True:
+        item = (yield)
+        print(item)
+
+
+@prime_coroutine
+def pprinter():
+    '''
+    Sink for pretty printing pipeline items.
+    TODO: complete example
+    '''
+    while True:
+        item = (yield)
+        pprint(item)
+        print()
