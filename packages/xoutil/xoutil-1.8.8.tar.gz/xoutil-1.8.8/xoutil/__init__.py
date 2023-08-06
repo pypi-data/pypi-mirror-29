d12a7f8a5d9e18575e1951fc5ff9187bc889f369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:55 +0000

    Add package xoutil/1.8.8/xoutil-1.8.8.tar.gz

---
+++ packages/xoutil/xoutil-1.8.8.tar.gz/xoutil-1.8.8/xoutil/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+
+'''Collection of disparate utilities.
+
+`xoutil` is essentially an extension to the Python's standard library, it does
+not turn into a full framework, but it's very useful to be used from a
+diversity of scenarios where compatibility is an important issue.
+
+'''
+
+# TODO: Deprecate this import
+from .symbols import Unset, Undefined, Ignored, Invalid  # noqa
