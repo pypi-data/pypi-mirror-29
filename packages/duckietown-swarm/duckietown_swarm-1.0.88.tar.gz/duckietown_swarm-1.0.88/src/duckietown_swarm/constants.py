e4897a600678560ead01ac28b51597bd4593ef99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:06 +0000

    Add package duckietown-swarm/1.0.88/duckietown_swarm-1.0.88.tar.gz

---
+++ packages/duckietown-swarm/duckietown_swarm-1.0.88.tar.gz/duckietown_swarm-1.0.88/src/duckietown_swarm/constants.py
@@ -0,0 +1,23 @@
+class DSwarmConstants:
+    port_dswarm = 8000
+#    port_ssh = 8001
+    port_shell = 8002
+    DELTA_PEER = 10 * 60
+    SDELTA_MAKE_SUMMARY = '5m'
+
+    SDELTA_INFO = '5d'
+
+    ALL_BRAINS = '/dswarm/*/brain'
+
+    IPFS_ADDRESSES = 'ipfs_addresses'
+    IPFS_PEERS = 'ipfs_peers'
+
+    PRI = 'pri'
+
+    # IRC_SERVER = 'irc.freenode.net'
+    IRC_SERVER = 'frankfurt.co-design.science'
+    IRC_CHANNEL = '#duckiebots'
+
+    PUBSUB_TOPIC = 'duckiebots'
+
+    PUBSUB_FRIENDS_GREETING = 'duckiebots\n'
---
+++ packages/duckietown-swarm/duckietown_swarm-1.0.88.tar.gz/duckietown_swarm-1.0.88/src/duckietown_swarm/offline.py
@@ -0,0 +1,97 @@
+import sys
+
+from contracts import contract
+from system_cmd import CmdException
+
+from .brain import DistributedRepoSwarm
+from .ipfs_utils import Timeout
+from .irc2 import MessagingInterface
+
+seen_mh = set()
+seen = set()
+
+
+@contract(mi=MessagingInterface)
+def read_nodes_summaries(mi):
+    timeout = '60s'
+    ipfsi = mi.get_ipfsi()
+
+    while True:
+#        print('waiting for summary')
+        envelopes = [mi.get_next_for_me()]
+#
+        n = len(envelopes)
+        if n > 1:
+            print('ignoring %s messages and process the last' % (n - 1))
+            envelopes = [envelopes[-1]]
+
+        for envelope in envelopes:
+#
+#            if envelope.maddr_from == mi.MADDR_BRAIN:
+#                print('skipping this from ourselves: %s' % envelope)
+#                continue
+
+#            print envelope
+
+            mh = envelope.contents
+            if mh in seen_mh:
+                continue
+
+            seen_mh.add(mh)
+            data_mh = mh + '/machines.txt'
+            print('getting %s' % data_mh)
+
+            try:
+                data = ipfsi.get(data_mh, timeout=timeout)
+            except Timeout:
+                print('timeout for %s' % data_mh)
+                continue
+            except CmdException as e:
+                print(str(e))
+                continue
+
+            data = data.strip()
+
+            lines = data.split('\n')
+
+            i = 0
+            for msg in lines:
+                if msg in seen:
+                    continue
+                else:
+#                    print('NEW: %s' % msg)
+                    seen.add(msg)
+                    i += 1
+                    from_channel = '/ipfs/' + mh
+                    mi.send_to_brain(from_channel, msg)
+
+            new_ones = i
+            print('read %s catchup messages: %s new ones' % (len(lines), new_ones))
+
+
+def go_offline():
+    s = sys.stdin.read()
+    dc = read_lines(s)
+
+    show = ['networks', 'peer', 'trusted', 'admin', 'files', 'verified', 'safe', 'summaries',
+            'pri']
+    for k in show:
+        for x in dc.query(k):
+            print('%s: %s' % (k, x))
+
+
+def read_lines(s):
+    lines = s.split('\n')
+    dc = DistributedRepoSwarm()
+    from_channel = 'stdin'
+    i = 0
+    for msg in lines:
+        dc.process(msg, from_channel)
+        i += 1
+    print('processed: %s' % i)
+    return dc
+
+
+if __name__ == '__main__':
+    go_offline()
+
