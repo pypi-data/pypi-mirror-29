c1aea715ffc0758478a1ee1323f5789ea301abac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:59 +0000

    Add package wires/18.1.0a1/wires-18.1.0a1.tar.gz

---
+++ packages/wires/wires-18.1.0a1.tar.gz/wires-18.1.0a1/src/wires/__init__.py
@@ -0,0 +1,30 @@
+# ----------------------------------------------------------------------------
+# Python Wires
+# ----------------------------------------------------------------------------
+# Copyright (c) Tiago Montes.
+# See LICENSE for deatils.
+# ----------------------------------------------------------------------------
+
+
+from __future__ import absolute_import
+
+
+
+__version__ = '18.1.0a1'
+
+__title__ = 'wires'
+__description__ = 'Python Wires'
+
+__license__ = 'MIT'
+__uri__ = 'https://github.com/tmontes/python-wires/'
+
+__author__ = 'Tiago Montes'
+__email__ = 'tiago.montes@gmail.com'
+
+
+
+from . _shell import WiresShell as Wires
+from . _singleton import wire, unwire
+
+
+# ----------------------------------------------------------------------------
