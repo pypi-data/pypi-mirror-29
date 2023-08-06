adff727de5282d638a0b76e8eec8016259196d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:07 +0000

    Add package physt/0.3.32/physt-0.3.32.tar.gz

---
+++ packages/physt/physt-0.3.32.tar.gz/physt-0.3.32/physt/plotting/__init__.py
@@ -0,0 +1,153 @@
+"""Plotting for physt histograms.
+
+Available backends:
+- matplotlib
+- vega
+- folium
+"""
+
+from __future__ import absolute_import
+from collections import OrderedDict
+
+
+backends = OrderedDict()
+
+# Use variant without exception catching if you want to debug import of backends.
+# from . import matplotlib as mpl_backend
+# backends["matplotlib"] = mpl_backend
+# from . import folium as folium_backend
+# backends["folium"] = folium_backend
+# from . import vega as vega_backend
+# backends["vega"] = vega_backend
+
+try:
+    from . import matplotlib as mpl_backend
+    backends["matplotlib"] = mpl_backend
+except:
+    pass
+
+try:
+    from . import folium as folium_backend
+    backends["folium"] = folium_backend
+except:
+    pass
+
+try:
+    from . import vega as vega_backend
+    backends["vega"] = vega_backend
+except:
+    pass
+
+
+if backends:
+    _default_backend = list(backends.keys())[0]
+else:
+    _default_backend = None
+
+
+def set_default_backend(name):
+    """Choose a default backend.
+    
+    Parameters
+    ----------
+    name: str
+    """
+    global _default_backend
+    if name == "bokeh":
+        raise RuntimeError("Support for bokeh has been discontinued. At some point, we may return to support holoviews.")
+    if not name in backends:
+        raise RuntimeError("Backend {0} is not supported".format(name))
+    _default_backend = name
+
+
+def _get_backend(name=None):
+    """Get a plotting backend.
+
+    Tries to get it using the name - or the default one.
+
+    Parameters
+    ----------
+    name: Optional[str]
+        Name of the backend. If not specified, default one is selected.
+    """
+    if not backends:
+        raise RuntimeError("No plotting backend available. Please, install matplotlib (preferred) or bokeh (limited).")
+    if not name:
+        name = _default_backend
+    if name == "bokeh":
+        raise RuntimeError("Support for bokeh has been discontinued. At some point, we may return to support holoviews.")
+    backend = backends.get(name)
+    if not backend:
+        raise RuntimeError("Backend {0} does not exist. Use one of the following: {1}".format(name, ", ".join(backends.keys())))
+    return name, backends[name]
+
+
+def plot(histogram, kind=None, backend=None, **kwargs):
+    """Universal plotting function.
+
+    All keyword arguments are passed to the plotting methods.
+
+    Parameters
+    ----------
+    histogram: physt.HistogramBase
+    kind: Optional[str]
+        Type of the plot (like "scatter", "line", ...), similar to pandas
+    backend: Optional[str]
+    """
+    backend_name, backend = _get_backend(backend)
+    if kind is None:
+        kinds = [t for t in backend.types if histogram.ndim in backend.dims[t]]
+        if not kinds:
+            raise RuntimeError("No histogram type is supported for {0}"
+                               .format(histogram.__class__.__name__))
+        kind = kinds[0]
+    if kind in backend.types:
+        method = getattr(backend, kind)
+        return method(histogram, **kwargs)
+    else:
+        raise RuntimeError("Histogram type error: {0} missing in backend {1}"
+                           .format(kind, backend_name))
+
+
+class PlottingProxy(object):
+    """Proxy enabling to call plotting methods on histogram objects.
+
+    It can be used both as a method or as an object containing methods. In any case,
+    it only forwards the call to the universal plot() function.
+
+    The __dir__ method should offer all plotting methods supported by the currently
+    selected backend.
+
+    Example
+    -------
+        plotter = histogram.plot
+        plotter(...)          # Plots using defaults
+        plotter.bar(...)      # Plots as a specified plot type ("bar")
+
+    Note
+    ----
+    Inspiration taken from the way how pandas deals with this.
+
+    """
+
+    def __init__(self, h):
+        self.histogram = h
+
+    def __call__(self, kind=None, **kwargs):
+        """Use the plotter as callable.
+
+        Parameters
+        ----------
+        histtype: Optional[str]
+        """
+        return plot(self.histogram, kind=kind, **kwargs)
+
+    def __getattr__(self, name):
+        """Use the plotter as a proxy object with separate plotting methods."""
+        def plot_function(**kwargs):
+            return plot(self.histogram, name, **kwargs)
+        return plot_function
+
+    def __dir__(self):
+        _, backend = _get_backend()
+        return tuple((t for t in backend.types if self.histogram.ndim in backend.dims[t]))
---
+++ packages/physt/physt-0.3.32.tar.gz/physt-0.3.32/physt/plotting/common.py
@@ -0,0 +1,67 @@
+"""
+Functions that are shared by several (all) plotting backends.
+
+"""
+from __future__ import absolute_import
+
+
+def get_data(histogram, density=False, cumulative=False, flatten=False):
+    """Get histogram data based on plotting parameters.
+
+    Parameters
+    ----------
+    h : physt.histogram_base.HistogramBase
+    density : bool
+        Whether to divide bin contents by bin size
+    cumulative : bool
+        Whether to return cumulative sums instead of individual
+    flatten : bool
+        Whether to flatten multidimensional bins
+
+    Returns
+    -------
+    np.ndarray
+
+    """
+    if density:
+        if cumulative:
+            data = (histogram / histogram.total).cumulative_frequencies
+        else:
+            data = histogram.densities
+    else:
+        if cumulative:
+            data = histogram.cumulative_frequencies
+        else:
+            data = histogram.frequencies
+
+    if flatten:
+        data = data.flatten()
+    return data
+
+
+def get_err_data(histogram, density=False, cumulative=False, flatten=False):
+    """Get histogram error data based on plotting parameters.
+
+    Parameters
+    ----------
+    h : physt.histogram_base.HistogramBase
+    density : bool
+        Whether to divide bin contents by bin size
+    cumulative : bool
+        Whether to return cumulative sums instead of individual
+    flatten : bool
+        Whether to flatten multidimensional bins
+
+    Returns
+    -------
+    np.ndarray
+    """
+    if cumulative:
+        raise RuntimeError("Error bars not supported for cumulative plots.")
+    if density:
+        data = histogram.errors / histogram.bin_sizes
+    else:
+        data = histogram.errors
+    if flatten:
+        data = data.flatten()
+    return data
