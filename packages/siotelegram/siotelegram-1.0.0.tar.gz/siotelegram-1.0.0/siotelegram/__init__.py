3371fd847b66b1d4386bf984fbb5b34fe4d950b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:48 +0000

    Add package siotelegram/1.0.0/siotelegram-1.0.0.tar.gz

---
+++ packages/siotelegram/siotelegram-1.0.0.tar.gz/siotelegram-1.0.0/siotelegram/__init__.py
@@ -0,0 +1,13 @@
+from .protocol import *
+from .io import *
+
+
+__all__ = [
+    protocol.__all__ +
+    io.__all__ +
+    ("version", "__version__")
+]
+
+
+__version__ = "1.0.0"
+version = tuple(map(int, str.split(__version__, ".")))
