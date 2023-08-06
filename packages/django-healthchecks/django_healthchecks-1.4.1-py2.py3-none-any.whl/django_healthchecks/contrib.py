40dc88918d5325b9826270595077e249f535a012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:10 +0000

    Add package django-healthchecks/1.4.1/django_healthchecks-1.4.1-py2.py3-none-any.whl

---
+++ packages/django-healthchecks/django_healthchecks-1.4.1-py2.py3-none-any.whl/django_healthchecks/contrib.py
@@ -0,0 +1,36 @@
+import uuid
+
+from django.core.cache import cache
+from django.db import connection
+
+
+def check_database():
+    """Check if the application can perform a dummy sql query"""
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT 1; -- Healthcheck')
+        row = cursor.fetchone()
+    return row[0] == 1
+
+
+def check_cache_default():
+    """Check if the application can connect to the default cached and
+    read/write some dummy data.
+
+    """
+    dummy = str(uuid.uuid4())
+    key = 'healthcheck:%s' % dummy
+    cache.set(key, dummy, timeout=5)
+    cached_value = cache.get(key)
+    return cached_value == dummy
+
+
+def check_dummy_true():
+    return True
+
+
+def check_dummy_false():
+    return False
+
+
+def check_remote_addr(request):
+    return request.META['REMOTE_ADDR']
