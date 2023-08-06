445136e2704f5518d09cee38be41f3c93f57cc42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:44 +0000

    Add package SAFMI_AFM/1.0.1/SAFMI_AFM-1.0.1.tar.gz

---
+++ packages/SAFMI_AFM/SAFMI_AFM-1.0.1.tar.gz/SAFMI_AFM-1.0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 19:58:29 2018
+
+@author: sarth
+"""
+
+from setuptools import setup
+
+setup(
+name = 'SAFMI_AFM',
+version = '1.0.1',
+packages=['SAFMI/UserImageProcessing', 'SAFMI/ImageDisorderPrediction']
+)
