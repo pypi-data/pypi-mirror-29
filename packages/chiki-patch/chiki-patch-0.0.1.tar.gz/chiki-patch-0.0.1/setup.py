618d1fb66d0c92dc30818ecec93c0d603924b56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:08 +0000

    Add package chiki-patch/0.0.1/chiki-patch-0.0.1.tar.gz

---
+++ packages/chiki-patch/chiki-patch-0.0.1.tar.gz/chiki-patch-0.0.1/setup.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+import re
+import os
+from setuptools import setup, find_packages
+
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def read(fname):
+    return open(fpath(fname)).read()
+
+
+file_text = read(fpath('chiki_patch.py'))
+
+
+def grep(attrname):
+    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
+    strval, = re.findall(pattern, file_text)
+    return strval
+
+
+def get_data_files(*dirs):
+    results = []
+    for src_dir in dirs:
+        for root, dirs, files in os.walk(src_dir):
+            results.append((root, map(lambda f: root + "/" + f, files)))
+    return results
+
+
+setup(
+    name='chiki-patch',
+    version='0.0.1',
+    url='https://github.com/endsh/chiki-patch',
+    author='Linshao',
+    author_email='438985635@qq.com',
+    description='chiki patch libs.',
+    py_modules=['chiki_patch'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask==0.10.1',
+        'Flask-Login==0.2.11',
+    ],
+)
