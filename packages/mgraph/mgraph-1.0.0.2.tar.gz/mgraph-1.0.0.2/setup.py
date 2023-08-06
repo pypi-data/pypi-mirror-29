dc75524bc3ef3e3e726a686c48aa4e8fae957624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:23 +0000

    Add package mgraph/1.0.0.2/mgraph-1.0.0.2.tar.gz

---
+++ packages/mgraph/mgraph-1.0.0.2.tar.gz/mgraph-1.0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup( name = "mgraph",
+       url = "https://bitbucket.org/mjr129/mgraph",
+       version = "1.0.0.2",
+       description = "Yet another graphing library. This library supports Groot, providing functionality for dealing with graphs somewhere on the phylogenetic tree/network border.",
+       author = "Martin Rusilowicz",
+       license = "https://www.gnu.org/licenses/agpl-3.0.html",
+       packages = ["mgraph"],
+       python_requires = ">=3.6",
+       install_requires = ["ete3",
+                           "mhelper"]
+       )
