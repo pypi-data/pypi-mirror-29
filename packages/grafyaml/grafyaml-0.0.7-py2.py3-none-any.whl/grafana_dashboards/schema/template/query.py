fa84ae1d883062e31dea7ee93cbeb08ce9f0aa5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:48 +0000

    Add package grafyaml/0.0.7/grafyaml-0.0.7-py2.py3-none-any.whl

---
+++ packages/grafyaml/grafyaml-0.0.7-py2.py3-none-any.whl/grafana_dashboards/schema/template/query.py
@@ -0,0 +1,32 @@
+# Copyright 2015 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import voluptuous as v
+
+from grafana_dashboards.schema.template.base import Base
+
+
+class Query(Base):
+
+    def get_schema(self):
+        query = {
+            v.Required('includeAll', default=False): v.All(bool),
+            v.Required('multi', default=False): v.All(bool),
+            v.Required('query', default=''): v.All(str),
+            v.Required('refresh', default=False): v.All(bool),
+            v.Optional('datasource'): v.All(str),
+            v.Optional('hide'): v.All(int, v.Range(min=0, max=2)),
+        }
+        query.update(self.base)
+        return v.Schema(query)
