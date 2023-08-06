e0a05c8d53d25072dfb167b4396325bb2e812f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:39 +0000

    Add package cacheunittest/0.1.6/cacheunittest-0.1.6.tar.gz

---
+++ packages/cacheunittest/cacheunittest-0.1.6.tar.gz/cacheunittest-0.1.6/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='cacheunittest',
+    version='0.1.6',
+    author='Patrice Eber',
+    author_email='patrice.eber@arte.tv',
+    url='https://pypi.org/pypi/CacheUnitTest/',
+    license='LICENSE.txt',
+    description='Varnish unit test',
+    long_description=open('README.txt').read(),
+    install_requires=[
+    "request >= 2.18"]
+)
