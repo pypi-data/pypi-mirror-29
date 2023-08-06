b4fc117034aab64c494aa6f4c6b34d0e1eff3ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:18 +0000

    Add package redlemur/0.26/redlemur-0.26.tar.gz

---
+++ packages/redlemur/redlemur-0.26.tar.gz/redlemur-0.26/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'redlemur',
+  packages = ['lemur'],
+  version = '0.26',
+  description = 'High level visualization library',
+  author = 'Nitin Kumar',
+  author_email = 'nkumar14@jhu.edu',
+  url = 'https://github.com/NeuroDataDesign/lemur',
+  download_url = 'https://github.com/NeuroDataDesign/lemur/archive/0.26.tar.gz',
+  keywords = ['visualization', 'neuroscience', 'embeddings'],
+  classifiers = [],
+)
