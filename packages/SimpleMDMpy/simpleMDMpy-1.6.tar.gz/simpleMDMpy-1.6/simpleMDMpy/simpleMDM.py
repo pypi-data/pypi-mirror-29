d266ecf2f32dd2f13b3b00b926d53d2f3bd6f3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:50 +0000

    Add package SimpleMDMpy/1.6/simpleMDMpy-1.6.tar.gz

---
+++ packages/SimpleMDMpy/simpleMDMpy-1.6.tar.gz/simpleMDMpy-1.6/simpleMDMpy/simpleMDM.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python 
+
+import requests
+
+class ApiError(Exception):
+    pass
+
+class connection:
+    proxyDict = dict()
+
+    def __init__(self, apiKey):
+        self.apiKey = apiKey
+
+    def _url(self, path):
+        return 'https://a.simplemdm.com/api/v1' + path
+
+    def _getData(self, url, data=None):
+        resp = requests.get(url, data, auth=(self.apiKey, ""), proxies=self.proxyDict)
+        return resp
+        
+    
+    def _patchData(self, url, data, files=None):
+        resp = requests.patch(url, data, auth=(self.apiKey, ""), files=files, proxies=self.proxyDict)
+        return resp
+    
+    def _postData(self, url, data, files=None):
+        resp = requests.post(url, data, auth=(self.apiKey, ""), files=files, proxies=self.proxyDict)
+        return resp
+        
+    
+    def _putData(self, url, data, files=None):
+        resp = requests.put(url, data, auth=(self.apiKey, ""), files=files, proxies=self.proxyDict)
+        return resp
+    
+    def _deleteData(self, url):
+        return requests.delete(url, auth=(self.apiKey, ""), proxies=self.proxyDict)
