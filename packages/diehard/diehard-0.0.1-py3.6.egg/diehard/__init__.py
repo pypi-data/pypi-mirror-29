dccc66c100aec57080f9280f2f9fc1da70a4047f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:20 +0000

    Add package diehard/0.0.1/diehard-0.0.1-py3.6.egg

---
+++ packages/diehard/diehard-0.0.1-py3.6.egg/diehard/__init__.py
@@ -0,0 +1,12 @@
+"""
+Tests for randomness.
+"""
+
+__title__ = 'diehard'
+__version__ = '0.0.1'
+__build__ = 0x021300
+__author__ = 'Elton Law'
+__license__ = 'Apache License 2.0'
+__copyright__ = 'Copyright 2018 Elton law'
+
+__all__ = [""]
