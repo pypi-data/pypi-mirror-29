020a09ac089c425836db72b8d082ce89f72881b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:48 +0000

    Add package scopus.wp/0.3.0.10/scopus.wp-0.3.0.10.tar.gz

---
+++ packages/scopus.wp/scopus.wp-0.3.0.10.tar.gz/scopus.wp-0.3.0.10/ScopusWp/scopus/install.py
@@ -0,0 +1,65 @@
+from jutil.installation import SetupController, FileSetupController, installation_database_singleton
+
+from ScopusWp.config import PATH, Config
+
+import pathlib
+import os
+
+
+class Db(installation_database_singleton(
+    Config.get_instance()['MYSQL']['username'],
+    Config.get_instance()['MYSQL']['password'],
+    Config.get_instance()['MYSQL']['host']
+)):
+    pass
+
+
+class ScopusSetup(SetupController):
+
+    def __init__(self, project_path):
+        self.path = pathlib.Path(project_path) / 'scopus'
+
+        self.authors_setup = FileSetupController(
+            str(self.path),
+            'authors.ini',
+            os.path.join(PATH, 'scopus', 'templates', 'authors.ini'),
+            shorthand='SHORTHAND',
+            ids=[],
+            first_name='',
+            last_name='',
+            keywords=[],
+            whitelist=[],
+            blacklist=[]
+        )
+
+    def run(self):
+        self.setup_files()
+        self.setup_database()
+
+    def create_folder(self):
+        if not self.path.exists():
+            self.path.mkdir()
+            print('CREATED SCOPUS FOLDER IN PROJECT FOLDER')
+
+    def setup_files(self):
+        print('\n[SCOPUS FILE SETUP]')
+        self.create_folder()
+
+        self.authors_setup.run()
+
+    def setup_database(self):
+        print('\n[SCOPUS DATABASE SETUP]')
+        try:
+            from ScopusWp.scopus.persistency import ScopusMySQLDatabase, BASE
+
+            # Creating the database
+            Db.create_database('__scopus')
+            print('"__scopus" DATABASE CREATED')
+            # Creating all the tables according to the models
+            session = ScopusMySQLDatabase.get_session()
+            ScopusMySQLDatabase.create_database(BASE)
+            print('SCOPUS DATABASE TABLES CREATED')
+
+        except Exception as e:
+            print('FAILED TO INSTALL SCOPUS DATA BASE DUE TO EXCEPTION: "{}"'.format(str(e).strip('\n')))
+
