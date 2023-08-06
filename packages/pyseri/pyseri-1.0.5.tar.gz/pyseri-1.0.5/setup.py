264c6f98d8c5b8d1fcd70793394ddb36df0ceb98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:40 +0000

    Add package pyseri/1.0.5/pyseri-1.0.5.tar.gz

---
+++ packages/pyseri/pyseri-1.0.5.tar.gz/pyseri-1.0.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+PACKAGE = "pyseri"
+NAME = "pyseri"
+DESCRIPTION = "a python object to dict serialzer tool"
+AUTHOR = "Taylor"
+AUTHOR_EMAIL = "tank357@icloud.com"
+URL = "https://github.com/TaylorHere/pyseri"
+VERSION = "1.0.5"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description="""
+
+    """,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="GPL",
+    url=URL,
+    packages=['pyseri'],
+    package_data={},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+    ],
+    zip_safe=False,
+    install_requires=[],
+    entry_points={
+    }
+
+)
