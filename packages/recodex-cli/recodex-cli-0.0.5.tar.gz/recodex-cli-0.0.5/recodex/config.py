40a8a1afdec0a8f9942f9621093e52c42eedeea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:32 +0000

    Add package recodex-cli/0.0.5/recodex-cli-0.0.5.tar.gz

---
+++ packages/recodex-cli/recodex-cli-0.0.5.tar.gz/recodex-cli-0.0.5/recodex/config.py
@@ -0,0 +1,18 @@
+from ruamel import yaml
+from typing import NamedTuple, Optional
+
+from pathlib import Path
+
+
+class UserContext(NamedTuple):
+    api_url: Optional[str] = None
+    api_token: Optional[str] = None
+
+    @classmethod
+    def load(cls, config_path: Path):
+        config = yaml.safe_load(config_path.open("r"))
+        return cls(**config)
+
+    def store(self, config_path: Path):
+        config_path.parent.mkdir(parents=True, exist_ok=True)
+        yaml.dump(dict(self._asdict()), config_path.open("w"))
