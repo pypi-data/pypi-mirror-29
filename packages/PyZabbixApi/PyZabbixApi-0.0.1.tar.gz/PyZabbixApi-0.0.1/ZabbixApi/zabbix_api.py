098a4752c32666755db7478d8c514a65e534c2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:55 +0000

    Add package PyZabbixApi/0.0.1/PyZabbixApi-0.0.1.tar.gz

---
+++ packages/PyZabbixApi/PyZabbixApi-0.0.1.tar.gz/PyZabbixApi-0.0.1/ZabbixApi/zabbix_api.py
@@ -0,0 +1,78 @@
+import requests
+import json
+
+
+class ZabbixApi:
+    def __init__(self, api_url, user, password):
+        self.api_url = api_url
+        login_data = {
+            "jsonrpc": "2.0",
+            "method": "user.login",
+            "params": {
+                "user": user,
+                "password": password
+            },
+            "id": 1
+        }
+        self.headers = {
+            'Content-Type': 'application/json'
+        }
+        self.access_token = requests.post(self.api_url, data=json.dumps(login_data), headers=self.headers).json()[
+            'result']
+
+    def get_item_history(self, item, start, stop):
+        post_param = {
+            "jsonrpc": "2.0",
+            "method": "history.get",
+            "params": {
+                "output": "extend",
+                "history": 0,
+                "itemids": item,
+                "time_from": start,
+                "time_till": stop,
+                "sortfield": "clock",
+                "sortorder": "DESC"
+            },
+            "auth": self.access_token,
+            "id": 1
+        }
+        result = requests.post(self.api_url, data=json.dumps(post_param), headers=self.headers).json()[
+            'result']
+        return result
+
+    def get_host_info(self, ip):
+        post_param = {
+            "jsonrpc": "2.0",
+            "method": "host.get",
+            "params": {
+                "filter": {
+                    "host": [
+                        ip
+                    ]
+                }
+            },
+            "auth": "d75145c592a9e6590ac5e1d388964759",
+            "id": 1
+        }
+        result = requests.post(self.api_url, data=json.dumps(post_param), headers=self.headers).json()[
+            'result']
+        return result
+
+    def get_host_items(self, hostid, key):
+        post_param = {
+            "jsonrpc": "2.0",
+            "method": "item.get",
+            "params": {
+                "output": "extend",
+                "hostids": hostid,
+                "search": {
+                    "key_": key
+                },
+                "sortfield": "name"
+            },
+            "auth": self.access_token,
+            "id": 1
+        }
+        result = requests.post(self.api_url, data=json.dumps(post_param), headers=self.headers).json()['result']
+        return result
+
---
+++ packages/PyZabbixApi/PyZabbixApi-0.0.1.tar.gz/PyZabbixApi-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='PyZabbixApi',
+    version='0.0.1',
+    author='jun',
+    author_email='jun.mr@qq.com',
+    url='http://docs.51pub.cn/python/zabbixapi',
+    packages=['ZabbixApi'],
+    description='a zabbix api module',
+    license='MIT',
+    install_requires=['requests>=2.1'],
+)
