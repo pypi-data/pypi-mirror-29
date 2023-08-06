41202336a4b2f9ec2745e779da66793358230de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:56 +0000

    Add package mshow/5.2/mshow-5.2.tar.gz

---
+++ packages/mshow/mshow-5.2.tar.gz/mshow-5.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mshow',
+      version='5.2',
+      description='Sample Message Showing Library',
+      url='https://github.com/nishantsinghdev/mshow',
+      author='Nishant Singh',
+      author_email='nishant@example.com',
+      license='MIT',
+      packages=['mshow'],
+      zip_safe=False)
