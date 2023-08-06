f3004e80a74a1d3ece6da05ddb38d1412468baca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:58 +0000

    Add package business-duration/0.3/business_duration-0.3.tar.gz

---
+++ packages/business-duration/business_duration-0.3.tar.gz/business_duration-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='business_duration',
+	version='0.3',
+	description='Calculates business duration in days, hours, minutes and seconds by excluding weekends, public holidays and non-business hours',
+	url='https://github.com/gnaneshwar441/Business_Duration',
+	author='Gnaneshwar G',
+	author_email='gnaneshwar441@gmail.com',
+	license='MIT',
+	packages=['business_duration'],
+	keywords = ['business', 'duration', 'time', 'hour', 'day', 'working'],
+	zip_safe=False)
