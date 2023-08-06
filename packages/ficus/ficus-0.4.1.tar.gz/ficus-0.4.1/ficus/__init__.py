642f3fea33ee6953013e4ff21ed7ff5a4bf469de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:07 +0000

    Add package ficus/0.4.1/ficus-0.4.1.tar.gz

---
+++ packages/ficus/ficus-0.4.1.tar.gz/ficus-0.4.1/ficus/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from os.path import join, dirname
+rel_path = dirname(__file__)
+__version__ = open(join(rel_path, 'VERSION')).read().strip()
+
+from ficus.manager import FigureManager
