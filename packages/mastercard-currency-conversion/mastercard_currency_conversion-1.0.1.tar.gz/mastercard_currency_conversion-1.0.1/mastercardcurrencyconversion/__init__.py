2cb40e398c1faea5c16c07c84f3d6c0ef6680613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:15 +0000

    Add package mastercard-currency-conversion/1.0.1/mastercard_currency_conversion-1.0.1.tar.gz

---
+++ packages/mastercard-currency-conversion/mastercard_currency_conversion-1.0.1.tar.gz/mastercard_currency_conversion-1.0.1/mastercardcurrencyconversion/__init__.py
@@ -0,0 +1,10 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardcurrencyconversion.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardcurrencyconversion.conversionrate import *
+from mastercardcurrencyconversion.currencies import *
+from mastercardcurrencyconversion.rateissued import *
