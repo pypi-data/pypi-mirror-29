80e2ced39a163419b6bd2b721a2baa905c1fd48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:52 +0000

    Add package pypi-test1234/1.0.0/pypi_test1234-1.0.0.tar.gz

---
+++ packages/pypi-test1234/pypi_test1234-1.0.0.tar.gz/pypi_test1234-1.0.0/first_test.py
@@ -0,0 +1,8 @@
+''' This is a test file used for figuring out how to upload
+    a module to pypi'''
+import other_test
+
+def first_test(x,y):
+    ''' :param: name of user as string
+        :return: n/a'''
+    return other_test.add_numbers(x,y)
---
+++ packages/pypi-test1234/pypi_test1234-1.0.0.tar.gz/pypi_test1234-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup   (
+            name            = 'pypi_test1234',
+            version         = '1.0.0',
+            py_modules      = ['first_test'],
+            author          = 'rb',
+            author_email    = 'royce5branning@yahoo.com',
+            url             = 'https://upload.pypi.org/legacy/',
+            description     = 'This is a quick test to try out pypi',
+        )
