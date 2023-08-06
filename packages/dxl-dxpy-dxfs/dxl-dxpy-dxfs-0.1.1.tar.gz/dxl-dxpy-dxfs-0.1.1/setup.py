9d24cd5f779a1f6bb05c118bb66ed187178c7113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:37 +0000

    Add package dxl-dxpy-dxfs/0.1.1/dxl-dxpy-dxfs-0.1.1.tar.gz

---
+++ packages/dxl-dxpy-dxfs/dxl-dxpy-dxfs-0.1.1.tar.gz/dxl-dxpy-dxfs-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(name='dxl-dxpy-dxfs',
+      version='0.1.1',
+      description='File system library.',
+      url='https://github.com/Hong-Xiang/dxl',
+      author='Hong Xiang',
+      author_email='hx.hongxiang@gmail.com',
+      license='MIT',
+      packages=['dxl'],
+      package_dir = {'': 'src/python'},
+      install_requires=[
+          'click',
+          'rx',
+      ],
+      scripts=['src/cli/dxfs.py'],
+      namespace_packages = ['dxl'],
+      zip_safe=False)
