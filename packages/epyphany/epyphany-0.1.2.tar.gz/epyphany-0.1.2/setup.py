465dcd7c7147cd32dc8ca78efcb5f7de5d3b5415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:46 +0000

    Add package epyphany/0.1.2/epyphany-0.1.2.tar.gz

---
+++ packages/epyphany/epyphany-0.1.2.tar.gz/epyphany-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import os
+from distutils.core import setup
+
+
+setup(
+    name = 'epyphany',
+    version = '0.1.2',
+    packages=['epyphany'],
+    description = ('A script for facilitating network service discovery.'),
+    python_requires='>=3.5.3',
+
+    author = 'Stumblinbear',
+    author_email = 'stumblinbear@gmail.com',
+
+    license = 'GNU',
+    keywords = 'network discovery discover service broadcast packet',
+    url = 'https://github.com/Secret-Web/epyphany',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: AsyncIO',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: System :: Networking'
+    ],
+)
