a8e39118aeaa134ef17c8334ff5047b0714edc54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package waldur-auth-openid/0.8.9/waldur-auth-openid-0.8.9.tar.gz

---
+++ packages/waldur-auth-openid/waldur-auth-openid-0.8.9.tar.gz/waldur-auth-openid-0.8.9/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+dev_requires = [
+    'Sphinx==1.2.2',
+]
+
+install_requires = [
+    'django-openid-auth>=0.14',
+    'waldur-core>=0.151.0',
+]
+
+setup(
+    name='waldur-auth-openid',
+    version='0.8.9',
+    author='OpenNode Team',
+    author_email='info@opennodecloud.com',
+    url='http://waldur.com',
+    description='Waldur plugin bringing OpenID-based authentication support.',
+    long_description=open('README.rst').read(),
+    license='MIT',
+    package_dir={'': 'src'},
+    packages=find_packages('src', exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
+    install_requires=install_requires,
+    zip_safe=False,
+    extras_require={
+        'dev': dev_requires,
+    },
+    entry_points={
+        'waldur_extensions': (
+            'waldur_auth_openid = waldur_auth_openid.extension:WaldurAuthOpenIDExtension',
+        ),
+    },
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
