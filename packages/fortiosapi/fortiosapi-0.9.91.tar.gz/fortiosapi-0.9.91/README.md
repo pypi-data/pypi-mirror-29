d8d96b20dea2758b8bad301b6fc1e53a8ebbf28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:23 +0000

    Add package fortiosapi/0.9.91/fortiosapi-0.9.91.tar.gz

---
+++ packages/fortiosapi/fortiosapi-0.9.91.tar.gz/fortiosapi-0.9.91/README.md
@@ -0,0 +1,21 @@
+# fortiosAPI
+
+Python library to configure Fortinet devices (REST API and SSH)
+
+Compare to the REST API there a few add-ons:
+ In addition to get,put,post,delete methods there is a set which will
+ try to post and if failing will put and collect the mkey directly.
+
+## Multi vdom
+In multi vdom environment use vdom=global in the API call.
+As it is a reserved word the API will switch to use the global=1 and
+take care of the differences in the repsonses.
+
+## Schema
+There is a get_schema call and an example to get the schema of the
+differents methods to ease writting them.
+
+## License (5.6)
+A rest call to check and force license validation check starting with 5.6
+See license.
+usage of schema and mkey for every call for 5.6 
---
+++ packages/fortiosapi/fortiosapi-0.9.91.tar.gz/fortiosapi-0.9.91/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='fortiosapi',
+    version='0.9.91',
+    description=('Python modules to interact with fortinet'
+                 ' products configuration rest and ssh'),
+    long_description=readme(),
+    # Valid Classifiers are here:
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python ',
+        'Topic :: Security',
+    ],
+    keywords='Fortinet fortigate fortios rest api',
+    install_requires=['requests', 'paramiko'],
+    author='Nicolas Thomas',
+    author_email='nthomas@fortinet.com',
+    url='https://github.com/fortinet-solutions-cse/fortiosapi',
+    include_package_data=True,
+    packages=['fortiosapi'],
+)
