0a19e4aa5f86affdf0daf3d9238da2f101573702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:01 +0000

    Add package deepbiome_scripts/0.2/deepbiome_scripts-0.2.tar.gz

---
+++ packages/deepbiome_scripts/deepbiome_scripts-0.2.tar.gz/deepbiome_scripts-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'deepbiome_scripts',
+  packages = ['deepbiome_scripts'], 
+  version = '0.2',
+  description = 'Package for useful scripts used in DeepBiome pipeline',
+  author = 'Diego Borges',
+  author_email = 'dborges@deepbiometherapeutics.com',
+  url = 'https://www.deepbiometherapeutics.com/', 
+  download_url = 'https://github.com/deepbiome/BGC_pipeline/archive/0.2.tar.gz', 
+  keywords = ['deepbiome','metagenome'], 
+  install_requires=[''],
+  classifiers = [],
+)
