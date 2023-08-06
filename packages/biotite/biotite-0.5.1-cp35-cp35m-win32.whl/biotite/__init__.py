c4734214aab4b94e8535566ae35a4da694fe9e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:45 +0000

    Add package biotite/0.5.1/biotite-0.5.1-cp35-cp35m-win32.whl

---
+++ packages/biotite/biotite-0.5.1-cp35-cp35m-win32.whl/biotite/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2017 Patrick Kunzmann.
+# This source code is part of the Biotite package and is distributed under the
+# 3-Clause BSD License. Please see 'LICENSE.rst' for further information.
+
+from .file import *
+from .temp import *
+from .copyable import * 
+
+
+__version__ = "0.5.1"
