11614bb5d4c5b7c0f3a2056d16d11c5cccc134b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:21 +0000

    Add package classtest/1.0.2/classtest-1.0.2.tar.gz

---
+++ packages/classtest/classtest-1.0.2.tar.gz/classtest-1.0.2/setup.py
@@ -0,0 +1,13 @@
+#coding=utf-8  
+#from distutils.core import setup
+from setuptools import setup,find_packages
+
+setup(
+    name = 'classtest',
+    version = '1.0.2',
+    py_modules = ['classtest'],
+    author = 'cx_fooxcool',
+    author_email = 'cxmyforevermemory@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple test of class',
+    )
