7542d267d5c7bca308f46cd3a02979618ec70d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:29 +0000

    Add package noisyduck/0.2.0/noisyduck-0.2.0.tar.gz

---
+++ packages/noisyduck/noisyduck-0.2.0.tar.gz/noisyduck-0.2.0/HISTORY.rst
@@ -0,0 +1,16 @@
+=======
+History
+=======
+
+0.1.0 (2018-02-02)
+------------------
+
+* First release on PyPI.
+
+0.1.5 (2018-03-12)
+------------------
+
+* New governing equations
+* Tests for uniform axial mean flow
+* New API + documentation
+
---
+++ packages/noisyduck/noisyduck-0.2.0.tar.gz/noisyduck-0.2.0/setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click>=6.0',
+    # TODO: put package requirements here
+    'numpy>=1.7',
+    'scipy',
+    'matplotlib'
+]
+
+setup_requirements = [
+    'pytest-runner',
+    # TODO(nwukie): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='noisyduck',
+    version='0.2.0',
+    description="A python tool for computing eigenmode decompositions of duct flows.",
+    long_description=readme + '\n\n' + history,
+    author="Nathan Wukie",
+    author_email='nathan.wukie@gmail.com',
+    url='https://github.com/nwukie/noisyduck',
+    #packages=find_packages(include=['noisyduck']),
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'noisyduck=noisyduck.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    zip_safe=False,
+    keywords='noisyduck',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
