79ed8f9c2712694aef3804cc6e4bdb793b6cdeea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:26 +0000

    Add package botPythonPackages/0.3/botPythonPackages-0.3.tar.gz

---
+++ packages/botPythonPackages/botPythonPackages-0.3.tar.gz/botPythonPackages-0.3/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name = 'botPythonPackages',
+  packages = ['packages'],
+  version = '0.3',
+  description = "Basic utility packages for Coinecy' bot",
+  author = 'Ezio Tedeschi',
+  author_email = 'ezio.tedeschi@cryptoexchange.fr',
+  url = 'https://git.cryptoexchange.fr/opensource/botPythonPackages',
+  download_url = 'https://git.cryptoexchange.fr/opensource/botPythonPackages/archive/botPythonPackages-0.3.tar.gz',
+  keywords = ['coinecy'],
+  classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Programming Language :: Python :: 3.6'
+  ]
+)
