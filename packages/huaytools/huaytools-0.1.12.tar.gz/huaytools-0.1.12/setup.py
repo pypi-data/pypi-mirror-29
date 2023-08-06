4ea0fd769d34c1a6fa912d7db0f7a7fde99f3bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:23 +0000

    Add package huaytools/0.1.12/huaytools-0.1.12.tar.gz

---
+++ packages/huaytools/huaytools-0.1.12.tar.gz/huaytools-0.1.12/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: huay
+# Mail: imhuay@163.com
+# Created Time:  2018-1-26 11:33:13
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name="huaytools",
+    version="0.1.12",
+    keywords=("huay", "huaytools"),
+    description="huay's tools",
+    long_description="huay's tools",
+    license="MIT Licence",
+
+    url="https://github.com/imhuay/huaytools",
+    author="huay",
+    author_email="imhuay@163.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=['six', 'bs4']
+)
