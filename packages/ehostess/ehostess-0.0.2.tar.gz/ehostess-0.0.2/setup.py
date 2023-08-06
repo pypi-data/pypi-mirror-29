648cc2a118f9b687c19d495332536554b25cd28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:16 +0000

    Add package ehostess/0.0.2/ehostess-0.0.2.tar.gz

---
+++ packages/ehostess/ehostess-0.0.2.tar.gz/ehostess-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ehostess',
+    version='0.0.2',
+    description='A toolkit for working with eHost, pyConText, and other annotation and NLP tools.',
+    url='https://github.com/MMontgomeryTaggart/eHostess',
+    author='Max Taggart',
+    author_email = "max.taggart@utah.edu",
+    license='MIT',
+    packages=['eHostess'],
+    download_url = 'https://github.com/MMontgomeryTaggart/eHostess/archive/0.0.2.tar.gz'
+)
