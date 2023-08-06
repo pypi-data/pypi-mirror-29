1966ce9f6d9efd4e3132c0a657a75cb3b98b1e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:40 +0000

    Add package openbanking/0.1/openbanking-0.1.tar.gz

---
+++ packages/openbanking/openbanking-0.1.tar.gz/openbanking-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'openbanking',
+  packages = ['openbanking'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Simple Open Banking wrapper for Humans.',
+  author = 'Glyn Jackson',
+  author_email = 'me@glynjackson.org',
+  url = 'https://bitbucket.org/glynjackson/openbanking',
+  download_url = '',
+  keywords = ['openbanking', 'open  banking', 'banking api'], # arbitrary keywords
+  classifiers = [],
+)
