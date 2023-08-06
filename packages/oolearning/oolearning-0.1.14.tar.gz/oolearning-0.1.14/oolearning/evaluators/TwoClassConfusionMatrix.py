c6ba196150f4d9a0c6c28ac06ed909163cdd41ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:12 +0000

    Add package oolearning/0.1.14/oolearning-0.1.14.tar.gz

---
+++ packages/oolearning/oolearning-0.1.14.tar.gz/oolearning-0.1.14/oolearning/evaluators/TwoClassConfusionMatrix.py
@@ -0,0 +1,151 @@
+from typing import Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from oolearning.evaluators.ConfusionMatrix import ConfusionMatrix
+
+
+class TwoClassConfusionMatrix(ConfusionMatrix):
+    def __init__(self,
+                 actual_classes: np.ndarray,
+                 predicted_classes: np.ndarray,
+                 positive_class: object):
+        unique_classes = list(set(actual_classes))  # get unique values then convert to list
+        assert len(unique_classes) == 2
+        negative_class = unique_classes[0] if positive_class == unique_classes[1] else unique_classes[1]
+
+        super().__init__(actual_classes=actual_classes,
+                         predicted_classes=predicted_classes,
+                         class_order=[negative_class, positive_class])
+
+        self._positive_class = positive_class
+        self._negative_class = negative_class
+        category_list = [self._negative_class, self._positive_class]
+
+        self._actual_positives = self.matrix.loc[self._positive_class][category_list].sum()
+        self._actual_negatives = self.matrix.loc[self._negative_class][category_list].sum()
+
+        self._true_positives = self.matrix.loc[self._positive_class, self._positive_class]
+        self._true_negatives = self.matrix.loc[self._negative_class, self._negative_class]
+        self._false_positives = self.matrix.loc[self._negative_class, self._positive_class]
+        self._false_negatives = self.matrix.loc[self._positive_class, self._negative_class]
+
+    @property
+    def sensitivity(self) -> float:
+        """
+        :return: a.k.a true positive rate
+        """
+        return 0 if self._actual_positives == 0 else self._true_positives / self._actual_positives
+
+    @property
+    def specificity(self) -> float:
+        """
+        :return: a.k.a false positive rate
+        """
+        return 0 if self._actual_negatives == 0 else self._true_negatives / self._actual_negatives
+
+    @property
+    def true_positive_rate(self) -> float:
+        return self.sensitivity
+
+    @property
+    def true_negative_rate(self) -> float:
+        return self.specificity
+
+    @property
+    def false_negative_rate(self) -> float:
+        return 0 if self._actual_positives == 0 else self._false_negatives / self._actual_positives
+
+    @property
+    def false_positive_rate(self) -> float:
+        return 0 if self._actual_negatives == 0 else self._false_positives / self._actual_negatives
+
+    @property
+    def accuracy(self) -> Union[float, None]:
+        return None if self.total_observations == 0 else \
+            (self._true_negatives + self._true_positives) / self.total_observations
+
+    @property
+    def error_rate(self) -> Union[float, None]:
+        return None if self.total_observations == 0 else \
+            (self._false_positives + self._false_negatives) / self.total_observations
+
+    @property
+    def positive_predictive_value(self) -> float:
+        return 0 if (self._true_positives + self._false_positives) == 0 else \
+            self._true_positives / (self._true_positives + self._false_positives)
+
+    @property
+    def negative_predictive_value(self) -> float:
+        return 0 if (self._true_negatives + self._false_negatives) == 0 else \
+            self._true_negatives / (self._true_negatives + self._false_negatives)
+
+    @property
+    def prevalence(self) -> Union[float, None]:
+        return None if self.total_observations == 0 else \
+            (self._true_positives + self._false_negatives) / self.total_observations
+
+    @property
+    def kappa(self) -> Union[float, None]:
+        if self.total_observations == 0 or \
+                ((self._true_negatives + self._false_negatives) / self.total_observations) == 0:
+            return None
+        # proportion of the actual agreements
+        # add the proportion of all instances where the predicted type and actual type agree
+        pr_a = (self._true_negatives + self._true_positives) / self.total_observations
+        # probability of both predicted and actual being negative
+        p_negative_prediction_and_actual = \
+            ((self._true_negatives + self._false_positives) / self.total_observations) * \
+            ((self._true_negatives + self._false_negatives) / self.total_observations)
+        # probability of both predicted and actual being positive
+        p_positive_prediction_and_actual = \
+            self.prevalence * ((self._false_positives + self._true_positives) / self.total_observations)
+        # probability that chance alone would lead the predicted and actual values to match, under the
+        # assumption that both are selected randomly (i.e. implies independence) according to the observed
+        # proportions (probability of independent events = P(A & B) == P(A) * P(B)
+        pr_e = p_negative_prediction_and_actual + p_positive_prediction_and_actual
+        return (pr_a - pr_e) / (1 - pr_e)
+
+    @property
+    def f1_score(self) -> float:
+        if self.positive_predictive_value is None or \
+                self.sensitivity is None or \
+                (self.positive_predictive_value + self.sensitivity) == 0:
+            return 0
+
+        return 2 * (self.positive_predictive_value * self.sensitivity) / \
+            (self.positive_predictive_value + self.sensitivity)
+
+    @property
+    def all_quality_metrics(self) -> dict:
+        return {'Kappa': self.kappa,
+                'F1 Score': self.f1_score,
+                'Two-Class Accuracy': self.accuracy,
+                'Error Rate': self.error_rate,
+                'True Positive Rate': self.sensitivity,
+                'True Negative Rate': self.specificity,
+                'False Positive Rate': self.false_positive_rate,
+                'False Negative Rate': self.false_negative_rate,
+                'Positive Predictive Value': self.positive_predictive_value,
+                'Negative Predictive Value': self.negative_predictive_value,
+                'Prevalence': self.prevalence,
+                'No Information Rate': max(self.prevalence, 1-self.prevalence),  # i.e. largest class %
+                'Total Observations': self.total_observations}
+
+    def plot_all_quality_metrics(self):
+        # noinspection PyTypeChecker
+        x = pd.DataFrame.from_dict([self.all_quality_metrics])
+        x = x[list(self.all_quality_metrics.keys())].drop(columns='Total Observations')
+
+        fig, ax = plt.subplots()
+        p = x.plot(kind='box',
+                   rot=20,
+                   title='Quality Scores',
+                   yticks=np.linspace(start=0, stop=1, num=21),
+                   grid=True,
+                   ax=ax)
+        plt.xticks(ha='right')
+
+        return p
---
+++ packages/oolearning/oolearning-0.1.14.tar.gz/oolearning-0.1.14/oolearning/evaluators/TwoClassEvaluator.py
@@ -0,0 +1,112 @@
+from typing import Union
+
+import numpy as np
+import pandas as pd
+
+from oolearning.evaluators.TwoClassConfusionMatrix import TwoClassConfusionMatrix
+from oolearning.evaluators.EvaluatorBase import EvaluatorBase
+
+
+class TwoClassEvaluator(EvaluatorBase):
+    """
+    Class representing a confusion confusion_matrix for two-class (or 2 category) classifiers.
+    """
+    def __init__(self, positive_class: object):
+        """
+             |                  | Predicted Negative | Predicted Positive |
+             | ---------------- | ------------------ | ------------------ |
+             | Actual Negative  | True Negative      | False Positive     |
+             | Actual Positive  | False Negative     | True Positive      |
+
+             if `positive_class` is None, then the confusion matrix is not arranged by category.
+
+             For multi-class problems, `positive_class` is not applicable.
+         """
+        self._positive_class = positive_class
+        self._confusion_matrix = None
+
+    def evaluate(self, actual_values: np.ndarray, predicted_values: np.ndarray):
+
+        self._confusion_matrix = TwoClassConfusionMatrix(actual_classes=actual_values,
+                                                         predicted_classes=predicted_values,
+                                                         positive_class=self._positive_class)
+
+    @classmethod
+    def from_classes(cls,
+                     actual_classes: np.ndarray,
+                     predicted_classes: np.ndarray,
+                     positive_class) -> 'TwoClassEvaluator':
+        evaluator = TwoClassEvaluator(positive_class=positive_class)
+        evaluator.evaluate(actual_values=actual_classes, predicted_values=predicted_classes)
+        return evaluator
+
+    @property
+    def matrix(self) -> pd.DataFrame:
+        return self._confusion_matrix.matrix
+
+    @property
+    def confusion_matrix(self) -> TwoClassConfusionMatrix:
+        return self._confusion_matrix
+
+    @property
+    def total_observations(self) -> int:
+        return self._confusion_matrix.total_observations
+
+    @property
+    def sensitivity(self) -> Union[float, None]:
+        return self._confusion_matrix.sensitivity
+
+    @property
+    def specificity(self) -> Union[float, None]:
+        return self._confusion_matrix.specificity
+
+    @property
+    def true_positive_rate(self) -> Union[float, None]:
+        return self._confusion_matrix.true_positive_rate
+
+    @property
+    def true_negative_rate(self) -> Union[float, None]:
+        return self._confusion_matrix.true_negative_rate
+
+    @property
+    def false_negative_rate(self) -> Union[float, None]:
+        return self._confusion_matrix.false_negative_rate
+
+    @property
+    def false_positive_rate(self) -> Union[float, None]:
+        return self._confusion_matrix.false_positive_rate
+
+    @property
+    def accuracy(self) -> Union[float, None]:
+        return self._confusion_matrix.accuracy
+
+    @property
+    def error_rate(self) -> Union[float, None]:
+        return self._confusion_matrix.error_rate
+
+    @property
+    def positive_predictive_value(self) -> Union[float, None]:
+        return self._confusion_matrix.positive_predictive_value
+
+    @property
+    def negative_predictive_value(self) -> Union[float, None]:
+        return self._confusion_matrix.negative_predictive_value
+
+    @property
+    def prevalence(self) -> Union[float, None]:
+        return self._confusion_matrix.prevalence
+
+    @property
+    def kappa(self) -> Union[float, None]:
+        return self._confusion_matrix.kappa
+
+    @property
+    def f1_score(self) -> Union[float, None]:
+        return self._confusion_matrix.f1_score
+
+    @property
+    def all_quality_metrics(self) -> dict:
+        return self._confusion_matrix.all_quality_metrics
+
+    def plot_all_quality_metrics(self):
+        return self._confusion_matrix.plot_all_quality_metrics()
