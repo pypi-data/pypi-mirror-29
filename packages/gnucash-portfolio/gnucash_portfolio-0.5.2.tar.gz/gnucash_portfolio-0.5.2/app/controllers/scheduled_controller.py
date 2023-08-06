3c63a01149983cea85388310852505046b35105c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:42 +0000

    Add package gnucash-portfolio/0.5.2/gnucash_portfolio-0.5.2.tar.gz

---
+++ packages/gnucash-portfolio/gnucash_portfolio-0.5.2.tar.gz/gnucash_portfolio-0.5.2/app/controllers/scheduled_controller.py
@@ -0,0 +1,128 @@
+""" Scheduled Transactions """
+
+#from logging import log, DEBUG
+from typing import List
+from flask import Blueprint, render_template, request
+try: import simplejson as json
+except ImportError: import json
+from piecash import ScheduledTransaction
+from gnucash_portfolio.bookaggregate import BookAggregate
+from gnucash_portfolio.lib import datetimeutils
+from app.models.transaction_models import ScheduledTxInputModel
+
+
+scheduled_controller = Blueprint( # pylint: disable=invalid-name
+    'scheduled_controller', __name__, url_prefix='/scheduled')
+
+@scheduled_controller.route('/', methods=['GET', 'POST'])
+def scheduled_transactions():
+    """ Lists scheduled transactions """
+    input_model = __parse_sch_tx_search_params()
+
+    if not input_model:
+        # Initial run
+        input_model = ScheduledTxInputModel()
+
+    with BookAggregate() as svc:
+        model = {
+            "data": __load_model_for_scheduled_transactions(input_model, svc)
+        }
+        output = render_template('scheduled.html', model=model, input_model=input_model)
+    return output
+
+@scheduled_controller.route('/calendar')
+def calendar():
+    """ Full Calendar """
+    # todo load data
+    return render_template('scheduled.calendar.html')
+
+@scheduled_controller.route('/duedate')
+def scheduled_with_due_date():
+    """ Returns scheduled transactions with their next occurrence date """
+    input_model = __parse_sch_tx_search_params()
+    if not input_model:
+        # Initial run
+        input_model = ScheduledTxInputModel()
+
+    with BookAggregate() as svc:
+        transactions = __load_model_for_scheduled_transactions(input_model, svc)
+        transactions = __load_due_dates(svc, transactions)
+        model = {
+            "data": transactions
+        }
+        output = render_template('scheduled.duedate.html', model=model, input_model=input_model)
+    return output
+
+##################
+# API
+
+@scheduled_controller.route('/api/top10')
+def api_top_10():
+    return api_transactions(10)
+
+@scheduled_controller.route('/api/transactions/<int:count>')
+def api_transactions(count: int):
+    """ Returns next n scheduled transactions in JSON """
+    assert isinstance(count, int)
+
+    with BookAggregate() as svc:
+        upcoming = svc.scheduled.get_upcoming(count)
+        result = __get_api_model_from_sx(upcoming)
+        json_result = json.dumps(result)
+    return json_result
+
+##################
+# Partials
+
+@scheduled_controller.route('/partial/top10')
+def topten_partial():
+    """ Partial for scheduled transactions. Displays ten upcoming transactions
+    for the dashboard. """
+    with BookAggregate() as svc:
+        upcoming = svc.scheduled.get_upcoming(10)
+        model = {
+            "list": [tx for tx in upcoming]
+        }
+        return render_template('_scheduled.top10.html', model=model)
+
+#################
+# Private
+
+def __get_api_model_from_sx(transactions: List[ScheduledTransaction]):
+    result = []
+    for tx in transactions:
+        result.append({
+            "title": tx.name,
+            "start": datetimeutils.get_iso_string(tx["next_date"].value),
+            "allDay": True
+        })
+    return result
+
+def __parse_sch_tx_search_params() -> ScheduledTxInputModel:
+    """ Parses the search parameters from the request """
+    if not request.form:
+        return None
+
+    model = ScheduledTxInputModel()
+    model.period_str = request.form.get("period")
+
+    return model
+
+def __load_model_for_scheduled_transactions(
+        search: ScheduledTxInputModel, svc: BookAggregate) -> List[ScheduledTransaction]:
+    """ loads data for scheduled transactions """
+    if not search:
+        return None
+
+    query = svc.scheduled.get_enabled()
+    return query
+
+def __load_due_dates(
+        svc: BookAggregate, transactions: List[ScheduledTransaction]
+    ) -> List[ScheduledTransaction]:
+    """ Populates due dates on scheduled transactions """
+    for sx in transactions:
+        agg = svc.scheduled.get_aggregate_for(sx)
+        sx["due_date"] = agg.get_next_occurrence()
+
+    return transactions
---
+++ packages/gnucash-portfolio/gnucash_portfolio-0.5.2.tar.gz/gnucash_portfolio-0.5.2/gnucash_portfolio/lib/datetimeutils.py
@@ -0,0 +1,146 @@
+""" Date/time utilities """
+from logging import log, DEBUG
+import calendar
+from datetime import datetime, date, time, timedelta
+#import dateutil
+from dateutil.relativedelta import relativedelta
+
+ISO_SHORT_FORMAT = "%Y-%m-%d"
+ISO_LONG_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+def add_days(value: datetime, days: int) -> datetime:
+    """ Adds days """
+    return value + relativedelta(days=days)
+
+def add_months(date_value: date, value: int) -> date:
+    """ Add a number of months to the given date """
+    return date_value + relativedelta(months=value)
+
+def get_datetime_from_date(value: date) -> datetime:
+    """ Gets a new datetime value from date """
+    return datetime(value.year, value.month, value.day)
+
+def get_day_name(value: datetime) -> str:
+    """ Returns the day name """
+    weekday = value.isoweekday() - 1
+    # log(DEBUG, weekday)
+    return calendar.day_name[weekday]
+
+def get_days_in_month(year: int, month: int) -> int:
+    """ Returns number of days in the given month.
+    1-based numbers as arguments. i.e. November = 11 """
+    month_range = calendar.monthrange(year, month)
+    return month_range[1]
+
+def get_end_of_month(date_val: date) -> date:
+    """ Provides end of the month for the given date """
+    # Increase month by 1,
+    result = date_val + relativedelta(months=1)
+    # take the 1st day of the (next) month,
+    result = result.replace(day=1)
+    # subtract one day
+    result = result - relativedelta(days=1)
+    return result
+
+def get_from_gnucash26_date(date_str: str) -> date:
+    """ Creates a datetime from GnuCash 2.6 date string """
+    date_format = "%Y%m%d"
+    result = datetime.strptime(date_str, date_format).date()
+    return result
+
+def get_iso_string(value: date) -> str:
+    """ Returns full ISO string for the given date """
+    my_datetime = get_datetime_from_date(value)
+    return datetime.isoformat(my_datetime)
+
+def today_date() -> date:
+    """ Returns today as a date """
+    return datetime.today().date()
+
+def today() -> datetime:
+    """ Returns today (date only) as datetime """
+    date_today = today_date()
+    today_datetime = datetime.combine(date_today, time.min)
+    return today_datetime
+
+def get_period_start(period: str) -> datetime:
+    """ Parse period string and return the from date """
+    period_obj = parse_period(period)
+    return period_obj[0]
+
+def get_period_end(period: str) -> datetime:
+    """ Parse period string and return the 'to' date """
+    obj = parse_period(period)
+    return obj[1]
+
+def parse_iso_date(date_str: str) -> datetime:
+    """ Parse ISO date string (YYYY-MM-DD) """
+    return datetime.strptime(date_str, ISO_SHORT_FORMAT)
+
+def parse_iso_long_date(date_str: str) -> datetime:
+    """ Parse ISO date string (YYYY-MM-DDTHH:mm:ss) """
+    return datetime.strptime(date_str, ISO_LONG_FORMAT)
+
+def parse_period(period: str):
+    """ parses period from date range picker """
+    period = period.split(" - ")
+    date_from = datetime.strptime(period[0], "%Y-%m-%d").replace(hour=0, minute=0, second=0)
+    date_to = datetime.strptime(period[1], "%Y-%m-%d").replace(hour=23, minute=59, second=59)
+
+    return (date_from, date_to)
+
+def parse_us_date(date_us: str) -> datetime:
+    """ Parses US date mm/dd/yyyy """
+    return datetime.strptime(date_us, "%m/%d/%Y")
+
+def start_of_day(datum: datetime) -> datetime:
+    """ Returns start of day """
+    return datetime(datum.year, datum.month, datum.day)
+
+def end_of_day(datum: datetime) -> datetime:
+    """ End of day """
+    return datetime(datum.year, datum.month, datum.day, 23, 59, 59)
+
+def get_period(date_from: date, date_to: date) -> str:
+    """ Returns the period string from the given dates """
+    assert isinstance(date_from, date)
+    assert isinstance(date_to, date)
+
+    str_from: str = date_from.isoformat()
+    str_to: str = date_to.isoformat()
+
+    return str_from + " - " + str_to
+
+def get_period_last_week() -> str:
+    """ Returns the last week as a period string """
+    today = today_date()
+    start_date = today - timedelta(days=7)
+    period = get_period(start_date, today)
+    return period
+
+def get_period_last_30_days() -> str:
+    """ Returns the last week as a period string """
+    today = today_date()
+    start_date = today - timedelta(days=30)
+    period = get_period(start_date, today)
+    return period
+
+def get_period_last_3_months() -> str:
+    """ Returns the last week as a period string """
+    today = today_date()
+    start_date = today - timedelta(weeks=13)
+    period = get_period(start_date, today)
+    return period
+
+def is_end_of_month(value: date) -> bool:
+    """ Checks if the date is at the end of the month """
+    end_of_month = get_end_of_month(value)
+    return value == end_of_month
+
+def subtract_days(value: date, days: int) -> date:
+    """ Subtracts dates from the given value """
+    return value - relativedelta(days=days)
+
+def subtract_months(value: date, months: int) -> date:
+    """ Subtracts number of months from a date """
+    return value - relativedelta(months=months)
