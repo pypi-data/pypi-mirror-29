712d85ab48da4958cbbecc91e06cfca23de4773a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:02 +0000

    Add package blake256/0.1.1/blake256-0.1.1.tar.gz

---
+++ packages/blake256/blake256-0.1.1.tar.gz/blake256-0.1.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="blake256",
+    version="0.1.1",
+    description="A Python module supporting blake256 hashing",
+    license="MIT",
+    author="cipherZ",
+    packages=["blake256"],
+    url="https://github.com/cipherzzz/blake256_hash",
+    download_url='https://github.com/cipherzzz/blake256_hash/tarball/0.1.1',
+    classifiers=[]
+)
