43c88a7e80748c316ce7f4fe44fe4be1539e70e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:48 +0000

    Add package shortbread/0.3.2/shortbread-0.3.2.tar.gz

---
+++ packages/shortbread/shortbread-0.3.2.tar.gz/shortbread-0.3.2/shortbread/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Shortbread."""
+from shortbread import shortbread
+
+__author__ = """Diane Chen"""
+__email__ = 'dianedc@purpleweb.com'
+__version__ = '0.3.2'
