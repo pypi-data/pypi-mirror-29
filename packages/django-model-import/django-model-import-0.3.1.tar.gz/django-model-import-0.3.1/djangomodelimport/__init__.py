ebc150b3797ffc325250b0a18cbc0b19c7309f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:00 +0000

    Add package django-model-import/0.3.1/django-model-import-0.3.1.tar.gz

---
+++ packages/django-model-import/django-model-import-0.3.1.tar.gz/django-model-import-0.3.1/djangomodelimport/__init__.py
@@ -0,0 +1,9 @@
+from .core import ModelImporter  # noqa
+from .forms import ImporterModelForm  # noqa
+from .fields import CachedChoiceField, PreloadedChoiceField, DateTimeParserField, FlatRelatedField, JSONField  # noqa
+from .widgets import CompositeLookupWidget  # noqa
+from .parsers import ImportParser, TablibCSVImportParser, TablibXLSXImportParser  # noqa
+
+VERSION = (0, 3, 1)
+
+__version__ = '.'.join(str(x) for x in VERSION[:(2 if VERSION[2] == 0 else 3)])  # noqa
