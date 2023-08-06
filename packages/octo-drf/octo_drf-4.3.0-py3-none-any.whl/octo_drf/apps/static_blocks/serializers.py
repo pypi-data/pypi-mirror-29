5c3150783f79e4acb41cde16fa0ea3b06017827f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:00 +0000

    Add package octo-drf/4.3.0/octo_drf-4.3.0-py3-none-any.whl

---
+++ packages/octo-drf/octo_drf-4.3.0-py3-none-any.whl/octo_drf/apps/static_blocks/serializers.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from octo_drf.apps.base.serializers import BaseModelSerializer
+from rest_framework import serializers
+from .models import StaticBlock
+
+
+class StaticBlockSerializer(BaseModelSerializer):
+
+    image_fields = ['image1', 'image2', 'image3', 'image4']
+
+    class Meta:
+        model = StaticBlock
+        fields = ('pk', 'order', 'title', 'announce',
+                  'content', 'is_published',
+                  )
+
+class StaticBlockCountSerializerMixin(serializers.ModelSerializer):
+
+    count_static_blocks = serializers.SerializerMethodField()
+
+    def get_count_static_blocks(self, instance):
+        return instance.get_count_static_blocks()
+
