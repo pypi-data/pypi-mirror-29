cf2b0c412bd5b1e9a4ac85f5d885ed0fad1e8043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:01 +0000

    Add package aiorpcX/0.4.5/aiorpcX-0.4.5-py3-none-any.whl

---
+++ packages/aiorpcX/aiorpcX-0.4.5-py3-none-any.whl/aiorpcx/__init__.py
@@ -0,0 +1,14 @@
+from .framing import *
+from .jsonrpc import *
+from .rpc import *
+from .socks import *
+from .util import *
+
+_version = (0, 4, 5)
+_version_str = '.'.join(str(part) for part in _version)
+
+__all__ = (framing.__all__ +
+           jsonrpc.__all__ +
+           rpc.__all__ +
+           socks.__all__ +
+           util.__all__)
