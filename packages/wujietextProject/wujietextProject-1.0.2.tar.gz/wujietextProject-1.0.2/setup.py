730a2ab58dc715219a6ecd98cdf5781eed69251d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:19 +0000

    Add package wujietextProject/1.0.2/wujietextProject-1.0.2.tar.gz

---
+++ packages/wujietextProject/wujietextProject-1.0.2.tar.gz/wujietextProject-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name ='wujietextProject',
+        version='1.0.2',
+        py_modules=['wujietextProject'],
+        author='holybird',
+        author_email='wujie19891224a@gmail.com',
+        url='',
+        description='修改了方法名，优化了打印效果',
+    )
