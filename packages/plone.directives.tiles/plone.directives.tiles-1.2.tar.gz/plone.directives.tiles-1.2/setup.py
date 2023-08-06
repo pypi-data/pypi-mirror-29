ec6927406d5b2af7fdfc46d106dcab87dab010a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:59 +0000

    Add package plone.directives.tiles/1.2/plone.directives.tiles-1.2.tar.gz

---
+++ packages/plone.directives.tiles/plone.directives.tiles-1.2.tar.gz/plone.directives.tiles-1.2/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+version = '1.2'
+tests_require = [
+    'zope.configuration',
+    'zope.publisher',
+    'zope.traversing',
+    'zope.interface',
+    'zope.schema',
+]
+
+setup(
+    name='plone.directives.tiles',
+    version=version,
+    description="Grokkers for plone.tiles",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='plone tiles deco blocks grok directives',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='https://github.com/plone/plone.directives.tiles',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['plone', 'plone.directives'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'martian',
+        'grokcore.component',
+        'grokcore.security',
+        'zope.component',
+        'plone.tiles>=1.1',
+        'five.grok',
+        'zope.deferredimport',
+    ],
+    tests_require=tests_require,
+    extras_require=dict(test=tests_require),
+)
