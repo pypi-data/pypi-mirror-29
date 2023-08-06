06d1d9831ead470ff028334fadc2f6a802705b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:58 +0000

    Add package Rest-Utils/0.4.1/rest-utils-0.4.1.tar.gz

---
+++ packages/Rest-Utils/rest-utils-0.4.1.tar.gz/rest-utils-0.4.1/rest_utils/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Author  :   windpro
+E-mail  :   windprog@gmail.com
+Date    :   17/11/15
+Desc    :   
+"""
+from .sa_util import migrate_skip
+from .manager import APIManager
