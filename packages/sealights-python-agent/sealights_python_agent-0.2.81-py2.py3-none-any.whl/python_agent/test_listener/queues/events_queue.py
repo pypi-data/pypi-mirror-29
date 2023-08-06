0c4ef26eb1a526bc44529e8759ea0ad25b0e7ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:32 +0000

    Add package sealights-python-agent/0.2.81/sealights_python_agent-0.2.81-py2.py3-none-any.whl

---
+++ packages/sealights-python-agent/sealights_python_agent-0.2.81-py2.py3-none-any.whl/python_agent/test_listener/queues/events_queue.py
@@ -0,0 +1,31 @@
+import logging
+import sys
+
+from python_agent.packages.blinker import signal
+
+if sys.version_info >= (3, 0):
+    import queue as Queue
+else:
+    from Queue import Queue
+
+
+log = logging.getLogger(__name__)
+
+
+class EventsQueue(Queue, object):
+    def __init__(self, maxsize=0):
+        super(EventsQueue, self).__init__(maxsize=maxsize)
+
+    def put(self, item, block=True, timeout=None):
+        super(EventsQueue, self).put(item, block=block, timeout=timeout)
+        if self.full():
+            events_queue_full = signal('events_queue_full')
+            log.info("Events Queue is Full. Signaling...")
+            events_queue_full.send()
+
+    def get_all(self):
+        events = []
+        while not self.empty():
+            event_item = self.get()
+            events.append(event_item)
+        return events
