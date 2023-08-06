e23e56d9c8a7a1e6afe4b891f10b9738c39adc35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:16 +0000

    Add package flask-journey/0.1.4/flask-journey-0.1.4.tar.gz

---
+++ packages/flask-journey/flask-journey-0.1.4.tar.gz/flask-journey-0.1.4/flask_journey/exceptions.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+class InvalidPath(Exception):
+    pass
+
+
+class InvalidBundlesType(Exception):
+    pass
+
+
+class ConflictingPath(Exception):
+    pass
+
+
+class IncompatibleSchema(Exception):
+    pass
+
+
+class IncompatibleBundle(Exception):
+    pass
+
+
+class InvalidBlueprint(Exception):
+    pass
+
+
+class NoBundlesAttached(Exception):
+    pass
+
+
+class MissingBlueprints(Exception):
+    pass
+
+
