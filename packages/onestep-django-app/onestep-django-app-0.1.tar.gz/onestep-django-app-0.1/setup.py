fc730d846fc34221b2400c46af098923ab3da8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:51 +0000

    Add package onestep-django-app/0.1/onestep-django-app-0.1.tar.gz

---
+++ packages/onestep-django-app/onestep-django-app-0.1.tar.gz/onestep-django-app-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'onestep-django-app',
+  packages = ['onestep-django-app'], # this must be the same as the name above
+  version = '0.1',
+  description = 'installing a django app in single step',
+  author = 'Vinay Krishna',
+  author_email = 'vinay221097@gmail.com',
+  url = 'https://github.com/vinay221097/onestep-django-app', # use the URL to the github repo
+  download_url = 'https://github.com/vinay221097/onestep-django-app/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+)
