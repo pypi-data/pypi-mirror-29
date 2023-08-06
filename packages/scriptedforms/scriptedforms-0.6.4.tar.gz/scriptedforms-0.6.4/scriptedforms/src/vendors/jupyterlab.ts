4193da9255b79eebd1d6e8cd80746b6953636424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:37 +0000

    Add package scriptedforms/0.6.4/scriptedforms-0.6.4.tar.gz

---
+++ packages/scriptedforms/scriptedforms-0.6.4.tar.gz/scriptedforms-0.6.4/scriptedforms/src/vendors/jupyterlab.ts
@@ -0,0 +1,12 @@
+import "@jupyterlab/codemirror"
+import "@jupyterlab/docregistry"
+import "@jupyterlab/outputarea"
+import "@jupyterlab/rendermime"
+import "@jupyterlab/services"
+import "@jupyterlab/theme-light-extension"
+import "@phosphor/signaling"
+import "@phosphor/widgets"
+
+import '@jupyterlab/theme-light-extension/style/variables.css'
+import '@jupyterlab/codemirror/style/index.css';
+import '@jupyterlab/rendermime/style/index.css';
