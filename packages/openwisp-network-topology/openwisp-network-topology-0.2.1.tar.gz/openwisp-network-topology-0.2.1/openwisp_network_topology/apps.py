ea2d64a4e610693c4fc240fbed23d02825a37220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:34 +0000

    Add package openwisp-network-topology/0.2.1/openwisp-network-topology-0.2.1.tar.gz

---
+++ packages/openwisp-network-topology/openwisp-network-topology-0.2.1.tar.gz/openwisp-network-topology-0.2.1/openwisp_network_topology/apps.py
@@ -0,0 +1,6 @@
+from django_netjsongraph.apps import DjangoNetjsongraphConfig
+
+
+class OpenwispNetworkTopologyConfig(DjangoNetjsongraphConfig):
+    name = 'openwisp_network_topology'
+    label = 'topology'
