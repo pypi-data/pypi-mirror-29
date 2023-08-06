eb3b6eeaa082372b6eac9263b58e963ca160e010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:31 +0000

    Add package django-markdown-gcir/0.1.0/django-markdown-gcir-0.1.0.tar.gz

---
+++ packages/django-markdown-gcir/django-markdown-gcir-0.1.0.tar.gz/django-markdown-gcir-0.1.0/django_markdown/__init__.py
@@ -0,0 +1,6 @@
+""" Django-Markdown supports markdown in Django. """
+
+__version__ = "0.1.0"
+__project__ = "django-markdown-gcir"
+__author__ = "Geoffrey ROYER <geoffrey.royer@gmail.com>"
+__license__ = "GNU LGPL"
