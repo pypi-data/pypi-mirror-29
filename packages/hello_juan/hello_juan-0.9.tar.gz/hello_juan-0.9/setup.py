4f241a25b92a75450d8bedd722371d62be1d39d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:05 +0000

    Add package hello_juan/0.9/hello_juan-0.9.tar.gz

---
+++ packages/hello_juan/hello_juan-0.9.tar.gz/hello_juan-0.9/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'hello_juan',
+  packages = ['hello_juan'], # this must be the same as the name above
+  version = '0.9',
+  description = 'Un saludo a Juan',
+  author = 'Juan Manuel Mosca',
+  author_email = 'juanmosca@gmail.com',
+  licence = 'MIT',
+  url = 'https://pypi.python.org/pypi/hello_juan', 
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+  entry_points={
+      'console_scripts' : [
+          'hello_juan = hello_juan.say_hello:say_it' 
+          ]
+      }
+)
