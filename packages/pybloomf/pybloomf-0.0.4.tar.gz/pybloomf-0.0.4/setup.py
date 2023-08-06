6e64ed59d1ca4e58c9b8168a6f4c84d7ff42d3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:04 +0000

    Add package pybloomf/0.0.4/pybloomf-0.0.4.tar.gz

---
+++ packages/pybloomf/pybloomf-0.0.4.tar.gz/pybloomf-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pybloomf',
+    version='0.0.4',
+    description='Python implementation of Bloom filter algorithm',
+    url='https://github.com/tomasrasymas/pybloomf',
+    author='Tomas Rasymas',
+    author_email='tomas.rasymas@gmail.com',
+    license='MIT',
+    data_files=[('', ['LICENSE'])],
+    install_requires=[
+        'bitarray>=0.8.1',
+        'mmh3>=2.5.1',
+    ],
+    test_suite='tests',
+    packages=['pybloomf']
+)
