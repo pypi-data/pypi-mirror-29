8ea2d870d04a1e47d5b183787f8c38f62eac91ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:48 +0000

    Add package pyconfigreader/0.2.4/pyconfigreader-0.2.4.tar.gz

---
+++ packages/pyconfigreader/pyconfigreader-0.2.4.tar.gz/pyconfigreader-0.2.4/setup.py
@@ -0,0 +1,20 @@
+import pypandoc
+from setuptools import setup
+
+try:
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except ImportError:
+    long_description = 'Usage available at http://github.com/giantas/pyconfigreade'
+
+setup(name='pyconfigreader',
+      version='0.2.4',
+      description='A simple module for handling configurations and config files',
+      long_description=long_description,
+      url='http://github.com/giantas/pyconfigreader',
+      author='Aswa Paul',
+      license='MIT',
+      packages=['pyconfigreader'],
+      install_requires=[
+          'testfixtures',
+      ],
+      zip_safe=False)
