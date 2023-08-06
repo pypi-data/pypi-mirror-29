29c48c257a4293e995103cce890541c1fecdd61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:23 +0000

    Add package vcfstoreclient/0.3/vcfstoreclient-0.3.tar.gz

---
+++ packages/vcfstoreclient/vcfstoreclient-0.3.tar.gz/vcfstoreclient-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='vcfstoreclient',
+      version='0.3',
+      description='Client for AWS vcfstore',
+      url='https://genomeinfo.github.io/',
+      author='Conrad Leonard',
+      author_email='conrad.leonard@qimrberghofer.edu.au',
+      license='MIT',
+      install_requires=[
+          'aws_requests_auth',
+          'boto3',
+          'filecache',
+          'python_jsonschema_objects',
+          'requests'
+          ],
+      scripts=['bin/vcfstoreclient'],
+      packages=['vcfstoreclient'],
+      zip_safe=False)
