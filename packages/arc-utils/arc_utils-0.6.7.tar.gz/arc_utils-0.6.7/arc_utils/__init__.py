b4dcf28e7142137a8ae95ea99b54a87eb490fd54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:40 +0000

    Add package arc-utils/0.6.7/arc_utils-0.6.7.tar.gz

---
+++ packages/arc-utils/arc_utils-0.6.7.tar.gz/arc_utils-0.6.7/arc_utils/__init__.py
@@ -0,0 +1,14 @@
+"""A collection of python utilities for ArcGIS Desktop and ArcGIS Pro
+"""
+__version__ = '0.6.6'
+__author__ = 'Grant Herbert'
+
+__all__ = ['gdb.py', 'table.py']
+
+import sys
+from arc_utils import gdb
+from arc_utils import table
+if sys.version_info.major == 3:
+    from arc_utils import aprx
+else:
+    from arc_utils import mxd
---
+++ packages/arc-utils/arc_utils-0.6.7.tar.gz/arc_utils-0.6.7/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+# Utility function to read the README file.
+# Used for the long_description.
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+setup(
+    name='arc_utils',
+    version='0.6.7',
+    description="Python utilities for use with Esri ArcGIS Desktop software",
+    long_description=read('README.md'),
+    url='https://github.com/gdherbert/arc_utils',
+    author='Grant Herbert',
+    author_email='gdherbert@gmail.com',
+    keywords='esri arcpy arcgis',
+    download_url = 'https://github.com/gdherbert/arc_utils/tree/0.6.7',
+    classifiers=[
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Programming Language :: Python',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Win32 (MS Windows)',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        #'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.2',
+        #'Programming Language :: Python :: 3.3',
+        #'Programming Language :: Python :: 3.4',
+    ],
+    packages=find_packages()
+    )
