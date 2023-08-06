b4bfe5ff66b3ff1c4dfc175c5e07639cdcca10b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:49 +0000

    Add package python-mlboardclient/0.0.1.dev23/python_mlboardclient-0.0.1.dev23-py2.py3-none-any.whl

---
+++ packages/python-mlboardclient/python_mlboardclient-0.0.1.dev23-py2.py3-none-any.whl/mlboardclient/utils.py
@@ -0,0 +1,115 @@
+import errno
+import functools
+import json
+import os
+import signal
+import subprocess
+import yaml
+
+from six.moves.urllib import parse
+from six.moves.urllib import request
+
+from mlboardclient import exceptions
+
+
+def do_action_on_many(action, resources, success_msg, error_msg):
+    """Helper to run an action on many resources."""
+    failure_flag = False
+
+    for resource in resources:
+        try:
+            action(resource)
+            print(success_msg % resource)
+        except Exception as e:
+            failure_flag = True
+            print(e)
+
+    if failure_flag:
+        raise exceptions.MlboardClientException(error_msg)
+
+
+def load_content(content):
+    if content is None or content == '':
+        return dict()
+
+    try:
+        data = yaml.safe_load(content)
+    except Exception:
+        data = json.loads(content)
+
+    return data
+
+
+def load_file(path):
+    with open(path, 'r') as f:
+        return load_content(f.read())
+
+
+def get_contents_if_file(contents_or_file_name):
+    """Get the contents of a file.
+
+    If the value passed in is a file name or file URI, return the
+    contents. If not, or there is an error reading the file contents,
+    return the value passed in as the contents.
+
+    For example, a workflow definition will be returned if either the
+    workflow definition file name, or file URI are passed in, or the
+    actual workflow definition itself is passed in.
+    """
+    try:
+        if parse.urlparse(contents_or_file_name).scheme:
+            definition_url = contents_or_file_name
+        else:
+            path = os.path.abspath(contents_or_file_name)
+            definition_url = parse.urljoin(
+                'file:',
+                request.pathname2url(path)
+            )
+        return request.urlopen(definition_url).read().decode('utf8')
+    except Exception:
+        return contents_or_file_name
+
+
+def load_json(input_string):
+    try:
+        with open(input_string) as fh:
+            return json.load(fh)
+    except IOError:
+        return json.loads(input_string)
+
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise exceptions.TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            if seconds > 0:
+                signal.signal(signal.SIGALRM, _handle_timeout)
+                signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                if seconds > 0:
+                    signal.alarm(0)
+            return result
+
+        return functools.wraps(func)(wrapper)
+
+    return decorator
+
+
+def stream_targz(path):
+    if not os.path.exists(path):
+        raise RuntimeError('%s: No such file or directory' % path)
+
+    if os.path.isdir(path):
+        dirname = os.path.dirname(path)
+        basename = os.path.basename(path)
+
+        cmd = ['tar', 'czf', '-', basename]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, cwd=dirname)
+
+        return p.stdout
+    else:
+        return open(path, 'rb')
