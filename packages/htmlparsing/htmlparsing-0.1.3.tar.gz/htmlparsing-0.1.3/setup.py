589ae79b28a7fa299d26d452ea4d93da0c7f1c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:41 +0000

    Add package htmlparsing/0.1.3/htmlparsing-0.1.3.tar.gz

---
+++ packages/htmlparsing/htmlparsing-0.1.3.tar.gz/htmlparsing-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="htmlparsing",
+    version="0.1.3",
+    description="Pure HTML parsing library.",
+    author="Jiuli Gao",
+    author_email="gaojiuli@gmail.com",
+    url='https://github.com/gaojiuli/htmlparsing',
+    py_modules=['htmlparsing'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'parse',
+        'lxml',
+        'html2text'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
