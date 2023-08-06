47c0eda8522e2c655312e8fec4c3fc541098278f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:59 +0000

    Add package ahocorapy/1.3.1/ahocorapy-1.3.1.tar.gz

---
+++ packages/ahocorapy/ahocorapy-1.3.1.tar.gz/ahocorapy-1.3.1/src/ahocorapy_visualizer/visualizer.py
@@ -0,0 +1,33 @@
+from builtins import str
+from builtins import object
+import pygraphviz as pgv
+
+
+class Visualizer(object):
+
+    def __init__(self):
+        self._added = set()
+
+
+    def _add_state_and_children(self, graph, state, added_state_ids):
+        if state.identifier in added_state_ids:
+            return
+        if state.success:
+            graph.add_node(
+                    state.identifier,
+                    color='green',
+                    label=str(state.identifier) + ' [' +
+                    state.matched_keyword + ']')
+        else:
+            graph.add_node(state.identifier)
+        added_state_ids.add(state.identifier)
+        for symbol, child in state.transitions.items():
+            self._add_state_and_children(graph, child, added_state_ids)
+            graph.add_edge(state.identifier, child.identifier, label=symbol)
+    
+    
+    def draw(self, filename, kwtree):
+        graph = pgv.AGraph(directed=True)
+        added_state_ids = set()
+        self._add_state_and_children(graph, kwtree._zero_state, added_state_ids)
+        graph.draw(filename, prog='dot')
