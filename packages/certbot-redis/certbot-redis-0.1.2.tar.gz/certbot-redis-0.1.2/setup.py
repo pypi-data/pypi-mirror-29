433ca08d7f8da9d153916d27a704cd0852aa09f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:59 +0000

    Add package certbot-redis/0.1.2/certbot-redis-0.1.2.tar.gz

---
+++ packages/certbot-redis/certbot-redis-0.1.2.tar.gz/certbot-redis-0.1.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
+setup(
+    name='certbot-redis',  # Required
+    version='0.1.2',  # Required
+    description='Certbot plugin for Redis',
+    url='https://github.com/deathowl/certbot-redis-plugin',  # Optional
+
+    author='Balint Csergo',  # Optional
+
+    author_email='<bcsergo@emarsys.com>',  # Optional
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='certbot redis',  # Optional
+
+    packages=find_packages(),  # Required
+
+    install_requires=install_requires,
+    include_package_data=True,
+    entry_points={
+        'letsencrypt.plugins': [
+            'redis = certbot_redis.plugin:Authenticator',
+        ],
+    },
+
+
+
+)
