2b74ca2744b252b0f6e01041d1a0c942a84c02d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:22 +0000

    Add package django-saml-idp/0.24.1/django-saml-idp-0.24.1.tar.gz

---
+++ packages/django-saml-idp/django-saml-idp-0.24.1.tar.gz/django-saml-idp-0.24.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import saml2idp
+
+
+with open('README.rst') as readme:
+    description = readme.read()
+
+
+with open('HISTORY.rst') as history:
+    changelog = history.read()
+
+
+setup(
+    name='django-saml-idp',
+    version=saml2idp.__version__,
+    author='Gabriel de Forest, Sebastian Vetter',
+    author_email='sebastian@mobify.com',
+    description='SAML 2.0 IdP for Django',
+    long_description='\n\n'.join([description, changelog]),
+    install_requires=[
+        'Django>=1.8',
+        'M2Crypto==0.26.0',
+        'BeautifulSoup>=3.2.0',
+        'structlog'],
+    license='MIT',
+    packages=['saml2idp'],
+    url='http://github.com/deforestg/dj-saml-idp',
+    zip_safe=False,
+    include_package_data=True,
+)
