9f30c7af943fcac49e147ce4a1454f3485119ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:21 +0000

    Add package asyncnsq/0.3.7/asyncnsq-0.3.7.tar.gz

---
+++ packages/asyncnsq/asyncnsq-0.3.7.tar.gz/asyncnsq-0.3.7/setup.py
@@ -0,0 +1,71 @@
+import re
+import os.path
+import sys
+from setuptools import setup, find_packages
+
+
+install_requires = []
+
+PY_VER = sys.version_info
+
+if PY_VER >= (3, 4):
+    pass
+elif PY_VER >= (3, 3):
+    install_requires.append('asyncio')
+else:
+    raise RuntimeError("asyncnsq doesn't support Python version prior 3.3")
+
+
+def read(*parts):
+    with open(os.path.join(*parts), 'rt') as f:
+        return f.read().strip()
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__),
+                           'asyncnsq', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            raise RuntimeError('Cannot find version in asyncnsq/__init__.py')
+
+
+classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Operating System :: POSIX',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+]
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(name='asyncnsq',
+      version=read_version(),
+      description=("asyncio async/await nsq support"),
+      long_description=long_description,
+      classifiers=classifiers,
+      platforms=["POSIX"],
+      author="aohan237",
+      author_email="aohan237@gmail.com",
+      url="https://github.com/aohan237/asyncnsq",
+      license="MIT",
+      packages=find_packages(exclude=["tests"]),
+      install_requires=install_requires,
+      include_package_data=True,
+      )
