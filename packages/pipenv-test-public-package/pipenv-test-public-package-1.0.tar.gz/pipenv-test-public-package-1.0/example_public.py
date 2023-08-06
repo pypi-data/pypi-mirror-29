49f5e4407e1a8395d348b899b3b50275927becec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:50 +0000

    Add package pipenv-test-public-package/1.0/pipenv-test-public-package-1.0.tar.gz

---
+++ packages/pipenv-test-public-package/pipenv-test-public-package-1.0.tar.gz/pipenv-test-public-package-1.0/example_public.py
@@ -0,0 +1,2 @@
+import sys
+sys.stdout.write('This is a public package\n')
---
+++ packages/pipenv-test-public-package/pipenv-test-public-package-1.0.tar.gz/pipenv-test-public-package-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='pipenv-test-public-package',
+    version='1.0',
+    url='https://gist.github.com/ryanwilsonperkin/d8d591e08e0996414eea27b25f4bb68c',
+    license='MIT',
+    py_modules=['example_public'],
+)
