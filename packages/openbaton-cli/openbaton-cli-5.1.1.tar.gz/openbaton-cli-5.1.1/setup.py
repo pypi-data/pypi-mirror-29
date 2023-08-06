581d896d8db3bbca6e41b1863a6be8b4991d0a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:35 +0000

    Add package openbaton-cli/5.1.1/openbaton-cli-5.1.1.tar.gz

---
+++ packages/openbaton-cli/openbaton-cli-5.1.1.tar.gz/openbaton-cli-5.1.1/setup.py
@@ -0,0 +1,40 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="openbaton-cli",
+    version="5.1.1",
+    author="Open Baton",
+    author_email="dev@openbaton.org",
+    description="The Open Baton CLI",
+    license="Apache 2",
+    keywords="python vnfm nfvo open baton openbaton sdk cli rest",
+    url="http://openbaton.github.io/",
+    packages=find_packages(),
+    scripts=["openbaton"],
+    install_requires=[
+        'requests',
+        'tabulate',
+        'argcomplete',
+        'configparser',
+    ],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        'Topic :: Software Development :: Build Tools',
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    entry_points={
+        'console_scripts': [
+            'openbaton = org.openbaton.cli.openbaton:start',
+        ]
+    }
+)
