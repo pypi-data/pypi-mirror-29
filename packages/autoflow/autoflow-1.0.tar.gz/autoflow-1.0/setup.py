9944507224476faeae2f8727d09c261c47e77b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:49 +0000

    Add package autoflow/1.0/autoflow-1.0.tar.gz

---
+++ packages/autoflow/autoflow-1.0.tar.gz/autoflow-1.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+  name='autoflow',
+  version='1.0',
+  author='Nikhil Kothari',
+  description='Python DSL in the works',
+  license='Apache 2.0',
+  keywords='',
+  url='https://github.com/nikhilk',
+  packages=[],
+  install_requires=[],
+  classifiers=[])
+
