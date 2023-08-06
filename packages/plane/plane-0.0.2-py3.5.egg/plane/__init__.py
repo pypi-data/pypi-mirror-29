4b06a31c6d207d597e5af477d149d6260e712bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:10 +0000

    Add package plane/0.0.2/plane-0.0.2-py3.5.egg

---
+++ packages/plane/plane-0.0.2-py3.5.egg/plane/__init__.py
@@ -0,0 +1,9 @@
+from .punctuation import remove_punctuation
+from .main import replace, extract, build_new_regex
+from .pattern import (
+    RESTRICT_URL,
+    EMAIL,
+    TELEPHONE,
+    SPACE,
+    HTML,
+)
