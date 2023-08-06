6a687b0d89b4f51e2cff6f103a288c14493face2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:24 +0000

    Add package naruhodo/0.2.6/naruhodo-0.2.6.tar.gz

---
+++ packages/naruhodo/naruhodo-0.2.6.tar.gz/naruhodo-0.2.6/naruhodo/__init__.py
@@ -0,0 +1,18 @@
+"""
+Naruhodo 0.2.1
+
+A python library for automatic semantic graph generation from human-readable text.
+
+## Change-Log
+
+* 0.2.1
+  * Primitive coreference resolution for Japanese.
+* 0.2.0
+  * Major API change for multi-language support and parallel processing. 
+  * Parallel processing support for parsing using multiprocessing module. 
+* 0.1.0
+  * Initial version
+
+"""
+
+from naruhodo.core.parser import parser
