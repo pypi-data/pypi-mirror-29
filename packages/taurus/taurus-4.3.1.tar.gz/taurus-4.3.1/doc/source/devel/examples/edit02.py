f89313d5560dfbb9323169e3a1c00e1ef813fd28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:59 +0000

    Add package taurus/4.3.1/taurus-4.3.1.tar.gz

---
+++ packages/taurus/taurus-4.3.1.tar.gz/taurus-4.3.1/doc/source/devel/examples/edit02.py
@@ -0,0 +1,33 @@
+import sys
+from taurus.external.qt import Qt
+from taurus.qt.qtgui.container import TaurusWidget
+from taurus.qt.qtgui.display import TaurusLabel
+from taurus.qt.qtgui.input import TaurusValueSpinBox
+
+from taurus.qt.qtgui.application import TaurusApplication
+app = TaurusApplication(sys.argv)
+
+panel = TaurusWidget()
+layout = Qt.QHBoxLayout()
+panel.setLayout(layout)
+
+w1 = TaurusLabel()
+w2 = TaurusLabel()
+w3 = TaurusValueSpinBox()
+w4 = TaurusLabel()
+layout.addWidget(w1)
+layout.addWidget(w2)
+layout.addWidget(w3)
+layout.addWidget(w4)
+w1.setUseParentModel(True)
+w2.setUseParentModel(True)
+w3.setUseParentModel(True)
+w4.setUseParentModel(True)
+panel.setModel('sys/taurustest/1')
+w1.setModel('/position#label')
+w2.setModel('/position')
+w3.setModel('/position')
+w4.setModel('/position#unit')
+
+panel.show()
+sys.exit(app.exec_())
