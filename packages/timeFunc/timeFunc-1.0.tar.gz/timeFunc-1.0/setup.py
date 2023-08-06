f803838c5addc40d0c1f8333904ae11ce0491705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:21 +0000

    Add package timeFunc/1.0/timeFunc-1.0.tar.gz

---
+++ packages/timeFunc/timeFunc-1.0.tar.gz/timeFunc-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'timeFunc',
+    license = 'MIT',
+    packages = find_packages(),
+    version = '1.0',
+    description = 'A decorator type timing function',
+    author = 'Joshua Hinchliffe',
+    author_email = 'joshuahinchliffe97@gmail.com',
+    url = 'https://github.com/madpianoguy/timeFunc',
+    download_url = 'https://github.com/madpianoguy/timeFunc/archive/1.0.tar.gz',
+    keywords = ['timing','decorator','madpianoguy'],
+    classifiers = [],
+)
