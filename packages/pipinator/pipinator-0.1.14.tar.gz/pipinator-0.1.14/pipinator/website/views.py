3393bee7b5b259ed8d532bc85f9df343fbe3a895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:39 +0000

    Add package pipinator/0.1.14/pipinator-0.1.14.tar.gz

---
+++ packages/pipinator/pipinator-0.1.14.tar.gz/pipinator-0.1.14/pipinator/website/views.py
@@ -0,0 +1,19 @@
+import os
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.template import loader
+
+from easy_thumbnails.files import get_thumbnailer
+
+
+def index(request):
+    template = loader.get_template('website/index.html')
+    thumb_url = get_thumbnailer(settings.BASE_DIR +
+                                '/pipinator/website/static/mountains-big.jpg')['thumb'].url
+
+    context = {
+        'thumb_filename': os.path.basename(thumb_url)
+    }
+
+    return HttpResponse(template.render(context, request))
