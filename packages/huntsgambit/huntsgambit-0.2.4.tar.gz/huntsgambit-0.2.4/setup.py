8b1e5da1ad1b6e922eb3480f5788b8dcb0535120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:01 +0000

    Add package huntsgambit/0.2.4/huntsgambit-0.2.4.tar.gz

---
+++ packages/huntsgambit/huntsgambit-0.2.4.tar.gz/huntsgambit-0.2.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+     name='huntsgambit',
+     version='0.2.4',
+     url="https://github.com/abel1311/hunts_gambit",
+     entry_points={
+        "console_scripts": ['is_pwnified = huntsgambit.is_pwnified:main']
+        },
+     install_requires=[
+         'requests',
+         'argparse',
+         'pandas>=0.19.1'
+     ]
+)
