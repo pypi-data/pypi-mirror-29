01960e6150bfb16f136e5f2c9222979c821a3a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:25 +0000

    Add package fulford.data/1.0.3/fulford.data-1.0.3.tar.gz

---
+++ packages/fulford.data/fulford.data-1.0.3.tar.gz/fulford.data-1.0.3/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+desc = "Declarative processing, transforming, and validating of data."
+
+kwargs = {
+    "name": "fulford.data",
+    "description": desc,
+    "author": "James Patrick Fulford",
+    "author_email": "james.patrick.fulford@gmail.com",
+    "url": "https://github.com/jamesfulford/fulford.data",
+    "license": "Apache-2.0",
+
+    "version": "1.0.3",
+
+    "packages": find_packages()
+}
+
+setup(
+    **kwargs
+)
