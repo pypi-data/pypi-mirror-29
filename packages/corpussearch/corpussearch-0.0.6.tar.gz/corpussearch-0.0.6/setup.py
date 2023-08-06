546fe7d4f503188cc61d6fa37a3af286ef119dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:32 +0000

    Add package corpussearch/0.0.6/corpussearch-0.0.6.tar.gz

---
+++ packages/corpussearch/corpussearch-0.0.6.tar.gz/corpussearch-0.0.6/setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    # Application name:
+    name="corpussearch",
+
+    # Version number (initial):
+    version="0.0.6",
+
+    # Application author details:
+    author="Malte Vogl",
+    author_email="mvogl@mpiwg-berlin.mpg.de",
+
+    # Packages
+    packages=find_packages(exclude=('tests')),
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    url='https://github.com/TOPOI-DH/corpussearch/',
+
+    # Details
+
+    license='GPLv3',
+    description="Tools for loading and analyzing large text corpora.",
+
+    long_description=readme,
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Text Processing :: General',
+        'Topic :: Text Processing :: Indexing',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+    ],
+
+    project_urls={
+        'Home': 'https://github.com/TOPOI-DH/corpussearch/',
+        'Tracker': 'https://github.com/TOPOI-DH/corpussearch/issues',
+        'Download': 'https://github.com/TOPOI-DH/corpussearch/archive/0.0.6.tar.gz',
+    },
+
+    python_requires='>=3',
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "pandas",
+        "citableclass",
+        "ipywidgets",
+        "Ipython",
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
