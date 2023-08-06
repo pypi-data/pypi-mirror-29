b9a066aacbf82120da6b77cc61896c0653a6e55b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:08 +0000

    Add package pygtails/0.1.4/pygtails-0.1.4.tar.gz

---
+++ packages/pygtails/pygtails-0.1.4.tar.gz/pygtails-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='pygtails',
+      version='0.1.4',
+      description='A simple wrapper around pygame',
+      long_description=long_description,
+      url='http://pygtails.readthedocs.io/en/latest/',
+      author='Josie Thompson',
+      classifiers=['Development Status :: 1 - Planning',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Natural Language :: English',
+                   'Programming Language :: Python :: 3 :: Only',
+                   'Topic :: Software Development :: Libraries :: pygame'],
+      python_requires=">=3",
+      package_dir={'': 'src'},
+      packages=[''],
+      project_urls={'Source': 'https://github.com/josiest/pygtails/'})
