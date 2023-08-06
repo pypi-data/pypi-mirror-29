ba91db9ab2fc1a6b26236aa78b61355af1a048ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:01 +0000

    Add package sessionlib/0.1.8/sessionlib-0.1.8-py2.py3-none-any.whl

---
+++ packages/sessionlib/sessionlib-0.1.8-py2.py3-none-any.whl/sessionlib/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for sessionlib."""
+
+from .sessionlib import Session, sessionaware
+
+__author__ = """Paulo Romeira"""
+__email__ = 'paulo@pauloromeira.com'
+__version__ = '0.1.8'
