54c225e1ffc23fb013bd520cf455e1a7fb615e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:41 +0000

    Add package PyEELS/1.0.8.dev1/PyEELS-1.0.8.dev1.tar.gz

---
+++ packages/PyEELS/PyEELS-1.0.8.dev1.tar.gz/PyEELS-1.0.8.dev1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup, Extension
+import numpy as np
+
+ext_modules = [ Extension('pyeels.cpyeels', sources = ['./pyeels/_spectrum/spectrum.c'], extra_compile_args=['-std=c99']) ]
+
+#readme = open('README.md','r')
+
+setup(
+      name = 'PyEELS',
+      version = '1.0.8.dev1',
+      author = "Sindre R. Bilden",
+      author_email = "s.r.bilden@fys.uio.no",
+      url = 'https://github.com/sindrerb/pyeels',
+      description = ("Python package for simulating EELS from band structures"),
+      #long_description = readme.read(),
+      install_requires=['hyperspy', 'pythtb'], #Not longer required: 'spglib'
+      packages=['pyeels'],
+      include_dirs = [np.get_include()],
+      ext_modules = ext_modules
+      )
+
+#readme.close()
