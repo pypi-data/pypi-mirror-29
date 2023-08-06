49c8a18d911dce91e45f8dc76c86ea5314cf41f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:54 +0000

    Add package zvit/0.0.1/zvit-0.0.1.tar.gz

---
+++ packages/zvit/zvit-0.0.1.tar.gz/zvit-0.0.1/zvit/version.py
@@ -0,0 +1,10 @@
+#
+# THIS FILE IS GENERATED FROM ZVIT SETUP.PY
+#
+short_version = "0.0.1"
+version       = "0.0.1"
+full_version  = "0.0.1"
+git_revision  = "375858e0998e05cc91162ea68f1bb980ed4801d5"
+sem_revision  = "0.0.1"
+release       = True
+build_time    = "20180307T002727Z"
