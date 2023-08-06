5e593fdb878db0e474f6b034a659dcd1313361ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:20 +0000

    Add package toolconfig/0.0.2/toolconfig-0.0.2.tar.gz

---
+++ packages/toolconfig/toolconfig-0.0.2.tar.gz/toolconfig-0.0.2/README.md
@@ -0,0 +1,4 @@
+toolconfig is intended for command line-tools with multiple profile/config
+support. It is particularly useful for developer tools as it stores config
+files in a user's home directory by default. It can automatically expand path
+settings.
---
+++ packages/toolconfig/toolconfig-0.0.2.tar.gz/toolconfig-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name="toolconfig",
+    version="0.0.2",
+    author="Daniel M. Weeks",
+    author_email="dan@danweeks.net",
+    description=("JSON config helper"),
+    license="GPL-2",
+    keywords="json config expansion",
+    url="https://github.com/doctaweeks/toolconfig",
+    packages=['toolconfig'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+)
