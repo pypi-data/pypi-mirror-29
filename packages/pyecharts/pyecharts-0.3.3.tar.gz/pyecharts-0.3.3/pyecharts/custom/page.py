5d2e1660642ad21cf8e755d581ca350f72f8ef01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:27 +0000

    Add package pyecharts/0.3.3/pyecharts-0.3.3.tar.gz

---
+++ packages/pyecharts/pyecharts-0.3.3.tar.gz/pyecharts-0.3.3/pyecharts/custom/page.py
@@ -0,0 +1,94 @@
+# coding=utf-8
+
+from jinja2 import Markup
+
+import pyecharts.utils as utils
+import pyecharts.engine as engine
+from pyecharts.conf import CURRENT_CONFIG
+import pyecharts.constants as constants
+
+
+class Page(list):
+    """
+    A composite object to present multiple charts vertically in a single page
+    """
+
+    def __init__(self, page_title=constants.PAGE_TITLE):
+        list.__init__([])
+        self._page_title = page_title
+
+    def add(self, achart_or_charts):
+        """
+        Append chart(s) to the rendering page
+
+        :param achart_or_charts:
+        :return:
+        """
+        if isinstance(achart_or_charts, list):
+            self.extend(achart_or_charts)
+        else:
+            self.append(achart_or_charts)
+
+    def render(self,
+               path='render.html',
+               template_name='simple_page.html',
+               object_name='page',
+               extra_context=None):
+        context = {object_name: self}
+        context.update(extra_context or {})
+        html = engine.render(template_name, **context)
+        utils.write_utf8_html_file(path, html)
+
+    def render_embed(self):
+        """
+        Produce rendered charts in html for embedding purpose
+
+        :return:
+        """
+        return Markup('<br/> '.join([chart.render_embed() for chart in self]))
+
+    def get_js_dependencies(self):
+        """
+        Declare its javascript dependencies for embedding purpose
+        """
+        return CURRENT_CONFIG.produce_html_script_list(
+            self.js_dependencies)
+
+    def _repr_html_(self):
+        """
+
+        :return:
+        """
+        doms = components = ""
+        dependencies = self.js_dependencies
+        for chart in self:
+            doms += chart._render_notebook_dom_()
+            components += chart._render_notebook_component_()
+
+        require_config = CURRENT_CONFIG.produce_require_configuration(
+            dependencies)
+        return engine.render_notebook(
+            "notebook.html",
+            single_chart=components,
+            dom=doms,
+            **require_config)
+
+    @property
+    def js_dependencies(self):
+        # Treat self as a list,not a page
+        return utils.merge_js_dependencies(*self)
+
+    @property
+    def page_title(self):
+        return self._page_title
+
+    @classmethod
+    def from_charts(cls, *args):
+        """
+        A shortcut class method for building page object from charts.
+        :param args:
+        :return:
+        """
+        p = cls()
+        p.extend(args)
+        return p
---
+++ packages/pyecharts/pyecharts-0.3.3.tar.gz/pyecharts-0.3.3/pyecharts/engine.py
@@ -0,0 +1,167 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
+from jinja2 import Environment, FileSystemLoader, environmentfunction, Markup
+
+import pyecharts.conf as conf
+import pyecharts.utils as utils
+
+LINK_SCRIPT_FORMATTER = '<script type="text/javascript" src="{}"></script>'
+EMBED_SCRIPT_FORMATTER = '<script type="text/javascript">\n{}\n</script>'
+CHART_DIV_FORMATTER = '<div id="{chart_id}" style="width:{width};height:{height};"></div>'  # flake8: noqa
+CHART_CONFIG_FORMATTER = """
+var myChart_{chart_id} = echarts.init(document.getElementById('{chart_id}'));
+var option_{chart_id} = {options};
+myChart_{chart_id}.setOption(option_{chart_id});
+"""
+
+
+@environmentfunction
+def echarts_js_dependencies(env, *args):
+    """ Render script html nodes in external link mode.
+
+    :param env:
+    :param args:
+    """
+    current_config = env.pyecharts_config
+    dependencies = utils.merge_js_dependencies(*args)
+
+    if current_config.js_embed:
+        contents = current_config.read_file_contents_from_local(dependencies)
+
+        return Markup(
+            '\n'.join([EMBED_SCRIPT_FORMATTER.format(c) for c in contents])
+        )
+    else:
+        js_links = current_config.generate_js_link(dependencies)
+        return Markup(
+            '\n'.join([LINK_SCRIPT_FORMATTER.format(j) for j in js_links])
+        )
+
+
+@environmentfunction
+def echarts_js_dependencies_embed(env, *args):
+    """ Render script html nodes in embed mode,Only used for local files.
+
+    :param env:
+    :param args:
+    """
+    current_config = env.pyecharts_config
+    dependencies = utils.merge_js_dependencies(*args)
+    contents = current_config.read_file_contents_from_local(dependencies)
+    return Markup(
+        '\n'.join([EMBED_SCRIPT_FORMATTER.format(c) for c in contents])
+    )
+
+
+@environmentfunction
+def echarts_container(env, chart):
+    """ Render a div html element for a chart.
+
+    :param env:
+    :param chart: A pyecharts.base.Base object
+    """
+
+    return Markup(
+        CHART_DIV_FORMATTER.format(
+            chart_id=chart.chart_id,
+            width=utils.to_css_length(chart.width),
+            height=utils.to_css_length(chart.height)
+        ))
+
+
+def generate_js_content(*charts):
+    """ Generate the initial code fragment for one or some chart instances.
+
+    :param charts:
+    :return:
+    """
+    contents = []
+    for chart in charts:
+        js_content = CHART_CONFIG_FORMATTER.format(
+            chart_id=chart.chart_id,
+            options=utils.json_dumps(chart.options, indent=4)
+        )
+        contents.append(js_content)
+    contents = '\n'.join(contents)
+    return contents
+
+
+@environmentfunction
+def echarts_js_content(env, *charts):
+    """ Render script html node for echarts initial code.
+
+    :param env:
+    :param chart:
+    """
+    return Markup(EMBED_SCRIPT_FORMATTER.format(generate_js_content(*charts)))
+
+
+@environmentfunction
+def echarts_js_content_wrap(env, *charts):
+    """ Render echarts initial code for a chart.
+
+    :param env:
+    :param charts:
+    """
+    return Markup(generate_js_content(*charts))
+
+
+# Public API,see document for more detail.
+
+ECHAERTS_TEMPLATE_FUNCTIONS = {
+    'echarts_js_dependencies': echarts_js_dependencies,
+    'echarts_js_dependencies_embed': echarts_js_dependencies_embed,
+    'echarts_container': echarts_container,
+    'echarts_js_content': echarts_js_content,
+    'echarts_js_content_wrap': echarts_js_content_wrap
+}
+
+
+class BaseEnvironment(Environment):
+    """
+    Add config and echarts template functions to a Environment object.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.pyecharts_config = kwargs.pop('pyecharts_config', None)
+        if self.pyecharts_config is None:
+            raise TypeError(
+                'no pyecharts_config for this environment specified')
+        super(BaseEnvironment, self).__init__(*args, **kwargs)
+        self.globals.update(ECHAERTS_TEMPLATE_FUNCTIONS)
+
+
+class EchartsEnvironment(BaseEnvironment):
+    """
+    Built-in jinja2 template engine for pyecharts
+    """
+
+    def __init__(self, pyecharts_config=None, *args, **kwargs):
+        pyecharts_config = pyecharts_config or conf.PyEchartsConfig()
+        loader = kwargs.pop('loader', None)
+        if loader is None:
+            loader = FileSystemLoader(pyecharts_config.echarts_template_dir)
+        super(EchartsEnvironment, self).__init__(
+            pyecharts_config=pyecharts_config,
+            keep_trailing_newline=True,
+            trim_blocks=True,
+            lstrip_blocks=True,
+            loader=loader,
+            *args,
+            **kwargs)
+
+
+def render(template_file, notebook=False, **context):
+    config = conf.CURRENT_CONFIG
+    echarts_env = EchartsEnvironment(
+        pyecharts_config=config,
+        loader=FileSystemLoader(
+            [config.echarts_template_dir, conf.DEFAULT_TEMPLATE_DIR])
+    )
+    template = echarts_env.get_template(template_file)
+    return template.render(**context)
+
+
+def render_notebook(template_file, **context):
+    return render(template_file, notebook=True, **context)
