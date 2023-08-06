51403c4e8fdeb832c51469fafc7cd407d48bfffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:45 +0000

    Add package httprunner/0.9.5/HttpRunner-0.9.5.tar.gz

---
+++ packages/httprunner/HttpRunner-0.9.5.tar.gz/HttpRunner-0.9.5/httprunner/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.9.5'
+
+from httprunner.task import HttpRunner
---
+++ packages/httprunner/HttpRunner-0.9.5.tar.gz/HttpRunner-0.9.5/setup.py
@@ -0,0 +1,52 @@
+#encoding: utf-8
+import io
+
+from httprunner import __version__
+from setuptools import find_packages, setup
+
+with io.open("README.rst", encoding='utf-8') as f:
+    long_description = f.read()
+
+install_requires = [
+    "requests",
+    "PyYAML",
+    "Jinja2",
+    "har2case",
+    "colorama",
+    "colorlog"
+]
+
+setup(
+    name='HttpRunner',
+    version=__version__,
+    description='HTTP test runner, not just about api test and load test.',
+    long_description=long_description,
+    author='Leo Lee',
+    author_email='mail@debugtalk.com',
+    url='https://github.com/HttpRunner/HttpRunner',
+    license='MIT',
+    packages=find_packages(exclude=["examples", "tests", "tests.*"]),
+    package_data={
+        'httprunner': ['locustfile_template', "templates/default_report_template.html"],
+    },
+    keywords='api test',
+    install_requires=install_requires,
+    extras_require={
+        'dev': ['flask']
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ate=httprunner.cli:main_hrun',
+            'httprunner=httprunner.cli:main_hrun',
+            'hrun=httprunner.cli:main_hrun',
+            'locusts=httprunner.cli:main_locust'
+        ]
+    }
+)
