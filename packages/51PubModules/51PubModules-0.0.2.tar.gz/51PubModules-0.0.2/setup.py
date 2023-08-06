beab1bcee8d5109598f99ee38169802d6198574e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:36 +0000

    Add package 51PubModules/0.0.2/51PubModules-0.0.2.tar.gz

---
+++ packages/51PubModules/51PubModules-0.0.2.tar.gz/51PubModules-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='51PubModules',
+    version='0.0.2',
+    author='jun',
+    author_email='jun.mr@qq.com',
+    url='http://docs.51pub.cn/python/opmodules',
+    packages=['opmysql'],
+    description='system manage modules',
+    license='MIT',
+    install_requires=['pymysql'],
+)
