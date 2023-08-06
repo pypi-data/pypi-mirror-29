ad992972136c2ee29b2fb4a1a3c024e8eed2051b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:24 +0000

    Add package cloud-cleaner/0.0.2/cloud_cleaner-0.0.2-py3-none-any.whl

---
+++ packages/cloud-cleaner/cloud_cleaner-0.0.2-py3-none-any.whl/tests/cloud_cleaner/bin/test_entrypoint.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from unittest.mock import Mock
+from argparse import ArgumentParser
+
+from cloud_cleaner.bin.entrypoint import cloud_clean
+from cloud_cleaner.config import CloudCleanerConfig
+from cloud_cleaner.resources import ALL_RESOURCES
+
+
+class TestEntrypoint(TestCase):
+    def test_cloud_cleaner_noopts(self):
+        parser = ArgumentParser()
+        config = CloudCleanerConfig(parser=parser, args=[])
+        with self.assertRaises(KeyError):
+            cloud_clean(["--os-auth-url", "http://no.com"], config)
+        self.assertIsNone(config.get_resource())
+
+    def test_cloud_cleaner_server(self):
+        config = CloudCleanerConfig(args=[])
+        ALL_RESOURCES["server"].process = Mock()
+        ALL_RESOURCES["server"].clean = Mock()
+        cloud_clean(args=["--os-auth-url", "http://no.com", "server",
+                          "--name", "derp"], config=config)
+        self.assertEqual("server", config.get_resource())
+        self.assertEqual("derp", config.get_arg("name"))
+        self.assertEqual(1, len(ALL_RESOURCES["server"].process.mock_calls))
+        self.assertEqual(0, len(ALL_RESOURCES["server"].clean.mock_calls))
+
+    def test_resource_type(self):
+        ALL_RESOURCES["server"].process = Mock()
+        ALL_RESOURCES["server"].clean = Mock()
+        cloud_clean(args=["--os-auth-url", "http://no.com", "-f", "server"])
+        self.assertEqual(1, len(ALL_RESOURCES["server"].process.mock_calls))
+        self.assertEqual(1, len(ALL_RESOURCES["server"].clean.mock_calls))
---
+++ packages/cloud-cleaner/cloud_cleaner-0.0.2-py3-none-any.whl/tests/cloud_cleaner/test_config.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+from argparse import ArgumentParser
+from logging import getLogger, WARNING, INFO, DEBUG
+from shade import OpenStackCloud
+from cloud_cleaner.config import CloudCleanerConfig
+
+
+class TestConfig(TestCase):
+    def test_set_args(self):
+        parser = ArgumentParser()
+        config = CloudCleanerConfig(parser=parser, args=[])
+        config.add_subparser("item")
+        config.set_args(["--os-auth-url", "http://no.com", "item"])
+        config.parse_args()
+        self.assertEqual("item", config.get_resource())
+        config.warning("Dummy warning")
+        log = getLogger("cloud_cleaner")
+        self.assertEqual(log.getEffectiveLevel(), WARNING)
+        self.assertIsNone(config.get_arg("no_arg"))
+
+    def test_subcommand_incorrect(self):
+        parser = ArgumentParser()
+        config = CloudCleanerConfig(parser=parser, args=["notfound"])
+        config.set_args(["notfound"])
+        config.add_subparser("found")
+        # ArgumentError raises SystemExit internally, apparently
+        with self.assertRaises(SystemExit):
+            config.parse_args()
+
+    def test_get_cloud_is_none(self):
+        parser = ArgumentParser()
+        config = CloudCleanerConfig(parser=parser, args=[])
+        self.assertIsNone(config.get_cloud())
+
+    def test_verbose_one(self):
+        args = ["--os-auth-url", "http://no.com", "-v"]
+        config = CloudCleanerConfig(args=args)
+        config.parse_args()
+        log = getLogger("cloud_cleaner")
+        self.assertEqual(log.getEffectiveLevel(), INFO)
+        shade = config.get_shade()
+        self.assertIsInstance(shade, OpenStackCloud)
+
+    def test_verbose_two(self):
+        args = ["--os-auth-url", "http://no.com", "-vv"]
+        config = CloudCleanerConfig(args=args)
+        config.parse_args()
+        log = getLogger("cloud_cleaner")
+        self.assertEqual(log.getEffectiveLevel(), DEBUG)
