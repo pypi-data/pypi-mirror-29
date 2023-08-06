5822f1a1c4732d8a30e5a6c60713ab506d72bc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:28 +0000

    Add package PyGObject/3.28.0/PyGObject-3.28.0.tar.gz

---
+++ packages/PyGObject/PyGObject-3.28.0.tar.gz/PyGObject-3.28.0/.gitlab-ci/run-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TAG="lazka/pygobject:pyenv"
+
+sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
+    --file "Dockerfile" .
+sudo docker run -e PYENV_VERSION='3.6.4' --rm \
+    --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
+    --tty --interactive "${TAG}" bash
