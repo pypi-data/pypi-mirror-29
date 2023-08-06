bd893f655ea4a2b60b2aa1acf995b11af6a47c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:56 +0000

    Add package leccap/0.0.5/leccap-0.0.5.tar.gz

---
+++ packages/leccap/leccap-0.0.5.tar.gz/leccap-0.0.5/leccap/constants.py
@@ -0,0 +1,8 @@
+# urls
+AUTH_URL = "https://weblogin.umich.edu/cosign-bin/cosign.cgi"
+AUTH_PAGE_URL = "https://weblogin.umich.edu"
+API_BASE_URL = "https://leccap.engin.umich.edu/leccap/viewer/api/recordings/"
+
+DEFAULT_CONCURRENCY = 5
+DEFAULT_MSG = ""
+DEFAULT_DIR = "."
