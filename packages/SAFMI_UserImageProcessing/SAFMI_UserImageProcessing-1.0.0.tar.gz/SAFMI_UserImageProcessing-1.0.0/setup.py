7570c955e406cd0ad53b98da6bfe0bb4a3af26cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:39 +0000

    Add package SAFMI_UserImageProcessing/1.0.0/SAFMI_UserImageProcessing-1.0.0.tar.gz

---
+++ packages/SAFMI_UserImageProcessing/SAFMI_UserImageProcessing-1.0.0.tar.gz/SAFMI_UserImageProcessing-1.0.0/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 19:58:29 2018
+
+@author: sarth
+"""
+
+from setuptools import setup
+
+setup(
+name = 'SAFMI_UserImageProcessing',
+version = '1.0.0',
+scripts=['SAFMI/UserImageProcessing/UserImageProcessing']
+)
+
+setup(
+name = 'SAFMI_UserPrediction',
+version = '1.0.0',
+scripts=['SsAFMI_UserPrediction'] 
+)
