9b9d65487a8e0e55d6f4cb78eba2713a3b02f409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:05 +0000

    Add package import-anywhere/1.0/import_anywhere-1.0.tar.gz

---
+++ packages/import-anywhere/import_anywhere-1.0.tar.gz/import_anywhere-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(name='import_anywhere',
+      version='1.0',
+      description='Python Package allowing relative imports, no dependence on where python is run from',
+      author='Era Choshen',
+      author_email='erachoshen@gmail.com',
+      license='MIT',
+      packages=['import_anywhere'],
+      url= 'https://github.com/ec2604/import_anywhere',
+      download_url='https://github.com/ec2604/import_anywhere/archive/1.0.tar.gz',
+      keywords=['relative_imort'],
+      python_requires = '>=3',
+     )
