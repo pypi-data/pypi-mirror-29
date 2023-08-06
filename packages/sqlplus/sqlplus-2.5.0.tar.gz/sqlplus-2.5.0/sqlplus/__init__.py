0aea3150850f3f7e38f79b7d46ed461fc9bd4d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:28 +0000

    Add package sqlplus/2.5.0/sqlplus-2.5.0.tar.gz

---
+++ packages/sqlplus/sqlplus-2.5.0.tar.gz/sqlplus-2.5.0/sqlplus/__init__.py
@@ -0,0 +1,12 @@
+"""
+"""
+# 'connect' is deprecated,
+# Often times I feel isconsec is too much
+from .core import Row, Rows, readxl, process, Load, Map, Union, Join, tocsv, drop, rename
+from .util import isnum, dmath, isconsec, grouper
+
+
+__all__ = ['process', 'Row', 'Rows', 'isnum', 'dmath', 'readxl', 'grouper',
+           'isconsec', 'Map', 'Load', 'Union', 'Join', 'rename', 'tocsv', 'drop']
+
+
