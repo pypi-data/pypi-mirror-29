c643e64e1a3043d7b3eddc3e5b8613cf875a8490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:39 +0000

    Add package remlang/0.1.2/remlang-0.1.2-py3-none-any.whl

---
+++ packages/remlang/remlang-0.1.2-py3-none-any.whl/remlang/intepreter.py
@@ -0,0 +1,67 @@
+from .compiler.ast import (default_env, handle_error, MetaInfo,
+                           token, ast_for_statement)
+from .compiler.rem_parser import statement
+from Ruikowa.ErrorFamily import DSLSyntaxError
+import logging
+from .standard.default import LICENSE_INFO
+import warnings
+
+warnings.filterwarnings("ignore")
+
+logger = logging.Logger('catch_all')
+main = default_env['main']
+print(LICENSE_INFO)
+
+
+def repl():
+    left = []
+    count = None
+
+    parser = handle_error(statement)
+    while True:
+        try:
+            inp = input('>> ' if count is None else '   ')
+            if not inp:
+                continue
+            if inp == ':manager':
+                print(main['@module_manager'])
+                continue
+            elif inp == ':modules':
+                print(main['@module_manager']['@modules'])
+                continue
+            elif inp == ':vars':
+                print(main)
+                continue
+        except KeyboardInterrupt:
+            import sys
+            print('Good Bye~')
+            sys.exit(0)
+
+        meta = MetaInfo(fileName='<repr>')
+
+        left.extend(token(inp))
+        try:
+            stmt = parser(left, meta=meta, partial=False)
+            try:
+                left.clear()
+                if count is not None:
+                    count = None
+                ret = ast_for_statement(stmt, main)
+                if ret is not None:
+                    print('=> ', repr(ret))
+
+            except BaseException as e:
+                logger.error(e, exc_info=True)
+                continue
+
+        except DSLSyntaxError:
+
+            now_count = meta.count
+            is_incremental = count is None or now_count > count
+
+            if is_incremental:
+                count = now_count
+            else:
+                left.clear()
+                count = None
+                continue
