45a5f2fbdd54402568dfb015b4ec5051bed5fe87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:54 +0000

    Add package canonicalwebteam.http/0.0.1/canonicalwebteam.http-0.0.1.tar.gz

---
+++ packages/canonicalwebteam.http/canonicalwebteam.http-0.0.1.tar.gz/canonicalwebteam.http-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='canonicalwebteam.http',
+    version='0.0.1',
+    author='Canonical Webteam',
+    url='https://github.com/canonical-webteam/http',
+    packages=[
+        'canonicalwebteam',
+        'canonicalwebteam.http'
+    ],
+    description=(
+        'Basic opinionated functions for making HTTP calls '
+        'in Webteam python projects'
+    ),
+    install_requires=[
+        "requests-cache >= 0.4.0",
+        "prometheus-client >= 0.1.1",
+    ],
+)
