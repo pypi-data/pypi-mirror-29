e5ab4d00d1097062bfd1c76c1ba6b42702921654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:01 +0000

    Add package ttag/0.1/ttag-0.1.zip

---
+++ packages/ttag/ttag-0.1.zip/ttag-0.1/setup.py
@@ -0,0 +1,14 @@
+# -*-coding:utf-8 -*-
+from setuptools import setup
+
+setup(
+    name = "ttag",
+    description = "translate original json data into the new json data",
+    version = "0.1",
+    author = "slc",
+    author_email = "1427784107@qq.com",
+    install_requires = [],
+    packages = [
+        "ttag"
+    ]
+)
