25026beec614f5d210595c8db0c9a3535f13948f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:49 +0000

    Add package scopus.wp/0.2.2.12/scopus.wp-0.2.2.12.tar.gz

---
+++ packages/scopus.wp/scopus.wp-0.2.2.12.tar.gz/scopus.wp-0.2.2.12/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='scopus.wp',
+    version='0.2.2.12',
+    description='A tool for a wordpress server which will automatically post science publications from scopus database',
+    url='https://github.com/the16thpythonist/ScopusWp',
+    author='Jonas Teufel',
+    author_email='jonseb1998@gmail.com',
+    license='MIT',
+    packages=['ScopusWp', 'ScopusWp/scopus'],
+    include_package_data=False,
+    install_requires=[
+        'requests>=2.0',
+        'mysqlclient>=1.2',
+        'unidecode>=0.4',
+        'tabulate>=0.8',
+        'python-wordpress-xmlrpc>=2.3'
+    ],
+    python_requires='>=3, <4',
+    package_data={
+        '': []
+    },
+)
