8f0f3a65295a1a1f8563b2f04230e44e5d14dd12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:38 +0000

    Add package pychord/0.3.0/pychord-0.3.0.tar.gz

---
+++ packages/pychord/pychord-0.3.0.tar.gz/pychord-0.3.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+version = '0.3.0'
+
+try:
+    import pypandoc
+    read_md = lambda f: pypandoc.convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+setup(
+    name='pychord',
+    version=version,
+    description="A library to handle musical chords in python.",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        ],
+    keywords='music chord',
+    author='Yuma Mihira',
+    author_email='info@yurax2.com',
+    url='https://github.com/yuma-m/pychord',
+    license='MIT',
+    packages=find_packages(exclude=['test']),
+    include_package_data=True,
+    zip_safe=True,
+    long_description=read_md('README.md'),
+    test_suite='test',
+)
