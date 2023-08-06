006f1d1f38f2b7ce4dd1c6368490cd1db2de8bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:58 +0000

    Add package russell-cli/0.6.10/russell-cli-0.6.10.tar.gz

---
+++ packages/russell-cli/russell-cli-0.6.10.tar.gz/russell-cli-0.6.10/russell/client/env.py
@@ -0,0 +1,21 @@
+from russell.exceptions import *
+from russell.client.base import RussellHttpClient
+from russell.log import logger as russell_logger
+
+
+class EnvClient(RussellHttpClient):
+    """
+    Client to interact with Env api
+    """
+    def __init__(self):
+        self.url = "/env"
+        super(EnvClient, self).__init__()
+
+    def get_all(self):
+        try:
+            response = self.request("GET",
+                                    self.url)
+            return response
+        except Exception as e:
+            russell_logger.info("Error while retrieving env: {}".format(e.message))
+            return {}
