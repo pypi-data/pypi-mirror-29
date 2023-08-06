648a6b1fcf432a5e9543a6048ce1fa7ba5c6bc55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:15 +0000

    Add package reflink/0.2.0/reflink-0.2.0-cp36-cp36m-win_amd64.whl

---
+++ packages/reflink/reflink-0.2.0-cp36-cp36m-win_amd64.whl/reflink/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Python reflink."""
+
+__author__ = """Ruben De Smet"""
+__email__ = 'pypi@rubdos.be'
+__version__ = '0.1.3'
+
+from .reflink import reflink, supported_at
+from .error import ReflinkImpossibleError
