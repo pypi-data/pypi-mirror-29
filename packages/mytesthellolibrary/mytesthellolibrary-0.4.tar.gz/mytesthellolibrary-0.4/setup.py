1104dd40e9cf1c33c51e6eb21fac50b0b8ef962d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:53 +0000

    Add package mytesthellolibrary/0.4/mytesthellolibrary-0.4.tar.gz

---
+++ packages/mytesthellolibrary/mytesthellolibrary-0.4.tar.gz/mytesthellolibrary-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='mytesthellolibrary',
+      version='0.4',
+      description='sum two numbers',
+      url='https://github.com/sebasgverde/mytesthellolibrary',
+      author='Sebastian Garcia Valencia',
+      author_email='sebasgverde@gmail.com',
+      license='MIT',
+      packages=['mytesthellolibrary'],
+      python_requires='>=2.7',
+      install_requires=[])
