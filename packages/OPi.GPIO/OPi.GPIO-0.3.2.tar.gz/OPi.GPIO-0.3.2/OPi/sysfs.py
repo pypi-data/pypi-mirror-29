db13070daa2fac39024202bda493a177805b968d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:43 +0000

    Add package OPi.GPIO/0.3.2/OPi.GPIO-0.3.2.tar.gz

---
+++ packages/OPi.GPIO/OPi.GPIO-0.3.2.tar.gz/OPi.GPIO-0.3.2/OPi/sysfs.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 Richard Hull
+# See LICENSE.md for details.
+
+from contextlib import contextmanager
+from OPi.constants import HIGH, LOW, IN, OUT, \
+    NONE, RISING, FALLING, BOTH
+
+
+@contextmanager
+def value_descriptor(pin, mode="r"):
+    path = "/sys/class/gpio/gpio{0}/value".format(pin)
+    with open(path, mode) as fp:
+        yield fp
+
+
+def export(pin):
+    path = "/sys/class/gpio/export"
+    with open(path, "w") as fp:
+        fp.write(str(pin))
+
+
+def unexport(pin):
+    path = "/sys/class/gpio/unexport"
+    with open(path, "w") as fp:
+        fp.write(str(pin))
+
+
+def direction(pin, dir):
+    assert dir in [IN, OUT]
+    path = "/sys/class/gpio/gpio{0}/direction".format(pin)
+    with open(path, "w") as fp:
+        if dir == IN:
+            fp.write("in")
+        else:
+            fp.write("out")
+
+
+def input(pin):
+    with value_descriptor(pin) as fp:
+        value = fp.read()
+        if value.strip() == str(LOW):
+            return LOW
+        else:
+            return HIGH
+
+
+def output(pin, value):
+    str_value = "1" if value else "0"
+    with value_descriptor(pin, "w") as fp:
+        fp.write(str_value)
+
+
+def edge(pin, trigger):
+    assert trigger in [NONE, RISING, FALLING, BOTH]
+    path = "/sys/class/gpio/gpio{0}/edge".format(pin)
+    opts = {
+        NONE: "none",
+        RISING: "rising",
+        FALLING: "falling",
+        BOTH: "both"
+    }
+    with open(path, "w") as fp:
+        fp.write(opts[trigger])
---
+++ packages/OPi.GPIO/OPi.GPIO-0.3.2.tar.gz/OPi.GPIO-0.3.2/doc/install.rst
@@ -0,0 +1,11 @@
+Installation
+------------
+.. note:: The library has been tested against Python 2.7 and 3.4+.
+
+   For **Python3** installation, substitute ``pip3`` for ``pip`` in the 
+   instructions below.
+
+Install the latest version of the library directly from
+`PyPI <https://pypi.python.org/pypi?:action=display&name=OPi.GPIO>`_::
+
+  $ sudo pip install --upgrade OPi.GPIO
