76f5e9e1c00db32d684c73716efc95a43144401e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:38 +0000

    Add package ohbotMac/1.1/ohbotMac-1.1.tar.gz

---
+++ packages/ohbotMac/ohbotMac-1.1.tar.gz/ohbotMac-1.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name = 'ohbotMac',
+    packages = ['ohbotMac'],
+    package_data={'': ['MotorDefinitionsv21.omd','Silence1.wav']},
+    include_package_data=True,
+    version = '1.1',  
+    description = 'python library for controlling Ohbot on a Mac',
+    author = 'ohbot',
+    author_email = 'info@ohbot.co.uk',
+    url = 'https://github.com/ohbot/ohbotWin-python',
+    download_url = 'https://github.com/ohbot/ohbotMac-python/archive/1.1.tar.gz',
+    keywords = ['ohbot', 'robot'],
+    classifiers = [],
+    install_requires=[
+          'pyserial','lxml','playsound',
+      ],
+)
