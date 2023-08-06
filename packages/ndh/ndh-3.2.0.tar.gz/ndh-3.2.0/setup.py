2449881d4e10026d4d58c5bd3228c7213109c514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:25 +0000

    Add package ndh/3.2.0/ndh-3.2.0.tar.gz

---
+++ packages/ndh/ndh-3.2.0.tar.gz/ndh-3.2.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+readme_file = os.path.join(os.path.dirname(__file__), 'README.rst')
+if not os.path.isfile(readme_file):
+    readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+with open(readme_file) as readme:
+    README = readme.read()
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.in')) as requirements:
+    REQUIREMENTS = [req.split('#egg=')[1] if '#egg=' in req else req for req in requirements.readlines()]
+
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='ndh',
+    version='3.2.0',
+    packages=['ndh'],
+    install_requires=REQUIREMENTS,
+    include_package_data=True,
+    license='BSD',
+    description='Nim’s Django Helpers',
+    long_description=README,
+    url='https://github.com/nim65s/ndh',
+    author='Guilhem Saurel',
+    author_email='webmaster@saurel.me',
+    python_requires='>=3.6',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
