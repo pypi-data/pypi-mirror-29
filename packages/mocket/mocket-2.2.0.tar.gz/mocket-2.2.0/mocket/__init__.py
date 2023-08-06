b58a6db2747515d1a45988ae11f5965648955524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:52 +0000

    Add package mocket/2.2.0/mocket-2.2.0.tar.gz

---
+++ packages/mocket/mocket-2.2.0.tar.gz/mocket-2.2.0/mocket/__init__.py
@@ -0,0 +1,10 @@
+try:
+    # Py2
+    from mocket import mocketize, Mocket, MocketEntry
+except ImportError:
+    # Py3
+    from mocket.mocket import mocketize, Mocket, MocketEntry
+
+__all__ = (mocketize, Mocket, MocketEntry)
+
+__version__ = '2.2.0'
