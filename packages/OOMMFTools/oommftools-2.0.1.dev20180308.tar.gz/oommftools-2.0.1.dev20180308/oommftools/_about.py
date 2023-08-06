c7fa36a89cf2bd10e71e6807a9e33d93755a4c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:22 +0000

    Add package OOMMFTools/2.0.1.dev20180308/oommftools-2.0.1.dev20180308.tar.gz

---
+++ packages/OOMMFTools/oommftools-2.0.1.dev20180308.tar.gz/oommftools-2.0.1.dev20180308/oommftools/_about.py
@@ -0,0 +1,16 @@
+__title__ = "OOMMFTools"
+__summary__ = ("OOMMFTools is a set of drag-and-drop GUI utilities ",
+    "to assist in OOMMF postprocessing, including image",
+    "conversion and vector file manipulation.")
+__uri__ = 'https://github.com/deparkes/OOMMFTools'
+__version__ = '2.0.1'
+__author__ = "Mark Mascaro and Contributors"
+__maintainer__ = "Duncan Parkes"
+__email__ = "deparkes@gmail.com"
+
+__license__ = ("OOMMFTools is free software; you can redistribute",
+            "it and/or modify it under the terms of the GNU ",
+            "General Public License as published by the Free ",
+            "Software Foundation, either version 2 of the License,"
+            "or (at your option) any later version.")
+__copyright__ = "2010 %s" % __author__
---
+++ packages/OOMMFTools/oommftools-2.0.1.dev20180308.tar.gz/oommftools-2.0.1.dev20180308/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+try:
+    import m2r
+    long_description = m2r.parse_from_file('README.md')
+except(IOError, ImportError):
+    print('Markdown not converted to rst')
+    long_description = open('README.md').read()
+
+about = {}
+with open("./oommftools/_about.py") as fp:
+    exec(fp.read(), about)
+    
+setup(
+   name=about['__title__'].lower(),
+   version=about['__version__'],
+   description=''.join(about['__summary__']),
+   long_description=long_description,
+   author=about['__author__'],
+   maintainer=about['__maintainer__'],
+   author_email=about['__email__'],
+   url=about['__uri__'],
+   packages=['oommftools'],  #same as name
+   install_requires=['wxpython', 'scipy', 'numpy'], #external packages as dependencies
+)
