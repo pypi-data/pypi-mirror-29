a9b3f84d95b599b5a683f51e5d358b79d170eda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:03 +0000

    Add package OASYS1-ALS-ShadowOui/0.0.2/OASYS1-ALS-ShadowOui-0.0.2.tar.gz

---
+++ packages/OASYS1-ALS-ShadowOui/OASYS1-ALS-ShadowOui-0.0.2.tar.gz/OASYS1-ALS-ShadowOui-0.0.2/orangecontrib/shadow/als/widgets/sources/__init__.py
@@ -0,0 +1,21 @@
+"""
+=========
+Associate
+=========
+
+Widgets for association rules.
+
+"""
+
+# Category description for the widget registry
+
+NAME = "ALS Sources"
+
+DESCRIPTION = "Widgets for Shadow Sources."
+
+BACKGROUND = "#fffeb3"
+#BACKGROUND = "#FFFF00"
+
+ICON = "icons/source.png"
+
+PRIORITY = 101
