1d39206b91aa6b8cde769aa0a469f9e5efd2dbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:20 +0000

    Add package my-test-hello-library/0.2/my-test-hello-library-0.2.tar.gz

---
+++ packages/my-test-hello-library/my-test-hello-library-0.2.tar.gz/my-test-hello-library-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='my-test-hello-library',
+      version='0.2',
+      description='sum two numbers',
+      url='https://github.com/username/repo',
+      author='Sebastian Garcia Valencia',
+      author_email='sebasgverde@gmail.com',
+      license='MIT',
+      packages=['mytesthellolibrary'],
+      install_requires=[])
