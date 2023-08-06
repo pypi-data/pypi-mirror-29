c1f67ea170e11c64681d17e85b789f308489bad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:09 +0000

    Add package honk/0.4/honk-0.4.tar.gz

---
+++ packages/honk/honk-0.4.tar.gz/honk-0.4/setup.py
@@ -0,0 +1,22 @@
+from __future__ import with_statement
+from setuptools import setup
+
+setup(
+    name='honk',
+    version='0.4',
+    description='Tool for dumping output of HTTP requests',
+    url='https://github.com/ryantownshend/honk',
+    author='Ryan Townshend',
+    author_email='citizen.townshend@gmail.com',
+    install_requires=[
+        'click>=6.7',
+        'click-log>=0.2.1',
+        'PyYAML>=3.12',
+    ],
+    py_modules=['honk'],
+    entry_points={
+        'console_scripts': [
+            'honk = honk:main'
+        ],
+    },
+)
