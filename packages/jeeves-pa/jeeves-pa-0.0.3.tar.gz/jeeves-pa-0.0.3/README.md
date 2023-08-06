da89b2690538a7af518ec0306fcb4b3de6d8a7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:57 +0000

    Add package jeeves-pa/0.0.3/jeeves-pa-0.0.3.tar.gz

---
+++ packages/jeeves-pa/jeeves-pa-0.0.3.tar.gz/jeeves-pa-0.0.3/README.md
@@ -0,0 +1,16 @@
+jeeves
+======
+
+Jeeves is a personal assistant similar to alexa/google, based on code from the jasper project.
+
+Usage
+-----
+
+You must have a populated project first. Start with: `python -m jeeves.populate`
+
+Run with `python -m jeeves`
+
+License
+-------
+
+See included LICENSE file for more details
---
+++ packages/jeeves-pa/jeeves-pa-0.0.3.tar.gz/jeeves-pa-0.0.3/setup.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+import os
+import msgfmt
+import setuptools
+from setuptools.command.bdist_egg import bdist_egg
+from distutils.command.build import build
+
+APPNAME = 'jeeves'
+
+
+class jeeves_bdist_egg(bdist_egg):
+    def run(self):
+        self.run_command('build_i18n')
+        setuptools.command.bdist_egg.bdist_egg.run(self)
+
+
+class jeeves_build_i18n(setuptools.Command):
+    description = 'compile PO translations to MO files'
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for root, _, filenames in os.walk(os.path.dirname(__file__)):
+            for po_filename in filenames:
+                filename, ext = os.path.splitext(po_filename)
+                if ext != '.po':
+                    continue
+                path = os.path.join(root, filename)
+                po_path = os.extsep.join([path, 'po'])
+                mo_path = os.extsep.join([path, 'mo'])
+                print('compile %s -> %s' % (po_path, mo_path))
+                with open(mo_path, 'wb') as f:
+                    f.write(msgfmt.Msgfmt(po_path).get())
+
+
+class jeeves_build(build):
+    sub_commands = build.sub_commands + [
+        ('build_i18n', None)
+    ]
+
+
+setuptools.setup(
+    name='jeeves-pa',
+    version='0.0.3',
+    url='http://github.com/narfman0/jeeves/',
+    license='MIT',
+
+    author='Jon Robison',
+    author_email='narfman0@gmail.com',
+
+    description=(
+        'Jeeves is an open source platform for developing ' +
+        'always-on, voice-controlled applications.'
+    ),
+
+    install_requires=[
+        'APScheduler',
+        'argparse',
+        'mock',
+        'python-slugify',
+        'pytz',
+        'PyYAML',
+        'requests'
+    ],
+
+    packages=[APPNAME],
+    package_data={
+        APPNAME: [
+            'data/audio/*.wav',
+            'data/locale/*.po',
+            'data/locale/*.mo',
+            'data/standard_phrases/*.txt',
+            '../plugins/*/*/*.py',
+            '../plugins/*/*/plugin.info',
+            '../plugins/*/*/*.txt',
+            '../plugins/*/*/locale/*.po',
+            '../plugins/*/*/locale/*.mo',
+            '../plugins/*/*/tests/*.py'
+        ]
+    },
+
+    data_files=[
+        ('share/doc/%s' % APPNAME, [
+            'AUTHORS.md',
+            'CONTRIBUTING.md',
+            'LICENSE.md',
+            'README.md'
+        ])
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'jeeves = %s.main:main' % APPNAME,
+        ],
+    },
+
+    cmdclass={
+        'bdist_egg': jeeves_bdist_egg,
+        'build': jeeves_build,
+        'build_i18n': jeeves_build_i18n,
+    },
+
+    test_suite='tests',
+)
