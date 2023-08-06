a567846866ab5dd38dd21f32b150d69524e70d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:20 +0000

    Add package networkutil/1.13.0/networkutil-1.13.0.tar.gz

---
+++ packages/networkutil/networkutil-1.13.0.tar.gz/networkutil-1.13.0/networkutil/ftp_helper.py
@@ -0,0 +1,50 @@
+
+import os
+from ftplib import FTP
+
+__author__ = u'Oli Davis'
+__copyright__ = u'Copyright (C) 2016 Oli Davis'
+
+
+class FTPHelper(FTP):
+
+    def __init__(self,
+                 *args,
+                 **kwargs):
+
+        FTP.__init__(self, *args, **kwargs)
+
+        self.buffer = []
+
+    def list_current_ftp_dir(self):
+
+        self.buffer = []
+
+        self.dir(self.__handle_list_response)
+
+        return self.buffer
+
+    def __handle_list_response(self,
+                               line):
+
+        filename = line.split(u' ').pop()
+
+        # Append separator for directories
+        if line.startswith(u'd'):
+            filename += u'/'
+
+        self.buffer.append(filename)
+
+    def download_file(self,
+                      filename,
+                      output_dir):
+
+        local_file = open(u'{d}{s}{f}'.format(d=output_dir,
+                                              s=os.sep,
+                                              f=filename), 'wb')
+
+        self.retrbinary(u'RETR {f}'.format(f=filename),
+                        local_file.write,
+                        1024)
+
+        local_file.close()
---
+++ packages/networkutil/networkutil-1.13.0.tar.gz/networkutil-1.13.0/networkutil/sniffer/ethernet/__init__.py
@@ -0,0 +1,5 @@
+
+from .ethernet import EthernetFrame
+from .vlan import VLANTag
+from ._constants import (ETHERNET_HEADER_LENGTH,
+                         ETHERNET_VLAN_LENGTH)
