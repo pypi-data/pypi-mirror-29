e68bc8b8d8cce6d6f7d2653ff0ba0b656735434b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:58 +0000

    Add package hikvision-recover/1.0/hikvision-recover-1.0.tar.gz

---
+++ packages/hikvision-recover/hikvision-recover-1.0.tar.gz/hikvision-recover-1.0/hikvision_recover/recover.py
@@ -0,0 +1,27 @@
+def rshift(val, n):
+    return (val % 0x100000000) >> n
+
+def char_shift(char):
+    c = ord(char)
+    shifts = {
+        range(0, 51): 33,
+        range(51, 53): 62,
+        range(53, 55): 47,
+        range(55, 57): 66
+    }
+
+    for r, value in shifts.items():
+        if c in r:
+            return chr(c + value)
+    else:
+        return char
+
+
+def get_code(serial, year, month, day):
+    serial = serial + year + month + day
+    magic_number = sum(ord(char) * index ^ index for index, char in enumerate(serial, 1))
+    magic_number *= 1751873395
+    magic_number = rshift(magic_number, 0)
+    recovery_code = ''.join(char_shift(digit) for digit in str(magic_number))
+
+    return recovery_code
