aedc226b2e32280ad983ed9bfc46c6fd4fa890ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:30 +0000

    Add package splits/0.1.5/splits-0.1.5.tar.gz

---
+++ packages/splits/splits-0.1.5.tar.gz/splits-0.1.5/splits/test.py
@@ -0,0 +1,11 @@
+from splits.s3 import *
+print(S3().getstring('s3://stitchfix.aa.warehouse/dw/prod/item/.metadata'))
+f = S3File('s3://stitchfix.aa.warehouse/dw/prod/item/.metadata')
+for i in f.readlines():
+    print(i)
+
+
+from r2d2 import df_to_hive
+import pandas as pd
+df = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
+df_to_hive(df, 'kurt', 'table7')
