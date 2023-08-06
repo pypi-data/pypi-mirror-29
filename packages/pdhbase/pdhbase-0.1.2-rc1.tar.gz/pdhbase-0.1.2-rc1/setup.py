5cad1eb57e28268d532cf8d6955e9a00bfb9d374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:18 +0000

    Add package pdhbase/0.1.2-rc1/pdhbase-0.1.2-rc1.tar.gz

---
+++ packages/pdhbase/pdhbase-0.1.2-rc1.tar.gz/pdhbase-0.1.2-rc1/setup.py
@@ -0,0 +1,20 @@
+__author__ = 'emmanuel'
+
+from distutils.core import setup
+
+setup(
+    name='pdhbase',
+    version='0.1.2-rc1',
+    author='Livingstone S E',
+    author_email='livingstone.s.e@gmail.com',
+    packages=['pdhbase'],
+    url='https://pypi.python.org/pypi/pdhbase',
+    license='LICENSE',
+    description='Read and write pandas dataframes to hbase.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "numpy >= 1.8.1",
+        "pandas >= 0.13.0",
+        "happybase >= 0.6"
+    ],
+)
