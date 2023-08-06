90bcd066f74340b726fb0c58a8b101eefbaca709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:53 +0000

    Add package sticks/0.0.7/sticks-0.0.7.tar.gz

---
+++ packages/sticks/sticks-0.0.7.tar.gz/sticks-0.0.7/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+with open("README.rst") as readme_file:
+    long_description = readme_file.read()
+
+setup(  
+    name="sticks",
+    packages=find_packages(),
+    version='0.0.7',
+    description="Simple game application.",
+    long_description=long_description,
+    author="ganeshhubale.",
+    author_email="ganeshhubale03@gmail.com",
+    url="https://github.com/ganeshhubale/game_of_sticks",
+    license="MIT",
+    py_modules=['sticks.__init__'],
+    namespace_packages=[],
+    entry_points={
+        'console_scripts': [
+            'sticks = sticks.__init__:main',
+        ],
+    },
+)
