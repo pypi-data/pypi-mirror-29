49367d436d181d84ca1e2a47dcaad16ef2ceb2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:08 +0000

    Add package threadspider/0.2.1/threadspider-0.2.1.tar.gz

---
+++ packages/threadspider/threadspider-0.2.1.tar.gz/threadspider-0.2.1/setup.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+__author__ = 'admin'
+# --------------------------------
+# Created by admin  on 2016/11/29.
+# ---------------------------------
+from setuptools import  setup,find_packages
+setup(
+    name="threadspider",
+    packages=["threadspider","threadspider.utils"],
+    version='0.2.1',
+    install_requires=["setuptools","psutil","selenium"],
+)
