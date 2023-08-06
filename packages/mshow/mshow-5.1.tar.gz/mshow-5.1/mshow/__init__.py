0e2c9d4acbfc083ee1b3c8b91ef7ff08eab90998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:56 +0000

    Add package mshow/5.1/mshow-5.1.tar.gz

---
+++ packages/mshow/mshow-5.1.tar.gz/mshow-5.1/mshow/__init__.py
@@ -0,0 +1,5 @@
+def info():
+    print("Library By : Nishant Singh\n")
+
+def show1():
+    print("Hello Guys !!\n Its a sample fun() for Suprath Technologies.\n")
---
+++ packages/mshow/mshow-5.1.tar.gz/mshow-5.1/mshow/text.py
@@ -0,0 +1,3 @@
+
+def show2():
+    print("Bye Guys !!\n Have a nice time.\n")
