ee2f93a1579798140cae00d90dc50226d8b1deda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:20 +0000

    Add package hawkeye/0.1dev/hawkeye-0.1dev.tar.gz

---
+++ packages/hawkeye/hawkeye-0.1dev.tar.gz/hawkeye-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='hawkeye',
+    version='0.1dev',
+    packages=['hawkeye',],
+    license='Apache License 2.0',
+    long_description=open('README.txt').read(),
+)
