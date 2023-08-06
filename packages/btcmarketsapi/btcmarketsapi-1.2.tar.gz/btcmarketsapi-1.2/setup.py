394464085b9b2e28b052786f303cfb723f4d228b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:39 +0000

    Add package btcmarketsapi/1.2/btcmarketsapi-1.2.tar.gz

---
+++ packages/btcmarketsapi/btcmarketsapi-1.2.tar.gz/btcmarketsapi-1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='btcmarketsapi',
+      version='1.2',
+      description='Python3 Wrapper for the BTCMarkets APIs',
+      url='https://github.com/SigmaAdvancedAnalytics/btcmarketsapi',
+      author='Joshua Barber',
+      author_email='contact@jbarber.info',
+      license='MIT',
+      packages=['btcmarketsapi'],
+      zip_safe=False)
