ecdd851717332f3202307d9dcef96c1ab8405a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:35 +0000

    Add package aztfgen/0.1.0/aztfgen-0.1.0.tar.gz

---
+++ packages/aztfgen/aztfgen-0.1.0.tar.gz/aztfgen-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='aztfgen',
+    version='0.1.0',
+    description='Generate terraform config from deployed azure resources.',
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    url='http://github.com/glenjamin/azure-terraform-generate',
+    author='Glen Mailer',
+    author_email='glen@stainlessed.co.uk',
+    license='MIT',
+    packages=['aztfgen'],
+    python_requires='>=3.5',
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': ['aztfgen=aztfgen:main'],
+    }
+)
