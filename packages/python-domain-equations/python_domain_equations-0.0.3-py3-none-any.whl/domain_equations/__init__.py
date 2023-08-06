2373f1bc309fd654b802741506f2b2a161d3d897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:34 +0000

    Add package python-domain-equations/0.0.3/python_domain_equations-0.0.3-py3-none-any.whl

---
+++ packages/python-domain-equations/python_domain_equations-0.0.3-py3-none-any.whl/domain_equations/__init__.py
@@ -0,0 +1,14 @@
+"""
+@copyright: 2018 by Pauli Rikula <pauli.rikula@gmail.com>
+
+@license: MIT <http://www.opensource.org/licenses/mit-license.php>
+
+
+Represent and generate domain model classes from category-like equations.
+
+"""
+
+from .property_graph import PropertyGraph
+
+
+__all__ = ['PropertyGraph']
