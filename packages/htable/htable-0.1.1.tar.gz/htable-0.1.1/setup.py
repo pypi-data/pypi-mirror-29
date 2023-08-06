34f500b949a02d850a27f3f315e2a19906ab8d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:49 +0000

    Add package htable/0.1.1/htable-0.1.1.tar.gz

---
+++ packages/htable/htable-0.1.1.tar.gz/htable-0.1.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+desc = r'Table-like formats to latex \table[H]'
+
+setup(
+    name='htable',
+    version='0.1.1',
+    description=desc,
+    long_description=desc,
+    url='https://github.com/cdown/htable',
+    license='Public Domain',
+
+    author='Chris Down',
+    author_email='chris@chrisdown.name',
+
+    py_modules=['htable'],
+
+    classifiers=[
+        'License :: Public Domain',
+        'Programming Language :: Python :: 3',
+    ],
+
+    install_requires=['tabulate'],
+)
