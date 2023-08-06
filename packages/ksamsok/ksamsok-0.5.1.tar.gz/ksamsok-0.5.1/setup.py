8c5e4c636155385dd90cb89afd5b99ff60a48a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:09 +0000

    Add package ksamsok/0.5.1/ksamsok-0.5.1.tar.gz

---
+++ packages/ksamsok/ksamsok-0.5.1.tar.gz/ksamsok-0.5.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+version = '0.5.1'
+repo = 'ksamsok-py'
+
+setup(
+  name = 'ksamsok',
+  packages = ['ksamsok'],
+  install_requires=['lxml', 'requests'],
+  version = version,
+  description = 'API library for the cultural heritage K-Samsök(SOCH) aggregator.',
+  author = 'Albin Larsson',
+  author_email = 'albin.post@gmail.com',
+  url = 'https://github.com/Abbe98/' + repo,
+  download_url = 'https://github.com/Abbe98/' + repo + '/tarball/' + version,
+  keywords = ['SOCH', 'K-Samsök', 'heritage', 'cultural', 'API'],
+  classifiers = [],
+)
