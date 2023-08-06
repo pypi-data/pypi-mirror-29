acac7b0229a57d5542bda5b471c4b86914dad3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:24 +0000

    Add package arfx/2.2.6/arfx-2.2.6-cp36-cp36m-macosx_10_13_x86_64.whl

---
+++ packages/arfx/arfx-2.2.6-cp36-cp36m-macosx_10_13_x86_64.whl/arfx/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+"""
+arfx is a tool for copying data to and from ARF files. It is intended for simple
+operations involving single channel datasets, though it can also be used to copy
+entire entries between ARF files.
+"""
+
+from .core import __version__
+__all__ = ['arfx']
