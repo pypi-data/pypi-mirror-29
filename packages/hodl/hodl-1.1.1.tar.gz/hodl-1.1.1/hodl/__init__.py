3931ec9189ab31fbaa7d4c17c85fc214f143b9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:38 +0000

    Add package hodl/1.1.1/hodl-1.1.1.tar.gz

---
+++ packages/hodl/hodl-1.1.1.tar.gz/hodl-1.1.1/hodl/__init__.py
@@ -0,0 +1,337 @@
+#!bin/python
+from __future__ import print_function, unicode_literals
+
+try:
+    from urllib.request import urlopen, Request, HTTPError
+    import configparser as cp
+except ImportError:
+    import ConfigParser as cp
+    from urllib2 import urlopen, Request, HTTPError
+import argparse
+from colorama import init, Fore, Back
+import json
+from multiprocessing.pool import ThreadPool
+import os
+
+# constants
+description = "Your friendly, no-nonsense tool to " \
+              "instantaneously check cryptocurrency prices"
+epilog = "hodl.py: helping you HODL one day at a time :)"
+__version__ = "v.1.1.0"
+cryptos = ['btc', 'bch', 'eth', 'ltc', 'xmr', 'xrp']
+iso4217codes = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG",
+                "AZN",
+                "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB",
+                "BRL",
+                "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP",
+                "CNY",
+                "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP",
+                "DZD",
+                "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GGP",
+                "GHS",
+                "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG",
+                "HUF",
+                "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD",
+                "JOD",
+                "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD",
+                "KZT",
+                "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA",
+                "MKD",
+                "MMK", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR",
+                "MZN",
+                "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN",
+                "PGK",
+                "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF",
+                "SAR",
+                "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SPL",
+                "SRD",
+                "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP",
+                "TRY",
+                "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS",
+                "VEF",
+                "VND", "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER",
+                "ZAR",
+                "ZMW", "ZWD"]
+
+# load config file
+config_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                               'conf', 'config.ini')
+config = cp.ConfigParser()
+config.read(config_filename)
+
+# initialise colorama and thread pool
+init(autoreset=True)
+pool = ThreadPool(processes=6)
+
+
+def binance_convert_crypto(frm="LTC", to="BTC"):
+    """Returns the conversion price from one cypto to another
+    using the binance API"""
+    try:
+        url = "https://api.binance.com/api/v3/ticker/price?symbol={}{}".format(
+            frm.upper(), to.upper())
+        req = Request(url)
+        r = urlopen(req).read()
+        data = json.loads(r.decode("utf-8"))
+        return "1 {} = {} {}".format(frm.upper(), data["price"], to.upper())
+    except HTTPError:
+        try:
+            url = "https://api.binance.com/api/v3/" \
+                  "ticker/price?symbol={}{}".format(to.upper(), frm.upper())
+            req = Request(url)
+            r = urlopen(req).read()
+            data = json.loads(r.decode("utf-8"))
+            return "1 {} = {} {}".format(frm.upper(),
+                                         1.0 / float(data["price"]), to.upper())
+        except HTTPError:
+            return "[*] error, check you are using correct crypto symbols"
+
+
+def coinbase_convert_crypto(frm="LTC", to="BTC"):
+    """Returns the conversion price between the supplied crypto
+    and fiat currencies using the coinbase API"""
+    try:
+        if frm in ['btc', 'bch', 'eth', 'ltc'] and \
+                to in ['btc', 'bch', 'eth', 'ltc']:
+            url = "https://api.coinbase.com/v2/prices/{}-{}/spot"
+            req = Request(url.format(frm.upper(), "USD"))
+            r = urlopen(req).read()
+            data = json.loads(r.decode("utf-8"))
+            frm_price = float(data["data"]["amount"])
+            req = Request(url.format(to.upper(), "USD"))
+            r = urlopen(req).read()
+            data = json.loads(r.decode("utf-8"))
+            to_price = float(data["data"]["amount"])
+            return "1 {} = {} {}".format(frm.upper(),
+                                         round(frm_price /
+                                               to_price, 8), to.upper())
+        else:
+            return "1 {} = {} {}".format(frm.upper(),
+                                         str(binance_convert_crypto(frm, to)),
+                                         to.upper())
+    except (HTTPError, IndexError):
+        return "[*] error, check that you are using correct crypto symbols"
+
+
+def get_price(crypto="BTC", fiat=config.get("currency", "FIAT")):
+    """Returns the conversion price between the supplied
+    crypto and fiat currencies"""
+    try:
+        if crypto in ['btc', 'bch', 'eth', 'ltc']:
+            url = 'https://api.coinbase.com/v2/prices/{}-{}/spot'.format(
+                crypto.upper(),
+                fiat)
+            req = Request(url)
+            r = urlopen(req).read()
+            data = json.loads(r.decode('utf-8'))
+            return "1 {} = {} {}".format(data['data']['base'],
+                                         data['data']['amount'],
+                                         data['data']['currency'])
+        else:
+            return "1 {0} = {1:.2f} {2}".format(crypto.upper(),
+                                                get_crypto_price(crypto, fiat),
+                                                fiat)
+    except (HTTPError, IndexError):
+        return "[*] error, check you are using correct crypto and fiat symbols"
+
+
+def get_crypto_price(crypto, fiat):
+    """Helper function to convert any cryptocurrency to fiat"""
+    converted_btc_value = float(binance_convert_crypto(
+        crypto, "BTC").split('=')[1].strip().split()[0])
+
+    # grab latest bitcoin price
+    btc_price = float(get_price("btc", fiat).split('=')[1].strip().split()[0])
+    # converted_btc_value * latest reading
+    return converted_btc_value * btc_price
+
+
+def get_majors(fiat=config.get("currency", "FIAT")):
+    """Returns the conversion prices for all supported crypto-currencies"""
+    reports = []
+    for crypto in cryptos:
+        async_result = pool.apply_async(get_price, (crypto.lower(), fiat,))
+        reports.append(async_result.get())
+    return reports
+
+
+def adjust_readings_to_new_fiat(fiat):
+    """Helper function to bulk update readings in the configuration file when
+    a new default fiat is set"""
+    for data in get_majors(fiat):
+        record_data('readings',
+                    data.split('=')[0].strip().split(' ')[1].lower(),
+                    data.split('=')[1].strip().split()[0])
+    print("[*] success: {} configured as standard fiat".format(fiat))
+
+
+def set_fiat(fiat):
+    """Sets the default fiat currency for the user"""
+    try:
+        print("[*] updating standard fiat to {} ...".format(fiat))
+        adjust_readings_to_new_fiat(fiat)
+        config.set('currency', 'FIAT', fiat)
+        with open(config_filename, 'w') as configfile:
+            config.write(configfile)
+    except Exception as e:
+        print("[*] error: while configuring fiat: report: ", e)
+
+
+def record_data(section, base, amount):
+    """Helper function to sanitise and save data to config.ini"""
+    try:
+        if float(amount) >= 0:
+            config.set(section, base, str(amount))
+            with open(config_filename, 'w') as configfile:
+                config.write(configfile)
+            if section == "portfolio":
+                print("[*] {} portfolio value set at {} coins".format(
+                    base.upper(), amount))
+        else:
+            print(
+                "HODL: error: invalid choice: {} "
+                "(please supply a positive number)".format(
+                    amount))
+    except ValueError:
+        print("HODL: error: invalid choice: {} (please supply a number)".format(
+            amount))
+
+
+def print_portfolio_value(base=None):
+    """Prints the value of the user's portfolio holdings"""
+    portfolio_currency = config.get("currency", "FIAT")
+    if base:
+        holding = float(config.get("portfolio", base)) * float(
+            config.get("readings", base))
+        print("[*] {} portfolio value: ".format(base.upper()) +
+              "{0:.2f} ".format(holding) +
+              "{}".format(portfolio_currency))
+    else:
+        for base in cryptos:
+            holding = float(config.get("portfolio", base)) * float(
+                config.get("readings", base))
+            print("[*] {} portfolio value: ".format(base.upper()) +
+                  "{0:.2f} ".format(holding) +
+                  "{}".format(portfolio_currency))
+
+
+def print_report(report, alignment=0, first_run=False):
+    """Prints a crypto-currency exchange rate report"""
+    i = alignment - len(report) + 2
+    try:
+        if "[*] error, check you are using correct " \
+           "crypto and fiat symbols" in report:
+            print(report)
+        else:
+            base = report.split("=")[0].split(" ")[1]
+            current_amount = float(report.split("=")[1].split(" ")[1])
+            # recover cached record
+            previous_amount = float(config.get("readings", base.upper()))
+            if current_amount > previous_amount and not first_run:
+                change = Fore.WHITE + " ".rjust(
+                    i) + Back.GREEN + " {0:.2f}%".format(
+                    100.0 * current_amount / previous_amount - 100)
+                print(report + change)
+            elif current_amount < previous_amount and not first_run:
+                change = Fore.WHITE + " ".rjust(
+                    i) + Back.RED + "{0:.2f}%".format(
+                    100.0 * current_amount / previous_amount - 100)
+                print(report + change)
+            elif current_amount == previous_amount and not first_run:
+                change = Fore.WHITE + " ".rjust(i) + Back.BLUE + " 0.00%"
+                print(report + change)
+            elif first_run:
+                print(report)
+            record_data("readings", base, str(current_amount))
+    except ZeroDivisionError:
+        # all readings in the config are set to 0, meaning the user is running
+        # the script for the first time. We can thus skip calculating the
+        # percentage change in the report by setting first_run to True
+        base = report.split("=")[0].split(" ")[1]
+        current_amount = float(report.split("=")[1].split(" ")[1])
+        record_data("readings", base, str(current_amount))
+        print_report(report=report, first_run=True)
+
+
+def main():
+    """Main program entry point; parses and interprets command line arguments"""
+    parser = argparse.ArgumentParser(prog="HODL", description=description,
+                                     epilog=epilog)
+    group = parser.add_mutually_exclusive_group()
+    group_2 = parser.add_mutually_exclusive_group()
+    group_2.add_argument("-cp", "--configure_portfolio",
+                         help="configure portfolio command",
+                         nargs=2, metavar=('CRYPTOCURRENCY', 'AMOUNT'))
+    group_2.add_argument("-vp", "--view_portfolio",
+                         help="view portfolio command", const='all', nargs="?")
+    parser.add_argument('-c', '--crypto',
+                        help='set the crypto-currency you wish to price check',
+                        choices=cryptos)
+    group.add_argument('-f', '--fiat',
+                       help='set fiat conversion currency',
+                       choices=iso4217codes,
+                       metavar="ISO 4217 CODE")
+    group.add_argument('-sf', '--set_fiat',
+                       help='set your preferred fiat currency',
+                       choices=iso4217codes,
+                       metavar="ISO 4217 CODE")
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s {version}'.format(
+                            version=__version__))
+    parser.add_argument('-cc', '--convert_crypto',
+                        help="convert one crypto to another",
+                        choices=cryptos,
+                        nargs=2)
+    args = parser.parse_args()
+
+    if args.crypto and args.fiat:
+        print(get_price(args.crypto, args.fiat))
+    elif (args.set_fiat and args.crypto) or (args.crypto and args.set_fiat):
+        set_fiat(args.set_fiat)
+        report = get_price(args.crypto)
+        print_report(report, len(report))
+    elif args.crypto:
+        print(get_price(args.crypto))
+    elif args.set_fiat:
+        set_fiat(args.set_fiat)
+    elif args.configure_portfolio:
+        if args.configure_portfolio[0] in cryptos:
+            record_data("portfolio", args.configure_portfolio[0],
+                        args.configure_portfolio[1])
+        else:
+            print(
+                "HODL: error: argument -cp/--configure_portfolio: invalid choice:"
+                " '{}' (choose from {})".format(
+                    args.configure_portfolio[0],
+                    [str(crypto) for crypto in cryptos]))
+    elif args.view_portfolio:
+        if args.view_portfolio == "all":
+            print_portfolio_value()
+        else:
+            if args.view_portfolio in cryptos:
+                print_portfolio_value(args.view_portfolio)
+            else:
+                print(
+                    "HODL: error: argument -vp/--view_portfolio: invalid choice:"
+                    " '{}' (choose from {})".format(
+                        args.view_portfolio,
+                        [str(crypto) for crypto in cryptos]))
+    elif args.convert_crypto:
+        if args.convert_crypto[0] in cryptos \
+                and args.convert_crypto[1] in cryptos:
+            print(coinbase_convert_crypto(args.convert_crypto[0],
+                                          args.convert_crypto[1]))
+        else:
+            print("HODL: error: argument -cc/--convert_crypto: invalid choice:"
+                  " '{}' (choose from {})".format(
+                args.view_portfolio, [str(crypto) for crypto in cryptos]))
+
+    else:
+        reports = get_majors()
+        i = len(max(reports, key=len))
+        for report in reports:
+            print_report(report, i)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/hodl/hodl-1.1.1.tar.gz/hodl-1.1.1/setup.py
@@ -0,0 +1,113 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+long_description = """
+HODL is your friendly, no-nonsense tool to instantaneously check cryptocurrency
+prices on the command line, helping you HODL one day at a time :)
+
+Please visit the project's main documentation page, found at:
+https://github.com/errantbot/hodl
+"""
+
+setup(
+    name='hodl',
+    version='1.1.1',
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'venv']),
+    install_requires=['colorama'],  # Required,
+
+    url='https://github.com/errantbot/hodl',
+    license='MIT',
+    author='errantbot',
+    author_email='errantbot@tutanota.com',
+    description='A  friendly, no-nonsense tool to instantaneously check '
+                'cryptocurrency prices.',
+    # This is an optional longer description of your project that represents
+    # the body of text which users will see when they visit PyPI.
+    #
+    # Often, this is the same as your README, so you can just read it in from
+    # that file directly (as we have already done above)
+    #
+    # This field corresponds to the "Description" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#description-optional
+    long_description=long_description,  # Optional
+
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: System Administrators',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Other/Nonlisted Topic',
+        'Topic :: Terminals',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+
+        # project language
+        'Natural Language :: English',
+
+        # compatible os
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+    ],
+    # This field adds keywords for your project which will appear on the
+    # project page. What does your project relate to?
+    #
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords="cryptocurrencies exchange-rates coinbase command-line-tool hodl bitcoin bitcoin-api litecoin "
+             "litecoin-price bitcoin-price ethereum bitcoin-cash",
+    # Optional
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    #
+    # If using Python 2.6 or earlier, then these have to be included in
+    # MANIFEST.in as well.
+    package_data={  # Optional
+        'hodl': ['conf/*', ],
+    },
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'hodl=hodl:main',
+        ], },
+)
