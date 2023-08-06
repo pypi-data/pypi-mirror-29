aa3c3ff53fe4c76ad09a4875df9472db82bb00da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:41 +0000

    Add package sockjs/0.7.1/sockjs-0.7.1.tar.gz

---
+++ packages/sockjs/sockjs-0.7.1.tar.gz/sockjs-0.7.1/tests/test_transport_xhrstreaming.py
@@ -0,0 +1,32 @@
+from unittest import mock
+
+import pytest
+
+from sockjs.transports import xhrstreaming
+
+
+@pytest.fixture
+def make_transport(make_request, make_fut):
+    def maker(method='GET', path='/', query_params={}):
+        manager = mock.Mock()
+        session = mock.Mock()
+        session._remote_closed = make_fut(1)
+        request = make_request(method, path, query_params=query_params)
+        request.app.freeze()
+        return xhrstreaming.XHRStreamingTransport(manager, session, request)
+
+    return maker
+
+
+async def test_process(make_transport, make_fut):
+    transp = make_transport()
+    transp.handle_session = make_fut(1)
+    resp = await transp.process()
+    assert transp.handle_session.called
+    assert resp.status == 200
+
+
+async def test_process_OPTIONS(make_transport):
+    transp = make_transport(method='OPTIONS')
+    resp = await transp.process()
+    assert resp.status == 204
