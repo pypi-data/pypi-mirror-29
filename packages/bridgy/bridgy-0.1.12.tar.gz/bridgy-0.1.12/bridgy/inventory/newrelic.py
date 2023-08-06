c82f9cb2df8ce1cb8e6570d0c57ceacd2a7ceea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:28 +0000

    Add package bridgy/0.1.12/bridgy-0.1.12.tar.gz

---
+++ packages/bridgy/bridgy-0.1.12.tar.gz/bridgy-0.1.12/bridgy/inventory/newrelic.py
@@ -0,0 +1,55 @@
+import os
+import json
+import requests
+try:
+    from urllib.parse import quote_plus
+except ImportError:
+    from urllib import quote_plus
+
+from bridgy.inventory.source import InventorySource, Instance
+
+class NewRelicInventory(InventorySource):
+
+    name = 'newrelic'
+    url = "https://insights-api.newrelic.com/v1/accounts/{}/query?nrql={}"
+
+    def __init__(self, account_number, insights_query_api_key, data_path, proxies=None, **kwargs):
+        if 'name' not in kwargs:
+            kwargs['name'] = "acct:"+str(account_number)
+
+        super(NewRelicInventory, self).__init__(account_number, insights_query_api_key, data_path, proxies, **kwargs)
+
+        self.account_number = account_number
+        self.insights_query_api_key = insights_query_api_key
+        self.data_file = os.path.join(data_path, '%s.json' % str(account_number))
+        self.query = quote_plus("SELECT entityName, fullHostname, hostname, ipV4Address from NetworkSample LIMIT 999")
+        if proxies:
+            self.proxies = proxies
+        else:
+            self.proxies = {}
+
+    def update(self):
+        headers = {'X-Query-Key': self.insights_query_api_key,
+                   'Accept': 'application/json'}
+
+        response = requests.get(NewRelicInventory.url.format(self.account_number, self.query),
+                                headers=headers,
+                                proxies=self.proxies)
+
+        with open(self.data_file, 'w') as data_file:
+            data_file.write(response.text)
+
+    def instances(self):
+        instances = set()
+        with open(self.data_file, 'r') as data_file:
+            data = json.load(data_file)
+
+        for results_dict in data['results']:
+            for event_dict in results_dict['events']:
+                hostname = event_dict['hostname']
+                address = event_dict['ipV4Address'].strip().split("/")[0]
+                if hostname == None:
+                    hostname = address
+                instances.add(Instance(hostname, address, None, self.name))
+
+        return list(instances)
---
+++ packages/bridgy/bridgy-0.1.12.tar.gz/bridgy-0.1.12/bridgy/utils.py
@@ -0,0 +1,53 @@
+import logging
+import uuid
+import sys
+import os
+
+from blessings import Terminal
+
+term = Terminal()
+logger = logging.getLogger()
+
+
+class UnsupportedPlatform(Exception): pass
+
+def platform():
+
+    if 'linux' in sys.platform:
+        return 'linux'
+    elif 'darwin' in  sys.platform:
+        return 'osx'
+    elif 'win' in sys.platform:
+        return 'windows'
+    return sys.platform
+
+class SupportedPlatforms(object):
+    def __init__(self, *platforms):
+        self.platforms = platforms
+
+    def __call__(self, func):
+        decorator_self = self
+        def wrapper(*args, **kwargs):
+            decorator_self.check_supported_platforms()
+            func(*args,**kwargs)
+        return wrapper
+
+    def check_supported_platforms(self):
+        normalized = platform()
+
+        if normalized not in self.platforms:
+            raise UnsupportedPlatform('Unsupported platform (%s)' % normalized)
+
+def shortUuid():
+    return str(uuid.uuid4())[:8]
+
+def memoize(f):
+    class memodict(dict):
+        def __init__(self, f):
+            self.f = f
+        def __call__(self, *args):
+            return self[args]
+        def __missing__(self, key):
+            ret = self[key] = self.f(*key)
+            return ret
+    return memodict(f)
