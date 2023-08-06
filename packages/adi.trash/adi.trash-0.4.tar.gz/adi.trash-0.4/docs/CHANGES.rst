b1a168773ad70c64acac6e05968d5959f216ab18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:01 +0000

    Add package adi.trash/0.4/adi.trash-0.4.tar.gz

---
+++ packages/adi.trash/adi.trash-0.4.tar.gz/adi.trash-0.4/docs/CHANGES.rst
@@ -0,0 +1,29 @@
+Changelog for adi.trash
+=======================
+ 
+
+0.4 (2018-03-02)
+----------------
+
+- 'ParseResult' object has no attribute 'endswith', fix error.  [amariscal]
+
+
+0.3 (2017-07-27)
+----------------
+
+- Regard URL can contain parameters, fixes #3. [idgserpro]
+
+- Add plone.api as a dependency. [idgserpro]
+
+
+0.2 (2017-04-12)
+----------------
+
+- Merge PR from idgserpro, correct repo-URL. [ida]
+
+
+0.1 (2015-11-01)
+---------------
+
+- Initial commit.
+    
---
+++ packages/adi.trash/adi.trash-0.4.tar.gz/adi.trash-0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.4'
+
+setup(name='adi.trash',
+      version=version,
+      description="Plone addon, changing deletion-behaviour: Moves items into a trash-folder, instead of deleting them.",
+      long_description=open("README.rst").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Ida Ebkes',
+      author_email='contact@ida-ebkes.eu',
+      url='https://github.com/ida/adi.trash',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['adi'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'plone.api',
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
