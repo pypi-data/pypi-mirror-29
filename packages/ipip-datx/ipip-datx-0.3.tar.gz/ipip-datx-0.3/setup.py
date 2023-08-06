1d3f89649a7da17af795bddaef0aeb609743feb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:49 +0000

    Add package ipip-datx/0.3/ipip-datx-0.3.tar.gz

---
+++ packages/ipip-datx/ipip-datx-0.3.tar.gz/ipip-datx-0.3/setup.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+import io
+from setuptools import setup
+
+with io.open('README.md', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+setup(
+    name='ipip-datx',
+    version=0.3,
+    description=(
+        'IPIP.net官方支持维护的IP数据库datx格式解析代码库'
+    ),
+    long_description=readme,
+    author='IPIP.net',
+    author_email='frk@ipip.net',
+    maintainer='frk',
+    maintainer_email='frk@ipip.net',
+    license='Apache License Version 2.0',
+    packages=['datx'],
+    platforms=["all"],
+    url='https://github.com/ipipdotnet/datx-python',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
