f73115b3ce22b2c4d70fbbe11a903cf10d11d9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:06 +0000

    Add package kytos/2018.1b1/kytos-2018.1b1.tar.gz

---
+++ packages/kytos/kytos-2018.1b1.tar.gz/kytos-2018.1b1/kytos/core/constants.py
@@ -0,0 +1,10 @@
+"""Module with the main constants from Kytos.
+
+This constantes may be overridden by values passed on the controller
+instantiation.
+"""
+# Min time (seconds) to send a new EchoRequest to a switch
+POOLING_TIME = 3
+CONNECTION_TIMEOUT = 70
+#: FLOOD_TIMEOUT in microseconds
+FLOOD_TIMEOUT = 100000
