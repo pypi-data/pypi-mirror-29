2c9201ccbe91600f7e6f35c860af346e342b58e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:35 +0000

    Add package ladon/1.0.0/ladon-1.0.0.tar.gz

---
+++ packages/ladon/ladon-1.0.0.tar.gz/ladon-1.0.0/examples/services/start.py
@@ -0,0 +1,4 @@
+#!/home/jakob/dev/ladon/venv/bin/python
+# EASY-INSTALL-SCRIPT: 'ladon==1.0.0','ladon-3.5-ctl'
+__requires__ = 'ladon==1.0.0'
+__import__('pkg_resources').run_script('ladon==1.0.0', 'ladon-3.5-ctl')
