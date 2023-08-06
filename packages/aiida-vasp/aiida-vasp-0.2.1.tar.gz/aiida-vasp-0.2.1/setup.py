c87dc78356d0711bb4262092f834004014826649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:26 +0000

    Add package aiida-vasp/0.2.1/aiida-vasp-0.2.1.tar.gz

---
+++ packages/aiida-vasp/aiida-vasp-0.2.1.tar.gz/aiida-vasp-0.2.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Install the aiida_vasp python package.
+
+usage: pip install -e .[graphs]
+"""
+
+import os
+import json
+from setuptools import setup, find_packages
+
+SETUP_JSON_PATH = os.path.join(os.path.dirname(__file__), 'setup.json')
+
+if __name__ == '__main__':
+    with open(SETUP_JSON_PATH, 'r') as info:
+        SETUP_KWARGS = json.load(info)
+    setup(packages=find_packages(exclude=['aiida']), **SETUP_KWARGS)
