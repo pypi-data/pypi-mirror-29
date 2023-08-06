9ab379a4d5a451e5f9d901b32130c25b24540430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:10 +0000

    Add package expy/0.9.11.2/expy-0.9.11.2.tar.gz

---
+++ packages/expy/expy-0.9.11.2.tar.gz/expy-0.9.11.2/expy/test/send_trigger.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+##### package test #####
+import sys
+sys.path = ['././']+sys.path
+################
+
+from expy import *  # Import the needed functions
+
+'Parallel Port'
+start(port=0xC100)
+sendTrigger(0, mode='P')
+
+'Serial Port'
+start(port='COM1')
+sendTrigger('something', mode='S')
+
+# import serial
+# ser = serial.Serial(baudrate=115200)
+# ser.port = 'COM1' # set the port
+# try:
+#     ser.open()
+# except:
+#     print('Could not open port')
+
+# ser.write(b'something') # send a string directly
+
+# tg = 'something'
+# ser.write(bytes(tg,encoding='utf8')) # send a string which might change
+
+# n=int('0b00010001',2)
+# ser.write(n.to_bytes((n.bit_length()+7)//8, 'big')) # send a binary code
