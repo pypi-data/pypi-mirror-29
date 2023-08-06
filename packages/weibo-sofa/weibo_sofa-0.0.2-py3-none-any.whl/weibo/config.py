665a19cdf22d7b18fff0461e5d62837d0ab55f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:40 +0000

    Add package weibo-sofa/0.0.2/weibo_sofa-0.0.2-py3-none-any.whl

---
+++ packages/weibo-sofa/weibo_sofa-0.0.2-py3-none-any.whl/weibo/config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+""" 
+@author: W@I@S@E 
+@contact: wisecsj@gmail.com 
+@site: https://wisecsj.github.io 
+@file: config.py 
+@time: 2018/2/19 20:20 
+"""
+
+import sys
+
+LOGFILE_PATH = 'weibo.log'
+
+# logger Stream handler level
+DEBUG = False
+
+# comment content
+CONTENT = '沙发'
+
+# sleep after a loop
+INTERVAL = (1, 1.5, 2)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 需要抢其沙发的用户uid
+# 测试号；鱼；PUBG；angrybb
+UID = ['5860185885', '2914927101', '6037906900', '1903031221']
