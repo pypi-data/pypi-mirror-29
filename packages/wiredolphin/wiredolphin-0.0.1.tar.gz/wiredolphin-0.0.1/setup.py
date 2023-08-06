9d6a654b195312686bd27d39e2b7d011b45ed8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:29 +0000

    Add package wiredolphin/0.0.1/wiredolphin-0.0.1.tar.gz

---
+++ packages/wiredolphin/wiredolphin-0.0.1.tar.gz/wiredolphin-0.0.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='wiredolphin',
+      version='0.0.1',
+      description='Network capture.',
+      author='laixintao',
+      author_email='laixintao1995@163.com',
+      url='https://www.python.org/',
+      packages=['wiredolphin'],
+     )
---
+++ packages/wiredolphin/wiredolphin-0.0.1.tar.gz/wiredolphin-0.0.1/wiredolphin/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from prompt_toolkit import prompt
+
+if __name__ == '__main__':
+    answer = prompt('Give me some input: ')
+    print('You said: %s' % answer)
