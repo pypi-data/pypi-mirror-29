6e16ee7b4a099576e035d5c24e48ac80c306148d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:32 +0000

    Add package python-rsdclient/0.1.3/python_rsdclient-0.1.3-py2-none-any.whl

---
+++ packages/python-rsdclient/python_rsdclient-0.1.3-py2-none-any.whl/rsdclient/osc/v1/storage_service.py
@@ -0,0 +1,52 @@
+#   Copyright 2017 Intel, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+import json
+
+from rsdclient.common import command
+
+
+class ListStorageServices(command.Command):
+    """List all storage services."""
+
+    _description = "List all storage services"
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+        rsd_client = self.app.client_manager.rsd
+        storage_service_list = rsd_client.storage_service.list()
+        print(storage_service_list)
+
+
+class ShowStorageServices(command.Command):
+    """Show storage service details"""
+
+    _description = "Show storage service details"
+
+    def get_parser(self, prog_name):
+        parser = super(ShowStorageServices, self).get_parser(prog_name)
+        parser.add_argument(
+            'storageservice',
+            metavar='<storage service>',
+            help='ID of the storage service.')
+
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+        rsd_client = self.app.client_manager.rsd
+        storage_detail = rsd_client.storage_service.show(
+            parsed_args.storageservice)
+        print("{0}".format(json.dumps(storage_detail, indent=2)))
---
+++ packages/python-rsdclient/python_rsdclient-0.1.3-py2-none-any.whl/rsdclient/v1/storage_service.py
@@ -0,0 +1,59 @@
+#   Copyright 2017 Intel, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+import os
+
+from rsdclient.common import base
+from rsdclient.common import utils
+
+
+class StorageServiceManager(base.Manager):
+    _resource_name = 'storages'
+
+    def __init__(self, *args, **kwargs):
+        super(StorageServiceManager, self).__init__(*args, **kwargs)
+        self.storage_service_path = self.client._storage_service_path
+
+    def _get_storage_service_uri(self, storage_service_id):
+        return os.path.join(self.storage_service_path, storage_service_id)
+
+    def list(self):
+        storage_service_collection = \
+            self.client.get_storage_service_collection()
+        storages = [utils.extract_attr(self.client.
+                    get_storage_service(storage_uri))
+                    for storage_uri in storage_service_collection.
+                    members_identities]
+        storage_info_table = utils.print_dict(
+            storages, ["Identity", "Name", "Description"])
+        return storage_info_table
+
+    def show(self, storage_id):
+        storage = self.client.get_storage_service(
+            self._get_storage_service_uri(storage_id))
+        storage_dict = utils.extract_attr(storage)
+
+        # Append sub-items attributions
+        storage_dict['remote_targets'] = [
+            utils.extract_attr(item)
+            for item in storage.remote_targets.get_members()]
+        storage_dict['physical_drives'] = [
+            utils.extract_attr(item)
+            for item in storage.physical_drives.get_members()]
+        storage_dict['logical_drives'] = [
+            utils.extract_attr(item)
+            for item in storage.logical_drives.get_members()]
+
+        return storage_dict
