5b08879d58ffe1a4e50915fa438b560336711ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:04 +0000

    Add package chengxiang/1.0.2/chengxiang-1.0.2.tar.gz

---
+++ packages/chengxiang/chengxiang-1.0.2.tar.gz/chengxiang-1.0.2/setup.py
@@ -0,0 +1,13 @@
+#coding=utf-8  
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name = 'chengxiang',
+    version = '1.0.2',
+    py_modules = ['cxfirsttry'],
+    author = 'cx_fooxcool',
+    author_email = 'cxmyforevermemory@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
