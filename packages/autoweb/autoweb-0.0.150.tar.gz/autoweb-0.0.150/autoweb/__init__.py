8de7178d03b4cd0fab5ed650a68995d2592d0c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:03 +0000

    Add package autoweb/0.0.150/autoweb-0.0.150.tar.gz

---
+++ packages/autoweb/autoweb-0.0.150.tar.gz/autoweb-0.0.150/autoweb/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.0.150'
+
+from autoweb.test import test
+
+# import autoweb.test
+#
+# def test():
+#     print("Hello, I'm amoblin.")
+#
+#
+# if __name__ == '__main__':
+#     test()
+
+# pip install --upgrade -t . autoweb
+
+__all__ = ['test']
