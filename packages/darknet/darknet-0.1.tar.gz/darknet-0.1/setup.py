daee0daf23c47d1aa713392794aee7952647b478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:33 +0000

    Add package darknet/0.1/darknet-0.1.tar.gz

---
+++ packages/darknet/darknet-0.1.tar.gz/darknet-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'darknet',
+    version = '0.1',
+    keywords = ('basic tools','utils'),
+    description = 'basic package for using',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+      ],
+    license = 'MIT License',
+    #install_requires = ['simplejson>=1.1'],
+
+    author = 'wangjj',
+    author_email = 'hzwangjj@gmail.com',
+
+    packages = find_packages(),
+    platforms = "any"
+)
