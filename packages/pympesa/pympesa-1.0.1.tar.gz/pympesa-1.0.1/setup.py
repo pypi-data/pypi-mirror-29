e682fdf683eb7ffae109cbf673842ef918a723cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:51 +0000

    Add package pympesa/1.0.1/pympesa-1.0.1.tar.gz

---
+++ packages/pympesa/pympesa-1.0.1.tar.gz/pympesa-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pympesa',
+      version='1.0.1',
+      description='Mpesa rest api library',
+      url='https://github.com/pythias-io/pympesa',
+      author='Andrew Kamau',
+      author_email='andrew@pythias.io',
+      license='MIT',
+      packages=['pympesa'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
