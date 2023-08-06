8141271d19c4e1926f22f19a822f781c7c784bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:54 +0000

    Add package sunmeng666/1.0/sunmeng666-1.0.tar.gz

---
+++ packages/sunmeng666/sunmeng666-1.0.tar.gz/sunmeng666-1.0/setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='sunmeng666',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Sun Ward',
+      author_email='www.smeng@gmail.com',
+      url='https://gitlab-common.dev.migame.g.mi.com/autouitest/moats.git',
+      packages=['sunmeng666', 'sunmeng666.command'],
+     )
