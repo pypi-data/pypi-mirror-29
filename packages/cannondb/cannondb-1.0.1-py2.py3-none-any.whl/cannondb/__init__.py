52b077a048067aef8180406e7af224977f8be4f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:43 +0000

    Add package cannondb/1.0.1/cannondb-1.0.1-py2.py3-none-any.whl

---
+++ packages/cannondb/cannondb-1.0.1-py2.py3-none-any.whl/cannondb/__init__.py
@@ -0,0 +1,12 @@
+from cannondb.database import CannonDB
+from cannondb.storages import FileStorage, MemoryStorage
+
+__version__ = '1.0.1'
+
+__all__ = ('CannonDB', 'FileStorage', 'MemoryStorage')
+
+
+def connect(file_name: str = 'database', cache_size: int = 128, *, order=100, page_size=8192, key_size=16,
+            value_size=64, file_cache=1024, **kwargs):
+    return CannonDB(file_name=file_name, cache_size=cache_size, order=order, page_size=page_size, key_size=key_size,
+                    value_size=value_size, file_cache=file_cache, **kwargs)
