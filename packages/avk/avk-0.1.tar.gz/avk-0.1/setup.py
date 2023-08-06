c7ff1b829a899f8c5a47b29208dd30052e544e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:10 +0000

    Add package avk/0.1/avk-0.1.tar.gz

---
+++ packages/avk/avk-0.1.tar.gz/avk-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='avk',
+    version='0.01',
+    install_requires=['aiohttp'],
+    url='https://github.com/kolayv/avk',
+    license='apache v2',
+    author='kolayv',
+    author_email='kolay.v@ya.ru',
+    description='asyncio vk api wrapper'
+)
