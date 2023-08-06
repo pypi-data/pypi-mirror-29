2f85fcc47a03f59160bc9bcf0da942c8bae2b505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:40 +0000

    Add package nbconvert-pyencoded/5.3.2.dev1/nbconvert-pyencoded-5.3.2.dev1.tar.gz

---
+++ packages/nbconvert-pyencoded/nbconvert-pyencoded-5.3.2.dev1.tar.gz/nbconvert-pyencoded-5.3.2.dev1/nbconvert/exporters/__init__.py
@@ -0,0 +1,16 @@
+from .base import (export, get_exporter, 
+                   ExporterNameError, get_export_names)
+from .exporter_locator import export_by_name
+from .html import HTMLExporter
+from .slides import SlidesExporter
+from .templateexporter import TemplateExporter
+from .latex import LatexExporter
+from .markdown import MarkdownExporter
+from .asciidoc import ASCIIDocExporter
+from .notebook import NotebookExporter
+from .pdf import PDFExporter
+from .python import PythonExporter
+from .encoded_python import EncodedPythonExporter
+from .rst import RSTExporter
+from .exporter import Exporter, FilenameExtension
+from .script import ScriptExporter
---
+++ packages/nbconvert-pyencoded/nbconvert-pyencoded-5.3.2.dev1.tar.gz/nbconvert-pyencoded-5.3.2.dev1/nbconvert/exporters/encoded_python.py
@@ -0,0 +1,37 @@
+"""Python script Exporter class"""
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+import copy
+import json
+
+from traitlets import default
+
+from .templateexporter import TemplateExporter
+
+
+def strip_cells(notebook):
+    clone = copy.deepcopy(notebook)
+    del clone['cells']
+    return json.dumps(clone, sort_keys=True)
+
+
+class EncodedPythonExporter(TemplateExporter):
+    """
+    Exports a Python code file.
+    """
+    @default('file_extension')
+    def _file_extension_default(self):
+        return '.py'
+
+    @default('template_file')
+    def _template_file_default(self):
+        return 'encoded_python.tpl'
+
+    def _init_resources(self, resources=None):
+        resources = super(EncodedPythonExporter, self)._init_resources(resources)
+        resources['strip_cells'] = strip_cells
+        return resources
+
+    output_mimetype = 'text/x-python'
