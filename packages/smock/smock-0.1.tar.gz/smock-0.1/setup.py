15e8f0633c4d1bdbeaac637325320cd89017492c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:56 +0000

    Add package smock/0.1/smock-0.1.tar.gz

---
+++ packages/smock/smock-0.1.tar.gz/smock-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'smock',
+  packages = ['smock'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Serverboards Mock library',
+  author = 'David Moreno',
+  author_email = 'dmoreno@serverboards.io',
+  url = 'https://github.com/serverboards/smock', 
+  download_url = 'https://github.com/serverboards/smock/archive/0.1.tar.gz', 
+  keywords = ['testing', 'mock', 'mocking'], 
+  classifiers = [],
+)
