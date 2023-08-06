7067ba1e56816ed244305e7b2412c925080401dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:45 +0000

    Add package shuup-api-permission/0.1.3/shuup-api-permission-0.1.3.tar.gz

---
+++ packages/shuup-api-permission/shuup-api-permission-0.1.3.tar.gz/shuup-api-permission-0.1.3/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="shuup-api-permission",
+    version="0.1.3",
+    description="Shuup API Permission",
+    license="MIT",
+    author="Christian Hess",
+    author_email="christianhess.rlz@gmail.com",
+    url="https://github.com/chessbr/shuup-api-permission",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "rest-jwt-permission>=0.2.0,<2"
+    ],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
