0989c0b27a5e9ef78b97f4200bae5cba10bdbd7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:36 +0000

    Add package upt-cpan/0.1.1/upt-cpan-0.1.1.tar.gz

---
+++ packages/upt-cpan/upt-cpan-0.1.1.tar.gz/upt-cpan-0.1.1/setup.py
@@ -0,0 +1,40 @@
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+
+def _reqs(filename):
+    with open(filename) as f:
+        return f.read().split('\n')
+
+def _install_reqs():
+    return _reqs('requirements.txt')
+
+def _test_reqs():
+    return _reqs('test-requirements.txt')
+
+setup(
+    name='upt-cpan',
+    author='Cyril Roelandt',
+    author_email='tipecaml@gmail.com',
+    url='https://framagit.org/upt/upt-cpan',
+    version='0.1.1',
+    description='CPAN frontend for upt',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires = _install_reqs(),
+    packages = find_packages(),
+    entry_points = {
+        'upt.frontends': [
+            'cpan=upt_cpan.upt_cpan:CPANFrontend',
+        ]
+    },
+)
