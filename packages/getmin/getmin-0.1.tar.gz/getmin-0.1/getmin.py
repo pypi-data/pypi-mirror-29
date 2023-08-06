2bc7924dbee47ab89208d530d01f5abdda3edc69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:31 +0000

    Add package getmin/0.1/getmin-0.1.tar.gz

---
+++ packages/getmin/getmin-0.1.tar.gz/getmin-0.1/getmin.py
@@ -0,0 +1,6 @@
+﻿def getmin(a,b):
+    if a<=b:
+        print('最小值是：',a)
+    else:
+        print('最小值是：',b)
+
---
+++ packages/getmin/getmin-0.1.tar.gz/getmin-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='getmin',
+    version='0.1',
+    py_modules=['getmin'],
+    author='liu',
+    author_email='2914169859@qq.com',
+    )
