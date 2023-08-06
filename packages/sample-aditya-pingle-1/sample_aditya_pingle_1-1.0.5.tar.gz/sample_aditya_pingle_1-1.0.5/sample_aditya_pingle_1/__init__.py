865740067cc1703fcaff717baf982f0755907c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:19 +0000

    Add package sample-aditya-pingle-1/1.0.5/sample_aditya_pingle_1-1.0.5.tar.gz

---
+++ packages/sample-aditya-pingle-1/sample_aditya_pingle_1-1.0.5.tar.gz/sample_aditya_pingle_1-1.0.5/sample_aditya_pingle_1/__init__.py
@@ -0,0 +1,12 @@
+def spit_out():
+    print "Spat!"
+    print "Success"
+    return True
+
+def main():
+    print "Success!!"
+    spit_out()
+
+if __name__ == "__main__":
+    print "Success!!"
+    spit_out()
