a4a6bcf03ce72285cf3ceb7ae05df5ed305ee8d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:36 +0000

    Add package pynimations/2.2.5/pynimations-2.2.5.tar.gz

---
+++ packages/pynimations/pynimations-2.2.5.tar.gz/pynimations-2.2.5/pynimations/__init__.py
@@ -0,0 +1,5 @@
+import pynimations.progressbar as progressbar
+import pynimations.rotator as rotator
+import pynimations.casetoggle as casetoggle
+import pynimations.typewriter as typewriter
+import pynimations.scroll as scroll
