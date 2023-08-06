45e3dc339aeaf3bf8210d43c653bd0f1996d2599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:43 +0000

    Add package PyEELS/1.0.1.dev1/PyEELS-1.0.1.dev1.tar.gz

---
+++ packages/PyEELS/PyEELS-1.0.1.dev1.tar.gz/PyEELS-1.0.1.dev1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup, Extension
+import numpy as np
+
+ext_modules = [ Extension('pyeels.cpyeels', sources = ['./pyeels/_spectrum/spectrum.c']) ]
+
+readme = open('README.md','r')
+
+setup(
+      name = 'PyEELS',
+      version = '1.0.1.dev1',
+      author = "Sindre R. Bilden",
+      author_email = "s.r.bilden@fys.uio.no",
+      url = 'https://github.com/sindrerb/pyeels',
+      description = ("A collection of functions for simulating EELS"),
+      long_description = readme.read(),
+      install_requires=['hyperspy', 'pythtb', 'spglib'],
+      packages=['pyeels'],
+      ext_modules = ext_modules
+      )
+
+readme.close()
