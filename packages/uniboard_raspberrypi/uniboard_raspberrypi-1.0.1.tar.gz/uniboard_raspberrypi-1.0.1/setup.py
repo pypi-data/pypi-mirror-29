954257f717eeebcde70c5dc068207bb6e6495f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:38 +0000

    Add package uniboard_raspberrypi/1.0.1/uniboard_raspberrypi-1.0.1.tar.gz

---
+++ packages/uniboard_raspberrypi/uniboard_raspberrypi-1.0.1.tar.gz/uniboard_raspberrypi-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='uniboard_raspberrypi',
+    version='1.0.1',
+    py_modules=['uniboard_raspberrypi'],
+    author='Chuan Shao',
+    author_email='shaochuancs@gmail.com',
+    url='https://github.com/shaochuancs/uniboard-raspberrypi',
+    description='Raspberry Pi Client Uniboard',
+    license='MIT',
+    install_requires=['paho-mqtt']
+)
