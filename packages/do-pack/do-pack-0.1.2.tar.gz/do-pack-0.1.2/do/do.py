4f9eb5c8493d1019fc15a75d39d1cdaf62979437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:23 +0000

    Add package do-pack/0.1.2/do-pack-0.1.2.tar.gz

---
+++ packages/do-pack/do-pack-0.1.2.tar.gz/do-pack-0.1.2/do/do.py
@@ -0,0 +1,107 @@
+import click
+import os
+import do.skeleton
+import do.licenses
+import do.template_config
+
+
+def clear(): return os.system('cls')
+
+
+@click.group()
+def main():
+    """
+    Simple A command-line tool to create python packages.
+    """
+    pass
+
+
+@main.command()
+@click.argument('project-name')
+def create(project_name):
+    """
+    creates an empty python package.
+    """
+    click.echo('\nPypro will create your {} Project Structure.'
+               .format(project_name))
+    if click.confirm('Do you want to continue?'):
+        do.skeleton.make_skeleton(project_name)
+        click.echo('{} was created on {}'.format(project_name,
+                                                 os.path.join(os.getcwd())))
+    else:
+        click.echo('Aborded!')
+
+
+@main.command()
+def assistant():
+    """
+    A step by step assistant.
+    """
+    clear()
+    # click.echo('do will now start the assistant.')
+    if click.confirm('>> do will now start the assistant.' +
+                     ' Do you want to continue?'):
+
+        project_name = click.prompt('\nproject name')
+        setup_version = click.prompt('version')
+        setup_description = click.prompt('description')
+        setup_author = click.prompt('author')
+        setup_author_email = click.prompt('author_email')
+        setup_url = click.prompt('url')
+        click.echo('\n>> Select one of the following LICENSES ' +
+                   '(more detailed info in https://choosealicense.com):\n')
+
+        do.licenses.show()
+        chosen_licence = click.prompt(
+            '\nEnter the number of the license to choose one')
+
+        # using a dict instead of an if statement
+        def lice(num=chosen_licence):
+            return {
+                '1': lambda: do.licenses.choose(
+                    'Apache License 2.0', setup_author),
+                '2': lambda: do.licenses.choose(
+                    'BSD License', setup_author),
+                '3': lambda: do.licenses.choose(
+                    'GNU Affero General Public License v3', setup_author),
+                '4': lambda: do.licenses.choose(
+                    'GNU Lesser General Public License v3', setup_author),
+                '5': lambda: do.licenses.choose(
+                    'GNU General Public License v3', setup_author,
+                    project_name),
+                '6': lambda: do.licenses.choose(
+                    'MIT License', setup_author),
+                '7': lambda: do.licenses.choose(
+                    'Mozilla Public License Version 2.0'),
+                '8': lambda: do.licenses.choose(
+                    'Unlicensed')
+            }.get(num, lambda: None)()
+
+        chosen_licence = lice()
+
+        setup = do.template_config.setup_template(
+            project_name,
+            setup_version,
+            setup_description,
+            setup_author,
+            setup_author_email,
+            setup_url
+        )
+
+        authors = do.template_config.authors_template(
+            project_name,
+            setup_author,
+            setup_author_email
+        )
+
+        do.skeleton.make_skeleton(
+            project_name,
+            authors,
+            chosen_licence,
+            setup,
+            assist=True
+        )
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/do-pack/do-pack-0.1.2.tar.gz/do-pack-0.1.2/do/skeleton.py
@@ -0,0 +1,97 @@
+import os
+import sys
+import json
+import click
+
+
+def make_skeleton(project,
+                  authors=None,
+                  choosen_license=None,
+                  setup=None,
+                  assist=False):
+    """
+    Create de skeleton of the python project and
+    Redirect the files and folders to the proper function.
+    """
+    # TODO: 50% - implement a template system for the skeleton in .json
+    try:
+        # get the path for the default skeleton
+        default_skeleton = os.path.join(
+            os.path.dirname(__file__), 'templates\\default_structure.json')
+    except FileNotFoundError:
+        click.echo('Template file not found. Aborted!')
+        sys.exit(1)
+
+    # open the default skeleton template from templates folder
+    with open(default_skeleton) as f:
+        skeleton = json.load(f)
+
+    def makedir(directory):
+        """
+        Make the folders tree.
+        """
+        # change the name of base and bin for the name of the project
+        if directory == 'base' or directory == 'bin':
+            directory = project
+        # write the folders name
+        try:
+            os.makedirs(directory)
+            os.chdir(directory)
+        except FileExistsError:
+            click.echo('Folder {} alredy exists. Aborted!'.format(directory))
+            sys.exit(1)
+
+    def makefile(file, assist=False):
+        """
+        Make the files for the project.
+        """
+        # change the names project.py and test_project.py
+        if file == 'project.py':
+            file = project + '.py'
+        elif file == 'test_project.py':
+            file = 'test_' + project + '.py'
+        # assist=True == assistant
+        if assist:
+            if file == 'LICENSE':
+                writefile(file, choosen_license)
+            elif file == 'setup.py':
+                writefile(file, setup)
+            elif file == 'AUTHORS.rst':
+                writefile(file, authors)
+            else:
+                writefile(file)
+        # assist=False == project (write an empty file)
+        else:
+            writefile(file)
+
+    def writefile(file, content=''):
+        """
+        Function that write the files and go back one folder
+        for the sake of the stucture.
+        """
+        # if  file == '..' go back one directory
+        if file == '..':
+            os.chdir('..')
+        else:
+            try:
+                with open(file, 'w') as f:
+                    f.write(content)
+            except Exception as e:
+                click.echo('Error wrinting {}. Aborted!'.format(file))
+                sys.exit(1)
+
+    # from here starts the program
+    for folder in skeleton.keys():
+        # create the folders
+        makedir(folder)
+
+        for files in skeleton[folder]:
+            # assist=True == assistant mode - assist=False == create()
+            if assist:
+                makefile(files, assist=True)
+            else:
+                makefile(files)
+
+
+if __name__ == '__main__':
+    make_skeleton('test')
