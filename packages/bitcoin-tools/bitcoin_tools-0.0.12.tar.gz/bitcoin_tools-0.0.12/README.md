1a0eeac6dfcf992e9b08d13bd497f3303a935c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:21 +0000

    Add package bitcoin-tools/0.0.12/bitcoin_tools-0.0.12.tar.gz

---
+++ packages/bitcoin-tools/bitcoin_tools-0.0.12.tar.gz/bitcoin_tools-0.0.12/README.md
@@ -0,0 +1,46 @@
+# Python Bitcoin Tools Library
+
+A Python Bitcoin Tools Library
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+Python 3 - https://www.python.org/downloads/
+```
+
+## Running the tests
+
+```
+python test.py
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Built With
+
+* [Python 3](https://www.python.org)
+
+
+## Authors
+
+* **Christopher Coverdale** - *Initial work* - [ccdle12](https://github.com/ccdle12)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Jimmy Song
+* Vitalik Buterin
+* Richard Kiss
+
---
+++ packages/bitcoin-tools/bitcoin_tools-0.0.12.tar.gz/bitcoin_tools-0.0.12/bitcoin_tools/blockchain_explorer_helper.py
@@ -0,0 +1,121 @@
+import json
+import requests
+
+# 1st case -> move to its own Class?
+# 5757068b376143b8aa5f7fc137dc2351
+block_cypher_url = "https://api.blockcypher.com/v1/btc/test3"
+block_cypher_token = "?token=c355cd4305b042a980c6b83c4e21bb4d"
+
+# 2nd case -> move to its own Class?
+block_trail_url = "https://api.blocktrail.com/v1/tBTC"
+block_trail_token = "?api_key=8a2a363be9edf703cba639d7e3f8a1831978f483"
+
+def ping():
+    return requests.get(block_cypher_url)
+
+
+def get_balance(address):
+    balance_url = "/addrs/{}/balance".format(address)
+    response = requests.get(block_cypher_url + balance_url)
+
+    if response.status_code != 200:
+        raise RuntimeError("The server returned an error: {}".format(response.json()))
+
+    return response
+
+
+def send_tx(raw_tx):
+    send_tx_url = "/txs/push"
+
+    json_tx = create_json_tx(raw_tx)
+
+    response = requests.post(block_cypher_url + send_tx_url + block_cypher_token, data=json_tx)
+
+    if response.status_code == 400:
+        raise RuntimeError("Error sending the tx: {}".format(response.json()))
+
+    return response
+
+
+def get_transaction(transaction_hash):
+    get_tx_url = "/txs/{}".format(transaction_hash)
+
+    response = requests.get(block_cypher_url + get_tx_url)
+
+    return response
+
+
+def decode_transaction(raw_tx):
+    decode_tx_url = "/txs/decode"
+
+    json_tx = create_json_tx(raw_tx)
+
+    response = requests.post(block_cypher_url + decode_tx_url + block_cypher_token, data=json_tx)
+
+    return response
+
+
+def create_json_tx(raw_tx):
+    tx_object = {"tx": raw_tx}
+    return json.dumps(tx_object)
+
+def request_balance(address):
+    # 1st case
+    request_balance_url = "/addrs/{}/balance".format(address)
+
+    try:
+        response = requests.get(block_cypher_url + request_balance_url)
+        # if response.status_code != 200:
+            # raise RuntimeError("The server returned an error: {}".format(response.json()))
+    except:
+        raise RuntimeError("API Limit reached: {}".format(response.json()))
+
+    # 2nd case
+    # Block trail does not have a balance request
+    try:
+        request_UTXOs_url = "/address/{}/unspent-outputs".format(address)
+
+        response = requests.get(block_trail_url + request_UTXOs_url + block_trail_token)
+        # if response.status_code != 200:
+            # raise RuntimeError("The server returned an error: {}".format(response.json()))
+    except:
+        raise RuntimeError("API Limit reached: {}".format(response.json()))
+        
+    print("Response from Block Trail: {}".format(response))
+
+    return response
+
+def request_UTXOs(address):
+
+    response = None
+    schema = None
+
+    # 1st case
+    # try: 
+    #     request_UTXOs_url = "/addrs/{}".format(address)
+
+    #     response = requests.get(block_cypher_url + request_UTXOs_url)
+
+    #     # if response.status_code != 200:
+    #         # raise RuntimeError("The server returned an error: {}".format(response.json()))
+
+    #     schema = 'block_cypher'
+    # except:
+    #     raise RuntimeError("API Limit reached: {}".format(response.json()))
+
+
+    # 2nd case
+    try: 
+        request_UTXOs_url = "/address/{}/unspent-outputs".format(address)
+        response = requests.get(block_trail_url + request_UTXOs_url + block_trail_token)
+        print("Block_trail resp: {}".format(response.json()))
+
+        # if response.status_code != 200:
+            # raise RuntimeError("The server returned an error: {}".format(response.json()))
+
+        schema = 'block_trail'
+
+    except:
+        raise RuntimeError("API Limit reached: {}".format(response.json()))    
+
+    return (response, schema)
