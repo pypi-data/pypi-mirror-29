799170d3e31b21ef7495f0a28c7de6813f0c601c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:03 +0000

    Add package cbrf/0.4/cbrf-0.4.tar.gz

---
+++ packages/cbrf/cbrf-0.4.tar.gz/cbrf-0.4/cbrf/const.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+CURRENCY_CODES = {
+    # http://www.cbr.ru/scripts/XML_val.asp?d=0
+    'USD': 'R01235',
+    'EUR': 'R01239',
+}
+
+CBRF_API_URLS = {
+    'info': 'http://www.cbr.ru/scripts/XML_valFull.asp',
+    'daily_rus': 'http://www.cbr.ru/scripts/XML_daily.asp?',
+    'daily_eng': 'http://www.cbr.ru/scripts/XML_daily_eng.asp?',
+    'dynamic': 'http://www.cbr.ru/scripts/XML_dynamic.asp?',
+}
---
+++ packages/cbrf/cbrf-0.4.tar.gz/cbrf-0.4/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+import ast
+import re
+from distutils.core import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('cbrf/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+print(version)
+
+setup(
+    name='cbrf',
+    version=version,
+    packages=['cbrf'],
+    install_requires=[
+        "requests",
+    ],
+    url='https://github.com/Egregors/cbrf',
+    license='MIT',
+    author='Vadim Iskuchekov (@egregors)',
+    author_email='egregors@yandex.ru',
+    description='Wrapper for The Central Bank of the Russian Federation site API',
+    long_description=open('README.md').read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 3.6'
+    ],
+)
