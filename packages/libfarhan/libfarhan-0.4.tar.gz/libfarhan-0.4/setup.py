72470b74dc6252cb16c890ace06617349cfe1976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:04 +0000

    Add package libfarhan/0.4/libfarhan-0.4.tar.gz

---
+++ packages/libfarhan/libfarhan-0.4.tar.gz/libfarhan-0.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='libfarhan',
+    packages=['libfarhan'],  # this must be the same as the name above
+    version='0.4',
+    description='Libfarhan test library',
+    author='Farhan Attamimi',
+    author_email='farhan@sourcegraph.com',
+    url='https://github.com/attfarhan/libfarhan',  # use the URL to the github repo
+    # I'll explain this in a second
+    download_url='https://github.com/attfarhan/libfarhan/archive/0.1.tar.gz',
+    keywords=['test', 'python', 'farhan'],  # arbitrary keywords
+    classifiers=[],
+)
