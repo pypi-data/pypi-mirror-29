3645b57e0390705229d7849865f65967dd5f700c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:23 +0000

    Add package testtestasdf/0.0.3/testtestasdf-0.0.3-py2.py3-none-any.whl

---
+++ packages/testtestasdf/testtestasdf-0.0.3-py2.py3-none-any.whl/testtestasdf/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import requests, json, sys
+
+def testtestasdf():
+
+	query = '+'.join(sys.argv[1:]).replace(" ", "+")
+	if query == "" or query == " ":
+		print ('Usage: wciwt "game of thrones"')
+		exit()
+
+	json = (requests.get("https://wciwt.in/api/?format=json&query=" + query)).json()
+	if (json['total_results'] == 0):
+		print ("No such title in our database")
+	else:
+		for service in json['results']:
+			if len(json['results'][service]) != 0:
+				print ("\n{}: {}\n".format(service.upper(), ', '.join(str(x) for x in json['results'][service])))
+
+if __name__ == "__main__":
+	testtestasdf()
