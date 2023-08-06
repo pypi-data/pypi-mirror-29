e990aed89e893667997d861b468cb2d5bb1b629e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:49 +0000

    Add package dicom-contour/0.3/dicom_contour-0.3.tar.gz

---
+++ packages/dicom-contour/dicom_contour-0.3.tar.gz/dicom_contour-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+     name='dicom_contour',    
+     version=0.3,
+     description='A library which converts DICOM images and contours into numpy arrays',
+     author='Kerem Turgutlu',
+     author_email='kcturgutlu@dons.usfca.edu',
+     license='MIT',
+     url='https://github.com/KeremTurgutlu/dicom-contour',
+     install_requires=
+	['pydicom', 'numpy', 'scipy', 'matplotlib'],
+     keywords=['dicom', 'contour', 'medical image'],
+     
+)
