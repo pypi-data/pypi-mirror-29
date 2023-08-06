5141b6c9b6beccf396b08208f81b27968e48fd55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:08 +0000

    Add package voodoo-cli/2.6.8/voodoo_cli-2.6.8-py34+-none-any.whl

---
+++ packages/voodoo-cli/voodoo_cli-2.6.8-py34+-none-any.whl/voodoo/hook.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import pkg_resources
+import yaml
+from datetime import datetime
+from plumbum.cli.terminal import ask
+from plumbum import local
+from unidecode import unidecode
+
+
+class Hook(object):
+
+    def __init__(self, voodoo, logger):
+        self.logger = logger
+        self.voodoo = voodoo
+        self._run = voodoo._run
+        self._compose = getattr(voodoo, 'compose', None)
+        super(Hook, self).__init__()
+
+
+class GetMainService(Hook):
+    _service = None
+
+    def run(self):
+        pass
+
+
+class Deploy(Hook):
+    _service = None
+
+    def __init__(self, *args, **kwargs):
+        super(Deploy, self).__init__(*args, **kwargs)
+        self._called_log = []
+
+    def _build(self):
+        self._run(self._compose['build'])
+
+    def _stop_container(self):
+        self._run(self._compose['down'])
+
+    def _start_maintenance(self):
+        pass
+
+    def _update_application(self):
+        pass
+
+    def _stop_maintenance(self):
+        pass
+
+    def _start_container(self):
+        self._run(self._compose['up', '-d'])
+
+    def _call_and_log(self, func, message):
+        start = datetime.now()
+        self.logger.info(message)
+        getattr(self, func)()
+        end = datetime.now()
+        funcname = func.replace('_', ' ').strip().capitalize()
+        message = '%s done in %s' % (funcname, end - start)
+        self.logger.info(message)
+        self._called_log.append(message)
+
+    def _resume_log(self):
+        self.logger.info("\n\nDeploy Log Resume:\n")
+        for msg in self._called_log:
+            self.logger.info(msg)
+
+    def _deploy(self):
+        self._call_and_log('_build', 'Start Building the application:')
+        self._call_and_log('_stop_container', 'Stop the application:')
+        self._call_and_log('_update_application', 'Update the application:')
+        self._call_and_log('_stop_maintenance', 'Stop the maintenance page:')
+        self._call_and_log('_start_container', 'Start the application:')
+
+    def run(self):
+        self._call_and_log('_deploy', 'Deploying the Application')
+        self._resume_log()
+
+
+class InitRunDev(Hook):
+    _service = None
+
+    def run(self):
+        pass
+
+
+class GenerateDevComposeFile(Hook):
+    _service = None
+    _map_user_for_service = None
+
+    def __init__(self, *args, **kwargs):
+        super(GenerateDevComposeFile, self).__init__(*args, **kwargs)
+        # Do not use os.path.join()
+        resource_path = '/%s/docker-compose.yml' % self._service
+        template = pkg_resources.resource_stream(__name__, resource_path)
+        self.config = yaml.safe_load(template)
+
+    def _add_map_uid(self):
+        if not self._map_user_for_service:
+            return
+        # inject uid for sharing file with some host
+        uid = os.getuid()
+        for service_name in self._map_user_for_service:
+            if service_name in self.config['services']:
+                service = self.config['services'][self._service]
+                if not 'environment' in service:
+                    service['environment'] = []
+                for key in ['USERMAP_UID', 'USERMAP_GID']:
+                    service['environment'].append("%s=%s" % (key,uid))
+
+    def get_default_volume(self):
+        return []
+
+    def _add_default_volume(self):
+        service = self.config['services'][self._service]
+        default_volume = self.get_default_volume()
+        if default_volume:
+            if not 'volumes' in service:
+                service['volumes'] = []
+            service['volumes'] += default_volume
+
+    def _ask_optional_service(self):
+        """Container can be set as optional by adding the key
+        "optional" and "description". This method will ask the user to
+        use or not this optional container"""
+        answer = {}
+        for name, config in self.config['services'].items():
+            if config.get('optional'):
+                option = config['optional']
+                if not option in answer:
+                    answer[option] = ask(
+                        "%s. Do you want to install it"
+                        % option, default=False)
+                if answer[option]:
+                    # remove useless dockercompose key
+                    del self.config['services'][name]['optional']
+                    if not 'links' in self.config['services'][self._service]:
+                        self.config['services'][self._service]['links'] = []
+                    self.config['services'][self._service]['links'].append(name)
+                else:
+                    del self.config['services'][name]
+
+    def _add_container_name(self):
+        project_name = unidecode(local.cwd.name.decode('utf-8'))\
+            .replace(' ', '').lower()
+        for name, config in self.config['services'].items():
+            expose = config.pop('expose', False)
+            if expose:
+                if name != self._service:
+                    dns = "%s.%s.vd" %(name, project_name)
+                else:
+                    dns = "%s.vd" % project_name
+                if not 'environment' in config:
+                    config['environment'] = []
+                config['environment'].append("VIRTUAL_HOST=%s" % dns)
+                config['environment'].append("VIRTUAL_PORT=%s" % expose)
+                config['networks'] = {}
+                config['networks']['default'] = {}
+                config['networks']['default']['aliases'] = []
+                config['networks']['default']['aliases'].append(dns)
+
+    def _update_config_file(self):
+        self._ask_optional_service()
+        self._add_map_uid()
+        self._add_default_volume()
+        self._add_container_name()
+
+    def run(self):
+        with open('dev.docker-compose.yml', 'w') as dc_tmp_file:
+            self._update_config_file()
+            dc_tmp_file.write(yaml.dump(self.config, default_flow_style=False))
---
+++ packages/voodoo-cli/voodoo_cli-2.6.8-py34+-none-any.whl/voodoo/odoo/hook.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from ..hook import Deploy, InitRunDev, GenerateDevComposeFile, GetMainService
+from plumbum import local
+from plumbum.cmd import git
+
+import os
+
+
+class OdooGetMainService(GetMainService):
+    _service = 'odoo'
+
+    def run(self):
+        if os.path.exists('buildout.cfg'):
+            return 'odoo'
+        return None
+
+
+class OdooDeploy(Deploy):
+    _service = 'odoo'
+
+    def _build(self):
+        buildout_file = "%s.buildout.cfg" % self.voodoo.env
+        if not os.path.isfile(buildout_file):
+            print (
+                "The %s is missing, please add one before deploying"
+                % buildout_file)
+        return super(OdooDeploy, self)._build()
+
+    def _update_application(self):
+        self._run(self._compose['run', '--rm', 'odoo', 'ak', 'upgrade'])
+
+
+class GenerateDevComposeFile(GenerateDevComposeFile):
+    _service = 'odoo'
+    _map_user_for_service = ['db']
+
+    def get_default_volume(self):
+        path = '/home/${USER}/.voodoo/shared'
+        return [':'.join([path, path])]
+
+    def _update_config_file(self):
+        super(GenerateDevComposeFile, self)._update_config_file()
+        path = '/home/${USER}/.voodoo/shared/maintainer_quality_tools'
+        self.config['services']['odoo']['environment'].append(
+            "MAINTAINER_QUALITY_TOOLS=%s" % path)
+
+
+class OdooInitRunDev(InitRunDev):
+    _service = 'odoo'
+
+    def _get_odoo_cache_path(self):
+        cache_path = os.path.join(self.voodoo.parent.shared_folder, 'cached_odoo')
+        if not os.path.exists(cache_path):
+            os.makedirs(cache_path)
+        odoo_cache_path = os.path.join(cache_path, 'odoo')
+        if not os.path.exists(odoo_cache_path):
+            print (
+                "First run of Voodoo; there is no Odoo repo in %s! \n"
+                "Will now download Odoo from Github, "
+                "this can take a while...\n"
+                "If you already have a local Odoo repo (from OCA) "
+                "then you can you can abort the download "
+                "and paste your repo or make a symbolink link in %s"
+                % (odoo_cache_path, odoo_cache_path))
+            self._run(git["clone", self.voodoo.parent.odoo, odoo_cache_path])
+        else:
+            print("Updating Odoo cache...")
+            with local.cwd(odoo_cache_path):
+                self._run(git["pull"])
+        return odoo_cache_path
+
+    def _get_odoo(self, odoo_path):
+        if not os.path.exists('parts'):
+            os.makedirs('parts')
+        odoo_cache_path = self._get_odoo_cache_path()
+        self._run(git["clone", "file://%s" % odoo_cache_path, odoo_path])
+
+    def _copy_eggs_directory(self, dest):
+        self._run(self._compose[
+            'run', 'odoo', 'cp', '-r', '/opt/voodoo/eggs', dest])
+
+    def _get_maintainer_quality_tools_path(self):
+        path = local.path(
+            "%s/maintainer_quality_tools"
+            % self.voodoo.parent.shared_folder)
+        if not path.is_dir():
+            print (
+                "First run of Voodoo; there is no Maintainer Quality Tools "
+                "repo in %s! \nWill now download it from Github, "
+                "this can take a while...\n"
+                % (path._path))
+            self._run(git["clone",
+                          self.voodoo.parent.maintainer_quality_tools,
+                          path._path])
+
+    def run(self):
+        # create db directory data and socket if missing
+        for directory in ['socket', 'data']:
+            path = os.path.join('.db', directory)
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+        # Create maintainer quality tools
+        self._get_maintainer_quality_tools_path()
+
+        # Create odoo directory from cache if do not exist
+        odoo_path = os.path.join('parts', 'odoo')
+        if not os.path.exists(odoo_path):
+            self._get_odoo(odoo_path)
+
+        # Create shared eggs directory if not exist
+        home = os.path.expanduser("~")
+        eggs_path = os.path.join(home, '.voodoo', 'shared', 'eggs')
+        if not os.path.exists(eggs_path):
+            self._copy_eggs_directory(eggs_path)
+
+        # Init eggs directory : share it or generate a new one
+        if not os.path.exists('eggs'):
+            if self.voodoo.parent.shared_eggs:
+                os.symlink(eggs_path, 'eggs')
+            else:
+                self._copy_eggs_directory(eggs_path)
