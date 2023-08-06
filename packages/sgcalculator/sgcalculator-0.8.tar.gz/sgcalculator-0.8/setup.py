1db05e04e1d66a960bfd01c7319ed87bfe356087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:36 +0000

    Add package sgcalculator/0.8/sgcalculator-0.8.tar.gz

---
+++ packages/sgcalculator/sgcalculator-0.8.tar.gz/sgcalculator-0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sgcalculator',
+      version='0.8',
+      description=' Simple analytics module for looking at population data.',
+      url='https://bitbucket.org/renford/sgcalculator',
+      author='Renford Alexander',
+      author_email='alexander.Renford@gmail.com',
+      license='MIT',
+      packages=['sgcalculator'],
+      zip_safe=False)
