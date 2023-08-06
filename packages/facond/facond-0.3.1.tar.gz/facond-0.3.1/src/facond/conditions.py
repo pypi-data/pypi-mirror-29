9e3573492bff954eb2529c0b7436b41fc910a8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:06 +0000

    Add package facond/0.3.1/facond-0.3.1.tar.gz

---
+++ packages/facond/facond-0.3.1.tar.gz/facond-0.3.1/src/facond/conditions.py
@@ -0,0 +1,20 @@
+"""Conditions for Actions."""
+
+from .js import JsDictMixin
+
+
+class Condition(JsDictMixin):
+    """Base condition."""
+
+
+class ValueIs(Condition):
+    """Rule that passes if a field has an arbitrary value."""
+
+    def __init__(self, field, value):
+        """Require a field to have a value."""
+        self.field = field
+        self.value = value
+
+    def validate(self, form):
+        """Return True if the form field has the given value."""
+        return form.data.get(self.field, None) == self.value
