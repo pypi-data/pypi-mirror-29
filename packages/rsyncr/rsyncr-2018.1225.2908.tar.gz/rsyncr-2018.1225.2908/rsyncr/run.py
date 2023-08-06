d3836ec48bfb68ecb5bcb036f88835dc613f5209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:01 +0000

    Add package rsyncr/2018.1225.2908/rsyncr-2018.1225.2908.tar.gz

---
+++ packages/rsyncr/rsyncr-2018.1225.2908.tar.gz/rsyncr-2018.1225.2908/rsyncr/run.py
@@ -0,0 +1,4 @@
+import os, subprocess, sys
+
+def main():  # wrapping the script call from the console_script
+  subprocess.Popen('"' + sys.executable + '" "' + os.path.join(os.path.dirname(os.path.abspath(__file__)), "rsyncr.py") + '" ' + " ".join(sys.argv[1:]), shell = True, bufsize = 1).wait()
