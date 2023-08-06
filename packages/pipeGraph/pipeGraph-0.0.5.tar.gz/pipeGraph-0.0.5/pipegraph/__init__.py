19fc38ac1e0a9e0aaa09b70e61f7ee0cfa1cbdd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:48 +0000

    Add package pipeGraph/0.0.5/pipeGraph-0.0.5.tar.gz

---
+++ packages/pipeGraph/pipeGraph-0.0.5.tar.gz/pipeGraph-0.0.5/pipegraph/__init__.py
@@ -0,0 +1,9 @@
+from .base import (PipeGraphRegressor,
+                   PipeGraphClassifier,
+                   )
+from . import base
+
+__all__ = ['PipeGraphClassifier',
+           'PipeGraphRegressor',
+           'base.py',
+           ]
