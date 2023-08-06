5a285e789c1cdde7dd4f88273df08eb87db8c0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:36 +0000

    Add package komica-save-file/1.0/komica_save_file-1.0.tar.gz

---
+++ packages/komica-save-file/komica_save_file-1.0.tar.gz/komica_save_file-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(name='komica_save_file',
+      version='1.0',
+      description='save files from komica',
+      author='gred0216',
+      author_email='betacrunch12345@gmail.com',
+      packages=[''],
+      )
