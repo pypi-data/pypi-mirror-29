0b29d19751f5d26253f76dd5453a18f4c56a4fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:21 +0000

    Add package strict_functions/2018.2.28.1/strict_functions-2018.2.28.1.tar.gz

---
+++ packages/strict_functions/strict_functions-2018.2.28.1.tar.gz/strict_functions-2018.2.28.1/strict_functions/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Author: cody
+# @Date:   2016-10-14 13:26:43
+# @Last Modified 2018-02-28
+# @Last Modified time: 2018-02-28 14:24:18
+
+import sys
+import os.path
+
+sys.path.append(os.path.dirname(__file__))
+
+del sys
+del os
+
+from c_version import c_version
+from input_types import input_types
+from never_parallel import never_parallel
+from noglobals import noglobals
+from on_fail import on_fail
+from output_type import output_type
+from overload import overload
+from self_aware import self_aware
+from strict_defaults import strict_defaults
+from strict_globals import strict_globals
+
