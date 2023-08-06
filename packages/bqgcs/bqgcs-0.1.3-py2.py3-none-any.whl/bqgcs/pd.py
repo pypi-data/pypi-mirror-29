68a0245eaf693191aa0964c08b2f528669f40047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:43 +0000

    Add package bqgcs/0.1.3/bqgcs-0.1.3-py2.py3-none-any.whl

---
+++ packages/bqgcs/bqgcs-0.1.3-py2.py3-none-any.whl/bqgcs/pd.py
@@ -0,0 +1,43 @@
+import six
+from google.cloud import bigquery
+
+if False:
+    from google.cloud import storage
+    import pandas as pd
+    from typing import *
+
+
+def pd_to_gcs(dataframe, blob):  # type: (pd.DataFrame, storage.Blob) -> None
+    extensions = blob.name.split('.')
+    compression = None
+    if extensions[-1] == "gz":
+        compression = "gzip"
+        extensions = extensions[:-1]
+
+    buf = six.StringIO()
+    ext = extensions[-1]
+    if ext == "csv":
+        dataframe.to_csv(buf, compression=compression, index=False)
+    elif ext == "json":
+        dataframe.to_json(buf, compression=compression)
+    else:
+        raise Exception("unsupported format: {}".format(ext))
+
+    blob.upload_from_file(buf, rewind=True)
+
+
+def generate_bq_schema(dataframe):  # type: (pd.DataFrame) -> [bigquery.SchemaField]
+    type_mapping = {
+        'i': 'INTEGER',
+        'b': 'BOOLEAN',
+        'f': 'FLOAT',
+        'O': 'STRING',
+        'S': 'STRING',
+        'U': 'STRING',
+        'M': 'TIMESTAMP',
+    }
+    dataframe.to_csv()
+    return [
+        bigquery.SchemaField(column_name, type_mapping.get(dtype.kind, 'STRING'))
+        for column_name, dtype in dataframe.dtypes.items()
+    ]
