f8a114e71d798bd23e0dc76a56f1ce90a1be976c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:33 +0000

    Add package django-pwned-validator/0.0.1/django-pwned-validator-0.0.1.tar.gz

---
+++ packages/django-pwned-validator/django-pwned-validator-0.0.1.tar.gz/django-pwned-validator-0.0.1/pwned/validators.py
@@ -0,0 +1,19 @@
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
+
+from client import PwnedClient
+from . import app_settings
+
+
+@deconstructible
+class PwnedValidator:
+    message = _('This password is known to be weak')
+    code = 'invalid'
+    client = PwnedClient
+
+    def __call__(self, password):
+        pwned_client = self.client()
+        count = pwned_client.count_occurrences(password)
+        if count >= app_settings.PWNED['OCCURRENCE_THRESHOLD']:
+            raise ValidationError(self.message, code=self.code)
