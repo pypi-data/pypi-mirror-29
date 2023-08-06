67d41beef09d4d232f5d5316d68a19d99126d8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:52 +0000

    Add package salabim/2.2.15/salabim-2.2.15.tar.gz

---
+++ packages/salabim/salabim-2.2.15.tar.gz/salabim-2.2.15/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = 'salabim',
+    packages = ['salabim'],
+    version = '2.2.15',
+    include_package_data=True,
+    description = 'discrete event simulation in Python',
+    author = 'Ruud van der Ham',
+    author_email = 'info@salabim.org',
+    url = 'https://github.com/salabim/salabim',
+    download_url = 'https://github.com/salabim/salabim',
+    keywords = ['statistics', 'math','simulation','des','discrete event simulation'],
+    classifiers = []
+)
