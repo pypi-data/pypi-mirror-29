b36da1b9b33001db85124c8ba34f023874871308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:51 +0000

    Add package AnkiTools/0.1/AnkiTools-0.1.tar.gz

---
+++ packages/AnkiTools/AnkiTools-0.1.tar.gz/AnkiTools-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='AnkiTools',
+      version='0.1',
+      description='Anki *.apkg reader in a human-readable format; and an editor.',
+      url='https://github.com/patarapolw/AnkiTools',
+      author='Pacharapol Withayasakpunt',
+      author_email='patarapolw@gmail.com',
+      license='MIT',
+      packages=['AnkiTools'],
+      install_requires=['requests'],
+      python_requires='>=2.7'
+      )
