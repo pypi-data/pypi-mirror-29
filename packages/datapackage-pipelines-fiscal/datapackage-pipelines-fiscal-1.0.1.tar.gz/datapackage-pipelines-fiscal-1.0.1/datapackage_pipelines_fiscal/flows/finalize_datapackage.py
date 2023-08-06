80295882ed599c3ad903fffe8b660cb9480786d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:58 +0000

    Add package datapackage-pipelines-fiscal/1.0.1/datapackage-pipelines-fiscal-1.0.1.tar.gz

---
+++ packages/datapackage-pipelines-fiscal/datapackage-pipelines-fiscal-1.0.1.tar.gz/datapackage-pipelines-fiscal-1.0.1/datapackage_pipelines_fiscal/flows/finalize_datapackage.py
@@ -0,0 +1,55 @@
+import os
+
+from .utils import extract_names, extract_storage_ids
+
+BUCKET = os.environ.get('S3_BUCKET_NAME')
+
+def finalize_datapackage_flow(source):
+
+    _, _, resource_name = extract_names(source)
+    dataset_id, _ = extract_storage_ids(source)
+
+    pipeline_steps = [
+                         (
+                             'load_metadata',
+                             {
+                                 'url': 'dependency://./denormalized_flow',
+                             }
+                         ),
+                         (
+                             'load_resource',
+                             {
+                                 'url': 'dependency://./denormalized_flow',
+                                 'resource': resource_name
+                             }
+                         ),
+                         (
+                             'fiscal.split_per_fiscal_year'
+                         ),
+                     ]
+    if BUCKET is not None:
+        pipeline_steps.append(
+            (
+                'aws.dump.to_s3',
+                {
+                    'bucket': BUCKET,
+                    'path': '{}/final'.format(dataset_id)
+                }
+            ),
+            ('fiscal.update_model_in_registry', {
+                'dataset-id': dataset_id,
+                'datapackage-url': 'https://{}/{}/final/datapackage.json'.format(BUCKET, dataset_id)
+            }),
+        )
+    else:
+        pipeline_steps.append(
+            (
+                'dump.to_path',
+                {
+                    'out-path': 'final'
+                }
+            )
+        )
+        
+
+    yield pipeline_steps, ['./denormalized_flow'], ''
