0e2a1dddff26027b6d2b1c85a49dc3272a1006e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:40 +0000

    Add package geo-street-talk/0.1.2.2/geo_street_talk-0.1.2.2.tar.gz

---
+++ packages/geo-street-talk/geo_street_talk-0.1.2.2.tar.gz/geo_street_talk-0.1.2.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='geo_street_talk',
+      version='0.1.2.2',
+      description='Geo-street-talk',
+      url='https://github.com/Streets-Data-Collaborative/geo-street-talk-global',
+      author='Yukun Wan',
+      author_email='yw3447@nyu.edu',
+      license='MIT',
+      packages=['geo_street_talk'],
+      zip_safe=False)
