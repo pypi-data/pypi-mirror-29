13375f397e82a2f140957610b8d8943ee3db09ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:52 +0000

    Add package CloeePy/0.0.0-rc1/CloeePy-0.0.0-rc1.tar.gz

---
+++ packages/CloeePy/CloeePy-0.0.0-rc1.tar.gz/CloeePy-0.0.0-rc1/cloeepy/__init__.py
@@ -0,0 +1,1 @@
+from cloeepy.cloeepy import CloeePy
---
+++ packages/CloeePy/CloeePy-0.0.0-rc1.tar.gz/CloeePy-0.0.0-rc1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='CloeePy',
+    version='0.0.0-rc1',
+    packages=['cloeepy',],
+    package_data = {
+        '.': ['*.md'],
+        'cloeepy': ['data/*.yml'],
+    },
+    include_package_data=True,    # include everything in source control
+    license='MIT',
+    description="Light weight framework for non-HTTP systems.",
+    install_requires=[
+          "pyaml==17.12.1",
+          "python-json-logger==0.1.8",
+          "PyYAML==3.12",
+     ],
+     url = "https://github.com/cloeeai/CloeePy",
+     author = "Scott Crespo",
+     author_email = "sccrespo@gmail.com",
+     keywords = "mini framework yaml cloee",
+     python_requires='~=3.3',
+)
