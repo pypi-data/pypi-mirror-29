c3c0f3b223742e1f65fd8a3e46e4917e244871e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:10 +0000

    Add package ode/0.1.1/ode-0.1.1.tar.gz

---
+++ packages/ode/ode-0.1.1.tar.gz/ode-0.1.1/ode/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """bierschenk"""
+__email__ = 'bierschenk.devel@gmail.com'
+__version__ = '0.1.1'
+
+
+# Import all functions transparently to ode.
+
+from . simple_explicit import ieuler
+from . simple_explicit import euler
+
+
+from . simple_implicit import ibackward_euler
+from . simple_implicit import backward_euler
