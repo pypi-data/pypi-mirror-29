3b7c3587c924ee35f2ae9ea40060f1841cb0781f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:45 +0000

    Add package pyskeleton/0.3.3/pyskeleton-0.3.3.tar.gz

---
+++ packages/pyskeleton/pyskeleton-0.3.3.tar.gz/pyskeleton-0.3.3/tests/test_hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+from pyskeleton import print_version
+
+
+def test_version():
+    assert print_version() == '0.3.3'
+
+
+
+
+# if __name__ == '__main__':
