2d200c66b44eee440a2686c5fcd415acd61d295c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:52 +0000

    Add package dailymail_tcs/0.6/dailymail_tcs-0.6.tar.gz

---
+++ packages/dailymail_tcs/dailymail_tcs-0.6.tar.gz/dailymail_tcs-0.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dailymail_tcs',
+  packages = ['dailymail_tcs'],
+  version = '0.6',
+  description = 'Extract title, summary and content from dailymail corpus.',
+  author = 'Ajjo',
+  author_email = 'ajjogames@gmail.com',
+  url = 'https://github.com/ajjo/dailymail-tcs.git',
+  download_url = 'https://github.com/ajjo/dailymail-tcs/archive/0.6.tar.gz',
+  keywords = ['dailymail', 'title', 'summary','content'], 
+  classifiers = [],
+)
