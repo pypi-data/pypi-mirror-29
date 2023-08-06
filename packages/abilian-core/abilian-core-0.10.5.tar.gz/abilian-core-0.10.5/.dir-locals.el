0b47d46623947455c9b10f66db2256e669d5497c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:37 +0000

    Add package abilian-core/0.10.5/abilian-core-0.10.5.tar.gz

---
+++ packages/abilian-core/abilian-core-0.10.5.tar.gz/abilian-core-0.10.5/.dir-locals.el
@@ -0,0 +1,15 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode
+  (python-indent-offset . 4)
+  (py-continuation-offset . 4)
+  (py-smart-indentation)
+  (py-indent-offset . 2)
+  (flycheck-pylintrc . "pylintrc")))
+
+
+
+
+
+
