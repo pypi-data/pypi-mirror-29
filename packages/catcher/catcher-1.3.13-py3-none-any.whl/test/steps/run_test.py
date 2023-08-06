ad6854a148e8d2b885cb11de39683d6155dfc714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:16 +0000

    Add package catcher/1.3.13/catcher-1.3.13-py3-none-any.whl

---
+++ packages/catcher/catcher-1.3.13-py3-none-any.whl/test/steps/run_test.py
@@ -0,0 +1,50 @@
+from os.path import join
+
+import os
+
+import requests_mock
+
+from catcher.core.runner import Runner
+from test.abs_test_class import TestClass
+from test.test_utils import check_file
+
+
+class RunTest(TestClass):
+    def __init__(self, method_name):
+        super().__init__('run_test', method_name)
+
+    # test run one action
+    def test_run_one_action(self):
+        self.populate_file('main.yaml', '''---
+        steps:
+            - echo: {from: 'test', to: main.output}
+        ''')
+        runner = Runner(self.test_dir, join(self.test_dir, 'main.yaml'), None)
+        runner.run_tests()
+        self.assertTrue(check_file(join(self.test_dir, 'main.output'), 'test'))
+
+    # test run several actions
+    def test_run_several_actions(self):
+        self.populate_file('main.yaml', '''---
+        steps:
+            - echo: {from: 'test1', to: main1.output}
+            - echo: {from: 'test2', to: main2.output}
+        ''')
+        runner = Runner(self.test_dir, join(self.test_dir, 'main.yaml'), None)
+        runner.run_tests()
+        self.assertTrue(check_file(join(self.test_dir, 'main1.output'), 'test1'))
+        self.assertTrue(check_file(join(self.test_dir, 'main2.output'), 'test2'))
+
+    # test run ignore errors
+    @requests_mock.mock()
+    def test_run_ignore_errors(self, m):
+        m.get('http://test.com', status_code=500)
+        self.populate_file('main.yaml', '''---
+        steps:
+            - http: {get: {url: 'http://test.com', response_code: 200}, ignore_errors: true}
+            - echo: {from: 'test2', to: main2.output}
+        ''')
+        runner = Runner(self.test_dir, join(self.test_dir, 'main.yaml'), None)
+        runner.run_tests()
+        self.assertTrue(not os.path.exists(join(self.test_dir, 'main1.output')))
+        self.assertTrue(check_file(join(self.test_dir, 'main2.output'), 'test2'))
