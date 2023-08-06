309beeedbdfd72ee1ec6ffdec65a83e8638afe97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:46 +0000

    Add package gridaurora/1.1.7/gridaurora-1.1.7.tar.gz

---
+++ packages/gridaurora/gridaurora-1.1.7.tar.gz/gridaurora-1.1.7/gridaurora/filterload.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+from pathlib import Path
+import logging
+from numpy import exp, log, ones_like, isfinite,spacing,column_stack,empty,asarray
+from scipy.interpolate import interp1d
+import h5py
+from xarray import DataArray
+# consider atmosphere
+try:
+    from lowtran import golowtran
+except ImportError as e:
+    logging.error(f'failure to load LOWTRAN, proceeding without atmospheric absorption model.  {e}')
+    golowtran=None
+'''
+gets optical System Transmittance from filter, sensor window, and QE spec.
+Michael Hirsch 2014
+references:
+BG3 filter datasheet: http://www.howardglass.com/pdf/bg3_datasheet.pdf
+QE: http://www.andor.com/pdfs/specifications/Andor_iXon_Ultra_897_Specifications.pdf
+     http://occult.mit.edu/instrumentation/MORIS/Documents/DU-897_BI.pdf
+window: http://www.andor.com/pdfs/specifications/Andor_Camera_Windows_Supplementary_Specifications.pdf
+'''
+
+def getSystemT(newLambda, bg3fn:Path, windfn,qefn,obsalt_km,zenang_deg, verbose=False):
+
+    bg3fn = Path(bg3fn).expanduser()
+
+    windfn = Path(windfn).expanduser()
+    qefn = Path(qefn).expanduser()
+
+    newLambda = asarray(newLambda)
+#%% atmospheric absorption
+    if golowtran is not None:
+        c1={'model':5,'itype':3,'iemsct':0,'h1':obsalt_km,'angle':zenang_deg,
+            'wlnmlim':(newLambda[0],newLambda[-1])}
+        if verbose:
+            print('loading LOWTRAN7 atmosphere model...')
+        atmT = golowtran(c1).loc[...,'transmission']
+        try:
+            atmTcleaned = atmT.values.squeeze()
+            atmTcleaned[atmTcleaned==0] = spacing(1) # to avoid log10(0)
+            fwl = interp1d(atmT.wavelength_nm,log(atmTcleaned),axis=0)
+        except AttributeError: #problem with lowtran
+            fwl = interp1d(newLambda,log(ones_like(newLambda)),kind='linear')
+    else:
+        fwl = interp1d(newLambda,log(ones_like(newLambda)),kind='linear')
+
+    atmTinterp = exp(fwl(newLambda))
+    if not isfinite(atmTinterp).all():
+        logging.error('problem in computing LOWTRAN atmospheric attenuation, results are suspect!')
+#%% BG3 filter
+    with h5py.File(bg3fn,'r',libver='latest') as f:
+        try:
+            assert isinstance(f['/T'], h5py.Dataset),'we only allow one transmission curve per file' # simple legacy behavior
+            fbg3  = interp1d(f['/wavelength'], log(f['/T']), kind='linear', bounds_error=False)
+        except KeyError:
+            raise KeyError('could not find /wavelength in {}'.format(f.filename))
+
+        try:
+            fname = f['T'].attrs['name'].item()
+            if isinstance(fname,bytes):
+                fname = fname.decode('utf8')
+        except KeyError:
+            fname =''
+#%% camera window
+    with h5py.File(windfn,'r',libver='latest') as f:
+        fwind = interp1d(f['/lamb'], log(f['/T']), kind='linear')
+#%% quantum efficiency
+    with h5py.File(qefn,'r',libver='latest') as f:
+        fqe =  interp1d(f['/lamb'], log(f['/QE']), kind='linear')
+#%% collect results into DataArray
+
+    T = DataArray(column_stack((exp(fbg3(newLambda)),
+                                exp(fwind(newLambda)),
+                                exp(fqe(newLambda)),
+                                atmTinterp,
+                                empty(newLambda.size),
+                                empty(newLambda.size))),
+                  coords={'wavelength_nm':newLambda,
+                         'filt':['filter','window','qe','atm','sysNObg3','sys']},
+                  dims=['wavelength_nm','filt'])
+                                           #atm is ALREADY exp()
+
+    T.loc[:,'sysNObg3'] = T.sel(filt=['window','qe','atm']).prod('filt')
+    T.loc[:,'sys']      = T.sel(filt=['window','qe','filter','atm']).prod('filt')
+
+    return T, fname
---
+++ packages/gridaurora/gridaurora-1.1.7.tar.gz/gridaurora-1.1.7/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+install_requires = ['python-dateutil','pytz','numpy','scipy','xarray','h5py',
+                    'sciencedates']
+tests_require=['pytest','nose','coveralls']
+# %%
+from setuptools import setup,find_packages
+
+setup(name='gridaurora',
+      packages=find_packages(),
+      author='Michael Hirsch, Ph.D.',
+      description='Gridding for auroral and ionospheric modeling',
+      url='https://github.com/scivision/gridaurora',
+      version='1.1.7',
+      package_data={'gridaurora':['data/RecentIndices.txt'],
+                    'gridaurora.precompute': ['*.h5']},
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Topic :: Scientific/Engineering :: Atmospheric Science',
+          'Programming Language :: Python :: 3',
+          ],
+      install_requires=install_requires,
+      python_requires='>=3.6',
+      extras_require={'plot':['matplotlib', 'seaborn', 'pathvalidate'],
+                      'io':['lowtran','transcarread','astropy','pandas',],
+                      'tests':tests_require},
+      tests_require=tests_require,
+	  )
+
