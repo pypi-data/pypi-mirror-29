65b69e0cbe1547dd9a6c8b8ae6a92ea42cca71a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:58 +0000

    Add package onespacemedia-server-management/3.0.5/onespacemedia_server_management-3.0.5-py3-none-any.whl

---
+++ packages/onespacemedia-server-management/onespacemedia_server_management-3.0.5-py3-none-any.whl/server_management/management/commands/backupdb.py
@@ -0,0 +1,38 @@
+from django.utils.timezone import now
+from fabric.api import env, local, settings, sudo
+
+from ._core import ServerManagementBaseCommand, load_config
+
+
+class Command(ServerManagementBaseCommand):
+
+    def handle(self, *args, **options):
+        # Load server config from project
+        config, remote = load_config(env, options.get('remote', ''), debug=options.get('debug', False))
+
+        with settings(warn_only=True):
+            # Dump the database on the server.
+            sudo('su - {user} -c \'pg_dump {name} -cOx -U {user} -f /home/{user}/{name}.sql --clean\''.format(
+                name=remote['database']['name'],
+                user=remote['database']['user'],
+            ))
+
+            # Create a backups folder
+            local('mkdir -p ~/Backups/')
+
+            # Pull the SQL file down.
+            local('scp {} {}@{}:/home/{}/{}.sql ~/Backups/{}-{}.sql'.format(
+                '' if not getattr(env, 'key_filename') else ' -i {} '.format(env.key_filename),
+                env.user,
+                env.host_string,
+                remote['database']['user'],
+                remote['database']['name'],
+                config['local']['database']['name'],
+                now().isoformat(),
+            ))
+
+            # Delete the file on the server.
+            sudo('rm /home/{}/{}.sql'.format(
+                remote['database']['user'],
+                remote['database']['name'],
+            ))
