7b64d5da9a238e6416ac6bf1743a7b02e58c7468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:30 +0000

    Add package penaltymodel/0.12.2/penaltymodel-0.12.2-py2.py3-none-any.whl

---
+++ packages/penaltymodel/penaltymodel-0.12.2-py2.py3-none-any.whl/penaltymodel/classes/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+from penaltymodel.classes.vartypes import *
+import penaltymodel.classes.vartypes
+
+from penaltymodel.classes.binary_quadratic_model import *
+import penaltymodel.classes.binary_quadratic_model
+
+from penaltymodel.classes.specification import *
+import penaltymodel.classes.specification
+
+from penaltymodel.classes.penaltymodel import *
+import penaltymodel.classes.penaltymodel
