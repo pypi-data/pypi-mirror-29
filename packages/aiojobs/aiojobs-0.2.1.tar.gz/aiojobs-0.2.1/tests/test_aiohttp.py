334d27afaf6757ab0a0c101bb62c9073629a349a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:12 +0000

    Add package aiojobs/0.2.1/aiojobs-0.2.1.tar.gz

---
+++ packages/aiojobs/aiojobs-0.2.1.tar.gz/aiojobs-0.2.1/tests/test_aiohttp.py
@@ -0,0 +1,89 @@
+import asyncio
+
+import pytest
+from aiohttp import web
+
+from aiojobs.aiohttp import atomic, get_scheduler, get_scheduler_from_app
+from aiojobs.aiohttp import setup as aiojobs_setup
+from aiojobs.aiohttp import spawn
+
+
+async def test_plugin(test_client):
+    job = None
+
+    async def coro():
+        await asyncio.sleep(10)
+
+    async def handler(request):
+        nonlocal job
+
+        job = await spawn(request, coro())
+        assert not job.closed
+        return web.Response()
+
+    app = web.Application()
+    app.router.add_get('/', handler)
+    aiojobs_setup(app)
+
+    client = await test_client(app)
+    resp = await client.get('/')
+    assert resp.status == 200
+
+    assert job.active
+    await client.close()
+    assert job.closed
+
+
+async def test_no_setup(test_client):
+    async def handler(request):
+        with pytest.raises(RuntimeError):
+            get_scheduler(request)
+        return web.Response()
+
+    app = web.Application()
+    app.router.add_get('/', handler)
+
+    client = await test_client(app)
+    resp = await client.get('/')
+    assert resp.status == 200
+
+
+async def test_atomic(test_client):
+    @atomic
+    async def handler(request):
+        await asyncio.sleep(0)
+        return web.Response()
+
+    app = web.Application()
+    app.router.add_get('/', handler)
+    aiojobs_setup(app)
+
+    client = await test_client(app)
+    resp = await client.get('/')
+    assert resp.status == 200
+
+    scheduler = get_scheduler_from_app(app)
+
+    assert scheduler.active_count == 0
+    assert scheduler.pending_count == 0
+
+
+async def test_atomic_from_view(test_client):
+    app = web.Application()
+
+    class MyView(web.View):
+        @atomic
+        async def get(self):
+            return web.Response()
+
+    app.router.add_route("*", "/", MyView)
+    aiojobs_setup(app)
+
+    client = await test_client(app)
+    resp = await client.get('/')
+    assert resp.status == 200
+
+    scheduler = get_scheduler_from_app(app)
+
+    assert scheduler.active_count == 0
+    assert scheduler.pending_count == 0
