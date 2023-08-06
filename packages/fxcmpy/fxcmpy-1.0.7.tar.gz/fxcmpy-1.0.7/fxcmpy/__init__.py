97895511c4d6768c61df7f6705119e10bf05373d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:30 +0000

    Add package fxcmpy/1.0.7/fxcmpy-1.0.7.tar.gz

---
+++ packages/fxcmpy/fxcmpy-1.0.7.tar.gz/fxcmpy-1.0.7/fxcmpy/__init__.py
@@ -0,0 +1,8 @@
+from fxcmpy.fxcmpy import fxcmpy as fxcmpy  
+from fxcmpy.fxcmpy_open_position import fxcmpy_open_position 
+from .fxcmpy_closed_position import *
+from .fxcmpy_order import *
+from .fxcmpy_oco_order import *
+
+import version
+__version__ = version.version
