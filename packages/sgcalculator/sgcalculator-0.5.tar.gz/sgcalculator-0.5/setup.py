2adcb495bc52c1b3bbb5d80ffe41a82785818136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:19 +0000

    Add package sgcalculator/0.5/sgcalculator-0.5.tar.gz

---
+++ packages/sgcalculator/sgcalculator-0.5.tar.gz/sgcalculator-0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sgcalculator',
+      version='0.5',
+      description=' Assignemnt for interview ',
+      url='https://bitbucket.org/renford/sgcalculator',
+      author='Renford Alexander',
+      author_email='alexander.Renford@gmail.com',
+      license='MIT',
+      packages=['sgcalculator'],
+      zip_safe=False)
