a2e6c73fa630dd70dcc79cc3acbeba6dad84979c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:48 +0000

    Add package django-social-share-lijo/0.1/django_social_share_lijo-0.1.tar.gz

---
+++ packages/django-social-share-lijo/django_social_share_lijo-0.1.tar.gz/django_social_share_lijo-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django_social_share_lijo',
+  packages = ['django_social_share_lijo'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A package for django social share',
+  author = 'Lijo Varghese',
+  author_email = 'lijov97@gmail.com',
+  url = 'https://github.com/lijoev/django_social_share.git', # use the URL to the github repo
+  download_url = 'https://github.com/lijoev/django_social_share/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
