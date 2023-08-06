05a33aae58bbee67cae884e2169ea0c2a2c77a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:24 +0000

    Add package nauka/0.0.2/nauka-0.0.2+6e6fadc8b83c0e14223f2bf29b66529a3839a8bb.tar.gz

---
+++ packages/nauka/nauka-0.0.2+6e6fadc8b83c0e14223f2bf29b66529a3839a8bb.tar.gz/nauka-0.0.2/src/nauka/version.py
@@ -0,0 +1,10 @@
+#
+# THIS FILE IS GENERATED FROM NAUKA SETUP.PY
+#
+short_version = "0.0.2"
+version       = "0.0.2"
+full_version  = "0.0.2"
+git_revision  = "6e6fadc8b83c0e14223f2bf29b66529a3839a8bb"
+sem_revision  = "0.0.2"
+release       = True
+build_time    = "20180313T084921Z"
