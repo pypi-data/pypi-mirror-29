e1b492b08a13fdf4302f6ad319045e006afdffb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:52 +0000

    Add package deployto/0.1.2/deployto-0.1.2-py2.py3-none-any.whl

---
+++ packages/deployto/deployto-0.1.2-py2.py3-none-any.whl/deployto/package.py
@@ -0,0 +1,31 @@
+from io import BytesIO
+import zipfile
+import glob
+import os
+
+
+def package(paths):
+    file_like_object = BytesIO()
+    with zipfile.ZipFile(file_like_object, 'w') as zipfile_ob:
+        for path in paths:
+            split = path.split(':', 1)
+            source = split[0]
+            target = source
+
+            if len(split) == 2:
+                target = split[1]
+            if os.path.isfile(source):
+                if not target:
+                    target = os.path.basename(source)
+                zipfile_ob.write(source, target, zipfile.ZIP_DEFLATED)
+            elif os.path.isdir(source):
+                for name in glob.glob("{}/*".format(source)):
+                    target_file = target + '/' + os.path.basename(name)
+                    zipfile_ob.write(name, target_file, zipfile.ZIP_DEFLATED)
+            else:
+                print('Why')
+                print(source)
+                print(target)
+        for name in zipfile_ob.namelist():
+            print(name)
+    return file_like_object.getvalue()
