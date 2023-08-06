f9daa13bb7c5219166e4f143ac775a6999616eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:48 +0000

    Add package notifhandler/0.1/notifhandler-0.1.tar.gz

---
+++ packages/notifhandler/notifhandler-0.1.tar.gz/notifhandler-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'notifhandler',
+  packages = ['notifhandler'], # this must be the same as the name above
+  version = '0.1',
+  description = 'GOR Notification Handler Tool',
+  author = 'Ayush Agarwal',
+  author_email = 'ayush.a@greyorange.sg',
+  url = '', # use the URL to the github repo
+  download_url = '', # I'll explain this in a second
+  keywords = ['notif-handler', 'notification'], # arbitrary keywords
+  classifiers = [],
+)
