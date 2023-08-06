89e96cb896a9bdf9c66e94c8704f966a213e5d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:05 +0000

    Add package skivvy/0.11/skivvy-0.11.tar.gz

---
+++ packages/skivvy/skivvy-0.11.tar.gz/skivvy-0.11/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='skivvy',
+    packages=['skivvy'],
+    version='0.11',
+    description='A simple tool for testing JSON/HTTP APIs',
+    author='Jonas Holmer',
+    author_email='jonas.holmer@gmail.com',
+    url='https://github.com/hyrfilm/skivvy',
+    download_url='https://github.com/hyrfilm/skivvy/archive/0.1.tar.gz',
+    keywords=['testing', 'automation', 'unit-testing', 'json', 'API', 'HTTP'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Intended Audience :: Developers',
+                 'Topic :: Software Development :: Quality Assurance',
+                 'Topic :: Software Development :: Testing',
+                 'Programming Language :: Python'],
+    install_requires=['pyopenssl', 'requests', 'docopt']
+)
