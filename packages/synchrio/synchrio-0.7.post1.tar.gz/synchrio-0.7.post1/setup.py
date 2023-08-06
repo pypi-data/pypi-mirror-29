20f4e9a8d04ae830374e29d101a5c7a19c8aa0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:46 +0000

    Add package synchrio/0.7.post1/synchrio-0.7.post1.tar.gz

---
+++ packages/synchrio/synchrio-0.7.post1.tar.gz/synchrio-0.7.post1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+__author__ = "J Patrick Dill"
+__version__ = "0.7r1"
+
+setup(
+    name="synchrio",
+    description="Wrapper class to run async code synchronously",
+
+    licence="GPLv3",
+    url="https://github.com/reshanie/synchrio/",
+
+    keywords="asyncio async blocking non-blocking",
+
+    version=__version__,
+    packages=["synchrio"],
+
+    author=__author__,
+    author_email="jamespatrickdill@gmail.com",
+
+
+)
