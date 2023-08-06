e62b712ad95e0f83b34071f0e6aa29e4d1f13266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:59 +0000

    Add package boomband/0.1/boomband-0.1.tar.gz

---
+++ packages/boomband/boomband-0.1.tar.gz/boomband-0.1/README.md
@@ -0,0 +1,3 @@
+#Boomband
+
+This is to test on how to make a package pelease don't take this seriously
---
+++ packages/boomband/boomband-0.1.tar.gz/boomband-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+  name = 'boomband',
+  packages = ['boomband'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Test for Boomband wifi service',
+  author = 'Mehtab zafar',
+  author_email = 'zafardaffer.6@gmail.com',
+  url= 'https://github.com/mzfr/package'
+  )
