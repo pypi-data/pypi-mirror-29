d9d647b6228beb870670dba0e3f1a7edca1f731c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:55 +0000

    Add package tmep/1.0.9/tmep-1.0.9.tar.gz

---
+++ packages/tmep/tmep-1.0.9.tar.gz/tmep-1.0.9/setup.py
@@ -0,0 +1,30 @@
+import versioneer
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='tmep',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Josef Friedrich',
+    author_email='josef@friedrich.rocks',
+    description=('Template and Macros Expansion for Path names.'),
+    license='MIT',
+    packages=['tmep'],
+    url='https://github.com/Josef-Friedrich/tmep',
+    install_requires=[
+        'unidecode',
+        'six',
+    ],
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Utilities',
+    ],
+    scripts=['bin/tmep-doc'],
+    zip_safe=False, )
