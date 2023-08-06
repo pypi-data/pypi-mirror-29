fefe3e0b352d567f95fa71dbbf9552b90968ee83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:07 +0000

    Add package canopen/0.6.0rc1/canopen-0.6.0rc1.tar.gz

---
+++ packages/canopen/canopen-0.6.0rc1.tar.gz/canopen-0.6.0rc1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+exec(open('canopen/version.py').read())
+
+description = open("README.rst").read()
+# Change links to stable documentation
+description = description.replace("/latest/", "/stable/")
+
+setup(
+    name="canopen",
+    url="https://github.com/christiansandberg/canopen",
+    version=__version__,
+    packages=find_packages(),
+    author="Christian Sandberg",
+    author_email="christiansandberg@me.com",
+    description="CANopen stack implementation",
+    keywords="CAN CANopen",
+    long_description=description,
+    license="MIT",
+    platforms=["any"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering"
+    ],
+    install_requires=["python-can>=2.0.0", "canmatrix"],
+    include_package_data=True,
+
+    # Tests can be run using `python setup.py test`
+    test_suite="nose.collector",
+    tests_require=["nose"]
+)
