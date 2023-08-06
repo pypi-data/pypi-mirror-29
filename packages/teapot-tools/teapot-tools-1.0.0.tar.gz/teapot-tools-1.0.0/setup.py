36a50c830a907fb62f7e7cb612655e7b0dfdb3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:42 +0000

    Add package teapot-tools/1.0.0/teapot-tools-1.0.0.tar.gz

---
+++ packages/teapot-tools/teapot-tools-1.0.0.tar.gz/teapot-tools-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'teapot-tools',
+    packages = ['teapot-tools'],
+    version = '1.0.0',
+    description = 'A python module to have fun with the Google 418 error.',
+    author = 'DismissedGuy',
+    author_email = 'dismissedguy#2118@discordmail.com',
+    url = 'https://github.com/Teapot-Tools/teapot-tools',
+    download_url = 'https://github.com/Teapot-Tools/teapot-tools/archive/1.0.0.tar.gz',
+    keywords = ['teapot', '418', 'teaset'],
+    classifiers = [],
+)
