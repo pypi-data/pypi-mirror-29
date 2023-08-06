8944e820664724f39738efba0ec09842596962ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:17 +0000

    Add package tfutil/0.1/tfutil-0.1.tar.gz

---
+++ packages/tfutil/tfutil-0.1.tar.gz/tfutil-0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='tfutil',
+        version='0.1',
+        #packages=setuptools.find_packages(exclude=['tests', 'docs']),
+        packages=['tfutil']
+    )
