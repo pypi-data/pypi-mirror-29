de17fe535dfd2bbd32f22b15cde4ca6d6bf3d083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:55 +0000

    Add package charpy/0.0.1/charpy-0.0.1.tar.gz

---
+++ packages/charpy/charpy-0.0.1.tar.gz/charpy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='charpy',
+      version='0.0.1',
+      description='Render chart from data',
+      url='http://github.com/sylhare/chartapp',
+      author='sylhare',
+      author_email='sylhare@outlook.com',
+      license='MIT',
+      packages=['charpy'],
+      install_requires=['flask'],
+      zip_safe=False)
