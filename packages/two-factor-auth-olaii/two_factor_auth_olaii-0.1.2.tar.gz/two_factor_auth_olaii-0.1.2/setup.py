552a6c588ed5369d4b43a1dd870a10cbb18d078d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:52 +0000

    Add package two-factor-auth-olaii/0.1.2/two_factor_auth_olaii-0.1.2.tar.gz

---
+++ packages/two-factor-auth-olaii/two_factor_auth_olaii-0.1.2.tar.gz/two_factor_auth_olaii-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'two_factor_auth_olaii',
+  packages = ['two_factor_auth'],
+  package_data={'two_factor_auth':['migrations/*', 'templates/*', 'oauth2/*', 'views/*']},
+  include_package_data=True,
+  version = '0.1.2',
+  description = 'Two-factor authentication for Django based on django-two-factor-auth and deux.',
+  author = 'Polona Remic',
+  author_email = 'polona@olaii.com',
+  url = 'https://gitlab.xlab.si/olaii/Olaii_2FA',
+  download_url = 'https://gitlab.xlab.si/olaii/Olaii_2FA',
+  install_requires=[
+        'Django>=1.11',
+        'django_otp>=0.4.2,<0.99',
+        'qrcode>=5.3',
+        'django-formtools',
+  ],
+  keywords = ['django', 'two-factor authentication'], # arbitrary keywords
+  classifiers = [],
+)
+
