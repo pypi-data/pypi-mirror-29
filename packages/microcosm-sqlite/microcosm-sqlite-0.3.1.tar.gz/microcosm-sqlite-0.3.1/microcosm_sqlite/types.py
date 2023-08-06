4c6bca4807508c988ae3c116668c571053db2454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:55 +0000

    Add package microcosm-sqlite/0.3.1/microcosm-sqlite-0.3.1.tar.gz

---
+++ packages/microcosm-sqlite/microcosm-sqlite-0.3.1.tar.gz/microcosm-sqlite-0.3.1/microcosm_sqlite/types.py
@@ -0,0 +1,25 @@
+"""
+Custom types.
+
+"""
+from distutils.util import strtobool
+from sqlalchemy.types import Boolean, TypeDecorator
+
+
+class Truthy(TypeDecorator):
+    """
+    Truthy-aware boolean value.
+
+    Supports string-valued inputs (and handles them as gracefully as possible)
+
+    """
+    impl = Boolean
+
+    def process_bind_param(self, value, dialect):
+        if isinstance(value, bool):
+            return value
+        if isinstance(value, str):
+            if not value:
+                return False
+            return strtobool(value)
+        return bool(value)
