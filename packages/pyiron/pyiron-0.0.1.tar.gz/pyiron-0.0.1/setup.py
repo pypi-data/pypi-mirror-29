b3c2f939d8a9d101f0fc0874989e1c1ac71a67fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:57 +0000

    Add package pyiron/0.0.1/pyiron-0.0.1.tar.gz

---
+++ packages/pyiron/pyiron-0.0.1.tar.gz/pyiron-0.0.1/setup.py
@@ -0,0 +1,36 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyiron',
+    version='0.0.1',
+    description='pyiron IDE base',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/jan-janssen/pyirondist',
+    author='Jan Janssen (MPIE)',
+    author_email='janssen@mpie.de',
+    license='BSD',
+
+    classifiers=[
+
+        'Development Status :: 4 - Beta',
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(),
+    install_requires=['pyironbase']
+    )
