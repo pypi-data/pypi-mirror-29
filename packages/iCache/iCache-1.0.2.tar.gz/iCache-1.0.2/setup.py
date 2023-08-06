c750f034ae172a0df13cf3ea1940f17b0ef107c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:12 +0000

    Add package iCache/1.0.2/iCache-1.0.2.tar.gz

---
+++ packages/iCache/iCache-1.0.2.tar.gz/iCache-1.0.2/setup.py
@@ -0,0 +1,27 @@
+import codecs
+import os
+import sys
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name='iCache',
+    version='1.0.2',
+    description='A Python Cache Model',
+    url='https://github.com/15281029/PyCache',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    author='Zhangbo',
+    author_email='15281029@bjtu.edu.cn',
+    py_modules=['iCache'],
+    license='MIT',
+    include_package_data=True,
+    zip_safe=True,
+)
