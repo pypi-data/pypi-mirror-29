54bdfca031555880b3f25b7779b92eee6ca33161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:51 +0000

    Add package nestercshao/1.0.0/nestercshao-1.0.0.tar.gz

---
+++ packages/nestercshao/nestercshao-1.0.0.tar.gz/nestercshao-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name			= 'nestercshao',
+		version			= '1.0.0',
+		py_modules		= ['nestercshao'],
+		author			= 'shaochuancs',
+		author_email	= 'shaochuancs@gmail.com',
+		url				= 'http://blog.shaochuancs.com',
+		description		= 'A simple printer'
+	)
