3f9e514dfe5469011ef1b3f850395c7ea1c61c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:43 +0000

    Add package sentry-swift-nodestore/0.1.2/sentry-swift-nodestore-0.1.2.tar.gz

---
+++ packages/sentry-swift-nodestore/sentry-swift-nodestore-0.1.2.tar.gz/sentry-swift-nodestore-0.1.2/sentry_swift_nodestore/backend.py
@@ -0,0 +1,38 @@
+"""
+sentry_swift_nodestore.backend
+~~~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2017 by Phillip Couto.
+:license: BSD, see LICENSE for more details.
+"""
+from __future__ import absolute_import
+
+import json
+from time import sleep
+
+import swiftclient
+
+from sentry.nodestore.base import NodeStorage
+
+class SwiftNodeStorage(NodeStorage):
+
+    def __init__(self, container_name=None, auth_url=None, user=None, key=None,
+        ttl=None, cacert=None):
+        print(cacert)
+        self.conn = swiftclient.Connection(auth_url, user, key, cacert, auth_version=1)
+        
+        self.container = container_name
+        self.ttl = ttl
+
+    def delete(self, id):
+        if self.ttl != None:
+            hdrs = {
+                'X-Delete-After': str(self.ttl)
+            }
+            self.conn.post_object(self.container, id, hdrs)
+        else:
+            self.conn.delete_object(self.container, id)
+    def get(self, id):
+        hdrs, body = self.conn.get_object(self.container, id)
+        return json.loads(body)
+    def set(self, id, data):
+        self.conn.put_object(self.container, id, json.dumps(data))
