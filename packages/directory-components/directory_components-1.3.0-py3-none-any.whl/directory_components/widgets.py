02ae519f00269f9bab982a9f1e6cdd600793cbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:35 +0000

    Add package directory-components/1.3.0/directory_components-1.3.0-py3-none-any.whl

---
+++ packages/directory-components/directory_components-1.3.0-py3-none-any.whl/directory_components/widgets.py
@@ -0,0 +1,35 @@
+from django.forms import widgets
+from django import forms
+
+
+class RadioSelect(widgets.RadioSelect):
+    template_name = 'directory_components/radio.html'
+    option_template_name = 'directory_components/radio_option.html'
+
+
+class CheckboxWithInlineLabel(forms.widgets.CheckboxInput):
+    template_name = 'directory_components/checkbox_inline.html'
+
+    def __init__(self, label='', help_text=None, *args, **kwargs):
+        self.label = label
+        self.help_text = help_text
+        super().__init__(*args, **kwargs)
+
+    def get_context(self, *args, **kwargs):
+        context = super().get_context(*args, **kwargs)
+        context['label'] = self.label
+        context['help_text'] = self.help_text
+        return context
+
+
+class ComponentsCheckboxSelectMultiple(forms.CheckboxSelectMultiple):
+    template_name = 'directory_components/checkbox_select.html'
+
+
+class CheckboxSelectInlineLabelMultiple(ComponentsCheckboxSelectMultiple):
+    option_template_name = 'directory_components/checkbox_inline_multiple.html'
+    css_class_name = 'checkbox-multiple'
+
+    def __init__(self, attrs=None):
+        super().__init__(attrs=attrs)
+        self.attrs['class'] = self.attrs.get('class', self.css_class_name)
