2eef3438dc030f85138a8297289cbc5a86ed4e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:49 +0000

    Add package bubble-dubble/2018.3.7/bubble-dubble-2018.3.7.tar.gz

---
+++ packages/bubble-dubble/bubble-dubble-2018.3.7.tar.gz/bubble-dubble-2018.3.7/bubble/frozen.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+hg_hash = "80fcee9951e8"
+version = "2018.3.7"
+url = "https://soft.snbl.eu/_sources/bubble.rst.txt"
+token = ".. Bubble Versions"
+text = "A new Bubble version is available at <a href=https://soft.snbl.eu/bubble.html#download>soft.snbl.eu</a>"
