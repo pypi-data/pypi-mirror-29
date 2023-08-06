60cdb6026646dc8e6e18105182a3d48f95066ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:16 +0000

    Add package simplegraphs/0.1/simplegraphs-0.1.tar.gz

---
+++ packages/simplegraphs/simplegraphs-0.1.tar.gz/simplegraphs-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='simplegraphs',
+      version='0.1',
+      description='Simple tools for building and analyzing graphs',
+      url='https://github.com/DamourYouKnow/simplegraphs',
+      author='DamourYouKnow',
+      author_email='damourba@gmail.com',
+      license='MIT',
+      packages=['simplegraphs'],
+      install_requires=[],
+      zip_safe=False)
