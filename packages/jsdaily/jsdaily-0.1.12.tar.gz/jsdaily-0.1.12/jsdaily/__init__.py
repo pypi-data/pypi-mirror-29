cd5583e7454fef9b2140a5fa445a8c288404e729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:48 +0000

    Add package jsdaily/0.1.12/jsdaily-0.1.12.tar.gz

---
+++ packages/jsdaily/jsdaily-0.1.12.tar.gz/jsdaily-0.1.12/jsdaily/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+from .update import main as update
+from .uninstall import main as uninstall
+from .reinstall import main as reinstall
+from .postinstall import main as postinstall
+from .dependency import main as dependency
