f92e884df40b85846ed967931bb304f1305b3ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:05 +0000

    Add package woopra.py/0.1.0/woopra.py-0.1.0.tar.gz

---
+++ packages/woopra.py/woopra.py-0.1.0.tar.gz/woopra.py-0.1.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['requests',]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Ben Lopatin",
+    author_email='ben@benlopatin.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="Alternative Woopra integration with Django support",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='woopra',
+    name='woopra.py',
+    packages=find_packages(include=['woopra_tracker']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/bennylope/woopra.py',
+    version='0.1.0',
+    zip_safe=False,
+)
