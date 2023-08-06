48a5a1c3aab3bb9ee47ccc8aa5a089292569acf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:57 +0000

    Add package django-restrictmethodorigin/0.1/django-restrictmethodorigin-0.1.tar.gz

---
+++ packages/django-restrictmethodorigin/django-restrictmethodorigin-0.1.tar.gz/django-restrictmethodorigin-0.1/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-restrictmethodorigin',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',  
+    description='A simple Django app to restrict http methods to specific origin',
+    long_description="""DJANGO-RESTRICTMETHODORIGIN is a simple Django app to bind origins with http methods.
+                        So that its possible to allow ony speicific origins to make speicific http requests like POST, GET, PUT etc""",
+    #long_description=README.rst,
+    url='https://github.com/Ruhshan/django-restrictmethodorigin',
+    author='Ruhshan Ahmed Abir',
+    author_email='ruhshan.ahmed@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',  
+        'Intended Audience :: Developers',  
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    python_requires='>=3',
+    keywords='django restrictmethod origin',
+
+)
