fdc7c5320debbd73a56b8eb15fd7be2d3d9344f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:22 +0000

    Add package python-trustwave-appscanner/0.1.2.dev0/python_trustwave_appscanner-0.1.2.dev0-py2-none-any.whl

---
+++ packages/python-trustwave-appscanner/python_trustwave_appscanner-0.1.2.dev0-py2-none-any.whl/appscanner/factories.py
@@ -0,0 +1,5 @@
+from tapioca_trustwave import Trustwave
+
+
+def get_assessment_run_result(application_id, assessment_run_id):
+    api = Trustwave()
---
+++ packages/python-trustwave-appscanner/python_trustwave_appscanner-0.1.2.dev0-py2-none-any.whl/python_trustwave_appscanner-0.1.2.dev0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,9 @@
+Python Trustwave Appscanner
+===========================
+
+
+```python
+class ThisIsATest:
+    def __init__(self):
+```
+
