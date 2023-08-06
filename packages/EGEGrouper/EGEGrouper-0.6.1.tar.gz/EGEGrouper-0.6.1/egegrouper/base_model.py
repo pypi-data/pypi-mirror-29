ae5bfd1a3c676542da5a2f1938bd9297709c503f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:22 +0000

    Add package EGEGrouper/0.6.1/EGEGrouper-0.6.1.tar.gz

---
+++ packages/EGEGrouper/EGEGrouper-0.6.1.tar.gz/EGEGrouper-0.6.1/egegrouper/base_model.py
@@ -0,0 +1,296 @@
+# EGEGrouper - Software for grouping electrogastroenterography examinations.
+
+# Copyright (C) 2017-2018 Aleksandr Popov
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from abc import ABC, abstractmethod
+
+class BaseModel(ABC):
+    """Model in MVC. Base class."""
+    
+    def __init__(self):
+        """Constructor.
+        
+        Create fields and set initial state of model.
+        
+        """
+        self._state = {}
+
+    def set_state(self, **kwargs):
+        """Set model state."""
+        for key in kwargs:
+            self._state[key] = kwargs[key]
+
+    def state(self):
+        """Return model state."""
+        return self._state
+
+    @staticmethod
+    def do_if_storage_opened(method):
+        """Decorator. If storage is not opened AttributeError raised."""
+        def wrapped(self, *args, **kwargs):
+            if not self.state()['storage_opened']:
+                raise AttributeError('Storage is not opened.')
+            return method(self, *args, **kwargs)
+        wrapped.__doc__ = method.__doc__
+        return wrapped
+
+    def open_or_create_storage(self, name):
+        """Open or create storage (if stoarge not exists).
+        
+        Parameters
+        ----------
+        name : str
+            Name of storage.
+        
+        """
+        if self.storage_exists(name):
+            self.open_storage(name)
+        else:
+            self.create_storage(name)
+
+    @abstractmethod
+    def create_storage(self, name):
+        """Create new storage.
+
+        Parameters
+        ----------
+        name : str
+            Storage name.
+
+        """
+        pass
+
+    @abstractmethod
+    def open_storage(self, name):
+        """Open storage.
+
+        Parameters
+        ----------
+        name : str
+            Storage name.
+
+        """
+        pass
+
+    @abstractmethod
+    def close_storage(self):
+        """Close current storage."""
+        pass
+
+    @abstractmethod
+    def storage_exists(self, name):
+        """Check if the storage exists.
+
+        Parameters
+        ----------
+        name : str
+            Name of storage.
+
+        Returns
+        -------
+        : bool
+            True if exists, False otherwise.
+
+        """
+        pass
+
+    @abstractmethod
+    def storage_info(self):
+        """Return common information about current storage.
+
+        Return
+        ------
+        data : list of tuples
+            Table with information about storage.
+        headers : tuple
+            Headers.
+
+        """
+        pass
+
+    @abstractmethod
+    def group_info(self, group_id):
+        """Return information about examinations of selected group.
+
+        Parameters
+        ----------
+        group_id : str
+            Group ID
+
+        Returns
+        -------
+        data : list of tuple
+            Examination descriptions.
+        headers : tuple
+            Headers.
+
+        """
+        pass
+
+    @abstractmethod
+    def insert_exam(self, exam):
+        """Add examination into current storage.
+
+        Parameters
+        ----------
+        exam : sme.Examination
+            Examination object
+
+        """
+        pass
+
+    @abstractmethod
+    def delete_exam(self, exam_id):
+        """Delete examination from current storage.
+
+        Parameters
+        ----------
+        exam_id : str
+            Examination ID.
+
+        """
+        pass
+    
+    @abstractmethod
+    def insert_group(self, name, description):
+        """Add new group of examinations.
+
+        Parameters
+        ----------
+        name : str
+            Name of new group.
+        description : str
+            Description for new group.
+
+        """
+        pass
+
+    @abstractmethod
+    def delete_group(self, group_id):
+        """Delete group of examinations from storage.
+
+        Parameters
+        ----------
+        group_id : str
+            Group ID.
+
+        """
+        pass
+
+    @abstractmethod
+    def group_exam(self, exam_id, group_ids, placed_in):
+        """Add and delete examination to and from groups.
+
+        Parameters
+        ----------
+        exam_id : str
+            Examination ID.
+        group_ids : list of str
+            Group IDs.
+        placed_in : list of bool
+            True for examinations to be placed in groups. Length of group_ids must be equal to length of placed_in.
+
+        """
+        pass
+
+    @abstractmethod
+    def where_exam(self, exam_id):
+        """Return description of groups where examination in or not in.
+
+        Parameters
+        ----------
+        exam_id : str
+            Examination ID.
+
+        Returns
+        -------
+        group_records : list of tuple
+            All group records.
+        headers : list of str
+            Names of group attributes.
+        placed_in : list of bool
+            True if exam in group, False otherwise.
+        
+        """
+        pass
+
+    @abstractmethod
+    def group_record(self, group_id):
+        """Return attribute names and values of selected group.
+
+        Parameters
+        ----------
+        group_id : str
+            Group ID.
+
+        Returns
+        -------
+        : OrderedDict
+            Attributes names and values for selected group.
+
+        """
+        pass
+
+    @abstractmethod
+    def update_group_record(self, group_id, attr):
+        """Update group record in storage.
+
+        Parameters
+        ----------
+        group_id : str
+            Group ID.
+        attr : OrderedDict
+            Attributes names and values.
+
+        """
+        pass
+    
+    @abstractmethod
+    def exam(self, exam_id):
+        """Return examination object.
+
+        Parameters
+        ----------
+        exam_id : str
+            Examination ID.
+
+        Returns
+        -------
+        sme.Examination
+            Examination object.
+
+        """
+        pass
+
+    @abstractmethod
+    def exams(self, group_id, meta_only=False):
+        """ Return exams from selected group or groups. 
+
+        Parameters
+        ----------
+        group_id : str or list of str
+            Group ID. If list, the union of sets of examinations from
+            groups returned.
+        meta_only: bool
+            If True, only meta data returned.
+
+        Returns
+        -------
+        exams: list of sme.Examination
+            Examinations list.
+
+        """
+        pass
---
+++ packages/EGEGrouper/EGEGrouper-0.6.1.tar.gz/EGEGrouper-0.6.1/egegrouper/tk_views.py
@@ -0,0 +1,337 @@
+# EGEGrouper - Software for grouping electrogastroenterography examinations.
+
+# Copyright (C) 2017-2018 Aleksandr Popov
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+class SimpleSignal:
+    """Provides simple signals and slots mechanism."""
+    def __init__(self):
+        """Constructor.
+
+        Create empty list of connected function.
+        """
+        self.slots = []
+
+    def emit(self, *args):
+        """Emit signal."""
+        for s in self.slots:
+            if s:
+                s(args)
+
+    def connect(self, slot):
+        """Connect signal with slot.
+        
+        Parameters
+        ----------
+        slot
+            Function to be connected with signal.
+        
+        """
+        for i in range(len(self.slots)):
+            if self.slots[i] == slot:
+                return
+            if self.slots[i] == None:
+                self.slots[i] = slot
+                return
+        self.slots.append(slot)
+
+    def disconnect(self, slot):
+        """Disconnect slot from signal.
+        
+        Parameters
+        ----------
+        slot
+            Name of connected function.
+        
+        """
+        for i in range(len(self.slots)):
+            if self.slots[i] == slot:
+                self.slots[i] = None
+
+
+class TableWidget(tk.Frame):
+    """Table widget."""
+    def __init__(self, parent, headers = []):
+        """Constructor.
+
+        Parameters
+        ----------
+        parent
+            Master for widget.
+        headers : list of str
+            Headers for table.
+
+        """
+        tk.Frame.__init__(self, parent)
+        self.scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
+        self.tree = ttk.Treeview(self, yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.tree.yview)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.set_columns(headers=headers)
+        # signals
+        self.item_opened = SimpleSignal()
+        self.tree.bind("<Double-1>", self.item_opened.emit)
+        self.tree.bind("<Return>", self.item_opened.emit)
+        self.item_selected = SimpleSignal()
+        self.tree.bind("<<TreeviewSelect>>", self.item_selected.emit)
+        
+    def selected_item_text(self):
+        """Return text in selected item.
+
+        Returns
+        -------
+        item_text : str.
+            Text of selected item.
+        
+        """
+        try:
+            item = self.tree.selection()[0]
+            item_text = self.tree.item(item,"text")
+        except IndexError:
+            item_text = None
+        finally:
+            return item_text
+
+    def set_columns(self, headers=[], anchors=[], widths=[]):
+        """ Setup columns.
+
+        Parameters
+        ----------
+        headers : list of str
+            Headers for table.
+        anchors : list of str
+            Anchors for columns. Use for align content.
+        widths : list of int
+            Widths of columns.
+
+        """
+        if len(headers) > 0:
+            self.column_ids = ["#0",] + headers[1:]
+            self.tree["columns"] = headers[1:]
+            for (cid, h) in zip(self.column_ids, headers):
+                self.tree.heading(cid, text=h)
+        if len(anchors) > 0:
+            for (cid, a) in zip(self.column_ids, anchors):
+                self.tree.column(cid, anchor=a)
+        if len(widths) > 0:
+            for (cid, w) in zip(self.column_ids, widths):
+                self.tree.column(cid, width=w)
+        self.tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+    def update_data(self, data):
+        """Fill table with data.
+
+        Parameters
+        ----------
+        data : list of tuple of str
+            Data in table rows.
+        
+        """
+        self.clear()
+        for (i, row) in zip(range(len(data)), data):
+            self.tree.insert("", tk.END, iid = str(i), text=str(row[0]), values=row[1:])
+        
+    def clear(self):
+        """Clear all items."""
+        self.tree.delete(*self.tree.get_children())
+
+    def remember_selection(self):
+        """Remember selected item."""
+        self._selected_item = self.tree.selection()[0]
+        
+    def restore_selection(self):
+        """Restore selection."""
+        if not self.tree.exists(self._selected_item):
+            return
+        self.tree.selection_set(self._selected_item)
+        
+class GroupingTable(TableWidget):
+    """Table widget for grouping."""
+    def __init__(self, parent):
+        """Constructor.
+
+        Parameters
+        ----------
+        parent
+            Master for widget.
+        """
+        tk.Frame.__init__(self, parent)
+        headers = ["ID", "Name", "In"]
+        widths = [50, None, 100]
+        anchors=[None, None, tk.CENTER]
+        super().__init__(parent, headers)
+        self.set_columns(widths=widths, anchors=anchors)
+        self.tree.bind("<Double-1>", self.toggle_item)
+        self.tree.bind("<Return>", self.toggle_item)
+
+    def toggle_item(self, event):
+        """Toggle item."""
+        if len(self.tree.selection()) == 0:
+            return
+        item = self.tree.selection()[0]
+        values = self.tree.item(item, 'values')
+        self.tree.item(item, values=(values[0], 'X' if values[1] == '' else ''))
+        
+    def checked_group_ids(self):
+        """Get IDs of checked groups.
+        
+        Returns
+        -------
+        groups_ids : list of str
+            IDs of all groups.
+        placed_in : list of bool
+            True if exam in group, False otherwise.
+
+        """
+        placed_in = []
+        group_ids = []
+        for item in self.tree.get_children():
+            group_ids.append(self.tree.item(item, 'text'))
+            placed_in.append(self.tree.item(item, 'values')[1] == 'X')
+        return group_ids, placed_in
+        
+class Message:
+    """Text message view."""
+    def show_data(self, text):
+        """ Show message box.
+
+        Parameters
+        ----------
+        text : str
+            Message text.
+
+        """
+        messagebox.showinfo("Message", text)
+
+class Storage(TableWidget):
+    """Table widget for stoarge info."""
+    def __init__(self, parent):
+        """Constructor.
+
+        Parameters
+        ----------
+        parent
+            Master for widget.
+
+        """
+        headers = ["ID", "Name", "Description", "Num"]
+        super().__init__(parent, headers)
+        widths = [50, None, None, 50]
+        anchors = [tk.CENTER, None, None, tk.CENTER]
+        self.set_columns(widths=widths, anchors=anchors)
+        self.last_group_id = None
+        
+    def show_data(self, data, headers):
+        """Fill table with data.
+
+        Parameters
+        ----------
+        data: list of tuples
+            Tabular data.
+        headers: tuple
+            Headers.
+
+        """
+        self.update_data(data)
+
+class Group(TableWidget):
+    """Table widget for stoarge info."""
+    def __init__(self, parent):
+        """Constructor.
+        
+        Parameters
+        ----------
+        parent
+            Master for widget.
+
+        """
+        headers = ["ID", "Name", "Diagnosis", "Age", "Gender"]
+        super().__init__(parent, headers)
+        widths = [50, None, None, None, None]
+        anchors = [None, None, None, tk.CENTER, tk.CENTER]
+        self.set_columns(widths=widths, anchors=anchors)
+        
+    def show_data(self, data, headers):
+        """Fill table with data.
+
+        Parameters
+        ----------
+        data: list of tuples
+            Tabular data.
+        headers: tuple
+            Headers.
+
+        """
+        self.update_data(data)
+
+class StatsTableWindow:
+    def __init__(self, parent):
+        """ Initialization. """
+        self.parent = parent
+
+    def show_data(self, data, headers):
+        """ Show data. 
+
+        Parameters
+        ----------
+
+        data: list of lists
+            Table content.
+        headers: list of str
+            Headers.
+        """
+        table = self._build_window(self.parent)
+        table.update_data(data)
+
+    def _build_window(self, parent):
+        """ Build and show window with table inside. """
+        master = tk.Toplevel(parent)
+        master.title("Statistics")
+        table = TableWidget(master) 
+        table.set_columns(
+            headers=['Category', 'Number'],
+            widths=[None, None],
+            anchors=[None, tk.CENTER],
+        )
+        table.pack()
+        master.transient(parent)
+        return table
+
+class WhereExam(GroupingTable):
+    """Tk view to show groups where examination is."""
+    
+    def show_data(self, group_records, headers, placed_in):
+        """Show data.
+
+        Parameters
+        ----------
+        group_records : list of tuple
+            Attributes of groups.
+        headers : tuple
+            Names of group attributes.
+        placed_in : list of bool
+            Indicators. True if examination placed in appropriate group, False overwise.
+
+        """
+        rows = [
+            (gr[0], gr[1], 'X' if p else '')
+            for p, gr in zip(placed_in, group_records)
+        ]
+        headers_ext = headers + ('', )
+        self.update_data(rows)
