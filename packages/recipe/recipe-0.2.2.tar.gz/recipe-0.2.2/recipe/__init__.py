ebf25a87eeebec06a679615899aea6a1d4d3ff76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:52 +0000

    Add package recipe/0.2.2/recipe-0.2.2.tar.gz

---
+++ packages/recipe/recipe-0.2.2.tar.gz/recipe-0.2.2/recipe/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Recipe
+~~~~~~~~~~~~~~~~~~~~~
+"""
+import logging
+
+from flapjack_stack import FlapjackStack
+
+from recipe import default_settings
+from recipe.core import Recipe
+from recipe.exceptions import BadIngredient, BadRecipe
+from recipe.ingredients import (
+    Dimension, DivideMetric, Filter, Having, IdValueDimension, Ingredient,
+    LookupDimension, Metric, WtdAvgMetric
+)
+from recipe.oven import get_oven
+from recipe.shelf import AutomaticShelf, Shelf
+
+SETTINGS = FlapjackStack()
+SETTINGS.add_layer(default_settings)
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+
+    class NullHandler(logging.Handler):
+
+        def emit(self, record):
+            pass
+
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
+__version__ = '0.2.2'
+
+__all__ = [
+    'BadIngredient', 'BadRecipe', 'Ingredient', 'Dimension', 'LookupDimension',
+    'IdValueDimension', 'Metric', 'DivideMetric', 'WtdAvgMetric', 'Filter',
+    'Having', 'Recipe', 'Shelf', 'AutomaticShelf', 'SETTINGS', 'get_oven'
+]
