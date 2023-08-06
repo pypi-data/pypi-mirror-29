0f76dad90559a0d9e987f28a917e13e9b28cfbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:51 +0000

    Add package Pyckson/1.3/Pyckson-1.3.tar.gz

---
+++ packages/Pyckson/Pyckson-1.3.tar.gz/Pyckson-1.3/src/pyckson/__init__.py
@@ -0,0 +1,8 @@
+from pyckson.decorators import *
+from pyckson.json import *
+from pyckson.parser import parse
+from pyckson.parsers.base import Parser
+from pyckson.serializer import serialize
+from pyckson.serializers.base import Serializer
+
+__version__ = '1.3'
