d808dbbb09e60d6804c44a5ed3f2ea611634e306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:39 +0000

    Add package python-cue/0.0/python-cue-0.0.tar.gz

---
+++ packages/python-cue/python-cue-0.0.tar.gz/python-cue-0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'python-cue',
+    packages = ['cue'],
+    version = '0.0',
+    description = '',
+    author = 'Davy Wybiral',
+    author_email = 'davy.wybiral@gmail.com',
+    url = 'https://github.com/wybiral',
+    keywords = [],
+    classifiers = [],
+)
