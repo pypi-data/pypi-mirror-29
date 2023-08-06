1e17fe7a7f236452247d82ac1af348578da79ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:56 +0000

    Add package PyStratum-pgSQL/0.10.9/PyStratum-pgSQL-0.10.9.tar.gz

---
+++ packages/PyStratum-pgSQL/PyStratum-pgSQL-0.10.9.tar.gz/PyStratum-pgSQL-0.10.9/pystratum_pgsql/wrapper/PgSqlSingleton1Wrapper.py
@@ -0,0 +1,22 @@
+"""
+PyStratum
+
+Copyright 2015-2016 Set Based IT Consultancy
+
+Licence MIT
+"""
+from pystratum.wrapper.Singleton1Wrapper import Singleton1Wrapper
+from pystratum_pgsql.wrapper.PgSqlWrapper import PgSqlWrapper
+
+
+class PgSqlSingleton1Wrapper(PgSqlWrapper, Singleton1Wrapper):
+    """
+    Wrapper method generator for stored procedures that are selecting 1 row with one column only.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def _write_result_handler(self, routine):
+        self._write_line(
+            'return StaticDataLayer.execute_sp_singleton1({0!s})'.format(str(self._generate_command(routine))))
+
+# ----------------------------------------------------------------------------------------------------------------------
