2420aa7af83a74b8005949b43186bf0fa0dae90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:33 +0000

    Add package udacity-pa/0.2.8/udacity-pa-0.2.8.tar.gz

---
+++ packages/udacity-pa/udacity-pa-0.2.8.tar.gz/udacity-pa-0.2.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='udacity-pa',
+    version='0.2.8',
+    author='S. Charles Brubaker',
+    author_email='cb@udacity.com',
+    packages=['udacity_pa'],
+    entry_points={
+        'console_scripts': [
+            'udacity = udacity_pa.projectassistant:main_func',
+            'udacity_pa = udacity_pa.projectassistant:main_func'
+        ],
+    },
+    include_package_data=True,
+    url='http://github.com/udacity/udacity-pa',
+    license='MIT',
+    description="CLI tool for Udacity's project assistant.",
+    keywords = 'Udacity',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "requests >= 2.2.1",
+        "requests-toolbelt >= 0.7.0",
+        "future"
+    ],
+)
