0c78685a8d3405ecc4d58ce0128f00efa9771899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:27 +0000

    Add package edgex-access/0.0.9/edgex_access-0.0.9.tar.gz

---
+++ packages/edgex-access/edgex_access-0.0.9.tar.gz/edgex_access-0.0.9/test/test_edge_access.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys, os, time
+import unittest
+import datetime
+
+import edgex_access
+
+
+class edge_access_testcases(unittest.TestCase):
+
+    def test_cfg(self):
+        pass
+    def test_store(self):
+        pass
+    def test_obj_write(self):
+        pass
+    def test_obj_read(self):
+        pass
+    def test_obj_del(self):
+        pass
+
+
+
+def run_all_tests():
+    print("Running all tests ...")
+    unittest.main()
+
+if __name__ == '__main__':
+    run_all_tests()
