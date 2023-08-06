967623496a4067e83bb68fb24dc7ee386417bc45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:01 +0000

    Add package pubg/0.0.1/pubg-0.0.1.tar.gz

---
+++ packages/pubg/pubg-0.0.1.tar.gz/pubg-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pubg',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/nicolaskenner/pubg',
+    license='MIT',
+    author='Nicolas Kenner',
+    author_email='nick@nicolaskenner.com',
+    description='Python wrapper for the official Battlegrounds API',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
