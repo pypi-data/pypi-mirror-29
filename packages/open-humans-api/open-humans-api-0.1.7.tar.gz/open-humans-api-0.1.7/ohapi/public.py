a160d9c89e07f700b05ac4ae7095bf9638791e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:52 +0000

    Add package open-humans-api/0.1.7/open-humans-api-0.1.7.tar.gz

---
+++ packages/open-humans-api/open-humans-api-0.1.7.tar.gz/open-humans-api-0.1.7/ohapi/public.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+import logging
+import os
+import re
+import signal
+
+from functools import partial
+try:
+    from urllib import urlencode
+except ImportError:
+    from urllib.parse import urlencode
+
+import click
+import concurrent.futures
+import requests
+
+from humanfriendly import format_size, parse_size
+
+from .api import get_page
+
+
+BASE_URL = 'https://www.openhumans.org'
+BASE_URL_API = '{}/api/public-data/'.format(BASE_URL)
+
+
+def signal_handler_cb(signal_name, frame):
+    """
+    Exit on Ctrl-C.
+    """
+    os._exit(1)
+
+
+def download_url(result, directory, max_bytes):
+    """
+    Download a file.
+    """
+    response = requests.get(result['download_url'], stream=True)
+
+    # TODO: make this more robust by parsing the URL
+    filename = response.url.split('/')[-1]
+    filename = re.sub(r'\?.*$', '', filename)
+    filename = '{}-{}'.format(result['user']['id'], filename)
+
+    size = int(response.headers['Content-Length'])
+
+    if size > max_bytes:
+        logging.info('Skipping {}, {} > {}'.format(filename, format_size(size),
+                                            format_size(max_bytes)))
+
+        return
+
+    logging.info('Downloading {} ({})'.format(filename, format_size(size)))
+
+    output_path = os.path.join(directory, filename)
+
+    try:
+        stat = os.stat(output_path)
+
+        if stat.st_size == size:
+            logging.info('Skipping "{}"; exists and is the right size'.format(
+                filename))
+
+            return
+        else:
+            logging.info('Removing "{}"; exists and is the wrong size'.format(
+                filename))
+
+            os.remove(output_path)
+    except OSError:
+        # TODO: check errno here?
+        pass
+
+    with open(output_path, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
+
+    logging.info('Downloaded {}'.format(filename))
+
+
+@click.command()
+@click.option('-s', '--source', help='the source to download files from')
+@click.option('-u', '--username', help='the user to download files from')
+@click.option('-d', '--directory', help='the directory for downloaded files',
+              default='.')
+@click.option('-m', '--max-size', help='the maximum file size to download',
+              default='128m')
+@click.option('-v', '--verbose', help='Report INFO level logging to stdout',
+              is_flag=True)
+@click.option('--debug', help='Report DEBUG level logging to stdout.',
+              is_flag=True)
+def download(source, username, directory, max_size, verbose, debug):
+    """
+    Download public data from Open Humans.
+    """
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    elif verbose:
+        logging.basicConfig(level=logging.INFO)
+
+    logging.debug("Running with source: '{}'".format(source) +
+                  " and username: '{}'".format(username) +
+                  " and directory: '{}'".format(directory) +
+                  " and max-size: '{}'".format(max_size))
+
+    signal.signal(signal.SIGINT, signal_handler_cb)
+
+    max_bytes = parse_size(max_size)
+
+    options = {}
+
+    if source:
+        options['source'] = source
+
+    if username:
+        options['username'] = username
+
+    page = '{}?{}'.format(BASE_URL_API, urlencode(options))
+
+    results = []
+    counter = 1
+
+    logging.info('Retrieving metadata')
+
+    while True:
+        logging.info('Retrieving page {}'.format(counter))
+
+        response = get_page(page)
+        results = results + response['results']
+
+        if response['next']:
+            page = response['next']
+        else:
+            break
+
+        counter += 1
+
+    logging.info('Downloading {} files'.format(len(results)))
+
+    download_url_partial = partial(download_url, directory=directory,
+                                   max_bytes=max_bytes)
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+        for value in executor.map(download_url_partial, results):
+            if value:
+                logging.info(value)
---
+++ packages/open-humans-api/open-humans-api-0.1.7.tar.gz/open-humans-api-0.1.7/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import sys
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+# Add backport of futures unless Python version is 3.2 or later.
+install_requires = [
+    'click>=6.3',
+    'humanfriendly>=1.44.3',
+    'requests>=2.9.1',
+    'arrow>=0.8.0',
+    ]
+if sys.version_info < (3, 2):
+    install_requires.append('futures>=3.0.5')
+
+setup(
+    name='open-humans-api',
+    author='Mad Price Ball',
+    author_email='support@openhumans.org',
+
+    url='https://github.com/OpenHumans/open-humans-api',
+
+    description='Tools for working with Open Humans APIs',
+    long_description=readme(),
+
+    version='0.1.7',
+
+    license='MIT',
+
+    keywords=['open-humans'],
+
+    classifiers=[
+        'Environment :: Console',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+
+    packages=['ohapi'],
+
+    entry_points={
+        'console_scripts': [
+            'ohpub-download = ohapi.public:download',
+            'ohproj-download = ohapi.command_line:download',
+            'ohproj-download-metadata = ohapi.command_line:download_metadata',
+            'ohproj-upload = ohapi.command_line:upload',
+            'ohproj-upload-metadata = ohapi.command_line:upload_metadata',
+        ]
+    },
+
+    install_requires=install_requires,
+)
