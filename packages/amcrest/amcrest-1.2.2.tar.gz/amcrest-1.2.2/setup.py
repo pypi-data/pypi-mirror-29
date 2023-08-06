48917d6e3f8328339ac79aeae9176b9aed606179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:29 +0000

    Add package amcrest/1.2.2/amcrest-1.2.2.tar.gz

---
+++ packages/amcrest/amcrest-1.2.2.tar.gz/amcrest-1.2.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='amcrest',
+      version='1.2.2',
+      description='Python wrapper implementation for Amcrest cameras.',
+      author='Douglas Schilling Landgraf, Marcelo Moreira de Mello',
+      author_email='dougsland@gmail.com, tchello.mello@gmail.com',
+      url='http://github.com/tchellomello/python-amcrest',
+      license='GPLv2',
+      install_requires=['requests', 'argcomplete'],
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      include_package_data=True,
+      test_suite='tests',
+      scripts=['cli/amcrest-cli', 'tui/amcrest-tui'],
+      zip_safe=True,
+      keywords="amcrest camera python",
+      classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+      ])
