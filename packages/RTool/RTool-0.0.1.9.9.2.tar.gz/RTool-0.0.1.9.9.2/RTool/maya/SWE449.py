79a842d92c0ef007b66723d96ff8f2e7daf0460d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:06 +0000

    Add package RTool/0.0.1.9.9.2/RTool-0.0.1.9.9.2.tar.gz

---
+++ packages/RTool/RTool-0.0.1.9.9.2.tar.gz/RTool-0.0.1.9.9.2/RTool/maya/SWE449.py
@@ -0,0 +1,51 @@
+'''RTool/maya/SWE449.py
+
+SWE449 Midterm Project
+
+'''
+
+import maya.cmds as mc
+import pymel as pm
+import MASH.api as mapi
+import maya.mel as mel
+import ntpath
+import getpass
+
+from RTool.maya.audio import AudioNode
+import RTool.maya.util as util
+
+
+wavPath = ("C:/Users/%s/Documents/maya/projects/default/sound/Keys_Noodle.wav"
+    %getpass.getuser())
+
+def CreateVisualizer(wavPath):
+    mc.file(new=True, force=True)
+    
+    fileNameWithExtention = ntpath.basename(wavPath)
+    fileName= fileNameWithExtention[:fileNameWithExtention.find(".")]
+    
+    cube = mc.polySphere()
+    
+    networkName = "testNetwork"
+    mashNetwork = mapi.Network()
+    mashNetwork.createNetwork(networkName)
+    
+    audioNode = AudioNode(mashNetwork, wavPath)
+        
+    #print(mc.getAttr(audioNode.name+".eqOutput"))
+    
+    mc.setAttr(audioNode.name+".scaleY", 100)
+    
+    mc.setAttr(networkName+"_Distribute"+".arrangement", 6) # 6 is Grid
+    mc.setAttr(networkName+"_Distribute"+".gridAmplitudeX", 30)
+    mc.setAttr(networkName+"_Distribute"+".gridAmplitudeZ", 10)
+    mc.setAttr(networkName+"_Distribute"+".gridx", 30)
+    
+    colorNode = mashNetwork.addNode("MASH_Color")
+    mc.setAttr(colorNode.name+".color", 1,0.654,0.654)
+    
+    colorFalloff = colorNode.addFalloff()
+    mc.setAttr(colorFalloff+".falloffShape", 2) # 2 is Cube
+        
+if __name__ == "__main__":
+    main()
---
+++ packages/RTool/RTool-0.0.1.9.9.2.tar.gz/RTool-0.0.1.9.9.2/RTool/maya/audio.py
@@ -0,0 +1,28 @@
+'''RTool/maya/audio.py
+
+Utility tools for maya audio
+
+~!* Must by run inside mayapy.exe *!~
+'''
+
+import maya.cmds as mc
+import MASH.api as mapi
+import maya.mel as mel
+import ntpath
+
+def AudioNode(mashNetwork, filePath):
+    '''
+
+    '''
+    fileNameWithExtention = ntpath.basename(filePath)
+    fileName= fileNameWithExtention[:fileNameWithExtention.find(".")]
+    audioNode = mashNetwork.addNode("MASH_Audio")
+    
+    mc.playbackOptions(edit=True, playbackSpeed=1, maxPlaybackSpeed=0) # sets playback to realtime 
+    
+    mel.eval("file -import -type \"audio\"  -ignoreVersion -ra true -mergeNamespacesOnClash false -namespace \"%se\" -options \"o=0\"  -pr  -importTimeRange \"combine\" \"%s\";"%(fileName,filePath))
+    mel.eval("doSoundImportArgList (\"1\", {\"%s\",\"0\"});"%(filePath))
+    
+    mc.setAttr(audioNode.name+".filename", filePath,type="string")
+    return(audioNode)
+
