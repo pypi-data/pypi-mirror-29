899e73acfb41afbdd25fcb02aa75ba83c437456d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:16 +0000

    Add package FoxDot/0.5.13/FoxDot-0.5.13.tar.gz

---
+++ packages/FoxDot/FoxDot-0.5.13.tar.gz/FoxDot-0.5.13/FoxDot/lib/Midi.py
@@ -0,0 +1,161 @@
+""" Module for converting handling MIDI in/out and functions relating to MIDI pitch calculation. """
+
+from __future__ import absolute_import, division, print_function
+
+try:
+    import rtmidi
+    from rtmidi import midiconstants
+    TIMING_CLOCK          = midiconstants.TIMING_CLOCK
+    SONG_POSITION_POINTER = midiconstants.SONG_POSITION_POINTER
+    SONG_START            = midiconstants.SONG_START
+except ImportError as _err:
+    pass
+
+from .Patterns import asStream
+from .Scale    import ScalePattern
+from .TimeVar  import TimeVar
+
+import math
+
+def miditofreq(midinote):
+    """ Converts a midi number to frequency """
+    return 440 * (2 ** ((midinote - 69.0)/12.0))
+
+midi2cps = miditofreq # alias
+
+def _log2(num):
+    return math.log(num) / math.log(2)
+
+def freqtomidi(freq):
+    return 12 * _log2((freq / 440)) + 69
+
+def midi(scale, octave, degree, root=0, stepsPerOctave=12):
+    """ Calculates a midinote from a scale, octave, degree, and root """
+
+    # Make sure we force timevars into real values
+
+    if isinstance(scale, ScalePattern) and isinstance(scale.data, TimeVar):
+
+        scale = asStream(scale.data.now())
+
+    # Force float
+    octave = float(octave)
+    degree = float(degree)
+    root   = float(root)
+    
+    # Floor val
+    lo = int(math.floor(degree))
+    hi = lo + 1
+
+    octave = octave + (lo // len(scale))
+    index  = lo % len(scale)
+
+    # Work out any microtones
+
+    micro = (degree - lo)
+
+    if micro > 0:
+
+        ex_scale = list(scale) + [stepsPerOctave]
+
+        diff  = ex_scale[index + 1] - scale[index]
+
+        micro = micro * diff
+
+    midival = stepsPerOctave * octave # Root note of scale
+    midival = midival + root          # Adjust for key
+    midival = midival + scale[index]  # Add the note
+    midival = midival + micro         # And any microtonal
+
+    #chroma = range(int(stepsPerOctave))
+    #scale_val = (scale[hi % len(scale)] - scale[lo % len(scale)]) * ((degree-lo)) + scale[lo % len(scale)]
+    #midival = scale_val + (octave * len(chroma)) + float(root)
+
+    return midival
+
+
+class MidiIn:
+    metro = None
+    def __init__(self, port_id=0):
+        """ Class for listening for MIDI clock messages
+            from a midi device """
+        try:
+
+            self.device = rtmidi.MidiIn()
+
+        except NameError:
+
+            raise ImportError(_err)
+
+        self.available_ports = self.device.get_ports()
+
+        if not self.available_ports:
+
+            raise MIDIDeviceNotFound
+
+        else:
+
+            print("MidiIn: Connecting to " + self.available_ports[port_id])
+
+        self.device.open_port(port_id)
+        self.device.ignore_types(timing=False)
+
+        self.pulse = 0
+        self.delta = 0.0
+        self.bpm   = 120.0
+        self.ppqn  = 24
+        self.beat  = 0
+
+    @classmethod
+    def set_clock(cls, tempo_clock):
+        cls.metro = tempo_clock
+        return
+
+    def update(self):
+        data = self.device.get_message()
+        if data is not None:
+            datatype, delta = data
+            if TIMING_CLOCK in datatype:
+                self.pulse += 1
+                self.delta += delta
+                if self.pulse == self.ppqn:
+                    self.bpm = 60.0 / self.delta
+                    self.pulse = 0
+                    self.delta = 0.0
+            elif SONG_POSITION_POINTER in datatype:
+                self.metro.set_time(datatype[1] / 4)
+        return
+
+    def get_beat(self):
+        """ If a beat value has been set, return it, otherwise return None """
+        val, self.beat = self.beat, None
+        return val
+
+    def close(self):
+        """ Closes the active port """
+        self.device.close_port()
+        return
+                
+from .SCLang import SynthDefProxy
+
+class MidiOut(SynthDefProxy):
+    def __init__(self, degree=0, **kwargs):
+        SynthDefProxy.__init__(self, self.__class__.__name__, degree, kwargs)
+
+class MIDIDeviceNotFound(Exception):
+    def __str__(self):
+        return self.__class__.__name__ + " Error"
+
+class rtMidiNotFound(Exception):
+    def __str__(self):
+        return self.__class__.__name__ + ": Module 'rtmidi' not found"
+
+
+if __name__ == "__main__":
+
+    a = MidiIn()
+
+    
+
+
+        
---
+++ packages/FoxDot/FoxDot-0.5.13.tar.gz/FoxDot-0.5.13/FoxDot/lib/Repeat.py
@@ -0,0 +1,470 @@
+from __future__ import absolute_import, division, print_function
+
+from .Code import WarningMsg
+from .Patterns import Pattern, asStream
+from .Utils import modi
+
+import threading
+
+class MethodList:
+    def __init__(self, root):
+        self.root=root
+        self.list_of_methods = []
+    
+    def __repr__(self):
+        return repr(self.list_of_methods)
+
+    def update(self, method, args, kwargs):
+        for i, info in enumerate(self.list_of_methods):
+            name, _, _  = info
+            if name == method:
+                self.list_of_methods[i] = (method, args, kwargs)
+                return
+        return ValueError
+
+    def remove(self, method):
+        for i, info in enumerate(self.list_of_methods):
+            name, args, kwargs = info
+            if name == method:
+                self.list_of_methods.pop(i)
+                return
+        raise ValueError
+
+    def contains(self, method):
+        for name, args, kwargs in self.list_of_methods:
+            if name == method:
+                return True
+        return False
+
+class Repeatable(object):
+    after_update_methods = []
+    method_synonyms      = {}
+    def __init__(self):
+        self.repeat_events        = {}
+        self.previous_patterns    = {}
+
+    def update_pattern_root(self, attr):
+        """ Update the base attribute pattern that methods are applied to """
+
+        if attr not in self.previous_patterns:
+
+            self.previous_patterns[attr] = MethodList(self.attr[attr])
+
+        else:
+
+            self.previous_patterns[attr].root = self.attr[attr]
+
+        self.update_pattern_methods(attr)     
+
+        return
+
+    def update_pattern_methods(self, attr):
+        """ Update the 'current' version of a pattern based on its root and methods stored """
+
+        if attr not in self.previous_patterns:
+
+            self.previous_patterns[attr] = MethodList(self.attr[attr])
+
+        data = self.previous_patterns[attr].root
+
+        for method, args, kwargs in self.previous_patterns[attr].list_of_methods:
+
+            call_pattern_method = getattr(Pattern, method)
+
+            data = call_pattern_method(data, *args, **kwargs)
+
+        self.attr[attr] = data
+
+        return
+
+    def after(self, n, cmd, *args, **kwargs):
+        """ Schedule self.cmd(args, kwargs) in 'n' beats time
+            ```
+            # Stop the player looping after 16 beats
+            p1 >> pads().after(16, "stop")
+            ```
+        """
+        quantise = kwargs.get("quantise", True)
+
+        try:
+
+            event = lambda: getattr(self, cmd)(*args, **kwargs)
+
+            if not quantise:
+
+                time = self.metro.now() + n
+
+            else:
+
+                time = self.metro.next_bar() + n
+
+            self.metro.schedule( event, time )
+
+        except:
+
+            pass
+        
+        return self
+
+    def get_attr_and_method_name(self, cmd):
+
+        if cmd in self.method_synonyms:
+
+            attr = [ self.method_synonyms[cmd] ]
+
+        else:
+
+            attr = cmd.split(".")
+
+        # We can also schedule attribute methods
+
+        if len(attr) == 1:
+
+            attr_name   = "degree"
+            method_name = attr[0]
+
+        elif len(attr) == 2:
+
+            attr_name = attr[0]
+            method_name = attr[1]
+
+        return attr_name, method_name
+
+    def is_pattern_method(self, method_name, attr="degree"):
+
+        if attr == "degree" and hasattr(self, method_name):
+
+            return False
+
+        elif hasattr(Pattern, method_name):
+
+            return True
+
+        else:
+
+            return False
+
+    def is_player_method(self, method_name, attr="degree"):
+
+        return hasattr(self, method_name) and attr == "degree"
+
+    def get_method_by_name(self, cmd):
+        # Make sure cmd is a method
+
+        attr_name, method_name = self.get_attr_and_method_name(cmd)
+
+        if True: # TODO move this back
+
+            if self.is_player_method(method_name, attr_name):
+
+                method = getattr(self, method_name)
+
+            elif self.is_pattern_method(method_name, attr_name):
+
+                def method(*args, **kwargs):
+
+                    # If there are no "old" patterns held in memory, use the pattern method and store
+
+                    attr = attr_name
+
+                    if attr not in self.previous_patterns:
+
+                        self.previous_patterns[attr] = MethodList(self.attr[attr]) # store the root
+
+                    # If this has already been called, "undo it"
+
+                    if self.previous_patterns[attr].contains(method_name):
+
+                        self.previous_patterns[attr].remove(method_name)
+
+                    # If not, add it to the list
+
+                    else:
+
+                        self.previous_patterns[attr].list_of_methods.append((method_name, args, kwargs))
+
+                    # Update the attribute
+
+                    self.update_pattern_methods(attr)
+
+                    return
+
+                method.__name__ = cmd # for debugging purposes
+
+            else:
+
+                WarningMsg("{} is not a valid method for type {}".format(cmd, self.__class__))
+
+                return None, None
+
+        assert callable(method)
+        
+        return attr_name, method
+        
+    def every(self, occurence, cmd, *args, **kwargs):
+        """ Every n beats, call a method (defined as a string) on the
+            object and use the args and kwargs. To call the method
+            every n-th beat of a timeframe, use the `cycle` keyword argument
+            to specify that timeframe.
+
+            ::
+                # Call the shuffle method every 4 beats
+
+                p1.every(4, 'shuffle')
+
+                # Call the stutter method on the 5th beat of every 8 beat cycle
+
+                p1.every(5, 'stutter', 4, cycle=8)
+
+                # If the method is not valid but *is* a valid Pattern method, that is called and reverted
+
+                p1.every(4, 'palindrome')
+        """
+
+        try:
+
+            attr, method = self.get_method_by_name(cmd)
+
+        except AttributeError:
+
+            WarningMsg("{} is not a valid method for type {}".format(cmd, self.__class__))
+
+            return self
+
+        # Collect the cycle length
+
+        cycle = kwargs.get("cycle", None)
+        ident = kwargs.get("ident", None)
+
+        kwargs = {key: value for key, value in kwargs.items() if key not in ("cycle", "ident")}
+
+        # If the method call already exists, just update it
+
+        if ident is not None:
+
+            cmd = "{}-{}".format(cmd, ident)
+
+        if cmd in self.repeat_events:
+
+            # Work out whether the method needs calling or not
+
+            call = self.repeat_events[cmd]
+
+            # Update the time details
+
+            call.update(occurence, cycle, args, kwargs)
+
+            attr, method_name = self.get_attr_and_method_name(cmd)
+
+            if self.is_pattern_method(method_name, attr):
+
+                # if n is even, it means the method is active # TODO -- put this in a class mate
+
+                n, acc = call.count()
+
+                if n % 2 == 1:
+
+                    if self.previous_patterns[attr].contains(method_name):
+
+                        self.previous_patterns[attr].remove(method_name)
+
+                else:
+
+                    if self.previous_patterns[attr].contains(method_name):
+
+                        self.previous_patterns[attr].update(method_name, args, kwargs)
+
+                # Update the attribute
+
+                self.update_pattern_methods(attr)
+
+            if not call.isScheduled():
+
+                call.schedule()
+
+        else:
+
+            call = MethodCall(self, method, occurence, cycle, args, kwargs)
+
+            self.repeat_events[cmd] = call
+
+            call.schedule()
+
+        return self
+
+    def stop_calling_all(self):
+        for method in list(self.repeat_events.keys()):
+            self.never(method)
+        return self
+            
+
+    def never(self, cmd, ident=None):
+        """ Stops calling cmd on repeat """
+
+        attr, method = self.get_attr_and_method_name(cmd)
+
+        if ident is not None:
+        
+            cmd = "{}-{}".format(cmd, ident)
+
+        try:
+            # If it a pattern method, undo it
+            
+            if self.previous_patterns[attr].contains(method):
+            
+                self.previous_patterns[attr].remove(method)
+            
+                self.update_pattern_methods(attr)
+            
+            self.repeat_events[cmd].stop()
+            
+            del self.repeat_events[cmd]
+        
+        except KeyError:
+        
+            err = "Player method '{}' not active".format(cmd)
+        
+            raise KeyError(err)
+        
+        return self
+
+class MethodCall:
+    """ Class to represent an object's method call that,
+        when called, schedules itself in the future """
+    def __init__(self, parent, method, n, cycle=None, args=(), kwargs={}):
+        
+        self.parent = parent  
+        self.method = method
+
+        self.cycle = cycle
+        self.when  = asStream(n)
+        
+        self.i, self.next = self.count()
+
+        self.this_when = float(self.when[self.i])
+        self.last_when = 0
+
+        self.args = args
+        self.kwargs = kwargs
+
+        self.after_update = False
+
+        self.stopping = False
+
+    def count(self):
+        """ Counts the number of times this method would have been called between clock start and now """
+
+        n = 0
+        acc = 0
+        dur = 0
+        now = float(self.parent.metro.now())
+
+        # Get durations
+
+        durations = self.when if self.cycle is None else asStream(self.cycle)
+        total_dur = float(sum(durations))
+
+        # How much time left to fit remainder in
+
+        try:
+       
+            acc = now - (now % total_dur)
+
+        except ZeroDivisionError:
+
+            acc = 0
+
+        # n is the index to return for calculating self.when[n]
+        # acc is when to start
+
+        n = int(len(durations) * (acc / total_dur))
+
+        if acc != now:
+
+            while True:
+
+                dur = float(durations[n])
+
+                acc += dur
+                n   += 1
+
+                if acc >= now:
+
+                    break
+
+        return n, acc
+
+    def __repr__(self):
+        return "<Future {}() call of '{}'>".format(self.method.__name__, self.parent)
+
+    def __call__(self, *args, **kwargs):
+        """ Proxy for parent object __call__, calls the enclosed method
+            and schedules it in the future. """
+
+        assert self.method is not None
+
+        # Return without scheduling if stopping
+        
+        if self.stopping:
+
+            return
+
+        # Update the current duration
+
+        self.last_when, self.this_when = self.this_when, float(self.when[self.i])
+
+        if self.cycle is not None:
+            
+            self.next += (float(modi(self.cycle, self.i)) + (self.this_when - self.last_when))
+
+        else:
+
+            self.next += self.this_when
+
+        try:
+
+            self.method.__call__(*self.args, **self.kwargs)
+
+        except Exception as e:
+
+            print("{} in '{}': {}".format(e.__class__.__name__, self.method.__name__, e))
+
+        # Re-schedule the method call
+
+        self.schedule()
+
+        # Increase the index to get the next duration
+        
+        self.i += 1
+
+        return
+
+    def schedule(self):
+        self.parent.metro.schedule(self, self.next)
+
+    def isScheduled(self):
+        """ Returns True if this is in the Tempo Clock """
+        return self in self.parent.metro
+
+    def stop(self):
+        self.stopping = True
+
+    def update(self, n, cycle, args=(), kwargs={}):
+        """ Updates the values of the MethodCall. Re-adjusts
+            the index if cycle has been changed """
+
+        self.when = asStream(n)
+        self.args = args
+        self.kwargs = kwargs
+
+        self.i, self.next = self.count()
+
+        # Amend the cycle (TODO!)
+
+        if cycle is not None and cycle != self.cycle:
+
+            self.next = self.parent.metro.next_bar() + self.when[self.i]
+
+        self.cycle = cycle
+        
+        return self
