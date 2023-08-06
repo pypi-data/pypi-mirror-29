8bc21dc6c00e22d359d00601291461227ce88e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:46 +0000

    Add package spymanga/0.1/spymanga-0.1.tar.gz

---
+++ packages/spymanga/spymanga-0.1.tar.gz/spymanga-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'spymanga',
+  packages = ['ScrapyManga'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A lib to download manga chapters',
+  author = 'Luiz Rodrigo',
+  author_email = 'luizrodrigo46@hotmail.com',
+  url = 'https://github.com/AntLouiz/ScraPyManga', # use the URL to the github repo
+  download_url = 'https://github.com/AntLouiz/ScrapyManga/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['scrapy', 'manga', 'web scraping'], # arbitrary keywords
+  classifiers = [],
+)
