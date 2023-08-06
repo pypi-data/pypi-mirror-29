6e2d9c3c7baeb9ddc8f0a480983c659e5e0e1404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:58 +0000

    Add package nnpush/0.21/nnpush-0.21.tar.gz

---
+++ packages/nnpush/nnpush-0.21.tar.gz/nnpush-0.21/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name = 'nnpush',
+  packages = ['nnpush'], # this must be the same as the name above
+  version = '0.21',
+  description = 'A library to hook into neural network training iterations so it can be monitored from the mobile app NNPush and be notified when training is complete!',
+  author = 'Richard Fox',
+  author_email = 'fox.ios.dev@gmail.com',
+  url = 'http://deepdescent.net', # use the URL to the github repo
+  keywords = ['neural network', 'monitor', 'training', 'deep learning', 'nnpush', 'nueral', 'network'], # arbitrary keywords
+  classifiers = []
+)
