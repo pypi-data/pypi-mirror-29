ad617f9d11d08602b81b58474f3f5b7c9168913b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:39 +0000

    Add package flask-optional-routes/0.5/flask_optional_routes-0.5.tar.gz

---
+++ packages/flask-optional-routes/flask_optional_routes-0.5.tar.gz/flask_optional_routes-0.5/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'flask_optional_routes',
+  packages = ['flask_optional_routes'],
+  version = '0.5',
+  description = 'This library allows users to specify optional paths',
+  author = 'Herbert Dawkins',
+  author_email = 'DrDawkins@ClearScienceInc.com',
+  url = 'https://github.com/sudouser2010/flask_optional_routes',
+  download_url = 'https://github.com/sudouser2010/flask_optional_routes/archive/0.4.tar.gz',
+  keywords = ['flask', 'option', 'route'],
+  classifiers = [],
+  python_requires='~=3.6',
+  install_requires=[
+  'flask==0.12.2'
+  ],
+  include_package_data=True,
+)
