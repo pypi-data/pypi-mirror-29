147449b4a479927251c9300e015a8084d90d836b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:58 +0000

    Add package mozilla-taar3/0.0.23/mozilla-taar3-0.0.23.tar.gz

---
+++ packages/mozilla-taar3/mozilla-taar3-0.0.23.tar.gz/mozilla-taar3-0.0.23/tests/test_ensemblerecommender.py
@@ -0,0 +1,52 @@
+from taar.context import Context
+from taar.recommenders.ensemble_recommender import WeightCache, EnsembleRecommender
+from .mocks import MockRecommenderFactory
+
+EXPECTED = {'legacy': 10000,
+            'collaborative': 1000,
+            'similarity': 100,
+            'locale': 10}
+
+
+class Mocker:
+    def get_s3_json_content(self, *args, **kwargs):
+        return {'ensemble_weights': EXPECTED}
+
+def test_weight_cache():   # noqa
+
+    ctx = Context()
+    ctx['utils'] = Mocker()
+
+    wc = WeightCache(ctx.child())
+    actual = wc.getWeights()
+    assert EXPECTED == actual
+
+
+def test_recommendations():
+    ctx = Context()
+
+    ctx['utils'] = Mocker()
+
+    EXPECTED_RESULTS = [('cde', 12000.0),
+                        ('bcd', 11000.0),
+                        ('abc', 10023.0),
+                        ('ghi', 3430.0),
+                        ('def', 3320.0),
+                        ('ijk', 3200.0),
+                        ('hij', 3100.0),
+                        ('lmn', 420.0),
+                        ('klm', 409.99999999999994),
+                        ('jkl', 400.0)]
+
+    factory = MockRecommenderFactory()
+    ctx['recommender_factory'] = factory
+
+    ctx['recommender_map'] = {'legacy': factory.create('legacy'),
+                              'collaborative': factory.create('collaborative'),
+                              'similarity': factory.create('similarity'),
+                              'locale': factory.create('locale')}
+    r = EnsembleRecommender(ctx.child())
+    client = {'client_id': '12345'}  # Anything will work here
+    recommendation_list = r.recommend(client, 10)
+    assert isinstance(recommendation_list, list)
+    assert recommendation_list == EXPECTED_RESULTS
