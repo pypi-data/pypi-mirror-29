2e578bf02c41e7a053668a210c566aa0c7251cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:25 +0000

    Add package zprocess/2.2.6/zprocess-2.2.6.tar.gz

---
+++ packages/zprocess/zprocess-2.2.6.tar.gz/zprocess-2.2.6/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# To upload a version to PyPI, run:
+#
+#    python setup.py sdist upload
+#
+# If the package is not registered with PyPI yet, do so with:
+#
+# python setup.py register
+
+from distutils.core import setup
+import os
+
+VERSION = '2.2.6'
+
+# Auto generate a __version__ package for the package to import
+with open(os.path.join('zprocess', '__version__.py'), 'w') as f:
+    f.write("__version__ = '%s'\n"%VERSION)
+
+setup(name='zprocess',
+      version=VERSION,
+      description="A set of utilities for multiprocessing using zeromq.",
+      author='Chris Billington',
+      author_email='chrisjbillington@gmail.com',
+      url='https://bitbucket.org/cbillington/zprocess/',
+      license="BSD",
+      packages=['zprocess', 'zprocess.locking', 'zprocess.logging']
+     )
