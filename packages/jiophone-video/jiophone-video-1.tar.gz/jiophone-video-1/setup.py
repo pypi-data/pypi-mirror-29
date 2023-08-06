5ad581a299d4bbed23c9e838d7a65b241d35c29d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:50 +0000

    Add package jiophone-video/1/jiophone-video-1.tar.gz

---
+++ packages/jiophone-video/jiophone-video-1.tar.gz/jiophone-video-1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'jiophone-video',
+  packages = ['jiophone-video'],
+  version = '1',
+  description = 'Convert video to JioPhone compatible',
+  author = 'Hritik Vijay',
+  author_email = 'Hritikxx8@gmail.com',
+  url = 'https://github.com/Hritik14/jiophone-video',
+  download_url = 'https://github.com/Hritik14/jiophone-video/archive/1.tar.gz',
+  keywords = ['Jio', 'Jio Phone', 'Video'],
+  classifiers = [],
+)
