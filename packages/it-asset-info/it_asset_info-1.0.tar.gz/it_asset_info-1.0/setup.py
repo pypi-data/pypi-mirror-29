dd619f0f565e4d9aafc7c743798d8ba4e98b15c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:30 +0000

    Add package it-asset-info/1.0/it_asset_info-1.0.tar.gz

---
+++ packages/it-asset-info/it_asset_info-1.0.tar.gz/it_asset_info-1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name = "it_asset_info",
+      version="1.0",
+      description= "Program to fetch System information to check software compatiblity",
+      author = "rahul",
+      url='http://github.com/rahulrksmau/SystemInfo',
+      author_email='rahul.k@emudhra.com',
+      packages=['it_asset_info'],
+      install_requires=[],
+      zip_safe=False
+      )
