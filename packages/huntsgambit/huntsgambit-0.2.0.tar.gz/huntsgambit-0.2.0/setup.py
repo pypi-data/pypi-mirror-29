6a49a79c8e5590da8140bb11565bf47ac22e8fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:00 +0000

    Add package huntsgambit/0.2.0/huntsgambit-0.2.0.tar.gz

---
+++ packages/huntsgambit/huntsgambit-0.2.0.tar.gz/huntsgambit-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+     name='huntsgambit',
+     version='0.2.0',
+     install_requires=[
+         'requests',
+         'argparse',
+         'pandas>=0.19.1'
+     ]
+)
