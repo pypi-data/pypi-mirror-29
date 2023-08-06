2ba9b191717bea40aede2f74d7d4f12f9a31eb12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:58 +0000

    Add package tc-aws/6.2.2/tc_aws-6.2.2.tar.gz

---
+++ packages/tc-aws/tc_aws-6.2.2.tar.gz/tc_aws-6.2.2/setup.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='tc_aws',
+    version='6.2.2',
+    description='Thumbor AWS extensions',
+    author='Thumbor-Community & William King',
+    author_email='willtrking@gmail.com',
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'python-dateutil',
+        'thumbor>=6.0.0,<7',
+        'tornado-botocore>=1.3.1',
+    ],
+    extras_require={
+        'tests': [
+            'pyvows',
+            'coverage',
+            'tornado_pyvows',
+            'boto',
+            'moto',
+            'mock',
+        ],
+    },
+)
