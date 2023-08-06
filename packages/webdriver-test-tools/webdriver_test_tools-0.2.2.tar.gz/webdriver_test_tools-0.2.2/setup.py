cf02873eb14d0c09495031ecc73d37fad24a726c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:01 +0000

    Add package webdriver-test-tools/0.2.2/webdriver_test_tools-0.2.2.tar.gz

---
+++ packages/webdriver-test-tools/webdriver_test_tools-0.2.2.tar.gz/webdriver_test_tools-0.2.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+        name='webdriver_test_tools',
+        version='0.2.2',
+        description='Tools for using Selenium WebDriver with Python unit testing',
+        url='https://github.com/connordelacruz/webdriver-test-tools',
+        download_url='https://github.com/connordelacruz/webdriver-test-tools/archive/0.2.2.tar.gz',
+        author='Connor de la Cruz',
+        author_email='connor.c.delacruz@gmail.com',
+        license='MIT',
+        packages=find_packages(),
+        install_requires=[
+            'selenium>=3.9',
+            'colour-runner>=0.0.5',
+            'randomuser>=0.3.0'
+            ]
+        )
---
+++ packages/webdriver-test-tools/webdriver_test_tools-0.2.2.tar.gz/webdriver_test_tools-0.2.2/webdriver_test_tools/__init__.py
@@ -0,0 +1,4 @@
+from .webdriver.support import expected_conditions as EC
+from . import data
+from . import test
+from . import test_module
