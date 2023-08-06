bf83a60477795bedd1d317cf2ebb1c1380e51605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:18 +0000

    Add package django-kongoauth/0.1.3.dev2/django-kongoauth-0.1.3.dev2.tar.gz

---
+++ packages/django-kongoauth/django-kongoauth-0.1.3.dev2.tar.gz/django-kongoauth-0.1.3.dev2/kongoauth/authentication.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import AnonymousUser
+from rest_framework import authentication
+from django.conf import settings
+from .user import TransientUser
+from .helpers import get_redis
+import json
+
+
+class KongOAuthAuthentication(authentication.BaseAuthentication):
+
+    def authenticate(self, request):
+        """
+        Uses Kong's OAuth headers to create a a 'Transit' User
+        note: this may need to be X_ (not HTTP_X_..) in real life:
+        """
+        anonymous = request.META.get('HTTP_X_ANONYMOUS_CONSUMER', None)
+        if not anonymous:
+            user_id = request.META.get('HTTP_X_AUTHENTICATED-USERID', None)
+            user = self.get_user(user_id)
+            return user, None
+        else:
+            return AnonymousUser(), False
+
+    @staticmethod
+    def get_user(user_id):
+        """
+        Gets User information from a permission redis instance
+        """
+        redis_key = getattr(settings, 'AUTH_REDIS_KEY',
+                            'authorization.{}').format(user_id)
+        redis = get_redis()
+        user_data = json.loads(redis.get(redis_key))
+        return TransientUser.from_redis_data(user_data)
+
