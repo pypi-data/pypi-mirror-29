eba45f37d6c41bdc7baae701d65532fc39e22b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:36 +0000

    Add package upt-cpan/0.1.1/upt_cpan-0.1.1-py3-none-any.whl

---
+++ packages/upt-cpan/upt_cpan-0.1.1-py3-none-any.whl/upt_cpan/upt_cpan.py
@@ -0,0 +1,47 @@
+# Copyright 2018      Cyril Roelandt
+#
+# Licensed under the 3-clause BSD license. See the LICENSE file.
+import requests
+import upt
+
+
+class CPANPackage(upt.Package):
+    pass
+
+
+class CPANFrontend(upt.Frontend):
+    name = 'cpan'
+
+    def _get_homepage(self, json_resources):
+        homepage = json_resources.get('homepage')
+        if homepage is None:
+            for kind in ('bugtracker', 'repository'):
+                d = json_resources.get(kind, {})
+                homepage = d.get('web')
+                if homepage is not None:
+                    break
+        return homepage
+
+    def parse(self, pkg_name):
+        release_name = pkg_name.replace('::', '-')
+        url = f'https://fastapi.metacpan.org/v1/release/{release_name}'
+        r = requests.get(url)
+        if not r.ok:
+            raise upt.InvalidPackageNameError(self.name, pkg_name)
+        json = r.json()
+
+        metadata = json.get('metadata', {})
+        version = metadata.get('version', '')
+        homepage = self._get_homepage(json.get('resources', {}))
+
+        try:
+            download_urls = [json['download_url']]
+        except KeyError:
+            download_urls = []
+
+        pkg_args = {
+            'summary': metadata.get('abstract', ''),
+            'homepage': homepage,
+            'download_urls': download_urls,
+        }
+        return CPANPackage(pkg_name, version, **pkg_args)
