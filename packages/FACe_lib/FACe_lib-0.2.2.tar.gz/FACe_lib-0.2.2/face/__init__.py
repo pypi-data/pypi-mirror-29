2bc84ff87c53d3c74c4ed9032795be0963f4ec0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:29 +0000

    Add package FACe_lib/0.2.2/FACe_lib-0.2.2.tar.gz

---
+++ packages/FACe_lib/FACe_lib-0.2.2.tar.gz/FACe_lib-0.2.2/face/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import (division, absolute_import, print_function, unicode_literals)
+
+__author__ = 'XaviTorello'
+__version__ = '0.2.2'
+
+from .face import FACe
