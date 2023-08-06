7cd1f69e662b888384e6afd5c08ed1c217e1ec34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:03 +0000

    Add package stimd/0.2/stimd-0.2.tar.gz

---
+++ packages/stimd/stimd-0.2.tar.gz/stimd-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stimd',
+	packages=['stimd'],
+	author= 'Seth Hirsh',
+	author_email = 'hirshs@uw.edu',
+	version='0.2',
+	scripts=['STIMD.py','Decompose_MP_periodic_sym.py','__init__.py']
+)
