fb83a671d73aa34d37e38550742af128a546052f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:43 +0000

    Add package polyaxon-lib/0.0.4/polyaxon-lib-0.0.4.tar.gz

---
+++ packages/polyaxon-lib/polyaxon-lib-0.0.4.tar.gz/polyaxon-lib-0.0.4/examples/configs_examples/lenet.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+import polyaxon_lib as plx
+
+from polyaxon_lib.polyaxonfile import local_runner
+
+
+if __name__ == "__main__":
+    """Creates an experiment using Lenet network.
+
+    Links:
+        * http://yann.lecun.com/exdb/publis/pdf/lecun-01a.pdf
+    """
+    plx.datasets.mnist.prepare('../data/mnist')
+    local_runner.run('./yaml_configs/lenet.yml')
