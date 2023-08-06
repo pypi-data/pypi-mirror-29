f81d547cd6cdf17e309351b61186387cd3b0836b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:41 +0000

    Add package hs/0.0.4/hs-0.0.4-py3-none-any.whl

---
+++ packages/hs/hs-0.0.4-py3-none-any.whl/hydroserving/helpers/upload.py
@@ -0,0 +1,21 @@
+from hydroserving.httpclient.api import UploadMetadata
+from hydroserving.helpers.assembly import assemble_model
+from hydroserving.models.model_metadata import ModelMetadata
+import click
+
+
+def upload_model(model_api, source, model):
+    tar = assemble_model(model)
+    model_metadata = ModelMetadata.from_folder_metadata(model)
+
+    click.echo("Uploading {} to {}".format(tar, model_api.remote_addr))
+
+    metadata = UploadMetadata(
+        model_name=model_metadata.model_name,
+        model_type=model_metadata.model_type,
+        target_source=source,
+        model_contract=model_metadata.model_contract.SerializeToString(),
+        description=model_metadata.description
+    )
+
+    return model_api.upload(tar, metadata)
