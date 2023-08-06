14db75b3e55a1cb9e0889a79685f46dbf2b87c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:12 +0000

    Add package amimageresizer/0.1/amimageresizer-0.1.tar.gz

---
+++ packages/amimageresizer/amimageresizer-0.1.tar.gz/amimageresizer-0.1/README.md
@@ -0,0 +1,3 @@
+# Image resizer
+>> please check example file
+<img src='https://i.imgur.com/7szzCwj.png'></img>
---
+++ packages/amimageresizer/amimageresizer-0.1.tar.gz/amimageresizer-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+name = 'amimageresizer',
+version = '0.1',
+packages = find_packages(),
+author = 'Ammad Khalid',
+author_email = 'ammadkhalid12@gmail.com',
+install_requires = ['python-resize-image', 'requests'],
+url = 'https://github.com/Ammadkhalid/image-resizer'
+)
