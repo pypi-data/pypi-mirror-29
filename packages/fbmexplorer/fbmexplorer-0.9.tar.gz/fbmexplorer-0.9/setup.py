8c7c3ec1689e078bf6785ddcabf2cd663156d03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:52 +0000

    Add package fbmexplorer/0.9/fbmexplorer-0.9.tar.gz

---
+++ packages/fbmexplorer/fbmexplorer-0.9.tar.gz/fbmexplorer-0.9/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+from io import open
+
+setup(name='fbmexplorer',
+      version='0.9',
+      description='Parser to extract Facebook Messenger Data, and explorer it on d3.js tool.',
+      long_description=open('README.rst').read(),
+      url='https://github.com/adurivault/FBMessage',
+      keywords=['facebook', 'chat', 'messenger', 'history', "visualization", "dataviz"],
+      author="adurivault",
+      author_email= "a.durivault@gmail.com",
+      license='MIT',
+      install_requires=[line.strip()
+                      for line in open("requirements.txt", "r",
+                                       encoding="utf-8").readlines()],
+      scripts=['bin/fbm-parse'],
+      packages=['fbmexplorer'],
+      zip_safe=False)
