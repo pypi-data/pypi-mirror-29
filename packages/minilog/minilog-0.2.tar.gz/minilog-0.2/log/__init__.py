078a18c529ef6653c1d54665f8426d852a80cb46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:56 +0000

    Add package minilog/0.2/minilog-0.2.tar.gz

---
+++ packages/minilog/minilog-0.2.tar.gz/minilog-0.2/log/__init__.py
@@ -0,0 +1,10 @@
+from .logger import *  # pylint: disable=wildcard-import
+from .helpers import init, silence
+
+d = debug
+i = info
+w = warn = warning
+e = error
+
+__project__ = 'minilog'
+__version__ = '0.2'
