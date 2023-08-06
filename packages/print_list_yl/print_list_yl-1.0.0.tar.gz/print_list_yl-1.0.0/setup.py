b2f1cb3b64fd6530bf96241719604e89249c5a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:22 +0000

    Add package print_list_yl/1.0.0/print_list_yl-1.0.0.tar.gz

---
+++ packages/print_list_yl/print_list_yl-1.0.0.tar.gz/print_list_yl-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='print_list_yl',
+    version='1.0.0',
+    py_modules = ['print_list_yl'],
+    author='yuanlei',
+    author_email='252948868@qq.com',
+    url='',
+    description='a simple printer of lists',
+)
