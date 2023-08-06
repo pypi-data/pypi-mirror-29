1fdb5bc0b0635c92439df3d8df65b3c8e4bf1296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:09 +0000

    Add package pyansys/0.25.0/pyansys-0.25.0-cp27-cp27m-win_amd64.whl

---
+++ packages/pyansys/pyansys-0.25.0-cp27-cp27m-win_amd64.whl/pyansys/__init__.py
@@ -0,0 +1,7 @@
+from pyansys._version import __version__
+
+from pyansys.archive_reader import *
+from pyansys.binary_reader import *
+from pyansys.binary_reader import FullReader
+from pyansys.cellquality import *
+from pyansys.ansys import ANSYS
