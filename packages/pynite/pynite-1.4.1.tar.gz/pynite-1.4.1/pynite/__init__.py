36e570dc748edafb999ae5355555fa942724ecf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:35 +0000

    Add package pynite/1.4.1/pynite-1.4.1.tar.gz

---
+++ packages/pynite/pynite-1.4.1.tar.gz/pynite-1.4.1/pynite/__init__.py
@@ -0,0 +1,15 @@
+from .core import Client, Player
+from .errors import *
+from .utils import API
+
+
+############
+# METADATA #
+############
+
+
+__version__ = 'v.1.4.1'
+__title__ = 'pynite'
+__license__ = 'MIT'
+__author__ = 'SharpBit & Umbresp'
+__github__ = 'https://github.com/cree-py/pynite'
---
+++ packages/pynite/pynite-1.4.1.tar.gz/pynite-1.4.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pynite',
+    version='1.4.1',
+    description='An async Python API wrapper for the Fortnite API',
+    long_description='Powered by fortnitetracker.com. Async python 3.5+',
+    url='https://github.com/cree-py/pynite',
+    author='SharpBit & Umbresp',
+    author_email='creepy.org3301@gmail.com',
+    license='MIT',
+    keywords=['fortnite, pynite, api-wrapper, async'],
+    packages=find_packages(),
+    install_requires=['aiohttp', 'python-box']
+)
