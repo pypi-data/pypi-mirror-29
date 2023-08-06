c77d356dde90eff42d810b5078d66c6ed4537d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:37 +0000

    Add package scrobbless/0.3.0/scrobbless-0.3.0.tar.gz

---
+++ packages/scrobbless/scrobbless-0.3.0.tar.gz/scrobbless-0.3.0/scrobbless/scrobbless_tester.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import time
+
+from scrobbless import Scrobbless
+
+s = Scrobbless()
+
+token = s.get_token()
+
+with open(".scrobbless", 'r+') as f:
+  session_key = f.readline()
+
+  if not session_key:
+    s.auth_user(token)
+
+    while not session_key:
+      session = s.get_session(token)
+
+      session_key = session['session']['key']
+
+    f.write(session_key)
+
+# s.update_np('Saves The Day', 'All-Star Me', 'Through Being Cool', session_key)
+
+s.scrobble('Saves The Day', 'You Vandal', 'Through Being Cool', str(int(time.time())), session_key)
