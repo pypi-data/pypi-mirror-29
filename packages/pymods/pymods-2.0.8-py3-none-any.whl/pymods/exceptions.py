605fc92f546a996c7a4a154f847dd05bec443c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:34 +0000

    Add package pymods/2.0.8/pymods-2.0.8-py3-none-any.whl

---
+++ packages/pymods/pymods-2.0.8-py3-none-any.whl/pymods/exceptions.py
@@ -0,0 +1,12 @@
+class PymodsException(Exception):
+    pass
+
+
+class NameSpaceInvalid(PymodsException):
+    def __str__(self):
+        return "Root is in an unexpected namespace"
+
+
+class ElementNotFound(PymodsException):
+    def __str__(self):
+        return "Record does not contain the specified element"
