77bf387862d43cb2f8af3a2569ffa4d4c2f7212e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:52 +0000

    Add package cargo-api/0.1.1/cargo-api-0.1.1.tar.gz

---
+++ packages/cargo-api/cargo-api-0.1.1.tar.gz/cargo-api-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='cargo-api',
+      version='0.1.1',
+      description='API utility functions to handle API responses',
+      url='https://gitlab.cargo.one/abc',
+      author='Mike Roetgers',
+      author_email='mr@cargo.one',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
