e6f0e508987975da12f243c18045119feb79a2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:02 +0000

    Add package protest/0.5.2/protest-0.5.2.tar.gz

---
+++ packages/protest/protest-0.5.2.tar.gz/protest-0.5.2/README.rst
@@ -0,0 +1,22 @@
+protest.py
+==========
+
+python module by aarón montoya-moraga, first released on march 2017.
+it is a part of "its-ok", his graduate thesis at nyu itp.
+
+installation
+------------
+
+install with pip:
+
+  pip install protest
+
+special thanks
+--------------
+
+allison parrish, andrew lazarow, justin peake, lauren mccarthy, sam lavigne, tigran paravyan, wipawe sirikolkarn, yuan gao, yuli cai, zoe bachman
+
+license
+-------
+
+mit
---
+++ packages/protest/protest-0.5.2.tar.gz/protest-0.5.2/setup.py
@@ -0,0 +1,34 @@
+# protest.py
+# by aaron montoya-moraga
+# march 2017
+
+# to distribute, on terminal do
+# python setup.py sdist
+
+# from distutils.core import setup
+
+from setuptools import *
+from codecs import open
+from os import path
+
+
+# taken from https://tom-christie.github.io/articles/pypi/
+here = path.abspath(path.dirname(__file__))
+
+# taken from https://tom-christie.github.io/articles/pypi/
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='protest',
+    version='0.5.2',
+    url='https://github.com/montoyamoraga/protestpy',
+    author='aaron montoya-moraga',
+    description='automatic generation of protesting material',
+    long_description=long_description,
+    license='MIT',
+    packages= find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['Pillow', 'videogrep', 'selenium<3.0.0', 'youtube_dl', 'chromedriver'],
+    package_data={'protest': ['*.ttf']}
+)
