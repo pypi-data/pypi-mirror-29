b795a01fb06ba16efe729e884fe9b4ef4b779904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:26 +0000

    Add package fxcmpy/1.0.4/fxcmpy-1.0.4.tar.gz

---
+++ packages/fxcmpy/fxcmpy-1.0.4.tar.gz/fxcmpy-1.0.4/fxcmpy/__init__.py
@@ -0,0 +1,11 @@
+from fxcmpy.fxcmpy import fxcmpy as fxcmpy  
+from fxcmpy.fxcmpy_open_position import fxcmpy_open_position 
+from .fxcmpy_closed_position import *
+from .fxcmpy_order import *
+from .fxcmpy_oco_order import *
+
+try:
+    with open('version', 'r') as f:
+        __version__ = f.read()
+except:
+    __version__ = 'NA'
