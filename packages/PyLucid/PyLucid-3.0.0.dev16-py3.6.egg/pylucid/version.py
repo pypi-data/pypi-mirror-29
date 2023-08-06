3c65e4cbaac80f6fff16d41d0b8519a92df452b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:14 +0000

    Add package PyLucid/3.0.0.dev16/PyLucid-3.0.0.dev16-py3.6.egg

---
+++ packages/PyLucid/PyLucid-3.0.0.dev16-py3.6.egg/pylucid/version.py
@@ -0,0 +1,11 @@
+"""
+    PyLucid
+    ~~~~~~~
+
+    :copyleft: 2009-2018 by the PyLucid team, see AUTHORS for more details.
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+# https://pypi.org/project/PyLucid/#history
+
+__version__ = "3.0.0.dev16"
