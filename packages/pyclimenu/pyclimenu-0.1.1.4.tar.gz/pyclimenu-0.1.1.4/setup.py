540c4a6caf259861461cca544c40b70ee5a70cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package pyclimenu/0.1.1.4/pyclimenu-0.1.1.4.tar.gz

---
+++ packages/pyclimenu/pyclimenu-0.1.1.4.tar.gz/pyclimenu-0.1.1.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name = 'pyclimenu',
+  packages = ['pyclimenu'],
+  version = '0.1.1.4',
+  description = 'The easy way to create command line menus',
+  author = 'Panagiotis Simakis',
+  author_email = 'simakis@autistici.org',
+  url = 'https://github.com/sp1thas/pyclimenu',
+  download_url = 'https://github.com/sp1thas/pyclimenu/archive/master.zip',
+  keywords = ['menu', 'command-line'],
+  include_package_data=True,
+  classifiers = [],
+)
