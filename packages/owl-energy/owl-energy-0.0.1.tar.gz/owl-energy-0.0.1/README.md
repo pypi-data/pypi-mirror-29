82efbcd539cac5e067629017f80cdb83a1620f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:59 +0000

    Add package owl-energy/0.0.1/owl-energy-0.0.1.tar.gz

---
+++ packages/owl-energy/owl-energy-0.0.1.tar.gz/owl-energy-0.0.1/README.md
@@ -0,0 +1,1 @@
+## OWL Energy by STRETCH
---
+++ packages/owl-energy/owl-energy-0.0.1.tar.gz/owl-energy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='owl-energy',
+    version='0.0.1',
+    packages=['owlenergy'],
+    url='https://stretchproject.org',
+    license='',
+    author='Ciaran McCormick',
+    author_email='ciaran.mccormick@open.ac.uk',
+    description='A python package for OWL energy monitor using sockets'
+)
