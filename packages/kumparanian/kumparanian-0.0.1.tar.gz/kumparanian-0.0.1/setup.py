f4b5fd572accbd0eb71954d8f48f791fbecb2f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:51 +0000

    Add package kumparanian/0.0.1/kumparanian-0.0.1.tar.gz

---
+++ packages/kumparanian/kumparanian-0.0.1.tar.gz/kumparanian-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Package details
+setup(
+    name="kumparanian",
+    version="0.0.1",
+    author="Bayu Aldi Yansyah",
+    author_email="bayualdiyansyah@gmail.com",
+    url="https://github.com/kumparan/kumparanian",
+    description="Kumparanian CLI",
+    long_description=open("README.rst", "r").read(),
+    license="BSD 3-Clause License",
+    packages=["kumparanian"],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
+    ]
+)
