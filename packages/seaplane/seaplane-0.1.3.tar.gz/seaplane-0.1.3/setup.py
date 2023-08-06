e0ff403452a24881bf76a563ba95c34f0e44c668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:29 +0000

    Add package seaplane/0.1.3/seaplane-0.1.3.tar.gz

---
+++ packages/seaplane/seaplane-0.1.3.tar.gz/seaplane-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='seaplane',
+      version='0.1.3',
+      description='A simple tool to help get local Kubernetes projects up and running.',
+      url='https://github.com/hwstovall/seaplane',
+      download_url='https://github.com/hwstovall/seaplane/archive/0.1.3.tar.gz',
+      author='hwstovall',
+      author_email='hwstovall@gmail.com',
+      license='MIT',
+      install_requires=[
+          'click',
+          'colorama',
+      ],
+      include_package_data=True,
+      packages=['seaplane'],
+      entry_points='''
+            [console_scripts]
+            seaplane=seaplane.script:seaplane
+      ''',
+      keywords=['kubernetes', 'minikube', 'development', 'docker', 'helm'],
+      zip_safe=False)
