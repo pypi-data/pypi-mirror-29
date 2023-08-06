6dd22a12f64218b224ccb34453b0ca2e5c065483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:04 +0000

    Add package mt-940/4.11.0/mt_940-4.11.0-py2.py3-none-any.whl

---
+++ packages/mt-940/mt_940-4.11.0-py2.py3-none-any.whl/mt940/parser.py
@@ -0,0 +1,54 @@
+# vim: fileencoding=utf-8:
+'''
+
+Format
+---------------------
+
+Sources:
+
+.. _Swift for corporates: http://www.sepaforcorporates.com/\
+    swift-for-corporates/account-statement-mt940-file-format-overview/
+.. _Rabobank MT940: https://www.rabobank.nl/images/\
+    formaatbeschrijving_swift_bt940s_1_0_nl_rib_29539296.pdf
+
+ - `Swift for corporates`_
+ - `Rabobank MT940`_
+
+::
+
+    [] = optional
+    ! = fixed length
+    a = Text
+    x = Alphanumeric, seems more like text actually. Can include special
+        characters (slashes) and whitespace as well as letters and numbers
+    d = Numeric separated by decimal (usually comma)
+    c = Code list value
+    n = Numeric
+'''
+
+import os
+
+import mt940
+
+
+def parse(src):
+    '''
+    Parses mt940 data and returns transactions object
+
+    :param src: file handler to read, filename to read or raw data as string
+    :return: Collection of transactions
+    :rtype: Transactions
+    '''
+
+    if hasattr(src, 'read'):  # pragma: no branch
+        data = src.read()
+    elif os.path.isfile(src):
+        src = open(src)
+        data = src.read()
+    else:
+        data = src
+
+    transactions = mt940.models.Transactions()
+    transactions.parse(data)
+
+    return transactions
