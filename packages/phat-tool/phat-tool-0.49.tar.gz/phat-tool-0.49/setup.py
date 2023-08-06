29b5e57f930c285e89a0f452bff4885b536295fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:40 +0000

    Add package phat-tool/0.49/phat-tool-0.49.tar.gz

---
+++ packages/phat-tool/phat-tool-0.49.tar.gz/phat-tool-0.49/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from settings import VERSION
+from main.main import AutoTest
+
+plugins = AutoTest.load_plugins()
+
+pkgs = ['plugins.' + pl.__name__.split('.')[0] for pl in plugins] + ['main', 'common']
+
+setup(name='phat-tool',
+      version=VERSION,
+      description='Pluggable HTTP Auto Testing',
+      author='Daniel Franca',
+      url='https://github.com/danielfranca/phat',
+      license='MIT',
+      packages=pkgs,
+      install_requires=['python-Levenshtein==0.11.2', 'beautifulsoup4==4.4.1', 'requests==2.4.0', 'paramiko==1.15.2', 'jsonpath-rw==1.4.0', 'selenium==2.53.2', 'responses==0.5.1'],
+      scripts=['phat', 'settings.py'],)
