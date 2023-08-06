0dcb396b67c220a5c16362da4fd985e5d75c8a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:00 +0000

    Add package lavalink/2.0.2.8/lavalink-2.0.2.8.tar.gz

---
+++ packages/lavalink/lavalink-2.0.2.8.tar.gz/lavalink-2.0.2.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='lavalink',
+  packages=['lavalink'],
+  version='2.0.2.8',
+  description='A lavalink interface built for discord.py',
+  author='Luke, William',
+  author_email='dev@crimsonxv.pro',
+  url='https://github.com/Devoxin/Lavalink.py',
+  download_url='https://github.com/Devoxin/Lavalink.py/archive/2.0.2.8.tar.gz',
+  keywords=['lavalink'],
+)
