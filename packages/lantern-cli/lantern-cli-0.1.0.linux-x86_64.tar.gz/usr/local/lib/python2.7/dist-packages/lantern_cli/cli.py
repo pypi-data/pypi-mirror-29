0dda922703da13fe31d144928778de3bcd03b287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:48 +0000

    Add package lantern-cli/0.1.0/lantern-cli-0.1.0.linux-x86_64.tar.gz

---
+++ packages/lantern-cli/lantern-cli-0.1.0.linux-x86_64.tar.gz/usr/local/lib/python2.7/dist-packages/lantern_cli/cli.py
@@ -0,0 +1,30 @@
+import click
+import templates
+
+
+@click.group()
+def cli():
+	"""
+    \b
+             * * * * * * * * * * * * * * *
+             *                           * 
+             *    Lantern Engine CLI     * 
+             *                           *
+             * * * * * * * * * * * * * * *
+	\b
+    This tool is for internal user
+    in Lantern.tech.
+    \b
+    "Happy Coding!"
+    """
+	pass
+
+
+@cli.command()
+def startapp():
+	"""Create a new MicroService project \n\n
+	   Check the generated README file (project root) for docker instructions. \n
+	   Usage:
+	   \n\t\t lantern-cli startapp [APP_NAME]
+	"""
+	templates.startapp()
