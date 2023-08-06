28041242ad005e8247c376fe3339f4da52eec1cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:30 +0000

    Add package heyshaun/0.1.3/heyshaun-0.1.3.tar.gz

---
+++ packages/heyshaun/heyshaun-0.1.3.tar.gz/heyshaun-0.1.3/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+    name='heyshaun',    # This is the name of your PyPI-package.
+    version='0.1.3',                          # Update the version number for new releases
+    scripts=['shaunak1']                  # The name of your scipt, and also the command you'll be using for calling it
+)
