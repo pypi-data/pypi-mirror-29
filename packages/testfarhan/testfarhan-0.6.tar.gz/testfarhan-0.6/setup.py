e6e5a46e44acbc0ce9d6581f296017b0385fb6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:50 +0000

    Add package testfarhan/0.6/testfarhan-0.6.tar.gz

---
+++ packages/testfarhan/testfarhan-0.6.tar.gz/testfarhan-0.6/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='testfarhan',
+    packages=['testfarhan'],  # this must be the same as the name above
+    version='0.6',
+    description='Testfarhan test library',
+    author='Farhan Attamimi',
+    author_email='farhan@sourcegraph.com',
+    url='https://github.com/attfarhan/testfarhan',  # use the URL to the github repo
+    # I'll explain this in a second
+    download_url='https://github.com/attfarhan/testfarhan/archive/0.6.tar.gz',
+    keywords=['test', 'python', 'farhan'],  # arbitrary keywords
+    classifiers=[],
+)
