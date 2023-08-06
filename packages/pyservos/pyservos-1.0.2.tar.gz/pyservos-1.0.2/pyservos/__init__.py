6c9398cc8c2c3d45f581421347a55d55f07385d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:15 +0000

    Add package pyservos/1.0.2/pyservos-1.0.2.tar.gz

---
+++ packages/pyservos/pyservos-1.0.2.tar.gz/pyservos-1.0.2/pyservos/__init__.py
@@ -0,0 +1,11 @@
+from .servoserial import ServoSerial
+from .packet import Packet
+from .ax12 import AX12
+from .xl320 import XL320
+from .packet import Packet
+
+
+__copyright__ = 'Copyright (c) 2016 Kevin Walchko'
+__license__ = 'MIT'
+__author__ = 'Kevin J. Walchko'
+__version__ = '1.0.2'
