94e5c4fd587131c29920f830b5cb3db21a64afdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:22 +0000

    Add package poetry/0.2.0/poetry-0.2.0-py3-none-any.whl

---
+++ packages/poetry/poetry-0.2.0-py3-none-any.whl/poetry/console/commands/__init__.py
@@ -0,0 +1,7 @@
+from .about import AboutCommand
+from .add import AddCommand
+from .install import InstallCommand
+from .lock import LockCommand
+from .new import NewCommand
+from .remove import RemoveCommand
+from .update import UpdateCommand
