89471c33b254173b5dc48549f90efae492ee176c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:34 +0000

    Add package Converto/1.0.1/Converto-1.0.1.tar.gz

---
+++ packages/Converto/Converto-1.0.1.tar.gz/Converto-1.0.1/converto/converto.py
@@ -0,0 +1,119 @@
+from __future__ import print_function
+from .file_finder import FileFinder
+from menu import Menu
+from ffmpy import FFmpeg
+from os import path, remove
+
+
+class Converto:
+    menu_options = list()
+    files = list()
+    config = None
+    main_menu = None
+    satisfied_menu = None
+    chosen_option = None
+    user_input = None
+
+    def __init__(self, config, user_input):
+        self.config = config
+        self.input = user_input
+
+    def show_main_menu(self):
+        menu_options = self._generate_menu_options()
+        self.main_menu = Menu(
+            title="Choose which command you would like to run")
+        self.main_menu.set_options(menu_options)
+        self.main_menu.open()
+
+    def _convert(self):
+        for f in self.files:
+            for i, command in enumerate(self.chosen_option.commands):
+                input_file, was_intermediate = self._get_file_to_operate_on(
+                    f, i)
+                ff = self._build_ffmpeg_command(input_file, i)
+                ff.run()
+                if was_intermediate:
+                    remove(previous_intermediary_file)
+                if self.chosen_option.multi_input:
+                    return
+
+    def _get_file_to_operate_on(self, input_file, i):
+        previous_intermediary_file = self._get_output_filename(input_file, (i - 1))
+        if path.isfile(previous_intermediary_file):
+            return previous_intermediary_file, True
+        else:
+            return input_file, False
+
+    def _build_ffmpeg_command(self, input_file, command_index):
+        command = self.chosen_option.commands[command_index]
+        output_filename = self._get_output_filename(input_file, command_index)
+        if self.chosen_option.multi_input:
+            inputs = {}
+            for f in self.files:
+                inputs[f] = command.input_options
+        else:
+            inputs = {input_file: command.input_options}
+        outputs = {output_filename: command.output_options}
+
+        ff = FFmpeg(
+            inputs=inputs,
+            outputs=outputs
+        )
+        return ff
+
+    def _get_output_filename(self, filename, command_index):
+        command = self.chosen_option.commands[command_index]
+        if self._is_intermediary(command_index):
+            output_filename = "{0}_INTERMEDIARY_{1}.{2}".format(
+                filename[:-4], command_index, command.output_extension)
+        elif command.output_filename_format:
+            output_filename = command.output_filename_format.format(
+                input_filename=filename, extension=command.output_extension)
+        else:
+            output_filename = "{0}.{1}".format(
+                filename[:-4], command.output_extension)
+        return output_filename
+
+    def _is_intermediary(self, index):
+        return index != len(self.chosen_option.commands) - 1
+
+    def _generate_menu_options(self):
+        menu_options = list()
+        for i, opt in enumerate(self.config.options):
+            menu_options.append(
+                (opt.name, lambda i=i: self._find_files_and_convert(i)))
+        return menu_options
+
+    def _find_files_and_convert(self, option_index):
+        self.main_menu.close()
+        self.chosen_option = self.config.options[option_index]
+        print("You chose \"{0}\"\n".format(self.chosen_option.name))
+        file_finder = FileFinder(self.chosen_option, self.user_input)
+        file_finder.get_user_input()
+        self.files = file_finder.files
+        self._ask_if_commands_look_right()
+
+    def _ask_if_commands_look_right(self):
+        command_list = ""
+        for command in self.chosen_option.commands:
+            ff = FFmpeg(
+                inputs={"{input_filename}": command.input_options},
+                outputs={"{output_filename}": command.output_options}
+            )
+            command_list = command_list + ff.cmd + "\n"
+        menu_title = "About to process these files: {0}\nUsing these commands:\n{1}\n\nDoes this look right?".format(
+            self.files, command_list)
+        self.satisfied_menu = Menu(title=menu_title)
+        self.satisfied_menu.set_options([
+            ("Yes, process the files now",
+             lambda: self._handle_user_satisfaction_choice(True)),
+            ("No, cancel", lambda: self._handle_user_satisfaction_choice(False)),
+        ])
+        self.satisfied_menu.open()
+
+    def _handle_user_satisfaction_choice(self, satisified):
+        self.satisfied_menu.close()
+        if satisified:
+            self._convert()
+        else:
+            exit(0)
---
+++ packages/Converto/Converto-1.0.1.tar.gz/Converto-1.0.1/converto/file_finder.py
@@ -0,0 +1,86 @@
+from __future__ import print_function
+from builtins import input
+from os import path, walk
+from menu import Menu
+
+
+class FileFinder:
+    files = list()
+    extension_to_find = None
+    option = None
+    user_satisfied = False
+    satisfied_menu = None
+    _user_input = None
+
+    def __init__(self, option, user_input=None):
+        self.option = option
+        self._user_input = user_input
+
+    def get_user_input(self):
+        while not self.user_satisfied:
+            if self._user_input:
+                user_input = self._user_input
+                self._user_input = None
+            else:
+                user_input = self._ask_user_for_files()
+            user_input = self._clean_user_input(user_input)
+            if path.isfile(user_input):
+                if self._file_is_right_extension(user_input):
+                    self.files.append(user_input)
+            elif path.isdir(user_input):
+                self.files = self._find_files_from_directory(user_input)
+            else:
+                print("The path entered: {0} is not valid.\nRetrying...".format(user_input))
+                continue
+            if len(self.files) == 0:
+                print("Selection must contain at least one file of extension types: {0}".format(self._get_exts_list()))
+                continue
+            self._ask_if_user_is_satisfied_with_files()
+
+    def _clean_user_input(self, user_input):
+        return user_input.replace(r"\ ", " ")
+
+    def _ask_user_for_files(self):
+        print("Valid input types: {0}\n".format(self._get_exts_list()))
+        return input("Enter the path of your file or directory of files: ").strip()
+
+    def _find_files_from_directory(self, user_input):
+        found_files = list()
+        for root, dirs, files in walk(user_input):
+            for f in files:
+                if self._file_is_right_extension(f):
+                    found_files.append(path.join(root,f))
+        return found_files
+
+    def _ask_if_user_is_satisfied_with_files(self):
+        satisfied = True
+        self.satisfied_menu = Menu(title = "Files to Process:\n{0}\nDoes this look right?".format(self._get_file_list()))
+        self.satisfied_menu.set_options([
+            ("Yes, continue", lambda: self._handle_user_satisfaction_choice(True)),
+            ("No, let me try again.", lambda: self._handle_user_satisfaction_choice(False)),
+            ("Add another file.", lambda: self._handle_user_satisfaction_choice(True, add_another=True)),
+        ])
+        self.satisfied_menu.open()
+
+    def _handle_user_satisfaction_choice(self, satisfied, add_another=False):
+        self.satisfied_menu.close()
+        if add_another:
+            return
+        if not satisfied:
+            self.files = list()
+        self.user_satisfied = satisfied
+        
+    def _get_file_list(self):
+        file_list_string = ''
+        for x in self.files:
+            file_list_string = file_list_string + x + "\n"
+        return file_list_string 
+
+    def _get_exts_list(self):
+        extension_list_string = ''
+        for x in self.option.valid_input_exts:
+            extension_list_string = extension_list_string + x + ", "
+        return extension_list_string[:-2]
+
+    def _file_is_right_extension(self, file_path):
+        return file_path.endswith(tuple(self.option.valid_input_exts))
