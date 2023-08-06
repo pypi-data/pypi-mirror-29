ee45948f702d38e069ca90c2eb6a42431aa898c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:08 +0000

    Add package tensorci-client/0.0.12/tensorci_client-0.0.12-py2.py3-none-any.whl

---
+++ packages/tensorci-client/tensorci_client-0.0.12-py2.py3-none-any.whl/tensorci_client/definitions.py
@@ -0,0 +1,11 @@
+# Headers
+socket_client_id_header = 'TensorCI-Client-ID'
+socket_client_secret_header = 'TensorCI-Client-Secret'
+train_cluster_header = 'TensorCI-Train-Secret'
+
+# Queues
+graph_update_queue = 'graph-update-queue'
+
+# Connection Info
+host_domain = 'tensorci.com'
+default_socket_port = 80
