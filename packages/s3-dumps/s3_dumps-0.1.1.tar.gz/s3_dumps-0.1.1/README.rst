7203de3653d171efe30c5da6372eceb63f227b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:19 +0000

    Add package s3-dumps/0.1.1/s3_dumps-0.1.1.tar.gz

---
+++ packages/s3-dumps/s3_dumps-0.1.1.tar.gz/s3_dumps-0.1.1/README.rst
@@ -0,0 +1,18 @@
+S3 Dumps
+==========
+
+.. image:: https://travis-ci.org/rakeshgunduka/s3_dumps.png?branch=master
+    :alt: Build Status
+    :target: https://travis-ci.org/rakeshgunduka/s3_dumps
+
+
+This is fork of S3 Backups <https://github.com/epicserve/s3-backups>. 
+Rewritten code to work with boto3 and upload to Amazon s3 and DigitalOcean Spaces. 
+
+Installation
+------------
+
+To install s3-dumps::
+
+    $ sudo pip install s3_dumps
+
---
+++ packages/s3-dumps/s3_dumps-0.1.1.tar.gz/s3_dumps-0.1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='s3_dumps',
+    packages=['s3_dumps'],
+    version=__import__('s3_dumps').__version__,
+    description='A package for backup DB and store in s3',
+    author='Reck31',
+    author_email='rakesh.gunduka@gmail.com',
+    url='https://github.com/rakeshgunduka/s3_dumps',
+    include_package_data=True,
+    license=open('LICENSE.txt').read(),
+    install_requires=[
+        "boto3"
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    entry_points={
+        'console_scripts': [
+            'postgres_to_s3=s3_dumps.postgres_to_s3:main',
+            'redis_to_s3=s3_dumps.redis_to_s3:main'
+        ],
+    },
+    scripts=[
+        's3_dumps/postgres_to_s3.py',
+        's3_dumps/redis_to_s3.py',
+        's3_dumps/connect.py',
+        's3_dumps/utils.py'
+    ],
+    keywords = ['s3', 'backup', 'postgres', 'redis', 'digitaloceanspaces', 'aws'],
+    zip_safe=False
+)
