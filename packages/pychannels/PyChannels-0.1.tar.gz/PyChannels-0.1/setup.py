1da342fb8f21df8f2af6fa924313c0fb1244b1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:04 +0000

    Add package pychannels/0.1/PyChannels-0.1.tar.gz

---
+++ packages/pychannels/PyChannels-0.1.tar.gz/PyChannels-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PyChannels',
+    version='0.1',
+    packages=['pychannels',],
+    license='The MIT License',
+    long_description=open('README.md').read(),
+)
