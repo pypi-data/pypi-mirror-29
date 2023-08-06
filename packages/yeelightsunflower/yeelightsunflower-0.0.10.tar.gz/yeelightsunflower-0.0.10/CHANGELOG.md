0692176ae0ba73e6a86cf2758e5db4f2abada866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:06 +0000

    Add package yeelightsunflower/0.0.10/yeelightsunflower-0.0.10.tar.gz

---
+++ packages/yeelightsunflower/yeelightsunflower-0.0.10.tar.gz/yeelightsunflower-0.0.10/CHANGELOG.md
@@ -0,0 +1,36 @@
+Changelog for yeelightsunflower
+===============================
+
+0.0.10 (2018-03-09)
+------------------
+
+- Correct string check and missing CHANGELOG (again :)
+        
+
+0.0.9 (2018-03-09)
+------------------
+
+- Handle and log invalid responses when getting lights (avoid IndexError)
+        
+
+0.0.8 (2017-02-25)
+------------------
+
+- Correct problem with CHANGELOG missing in PyPi package (update MANIFEST.in) 
+
+
+0.0.7 (2017-02-25)
+------------------
+
+- Try to reconnect socket when there's a send/receive error 
+- Set availability of lights to False if hub is unavailable
+
+
+0.0.6 (2017-02-23)
+------------------
+
+- Add time check to use cached data instead of fetching new data via TCP. This is useful since multiple bulb objects will get the hub's data in short succession, and all should just use the same values.
+
+0.0.5 (2017-02-17)
+------------------
+- Make library thread safe with threading.Lock
---
+++ packages/yeelightsunflower/yeelightsunflower-0.0.10.tar.gz/yeelightsunflower-0.0.10/setup.py
@@ -0,0 +1,43 @@
+import io
+from distutils.core import setup
+from yeelightsunflower import __version__, __author__, __license__, __title__
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+long_description = read('README.md', 'CHANGELOG.md')
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    'License :: OSI Approved :: MIT License',
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6"
+]
+setup(
+    name=__title__,
+    version=__version__,
+    license=__license__,
+    author=__author__,
+    packages=['yeelightsunflower'],
+    classifiers=classifiers,
+    url='https://github.com/lindsaymarkward/python-yeelight-sunflower',
+    author_email='lindsay.ward@jcu.edu.au',
+    maintainer='Lindsay Ward',
+    maintainer_email='lindsay.ward@jcu.edu.au',
+    description='Python package for interacting with Yeelight Sunflower bulbs',
+    long_description=long_description
+)
