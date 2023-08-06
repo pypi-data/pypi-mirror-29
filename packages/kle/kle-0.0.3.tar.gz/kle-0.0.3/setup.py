5dc7da1864ccd97f28112a17deb4d52a48d10530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:26 +0000

    Add package kle/0.0.3/kle-0.0.3.tar.gz

---
+++ packages/kle/kle-0.0.3.tar.gz/kle-0.0.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2018 jem@seethis.link
+# Licensed under the MIT license (http://opensource.org/licenses/MIT)
+
+from setuptools import setup
+import os
+
+try: # python3
+    fields = {}
+    with open(os.path.join("kle", "version.py")) as f:
+        exec(f.read(), fields)
+    __version__ = fields['__version__']
+except: # python2
+    execfile(os.path.join("kle", "version.py"))
+
+setup(
+    name = 'kle',
+    version = __version__,
+    description = "Python library for parsing keyboard layout files from "
+        "keyboard-layout-editor.com.",
+    url = "http://github.com/ahtn/python-kle",
+    author = "jem",
+    author_email = "jem@seethis.link",
+    license = 'MIT',
+    packages = ['kle'],
+    install_requires = [ 'six' ],
+    keywords = ['keyboard', 'keyboard-layout-editor'],
+    zip_safe = False
+)
