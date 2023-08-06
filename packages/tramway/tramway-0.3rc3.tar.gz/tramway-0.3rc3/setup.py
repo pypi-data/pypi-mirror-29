1a90dcad2a9043a02f02647b9e06d7df9a71f5ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:55 +0000

    Add package tramway/0.3rc3/tramway-0.3rc3.tar.gz

---
+++ packages/tramway/tramway-0.3rc3.tar.gz/tramway-0.3rc3/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+## see https://packaging.python.org/distributing/#setup-py
+
+from setuptools import setup
+from codecs import open
+from os import path
+
+# requirements moved to requirements.txt
+install_requires = []
+extras_require = {}
+setup_requires = ['pytest-runner']
+tests_require = ['pytest']
+
+
+pwd = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(pwd, 'README.rst'), encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(
+	name = 'tramway',
+	version = '0.3-rc3',
+	description = 'TRamWAy',
+	long_description = long_description,
+	url = 'https://github.com/DecBayComp/TRamWAy',
+	author = 'François Laurent',
+	author_email = 'francois.laurent@pasteur.fr',
+	license = 'CeCILL v2.1',
+	classifiers = [
+		'Intended Audience :: Science/Research',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+	],
+	keywords = '',
+	package_dir = {'tramway': 'tramway', 'tramway.demo': 'examples'},
+	packages = ['tramway', 'tramway.demo'],
+	scripts = ['scripts/tramway', 'scripts/tramway-demo'],
+	install_requires = install_requires,
+	extras_require = extras_require,
+	setup_requires = setup_requires,
+	tests_require = tests_require,
+	package_data = {},
+)
