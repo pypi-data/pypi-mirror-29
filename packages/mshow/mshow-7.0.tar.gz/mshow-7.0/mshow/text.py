ea4bc226549f5d9b3bbeb72d2a3c73042544c8ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:14 +0000

    Add package mshow/7.0/mshow-7.0.tar.gz

---
+++ packages/mshow/mshow-7.0.tar.gz/mshow-7.0/mshow/text.py
@@ -0,0 +1,6 @@
+
+def show():
+    print("Bye Guys !!\n Have a nice time.\n")
+
+def info():
+    print("This is test.py in 'mshow' library.")
---
+++ packages/mshow/mshow-7.0.tar.gz/mshow-7.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='mshow',
+      version = '7.0',
+      __version__='7.0',
+      description='Sample Message Showing Library',
+      url='https://github.com/nishantsinghdev/mshow',
+      author='Nishant Singh',
+      author_email='nishant@example.com',
+      license='MIT',
+      packages=['mshow'],
+      zip_safe=False)
