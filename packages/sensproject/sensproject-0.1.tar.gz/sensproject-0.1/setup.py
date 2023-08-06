022a007fdf8bdac630c031d4650f913de74631cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:54 +0000

    Add package sensproject/0.1/sensproject-0.1.tar.gz

---
+++ packages/sensproject/sensproject-0.1.tar.gz/sensproject-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+
+datadir = os.path.join('sensproject/skel')
+datafiles = [(d, [os.path.join(d,f) for f in files])
+    for d, folders, files in os.walk(datadir)]
+
+setup(
+  name = 'sensproject',
+  packages = ['sensproject'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A simple script to create new Senscape project',
+  author = 'Guillem Castro',
+  author_email = 'guillemcastro4@gmail.com',
+  entry_points={
+        'console_scripts': [
+            'sensproj = sensproject.projectcreator:main',
+        ],
+    },
+    data_files=datafiles
+)
