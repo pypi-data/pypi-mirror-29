2827c48198e70c980f996fd5a65a77ccb44b9eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:58 +0000

    Add package pyinvoke/1.0.0/pyinvoke-1.0.0.tar.gz

---
+++ packages/pyinvoke/pyinvoke-1.0.0.tar.gz/pyinvoke-1.0.0/setup.py
@@ -0,0 +1,31 @@
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+def readme():
+  if os.path.isfile('README.md') and any('dist' in x for x in sys.argv[1:]):
+    if os.system('pandoc -s README.md -o README.rst') != 0:
+      print('-----------------------------------------------------------------')
+      print('WARNING: README.rst could not be generated, pandoc command failed')
+      print('-----------------------------------------------------------------')
+      if sys.stdout.isatty():
+        input("Enter to continue... ")
+    else:
+      print("Generated README.rst with Pandoc")
+
+  if os.path.isfile('README.rst'):
+    with open('README.rst') as fp:
+      return fp.read()
+  return ''
+
+setup(
+  name='pyinvoke',
+  version='1.0.0',
+  license='MIT',
+  long_description=readme(),
+  url='https://github.com/NiklasRosenstein/py-invoke',
+  author='Niklas Rosenstein',
+  author_email='rosensteinniklas@gmail.com',
+  packages=find_packages()
+)
