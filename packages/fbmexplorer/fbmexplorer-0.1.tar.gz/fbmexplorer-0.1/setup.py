b9fb287daca45381ed74cfa46ca37f7447ff4d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:00 +0000

    Add package fbmexplorer/0.1/fbmexplorer-0.1.tar.gz

---
+++ packages/fbmexplorer/fbmexplorer-0.1.tar.gz/fbmexplorer-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fbmexplorer',
+      version='0.1',
+      description='Parser to extract Facebook Messenger Data, and explorer it on d3.js tool.',
+      url='https://github.com/adurivault/FBMessage',
+      author='adurivault, MathReynaud',
+      author_email='a.durivault@gmail.com, mathild.reynaud@gmail.com',
+      license='MIT',
+      packages=['fbmexplorer'],
+      zip_safe=False)
