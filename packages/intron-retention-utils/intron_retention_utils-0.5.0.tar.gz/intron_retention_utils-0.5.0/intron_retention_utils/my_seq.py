6adcddc0bc2dfa2110165271597ac9aee8f34c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:04 +0000

    Add package intron-retention-utils/0.5.0/intron_retention_utils-0.5.0.tar.gz

---
+++ packages/intron-retention-utils/intron_retention_utils-0.5.0.tar.gz/intron_retention_utils-0.5.0/intron_retention_utils/my_seq.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+import sys, re
+import pysam
+
+def get_seq(reference, chr, start, end):
+
+    seq = ""
+    for item in pysam.faidx(reference, chr + ":" + str(start) + "-" + str(end)):
+        # if item[0] == ">": continue
+        seq = seq + item.rstrip('\n')
+    seq = seq.replace('>', '')
+    seq = seq.replace(chr + ":" + str(start) + "-" + str(end), '')
+
+    if re.search(r'[^ACGTNacgtn]', seq) is not None:
+        print >> sys.stderr, "The return value in get_seq function includes non-nucleotide characters:"
+        print >> sys.stderr, seq
+        sys.exit(1)
+
+
+    return seq
+
+
+def reverse_complement(seq):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A',
+                  'W': 'W', 'S': 'S', 'M': 'K', 'K': 'M', 'R': 'Y', 'Y': 'R',
+                  'B': 'V', 'V': 'B', 'D': 'H', 'H': 'D', 'N': 'N'}
+
+    return("".join(complement.get(base, base) for base in reversed(seq)))
+
+
