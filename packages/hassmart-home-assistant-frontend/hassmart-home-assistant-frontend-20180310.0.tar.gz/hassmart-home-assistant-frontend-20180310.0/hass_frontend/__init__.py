7b7bf4278894b8942a479f4235e22b66a39fdf3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:48 +0000

    Add package hassmart-home-assistant-frontend/20180310.0/hassmart-home-assistant-frontend-20180310.0.tar.gz

---
+++ packages/hassmart-home-assistant-frontend/hassmart-home-assistant-frontend-20180310.0.tar.gz/hassmart-home-assistant-frontend-20180310.0/hass_frontend/__init__.py
@@ -0,0 +1,50 @@
+"""Frontend for Home Assistant."""
+import os
+from user_agents import parse
+
+FAMILY_MIN_VERSION = {
+    'Chrome': 54,          # Object.values
+    'Chrome Mobile': 54,
+    'Firefox': 47,         # Object.values
+    'Firefox Mobile': 47,
+    'Opera': 41,           # Object.values
+    'Edge': 14,            # Array.prototype.includes added in 14
+    'Safari': 10,          # Many features not supported by 9
+}
+
+
+def where():
+    """Return path to the frontend."""
+    return os.path.dirname(__file__)
+
+
+def version(useragent):
+    """Get the version for given user agent."""
+    useragent = parse(useragent)
+
+    # on iOS every browser is a Safari which we support from version 11.
+    if useragent.os.family == 'iOS':
+        # Was >= 10, temp setting it to 12 to work around issue #11387
+        return useragent.os.version[0] >= 12
+
+    version = FAMILY_MIN_VERSION.get(useragent.browser.family)
+    return version and useragent.browser.version[0] >= version
+VERSION = 'f1736024cc0c8ea1bcbe15b3bba6514f512c7b7a'
+CREATED_AT = 1521125797
+FINGERPRINTS = {
+    "config": "cd2deeed71a5edfc9608dfc6e4be34b5",
+    "dev-event": "7c1f35322b34c176e16f68729b98ae3b",
+    "dev-info": "dd10e4fa4b424625c23442568dfca574",
+    "dev-mqtt": "84c050acc4975584b7dd88242cea02aa",
+    "dev-service": "701e29bd4739269bd8262a6d0406a38e",
+    "dev-state": "3009a00cc61a60cbdae91289560e499a",
+    "dev-template": "bc28e6e7b0ba59cdcd23e248b37622a5",
+    "hassio": "421266c5f02dbccd48443907251fa741",
+    "history": "e0a69a68d79e2ace6261fd325ee9d780",
+    "iframe": "3f237a6addc81215890e8da2454da977",
+    "kiosk": "e47111bc9da3c12bc554ef415f6d912b",
+    "logbook": "e7a3f1d364999a7f8973e67ea8644fc8",
+    "mailbox": "aa5f9366332c7c20cd4760adeb60c767",
+    "map": "1a3c9adda58c22b1aa3ccb4cedae55e3",
+    "shopping-list": "793d9bdb323cb002bfa4e94214940f27"
+}
