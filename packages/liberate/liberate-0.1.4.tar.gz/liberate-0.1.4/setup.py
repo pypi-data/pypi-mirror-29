d3187007598a3f6a3e5bfb36a15cff1c0711525c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:16 +0000

    Add package liberate/0.1.4/liberate-0.1.4.tar.gz

---
+++ packages/liberate/liberate-0.1.4.tar.gz/liberate-0.1.4/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+from distutils.core import setup
+from pip.req import parse_requirements
+
+setup(
+  name='liberate',
+  version='0.1.4',
+  description='Video/Audio/Url liberation (to .ogg)',
+  author='Nichlas Severinsen',
+  author_email='ns@nsz.no',
+  url='https://notabug.org/necklace/liberate',
+  packages=['liberate'],
+  include_package_data=True,
+  package_data={'liberate': ['*.txt']},
+  scripts=['liberate/liberate'],
+  install_requires=parse_requirements('requirements.txt', session='hack')
+)
