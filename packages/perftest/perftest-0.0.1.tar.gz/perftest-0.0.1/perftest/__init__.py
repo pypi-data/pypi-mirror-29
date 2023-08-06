e84f46bef474e62ad1066e77f6a31b98bdc52a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:39 +0000

    Add package perftest/0.0.1/perftest-0.0.1.tar.gz

---
+++ packages/perftest/perftest-0.0.1.tar.gz/perftest-0.0.1/perftest/__init__.py
@@ -0,0 +1,20 @@
+'''
+perftest
+
+Profiling and performance tests, made like unit testing.
+'''
+
+__title__ = 'perftest'
+__version__ = '0.0.1'
+__all__ = ()
+__author__ = 'Johan Nestaas <johannestaas@gmail.com>'
+__license__ = 'GPLv3+'
+__copyright__ = 'Copyright 2018 Johan Nestaas'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
