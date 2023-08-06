f1ba1692eac0d3138bf786f7dffa756eea672993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:19 +0000

    Add package tibiawikisql/1.0.0a0/tibiawikisql-1.0.0a0.tar.gz

---
+++ packages/tibiawikisql/tibiawikisql-1.0.0a0.tar.gz/tibiawikisql-1.0.0a0/setup.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (3,6):
+    sys.exit('Sorry, Python < 3.6 is not supported')
+
+def get_version(package):
+    """Return package version as listed in `__version__` in `init.py`."""
+    init_py = open(os.path.join(package, '__main__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='tibiawikisql',
+    version=get_version("tibiawikisql"),
+    author='Galarzaa90',
+    author_email="allan.galarza@gmail.com",
+    url='https://github.com/Galarzaa90/tibiawiki-sql',
+    license='Apache 2.0',
+    install_requires=requirements,
+    description="Python script that generates a SQLite database from TibiaWiki articles",
+    long_description=readme,
+    packages=find_packages(),
+    include_package_data=True
+)
