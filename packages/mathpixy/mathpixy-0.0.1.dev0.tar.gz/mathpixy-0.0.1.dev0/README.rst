1286456d682c9cc07f5fe7f2dc7e9a1c76b4ce79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:19 +0000

    Add package mathpixy/0.0.1.dev0/mathpixy-0.0.1.dev0.tar.gz

---
+++ packages/mathpixy/mathpixy-0.0.1.dev0.tar.gz/mathpixy-0.0.1.dev0/README.rst
@@ -0,0 +1,7 @@
+mathpixy
+===============
+
+mathpixy is a gui, wrapper for the mathpix api for use on linux.
+
+Work in Progress!
+=================
---
+++ packages/mathpixy/mathpixy-0.0.1.dev0.tar.gz/mathpixy-0.0.1.dev0/mathpixy/mathpixy.py
@@ -0,0 +1,17 @@
+"""
+mathpixy.py
+~~~~~~~~
+
+stub code for mathpixy.py
+
+
+TODO: everything
+
+:copyright: @ 2018
+:author: elias julian marko garcia
+:license: MIT, see LICENSE
+"""
+
+
+def cli():
+    print("Coming soon!")
