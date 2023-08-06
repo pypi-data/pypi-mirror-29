021a47714d1d45482d9aaa362febfeb14fab4deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:50 +0000

    Add package pixiedust-node/0.2.5/pixiedust_node-0.2.5.tar.gz

---
+++ packages/pixiedust-node/pixiedust_node-0.2.5.tar.gz/pixiedust_node-0.2.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='pixiedust_node',
+      version='0.2.5',
+      description='Pixiedust extension for Node.js',
+      url='https://github.com/ibm-watson-data-lab/pixiedust_node',
+      install_requires=['pixiedust', 'pandas', 'ipython'],
+      package_data={
+        '': ['*.js','*.json']
+      },
+      author='David Taieb, Glynn Bird',
+      author_email='david_taieb@us.ibm.com, glynn.bird@gmail.com',
+      license='Apache 2.0',
+      packages=find_packages(),
+      include_package_data=False,
+      zip_safe=False)
