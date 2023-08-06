9daa7a7179230b311a7a9874e13104443c700133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:00 +0000

    Add package django-smart-selects/1.5.4/django-smart-selects-1.5.4.tar.gz

---
+++ packages/django-smart-selects/django-smart-selects-1.5.4.tar.gz/django-smart-selects-1.5.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as f:
+    long_desc = f.read()
+
+setup(name="django-smart-selects",
+      version="1.5.4",
+      description="Django application to handle chained model fields.",
+      long_description=long_desc,
+      author="Patrick Lauber",
+      author_email="digi@treepy.com",
+      url="https://github.com/digi604/django-smart-selects",
+      packages=find_packages(),
+      include_package_data=True,
+      tests_require=[
+          'flake8',
+      ],
+      )
