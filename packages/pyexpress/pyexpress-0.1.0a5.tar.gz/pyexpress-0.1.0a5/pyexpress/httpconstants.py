412060236e8bf59e062b33913de8268ed8f7fcfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:23 +0000

    Add package pyexpress/0.1.0a5/pyexpress-0.1.0a5.tar.gz

---
+++ packages/pyexpress/pyexpress-0.1.0a5.tar.gz/pyexpress-0.1.0a5/pyexpress/httpconstants.py
@@ -0,0 +1,8 @@
+class HttpConstants:
+    METHOD_GET = "GET"
+    METHOD_POST = "POST"
+    METHOD_PUT = "PUT"
+    METHOD_DELETE = "DELETE"
+
+    CONTENT_TYPE_JSON = "application/json"
+    CONTENT_TYPE_WWW_FORM = "application/x-www-form-urlencoded"
