ad1c24e38e2c497fc5a04c059994ab0ab1d8c0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:52 +0000

    Add package fxcmpy/1.1.5/fxcmpy-1.1.5.tar.gz

---
+++ packages/fxcmpy/fxcmpy-1.1.5.tar.gz/fxcmpy-1.1.5/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), 'fxcmpy'))
+import VERSION
+
+def readme():
+    try:
+        with open('README_short.rst', 'r') as f:
+            return f.read()
+    except:
+        return ''
+
+def read_version():
+    file_name = os.path.join('fxcmpy', 'VERSION')
+    with open(file_name, 'r') as f:
+        ret = f.read()
+    if ret[-1] == '\n':
+        ret = ret[:-1]
+    return ret
+
+setup(
+    name = 'fxcmpy',
+    packages = ['fxcmpy'], # this must be the same as the name above
+    version = VERSION.version,
+    description = 'A Python Wrapper Class for the RESTful API as provided by FXCM Forex Capital Markets Ltd.',
+    long_description = readme(),
+    author = 'The Python Quants GmbH',
+    author_email = 'admin@tpq.io',
+    license='BSD',
+    url = 'https://github.com/fxcm/fxcmpy', 
+    download_url = 'https://github.com/fxcm/fxcmpy', 
+    keywords = 'FXCM API Python Wrapper Finance Algo Trading',
+    install_requires=['pandas', 'socketIO_client', 'configparser', 'requests'], 
+    python_requires='>=3.4',
+    classifiers = ['Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6']
+)
