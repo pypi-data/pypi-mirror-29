fb1576917d047959e0fe2c730e254ff96947f2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:15 +0000

    Add package lifelogger/0.2.2/lifelogger-0.2.2.tar.gz

---
+++ packages/lifelogger/lifelogger-0.2.2.tar.gz/lifelogger-0.2.2/setup.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function
+
+from setuptools import setup, find_packages
+
+import lifelogger
+
+
+setup(
+    name='lifelogger',
+    version=lifelogger.__version__,
+    description=lifelogger.__doc__.strip(),
+    long_description=open('README.rst').read(),
+    url='https://github.com/adamchainz/lifelogger',
+    license=lifelogger.__license__,
+    author=lifelogger.__author__,
+    author_email='me@adamj.eu',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').readlines(),
+    entry_points={
+        'console_scripts': [
+            'lifelogger = lifelogger.__main__:main'
+        ],
+    },
+    package_data={
+        '': ['*.json'],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Office/Business :: Scheduling'
+    ],
+)
