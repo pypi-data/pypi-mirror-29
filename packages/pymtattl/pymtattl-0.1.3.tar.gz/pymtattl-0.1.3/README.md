7c470b8d5b1baba7e3a3f8a6927511f7dbac4b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:36 +0000

    Add package pymtattl/0.1.3/pymtattl-0.1.3.tar.gz

---
+++ packages/pymtattl/pymtattl-0.1.3.tar.gz/pymtattl-0.1.3/README.md
@@ -0,0 +1,115 @@
+# pymtattl
+
+## Introduction
+
+Download and store MTA Turnstile Data in text files or a SQLite database
+
+Automate downloading of turnstile entry/exit data from MTA. Allow to save data within requested time frame into a SQlite database and reshape data prior to 10/18/2014 into a more "relational" format. Considering to support more database types.
+
+MTA Turnstile Data: http://web.mta.info/developers/turnstile.html
+
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Download](#download)
+
+  * [Urls](#urls)
+
+  * [Text Files](#text-files)
+
+  * [SQlite Database](#sqlite-database)
+
+* [Caveats](#caveats)
+
+* [To-Do](#to-do)
+
+## Installation
+
+    pip install pymtattl
+
+## Requirements
+
+* Written for Python 3! Feel free to test and contribute using Python 2!
+* Requires bs4, Pandas
+
+## Download
+
+### Initate MTADownloader
+
+    from pymtattl.download import MTADownloader
+    mta_downlowder = MTADownloader(work_dir='Current', start=141018, end=None)
+
+Parameters
+
+* `work_dir`
+
+  - Type: String
+  - specify full folder directory to store downloaded data
+  - `'Current'`: default, uses current working directory `os.getcwd()`
+  - Or a specific valid directory
+
+* `start/end`
+
+  - Type: Integer or None
+  - define the date range to pull data files *(recommend testing with small date ranges, as downloading all files might be slow)*
+  - Example (yymmdd) for 2014-10-18: `141018`
+
+### Urls
+
+Get **urls** for data within date range and resource files (description, name key)
+
+    urls = mta_downloader.get_urls(keep_urls=True)
+
+* Set `keep_url = True` to save the returned urls in a text file *data_urls.txt*.
+
+* Returns list of url strings
+
+### Text Files
+
+Download requested data as separate **text files**
+
+    dat_dir = mta_downloader.download_to_txt()
+
+* Default create and store in a new folder **data** under working directory
+
+* `data_folder` *(optional)*: provide a custom folder name
+
+* Returns data folder directory
+
+### SQlite Database
+
+Reformat **All** local data or download **requested** data and store in a **SQLite database**
+
+    db_path = mta_downloader.download_to_db()
+
+* Create a SQlite database **data.db** under working directory and 3 tables
+
+  - **turnstile**: holds turnstile data
+  - **name_keys**: a matching table to lookup station name given remote and booth
+  - **file_names**: names of data files that are already in **turnstile** table
+
+* `data_path`:
+
+  - `''` | `None`: must run `download_to_txt()` first, then the function uses text files within data folder directory (instance attribute)
+  - Otherwise, specify a full data folder directory to search for existing data text files
+
+* If can not find local text files, choose to download text files first or directly store in the database (use with caution, could be very slow!)
+
+* Returns database directory
+
+## Caveats
+
+* Some know data issues and these rows will be skipped while building the database
+
+  - In Turnstile_120428.txt, one line with empty ('') exit number
+  - In Turnstile_120714.txt, first few lines could not be parsed
+
+## To-Do
+
+* De-cumulate entry and exit numbers, and store data within selected date range into a new table
+
+* A Summary table (ie. number of booth per station, average daily station entries/exits, ...) for "cleaned" data table above
+
+* More to come...
---
+++ packages/pymtattl/pymtattl-0.1.3.tar.gz/pymtattl-0.1.3/pymtattl/download.py
@@ -0,0 +1,189 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+import os
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+from .utils import createFolder, writeDB, getPubDate
+import sqlite3
+import pandas as pd
+
+
+class MTADownloader:
+    """
+    Download mta turnstile data page
+    """
+    MAIN_URL = 'http://web.mta.info/developers/'
+    TURNSTILE_URL = 'http://web.mta.info/developers/turnstile.html'
+
+    def __init__(self, work_dir='Current',
+                 start=None, end=None):
+        # define a valid folder for download data
+        self.work_dir = createFolder(work_dir, branch=None)
+        assert (start is None or type(start) == int), \
+            'Start should be None or 6-digit integer (yymmdd)'
+        assert (end is None or type(end) == int), \
+            'End should be None or 6-digit integer (yymmdd)'
+        self.start = start
+        self.end = end
+        self.dat_dir = ''
+
+    def get_urls(self, keep_urls=False):
+        """
+        Get and save data urls
+        """
+        soup = BeautifulSoup(urlopen(self.__class__.TURNSTILE_URL), 'lxml')
+        urls = soup.findAll('a', href=True)
+        # data file urls
+        dat_urls = [self.__class__.MAIN_URL + u.get('href')
+                    for u in urls
+                    if u.get('href').startswith('data')]
+        if self.start is not None:
+            dat_urls = [u for u in dat_urls
+                        if getPubDate(u) >= self.start]
+        if self.end is not None:
+            dat_urls = [u for u in dat_urls
+                        if getPubDate(u) <= self.end]
+        if not dat_urls:
+            raise ValueError('Can not identify any data urls '
+                             'within specified timeframe, '
+                             'please check start and end date.')
+
+        # resource file urls (headers, lookup tables for station names)
+        res_urls = [self.__class__.MAIN_URL + u.get('href')
+                    for u in urls
+                    if u.get('href').startswith('resources')]
+
+        print("{} data files and {} resource files available for download."
+              .format(len(dat_urls), len(res_urls)))
+
+        urls = dat_urls + res_urls
+        if keep_urls:
+            file_path = os.path.join(self.work_dir, 'data_urls.txt')
+            with open(file_path, 'w+') as f:
+                f.write('\n'.join(urls))
+        return urls
+
+    def download_to_txt(self, data_folder='data'):
+        """
+        Download text files
+        """
+        dat_dir = createFolder(self.work_dir, data_folder)
+        urls = self.get_urls()
+
+        i = 0
+        for url in urls:
+            fpath = os.path.join(dat_dir, url.split('/')[-1])
+            if not os.path.exists(fpath):
+                try:
+                    data = urlopen(url).read()
+                except HTTPError as err:
+                    print(err.code, err.reason)
+                with open(fpath, 'wb+') as f:
+                    f.write(data)
+                i += 1
+                if i % 10 == 0:
+                    print("Wrote {} files...".format(i))
+        print("Finish adding {} text files to {}. (# of Files in Dir: {})"
+              .format(i, dat_dir, len(os.listdir(dat_dir))))
+        self.dat_dir = dat_dir
+        return dat_dir
+
+    def init_db(self):
+        """
+        Create database or update missing tables if missing
+        """
+        db_path = os.path.join(self.work_dir, 'data.db')
+        new = False
+        if not os.path.exists(db_path):
+            new = True
+        try:
+            conn = sqlite3.connect(db_path)
+        except Exception as e:
+            print(e)
+            raise
+        c = conn.cursor()
+        c.execute('CREATE TABLE IF NOT EXISTS turnstiles '
+                  '(booth text, remote text, scp text, date text, time text, '
+                  'desc text, entries integer, exits integer)')
+        c.execute('CREATE TABLE IF NOT EXISTS name_keys '
+                  '(remote text, booth text, station text, '
+                  'line text, devision text)')
+        c.execute('CREATE TABLE IF NOT EXISTS file_names (file text)')
+        conn.commit()
+        conn.close()
+        if new:
+            print('Created: {}.'.format(db_path))
+        return db_path
+
+    def download_to_db(self, data_path=''):
+        """
+        Download data and store in a sqlite database
+        """
+        local = True
+        if data_path != '' and data_path is not None:
+            self.dat_dir = str(data_path)
+        try:
+            urls = os.listdir(self.dat_dir)
+        except Exception as e:
+            print("Couldn't find local data folder.")
+            txt = input("Download text files first? (Y/N)").lower()
+            if txt == 'y':
+                self.download_to_txt()
+                urls = os.listdir(self.dat_dir)
+            elif txt == 'n':
+                print("Download from urls, might take some time...")
+                urls = self.get_urls()
+                local = False
+            else:
+                print("Not valid input.")
+                return
+
+        db_path = self.init_db()
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+
+        get_efiles = c.execute('SELECT file FROM file_names').fetchall()
+        exist_files = [i[0] for i in get_efiles]
+
+        i = 0
+        for url in urls:
+            fname = url.split('/')[-1]
+            if fname not in exist_files and fname.startswith('turnstile'):
+                if local:
+                    with open(os.path.join(self.dat_dir, url), 'r') as f:
+                        data = f.read().split('\n')
+                else:
+                    data = urlopen(url).read().decode('utf-8').split('\n')
+                try:
+                    c = writeDB(fname, data, c)
+                except Exception as e:
+                    conn.close()
+                    print(e)
+                    raise
+                c.execute('INSERT INTO file_names VALUES (?)', (fname,))
+                i += 1
+
+            elif fname not in exist_files and fname.endswith('xls'):
+                if local:
+                    url = os.path.join(self.dat_dir, url)
+                try:
+                    res_data = pd.read_excel(url, header=0)
+                    res_data.columns = ['remote', 'booth', 'station',
+                                        'line', 'division']
+                    res_data.to_sql('name_keys', con=conn, if_exists="replace",
+                                    index=False)
+                except Exception as e:
+                    conn.close()
+                    print(e)
+                    raise
+                c.execute('INSERT INTO file_names VALUES (?)', (fname,))
+                i += 1
+
+            if i > 0 and i % 5 == 0:
+                conn.commit()
+                print("Adding {} files to the database...".format(i))
+        conn.commit()
+        conn.close()
+        print("Finish writing {} files to the database.".format(i))
+        return db_path
