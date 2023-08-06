7a4296dcc4224a75252fa241c596c3eca626678c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:28 +0000

    Add package pipsqueak/0.0.13/pipsqueak-0.0.13.tar.gz

---
+++ packages/pipsqueak/pipsqueak-0.0.13.tar.gz/pipsqueak-0.0.13/pipsqueak/pip/compat.py
@@ -0,0 +1,31 @@
+"""Stuff that differs in different Python versions and platform
+distributions."""
+from __future__ import absolute_import, division
+
+import os
+
+
+def samefile(file1, file2):
+    """Provide an alternative for os.path.samefile on Windows/Python2"""
+    if hasattr(os.path, 'samefile'):
+        return os.path.samefile(file1, file2)
+    else:
+        path1 = os.path.normcase(os.path.abspath(file1))
+        path2 = os.path.normcase(os.path.abspath(file2))
+        return path1 == path2
+
+
+def console_to_str(readline):
+    return readline
+
+
+def expanduser(path):
+    """
+    Expand ~ and ~user constructions.
+
+    Includes a workaround for http://bugs.python.org/issue14768
+    """
+    expanded = os.path.expanduser(path)
+    if path.startswith('~/') and expanded.startswith('//'):
+        expanded = expanded[1:]
+    return expanded
