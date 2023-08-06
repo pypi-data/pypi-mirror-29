425eda921caff230f3cb77b0aeedb134c5247de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:57 +0000

    Add package cython-npm/0.1.0/cython_npm-0.1.0-py3-none-any.whl

---
+++ packages/cython-npm/cython_npm-0.1.0-py3-none-any.whl/cython_npm/__init__.py
@@ -0,0 +1,7 @@
+from . import cythoncompile
+
+__author__ = 'Minh Tuan Nguyen'
+__version__ = '0.1.0'
+__copyright__ = "Copyright 2018, Minh Tuan Nguyen"
+__credits__ = ["Minh Tuan Nguyen", ]
+__license__ = "MIT"
