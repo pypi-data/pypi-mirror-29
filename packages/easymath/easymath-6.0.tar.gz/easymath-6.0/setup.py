36c2b4986616bf03abeabb26d9afd3740c1d4bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:26 +0000

    Add package easymath/6.0/easymath-6.0.tar.gz

---
+++ packages/easymath/easymath-6.0.tar.gz/easymath-6.0/setup.py
@@ -0,0 +1,36 @@
+
+
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+'''here = path.adspath(path.dirname(_file_)
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()'''
+ 
+
+setup(
+    name='easymath',
+    version='6.0',
+    description='A simple calculation package',
+    author='krushithreddy',
+    author_email='krushithreddy567@gmail.com',
+    url='https://github.com/krushithreddy/math',
+    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords = 'math',
+    
+)
