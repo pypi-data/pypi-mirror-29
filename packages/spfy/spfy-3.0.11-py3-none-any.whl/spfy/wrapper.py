9f18086551b17182e5f75cd5b8959676fd837a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:03 +0000

    Add package spfy/3.0.11/spfy-3.0.11-py3-none-any.whl

---
+++ packages/spfy/spfy-3.0.11-py3-none-any.whl/spfy/wrapper.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import asyncio
+
+from pony.orm import db_session
+
+import fire
+import kick
+
+from . import APP_NAME, config
+from .client import SpotifyClient
+from .mixins import PlayerMixin, RecommenderMixin
+
+
+class Spotify(SpotifyClient, PlayerMixin, RecommenderMixin):
+    """Spotify high-level wrapper."""
+    cli = False
+
+    def __init__(self, email=None, username=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.loop = asyncio.get_event_loop()
+        self.email = email or config.auth.email
+        self.username = username or config.auth.username
+        if self.cli and not self.is_authenticated:
+            self.authenticate(email=self.email, username=self.username)
+
+    def __dir__(self):
+        names = super().__dir__()
+        names = [name for name in names if not name.startswith('_') and name != 'user']
+        return names
+
+    def update_config(self, name='config'):
+        kick.update_config(APP_NAME.lower(), variant=name)
+
+
+def main():
+    """Main function."""
+
+    try:
+        Spotify.cli = True
+        with db_session:
+            fire.Fire(Spotify)
+    except KeyboardInterrupt:
+        print('Quitting')
+
+
+if __name__ == '__main__':
+    main()
