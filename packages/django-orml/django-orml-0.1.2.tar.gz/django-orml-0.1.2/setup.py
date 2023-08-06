17f3d5b71bbfc68df024add7bca0750aadc011b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:03 +0000

    Add package django-orml/0.1.2/django-orml-0.1.2.tar.gz

---
+++ packages/django-orml/django-orml-0.1.2.tar.gz/django-orml-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'django-orml',
+  packages = ['orml'],
+  version = '0.1.2',
+  description = 'Django Object Relational Mapping Language',
+  author = 'Matt Roberts',
+  author_email = 'contact@maleero.com',
+  url = 'https://github.com/malero/django-orml',
+  download_url = 'https://github.com/malero/django-orml/archive/v0.1.1.tar.gz',
+  keywords = ['django', 'orm', 'ply'],
+  classifiers = [],
+  install_requires=['ply==3.10',],
+)
