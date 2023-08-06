e824df236e5aa070f4b7f2581c86de3cbbe3e890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:37 +0000

    Add package django-defaults/0.0.1/django-defaults-0.0.1.tar.gz

---
+++ packages/django-defaults/django-defaults-0.0.1.tar.gz/django-defaults-0.0.1/defaults/settings.py
@@ -0,0 +1,123 @@
+# Options specific to django-defaults
+
+DJANGO_ROOT = None
+ROUTES_MODULE = None
+SETTINGS_MODULE = None
+
+# Django settings
+
+DEBUG = None
+
+SECRET_KEY = None
+
+ALLOWED_HOSTS = []
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = None
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'defaults.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': None,
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_ROOT = None
+
+STATIC_URL = '/static/'
+
+
+def finalize(*, debug: bool = False):
+    """
+    Finalize settings at runtime.
+    """
+
+    global DEBUG, SECRET_KEY, ROOT_URLCONF, STATIC_ROOT
+
+    DEBUG = debug
+
+    if debug:
+        SECRET_KEY = 'POTATO'
+
+        ALLOWED_HOSTS.append('*')
+
+    ROOT_URLCONF = ROUTES_MODULE
+
+    DATABASES['default']['NAME'] = (DJANGO_ROOT / 'db.sqlite3').as_posix()
+
+    STATIC_ROOT = (DJANGO_ROOT / 'static').as_posix()
