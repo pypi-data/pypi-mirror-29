97fb941a107fe2c4520315f45003499dfc0bb035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:34 +0000

    Add package CloeePy-Boto/0.0.0/CloeePy-Boto-0.0.0.tar.gz

---
+++ packages/CloeePy-Boto/CloeePy-Boto-0.0.0.tar.gz/CloeePy-Boto-0.0.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='CloeePy-Boto',
+    version='0.0.0',
+    packages=['cloeepy_boto',],
+    package_data = {
+        'cloeepy_boto': ['data/*.yml'],
+    },
+    license='MIT',
+    description="Boto Plugin for CloeePy Framework",
+    long_description=open('README.md').read(),
+    install_requires=[
+        "boto3>=1,<2",
+        "CloeePy>=0",
+     ],
+     url = "https://github.com/cloeeai/CloeePy-Boto",
+     author = "Scott Crespo",
+     author_email = "sccrespo@gmail.com",
+     keywords = "mini framework cloee cloeepy boto boto3",
+     python_requires='~=3.3',
+)
