9c365f705eaf23ae8701831ef237941da8dcd86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:03 +0000

    Add package appknox/2.1.6/appknox-2.1.6-py2.py3-none-any.whl

---
+++ packages/appknox/appknox-2.1.6-py2.py3-none-any.whl/appknox/mapper.py
@@ -0,0 +1,50 @@
+# (c) 2017, XYSec Labs
+
+from collections import namedtuple
+
+
+def mapper(model: type, resource: dict) -> object:
+    """
+    Returns an object of type `model` from dictified JSON `resource`
+    """
+    attr = dict()
+    for field in model._fields:
+        if field == 'id':
+            attr[field] = resource['data']['id']
+        else:
+            attr[field] = resource['data']['attributes'][
+                field.replace('_', '-')]
+    return model(**attr)
+
+
+User = namedtuple(
+    'User',
+    ['id', 'email', 'first_name', 'lang', 'last_name', 'username']
+)
+
+Project = namedtuple(
+    'Project',
+    ['id', 'created_on', 'file_count', 'package_name', 'platform',
+     'updated_on']
+)
+
+File = namedtuple(
+    'File',
+    ['id', 'name', 'version', 'version_code', 'static_scan_progress']
+)
+
+Analysis = namedtuple(
+    'Analysis',
+    ['id', 'risk', 'status', 'cvss_base', 'findings', 'updated_on',
+     'vulnerability_id']
+)
+
+Vulnerability = namedtuple(
+    'Vulnerability',
+    ['name', 'description', 'intro', 'compliant', 'non_compliant', 'types']
+)
+
+PersonalToken = namedtuple(
+    'AccessToken',
+    ['name', 'key']
+)
