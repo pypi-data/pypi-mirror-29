a35237198fb9278e046fa064dd478d1d61dcf44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:56 +0000

    Add package linux_daemon/0.1/linux_daemon-0.1.tar.gz

---
+++ packages/linux_daemon/linux_daemon-0.1.tar.gz/linux_daemon-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name = 'linux_daemon',
+	version = '0.1',
+	scripts = ['linux_daemon'],
+	author = 'Victor Claessen',
+	author_email = 'victor@victorclaessen.nl',
+	url = 'http://www.victorclaessen.nl'
+)
